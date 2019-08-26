d65afb7107bcb30fd28dee27539c4c6768ab28f6
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-08-26 09:10:21 +0300

    Cleaned the code

--- container-images/creative/figma/figma-font-bindings/native/src/lib.rs
+++ container-images/creative/figma/figma-font-bindings/native/src/lib.rs
@@ -1,13 +1,8 @@
 #[macro_use]
 extern crate neon;
 
-use neon::prelude::*;
-
-mod fonts;
 mod async_font;
-mod types;
 
 register_module!(mut cx, {
     cx.export_function("getFonts", async_font::fonts_async)
-    // cx.export_function("getFontsSync", fonts::fonts)
 });
