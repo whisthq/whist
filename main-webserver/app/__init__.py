4ff1de377e912d1d6d17abf60ed10f7ff6f92288
Ming Ying <ming_ying@college.harvard.edu>
2019-11-24 18:54:05 -0500

    test

--- main-webserver/app/__init__.py
+++ main-webserver/app/__init__.py
@@ -1,18 +1,22 @@
 from .imports import *
 from .factory import *
+from celery.signals import worker_process_init
+
+@worker_process_init.connect
+def on_fork_close_session(**kwargs):
+    if db_session is not None:
+        db_session.close()
+        db_engine.dispose()
 
 def make_celery(app_name = __name__):
     backend = os.getenv('REDIS_URL')
     broker = os.getenv('REDIS_URL')
     return Celery(app_name, backend=backend, broker=broker)
 
-def dispose_engine(engine):
-    engine.dispose()
 
 celery = make_celery()
 engine = db.create_engine(
     os.getenv('DATABASE_URL'), echo=True, pool_pre_ping = True)
 conn = engine.connect()
-register_after_fork(engine, dispose_engine)
 app = create_app(celery = celery)
 CORS(app)
