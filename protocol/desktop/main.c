317a827e843ee714f6c9c5ade46845d73432b158
Ming Ying <ming_ying@college.harvard.edu>
2019-12-27 19:12:59 -0500

    multi threaded sending

--- protocol/desktop/main.c
+++ protocol/desktop/main.c
@@ -14,6 +14,8 @@
 #include <windows.h>
 
 #include "../include/fractal.h"
+
+#define SDL_MAIN_HANDLED
 #include "../include/SDL2/SDL.h"
 #include "../include/SDL2/SDL_thread.h"
 
@@ -21,15 +23,26 @@
 
 #define BUFLEN 1000
  
-static int32_t SendStream(void *opaque) {
-    printf("1");
+static int32_t SendStream1(void *opaque) {
     struct context context = *(struct context *) opaque;
     int slen = sizeof(context.addr);
 
     // Once again, the payload is irrelevant. Feel free to send your VoIP
     // data in here.
-    char *message = "Hello from the client!";
-    printf("here!");
+    char *message = "Hello from the first stream!";
+    while(1) {
+        if (sendto(context.s, message, strlen(message), 0, (struct sockaddr*)(&context.addr), slen) < 0)
+            printf("Could not send packet\n");
+    }
+}
+
+static int32_t SendStream2(void *opaque) {
+    struct context context = *(struct context *) opaque;
+    int slen = sizeof(context.addr);
+
+    // Once again, the payload is irrelevant. Feel free to send your VoIP
+    // data in here.
+    char *message = "Hello from the second stream!";
     while(1) {
         if (sendto(context.s, message, strlen(message), 0, (struct sockaddr*)(&context.addr), slen) < 0)
             printf("Could not send packet\n");
@@ -48,13 +61,13 @@ int main(int argc, char* argv[])
     printf("Winsock initialized successfully.\n");
 
     struct sockaddr_in receive_address, send_address;
-    int slen=sizeof(send_address);
+    int recv_size, slen=sizeof(send_address);
+    char recv_buf[BUFLEN];
     SOCKET s;
-    HANDLE ThreadHandles[2];
  
     memset((char *) &receive_address, 0, sizeof(receive_address));
     receive_address.sin_family = AF_INET;
-    receive_address.sin_port = htons(PORT); // This is not really necessary, we can also use 0 (any port)
+    receive_address.sin_port = htons(PORT + 1);
     receive_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
     struct context SendContext = {0};
@@ -69,28 +82,22 @@ int main(int argc, char* argv[])
     ReceiveContext.s = SendContext.s;
     ReceiveContext.addr = receive_address;
 
-    SDL_Thread *send_thread = SDL_CreateThread(SendStream, "SendStream", &SendContext);
+    SDL_Thread *send_stream_1 = SDL_CreateThread(SendStream1, "SendStream1", &SendContext);
+    SDL_Thread *send_stream_2 = SDL_CreateThread(SendStream2, "SendStream2", &SendContext);
 
-    // ThreadHandles[0] = (HANDLE)_beginthreadex(NULL, 0, &SendStream, (void *) &SendContext, 0, NULL);
-    // ThreadHandles[1] = (HANDLE)_beginthreadex(NULL, 0, &ReceiveStream, (void *) &SendContext, 0, NULL);
-
-    char recv_buf[BUFLEN];
-    int recv_size;
     while (1)
     {
-        if ((recv_size = recvfrom(SendContext.s, &recv_buf, sizeof(recv_buf), 0, (struct sockaddr*)(&SendContext.addr), &slen)) < 0) {
+        if ((recv_size = recvfrom(ReceiveContext.s, &recv_buf, sizeof(recv_buf), 0, (struct sockaddr*)(&ReceiveContext.addr), &slen)) < 0) {
             printf("Packet not received \n");
         } else {
             printf("Received message: %s\n", recv_buf);
         }
     }
+
  
     // Actually, we never reach this point...
     closesocket(SendContext.s);
     closesocket(ReceiveContext.s);
-
-    CloseHandle(ThreadHandles[0]);
-    CloseHandle(ThreadHandles[0]);
 
     WSACleanup();
 
