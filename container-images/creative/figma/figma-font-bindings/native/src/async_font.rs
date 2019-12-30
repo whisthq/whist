07bb9f9cc0c6e084a80dc7d8c5cfd6cd8a314e78
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-12-30 16:29:16 +0300

    Fix the reading of incorrect font files

--- container-images/creative/figma/figma-font-bindings/native/src/async_font.rs
+++ container-images/creative/figma/figma-font-bindings/native/src/async_font.rs
@@ -1,6 +1,6 @@
 extern crate libfonthelper;
 
-use libfonthelper::{get_fonts, types::FontMap};
+use libfonthelper::{font::Font, Fonts};
 use neon::{prelude::*, task::Task};
 
 struct Worker {
@@ -8,46 +8,66 @@ struct Worker {
 }
 
 impl Task for Worker {
-    type Output = FontMap;
+    type Output = Vec<Font>;
     type Error = String;
     type JsEvent = JsObject;
 
-    fn perform(&self) -> Result<FontMap, String> {
-        match get_fonts(&self.dirs) {
+    fn perform(&self) -> Result<Vec<Font>, String> {
+        match Fonts::new(&self.dirs) {
             Err(err) => return Err(String::from(err.to_string())),
-            Ok(fonts) => return Ok(fonts),
+            Ok(fonts) => {
+                let mut map: Vec<Font> = Vec::new();
+                for font in fonts {
+                    map.push(font);
+                }
+
+                return Ok(map);
+            }
         };
     }
 
-    fn complete(self, mut cx: TaskContext, result: Result<FontMap, String>) -> JsResult<JsObject> {
+    fn complete(
+        self,
+        mut cx: TaskContext,
+        result: Result<Vec<Font>, String>,
+    ) -> JsResult<JsObject> {
         let js_fonts = JsObject::new(&mut cx);
 
-        for (path, font) in result.unwrap() {
-            let js_font_entries = JsArray::new(&mut cx, font.len() as u32);
+        match result {
+            Err(err) => println!("Cannot get fonts, error: {}", err),
+            Ok(fonts) => {
+                for font in fonts {
+                    if (font.path.eq("")) {
+                        continue;
+                    }
 
-            for (index, entry) in font.iter().enumerate() {
-                let js_font_entry = JsObject::new(&mut cx);
+                    let js_font_entries = JsArray::new(&mut cx, font.entries.len() as u32);
 
-                let id = cx.string(&entry.id);
-                let postscript = cx.string(&entry.postscript);
-                let family = cx.string(&entry.family);
-                let style = cx.string(&entry.style);
-                let weight = cx.number(i32::from(entry.weight));
-                let stretch = cx.number(i32::from(entry.stretch));
-                let italic = cx.boolean(entry.italic);
+                    for (index, entry) in font.entries.iter().enumerate() {
+                        let js_font_entry = JsObject::new(&mut cx);
 
-                js_font_entry.set(&mut cx, "id", id)?;
-                js_font_entry.set(&mut cx, "postscript", postscript)?;
-                js_font_entry.set(&mut cx, "family", family)?;
-                js_font_entry.set(&mut cx, "style", style)?;
-                js_font_entry.set(&mut cx, "weight", weight)?;
-                js_font_entry.set(&mut cx, "stretch", stretch)?;
-                js_font_entry.set(&mut cx, "italic", italic)?;
+                        let id = cx.string(&entry.id);
+                        let postscript = cx.string(&entry.postscript);
+                        let family = cx.string(&entry.family);
+                        let style = cx.string(&entry.style);
+                        let weight = cx.number(i32::from(entry.weight));
+                        let stretch = cx.number(i32::from(entry.stretch));
+                        let italic = cx.boolean(entry.italic);
 
-                js_font_entries.set(&mut cx, index.to_string().as_str(), js_font_entry)?;
+                        js_font_entry.set(&mut cx, "id", id)?;
+                        js_font_entry.set(&mut cx, "postscript", postscript)?;
+                        js_font_entry.set(&mut cx, "family", family)?;
+                        js_font_entry.set(&mut cx, "style", style)?;
+                        js_font_entry.set(&mut cx, "weight", weight)?;
+                        js_font_entry.set(&mut cx, "stretch", stretch)?;
+                        js_font_entry.set(&mut cx, "italic", italic)?;
+
+                        js_font_entries.set(&mut cx, index.to_string().as_str(), js_font_entry)?;
+                    }
+
+                    js_fonts.set(&mut cx, font.path.as_str(), js_font_entries)?;
+                }
             }
-
-            js_fonts.set(&mut cx, path.as_str(), js_font_entries)?;
         }
 
         Ok(js_fonts)
