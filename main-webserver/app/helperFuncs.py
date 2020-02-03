38219be0275e2b012e2bc05fa4e6392edd1a2e1b
Ming Ying <ming_ying@college.harvard.edu>
2020-02-03 13:08:48 -0500

    test

--- main-webserver/app/helperFuncs.py
+++ main-webserver/app/helperFuncs.py
@@ -94,7 +94,7 @@ def createVMParameters(vmName, nic_id, vm_size):
         vm_reference = {
             'publisher': 'MicrosoftWindowsDesktop',
             'offer': 'Windows-10',
-            'sku': 'RS3-Pro',
+            'sku': 'RS2-ProN',
             'version': 'latest'
         }
 
