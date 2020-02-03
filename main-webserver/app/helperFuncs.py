b49624f403d0d5f24a63c5b19aece9a33918b0f7
Ming Ying <ming_ying@college.harvard.edu>
2020-02-03 13:03:11 -0500

    sku

--- main-webserver/app/helperFuncs.py
+++ main-webserver/app/helperFuncs.py
@@ -94,7 +94,7 @@ def createVMParameters(vmName, nic_id, vm_size):
         vm_reference = {
             'publisher': 'MicrosoftWindowsDesktop',
             'offer': 'Windows-10',
-            'sku': 'RS3-ProN',
+            'sku': 'RS3-Pro',
             'version': 'latest'
         }
 
