24b845d1e36b08f708f9114132721e9e4ee5bcaa
Ming Ying <ming_ying@college.harvard.edu>
2020-02-12 10:35:55 -0500

    temp

--- protocol/include/videodecode.c
+++ protocol/include/videodecode.c
@@ -16,6 +16,7 @@
 
 
 extern volatile DecodeType type = DECODE_TYPE_NONE;
+static enum AVPixelFormat hw_pix_fmt;
 static AVBufferRef *hw_device_ctx = NULL;
 /// @brief creates decoder decoder
 /// @details creates FFmpeg decoder
@@ -117,6 +118,67 @@ video_decoder_t*create_video_decoder(int in_width, int in_height, int out_width,
     decoder->sw_frame = av_frame_alloc();
     decoder->hw_frame = av_frame_alloc();
     av_hwframe_get_buffer(decoder->context->hw_frames_ctx, decoder->hw_frame, 0);
+  } else {
+     // set the appropriate video decoder format based on PS
+      #if defined(_WIN32)
+        hw_pix_fmt = AV_PIX_FMT_D3D11VA_VLD;
+        char *device_type = "d3d11va";
+      #elif __APPLE__
+        hw_pix_fmt = AV_PIX_FMT_VIDEOTOOLBOX;
+        char *device_type = "videotoolbox";
+      #else // linux
+        hw_pix_fmt = AV_PIX_FMT_VAAPI;
+        char *device_type = "vaapi";
+      #endif
+
+      // get the appropriate hardware device
+      decoder->device_type = av_hwdevice_find_type_by_name(device_type);
+      if (decoder->device_type == AV_HWDEVICE_TYPE_NONE) {
+          while((decoder->device_type = av_hwdevice_iterate_types(decoder->device_type)) != AV_HWDEVICE_TYPE_NONE)
+              mprintf(" %s", av_hwdevice_get_type_name(decoder->device_type));
+          return decoder;
+      }
+
+      decoder->codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+
+      for (int i = 0;; i++) {
+          const AVCodecHWConfig *config = avcodec_get_hw_config(decoder->codec, i);
+          if (!config) {
+              mprintf("Decoder %s does not support device type %s.\n",
+                      decoder->codec->name, av_hwdevice_get_type_name(decoder->device_type));
+              return decoder;
+          }
+          if (config->methods & AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX &&
+              config->device_type == decoder->device_type) {
+              hw_pix_fmt = config->pix_fmt;
+              break;
+          }
+      }
+
+      if (!(decoder->context = avcodec_alloc_context3(decoder->codec))) {
+        mprintf("alloccontext3 failed w/ error code: %d\n", AVERROR(ENOMEM));
+        return decoder;
+      }
+
+        decoder->context->get_format = get_hw_format;
+        av_opt_set(decoder->context->priv_data, "async_depth", "1", 0);
+
+      if (hw_decoder_init(decoder->context, decoder->device_type) < 0) {
+        mprintf("hwdecoder_init failed\n");
+        return decoder;
+      }
+
+      if (avcodec_open2(decoder->context, decoder->codec, NULL) < 0) {
+          mprintf("Failed to open codec for stream\n");
+          return decoder;
+      }
+
+      if (!(decoder->hw_frame = av_frame_alloc()) || !(decoder->sw_frame = av_frame_alloc())) {
+          mprintf("Can not alloc frame\n");
+
+          av_frame_free(&decoder->hw_frame);
+          av_frame_free(&decoder->sw_frame);
+      }
   }
 
   return decoder;
@@ -166,5 +228,22 @@ void *video_decoder_decode(video_decoder_t*decoder, void *buffer, int buffer_siz
 
     av_free_packet(&decoder->packet);
     return NULL;
+  } else {
+    decoder->packet.data = buffer;
+    decoder->packet.size = buffer_size;
+
+    if(avcodec_send_packet(decoder->context, &decoder->packet) < 0) {
+      return NULL;
+    }
+
+    if(avcodec_receive_frame(decoder->context, decoder->hw_frame) < 0) {
+      return NULL;
+    }
+
+    if (decoder->hw_frame->format == hw_pix_fmt) {
+        if (av_hwframe_transfer_data(decoder->sw_frame, decoder->hw_frame, 0) < 0) {
+            return NULL;
+        }
+    }
   }
 }
