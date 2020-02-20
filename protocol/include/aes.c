25ee7709a539c58ff131545e01df5515eef8d13c
Nicholas Pipitone <npip99@gmail.com>
2020-02-20 02:40:28 -0500

    succint comments

--- protocol/include/aes.c
+++ protocol/include/aes.c
@@ -62,85 +62,60 @@ int encrypt( unsigned char* plaintext, int plaintext_len, unsigned char* key,
 
     int ciphertext_len;
 
-    /* Create and initialise the context */
+    // Create and initialise the context
     if( !(ctx = EVP_CIPHER_CTX_new()) )
         handleErrors();
 
-    /*
-     * Initialise the encryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
+    // Initialise the encryption operation.
     if( 1 != EVP_EncryptInit_ex( ctx, EVP_aes_128_cbc(), NULL, key, iv ) )
         handleErrors();
 
-    /*
-     * Provide the message to be encrypted, and obtain the encrypted output.
-     * EVP_EncryptUpdate can be called multiple times if necessary
-     */
+    // Encrypt
     if( 1 != EVP_EncryptUpdate( ctx, ciphertext, &len, plaintext, plaintext_len ) )
         handleErrors();
     ciphertext_len = len;
 
-    /*
-     * Finalise the encryption. Further ciphertext bytes may be written at
-     * this stage.
-     */
+    // Finish encryption (Might add a few bytes)
     if( 1 != EVP_EncryptFinal_ex( ctx, ciphertext + ciphertext_len, &len ) )
         handleErrors();
     ciphertext_len += len;
 
-    /* Clean up */
+    // Free the context
     EVP_CIPHER_CTX_free( ctx );
 
     return ciphertext_len;
 }
 
-EVP_CIPHER_CTX* ctx = NULL;
 
 int decrypt( unsigned char* ciphertext, int ciphertext_len, unsigned char* key,
              unsigned char* iv, unsigned char* plaintext )
 {
+    EVP_CIPHER_CTX* ctx;
 
     int len;
 
     int plaintext_len;
 
-    if (ctx == NULL)
-        /* Create and initialise the context */
-        if( !(ctx = EVP_CIPHER_CTX_new()) )
-            handleErrors();
+    // Create and initialize the context
+    if( !(ctx = EVP_CIPHER_CTX_new()) )
+        handleErrors();
 
-    /*
-     * Initialise the decryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
+    // Initialize decryption
     if( 1 != EVP_DecryptInit_ex( ctx, EVP_aes_128_cbc(), NULL, key, iv ) )
         handleErrors();
 
-    /*
-     * Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary.
-     */
+    // Decrypt
     if( 1 != EVP_DecryptUpdate( ctx, plaintext, &len, ciphertext, ciphertext_len ) )
         handleErrors();
     plaintext_len = len;
 
-    /*
-     * Finalise the decryption. Further plaintext bytes may be written at
-     * this stage.
-     */
+    // Finish decryption
     if( 1 != EVP_DecryptFinal_ex( ctx, plaintext + len, &len ) )
         handleErrors();
     plaintext_len += len;
 
-    /* Clean up */
-    //EVP_CIPHER_CTX_free( ctx );
+    // Free context
+    EVP_CIPHER_CTX_free( ctx );
 
     return plaintext_len;
 }
