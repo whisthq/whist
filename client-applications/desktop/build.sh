4d6c1561bf118c9d22daf98d00034d804504861a
philippemnoel <philippe_noel@college.harvard.edu>
2020-03-05 15:16:30 -0500

    hardened runtime reqs, better build bash script

--- client-applications/desktop/build.sh
+++ client-applications/desktop/build.sh
@@ -14,5 +14,14 @@ cd fractal-protocol
 mkdir desktop
 cd ..
 cp .fractal-protocol/desktop/desktop fractal-protocol/desktop
-codesign -s "Fractal Computers, Inc." fractal-protoco/desktop/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libavcodec.58.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libavdevice.58.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libavfilter.7.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libavformat.58.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libavutil.56.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libpostproc.55.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libswresample.3.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/ffmpeg/macos/libswscale.5.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/openssl/macos/libcrypto.1.1.dylib fractal-protocol/desktop
+codesign -s "Fractal Computers, Inc." fractal-protocol/desktop/desktop
 yarn package
