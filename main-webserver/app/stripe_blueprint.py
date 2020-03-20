ff4818b396eae5e8b60a5ab806eb64b3a0a30343
mingy98 <ming_ying@college.harvard.edu>
2020-03-20 01:18:57 -0400

    retrieve customer

--- main-webserver/app/stripe_blueprint.py
+++ main-webserver/app/stripe_blueprint.py
@@ -3,29 +3,43 @@ from .helperFuncs import *
 
 stripe_bp = Blueprint('stripe_bp', __name__)
 
-@stripe_bp.route('/stripe', methods = ['POST'])
-def charge():
+@stripe_bp.route('/stripe/<action>', methods = ['POST'])
+def stripe(action):
 	stripe.api_key = os.getenv('STRIPE_SECRET') 
-	body = request.get_json()
-	token = body['token']
-	email = body['email']
 
-	customers = fetchCustomers()
-	for customer in customers:
-		if email == customer['email']:
-			return jsonify({'status': 400}), 400
+	if action == 'charge':
+		body = request.get_json()
+
+		token = body['token']
+		email = body['email']
+
+		customers = fetchCustomers()
+		for customer in customers:
+			if email == customer['email']:
+				return jsonify({'status': 400}), 400
 
 
-	new_customer = stripe.Customer.create(
-	  email = email,
-	  source = token
-	)
+		new_customer = stripe.Customer.create(
+		  email = email,
+		  source = token
+		)
 
-	stripe.Subscription.create(
-	  customer = new_customer['id'],
-	  items = [{"plan": "plan_GwV769WQdZOUJR"}]
-	)
+		stripe.Subscription.create(
+		  customer = new_customer['id'],
+		  items = [{"plan": "plan_GwV769WQdZOUJR"}]
+		)
 
-	insertCustomer(email, new_customer['id'])
+		insertCustomer(email, new_customer['id'])
 
-	return jsonify({'status': 200}), 200
+		return jsonify({'status': 200}), 200
+
+	elif action == 'retrieve':
+		body = request.get_json()
+
+		email = body['email']
+		customers = fetchCustomers()
+		for customer in customers:
+			if email == customer['email']:
+				return jsonify({'status': 200}), 200
+
+		return jsonify({'status': 400}), 400
