6e31c65f4d10000c22356ba83d64934222a1728c
Roshan Padaki <roshanpadaki@gmail.com>
2020-03-22 06:43:45 +0000

    software encode fix

--- protocol/include/videoencode.c
+++ protocol/include/videoencode.c
@@ -90,6 +90,7 @@ encoder_t *create_video_encoder(int in_width, int in_height, int out_width, int 
 		av_hwframe_get_buffer(encoder->context->hw_frames_ctx, encoder->hw_frame, 0);
 	} else {
 		mprintf("Initializing software encoder\n");
+		enum AVPixelFormat out_format = AV_PIX_FMT_YUV420P;
 
    		encoder->codec = avcodec_find_encoder(AV_CODEC_ID_H264);
 
@@ -100,7 +101,7 @@ encoder_t *create_video_encoder(int in_width, int in_height, int out_width, int 
 		encoder->context->time_base.num   = 1;
 		encoder->context->time_base.den   = 30;
 		encoder->context->gop_size        = gop_size;
-		encoder->context->pix_fmt         = AV_PIX_FMT_YUV420P;
+		encoder->context->pix_fmt         = out_format;
 
 		av_opt_set(encoder->context->priv_data, "preset", "ultrafast", 0);
 		av_opt_set(encoder->context->priv_data, "tune", "zerolatency", 0);
@@ -108,20 +109,20 @@ encoder_t *create_video_encoder(int in_width, int in_height, int out_width, int 
 		avcodec_open2(encoder->context, encoder->codec, NULL);
 
 		encoder->sw_frame = (AVFrame *) av_frame_alloc();
-		encoder->sw_frame->format = AV_PIX_FMT_YUV420P;
+		encoder->sw_frame->format = out_format;
 		encoder->sw_frame->width  = out_width;
 		encoder->sw_frame->height = out_height;
 		encoder->sw_frame->pts = 0;
 
 	// set frame size and allocate memory for it
-		int frame_size = avpicture_get_size(AV_PIX_FMT_YUV420P, out_width, out_height);
+		int frame_size = avpicture_get_size(out_format, out_width, out_height);
 		encoder->frame_buffer = malloc(frame_size);
 
 	// fill picture with empty frame buffer
-		avpicture_fill((AVPicture *) encoder->sw_frame, (uint8_t *) encoder->frame_buffer, AV_PIX_FMT_YUV420P, out_width, out_height);
+		avpicture_fill((AVPicture *) encoder->sw_frame, (uint8_t *) encoder->frame_buffer, out_format, out_width, out_height);
 
 	// set sws context for color format conversion
-		encoder->sws = sws_getContext(in_width, in_height, AV_PIX_FMT_RGB32, out_width, out_height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, 0, 0, 0);
+		encoder->sws = sws_getContext(in_width, in_height, AV_PIX_FMT_RGB32, out_width, out_height, out_format, SWS_BICUBIC, 0, 0, 0);
 	}
 
   // return created encoder
@@ -156,24 +157,34 @@ void destroy_video_encoder(encoder_t *encoder) {
 /// @details encode a RGB frame into encoded format as YUV color
 void video_encoder_encode(encoder_t *encoder, void *rgb_pixels) {
     // define input data to encoder
-	memset( encoder->sw_frame->data, 0, sizeof( encoder->sw_frame->data ) );
-	memset( encoder->sw_frame->linesize, 0, sizeof( encoder->sw_frame->linesize ) );
-	encoder->sw_frame->data[0] = (uint8_t*)rgb_pixels;
-	encoder->sw_frame->linesize[0] = encoder->in_width * 4;
-	
-        
-  // init packet to prepare encoding
-	av_packet_unref(&encoder->packet);
+    if(encoder->type == NVENC_ENCODE) {
+		memset( encoder->sw_frame->data, 0, sizeof( encoder->sw_frame->data ) );
+		memset( encoder->sw_frame->linesize, 0, sizeof( encoder->sw_frame->linesize ) );
+		encoder->sw_frame->data[0] = (uint8_t*)rgb_pixels;
+		encoder->sw_frame->linesize[0] = encoder->in_width * 4;
+		
+	        
+	  // init packet to prepare encoding
+		av_packet_unref(&encoder->packet);
 
-	av_init_packet(&encoder->packet);
-	int success = 0; // boolean for success or failure of encoding
+		av_init_packet(&encoder->packet);
+		int success = 0; // boolean for success or failure of encoding
 
-  // attempt to encode the frame
-	if(encoder->type == NVENC_ENCODE) {
-    	av_hwframe_transfer_data(encoder->hw_frame, encoder->sw_frame, 0);
-
+		av_hwframe_transfer_data(encoder->hw_frame, encoder->sw_frame, 0);
 		avcodec_encode_video2(encoder->context, &encoder->packet, encoder->hw_frame, &success);
 	} else {
+		uint8_t *in_data[1] = {(uint8_t *) rgb_pixels};
+		int in_linesize[1] = {encoder->in_width * 4};
+
+	  // scale to the encoder format
+		sws_scale(encoder->sws, in_data, in_linesize, 0, encoder->in_height, encoder->sw_frame->data, encoder->sw_frame->linesize);
+
+	  // init packet to prepare encoding
+		av_init_packet(&encoder->packet);
+		int success = 0; // boolean for success or failure of encoding
+
+	  // attempt to encode the frame
 		avcodec_encode_video2(encoder->context, &encoder->packet, encoder->sw_frame, &success);
+		av_free_packet(&encoder->packet);
 	}
 }
