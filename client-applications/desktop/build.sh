ada4bb88612001bea87d5fc3735fa31166256686
philippemnoel <philippe_noel@college.harvard.edu>
2020-03-24 21:51:46 -0400

    fixed build script

--- client-applications/desktop/build.sh
+++ client-applications/desktop/build.sh
@@ -14,14 +14,14 @@ cd fractal-protocol
 mkdir desktop
 cd ..
 cp .fractal-protocol/desktop/build64/desktop fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libavcodec.58.dylib fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libavdevice.58.dylib fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libavfilter.7.dylib fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libavformat.58.dylib fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libavutil.56.dylib fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libpostproc.55.dylib fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libswresample.3.dylib fractal-protocol/desktop
-cp .fractal-protocol/lib/ffmpeg/macos/libswscale.5.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libavcodec.58.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libavdevice.58.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libavfilter.7.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libavformat.58.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libavutil.56.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libpostproc.55.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libswresample.3.dylib fractal-protocol/desktop
+cp .fractal-protocol/lib/64/ffmpeg/macos/libswscale.5.dylib fractal-protocol/desktop
 codesign -s "Fractal Computers, Inc." fractal-protocol/desktop
 yarn -i
 yarn package
