f98362f2f7f63bf2886322f7b996c5291592a61a
Roshan Padaki <roshanpadaki@gmail.com>
2020-04-04 22:30:14 -0400

    is dumb

--- protocol/include/windowsinput.c
+++ protocol/include/windowsinput.c
@@ -473,8 +473,8 @@ void ReplayUserInput(input_device* device, struct FractalClientMessage* fmsg) {
             break;
         case MESSAGE_MOUSE_MOTION:
             // mouse motion event
-            ("MOUSE: x %d y %d r %d\n", fmsg->mouseMotion.x,
-             fmsg->mouseMotion.y, fmsg->mouseMotion.relative);
+            mprintf("MOUSE: x %d y %d r %d\n", fmsg->mouseMotion.x,
+                    fmsg->mouseMotion.y, fmsg->mouseMotion.relative);
 
             Event.type = INPUT_MOUSE;
             if (fmsg->mouseMotion.relative) {
