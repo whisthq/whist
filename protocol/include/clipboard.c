b12e5a548489335b985bc9cfce0c6b8ab8e11d90
Nicholas Pipitone <npip99@gmail.com>
2020-04-08 15:02:06 -0500

    set clipboard

--- protocol/include/clipboard.c
+++ protocol/include/clipboard.c
@@ -862,7 +862,7 @@ int UpdateClipboardThread( void* opaque )
 				strcat( cmd, " \"ssh://" );
 				strcat( cmd, (char*)server_ip );
 				strcat( cmd, "/" );
-				strcat( cmd, "C:\\Users\\vm1\\AppData\\Roaming\\FractalCache\\set_clipboard" );
+				strcat( cmd, "C:\\ProgramData\\FractalCache\\set_clipboard" );
 				strcat( cmd, "/\" -force " );
 				strcat( cmd, GET_CLIPBOARD );
 				strcat( cmd, " -ignorearchives -confirmbigdel=false -batch" );
