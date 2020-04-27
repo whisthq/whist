41450fdfef78789eec3a849844182f5572bd81c8
Philippe Noel <philippe_noel@college.harvard.edu>
2020-04-27 14:55:21 -0400

    fixed building with gif on windows, packaged app works

--- client-applications/desktop/build.bat
+++ client-applications/desktop/build.bat
@@ -12,7 +12,20 @@ rmdir /S/Q protocol
 mkdir protocol
 cd protocol
 mkdir desktop
+mkdir desktop\loading
 cd ..
-copy .protocol\desktop\build64 protocol\desktop
-copy \loading protocol\desktop
+copy .protocol\desktop\build64\FractalClient.exe protocol\desktop\FractalClient.exe
+copy .protocol\desktop\build64\unison.exe protocol\desktop\unison.exe
+copy .protocol\desktop\build64\ssh.exe protocol\desktop\ssh.exe
+copy .protocol\desktop\build64\sshkey protocol\desktop\sshkey
+copy .protocol\desktop\build64\sshkey.pub protocol\desktop\sshkey.pub
+copy .protocol\desktop\build64\avcodec-58.dll protocol\desktop\avcodec-58.dll
+copy .protocol\desktop\build64\avdevice-58.dll protocol\desktop\avdevice-58.dll
+copy .protocol\desktop\build64\avfilter-7.dll protocol\desktop\avfilter-7.dll
+copy .protocol\desktop\build64\avformat-58.dll protocol\desktop\avformat-58.dll
+copy .protocol\desktop\build64\avutil-56.dll protocol\desktop\avutil-56.dll
+copy .protocol\desktop\build64\postproc-55.dll protocol\desktop\postproc-55.dll
+copy .protocol\desktop\build64\swresample-3.dll protocol\desktop\swresample-3.dll
+copy .protocol\desktop\build64\swscale-5.dll protocol\desktop\swscale-5.dll
+copy loading protocol\desktop\loading
 yarn package
