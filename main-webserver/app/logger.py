b2c92399310d2df43d6802a58fc84902a65815ed
mingy98 <ming_ying@college.harvard.edu>
2020-05-07 14:12:01 -0400

    log levels

--- main-webserver/app/logger.py
+++ main-webserver/app/logger.py
@@ -19,21 +19,25 @@ logger.addHandler(syslog)
 logger.setLevel(logging.INFO)
 
 def sendInfo(ID, log, papertrail = True):
+	logger.setLevel(logging.INFO)
 	if papertrail:
 		logger.info('[WEBSERVER][{}] INFO: {}'.format(ID, log))
 	print('[WEBSERVER][{}] INFO: {}'.format(ID, log))
 
 def sendWarning(ID, log, papertrail = True):
+	logger.setLevel(logging.WARNING)
 	if papertrail:
 		logger.warning('[WEBSERVER][{}] WARNING: {}'.format(ID, log))
 	print('[WEBSERVER][{}] WARNING: {}'.format(ID, log))
 
 def sendError(ID, log, papertrail = True):
+	logger.setLevel(logging.ERROR)
 	if papertrail:
 		logger.error('[WEBSERVER][{}] ERROR: {}'.format(ID, log))
 	print('[WEBSERVER][{}] ERROR: {}'.format(ID, log))
 
 def sendCritical(ID, log, papertrail = True):
+	logger.setLevel(logging.CRITICAL)
 	if papertrail:
 		logger.critical('[WEBSERVER][{}] CRITICAL: {}'.format(ID, log))
 	print('[WEBSERVER][{}] CRITICAL: {}'.format(ID, log))
