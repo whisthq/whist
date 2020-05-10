3c1c0091dc896df82a7054f95a3fb9672bf57b3c
Roshan Padaki <roshanpadaki@gmail.com>
2020-05-10 18:06:44 -0400

    not more encoder recreation

--- protocol/server/main.c
+++ protocol/server/main.c
@@ -195,8 +195,8 @@ int32_t SendVideo(void* opaque) {
                     pending_encoder = false;
                     update_encoder = false;
                 } else {
-                    SDL_CreateThread(MultithreadedEncoderFactory,
-                                     "MultithreadedEncoderFactory", NULL);
+                    // SDL_CreateThread(MultithreadedEncoderFactory,
+                    //                  "MultithreadedEncoderFactory", NULL);
                 }
             }
         }
