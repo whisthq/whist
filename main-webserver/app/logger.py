06210769f9f14bbd7ac3ec8f85a5a59b407baa64
mingy98 <ming_ying@college.harvard.edu>
2020-05-14 15:57:03 -0400

    cleaned logger

--- main-webserver/app/logger.py
+++ main-webserver/app/logger.py
@@ -18,31 +18,21 @@ logger = logging.getLogger()
 logger.addHandler(syslog)
 
 def sendDebug(ID, log, papertrail = True):
-	logger.setLevel(logging.DEBUG)
 	if papertrail:
 		logger.info('[{} WEBSERVER][{}] INFO: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
-	print('[{} WEBSERVER][{}] INFO: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
 
 def sendInfo(ID, log, papertrail = True):
-	logger.setLevel(logging.INFO)
 	if papertrail:
 		logger.info('[{} WEBSERVER][{}] INFO: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
-	print('[{} WEBSERVER][{}] INFO: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
 
 def sendWarning(ID, log, papertrail = True):
-	logger.setLevel(logging.WARNING)
 	if papertrail:
 		logger.warning('[{} WEBSERVER][{}] WARNING: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
-	print('[{} WEBSERVER][{}] WARNING: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
 
 def sendError(ID, log, papertrail = True):
-	logger.setLevel(logging.ERROR)
 	if papertrail:
 		logger.error('[{} WEBSERVER][{}] ERROR: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
-	print('[{} WEBSERVER][{}] ERROR: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
 
 def sendCritical(ID, log, papertrail = True):
-	logger.setLevel(logging.CRITICAL)
 	if papertrail:
 		logger.critical('[{} WEBSERVER][{}] CRITICAL: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
-	print('[{} WEBSERVER][{}] CRITICAL: {}'.format(os.getenv('SERVER_TYPE'), ID, log))
