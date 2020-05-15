c87408f090818411a4397e31b3691bdc8b917f91
Nicholas Pipitone <npip99@gmail.com>
2020-05-15 22:32:28 +0000

    longest and average encode time

--- protocol/server/main.c
+++ protocol/server/main.c
@@ -261,7 +261,21 @@ int32_t SendVideo(void* opaque) {
             video_encoder_encode(encoder, device->frame_data);
             frames_since_first_iframe++;
 
-            LOG_INFO( "Encode Time: %f\n", GetTimer( t ) );
+            static int frame_stat_number = 0;
+            static double total_frame_time = 0.0;
+            static double max_frame_time = 0.0;
+
+            frame_stat_number++;
+            total_frame_time += GetTimer( t );
+            max_frame_time = max( max_frame_time, GetTimer( t ) );
+
+            if( frame_stat_number % 30 == 0 )
+            {
+                LOG_INFO( "Longest Encode Time: %f\n", max_frame_time );
+                LOG_INFO( "Average Encode Time: %f\n", total_frame_time / 30 );
+                total_frame_time = 0.0;
+                max_frame_time = 0.0;
+            }
 
             video_encoder_unset_iframe(encoder);
 
