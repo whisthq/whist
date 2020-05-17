6c7489b74e5ea683d632c1d9c15ee6649ab80256
mingy98 <ming_ying@college.harvard.edu>
2020-05-17 10:44:43 -0400

    currnet

--- protocol/server/main.c
+++ protocol/server/main.c
@@ -373,7 +373,7 @@ int32_t SendVideo(void* opaque) {
                         id++;
                     }
 
-                    // LOG_INFO( "Send Frame Time: %f\n", GetTimer( t ) );
+                    LOG_INFO( "Send Frame Time: %f, Send Frame Size: %d\n", GetTimer( t ), frame_size );
 
                     previous_frame_size = encoder->encoded_frame_size;
                     // double server_frame_time = GetTimer(server_frame_timer);
