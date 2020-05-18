ec31a888cb20b72b416b409ad6d96aed6b17486e
shamazo <hmnicholson12@gmail.com>
2020-05-18 19:26:14 -0400

    Linux compiler flag add -pthread instead of linking to it. To support WSL running with ubuntu 18.04, which only has cmake v1.10 in its repositories we use v1.10, if you want to compile on windows with MSVC set it to 1.15

--- protocol/cmake/CompilerOptions.cmake
+++ protocol/cmake/CompilerOptions.cmake
@@ -4,18 +4,6 @@ if (MSVC) #Windows MSVC compiler base flags
     set(CMAKE_C_FLAGS_RELEASE_INIT "/W4 /MP /MTd /O2i" )
 else() #GCC and Clang base flags
     set(CMAKE_C_FLAGS_INIT "")
-    set(CMAKE_C_FLAGS_DEBUG_INIT "-g \
-            -Wall \
-            -Wextra \
-            -Wno-missing-braces \
-            -Wno-unused-value \
-            -Wno-unused-parameter \
-            -O0")
-    set(CMAKE_C_FLAGS_RELEASE_INIT "-g \
-            -Wall \
-            -Wextra \
-            -Wno-missing-braces \
-            -Wno-unused-value \
-            -Wno-unused-parameter \
-            -O0")
+    set(CMAKE_C_FLAGS_DEBUG_INIT "-g -Wall -Wextra -Wno-missing-braces -Wno-unused-value -Wno-unused-parameter -O0 -pthread")
+    set(CMAKE_C_FLAGS_RELEASE_INIT "-g -Wall -Wextra -Wno-missing-braces -Wno-unused-value -Wno-unused-parameter -O0 -pthread")
 endif()
