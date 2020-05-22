928fde0ecacfe2cc98865c0cbc77d4d2817bd093
shamazo <hmnicholson12@gmail.com>
2020-05-22 12:17:49 -0400

    add get-shared-libs.sh to CMakeLists.txt and make directories if they don't exist

--- protocol/get-shared-libs.sh
+++ protocol/get-shared-libs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/shared-libs.tar.gz" | tar xz
 
+
+mkdir -p desktop/build32/
+mkdir -p server/build32/
+mkdir -p desktop/build64/
+mkdir -p server/build64/
 # in case we want to target Windows
 cp share/32/Windows/* desktop/build32/
 cp share/32/Windows/* server/build32/
 cp share/64/Windows/* desktop/build64/
 cp share/64/Windows/* server/build64/
+
+echo "done"
