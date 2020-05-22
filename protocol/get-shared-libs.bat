17e58453106e26c5b92d797ca1b9b07f4dd2ed92
Roshan Padaki <roshanpadaki@gmail.com>
2020-05-22 16:22:54 -0400

    Added mkdirs

--- protocol/get-shared-libs.bat
+++ protocol/get-shared-libs.bat
@@ -1,4 +1,9 @@
 curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/shared-libs.tar.gz" | tar xzf -
+mkdir server\build32
+mkdir server\build64
+mkdir desktop\build32
+mkdir desktop\build64
+
 copy share\32\Windows\* desktop\build32\
 copy share\32\Windows\* server\build32\
 copy share\64\Windows\* desktop\build64\
