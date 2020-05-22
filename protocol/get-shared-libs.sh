9c7370123484973eda0b0c194ab4374c0fc9f93f
Roshan Padaki <roshanpadaki@gmail.com>
2020-05-21 20:45:55 -0400

    fix fetch scripts

--- protocol/get-shared-libs.sh
+++ protocol/get-shared-libs.sh
@@ -3,6 +3,6 @@ curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/shared-libs.tar.g
 
 # in case we want to target Windows
 cp share/32/Windows/* desktop/build32/
-cp share/32/Windows/* desktop/build32/
+cp share/32/Windows/* server/build32/
 cp share/64/Windows/* desktop/build64/
-cp share/64/Windows/* desktop/build64/
+cp share/64/Windows/* server/build64/
