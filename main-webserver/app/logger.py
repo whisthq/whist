54cc6b5e1964460d53f06c6b5d394ebe66ef1a9c
Jonathan <jonathanxu25@gmail.com>
2020-05-25 13:39:44 -0400

    logging debug

--- main-webserver/app/logger.py
+++ main-webserver/app/logger.py
@@ -52,7 +52,7 @@ syslog.setFormatter(formatter)
 
 logger = logging.getLogger()
 # Sets the minimum logging priority to actually log (DEBUG < INFO < WARNING < ERROR < CRITICAL)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logger.addHandler(syslog)
 
 
