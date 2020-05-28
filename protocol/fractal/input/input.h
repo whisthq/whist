157f3deca2935649e7ff24e09a23c417c38f951d
philippemnoel <philippe_noel@college.harvard.edu>
2020-05-28 13:56:59 -0400

    done 3 more header files

--- protocol/fractal/input/input.h
+++ protocol/fractal/input/input.h
@@ -1,10 +1,21 @@
-/*
- * General input processing functions for Windows and Linux Ubuntu.
- *
- * Copyright Fractal Computers, Inc. 2020
- **/
 #ifndef INPUT_H
 #define INPUT_H
+/**
+Copyright Fractal Computers, Inc. 2020
+@file input.h
+@brief 
+
+============================
+Usage
+============================
+
+*/
+
+/*
+============================
+Includes
+============================
+*/
 
 #ifdef _WIN32
 #include "windowsinput.h"
@@ -12,13 +23,43 @@
 #include "linuxinput.h"
 #endif
 
+/*
+============================
+Public Functions
+============================
+*/
+
+/**
+@brief                          Initialize all cursors
+
+@returns                        Initialized input device struct defining mouse and keyboard states
+*/
 input_device_t* CreateInputDevice();
 
+/**
+@brief                          Returns the current cursor image
+
+@param input_device             The initialized input device struct to destroy and free the memory of
+*/
 void DestroyInputDevice(input_device_t* input_device);
 
+/**
+@brief                          Returns the current cursor image
+
+@param input_device             The initialized input device struct defining mouse and keyboard states for the user
+@param fmsg                     The Fractal message packet, defining one user action event, to replay on the computer
+
+@returns                        True if it replayed the event, False otherwise
+*/
 bool ReplayUserInput(input_device_t* input_device,
                      struct FractalClientMessage* fmsg);
 
+/**
+@brief                          Returns the current cursor image
+
+@param input_device             The initialized input device struct defining mouse and keyboard states for the user
+@param fmsg                     The Fractal message packet, defining one keyboard event, to update the keyboard state
+*/
 void UpdateKeyboardState(input_device_t* input_device,
                          struct FractalClientMessage* fmsg);
 
--- protocol/fractal/input/windowsinput.h
+++ protocol/fractal/input/windowsinput.h
@@ -1,15 +1,45 @@
-/*
- * User input processing on Windows.
- *
- * Copyright Fractal Computers, Inc. 2020
- **/
 #ifndef WINDOWS_INPUT_H
 #define WINDOWS_INPUT_H
+/**
+Copyright Fractal Computers, Inc. 2020
+@file windowsinput.h
+@brief Windows input device type and function to enter string on Winlogon screen
+
+============================
+Usage
+============================
+
+*/
+
+/*
+============================
+Includes
+============================
+*/
 
 #include "../core/fractal.h"
 
+
+/*
+============================
+Custom Types
+============================
+*/
+
 typedef char input_device_t;
 
+/*
+============================
+Public Functions
+============================
+*/
+
+/**
+@brief                          Inputs a provided series of character to Winlogon, to log in to the computer
+
+@param keycodes                 Enumeration of every character to enter, sequentially
+@param len                      Number of characters to enter                         
+*/
 void EnterWinString(enum FractalKeycode* keycodes, int len);
 
 #endif  // WINDOWS_INPUT_H
