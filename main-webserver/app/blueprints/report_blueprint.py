3e7d53826ddcfda05043c7e46ff48a6a6c065931
Jonathan <jonathanxu25@gmail.com>
2020-06-04 18:34:26 -0400

    saving changes

---
+++ main-webserver/app/blueprints/report_blueprint.py
@@ -0,0 +1,41 @@
+from app import *
+
+from app.logger import *
+
+report_bp = Blueprint("report_bp", __name__)
+
+
+@report_bp.route("/report/<action>", methods=["GET", "POST"])
+def statusReport(action, **kwargs):
+    body = request.get_json()
+    if action == "latest" and request.method == "GET":
+        command = text(
+            """
+        SELECT * 
+        FROM status_report
+        ORDER BY timestamp DESC
+        """
+        )
+        params = {}
+        try:
+            with engine.connect() as conn:
+                latest = cleanFetchedSQL(conn.execute(command, **params).fetchone())
+                return jsonify(latest), 200
+        except:
+            traceback.print_exc()
+    elif action == "userReport" and request.method == "GET":
+        command = text(
+            """
+        SELECT timestamp, users_online, eastus_unavailable, northcentralus_unavailable, southcentralus_unavailable
+        FROM status_report
+        ORDER BY timestamp ASC
+        """
+        )
+        params = {}
+        try:
+            with engine.connect() as conn:
+                latest = cleanFetchedSQL(conn.execute(command, **params).fetchall())
+                return jsonify(latest), 200
+        except:
+            traceback.print_exc()
+    return jsonify({}), 404
