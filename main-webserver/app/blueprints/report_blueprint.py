127c06ce2207a770ee78a8a10a3e9dbb8520d10d
Jonathan <jonathanxu25@gmail.com>
2020-06-04 18:38:14 -0400

    safe version

--- main-webserver/app/blueprints/report_blueprint.py
+++ main-webserver/app/blueprints/report_blueprint.py
@@ -5,10 +5,9 @@ from app.logger import *
 report_bp = Blueprint("report_bp", __name__)
 
 
-@report_bp.route("/report/<action>", methods=["GET", "POST"])
+@report_bp.route("/report/<action>", methods=["GET"])
 def statusReport(action, **kwargs):
-    body = request.get_json()
-    if action == "latest" and request.method == "GET":
+    if action == "latest":
         command = text(
             """
         SELECT * 
@@ -23,7 +22,7 @@ def statusReport(action, **kwargs):
                 return jsonify(latest), 200
         except:
             traceback.print_exc()
-    elif action == "userReport" and request.method == "GET":
+    elif action == "userReport":
         command = text(
             """
         SELECT timestamp, users_online, eastus_unavailable, northcentralus_unavailable, southcentralus_unavailable
@@ -34,7 +33,7 @@ def statusReport(action, **kwargs):
         params = {}
         try:
             with engine.connect() as conn:
-                latest = cleanFetchedSQL(conn.execute(command, **params).fetchall())
+                latest = cleanFetchedSQL(conn.execute(command, **params).fetchAll())
                 return jsonify(latest), 200
         except:
             traceback.print_exc()
