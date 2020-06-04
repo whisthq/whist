9e4063c08ec98b3528bb395e823a30747ba106d5
Isabelle Zheng <isa.zheng@gmail.com>
2020-06-04 14:26:52 -0700

    install apps in createdisk call

--- main-webserver/app/tasks.py
+++ main-webserver/app/tasks.py
@@ -124,7 +124,7 @@ def createEmptyDisk(self, disk_size, username, location, ID=-1):
 
 
 @celery.task(bind=True)
-def createDiskFromImage(self, username, location, vm_size, operating_system, ID=-1):
+def createDiskFromImage(self, username, location, vm_size, operating_system, apps, ID=-1):
     hr = 400
     payload = None
 
@@ -135,6 +135,9 @@ def createDiskFromImage(self, username, location, vm_size, operating_system, ID=
         )
         hr = payload["status"]
         sendInfo(ID, "Disk created with status {}".format(hr))
+
+    if hr == 200:
+        installApplications.apply_async([username, apps, ID]) # better task handling?
 
     sendDebug(ID, payload)
     payload["location"] = location
