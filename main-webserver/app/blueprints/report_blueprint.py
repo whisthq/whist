26057da1649b7a7a24d610480b5d85ea215ecf50
Jonathan <jonathanxu25@gmail.com>
2020-06-05 15:43:32 -0400

    saving progress for reports

--- main-webserver/app/blueprints/report_blueprint.py
+++ main-webserver/app/blueprints/report_blueprint.py
@@ -28,13 +28,14 @@ def statusReport(action, **kwargs):
         SELECT timestamp, users_online, eastus_unavailable, northcentralus_unavailable, southcentralus_unavailable
         FROM status_report
         ORDER BY timestamp ASC
+        LIMIT 24
         """
         )
         params = {}
         try:
             with engine.connect() as conn:
-                latest = cleanFetchedSQL(conn.execute(command, **params).fetchAll())
-                return jsonify(latest), 200
+                users = cleanFetchedSQL(conn.execute(command, **params).fetchall())
+                return jsonify(users), 200
         except:
             traceback.print_exc()
     return jsonify({}), 404
