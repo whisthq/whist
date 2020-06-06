191a97d46b002da987aa94ae10ccb64e9f0f72d6
shamazo <hmnicholson12@gmail.com>
2020-06-06 15:28:24 -0400

    Change get-shared-libs.bat to mkdir if not exists

--- protocol/get-shared-libs.bat
+++ protocol/get-shared-libs.bat
@@ -1,8 +1,8 @@
 curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/shared-libs.tar.gz" | tar xzf -
-mkdir server\build32
-mkdir server\build64
-mkdir desktop\build32
-mkdir desktop\build64
+if not exist server\build32 mkdir server\build32
+if not exist server\build64 mkdir server\build64
+if not exist desktop\build32 mkdir desktop\build32
+if not exist desktop\build64 mkdir desktop\build64
 
 copy share\32\Windows\* desktop\build32\Windows\
 copy share\32\Windows\* server\build32\Windows\
