1c4ceed6260ddf45c523a1fa2502cbf1573a4e3f
Hamish <hmnicholson12@gmail.com>
2020-06-07 17:50:06 +0000

    Change method of copying files.

--- protocol/get-shared-libs.bat
+++ protocol/get-shared-libs.bat
@@ -4,7 +4,7 @@ if not exist server\build64 mkdir server\build64
 if not exist desktop\build32 mkdir desktop\build32
 if not exist desktop\build64 mkdir desktop\build64
 
-copy share\32\Windows\* desktop\build32\Windows
-copy share\32\Windows\* server\build32\Windows
-copy share\64\Windows\* desktop\build64\Windows
-copy share\64\Windows\* server\build64\Windows
+xcopy /e /Y /c share\32\Windows\* desktop\build32\Windows\
+xcopy /e /Y /c share\32\Windows\* server\build32\Windows\
+xcopy /e /Y /c share\64\Windows\* desktop\build64\Windows\
+xcopy /e /Y /c share\64\Windows\* server\build64\Windows\
