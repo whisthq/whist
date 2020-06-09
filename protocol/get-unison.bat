3bf2a2cf8271733e0586eefe8858997b7741240e
sean-hackett <seanhackett55@gmail.com>
2020-06-09 13:10:35 -0400

    Remove unison binaries, fetch from S3

---
+++ protocol/get-unison.bat
@@ -0,0 +1,3 @@
+curl -s -O "https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/unison.exe"
+if not exist desktop\external_utils/Windows mkdir desktop\external_utils\Windows
+move unison.exe desktop\external_utils\Windows
