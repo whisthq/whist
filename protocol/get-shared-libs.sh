f32e6a2506defff2ed30cf9e15d0557ed2d47572
shamazo <hmnicholson12@gmail.com>
2020-06-09 16:54:35 -0400

    server -> desktop in get shared libs

--- protocol/get-shared-libs.sh
+++ protocol/get-shared-libs.sh
@@ -6,12 +6,12 @@ mkdir -p desktop/build32/Windows
 mkdir -p server/build32/Windows
 mkdir -p desktop/build64/Windows
 mkdir -p server/build64/Windows
-mkdir -p server/build64/Darwin
+mkdir -p desktop/build64/Darwin
 
 # in case we want to target Windows
 cp share/32/Windows/* desktop/build32/Windows/
 cp share/32/Windows/* server/build32/Windows/
 cp share/64/Windows/* desktop/build64/Windows/
 cp share/64/Windows/* server/build64/Windows/
-cp share/64/Darwin/* server/build64/Darwin/
+cp share/64/Darwin/* desktop/build64/Darwin/
 echo "done"
