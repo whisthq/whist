71e553b5708cc5a9d4c91b98f097d1faa1d248b2
Jonathan <jonathanxu25@gmail.com>
2020-06-17 12:08:59 -0400

    debugging header

--- main-webserver/app/blueprints/stripe_blueprint.py
+++ main-webserver/app/blueprints/stripe_blueprint.py
@@ -279,7 +279,7 @@ def payment(action, **kwargs):
     # Endpoint for stripe webhooks
     elif action == "hooks":
         body = request.get_json()
-        sigHeader = request.headers["HTTP_STRIPE_SIGNATURE"]
+        sigHeader = request.headers["STRIPE_SIGNATURE"]
         endpointSecret = "whsec_dlppdwofxgW17XsboVetpX9CvGMq9geQ"
         event = None
 
