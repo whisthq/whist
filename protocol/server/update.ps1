b5074eabdb0edb7c683cf69bf5b6d0f7f65100d9
Nicholas Pipitone <npip99@gmail.com>
2020-06-17 08:16:55 -0500

    use branch/Windows

--- protocol/server/update.ps1
+++ protocol/server/update.ps1
@@ -2,8 +2,8 @@ param (
     [string]$Branch = ""
 )
 
-if ($branch.Length -gt 0) {
-  $branch = "$branch/"
+if ($branch.Length -eq 0) {
+  exit
 }
 
 cd build64
@@ -18,8 +18,8 @@ $EncodedText = $EncodedText.Substring(0, 8)
 $GitHash = (git rev-parse --short HEAD)
 Set-Content version "${GitHash}-${EncodedText}"
 
-cmd.exe /c aws s3 cp update.bat s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/${branch}update.bat
-cmd.exe /c aws s3 cp FractalServer.exe s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/${branch}FractalServer.exe
-cmd.exe /c aws s3 cp version s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/${branch}version
+cmd.exe /c aws s3 cp update.bat s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/${branch}/Windows/update.bat
+cmd.exe /c aws s3 cp FractalServer.exe s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/${branch}/Windows/FractalServer.exe
+cmd.exe /c aws s3 cp version s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/${branch}/Windows/version
 
 type version
