d6442a2977b23ff2555b163adbc98f4b8b51b832
Isabelle Zheng <isa.zheng@gmail.com>
2020-06-19 13:11:28 -0700

    add requests tests

--- main-webserver/tests/test_app.py
+++ main-webserver/tests/test_app.py
@@ -2,33 +2,29 @@ import sys
 import os
 import tempfile
 import pytest
-import pytest_pgsql
+import requests
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from app import create_app, init_app
+SERVER_URL = "https://" + os.getenv("HEROKU_APP_NAME") + ".herokuapp.com" if os.getenv("HEROKU_APP_NAME") else "http://localhost:5000"
 
-@pytest.fixture
-def app():
-    app, jwtManager = create_app()
-    app = init_app(app)
-    return app
-
-def register(client, username, password):
-    return client.post(('/account/register'), json=dict(
+def login(username, password):
+    return requests.post((SERVER_URL + '/account/login'), json=dict(
         username=username,
         password=password
     ))
 
-def fetchvms(client, username):
-    return client.post(("/user/login"), json=dict(
+def fetchvms(username):
+    return requests.post((SERVER_URL + "/user/login"), json=dict(
         username=username
     ))
 
-def test_fetchvms(client):
+def test_fetchvms():
     email = 'isa.zheng@gmail.com'
-    rv = fetchvms(client, email)
+    rv = fetchvms(email)
     assert rv.status_code == 200
 
-    # json_data = rv.get_json()
-    # print(json_data)
-    # assert verify_token(email, json_data['access_token'])
+def test_login():
+    email = 'example@example.com'
+    rv = login(email, 'password')
+    json_data = rv.json()
+    print(json_data)
+    assert rv.status_code == 200
