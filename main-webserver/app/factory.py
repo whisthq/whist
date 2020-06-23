3104d1189c140e7e490173dee12c7fa982154ea3
mingy98 <ming@fractalcomputers.com>
2020-06-23 08:58:24 -0400

    email template directory fix

--- main-webserver/app/factory.py
+++ main-webserver/app/factory.py
@@ -8,7 +8,6 @@ PKG_NAME = os.path.dirname(os.path.realpath(__file__)).split("/")[-1]
 def create_app(app_name=PKG_NAME, **kwargs):
     template_dir = os.path.dirname(os.path.realpath(__file__))
     template_dir = os.path.join(template_dir, "templates")
-    print(template_dir)
     app = Flask(app_name, template_folder=template_dir)
     jwtManager = JWTManager(app)
     if kwargs.get("celery"):
