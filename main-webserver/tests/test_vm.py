a27b96d4a8bf304e995f8b6d890af82f87bcfbe9
Fractal Linter <linter@fractalcomputers.com>
2020-06-24 21:28:21 +0000

    Linted with Python Black

--- main-webserver/tests/test_vm.py
+++ main-webserver/tests/test_vm.py
@@ -6,35 +6,47 @@ from dotenv import load_dotenv
 import time
 
 load_dotenv()
-SERVER_URL = "https://" + os.getenv("HEROKU_APP_NAME") + ".herokuapp.com" if os.getenv("HEROKU_APP_NAME") else "http://localhost:5000"
+SERVER_URL = (
+    "https://" + os.getenv("HEROKU_APP_NAME") + ".herokuapp.com"
+    if os.getenv("HEROKU_APP_NAME")
+    else "http://localhost:5000"
+)
+
 
 def getStatus(id):
     print(id)
     resp = requests.get((SERVER_URL + "/status/" + id))
     return resp.json()
 
+
 def create(vm_size, location, operating_system, admin_password, input_token):
-    return requests.post((SERVER_URL + '/vm/create'), json={
-        "vm_size":vm_size,
-        "location":location,
-        "operating_system":operating_system,
-        "admin_password":admin_password
-    }, headers={
-        "Authorization": "Bearer " + input_token
-    })
+    return requests.post(
+        (SERVER_URL + "/vm/create"),
+        json={
+            "vm_size": vm_size,
+            "location": location,
+            "operating_system": operating_system,
+            "admin_password": admin_password,
+        },
+        headers={"Authorization": "Bearer " + input_token},
+    )
+
 
 def delete(vm_name, delete_disk):
-    return requests.post((SERVER_URL + "/vm/delete"), json={
-        "vm_name":vm_name,
-	    "delete_disk":delete_disk
-    })
+    return requests.post(
+        (SERVER_URL + "/vm/delete"),
+        json={"vm_name": vm_name, "delete_disk": delete_disk},
+    )
+
 
 def test_vm(input_token):
     # Testing create
-    resp = create("Standard_NV6_Promo", "eastus", "Windows", 'fractal123456789.',input_token)
+    resp = create(
+        "Standard_NV6_Promo", "eastus", "Windows", "fractal123456789.", input_token
+    )
     id = resp.json()["ID"]
     status = "PENDING"
-    while(status == "PENDING"):
+    while status == "PENDING":
         time.sleep(5)
         status = getStatus(id)["state"]
     if status != "SUCCESS":
@@ -45,9 +57,7 @@ def test_vm(input_token):
     resp = delete(getStatus(id)["output"]["vm_name"], True)
     id = resp.json()["ID"]
     status = "PENDING"
-    while(status == "PENDING"):
+    while status == "PENDING":
         time.sleep(5)
         status = getStatus(id)["state"]
     assert status == "SUCCESS"
-
-
