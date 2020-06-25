b05b9ab36a2123ccf4cf31417f68c66ec22a4289
Isabelle Zheng <isa.zheng@gmail.com>
2020-06-25 09:58:41 -0700

    import time for sleep

--- main-webserver/tests/conftest.py
+++ main-webserver/tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 import os
 import logging
+import time
 
 SERVER_URL = "https://" + os.getenv("HEROKU_APP_NAME") + ".herokuapp.com" if os.getenv("HEROKU_APP_NAME") else "http://localhost:5000"
 
@@ -15,7 +16,7 @@ def setup(request):
     LOGGER.info("Waiting for server to deploy...")
     i = 1
     while getVersions().status_code != 200:
-        sleep(3)
+        time.sleep(3)
         i += 1
         LOGGER.info(i + " times pinging server")
     LOGGER.info("Server deployed! Tests starting now.")
