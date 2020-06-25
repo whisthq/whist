b3f1727836fce239567e348b2323e65a4233607b
Roshan Padaki <roshanpadaki@gmail.com>
2020-06-24 20:20:07 -0400

    Document APIs

--- protocol/fractal/video/cpucapturetransfer.h
+++ protocol/fractal/video/cpucapturetransfer.h
@@ -1,5 +1,20 @@
 #ifndef CPU_CAPTURE_TRANSFER_H
 #define CPU_CAPTURE_TRANSFER_H
+/**
+ * Copyright Fractal Computers, Inc. 2020
+ * @file cpucapturetransfer.h
+ * @brief This code handles the data transfer from a video capture device to a
+ *        video encoder via the CPU.
+============================
+Usage
+============================
+*/
+
+/*
+============================
+Includes
+============================
+*/
 
 #include "videoencode.h"
 #if defined(_WIN32)
@@ -8,6 +23,23 @@
 #include "x11capture.h"
 #endif
 
+/*
+============================
+Public Functions
+============================
+*/
+
+/**
+ * @brief                         Transfer the texture stored in the capture
+ *                                device to a CPU buffer, and load that CPU
+ *                                buffer into the encoder's input frame
+ *
+ * @param device                  The capture device from which to get the
+ *                                texture
+ * @param encoder                 The encoder into which to load the frame data
+ *
+ * @returns                       0 on success, else -1
+ */
 int cpu_transfer_capture(CaptureDevice* device, video_encoder_t* encoder);
 
 #endif  // CPU_CAPTURE_TRANSFER_H
--- protocol/fractal/video/dxgicudacapturetransfer.h
+++ protocol/fractal/video/dxgicudacapturetransfer.h
@@ -1,10 +1,20 @@
 #ifndef DXGI_CUDA_CAPTURE_TRANSFER_H
 #define DXGI_CUDA_CAPTURE_TRANSFER_H
+/**
+ * Copyright Fractal Computers, Inc. 2020
+ * @file cpucapturetransfer.h
+ * @brief This code handles the data transfer from a video capture device to a
+ *        video encoder via the CPU.
+============================
+Usage
+============================
+*/
 
-// this is actually written in C++, so we have to include separately due to
-// operator overloads within. we do this now so the rest of dxgicapture.h
-// dependencies can be included together
-#include <D3D11.h>
+/*
+============================
+Defines
+============================
+*/
 
 // magic -- when NVCC compiles this as C++, include things as C, else treat as C
 #ifdef __cplusplus
@@ -19,13 +29,69 @@
 #define END_EXTERN_C
 #endif
 
+/*
+============================
+Includes
+============================
+*/
+
+/*
+ * this is actually written in C++, so we have to include separately due to
+ * operator overloads within. we do this now so the rest of dxgicapture.h
+ * dependencies can be included together
+ */
+#include <D3D11.h>
+
 BEGIN_EXTERN_C
 #include "dxgicapture.h"
 #include "videoencode.h"
+END_EXTERN_C
 
+/*
+============================
+Public Functions
+============================
+*/
+
+BEGIN_EXTERN_C
+
+/**
+ * @brief                         Check CUDA capabilities and, if possible,
+ *                                register the screen capture texture resource
+ *                                for CUDA access buffer into the encoder's
+ *                                input frame. Must be called after creating the
+ *                                capture device
+ *
+ * @param device                  The capture device to register
+ *
+ * @returns                       0 on success, 1 if CUDA is unavailable, and -1
+ *                                on error
+ */
 int dxgi_cuda_start_transfer_context(CaptureDevice* device);
+
+/**
+ * @brief                         Unregister the screen capture texture resource
+ *                                for the capture device. Must be called before
+ *                                destroying the capture device
+ */
 void dxgi_cuda_close_transfer_context();
+
+/**
+ * @brief                         Transfer the texture stored in the capture
+ *                                device to the hw_frame for an NVENC encoder.
+ *                                The DXGI-CUDA transfer context must first be
+ *                                started with
+ *                                `dxgi_cuda_start_transfer_context`
+ *
+ * @param device                  The capture device that has a texture
+ * @param encoder                 The NVENC encoder into which to load the frame
+ * data
+ *
+ * @returns                       0 on success, 1 if CUDA is unavailable, and -1
+ *                                on error
+ */
 int dxgi_cuda_transfer_capture(CaptureDevice* device, video_encoder_t* encoder);
+
 END_EXTERN_C
 
 #endif  // DXGI_CUDA_CAPTURE_TRANSFER_H
