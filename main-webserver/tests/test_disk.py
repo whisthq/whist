332e3ca415e735c58320315132f23d1b43ae7d67
Jonathan <jonathanxu25@gmail.com>
2020-06-29 13:46:04 -0400

    saving changes

--- main-webserver/tests/test_disk.py
+++ main-webserver/tests/test_disk.py
@@ -18,12 +18,52 @@ def getStatus(id):
     return resp.json()
 
 
-def createEmpty(disk_size, username, location):
-    requests.post(
+def createEmpty(disk_size, username):
+    return requests.post(
         (SERVER_URL + "/disk/createEmpty"),
-        json={"disk_size": disk_size, "username": username, "location": location},
+        json={"disk_size": disk_size, "username": username},
     )
 
 
-def test_createEmpty():
-    createEmpty(10, "fakefake@fake.com", "westus2")
+def test_createEmpty(input_token):
+    resp = createEmpty(10, "doesntexist@fake.com")
+    assert resp.status_code == 404
+    # Create an account and test it
+    requests.post(
+        (SERVER_URL + "/account/register"),
+        json={
+            "username": "fakefake@delete.com",
+            "password": "password",
+            "name": "fake",
+            "feedback": "Where are average things manufactured? The satisfactory.",
+        },
+    )
+    resp = createEmpty(10, "fakefake@delete.com")
+    assert resp.status_code == 202
+    id = resp.json()["ID"]
+    status = "PENDING"
+    while status == "PENDING" or status == "STARTED":
+        time.sleep(5)
+        status = getStatus(id)["state"]
+    assert status == "SUCCESS"
+    disk_name = getStatus(id)["output"]
+    
+    resp = requests.post(
+        (SERVER_URL + "/disk/fetchDisk"),
+        json={"disk_name": disk_name},
+        headers={"Authorization": "Bearer " + input_token},
+    )
+    assert resp.json()["disk"] is not None
+
+    requests.post(
+        (SERVER_URL + "/disk/delete"),
+        json={"username": "fakefake@delete.com"},
+        headers={"Authorization": "Bearer " + input_token},
+    )
+
+    requests.post(
+        (SERVER_URL + "/account/delete"),
+        json={"username": "fakefake@delete.com"},
+        headers={"Authorization": "Bearer " + input_token},
+    )
+    
