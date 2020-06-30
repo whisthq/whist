d3ce600b4cafcaf8dc620c65c46908789816ba13
Fractal Linter <linter@fractalcomputers.com>
2020-06-30 21:01:18 +0000

    Linted with Python Black

--- main-webserver/tests/conftest.py
+++ main-webserver/tests/conftest.py
@@ -8,7 +8,9 @@ pytest_plugins = ["helpers_namespace"]
 
 load_dotenv()
 SERVER_URL = (
-    "https://main-webserver-pr-" + str(os.getenv("TEST_HEROKU_PR_NUMBER")) + ".herokuapp.com"
+    "https://main-webserver-pr-"
+    + str(os.getenv("TEST_HEROKU_PR_NUMBER"))
+    + ".herokuapp.com"
     if os.getenv("CI") == "true"
     else "http://localhost:5000"
 )
