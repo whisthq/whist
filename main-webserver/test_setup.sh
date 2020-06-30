7d79e14bab46b866b4e49c1bb6a424474af581b9
Isabelle Zheng <isa.zheng@gmail.com>
2020-06-30 13:49:59 -0700

    try to debug shell script

--- main-webserver/test_setup.sh
+++ main-webserver/test_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo $GITHUB_PAT
+echo $HEROKU_TEST_RUN_COMMIT_VERSION
+
 response=$(curl -H "Authorization: token $GITHUB_PAT" -H "Accept: application/vnd.github.groot-preview+json" https://api.github.com/repos/fractalcomputers/main-webserver/commits/$HEROKU_TEST_RUN_COMMIT_VERSION/pulls)
 
 TEST_HEROKU_PR_NUMBER=$(echo $response | python -c "import json,sys;obj=json.load(sys.stdin);print(obj[0]['number']);")
