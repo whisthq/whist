dfc08f8dd2e18d0bef4fd28d0eb8adede5d1cb37
sean-hackett <seanhackett55@gmail.com>
2020-07-01 11:41:45 -0400

    Fix mouse coloring

--- protocol/desktop/peer.c
+++ protocol/desktop/peer.c
@@ -8,6 +8,18 @@ extern int client_id;
 
 extern volatile SDL_Window* window;
 
+void replaceColor(SDL_Surface *sfc, Uint32 old_color, Uint32 new_color) {
+    for (int x = 0; x < sfc->w; x++) {
+        for (int y = 0; y < sfc->h; y++) {
+            Uint8 * pixel = (Uint8*) sfc->pixels;
+            pixel += (y * sfc->pitch) + (x * sfc->format->BytesPerPixel);
+            if (*((Uint32*)pixel) == old_color) {
+                *((Uint32*)pixel) = new_color;
+            }
+        }
+    }
+}
+
 int renderPeers(SDL_Renderer *renderer, PeerUpdateMessage *msgs, size_t num_msgs) {
     SDL_Rect rect;
     int ret = 0;
@@ -20,41 +32,69 @@ int renderPeers(SDL_Renderer *renderer, PeerUpdateMessage *msgs, size_t num_msgs
         return -1;
     }
     SDL_LockSurface(cursor_surface);
-    uint32_t key_color = SDL_MapRGB(cursor_surface->format, 255, 255, 255);
-    SDL_SetColorKey(cursor_surface, SDL_TRUE, key_color);
-    SDL_UnlockSurface(cursor_surface);
-    SDL_Texture *cursor_texture = SDL_CreateTextureFromSurface(renderer, cursor_surface);
-    SDL_FreeSurface(cursor_surface);
-    if (cursor_texture == NULL) {
-        LOG_ERROR("Failed to create cursor texture from surface."
-                  " (Error: %s)", SDL_GetError());
+    if (cursor_surface->format->BytesPerPixel != 4) {
+        LOG_ERROR("Cursor image is not 32-bit RGB.");
+        SDL_FreeSurface(cursor_surface);
         return -1;
     }
+    uint32_t black = SDL_MapRGB(cursor_surface->format, 0, 0, 0);
+    uint32_t white = SDL_MapRGB(cursor_surface->format, 255, 255, 255);
+    SDL_SetColorKey(cursor_surface, SDL_TRUE, white);
+    SDL_UnlockSurface(cursor_surface);
 
     for (; num_msgs > 0; msgs++, num_msgs--) {
         if (client_id == msgs->peer_id) {
+            continue;
+        }
+
+        SDL_Surface *temp_surface = SDL_CreateRGBSurface(0,
+            cursor_surface->w, cursor_surface->h,
+            cursor_surface->format->BitsPerPixel,
+            cursor_surface->format->Rmask,
+            cursor_surface->format->Gmask,
+            cursor_surface->format->Bmask,
+            cursor_surface->format->Amask);
+        if (SDL_BlitSurface(cursor_surface, NULL, temp_surface, NULL) != 0) {
+            LOG_ERROR("Failed to blit cursor surface "
+                      "(Error: %s)", SDL_GetError());
+            SDL_FreeSurface(temp_surface);
+            continue;
+        }
+        SDL_LockSurface(temp_surface);
+        uint32_t color = SDL_MapRGB(temp_surface->format,
+                                msgs->color.r, msgs->color.g, msgs->color.b);
+        replaceColor(temp_surface, black, color);
+        SDL_Texture *temp_texture = SDL_CreateTextureFromSurface(renderer,
+                                                temp_surface);
+        SDL_FreeSurface(temp_surface);
+        if (temp_texture == NULL) {
+            LOG_ERROR("Failed to create cursor texture from surface."
+                      " (Error: %s)", SDL_GetError());
             continue;
         }
         rect.w = 25;
         rect.h = 25;
         rect.x = msgs->x * window_width / (int32_t)MOUSE_SCALING_FACTOR;
         rect.y = msgs->y * window_height / (int32_t)MOUSE_SCALING_FACTOR;
-        if (SDL_RenderCopy(renderer, cursor_texture, NULL, &rect) != 0) {
+        if (SDL_RenderCopy(renderer, temp_texture, NULL, &rect) != 0) {
             LOG_ERROR("Failed to render copy peer mouse.");
         }
-        rect.w = 5;
-        rect.h = 5;
-        rect.x -= rect.w / 2;
-        rect.y -= rect.h / 2;
-        SDL_SetRenderDrawColor(renderer, msgs->color.r, msgs->color.g, msgs->color.b, 255);
-        if (SDL_RenderFillRect(renderer, &rect) != 0) {
-          LOG_ERROR("Failed to render peer rectangle.");
-          ret = -1;
+        SDL_DestroyTexture(temp_texture);
+
+        if (msgs->is_controlling) {
+            rect.x = msgs->x * window_width / (int32_t)MOUSE_SCALING_FACTOR;
+            rect.y = msgs->y * window_height / (int32_t)MOUSE_SCALING_FACTOR;
+            rect.w = 5;
+            rect.h = 5;
+            rect.x -= rect.w / 2;
+            rect.y -= rect.h / 2;
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            if (SDL_RenderFillRect(renderer, &rect) != 0) {
+              LOG_ERROR("Failed to render peer rectangle.");
+              ret = -1;
+            }
         }
-        // if (msgs->is_controlling) {
-        //
-        // }
     }
-    SDL_DestroyTexture(cursor_texture);
+    SDL_FreeSurface(cursor_surface);
     return ret;
 }
