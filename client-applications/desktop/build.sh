c9d7273fdd2f5476c508a486d784035bf2b84640
Ming Ying <ming@Mings-MacBook-Air.local>
2020-07-02 08:01:04 -0400

    first try at fixing mac scripts

--- client-applications/desktop/build.sh
+++ client-applications/desktop/build.sh
@@ -12,30 +12,28 @@ mkdir protocol
 cd protocol
 mkdir desktop
 cd ..
-cp .protocol/desktop/build64/FractalClient protocol/desktop
-cp -R loading protocol/desktop
-cp .protocol/desktop/build64/sshkey protocol/desktop
-cp .protocol/desktop/build64/sshkey.pub protocol/desktop
-sudo chmod 600 protocol/desktop/sshkey # was previously in protocol, but operation not permitted from protocol
+
+# copy over the Unison executable and FFmpeg dylibs
+cp -R .protocol/lib/64/ffmpeg/Darwin protocol-build/desktop/
+
+sudo chmod 600 protocol-build/desktop/sshkey # was previously in protocol, but operation not permitted from protocol
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   # Linux Ubuntu
   # copy over the Unison executable and make executable files executable
-  cp .protocol/desktop/build64/linux_unison protocol/desktop
-  sudo chmod +x protocol/desktop/FractalClient
-  sudo chmod +x protocol/desktop/linux_unison
+  cp .protocol/desktop/build64/linux_unison protocol-build/desktop
+  sudo chmod +x protocol-build/desktop/FractalClient
+  sudo chmod +x protocol-build/desktop/linux_unison
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # Mac OSX
   # add logo to the FractalClient executable
   sips -i build/icon.png # take an image and make the image its own icon
   DeRez -only icns build/icon.png > tmpicns.rsrc # extract the icon to its own resource file
-  Rez -append tmpicns.rsrc -o protocol/desktop/FractalClient # append this resource to the file you want to icon-ize
-  SetFile -a C protocol/desktop/FractalClient # use the resource to set the icon
+  Rez -append tmpicns.rsrc -o protocol-build/desktop/FractalClient # append this resource to the file you want to icon-ize
+  SetFile -a C protocol-build/desktop/FractalClient # use the resource to set the icon
   rm tmpicns.rsrc # clean up
-  # copy over the Unison executable and FFmpeg dylibs
-  cp -R .protocol/lib/64/ffmpeg/Darwin protocol-build/desktop/
 
   # codesign the FractalClient executable
-  codesign -s "Fractal Computers, Inc." protocol/desktop/FractalClient
+  codesign -s "Fractal Computers, Inc." protocol-build/desktop/FractalClient
 fi
 yarn -i
 yarn package
