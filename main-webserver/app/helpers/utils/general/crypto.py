6c6b51a85de96cd13b116525153eedd0c1fac301
Isabelle Zheng <isa.zheng@gmail.com>
2020-07-02 10:15:39 -0700

    add helper to gen private key

---
+++ main-webserver/app/helpers/utils/general/crypto.py
@@ -0,0 +1,13 @@
+from app.imports import *
+
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+
+def genPrivateKey():
+    key = rsa.generate_private_key(
+    backend=crypto_default_backend(), public_exponent=65537, key_size=2048)
+    private_key = key.private_bytes(
+        crypto_serialization.Encoding.PEM, crypto_serialization.PrivateFormat.PKCS8, crypto_serialization.NoEncryption()).decode("utf-8")
+
+    return private_key
