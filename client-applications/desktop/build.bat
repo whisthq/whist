64c728d07f3dcae6532e4435a172756c3041855e
Ming Ying <ming@Mings-MacBook-Air.local>
2020-07-02 08:06:07 -0400

    fixed path again

--- client-applications/desktop/build.bat
+++ client-applications/desktop/build.bat
@@ -24,16 +24,16 @@ if "%1%" == "--help" (
     cmake -G "NMake Makefiles"
     nmake FractalClient
     cd ..
-    rmdir /S/Q protocol
-    mkdir protocol
-    cd protocol
+    rmdir /S/Q protocol-build
+    mkdir protocol-build 
+    cd protocol-build
     mkdir desktop
     mkdir desktop\loading
     cd ..
     xcopy /s .protocol\desktop\build64\Windows protocol-build\desktop
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe', 'rcedit-x64.exe')"
     powershell -Command "Invoke-WebRequest https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe -OutFile rcedit-x64.exe"
-    rcedit-x64.exe protocol\desktop\FractalClient.exe --set-icon build\icon.ico
+    rcedit-x64.exe protocol-build\desktop\FractalClient.exe --set-icon build\icon.ico
     del /Q rcedit-x64.exe
 
     if "%publish%" == "true" (
