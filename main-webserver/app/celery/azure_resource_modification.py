0a6c4f4cc2343bec12d6c68262b1a0cc8edaeb60
mingy98 <ming@fractalcomputers.com>
2020-07-05 16:45:55 -0400

    tentative disk attaching function

--- main-webserver/app/celery/azure_resource_modification.py
+++ main-webserver/app/celery/azure_resource_modification.py
@@ -34,10 +34,10 @@ def swapSpecificDisk(self, vm_name, disk_name, resource_group=os.getenv("VM_GROU
 
     attachDiskToVM(disk_name, vm_name, resource_group)
 
-    fractalVMStart(vm_name, s=self)
+    fractalVMStart(vm_name, resource_group=resource_group, s=self)
 
     lockVMAndUpdate(
-        vm_name=vm_name, state="RUNNING_AVAILABLE", lock=False, temporary_lock=0
+        vm_name=vm_name, state="RUNNING_AVAILABLE", lock=False, temporary_lock=0, resource_group=resource_group
     )
 
     output = fractalSQLSelect(
@@ -100,232 +100,230 @@ def deployArtifact(
     return {"status": SUCCESS, "payload": str(result.value[0])}
 
 
-# @celery_instance.task(bind=True)
-# def automaticAttachDisk(self, disk_name, resource_group=os.getenv("VM_GROUP")):
-#     """Find an available VM to attach a disk to
+@celery_instance.task(bind=True)
+def automaticAttachDisk(self, disk_name, resource_group=os.getenv("VM_GROUP")):
+    """Find an available VM to attach a disk to
 
-#     Args:
-#         disk_name (str): The name of the disk to swap out
+    Args:
+        disk_name (str): The name of the disk to swap out
 
-#     Returns:
-#         json:
-#     """
+    Returns:
+        json:
+    """
 
-#     sendInfo(ID, " Swap disk task for disk {} added to Redis queue".format(disk_name))
+    # Create an Azure instance of disk
 
-#     # Get the
-#     _, compute_client, _ = createClients()
+    _, compute_client, _ = createClients()
+    os_disk = compute_client.disks.get(resource_group, disk_name)
 
-#     os_disk = compute_client.disks.get(os.environ.get("VM_GROUP"), disk_name)
-#     os_type = "Windows" if "windows" in str(os_disk.os_type) else "Linux"
-#     needs_winlogon = os_type == "Windows"
-#     username = mapDiskToUser(disk_name)
-#     vm_name = os_disk.managed_by
+    # Figure out if the disk is Windows or Linux
 
-#     location = os_disk.location
-#     vm_attached = True if vm_name else False
+    os_type = "Windows" if "windows" in str(os_disk.os_type) else "Linux"
+    needs_winlogon = os_type == "Windows"
 
-#     if vm_attached:
-#         self.update_state(
-#             state="PENDING",
-#             meta={
-#                 "msg": "Boot request received successfully. Preparing your cloud PC."
-#             },
-#         )
-#         sendInfo(
-#             ID,
-#             " Azure says that disk {} belonging to {} is attached to {}".format(
-#                 disk_name, username, vm_name
-#             ),
-#         )
-#     else:
-#         self.update_state(
-#             state="PENDING",
-#             meta={"msg": "Boot request received successfully. Fetching your cloud PC."},
-#         )
-#         sendInfo(
-#             ID,
-#             " Azure says that disk {} belonging to {} is not attached to any VM".format(
-#                 disk_name, username
-#             ),
-#         )
+    # Get the username associated with the disk
 
-#     # Update the database to reflect the disk attached to the VM currently
-#     if vm_attached:
-#         vm_name = vm_name.split("/")[-1]
-#         sendInfo(ID, "{}is attached to {}".format(username, vm_name))
+    username = None
+    output = fractalSQLSelect(table_name="disks", params={"disk_name": disk_name})
 
-#         unlocked = False
-#         while not unlocked and vm_attached:
-#             if spinLock(vm_name, s=self) > 0:
-#                 unlocked = True
-#                 # Lock immediately
-#                 lockVMAndUpdate(
-#                     vm_name=vm_name,
-#                     state="ATTACHING",
-#                     lock=True,
-#                     temporary_lock=None,
-#                     change_last_updated=True,
-#                     verbose=False,
-#                     ID=ID,
-#                 )
-#                 lockVM(vm_name, True, username=username, disk_name=disk_name, ID=ID)
+    if output["success"] and output["rows"]:
+        username = output["rows"][0]["username"]
 
-#                 # Update database with new disk name and VM state
-#                 sendInfo(
-#                     ID,
-#                     " Disk {} belongs to user {} and is already attached to VM {}".format(
-#                         disk_name, username, vm_name
-#                     ),
-#                 )
-#                 updateDisk(disk_name, vm_name, location)
+    # Get the VM and location of the disk
 
-#                 self.update_state(
-#                     state="PENDING",
-#                     meta={
-#                         "msg": "Database updated. Sending signal to boot your cloud PC."
-#                     },
-#                 )
+    vm_name = os_disk.managed_by
+    location = os_disk.location
+    vm_attached = True if vm_name else False
 
-#                 sendInfo(
-#                     ID, " Database updated with {} and {}".format(disk_name, vm_name)
-#                 )
+    if vm_attached:
+    else:
+        self.update_state(
+            state="PENDING",
+            meta={"msg": "Boot request received successfully. Fetching your cloud PC."},
+        )
 
-#                 if fractalVMStart(vm_name, needs_winlogon=needs_winlogon, s=self) > 0:
-#                     sendInfo(ID, " VM {} is started and ready to use".format(vm_name))
-#                     self.update_state(
-#                         state="PENDING", meta={"msg": "Cloud PC is ready to use."}
-#                     )
-#                 else:
-#                     sendError(ID, " Could not start VM {}".format(vm_name))
-#                     self.update_state(
-#                         state="FAILURE",
-#                         meta={
-#                             "msg": "Cloud PC could not be started. Please contact support."
-#                         },
-#                     )
+    # If disk is already attached to a VM, start the VM and attach any secondary disks
+    
+    if vm_attached:
+        self.update_state(
+            state="PENDING",
+            meta={
+                "msg": "Boot request received successfully. Preparing your cloud PC."
+            },
+        )
+        
+        vm_name = vm_name.split("/")[-1]
+        unlocked = False
+        
+        while not unlocked and vm_attached:
+            # Wait to gain access to the VM
+            
+            if spinLock(vm_name, resource_group=resource_group, s = self) > 0:
+                # Access gained, lock the VM for ourselves
+                
+                unlocked = True
+                lockVMAndUpdate(
+                    vm_name=vm_name,
+                    state="ATTACHING",
+                    lock=True,
+                    temporary_lock=None,
+                    resource_group=resource_group 
+                )
 
-#                 vm_credentials = fetchVMCredentials(vm_name)
+                # Update database to make sure that the VM is associated with the correct disk
+                # and username
+                
+                fractalSQLUpdate(
+                    table_name=resourceGroupToTable(resource_group),
+                    conditional_params = {
+                        "vm_name": vm_name,
+                    },
+                    new_params = {
+                        "disk_name": disk_name,
+                        "username": username,
+                        "location": location
+                    }
+                )
 
-#                 attachSecondaryDisks(self, username, vm_name)
+                self.update_state(
+                    state="PENDING",
+                    meta={
+                        "msg": "Database updated. Sending signal to boot your cloud PC."
+                    },
+                )
 
-#                 lockVMAndUpdate(
-#                     vm_name=vm_name,
-#                     state="RUNNING_AVAILABLE",
-#                     lock=False,
-#                     temporary_lock=1,
-#                     change_last_updated=True,
-#                     verbose=False,
-#                     ID=ID,
-#                 )
+                if fractalVMStart(vm_name, needs_winlogon=needs_winlogon, resource_group=resource_group, s=self) > 0:
+                    self.update_state(
+                        state="PENDING", meta={"msg": "Cloud PC is ready to use."}
+                    )
+                else:
+                    self.update_state(
+                        state="FAILURE",
+                        meta={
+                            "msg": "Cloud PC could not be started. Please contact support."
+                        },
+                    )
 
-#                 return vm_credentials
-#             else:
-#                 os_disk = compute_client.disks.get(
-#                     os.environ.get("VM_GROUP"), disk_name
-#                 )
-#                 vm_name = os_disk.managed_by
-#                 location = os_disk.location
-#                 vm_attached = True if vm_name else False
+                attachSecondaryDisks(username, vm_name, resource_group, s=self)
 
-#     if not vm_attached:
-#         disk_attached = False
-#         while not disk_attached:
-#             vm = claimAvailableVM(disk_name, location, os_type, s=self)
-#             if vm:
-#                 try:
-#                     vm_name = vm["vm_name"]
+                lockVMAndUpdate(
+                    vm_name=vm_name,
+                    state="RUNNING_AVAILABLE",
+                    lock=False,
+                    temporary_lock=1,
+                    resource_group=resource_group 
+                )
 
-#                     vm_info = compute_client.virtual_machines.get(
-#                         os.getenv("VM_GROUP"), vm_name
-#                     )
+                output = fractalSQLSelect(table_name=resourceGroupToTable(vm_name), params={
+                    "vm_name": vm_name 
+                })
+                
+                if output["success"] and output["rows"]:
+                    return output["rows"][0]
+                else:
+                    return {}
+                
+            else:
+                os_disk = compute_client.disks.get(
+                    resource_group, disk_name
+                )
+                vm_name = os_disk.managed_by
+                location = os_disk.location
+                vm_attached = True if vm_name else False
 
-#                     for disk in vm_info.storage_profile.data_disks:
-#                         if disk.name != disk_name:
-#                             self.update_state(
-#                                 state="PENDING",
-#                                 meta={
-#                                     "msg": "Making sure that you have a stable connection."
-#                                 },
-#                             )
+    if not vm_attached:
+        disk_attached = False
+        while not disk_attached:
+            vm = claimAvailableVM(username, disk_name, location, resource_group, os_type, s=self)
+            if vm:
+                try:
+                    vm_name = vm["vm_name"]
 
-#                             sendInfo(
-#                                 ID,
-#                                 "Detaching disk {} from {}".format(disk_name, vm_name),
-#                             )
+                    vm_info = compute_client.virtual_machines.get(
+                        resource_group, vm_name
+                    )
 
-#                             detachDisk(disk.name, vm_name)
+                    for disk in vm_info.storage_profile.data_disks:
+                        if disk.name != disk_name:
+                            self.update_state(
+                                state="PENDING",
+                                meta={
+                                    "msg": "Making sure that you have a stable connection."
+                                },
+                            )
 
-#                     sendInfo(
-#                         ID,
-#                         "Disk {} was unattached. VM {} claimed for {}".format(
-#                             disk_name, vm_name, username
-#                         ),
-#                     )
+                            detachSecondaryDisk(disk.name, vm_name, resource_group)
 
-#                     if swapDiskAndUpdate(self, disk_name, vm_name, needs_winlogon) > 0:
-#                         self.update_state(
-#                             state="PENDING",
-#                             meta={"msg": "Data successfully uploaded to cloud PC."},
-#                         )
-#                         free_vm_found = True
-#                         updateOldDisk(vm_name)
-#                         attachSecondaryDisks(self, username, vm_name)
+                    if swapDiskAndUpdate(disk_name, vm_name, needs_winlogon, resource_group, s=self) > 0:
+                        self.update_state(
+                            state="PENDING",
+                            meta={"msg": "Data successfully uploaded to cloud PC."},
+                        )
+                        free_vm_found = True
+                        attachSecondaryDisks(username, vm_name, resource_group, s=self)
 
-#                         lockVMAndUpdate(
-#                             vm_name=vm_name,
-#                             state="RUNNING_AVAILABLE",
-#                             lock=False,
-#                             temporary_lock=1,
-#                             change_last_updated=True,
-#                             verbose=False,
-#                             ID=ID,
-#                         )
+                        lockVMAndUpdate(
+                            vm_name=vm_name,
+                            state="RUNNING_AVAILABLE",
+                            lock=False,
+                            temporary_lock=1,
+                            resource_group=resource_group
+                        )
 
-#                         return fetchVMCredentials(vm_name)
+                        output = fractalSQLSelect(table_name=resourceGroupToTable(vm_name), params={
+                            "vm_name": vm_name 
+                        })
+                        
+                        if output["success"] and output["rows"]:
+                            return output["rows"][0]
+                        else:
+                            return {}
 
-#                     vm_credentials = fetchVMCredentials(vm_name)
-#                     attachSecondaryDisks(self, username, vm_name)
+                    attachSecondaryDisks(username, vm_name, resource_group, s=self)
 
-#                     lockVMAndUpdate(
-#                         vm_name=vm_name,
-#                         state="RUNNING_AVAILABLE",
-#                         lock=False,
-#                         temporary_lock=1,
-#                         change_last_updated=True,
-#                         verbose=False,
-#                         ID=ID,
-#                     )
+                    lockVMAndUpdate(
+                        vm_name=vm_name,
+                        state="RUNNING_AVAILABLE",
+                        lock=False,
+                        temporary_lock=1,
+                        resource_group=resource_group
+                    )
 
-#                     disk_attached = True
-#                     sendInfo(
-#                         ID,
-#                         " VM {} successfully attached to disk {}".format(
-#                             vm_name, disk_name
-#                         ),
-#                     )
+                    disk_attached = True
+                    
+                    output = fractalSQLSelect(table_name=resourceGroupToTable(vm_name), params={
+                        "vm_name": vm_name 
+                    })
+                    
+                    if output["success"] and output["rows"]:
+                        return output["rows"][0]
+                    else:
+                        return {}
+                except Exception as e:
+                    fractalLog(
+                        function="automaticAttachDisk",
+                        label=username,
+                        logs="Critical error attaching disk: {error}".format(error=str(e)),
+                        level=logging.CRITICAL
+                    )
 
-#                     return vm_credentials
-#                 except Exception as e:
-#                     sendCritical(ID, str(e))
+            else:
+                self.update_state(
+                    state="PENDING",
+                    meta={
+                        "msg": "Running performance tests. This could take a few extra minutes."
+                    },
+                )
+                fractalLog(
+                    function="automaticAttachDisk",
+                    label=username,
+                    logs="No VMs available, waiting 30 seconds...",
+                    level=logging.WARNING
+                )
+                time.sleep(30)
 
-#             else:
-#                 self.update_state(
-#                     state="PENDING",
-#                     meta={
-#                         "msg": "Running performance tests. This could take a few extra minutes."
-#                     },
-#                 )
-#                 sendInfo(
-#                     ID,
-#                     "No VMs are available for {} using {}. Going to sleep...".format(
-#                         username, disk_name
-#                     ),
-#                 )
-#                 time.sleep(30)
-
-#     self.update_state(
-#         state="FAILURE",
-#         meta={"msg": "Cloud PC could not be started. Please contact support."},
-#     )
-#     return None
+    self.update_state(
+        state="FAILURE",
+        meta={"msg": "Cloud PC could not be started. Please contact support."},
+    )
+    return None
--- main-webserver/app/helpers/utils/azure/azure_helpers/azure_resource_modification_helpers.py
+++ main-webserver/app/helpers/utils/azure/azure_helpers/azure_resource_modification_helpers.py
@@ -1,75 +1,159 @@
-# from app import *
-# from app.helpers.utils.azure.azure_general import *
-# from app.helpers.utils.azure.azure_resource_locks import *
-# from app.helpers.utils.azure.azure_resource_state_management import *
+from app import *
+from app.helpers.utils.azure.azure_general import *
+from app.helpers.utils.azure.azure_resource_locks import *
+from app.helpers.utils.azure.azure_resource_state_management import *
+from app.helpers.utils.azure.azure_resource_modification import *
 
 
-# def swapDiskAndUpdate(s, disk_name, vm_name, needs_winlogon, resource_group):
-#     s.update_state(
-#         state="PENDING",
-#         meta={
-#             "msg": "Uploading the necessary data to our servers. This could take a few minutes."
-#         },
-#     )
+def swapDiskAndUpdate( disk_name, vm_name, needs_winlogon, resource_group, s=None):
+    s.update_state(
+        state="PENDING",
+        meta={
+            "msg": "Uploading the necessary data to our servers. This could take a few minutes."
+        },
+    )
 
-#     attachDiskToVM(disk_name, vm_name, resource_group)
+    attachDiskToVM(disk_name, vm_name, resource_group)
 
-#     fractalVMStart(
-#         vm_name,
-#         needs_restart=True,
-#         needs_winlogon=needs_winlogon,
-#         resource_group=resource_group,
-#         s=s,
-#     )
+    fractalVMStart(
+        vm_name,
+        needs_restart=True,
+        needs_winlogon=needs_winlogon,
+        resource_group=resource_group,
+        s=s,
+    )
 
-#     output = fractalSQLSelect(table_name="disks", params={"disk_name": disk_name})
+    output = fractalSQLSelect(table_name="disks", params={"disk_name": disk_name})
 
-#     if output["rows"] and output["success"]:
-#         fractalSQLUpdate(
-#             table_name=resourceGroupToTable(resource_group),
-#             conditional_params={"vm_name": vm_name},
-#             new_params={
-#                 "disk_name": disk_name,
-#                 "username": output["rows"][0]["username"],
-#             },
-#         )
+    if output["rows"] and output["success"]:
+        fractalSQLUpdate(
+            table_name=resourceGroupToTable(resource_group),
+            conditional_params={"vm_name": vm_name},
+            new_params={
+                "disk_name": disk_name,
+                "username": output["rows"][0]["username"],
+            },
+        )
+        
+    return 1
 
-# def attachSecondaryDisks(s, username, vm_name):
-#     output = fractalSQLSelect(table_name="disks", params={
-#         "username": username,
-#         "state": "ACTIVE",
-#         "main": False
-#     })
 
-#     if output["success"] and output["rows"]:
-#         secondary_disks = output["rows"]
+def attachSecondaryDisks(username, vm_name, resource_group, s=None):
+    output = fractalSQLSelect(
+        table_name="disks",
+        params={"username": username, "state": "ACTIVE", "main": False},
+    )
 
-#         lockVMAndUpdate(
-#             vm_name=vm_name,
-#             state="ATTACHING",
-#             lock=True,
-#             temporary_lock=None
-#         )
+    if output["success"] and output["rows"]:
+        secondary_disks = output["rows"]
 
-#         s.update_state(
-#             state="PENDING",
-#             meta={
-#                 "msg": "{} extra storage hard drive(s) were found on your cloud PC. Running a few extra tests.".format(
-#                     len(secondary_disks)
-#                 )
-#             },
-#         )
+        lockVMAndUpdate(
+            vm_name=vm_name,
+            state="ATTACHING",
+            lock=True,
+            temporary_lock=None,
+            resource_group=resource_group,
+        )
 
-#         for secondary_disk in secondary_disks:
-#             attachDisk(secondary_disk["disk_name"], vm_name)
+        s.update_state(
+            state="PENDING",
+            meta={
+                "msg": "{} extra storage hard drive(s) were found on your cloud PC. Running a few extra tests.".format(
+                    len(secondary_disks)
+                )
+            },
+        )
 
-#         # Lock immediately
-#         lockVMAndUpdate(
-#             vm_name=vm_name,
-#             state="RUNNING_AVAILABLE",
-#             lock=False,
-#             temporary_lock=1,
-#             change_last_updated=True,
-#             verbose=False,
-#             ID=ID,
-#         )
+        for secondary_disk in secondary_disks:
+            attachSecondaryDisk(
+                secondary_disk["disk_name"], vm_name, resource_group=resource_group, s=s
+            )
+
+        # Lock immediately
+        lockVMAndUpdate(
+            vm_name=vm_name,
+            state="RUNNING_AVAILABLE",
+            lock=False,
+            temporary_lock=1,
+            resource_group=resource_group,
+        )
+
+
+def claimAvailableVM(
+    username, disk_name, location, resource_group, os_type="Windows", s=None, ID=-1
+):
+    session = Session()
+
+    state_preference = ["RUNNING_AVAILABLE", "STOPPED", "DEALLOCATED"]
+
+    for state in state_preference:
+        command = text(
+            """
+            SELECT * FROM {table_name}
+            WHERE lock = :lock AND state = :state AND dev = :dev AND os = :os_type AND location = :location
+            AND (temporary_lock <= :temporary_lock OR temporary_lock IS NULL)
+            """.format(
+                table_name=resourceGroupToTable(resource_group)
+            )
+        )
+        params = {
+            "lock": False,
+            "state": state,
+            "dev": False,
+            "location": location,
+            "temporary_lock": dateToUnix(getToday()),
+            "os_type": os_type,
+        }
+
+        available_vm = cleanFetchedSQL(session.execute(command, params).fetchone())
+
+        if available_vm:
+            if s:
+                if state == "RUNNING_AVAILABLE":
+                    s.update_state(
+                        state="PENDING",
+                        meta={
+                            "msg": "Your cloud PC is powered on. Preparing your cloud PC (this could take a few minutes)."
+                        },
+                    )
+                else:
+                    s.update_state(
+                        state="PENDING",
+                        meta={
+                            "msg": "Your cloud PC is powered off. Preparing your cloud PC (this could take a few minutes)."
+                        },
+                    )
+
+            command = text(
+                """
+                UPDATE {table_name}
+                SET lock = :lock, username = :username, disk_name = :disk_name, state = :state, last_updated = :last_updated
+                WHERE vm_name = :vm_name
+                """.format(
+                table_name=resourceGroupToTable(resource_group)
+            )
+            params = {
+                "lock": True,
+                "username": username,
+                "disk_name": disk_name,
+                "vm_name": available_vm["vm_name"],
+                "state": "ATTACHING",
+                "last_updated": getCurrentTime(),
+            }
+            session.execute(command, params)
+            session.commit()
+            session.close()
+            return available_vm
+        else:
+            fractalLog(
+                function="claimAvailableVM",
+                label="username",
+                logs="Did not find any VMs in {location} with state {state}.".format(
+                    location=location, state=state
+                ),
+            )
+
+    session.commit()
+    session.close()
+    return None
+
