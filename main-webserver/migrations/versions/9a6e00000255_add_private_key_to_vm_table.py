aaeafd941371849485050e52d89198f3fe1dec8a
Fractal Linter <linter@fractalcomputers.com>
2020-07-06 16:17:13 +0000

    Linted with Python Black

--- main-webserver/migrations/versions/9a6e00000255_add_private_key_to_vm_table.py
+++ main-webserver/migrations/versions/9a6e00000255_add_private_key_to_vm_table.py
@@ -10,17 +10,15 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9a6e00000255'
-down_revision = '4c2d39511732'
+revision = "9a6e00000255"
+down_revision = "4c2d39511732"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('v_ms',
-        sa.Column('rsa_private_key', sa.String())
-    )
+    op.add_column("v_ms", sa.Column("rsa_private_key", sa.String()))
 
 
 def downgrade():
-    op.drop_column('v_ms', 'rsa_private_key')
+    op.drop_column("v_ms", "rsa_private_key")
