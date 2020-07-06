046d6eac41b28462a1474b3693cad85550631cd5
Isabelle Zheng <isa.zheng@gmail.com>
2020-07-06 14:23:54 -0700

    add helpers for hashing

--- main-webserver/app/helpers/utils/general/crypto.py
+++ main-webserver/app/helpers/utils/general/crypto.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
+import hashlib, binascii
 
 def genPrivateKey():
     key = rsa.generate_private_key(
@@ -16,3 +17,12 @@ def genPrivateKey():
     ).decode("utf-8")
 
     return private_key
+
+def hash_value(value):
+    dk = hashlib.pbkdf2_hmac('sha256', value.encode('utf-8'), os.getenv('SECRET_KEY').encode('utf-8'), 100000)
+    return binascii.hexlify(dk).decode('utf-8')
+
+def check_value(hashed_value, raw_value):
+    dk = hashlib.pbkdf2_hmac('sha256', raw_value.encode('utf-8'), os.getenv('SECRET_KEY').encode('utf-8'), 100000)
+    hr = binascii.hexlify(dk).decode('utf-8')
+    return hashed_value == hr
