ae678383cf8fe24b457be5e33aadf8796dbb4245
Isabelle Zheng <isa.zheng@gmail.com>
2020-07-06 11:40:57 -0700

    add script to generate private keys

---
+++ main-webserver/privateKey.py
@@ -0,0 +1,13 @@
+from app import *
+from app.helpers.utils.general.sql_commands import *
+from app.helpers.utils.general.crypto import *
+
+
+output = fractalRunSQL(command="""
+    SELECT *
+    FROM v_ms WHERE rsa_private_key IS NULL;
+""", params="")
+
+for vm in output["rows"]:
+    private_key = genPrivateKey()
+    fractalSQLUpdate(table_name="v_ms", conditional_params={"vm_name": vm["vm_name"]}, new_params={"rsa_private_key": private_key})
