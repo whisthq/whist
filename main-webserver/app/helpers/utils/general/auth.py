0117aa3ea8636cc782130736b5d0d47dca6c07d5
mingy98 <ming@fractalcomputers.com>
2020-07-08 16:04:41 -0400

    admin decorator, disk empty creation, table fetching endpoint

--- main-webserver/app/helpers/utils/general/auth.py
+++ main-webserver/app/helpers/utils/general/auth.py
@@ -5,12 +5,6 @@ from app.helpers.utils.general.sql_commands import *
 def fractalAuth(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        received_from = (
-            request.headers.getlist("X-Forwarded-For")[0]
-            if request.headers.getlist("X-Forwarded-For")
-            else request.remote_addr
-        )
-
         username = None
 
         try:
@@ -59,3 +53,42 @@ def fractalAuth(f):
         return f(*args, **kwargs)
 
     return wrapper
+
+
+from app.imports import *
+from app.helpers.utils.general.sql_commands import *
+
+
+def adminRequired(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        username = None
+
+        try:
+            if request.method == "POST":
+                username = body["username"]
+            elif request.method == "GET":
+                username = request.args.get("username")
+        except Exception as e:
+            return (
+                jsonify(
+                    {"error": "No username provided, cannot authorize Bearer token."}
+                ),
+                UNAUTHORIZED,
+            )
+
+        current_user = get_jwt_identity()
+        if not os.getenv("DASHBOARD_USERNAME") in current_user:
+            return (
+                jsonify(
+                    {
+                        "error": "Authorization failed. Provided username does not match username associated with provided Bearer token."
+                    }
+                ),
+                UNAUTHORIZED,
+            )
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
