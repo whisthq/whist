e056293ea017254c204f925c558c289b8db2bfad
Isabelle Zheng <isa.zheng@gmail.com>
2020-07-08 13:11:29 -0700

    add admin blueprint

--- main-webserver/app/factory.py
+++ main-webserver/app/factory.py
@@ -24,6 +24,7 @@ def init_app(app):
     from .blueprints.artifact_blueprint import artifact_bp
     from .blueprints.report_blueprint import report_bp
     from .blueprints.analytics_blueprint import analytics_bp
+    from .blueprints.admin_blueprint import admin_bp
 
     app.register_blueprint(account_bp)
     app.register_blueprint(token_bp)
@@ -33,5 +34,6 @@ def init_app(app):
     app.register_blueprint(artifact_bp)
     app.register_blueprint(report_bp)
     app.register_blueprint(analytics_bp)
+    app.register_blueprint(admin_bp)
 
     return app
