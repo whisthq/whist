983d32b55c713dd40206b9698d77e9d10abf8910
Jonathan <jonathanxu25@gmail.com>
2020-07-10 15:28:44 -0400

    fixed validateTokenHelper

--- main-webserver/app/helpers/blueprint_helpers/token_post.py
+++ main-webserver/app/helpers/blueprint_helpers/token_post.py
@@ -5,13 +5,12 @@ from app.helpers.utils.general.sql_commands import *
 
 def validateTokenHelper(token):
     params = {"token": token}
-    user = fractalSQLSelect("password_tokens", params)
-    if user:
-        timeIssued = user[0][1]
+    token = fractalSQLSelect("password_tokens", params)["rows"]
+    if token:
+        timeIssued = token[0]["time_issued"]
         diff = (
             dt.now() - dt.strptime(timeIssued, "%m-%d-%Y, %H:%M:%S")
         ).total_seconds()
-        conn.close()
         if diff > (60 * 10):
             return (
                 jsonify({"status": UNAUTHORIZED, "error": "Expired token"}),
@@ -20,5 +19,4 @@ def validateTokenHelper(token):
         else:
             return jsonify({"status": SUCCESS}), SUCCESS
     else:
-        conn.close()
         return jsonify({"status": UNAUTHORIZED, "error": "Invalid token"}), UNAUTHORIZED
