d029fee8051580b20abd50449a5c8a1e4061e588
mingy98 <ming@fractalcomputers.com>
2020-07-10 09:05:06 -0400

    google auth in progress

--- main-webserver/app/helpers/blueprint_helpers/google_auth_post.py
+++ main-webserver/app/helpers/blueprint_helpers/google_auth_post.py
@@ -15,42 +15,29 @@ def loginHelper(code, clientApp):
 
     if output["success"] and output["rows"]:
         if output["rows"][0]["google_login"]:
-            return (
-                jsonify(
-                    {
-                        "new_user": False,
-                        "is_user": True,
-                        "token": token,
-                        "access_token": access_token,
-                        "refresh_token": refresh_token,
-                        "username": username,
-                        "status": SUCCESS,
-                    }
-                ),
-                SUCCESS,
-            )
-        else:
-            return (
-                jsonify({"status": 403, "error": "Try using non-Google login",}),
-                403,
-            )
-
-    if clientApp:
-        return (jsonify({"status": 401, "error": "User has not registered"}), 401)
-
-    status = registerGoogleUser(username, name, token)
-
-    return (
-        jsonify(
-            {
-                "status": status,
-                "new_user": True,
+            return {
+                "new_user": False,
                 "is_user": True,
                 "token": token,
                 "access_token": access_token,
                 "refresh_token": refresh_token,
                 "username": username,
+                "status": SUCCESS,
             }
-        ),
-        status,
-    )
+        else:
+            return {"status": FORBIDDEN, "error": "Try using non-Google login"}
+
+    if clientApp:
+        return {"status": UNAUTHORIZED, "error": "User has not registered"}
+
+    status = registerGoogleUser(username, name, token)
+
+    return {
+        "status": status,
+        "new_user": True,
+        "is_user": True,
+        "token": token,
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "username": username,
+    }
