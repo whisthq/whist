dea75cefaca4f397b783c7e901f6aab0745b712a
Isabelle Zheng <isa.zheng@gmail.com>
2020-07-10 14:33:16 -0700

    fix stripe receive helper for when customers or credits does not exist

--- main-webserver/app/blueprints/payment/stripe_blueprint.py
+++ main-webserver/app/blueprints/payment/stripe_blueprint.py
@@ -6,6 +6,7 @@ stripe_bp = Blueprint("stripe_bp", __name__)
 
 @stripe_bp.route("/stripe/<action>", methods=["POST"])
 @fractalPreProcess
+@jwt_required
 @fractalAuth
 def payment(action, **kwargs):
     body = kwargs["body"]
