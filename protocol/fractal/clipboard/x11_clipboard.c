bec2d1486c9b56a9ac06e50a0a7b03adfa41047e
shamazo <hmnicholson12@gmail.com>
2020-07-13 10:56:57 -0400

    Fix whitespace in CLOSE_FDS in x11_clipboard.c. Fixes linux clipboard text.

--- protocol/fractal/clipboard/x11_clipboard.c
+++ protocol/fractal/clipboard/x11_clipboard.c
@@ -33,13 +33,12 @@ whatever files are in the SET_CLIPBOARD directory.
 
 #define CLOSE_FDS \
     "for fd in $(ls /proc/$$/fd); do\
-  case \"$fd\" in\
-    0|1|2|255)\
+  case \"$fd\" in 0|1|2|255)\
       ;;\
     *)\
       eval \"exec $fd>&-\"\
       ;;\
-  esac\
+  esac \
 done; "
 
 #define MAX_CLIPBOARD_SIZE 10000000
