38009dc6152d353ba65192d43a603475a74e4728
Isabelle Zheng <isa.zheng@gmail.com>
2020-07-15 11:07:47 -0700

    add script to generate private keys for all existing vms

--- main-webserver/app/constants/resources.py
+++ main-webserver/app/constants/resources.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = (
     os.getenv("DATABASE_URL")
---
+++ main-webserver/privateKey.py
@@ -0,0 +1,13 @@
+from app import *
+from app.helpers.utils.general.sql_commands import *
+from app.helpers.utils.azure.azure_resource_creation import *
+
+
+output = fractalRunSQL(command="""
+    SELECT *
+    FROM v_ms WHERE private_key IS NULL;
+""", params="")
+
+for vm in output["rows"]:
+    private_key = generatePrivateKey()
+    fractalSQLUpdate(table_name="v_ms", conditional_params={"vm_name": vm["vm_name"]}, new_params={"private_key": private_key})
