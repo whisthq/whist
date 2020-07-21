fb86c329771f4360e477b71a68f3914fe0f43851
Gavin Rees <gavinrees@college.harvard.edu>
2020-07-21 17:37:49 -0400

    Bugfixes
    I gotta start editing on my VM

--- protocol/fractal/utils/logging.c
+++ protocol/fractal/utils/logging.c
@@ -126,18 +126,18 @@ void startConnectionLog() {
     strcat(l, log_directory);
     strcat(l, "/log_connection.txt");
 
-    fprintf("Opening file\n"):
+    printf("Opening file\n"):
     FILE *log_connection_file = fopen(l, "wb");
 
     //  Set up log_connection.txt to see when a connection begins
     //  Use lock so there aren't any race conditions with
     //  Making a log_prev.txt at the same time
-    fprintf("Attempting to acquire mutex\n");
+    printf("Attempting to acquire mutex\n");
     SDL_LockMutex((SDL_mutex *) logger_mutex);
-    fprintf("Acquired mutex\n");
+    printf("Acquired mutex\n");
     int sz = ftell(mprintf_log_file);
     fprintf(log_connection_file, "%d\n%d", sz, 1);
-    fprintf("Printed successfully\n");
+    printf("Printed successfully\n");
     fclose(log_connection_file);
     SDL_UnlockMutex((SDL_mutex *) logger_mutex);
     printf("Completed startConnectionLog");
