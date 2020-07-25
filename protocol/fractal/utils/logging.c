1123d4e9dd540a20adf982588c43c93bc4182792
Fractal Linter <linter@fractalcomputers.com>
2020-07-25 00:03:23 +0000

    Linted via clang-format

--- protocol/fractal/utils/logging.c
+++ protocol/fractal/utils/logging.c
@@ -640,7 +640,7 @@ char *get_version() {
         static char buf[200];
         version = buf;
         int bytes = (int)fread(version, 1, min(length, (long)sizeof(buf) - 1),
-                             f);  // cast for compiler warning
+                               f);  // cast for compiler warning
         for (int i = 0; i < bytes; i++) {
             if (version[i] == '\n') {
                 version[i] = '\0';
