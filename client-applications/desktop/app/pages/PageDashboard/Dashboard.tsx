a89e92a77dcefb4fac6eb52bac68dd461714ab4a
philippemnoel <philippe_noel@college.harvard.edu>
2020-07-31 12:15:54 -0400

    ran yarn lint

--- client-applications/desktop/app/pages/PageDashboard/Dashboard.tsx
+++ client-applications/desktop/app/pages/PageDashboard/Dashboard.tsx
@@ -266,8 +266,9 @@ class Dashboard extends Component {
                                         >
                                             You can also exit via the shortchut{" "}
                                             <b>Ctrl+Alt+F4</b>, on Windows
-                                            devices, and <b>Fn+Ctrl+Option+F4</b>, on
-                                            MacOS devices.
+                                            devices, and{" "}
+                                            <b>Fn+Ctrl+Option+F4</b>, on MacOS
+                                            devices.
                                         </div>
                                         <div className={styles.subTitle}>
                                             Clipboard
