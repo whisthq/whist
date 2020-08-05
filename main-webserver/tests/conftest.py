0cd60821b7394ff76b4340211ff50a413112a67b
Isabelle Zheng <isa.zheng@gmail.com>
2020-08-05 10:56:53 -0700

    test parallelizing

--- main-webserver/tests/conftest.py
+++ main-webserver/tests/conftest.py
@@ -5,6 +5,28 @@ from dotenv import *
 
 load_dotenv(find_dotenv())
 
+# parallelizes tests
+def pytest_collection_modifyitems(config, items):
+    ci_node_total = int(os.getenv("CI_NODE_TOTAL", 1))
+    ci_node_index = int(os.getenv("CI_NODE_INDEX", 0))
+    if ci_node_total == 2:
+        fractalLog(
+            function="conftest",
+            label="conftest",
+            logs="Parallelizing tests",
+        )
+        if ci_node_index == 0:
+            items[:] = [
+                item
+                for index, item in enumerate(items)
+                if "disk" in item.name
+            ]
+        else:
+            items[:] = [
+                item
+                for index, item in enumerate(items)
+                if "vm" in item.name
+            ]
 
 @pytest.fixture
 def input_token():
