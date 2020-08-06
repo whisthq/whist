8ca5bbb1ec92e6332a1c3955e2823ae0f8a59a60
Gavin Rees <gavin@fractalcomputers.com>
2020-08-06 14:19:49 +0000

    Intermediate commit, adapting to new setup-scripts

--- container-images/chrome/entry.sh
+++ container-images/chrome/entry.sh
@@ -5,7 +5,7 @@ yes | ufw allow 5900;
 
 #Allow for ssh login
 rm /var/run/nologin
-echo $SSH_PUBLIC_KEY_AWS > ~/.ssh/authorized_keys
+# echo $SSH_PUBLIC_KEY_AWS > ~/.ssh/authorized_keys
 
 sudo rm /etc/udev/rules.d/90-fractal-input.rules
 sudo ln -s /home/fractal/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
