d9a0a39fd30c8d4cb10740a1b7e588564212fe9d
shamazo <hmnicholson12@gmail.com>
2020-08-11 10:53:37 -0400

    add auto sentry download

---
+++ protocol/get_latest_sentry.py
@@ -0,0 +1,16 @@
+"""
+Written by Hamish Nicholson 2020/08/11
+This file downloads and extracts the latest sentry-native release direct from their github
+"""
+
+import requests
+from zipfile import ZipFile
+
+response = requests.get("https://api.github.com/repos/getsentry/sentry-native/releases/latest")
+download_url = response.json()['assets'][0]['browser_download_url']
+r = requests.get(download_url, allow_redirects=True)
+open('sentry-native.zip', 'wb').write(r.content)
+
+with ZipFile('sentry-native.zip', 'r') as zipObj:
+    # Extract all the contents of zip file in current directory
+    zipObj.extractall('sentry-native')
