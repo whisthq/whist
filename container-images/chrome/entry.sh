7d3cab207ef4e0ab734a9a0b370d1e14b22a4b04
EC2 Default User <ec2-user@ip-172-31-74-115.ec2.internal>
2020-08-11 20:58:19 +0000

    Fixes to environment variables and gnome setup

--- container-images/chrome/entry.sh
+++ container-images/chrome/entry.sh
@@ -9,7 +9,7 @@ rm /var/run/nologin
 # echo $SSH_PUBLIC_KEY_AWS > ~/.ssh/authorized_keys
 
 sudo rm /etc/udev/rules.d/90-fractal-input.rules
-sudo ln -s /home/fractal/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
+sudo ln -sf /home/fractal/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
 # echo "Entry.sh handing off to bootstrap.sh" 
 
 #This from setup-scripts install fractal service
