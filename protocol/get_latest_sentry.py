c60d7b9f4c99953ec1f3ac6f21aca35a7b0ec83c
shamazo <hmnicholson12@gmail.com>
2020-08-12 15:05:39 -0400

    Run get_latest_sentry.py directly from the workflows.
    We are doing this because we want to use an authenticated API request and passing the github token around from github actions to cmake to python is more trouble than it is worth. Devs will need to run get_latest_sentry.py before compiling the first time they clone the repo.

--- protocol/get_latest_sentry.py
+++ protocol/get_latest_sentry.py
@@ -6,16 +6,36 @@ This file downloads and extracts the latest sentry-native release direct from th
 
 import requests
 from zipfile import ZipFile
-try:
-    response = requests.get("https://api.github.com/repos/getsentry/sentry-native/releases/latest")
-    download_url = response.json()['assets'][0]['browser_download_url']
-    r = requests.get(download_url, allow_redirects=True)
-    open('sentry-native.zip', 'wb+').write(r.content)
-except:
-    print("Failed to download sentry.")
-    print("Headers: ", response.headers)
-    print("Response: ", response.json())
+import argparse
 
-with ZipFile('sentry-native.zip', 'r') as zipObj:
-    # Extract all the contents of zip file in current directory
-    zipObj.extractall('sentry-native')
+def get_sentry(token=None):
+    try:
+        headers = {}
+        if token:
+            headers = {'Authorization': 'token %s' % token}
+        response = requests.get("https://api.github.com/repos/getsentry/sentry-native/releases/latest", headers=headers)
+        download_url = response.json()['assets'][0]['browser_download_url']
+        r = requests.get(download_url, allow_redirects=True)
+        open('sentry-native.zip', 'wb+').write(r.content)
+    except:
+        print("Failed to download sentry.")
+        print("Headers: ", response.headers)
+        print("Response: ", response.json())
+
+    with ZipFile('sentry-native.zip', 'r') as zipObj:
+        # Extract all the contents of zip file in current directory
+        zipObj.extractall('sentry-native')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "--token",
+        help="github PAT for optionally authenticated API requests",
+        default=None
+    )
+    args = parser.parse_args()
+    get_sentry(args.token)
