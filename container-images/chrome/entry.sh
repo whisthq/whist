20a22329f1e42ecd54cea5990efa14a5b3dcb118
EC2 Default User <ec2-user@ip-172-31-74-115.ec2.internal>
2020-08-13 21:24:01 +0000

    Bugfixes to help fractal-entrypoint.service

--- container-images/chrome/entry.sh
+++ container-images/chrome/entry.sh
@@ -8,12 +8,12 @@
 rm /var/run/nologin
 # echo $SSH_PUBLIC_KEY_AWS > ~/.ssh/authorized_keys
 
-sudo rm /etc/udev/rules.d/90-fractal-input.rules
-sudo ln -sf /home/fractal/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
+rm /etc/udev/rules.d/90-fractal-input.rules
+ln -sf /home/fractal/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
 # echo "Entry.sh handing off to bootstrap.sh" 
 
 #This from setup-scripts install fractal service
 echo "Start Pam Systemd Process for User fractal"
 export FRACTAL_UID=`id -u fractal`
-sudo install -d -o fractal /run/user/$FRACTAL_UID
-sudo systemctl start user@$FRACTAL_UID
+install -d -o fractal /run/user/$FRACTAL_UID
+systemctl start user@$FRACTAL_UID
