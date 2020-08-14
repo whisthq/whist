385cca283a3a3ee2e26c7570d7a65a1a24b1efad
Roshan Padaki <roshanpadaki@gmail.com>
2020-08-14 15:49:26 -0400

    oh boy

---
+++ protocol/fractal/input/x11input.h
@@ -0,0 +1,29 @@
+#ifndef LINUX_INPUT_H
+#define LINUX_INPUT_H
+/**
+ * Copyright Fractal Computers, Inc. 2020
+ * @file x11input.h
+ * @brief This file defines the X11 input device type.
+============================
+Usage
+============================
+*/
+
+/*
+============================
+Includes
+============================
+*/
+
+#include "../core/fractal.h"
+// #include "../utils/sdlscreeninfo.h"
+
+/*
+============================
+Custom Types
+============================
+*/
+
+typedef char input_device_t;
+
+#endif  // LINUX_INPUT_H
