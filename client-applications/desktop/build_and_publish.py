1af41b0bb262f18a5cfe678fbbe32ad0a6915476
shamazo <hmnicholson12@gmail.com>
2020-08-14 16:11:04 -0400

    try relative path for icon

--- client-applications/desktop/build_and_publish.py
+++ client-applications/desktop/build_and_publish.py
@@ -118,8 +118,9 @@ def prep_macos(desktop_dir: Path, protocol_dir: Path, codesign_identity: str) ->
         run_cmd(  # extract the icon to its own resource file
             ["DeRez", "-only", "icns", str(src_icon)], stdout=f
         )
+    run_cmd(["ls"])
     run_cmd(  # append this resource to the file you want to icon-ize
-        ["Rez", "-append", str(tmp_icon), "-o", str(client)]
+        ["Rez", "-append", "tmpicns.rsrc", "-o", str(client)]
     )
 
     run_cmd(["SetFile", "-a", "C", str(client)])  # use the resource to set the icon
