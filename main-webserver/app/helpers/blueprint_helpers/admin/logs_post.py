770b0e20202e7bfb41350649903627ffc39f72b1
Isabelle Zheng <isa.zheng@gmail.com>
2020-08-18 13:45:45 -0700

    update logs_post helper

--- main-webserver/app/helpers/blueprint_helpers/admin/logs_post.py
+++ main-webserver/app/helpers/blueprint_helpers/admin/logs_post.py
@@ -1,19 +1,25 @@
 from app import *
 
+from app.models.logs import *
+
 
 def bookmarkHelper(connection_id):
-    output = fractalSQLUpdate(
-        table_name="protocol_logs", conditional_params={"connection_id": connection_id},
-        new_params={"bookmarked": True}
-    )
+    try:
+        log = ProtocolLog.query.filter_by(connection_id=connection_id).first()
+        log.bookmarked = True
+        db.session.commit()
 
-    return {"status": SUCCESS} if output["success"] else {"status": BAD_REQUEST}
+        return {"status": SUCCESS}
+    except Exception:
+        return {"status": BAD_REQUEST}
 
 
 def unbookmarkHelper(connection_id):
-    output = fractalSQLUpdate(
-        table_name="protocol_logs", conditional_params={"connection_id": connection_id},
-        new_params={"bookmarked": False}
-    )
+        try:
+            log = ProtocolLog.query.filter_by(connection_id=connection_id).first()
+            log.bookmarked = False
+            db.session.commit()
 
-    return {"status": SUCCESS} if output["success"] else {"status": BAD_REQUEST}
+            return {"status": SUCCESS}
+        except Exception:
+            return {"status": BAD_REQUEST}
