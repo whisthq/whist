2fc9461c9fedf34d91fd4944c8208b431e4a10b5
Isabelle Zheng <isa.zheng@gmail.com>
2020-08-18 15:44:19 -0700

    serialize logs endpoints

--- main-webserver/app/helpers/blueprint_helpers/admin/logs_get.py
+++ main-webserver/app/helpers/blueprint_helpers/admin/logs_get.py
@@ -3,8 +3,10 @@ from app import *
 from app.models.logs import *
 from app.models.public import *
 
+from app.serializers.logs import *
+
 def logsHelper(connection_id, username, bookmarked):
-    session = Session()
+    log_schema = ProtocolLogSchema()
 
     if bookmarked:
         logs = ProtocolLog.query.filter_by(bookmarked=True).all()
@@ -13,9 +15,12 @@ def logsHelper(connection_id, username, bookmarked):
         if logs:
             connection_ids = [log.connection_id for log in logs]
 
+        logs = [log_schema.dump(log) for log in logs]
+
         return {"logs": logs, "connection_ids": connection_ids, "status": SUCCESS}
     if connection_id:
         logs = ProtocolLog.filter_by(connection_id=connection_id).order_by(ProtocolLog.timestamp).all()
+        logs = [log_schema.dump(log) for log in logs]
 
         return {"logs": logs, "status": SUCCESS}
     elif username:
@@ -23,9 +28,11 @@ def logsHelper(connection_id, username, bookmarked):
         user_id = user.user_id
 
         logs = ProtocolLog.query.filer_by(user_id=user_id).order_by(ProtocolLog.timestamp).all()
+        logs = [log_schema.dump(log) for log in logs]
 
         return {"logs": logs, "status": SUCCESS}
     else:
         logs = ProtocolLog.query.order_by(ProtocolLog.timestamp).all()
+        logs = [log_schema.dump(log) for log in logs]
 
         return {"logs": logs, "status": SUCCESS}
