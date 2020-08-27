b10d4c64090f40295cab2f310b82bc064716a0af
Roshan Padaki <roshanpadaki@gmail.com>
2020-08-27 10:41:28 -0400

    cleanup to prevent confusion

--- container-images/run.sh
+++ container-images/run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 runcontainer (){
-    docker run -it -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=$(cd $3;pwd),destination=/protocol -p 5900:5900 -p 32262:32262 -p 32263:32263/udp -p 32273:32273 -p 80:80 -p 2200:22 -p 443:443 -e VNC_SERVER_PASSWORD=password -t $1-systemd-$2
+    docker run -it -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=$(cd $3;pwd),destination=/protocol -p 32262:32262 -p 32263:32263/udp -p 32273:32273 $1-systemd-$2
 }
 
 docker build -f $1/Dockerfile.$2 $1 -t $1-systemd-$2
 container_id=$(runcontainer $1 $2 $3)
-# docker exec -u 0 $container_id /bin/bash -c "source /utils.sh && Enable-FractalFirewallRule"
-# docker exec -u 0 $container_id /bin/bash -c "yes | ufw allow 5900"
-# docker cp ../docker-systemctl-replacement/files/docker/systemctl.py
 
-echo "Running chrome in container: $container_id"
+echo "Running container with IP: $container_id"
 ipaddr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
-echo "Chrome is running at IP address $ipaddr"
+echo "Container is running with IP address $ipaddr"
 docker exec -it $container_id /bin/bash
 docker kill $container_id
 docker rm $container_id
