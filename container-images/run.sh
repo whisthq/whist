6644d49dc5f866fa978cc8a4bcc7f002a817053d
Ubuntu <ubuntu@ip-172-31-26-124.us-east-2.compute.internal>
2020-09-01 17:59:12 +0000

    Drop privilege flag from base docker run command
    This is still far from secure:
    - we expose all sorts of extra devices to the container
    - all seccomp filters are disabled (in order for Chrome to run without
    the --no-sandbox option (https://stackoverflow.com/a/62383642/2378475)
    - something else, probably
    
    However, it's a start! And we're definitely enabling far less privilege
    than before.

--- container-images/run.sh
+++ container-images/run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 runcontainer (){
-    docker run -it -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=$(cd $3;pwd),destination=/protocol -p 32262:32262 -p 32263:32263/udp -p 32273:32273 $1-systemd-$2
+    docker run -it -d \
+	    --device=/dev/vga_arbiter \
+	    --device=/dev/autofs \
+	    --device=/dev/console \
+	    --device=/dev/vhost-net \
+	    --device=/dev/vhost-vsock \
+	    --device=/dev/ptmx \
+	    --device=/dev/tty1 \
+	    --device=/dev/vcs1 \
+	    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+	    --tmpfs /run \
+	    --tmpfs /run/lock \
+	    -t \
+	    --security-opt seccomp=unconfined \
+	    --mount type=bind,source=$(cd $3;pwd),destination=/protocol -p 32262:32262 -p 32263:32263/udp -p 32273:32273 $1-systemd-$2
 }
 
 docker build -f $1/Dockerfile.$2 $1 -t $1-systemd-$2
