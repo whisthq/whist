5e96775b89759656cbe42d82dbf69c569f1933f9
djsavvy <savvy@fractalcomputers.com>
2020-09-02 16:44:32 +0000

    Explicitly enumerate all enabled container capabilities

--- container-images/run.sh
+++ container-images/run.sh
@@ -6,7 +6,22 @@ runcontainer (){
 	    --tmpfs /run \
 	    --tmpfs /run/lock \
 	    --security-opt seccomp=unconfined \
-	    --mount type=bind,source=$(cd $3;pwd),destination=/protocol -p 32262:32262 -p 32263:32263/udp -p 32273:32273 $1-systemd-$2
+            --cap-drop ALL \
+            --cap-add CAP_SETPCAP \
+            --cap-add CAP_MKNOD \
+            --cap-add CAP_AUDIT_WRITE \
+            --cap-add CAP_CHOWN \
+            --cap-add CAP_NET_RAW \
+            --cap-add CAP_DAC_OVERRIDE \
+            --cap-add CAP_FOWNER \
+            --cap-add CAP_FSETID \
+            --cap-add CAP_KILL \
+            --cap-add CAP_SETGID \
+            --cap-add CAP_SETUID \
+            --cap-add CAP_NET_BIND_SERVICE \
+            --cap-add CAP_SYS_CHROOT \
+            --cap-add CAP_SETFCAP \
+            --mount type=bind,source=$(cd $3;pwd),destination=/protocol -p 32262:32262 -p 32263:32263/udp -p 32273:32273 $1-systemd-$2
 }
 
 docker build -f $1/Dockerfile.$2 $1 -t $1-systemd-$2
