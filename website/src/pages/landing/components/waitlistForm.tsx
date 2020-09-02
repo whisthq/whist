bc31c7039fffc3d38ee0b3bf4888aaf9657235bc
mingy98 <ming@fractalcomputers.com>
2020-09-02 15:54:43 -0400

    latest

---
+++ website/src/pages/landing/components/waitlistForm.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+import "static/PageLanding.css";
+
+class WaitlistForm extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default WaitlistForm;
