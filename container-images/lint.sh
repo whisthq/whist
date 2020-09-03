76e85ec8e9a0d20b498267b71da2da0e12710c6c
philippemnoel <philippe_noel@college.harvard.edu>
2020-09-03 11:21:35 -0400

    added precommit support for dockerfiles!

---
+++ container-images/lint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export PATH=$PATH:/usr/local/bin
+
+if ! which hadolint &>/dev/null; then
+    >&2 echo 'hadolint command not found'
+    if [ -z "$(command -v brew)" ]; then
+        exit 1
+    fi
+    brew install hadolint
+fi
+
+hadolint "$@"
