e786302dc02ce9807df3bad52582c19d7875516c
mingy98 <ming@fractalcomputers.com>
2020-09-03 15:58:33 -0400

    landing functional component

--- website/src/pages/landing/landing.tsx
+++ website/src/pages/landing/landing.tsx
@@ -1,37 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import { db } from "utils/firebase";
 
 import "styles/landing.css";
 import WaitlistForm from "pages/landing/components/waitlistForm"
 
-type LandingProps = {};
-type LandingState = { waitlist: Array<any> };
 
-class Landing extends Component<LandingProps, LandingState> {
-  componentDidMount() {
-    const component = this;
-    this.getWaitlist().then(function (waitlist) {
-      component.setState({ waitlist: waitlist })
+function Landing(props: any) {
+  const [state, setState] = useState(() => {
+    return ({
+      "waitlist": []
     })
-  }
+  })
 
-  getWaitlist = async () => {
+  useEffect(() => {
+    getWaitlist().then(function (waitlist) {
+      setState(prevState => { return { ...prevState, "waitlist": waitlist } })
+    })
+  });
+
+  async function getWaitlist() {
     const waitlist = await db.collection("waitlist").get()
     return (waitlist.docs.map(doc => doc.data()))
   }
 
-  render() {
-    return (
-      <div style={{
-        fontWeight: "bold",
-        textAlign: "center",
-        marginTop: 200,
-      }}>
-        <WaitlistForm />
-      </div>
-    );
-  }
+  return (
+    <div style={{
+      fontWeight: "bold",
+      textAlign: "center",
+      marginTop: 200,
+    }}>
+      <WaitlistForm />
+    </div>
+  );
 }
 
 export default Landing;
