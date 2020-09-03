7b0df06c160f08ce0d0c9f823e269deb3833b879
Gavin Rees <gavin@fractalcomputers.com>
2020-09-03 13:52:43 +0000

    First iteration of ECS test fractal image. Packages protocol in image

---
+++ container-images/build-test.sh
@@ -0,0 +1,9 @@
+if [ -d $1/protocol-temp ]
+then
+   rm -rf $1/protocol-temp
+fi
+mkdir $1/protocol-temp
+sudo cp -r $3/server/build64/* $1/protocol-temp
+echo "Copied protocol over..."
+sudo chmod +r $1/protocol-temp/core*
+docker build -f $1/Dockerfile.$2 $1 -t $1-systemd-$2
