cb38581547ceae03db88445b9cf29abc5ef21d07
Roshan Padaki <roshanpadaki@gmail.com>
2020-09-04 12:00:30 -0400

    some typos

--- container-images/build-test.sh
+++ container-images/build-test.sh
@@ -5,5 +5,5 @@ fi
 mkdir $1/protocol-temp
 sudo cp -r $3/server/build64/* $1/protocol-temp
 echo "Copied protocol over..."
-sudo chmod +r $1/protocol-temp/core*
+sudo chmod +r $1/protocol-temp/*
 docker build -f $1/Dockerfile.$2 $1 -t $1-systemd-$2
