3aff5e6634fefe41a5310f8abafcf180c4321e9f
philippemnoel <philippe_noel@college.harvard.edu>
2020-09-09 00:07:29 -0400

    cleaning up a bit

--- container-images/testing-scripts/connection_tester.py
+++ container-images/testing-scripts/connection_tester.py
@@ -10,59 +10,58 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 
 test_port = 32263
 
+
 def server():
-	# conversation_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# conversation_socket.bind(('', conversation_port))
+    # conversation_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # conversation_socket.bind(('', conversation_port))
 
-	test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	test_socket.bind(('', test_port))
-	startTime = time.time()
+    test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    test_socket.bind(("", test_port))
+    startTime = time.time()
 
-	while time.time() - startTime < 30.0:
-		data, address = test_socket.recvfrom(4096)
-		print("Recieved %d bytes from %s" % (len(data), address))
-		if len(data) > 0:
-			timeDelay = current_milli_time() - int.from_bytes(data, byteorder='big')
-			print("Time delay is %d" % timeDelay)
+    while time.time() - startTime < 30.0:
+        data, address = test_socket.recvfrom(4096)
+        print("Recieved %d bytes from %s" % (len(data), address))
+        if len(data) > 0:
+            timeDelay = current_milli_time() - int.from_bytes(data, byteorder="big")
+            print("Time delay is %d" % timeDelay)
 
-	print("Been doing this for 30 seconds, retiring")
-	test_socket.close()
-	return
-
+    print("Been doing this for 30 seconds, retiring")
+    test_socket.close()
+    return
 
 
 def client(dest):
-	test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	startTime = time.time()
-	while time.time() - startTime < 12.0:
-		print("Sending data...")
-		test_socket.sendto(current_milli_time().to_bytes(8, byteorder='big'), (dest, test_port))
-		time.sleep(1)
-		
-
-
+    test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    startTime = time.time()
+    while time.time() - startTime < 12.0:
+        print("Sending data...")
+        test_socket.sendto(
+            current_milli_time().to_bytes(8, byteorder="big"), (dest, test_port)
+        )
+        time.sleep(1)
 
 
 def main():
-	is_server = False
-	destination = ""
-	if (sys.argv[1] in ["client", "server"]):
-		if sys.argv[1] == "server":
-			is_server = True
-	else:
-		print("Invalid first argument " + sys.argv[1])
+    is_server = False
+    destination = ""
+    if sys.argv[1] in ["client", "server"]:
+        if sys.argv[1] == "server":
+            is_server = True
+    else:
+        print("Invalid first argument " + sys.argv[1])
 
-	if not is_server:
-		try: 
-			destination = sys.argv[2]
-		except IndexError:
-			print("Need to specify the IP address to conect to")
+    if not is_server:
+        try:
+            destination = sys.argv[2]
+        except IndexError:
+            print("Need to specify the IP address to conect to")
 
+    if is_server:
+        server()
+    else:
+        client(destination)
 
-	if is_server:
-		server()
-	else:
-		client(destination)
 
 if __name__ == "__main__":
-	main()
+    main()
