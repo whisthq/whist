3e077293a26372a9e09886995bbaf6a774b923d4
mingy98 <ming@fractalcomputers.com>
2020-09-15 22:54:22 -0400

    typo fix

--- main-webserver/app/blueprints/mail/mail_blueprint.py
+++ main-webserver/app/blueprints/mail/mail_blueprint.py
@@ -10,7 +10,6 @@ def mail(action, **kwargs):
     body = kwargs["body"]
     if action == "forgot":
         return forgotPasswordHelper(body["username"])
-f
     elif action == "cancel":
         return cancelHelper(body["username"], body["feedback"])
 
