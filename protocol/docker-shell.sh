85eb09845ec744db5e01cf13ba8cd8ece3457e43
Roshan Padaki <roshanpadaki@gmail.com>
2020-09-16 20:30:12 -0400

    backwards compatile but programmable docker-shell.sh

--- protocol/docker-shell.sh
+++ protocol/docker-shell.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 container_name=protocol-builder-$(date +"%s")
-docker run -it --rm --mount type=bind,source=$(pwd),destination=/workdir --name $container_name \
-            --user $(id -u ${USER}) protocol-builder-ubuntu$1
+docker run -it --rm --mount type=bind,source=$(cd ${2:-.}; pwd),destination=/workdir --name $container_name \
+            --user $(id -u ${USER}) protocol-builder-ubuntu$1 \
+            ${3:-}
