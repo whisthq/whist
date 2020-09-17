a76a3faa308e6ab025d500c8f75b4d930b5237f8
Roshan Padaki <roshanpadaki@gmail.com>
2020-09-16 20:16:10 -0400

    better builder

---
+++ protocol/docker-build-protocol.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+container_name=protocol-builder-$(date +"%s")
+docker run -it --rm --mount type=bind,source=$(pwd),destination=/workdir --name $container_name \
+            --user $(id -u ${USER}) protocol-builder-ubuntu$1 \
+            sh -c ''' \
+            git clean -dfx && \
+            cmake . && \
+            make -j
+            '''
