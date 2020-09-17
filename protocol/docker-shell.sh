8a7a362aa53364ef2fadca3e762073a7e227f73d
Roshan Padaki <roshanpadaki@gmail.com>
2020-09-17 00:36:50 -0400

    fix bash args

--- protocol/docker-shell.sh
+++ protocol/docker-shell.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 container_name=protocol-builder-$(date +"%s")
-docker run -it --rm --mount type=bind,source=$(cd ${2:-.}; pwd),destination=/workdir --name $container_name \
-            --user $(id -u ${USER}) protocol-builder-ubuntu$1 \
-            ${3:-}
+mount_directory=$(cd ${2:-.}; pwd)
+username=$(id -u ${USER})
+command=${3:-'/bin/bash'}
+docker run -it \
+    --rm \
+    --mount type=bind,source=$mount_directory,destination=/workdir \
+    --name $container_name \
+    --user $username \
+    protocol-builder-ubuntu$1 \
+    sh -c "$command"
