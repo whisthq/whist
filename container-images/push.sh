3c9a979fa1c551e99225f9e06e118da730f21581
Ubuntu <ubuntu@ip-172-31-88-255.ec2.internal>
2020-09-21 20:07:39 +0000

    tag latest-stable

--- container-images/push.sh
+++ container-images/push.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 git_hash=$(git rev-parse --short HEAD)
-local_tag=fractal/$1:$git_hash.$2
+local_name=fractal/$1
+local_tag=$git_hash.$2
 region=${3:-us-east-1}
 ecr_uri=$(aws ecr get-authorization-token --region $region --query authorizationData[0].proxyEndpoint --output text | cut -c 9-)
 
 aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $ecr_uri
 
-docker tag $local_tag $ecr_uri/$local_tag
+docker tag $local_name:$local_tag $ecr_uri/$local_name:$local_tag
+docker tag $local_name:$local_tag $ecr_uri/$local_name:latest-stable
 docker push $ecr_uri/fractal/$1
