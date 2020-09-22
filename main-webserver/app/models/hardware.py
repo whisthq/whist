9ae5dc3d689ae835bd677c46354bda38780ac086
Leor <leor.fishman@gmail.com>
2020-09-22 14:49:38 -0400

    avg to max

--- main-webserver/app/models/hardware.py
+++ main-webserver/app/models/hardware.py
@@ -59,8 +59,8 @@ class SortedClusters(db.Model):
     __tablename__ = "cluster_sorted"
     __table_args__ = {"extend_existing": True, "schema": "hardware"}
     cluster = db.Column(db.String(250), primary_key=True, unique=True)
-    avgCPURemainingPerContainer = db.Column(db.Float, nullable=False, default=1024.0)
-    avgMemoryRemainingPerContainer = db.Column(db.Float, nullable=False, default=2000.0)
+    maxCPURemainingPerContainer = db.Column(db.Float, nullable=False, default=1024.0)
+    maxMemoryRemainingPerContainer = db.Column(db.Float, nullable=False, default=2000.0)
     pendingTasksCount = db.Column(db.Integer, nullable=False, default=0)
     runningTasksCount = db.Column(db.Integer, nullable=False, default=0)
     registeredContainerInstancesCount = db.Column(db.Integer, nullable=False, default=0)
