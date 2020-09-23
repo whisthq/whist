8acbea7d419716f7ca73155114773433d4d6dfb8
Philippe Noël <phil@fractalcomputers.com>
2020-09-23 00:28:55 -0400

    libcurl-dev dependency

--- protocol/server/linux-server-setup.sh
+++ protocol/server/linux-server-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install make curl libssl-dev libavcodec-dev libavdevice-dev libx11-dev libxtst-dev libxdamage-dev libasound2-dev xclip x11-xserver-utils -y
+sudo apt install make curl libcurl4-openssl-dev libssl-dev libavcodec-dev libavdevice-dev libx11-dev libxtst-dev libxdamage-dev libasound2-dev xclip x11-xserver-utils -y
 sudo rm /etc/udev/rules.d/90-fractal-input.rules
 sudo ln -s $(pwd)/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
