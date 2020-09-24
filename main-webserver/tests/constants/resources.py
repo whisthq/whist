78c66131594d7f804d35b356a414c5114679784e
Changalang1729 <eric.c1729@gmail.com>
2020-09-23 23:42:47 -0700

    test

--- main-webserver/tests/constants/resources.py
+++ main-webserver/tests/constants/resources.py
@@ -10,5 +10,5 @@ RESOURCE_GROUP = "FractalStaging"
 SERVER_URL = (
     "http://127.0.0.1:7730"
     if not os.getenv("CI") and not os.getenv("CI") == "true"
-    else "https://main-webserver-pr-" + os.getenv("TEST_HEROKU_PR_NUMBER") + ".herokuapp.com"
+    else "https://main-webserver" + os.getenv("TEST_HEROKU_PR_NUMBER") + ".herokuapp.com"
 )
