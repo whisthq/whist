560626dc5ea071b84a818bbbfcbb7dd1ec7c27ed
mingy98 <ming@fractalcomputers.com>
2020-09-28 14:15:32 -0400

    format test

--- main-webserver/app/helpers/utils/general/crypto.py
+++ main-webserver/app/helpers/utils/general/crypto.py
@@ -1,4 +1,5 @@
 from app.imports import *
+from app.helpers.utils.general.logs import *
 
 import hashlib, binascii
 
@@ -15,4 +16,6 @@ def check_value(hashed_value, raw_value):
         "sha256", raw_value.encode("utf-8"), SHA_SECRET_KEY.encode("utf-8"), 100000
     )
     hr = binascii.hexlify(dk).decode("utf-8")
+    fractalLog(function="", label="", logs=str(hashed_value))
+    fractalLog(function="", label="", logs=str(hr))
     return hashed_value == hr
