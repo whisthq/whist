2eea571dc0709eb2140b51645650af0b0e266c28
Savvy Raghuvanshi <savvy@fractalcomputers.com>
2020-09-29 16:41:38 +0000

    Always clean build the protocol, preventing cmake caching-induced bugs

--- container-images/build.sh
+++ container-images/build.sh
@@ -3,12 +3,15 @@
 git_hash=$(git rev-parse --short HEAD)
 
 # build protocol
+# note: we clean build to prevent cmake caching issues, for example when
+# switching container base from Ubuntu 18 to Ubuntu 20 and back
 ( cd base/protocol && ./docker-build-image.sh $2 )
 base/protocol/docker-shell.sh \
     $2 \
     $(pwd) \
     ''' \
     cd base/protocol && \
+    git clean -dfx && \
     cmake . && \
     make -j \
 '''
