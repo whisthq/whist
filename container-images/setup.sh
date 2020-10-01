836abc20f7cd46eab2c1ab2b26b43659193214b1
Savvy Raghuvanshi <savvy@fractalcomputers.com>
2020-10-01 21:03:51 +0000

    Add missing sudo

--- container-images/setup.sh
+++ container-images/setup.sh
@@ -20,7 +20,7 @@ sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update
-sudo apt-get update && apt-get install --no-install-recommends -y \
+sudo apt-get update && sudo apt-get install --no-install-recommends -y \
     nvidia-dkms-450=450.80.02-0ubuntu1 \
     nvidia-driver-450=450.80.02-0ubuntu1 \
     nvidia-settings=450.80.02-0ubuntu1 \
