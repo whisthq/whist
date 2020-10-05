9c7cbeaa65e64667a29895509e524756e8a0c12a
djsavvy <sraghuvanshi@college.harvard.edu>
2020-10-05 11:35:29 -0400

    Hello world using docker/client

--- ecs-host-service/ecs-host-service.go
+++ ecs-host-service/ecs-host-service.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
 
 func main() {
 	fmt.Println("hello world!")
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
 }
--- ecs-host-service/go.mod
+++ ecs-host-service/go.mod
@@ -1,3 +1,13 @@
 module github.com/fractalcomputers/ecs-host-service
 
 go 1.15
+
+require (
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20201002202402-0a1ea396d57c // indirect
+)
