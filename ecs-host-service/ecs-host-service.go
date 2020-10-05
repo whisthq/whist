d4fb253bb2cbe66357b674d90eea47312f276c38
tinalu99 <tinalu@college.harvard.edu>
2020-10-05 15:41:18 -0400

    fix merge conficts

--- ecs-host-service/ecs-host-service.go
+++ ecs-host-service/ecs-host-service.go
@@ -3,25 +3,38 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/events"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
 func main() {
-	fmt.Println("hello world!")
-
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
+	filters := filters.NewArgs()
+	filters.Add("type", events.ContainerEventType)
+	eventOptions := types.EventsOptions{
+		Filters: filters,
 	}
 
-	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-	}
+	events, errs := cli.Events(context.Background(), eventOptions)
+
+	loop:
+		for {
+			select {
+			case err := <-errs:
+				if err != nil && err != io.EOF {
+					panic(err)
+				}
+				break loop
+			case event := <-events:
+				fmt.Printf("%s %s %s\n", event.Type, event.ID, event.Action)
+			}
+		}
 }
--- ecs-host-service/go.mod
+++ ecs-host-service/go.mod
@@ -3,9 +3,10 @@ module github.com/fractalcomputers/ecs-host-service
 go 1.15
 
 require (
+	docker.io/go-docker v1.0.0 // indirect
 	github.com/containerd/containerd v1.4.1 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v17.12.0-ce-rc1.0.20200916142827-bd33bbf0497b+incompatible // indirect
+	github.com/docker/docker v17.12.0-ce-rc1.0.20200916142827-bd33bbf0497b+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
