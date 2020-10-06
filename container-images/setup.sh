7492abf3243894e242c5a2b51e63144d53c12b2f
djsavvy <savvy@tryfractal.com>
2020-10-06 03:48:00 +0000

    Fix cmake installation for ubuntu 20 host

--- container-images/setup.sh
+++ container-images/setup.sh
@@ -69,7 +69,7 @@ echo "================================================"
 echo "Installing cmake..."
 echo "================================================"
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
 sudo apt-get update
 sudo apt-get install -y kitware-archive-keyring
 sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
