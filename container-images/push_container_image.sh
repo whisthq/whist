169ffa811535374b37ede5b9f898789d1b723767
djsavvy <savvy@tryfractal.com>
2020-10-06 06:16:50 +0000

    Refactor push.sh to only use Ubuntu 20, and rename to match other script names

--- container-images/push_container_image.sh
+++ container-images/push_container_image.sh
@@ -2,8 +2,8 @@
 
 git_hash=$(git rev-parse --short HEAD)
 local_name=fractal/$1
-local_tag=$git_hash.$2
-region=${3:-us-east-1}
+local_tag=$git_hash.20
+region=${2:-us-east-1}
 ecr_uri=$(aws ecr get-authorization-token --region $region --query authorizationData[0].proxyEndpoint --output text | cut -c 9-)
 
 aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $ecr_uri
