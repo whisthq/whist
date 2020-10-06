369edca85148793b00c9b00b74773a54869f474c
djsavvy <savvy@tryfractal.com>
2020-10-06 04:21:20 +0000

    Actually create the docker group before adding ourselves to it

--- container-images/setup_ubuntu20_host.sh
+++ container-images/setup_ubuntu20_host.sh
@@ -15,6 +15,8 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
 sudo usermod -aG docker $USER
 
 
