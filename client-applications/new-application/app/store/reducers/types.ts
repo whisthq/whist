a142c42de7b0b8bcc70ae6e5ad51178a710fe41a
mingy98 <ming@fractalcomputers.com>
2020-10-06 12:54:30 -0400

    changed mianstateytpe

--- client-applications/new-application/app/store/reducers/types.ts
+++ client-applications/new-application/app/store/reducers/types.ts
@@ -1,23 +1,6 @@
 import { Dispatch as ReduxDispatch, Store as ReduxStore, Action } from 'redux'
 
-export type mainStateType = {
-    username: string
-    publicIP: string
-    warning: boolean
-    os: string
-    container_id: string
-    cluster: string
-    port32262: string
-    port32263: string
-    port32273: string
-    location: string
-    accessToken: string
-    refreshToken: string
-    accountLocked: boolean
-    promoCode: string
-    statusMessage: string
-    percentLoaded: number
-}
+export type mainStateType = any
 
 export type Dispatch = ReduxDispatch<Action<string>>
 
