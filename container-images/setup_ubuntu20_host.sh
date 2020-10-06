ea0cb83b29d12c9db918329238ac82625aee1edb
djsavvy <savvy@tryfractal.com>
2020-10-06 05:10:42 +0000

    Remove unnecessary cmake installation in Ubuntu 20 hosts

--- container-images/setup_ubuntu20_host.sh
+++ container-images/setup_ubuntu20_host.sh
@@ -78,17 +78,6 @@ sudo systemctl restart docker
 
 
 echo "================================================"
-echo "Installing cmake..."
-echo "================================================"
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
-sudo apt-get update
-sudo apt-get install -y kitware-archive-keyring
-sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
-sudo apt-get install -y cmake
-
-
-echo "================================================"
 echo "Cleaning up the image a bit..."
 echo "================================================"
 sudo apt autoremove
