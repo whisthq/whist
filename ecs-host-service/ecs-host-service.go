6557f029330c490b6cebf073182863fba90d003e
djsavvy <savvy@tryfractal.com>
2020-10-07 03:41:06 +0000

    Implement perm checking and filesystem init/cleanup

--- ecs-host-service/ecs-host-service.go
+++ ecs-host-service/ecs-host-service.go
@@ -4,12 +4,53 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
+
+// The location on disk where we store the container resource allocations
+const resourceMappingDirectory = "/fractal/containerResourceMappings"
+
+// Check that the program has been started with the correct permissions --- for
+// now, we just want to run as root, but this service could be assigned its own
+// user in the future
+func checkRunningPermissions() {
+	fmt.Println("Checking permissions...")
+
+	if os.Geteuid() != 0 {
+		panic("This service needs to run as root!")
+	}
+}
+
+// Create the directory used to store the container resource allocations (e.g.
+// TTYs) on disk
+func initializeFilesystem() {
+	fmt.Println("Initializing filesystem in %s", resourceMappingDirectory)
+
+	// check if resource mapping directory already exists --- if so, panic, since
+	// we don't know why it's there or if it's valid
+	if _, err := os.Lstat(resourceMappingDirectory); !os.IsNotExist(err) {
+		panic(fmt.Sprintf("%s already exists! Bailing out...", resourceMappingDirectory))
+	}
+
+	err := os.MkdirAll(resourceMappingDirectory, 0644|os.ModeSticky)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create directory %s", resourceMappingDirectory)
+		panic(err)
+	}
+}
+
+func uninitializeFilesystem() {
+	err := os.RemoveAll(resourceMappingDirectory)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to delete directory %s", resourceMappingDirectory)
+		panic(err)
+	}
+}
 
 func containerStartHandler(ctx context.Context, cli *client.Client, id string, portMap map[string]map[string]string, ttyState *[256]string) error {
 	// Assign an unused tty
@@ -38,6 +79,13 @@ func containerStopHandler(ctx context.Context, cli *client.Client, id string, po
 }
 
 func main() {
+	checkRunningPermissions()
+
+	// Note that we defer uninitialization so that in case of panic elsewhere, we
+	// still clean up
+	initializeFilesystem()
+	defer uninitializeFilesystem()
+
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
