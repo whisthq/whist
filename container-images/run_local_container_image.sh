dafef0acb6104fe77cc1f7a126c303bb7f2a2e4a
Roshan Padaki <roshan@tryfractal.com>
2020-10-12 15:11:48 -0400

    documentation!

--- container-images/run_local_container_image.sh
+++ container-images/run_local_container_image.sh
@@ -2,6 +2,6 @@
 set -Eeuo pipefail
 
 image=fractal/$1:current-build
-mount=$2
+mount=${2:-}
 
 exec ./run_container_image.sh $image:$tag $mount
--- container-images/run_remote_container_image.sh
+++ container-images/run_remote_container_image.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 repo_name=fractal/$1
 remote_tag=$2
 region=${3:-us-east-1}
-mount=$4
+mount=${4:-}
 ecr_uri=$(aws ecr get-authorization-token --region $region --query authorizationData[0].proxyEndpoint --output text | cut -c 9-)
 image=$ecr_uri/$repo_name:$remote_tag
 
