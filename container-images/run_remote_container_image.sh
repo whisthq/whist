b905a96b88e59b74c6cca4ffbe2f42210f5ad405
Roshan Padaki <roshan@tryfractal.com>
2020-10-12 14:41:33 -0400

    factor scripts for local and remote tests

---
+++ container-images/run_remote_container_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+repo_name=fractal/$1
+remote_tag=$2
+region=${3:-us-east-1}
+mount=$4
+ecr_uri=$(aws ecr get-authorization-token --region $region --query authorizationData[0].proxyEndpoint --output text | cut -c 9-)
+image=$ecr_uri/$repo_name:$remote_tag
+
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $ecr_uri
+
+docker pull $image
+
+exec ./run_container_image.sh $image $mount
