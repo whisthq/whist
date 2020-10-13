ecab1a4174bcab97013d40719e9bb382c3d63b17
18adrianoh <adrianohernandez2000gmail.com>
2020-10-13 15:50:19 -0700

    video embedd added

--- website/src/pages/landing/components/demoVideo.tsx
+++ website/src/pages/landing/components/demoVideo.tsx
@@ -1,19 +1,13 @@
 import React, { useState, useContext } from "react"
-import MainContext from "shared/context/mainContext"
 
 import ReactPlayer from "react-player"
 
+// we will want to change this
+export const DEMO_URL = "https://www.youtube.com/watch?v=PhhC_N6Bm_s"
+
 // must have url= passed in as a url
 function DemoVideo(props: any) {
-    const {} = useContext(MainContext)
-
-    return (
-        <div>
-            <ReactPlayer
-                url={props.url}
-            />
-        </div>
-    )
+    return <ReactPlayer url={props.url ? props.url : DEMO_URL} controls />
 }
 
 export default DemoVideo
