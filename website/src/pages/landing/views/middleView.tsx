7c4be7dbdef091cdbaed694a5f6785b90a5a5f79
mingy98 <ming@tryfractal.com>
2020-10-16 13:43:51 -0400

    ready to merge to staging

--- website/src/pages/landing/views/middleView.tsx
+++ website/src/pages/landing/views/middleView.tsx
@@ -23,11 +23,11 @@ const MiddleView = (props: any) => {
                     <SideBySide case={"Graphics"} reverse width={width} />
                 </div>
             </div>
-            <div style={{ position: "relative", width: "100%" }}>
+            {/* <div style={{ position: "relative", width: "100%" }}>
                 <div style={{ paddingTop: width > 720 ? 80 : 0 }}>
                     <SideBySide case={"DemoVideo"} width={width} />
                 </div>
-            </div>
+            </div> */}
             <Row
                 style={{
                     marginTop: width > 720 ? 120 : 0,
