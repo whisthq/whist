a72001ebb7d241a050a9602e09fc66df6700c591
mingy98 <ming@tryfractal.com>
2020-10-19 08:30:16 -0400

    tinas changes

--- client-applications/desktop/app/shared/constants/config.ts
+++ client-applications/desktop/app/shared/constants/config.ts
@@ -1,16 +1,16 @@
 const staging: any = {
     url: {
-        WEBSERVER_URL: "https://staging-webserver.tryfractal.com",
+        WEBSERVER_URL: "https://dev-webserver.herokuapp.com",
         //WEBSERVER_URL: " http://127.0.0.1:7730",
         FRONTEND_URL: "http://localhost:3000",
         GRAPHQL_HTTP_URL: "https://staging-database.tryfractal.com/v1/graphql",
         GRAPHQL_WS_URL: "wss://staging-database.tryfractal.com/v1/graphql",
+        GOOGLE_REDIRECT_URI: "com.tryfractal.app:/oauth2Callback",
     },
     keys: {
         STRIPE_PUBLIC_KEY: process.env.REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
         HASURA_ACCESS_KEY: process.env.REACT_APP_HASURA_STAGING_ACCESS_KEY,
         GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        GOOGLE_REDIRECT_URI: "com.tryfractal.app:/oauth2Callback",
     },
     sentry_env: "development",
 }
