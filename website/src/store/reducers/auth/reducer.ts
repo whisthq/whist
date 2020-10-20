7221b56a733fd172a5400485249ddeab82ab5869
mingy98 <ming@tryfractal.com>
2020-10-20 16:26:02 -0400

    quickly pushing hot fix to staging

--- website/src/store/reducers/auth/reducer.ts
+++ website/src/store/reducers/auth/reducer.ts
@@ -3,12 +3,15 @@ import { DEFAULT } from "store/reducers/auth/default"
 import * as PureAction from "store/actions/auth/pure"
 import * as SharedAction from "store/actions/shared"
 
+import { deep_copy } from "shared/utils/reducerHelpers"
+
 export default function (state = DEFAULT, action: any) {
+    var stateCopy = deep_copy(state)
     switch (action.type) {
         case PureAction.UPDATE_USER:
             return {
-                ...state,
-                user: Object.assign(state.user, action.body),
+                ...stateCopy,
+                user: Object.assign(stateCopy.user, action.body),
             }
         case SharedAction.RESET_STATE:
             return DEFAULT
