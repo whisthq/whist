0d23e3ba6ad7cf97a17e46c9ed1ad6e0a34bac37
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2020-10-22 17:14:14 -0400

    casting volatile renderer and clang-format

--- protocol/desktop/video.c
+++ protocol/desktop/video.c
@@ -572,11 +572,11 @@ int initMultithreadedVideo(void* opaque) {
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "best");
 
     // Show a black screen initially before anything else
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor((SDL_Renderer*)renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear((SDL_Renderer*)renderer);
+    SDL_RenderPresent((SDL_Renderer*)renderer);
 
-    videoContext.renderer = renderer;
+    videoContext.renderer = (SDL_Renderer*)renderer;
     if (!renderer) {
         LOG_WARNING("SDL: could not create renderer - exiting: %s", SDL_GetError());
         return -1;
@@ -628,8 +628,8 @@ int initMultithreadedVideo(void* opaque) {
     VideoData.renderscreen_semaphore = SDL_CreateSemaphore(0);
     VideoData.run_render_screen_thread = true;
 
-    RenderScreen(renderer);
-    SDL_DestroyRenderer(renderer);
+    RenderScreen((SDL_Renderer*)renderer);
+    SDL_DestroyRenderer((SDL_Renderer*)renderer);
     renderer = NULL;
     return 0;
 }
@@ -637,8 +637,8 @@ int initMultithreadedVideo(void* opaque) {
 
 void initVideo() {
     // renderer must be created in main thread, per SDL guidelines
-    renderer = SDL_CreateRenderer(
-        (SDL_Window*)window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer((SDL_Window*)window, -1,
+                                  SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
     VideoData.render_screen_thread = SDL_CreateThread(initMultithreadedVideo, "VideoThread", NULL);
 }
 
