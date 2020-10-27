d91eb1bffeb296d857a4c1423af80b562dd359c5
mingy98 <ming@fractalcomputers.com>
2020-10-27 10:01:35 -0400

    current

--- website/src/shared/constants/config.ts
+++ website/src/shared/constants/config.ts
@@ -48,4 +48,4 @@ const environment: any = {
 export const config: any =
     process.env.NODE_ENV === "development"
         ? environment.development
-        : environment.production
+        : environment.development
--- website/src/store/actions/auth/sideEffects.ts
+++ website/src/store/actions/auth/sideEffects.ts
@@ -1,5 +1,6 @@
 // ask the server to log me in with a google code
 export const GOOGLE_LOGIN = "GOOGLE_LOGIN"
+export const GOOGLE_SIGNUP = "GOOGLE_SIGNUP"
 
 // ask the server to log me in
 export const EMAIL_LOGIN = "EMAIL_LOGIN"
@@ -12,26 +13,37 @@ export const VALIDATE_RESET_TOKEN = "VALIDATE_RESET_TOKEN"
 export const FORGOT_PASSWORD = "FORGOT_PASSWORD"
 export const RESET_PASSWORD = "RESET_PASSWORD"
 
-export function googleLogin(code: any) {
+export function googleLogin(code: any, rememberMe?: boolean) {
     return {
         type: GOOGLE_LOGIN,
         code,
+        rememberMe
     }
 }
 
-export function emailLogin(email: string, password: string) {
+export function emailLogin(email: string, password: string, rememberMe?: boolean) {
     return {
         type: EMAIL_LOGIN,
         email,
         password,
+        rememberMe
     }
 }
 
-export function emailSignup(email: string, password: string) {
+export function emailSignup(email: string, password: string, rememberMe?: boolean) {
     return {
         type: EMAIL_SIGNUP,
         email,
         password,
+        rememberMe
+    }
+}
+
+export function googleSignup(code: any, rememberMe?: boolean) {
+    return {
+        type: GOOGLE_SIGNUP,
+        code,
+        rememberMe
     }
 }
 
