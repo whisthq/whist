7cff4790503c8231ee31011345b05ae8fc1f0066
18adrianoh <adrianohernandez2000gmail.com>
2020-10-28 20:55:40 -0700

    add careers and support apge

---
+++ website/src/pages/careers/careers.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Header from "shared/components/header"
+
+import "styles/shared.css"
+
+
+const Careers = (props: any) => {
+    return (
+        <div className="fractalContainer">
+            <Header color="black" />
+        </div>
+    )
+}
+
+export default Careers
---
+++ website/src/pages/support/support.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Header from "shared/components/header"
+
+import "styles/shared.css"
+
+const Support = (props: any) => {
+    return (
+        <div className="fractalContainer">
+            <Header color="black" />
+        </div>
+    )
+}
+
+export default Support
