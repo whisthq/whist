00cf4605b65bab7d68b35dcb9d07faa4e02a80b2
djsavvy <savvy@tryfractal.com>
2020-10-30 13:55:47 +0000

    Remove AES key print statement that was there for testing! (oops)

--- container-images/base/start-fractal-server.sh
+++ container-images/base/start-fractal-server.sh
@@ -11,7 +11,7 @@ IDENTIFIER=$(cat $FRACTAL_MAPPINGS_DIR/$CONTAINER_ID/$IDENTIFIER_FILENAME)
 if [ -f "$PRIVATE_KEY_FILENAME" ]
 then
      export FRACTAL_AES_KEY=$(cat $PRIVATE_KEY_FILENAME)
-     /bin/sh -c "echo $FRACTAL_AES_KEY && /usr/share/fractal/FractalServer --identifier=$IDENTIFIER"
+     /usr/share/fractal/FractalServer --identifier=$IDENTIFIER
 else
      /usr/share/fractal/FractalServer --identifier=$IDENTIFIER
 fi
