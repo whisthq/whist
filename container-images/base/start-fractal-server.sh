967b0fd9131f90eb99396910375f901f4b5e59d0
Philippe Noël <phil@tryfractal.com>
2020-10-30 10:40:50 -0400

    explain

--- container-images/base/start-fractal-server.sh
+++ container-images/base/start-fractal-server.sh
@@ -11,6 +11,7 @@ IDENTIFIER=$(cat $FRACTAL_MAPPINGS_DIR/$CONTAINER_ID/$IDENTIFIER_FILENAME)
 if [ -f "$PRIVATE_KEY_FILENAME" ]
 then
      export FRACTAL_AES_KEY=$(cat $PRIVATE_KEY_FILENAME)
+     # use --private-key without argument to make it read the private key from env variable
      /usr/share/fractal/FractalServer --private-key --identifier=$IDENTIFIER
 else
      /usr/share/fractal/FractalServer --identifier=$IDENTIFIER
