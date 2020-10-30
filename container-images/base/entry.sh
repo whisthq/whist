f5c0241d35b4adab2aa2bfe4a7bf2e4db3058a44
djsavvy <savvy@tryfractal.com>
2020-10-30 12:05:20 +0000

    Clean out fractal home directory; move files to /usr/share/fractal instead

--- container-images/base/entry.sh
+++ container-images/base/entry.sh
@@ -6,7 +6,7 @@
 rm /var/run/nologin
 # echo $SSH_PUBLIC_KEY_AWS > ~/.ssh/authorized_keys
 
-ln -sf /home/fractal/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
+ln -sf /usr/share/fractal/fractal-input.rules /etc/udev/rules.d/90-fractal-input.rules
 
 # begin wait loop to get tty number and port map
 CONTAINER_ID=$(basename $(cat /proc/1/cpuset))
