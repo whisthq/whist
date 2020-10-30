c67b0dc08855287383d3b086bc17834da0853329
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2020-10-30 02:00:32 -0400

    only create key file if env var is defined

--- container-images/base/docker-entrypoint.sh
+++ container-images/base/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo $FRACTAL_AES_KEY > /usr/share/fractal/private/aes_key
-unset FRACTAL_AES_KEY
+# if FRACTAL_AES_KEY is set, then create file and TODO: unset
+if [ -n "${FRACTAL_AES_KEY+1}" ]
+then
+    echo $FRACTAL_AES_KEY > /usr/share/fractal/private/aes_key
+fi
 exec /lib/systemd/systemd
