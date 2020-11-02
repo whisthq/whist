a0c6cf0cc8f84951387493d4f786c90691aeea7f
18adrianoh <adrianohernandez2000gmail.com>
2020-11-02 11:19:09 -0800

    wtf

--- website/src/pages/auth/views/verifyView.tsx
+++ website/src/pages/auth/views/verifyView.tsx
@@ -6,54 +6,14 @@ import {
     validateVerificationToken,
     sendVerificationEmail,
 } from "store/actions/auth/sideEffects"
-import { updateAuthFlow, updateUser, resetUser } from "store/actions/auth/pure"
-import DivSpace from "shared/components/divSpace"
+import { updateAuthFlow, resetUser } from "store/actions/auth/pure"
 import history from "shared/utils/history"
 
+import { DivSpace } from "shared/components/divSpace"
+
+import { Title} from "pages/auth/components/authUtils"
+
 import "styles/auth.css"
-
-/**
- * A title component that announces the state of the Verify page when it is not processing. It is used
- * to say (specifically) when verify is not being arrived at via email or when verification failed.
- * @param props.title the text announcing what state we are at.
- * @param props.subtitle a small subtitle than can add some clarification.
- */
-const Title = (props: { title: string; subtitle?: string }) => {
-    const { title, subtitle } = props
-
-    return (
-        <div>
-            <div
-                style={{
-                    width: 400,
-                    margin: "auto",
-                    marginTop: 120,
-                }}
-            >
-                <h2
-                    style={{
-                        color: "#111111",
-                        textAlign: "center",
-                    }}
-                >
-                    {title}
-                </h2>
-            </div>
-            {subtitle ? (
-                <div
-                    style={{
-                        color: "#3930b8", // let's add some variety
-                        textAlign: "center",
-                    }}
-                >
-                    {subtitle}
-                </div>
-            ) : (
-                <div />
-            )}
-        </div>
-    )
-}
 
 const RetryButton = (props: {
     text: string
@@ -120,18 +80,15 @@ const VerifyView = (props: {
         setCanRetry(false)
 
         dispatch(
-            updateUser({
-                emailVerificationToken: undefined,
-            })
-        )
-        dispatch(
             updateAuthFlow({
                 verificationEmailsSent: authFlow.verificationEmailsSent
                     ? authFlow.verificationEmailsSent + 1
                     : 1,
             })
         )
-        dispatch(sendVerificationEmail(user.user_id, user.accessToken))
+        dispatch(
+            sendVerificationEmail(user.user_id, user.emailVerificationToken)
+        )
         setTimeout(() => {
             // first show them that it's been sent
             setSentRetry(true)
@@ -144,9 +101,9 @@ const VerifyView = (props: {
     }
 
     useEffect(() => {
+        console.log("verify use effect")
         if (validUser && validToken && !processing) {
             dispatch(validateVerificationToken(token))
-
             setProcessing(true)
         }
         // want onComponentMount basically (thus [] ~ no deps ~ called only at the very beginning)
@@ -154,10 +111,8 @@ const VerifyView = (props: {
     }, [])
 
     useEffect(() => {
-        if (user.emailVerified) {
-            setProcessing(false)
-        }
-    }, [user.emailVerified, authFlow.verificationAttemptsExecuted])
+        setProcessing(false)
+    }, [user.emailVerified])
 
     if (!validToken) {
         return (
