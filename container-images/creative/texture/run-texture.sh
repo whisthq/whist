cad66c5c483d1e654b5c1c57176106dcb5a009cf
Ubuntu <ubuntu@ip-172-31-88-255.ec2.internal>
2020-11-04 00:16:36 +0000

    fix a directory issue

--- container-images/creative/texture/run-texture.sh
+++ container-images/creative/texture/run-texture.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd "/usr/bin/texturelab/" >/dev/null || exit 1
-yarn install
-yarn electron:serve
+pushd "/opt/texturelab/" >/dev/null || exit 1
+yarn install && yarn electron:serve
 popd >/dev/null || exit 1
+
