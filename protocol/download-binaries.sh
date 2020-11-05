c70b39d0d3de927737f8eb992f051546ad42256c
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2020-11-05 13:58:47 -0500

    remove 32 bit cp

--- protocol/download-binaries.sh
+++ protocol/download-binaries.sh
@@ -3,15 +3,11 @@
 # Get shared libs
 curl -s "https://fractal-dev-secrets.s3.amazonaws.com/shared-libs.tar.gz" | tar xz
 
-mkdir -p desktop/build32/Windows
-mkdir -p server/build32/Windows
 mkdir -p desktop/build64/Windows
 mkdir -p server/build64/Windows
 mkdir -p desktop/build64/Darwin
 
 # in case we want to target Windows
-cp share/32/Windows/* desktop/build32/Windows/
-cp share/32/Windows/* server/build32
 cp share/64/Windows/* desktop/build64/Windows/
 cp share/64/Windows/* server/build64
 
