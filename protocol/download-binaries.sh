197911101907a008c54977e61c3f568defee5ea6
philippemnoel <philippe_noel@college.harvard.edu>
2020-11-05 14:25:49 -0500

    compiles on mac

--- protocol/download-binaries.sh
+++ protocol/download-binaries.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Get shared libs
-curl -s "https://fractal-dev-secrets.s3.amazonaws.com/shared-libs.tar.gz" | tar xz
+echo "Downloading Shared Libs"
+curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/shared-libs.tar.gz" | tar xv
 
 mkdir -p desktop/build64/Windows
 mkdir -p server/build64/Windows
@@ -14,9 +15,4 @@ cp share/64/Windows/* server/build64
 # copy OSX FFmpeg which is in lib/
 cp lib/64/ffmpeg/Darwin/* desktop/build64/Darwin/
 
-# Get loading PNGs
-curl -s "https://fractal-dev-secrets.s3.amazonaws.com/loading.tar.gz" | tar xz
-rm -rf desktop/loading
-mv loading desktop
-
-echo "done"
+echo "Done"
