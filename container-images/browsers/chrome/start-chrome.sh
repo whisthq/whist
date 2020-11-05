845de77f3a321c7c771c06fe023f04dcf94d9b85
Philippe Noël <phil@tryfractal.com>
2020-11-05 08:39:25 -0500

    only ublock

--- container-images/browsers/chrome/start-chrome.sh
+++ container-images/browsers/chrome/start-chrome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec google-chrome --start-maximized --load-extensions=/opt/google/chrome/extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm,/opt/google/chrome/extensions/hdokiejnpimakedhajhdlcegeplioahd
+exec google-chrome --start-maximized --load-extensions=/opt/google/chrome/extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm
