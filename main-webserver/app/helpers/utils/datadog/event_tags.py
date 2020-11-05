17ea3a2ced129dd3e900fc30fc20473bd0bdf20c
18adrianoh <adrianohernandez2000gmail.com>
2020-11-05 14:11:19 -0800

    not tested, prolly doesn't work, code there tho

--- main-webserver/app/helpers/utils/datadog/event_tags.py
+++ main-webserver/app/helpers/utils/datadog/event_tags.py
@@ -3,6 +3,7 @@ on what they correspond to (which makes analysis way easier). Here are some basi
 used by our datadog loggers. Please use these constants to keep our code clean in a growing codebase.
 """
 
+"""Event type tags."""
 CONTAINER_CREATION = "container-creation"
 CONTAINER_DELETION = "container-deletion"
 
@@ -10,13 +11,45 @@ CLUSTER_CREATION = "cluster-creation"
 CLUSTER_DELETION = "cluster-delection"
 
 CONTAINER_LIFECYCLE = "container-lifecycle"
-CLUSTER_LIFECYCLE = "cluster-lifecycle"
 
+_type_tags = {
+    CONTAINER_CREATION,
+    CONTAINER_DELETION,
+    CLUSTER_CREATION,
+    CLUSTER_DELETION,
+    CONTAINER_LIFECYCLE,
+}
+
+"""Status tags"""
 SUCCESS = "success"
 FAILURE = "failure"
+
+_status_tags = {
+    SUCCESS,
+    FAILURE
+}
 
 # these need to be formatted
 # do something like CONTAINER_NAME.format(container_name="my_container")
 # to get "container-name:my_container"
 CONTAINER_NAME = "container-name:{container_name}"
 CLUSTER_NAME = "cluster-name:{cluster_name}"
+
+def validTags(tags):
+    """Check whether the given tags are valid in that they contain the minimum requirements to be
+    logged to datadog. There should not be more than one of these.
+
+    Args:
+        tags (List[str]): some tags that someone wants to put in some datadog event to be logged.
+
+    Returns:
+        boolean: [description]
+    """
+    has_status, has_type = 0, 0
+    for tag in tags:
+        if tag in _status_tags:
+            has_status += 1
+        elif tag in _type_tags:
+            has_type += 1
+
+    return has_status == 1 and has_type == 1
--- main-webserver/app/helpers/utils/datadog/events.py
+++ main-webserver/app/helpers/utils/datadog/events.py
@@ -2,6 +2,7 @@ from app import *
 from app.helpers.utils.general.logs import fractalLog
 
 import logging
+from time import time # TODO WTF
 from flask import current_app
 from datadog import initialize, api
 
@@ -13,13 +14,13 @@ from app.helpers.utils.datadog.event_tags import (
     CONTAINER_NAME as CONTAINER_NAME_F,  # container_name=
     CLUSTER_CREATION,
     CLUSTER_DELETION,
-    CLUSTER_LIFECYCLE,
     CLUSTER_NAME as CLUSTER_NAME_F,  # cluster_name=
     SUCCESS,
     FAILURE,
+    validTags,
 )
 
-
+# TODO (adriano) ideally in the future we'll want to initialize once
 def initDatadog():
     """A simple function that will initialize datadog for our usage in the future.
     This is meant to be called once in a webserver session. We do not want to be re-initializing for
@@ -41,19 +42,29 @@ def initDatadog():
 # a ton of docs on datadog filters/matches and other whatnot); it's also more flexible
 
 
-def datadogEvent(title, text="", tags=None):
+def datadogEvent(title, tags, text="", init=True):
     """Logs a datadog event (not the same as a regular log, basically.. a noteworthy log)
     to our account w/ the api and app keys as well as doing a fractalLog to keep it in
-    our main body of logs.
+    our main body of logs. We require tags to now allow people to make events that are not
+    easily categorizeable. We require 2 tags, one for the event-type and one for the status.
 
     Args:
-        title ([str]): The title of the event giving cursory information.
+        title (str): The title of the event giving cursory information.
         text (str, optional): Body text of the event, giving detailed information. Defaults to "".
-        tags ([List[str]], optional): Tags to tag the event with for filtering. Defaults to None.
+        tags (List[str]): Tags to tag the event with for filtering. Defaults to None.
     """
 
-    # TODO (adriano) ideally in the future we'll want to initialize once
-    initDatadog()
+    if not validTags(tags):
+        fractalLog(
+                function="datadogEvent",
+                label=None,
+                logs=f"Tried to log a datadog event with invalid tags: {str(tags)}",
+                level=logging.ERROR,
+            )
+        raise Exception("Datadog event tags were not valid.")
+
+    if init:
+        initDatadog()
 
     api.Event.create(title=title, text=text, tags=tags)
 
@@ -87,7 +98,7 @@ def datadogEvent_clusterCreate(cluster_name):
 
 
 def datadogEvent_containerDelete(container_name, cluster_name):
-    datadogLog(
+    datadogEvent(
         title="Deleted Container",
         text=f"Container {container_name} in cluster {container_cluster}",
         tags=[
@@ -98,9 +109,11 @@ def datadogEvent_containerDelete(container_name, cluster_name):
         ],
     )
 
+    datadogEvent_containerLifecycle(container_name)
+
 
 def datadogEvent_clusterDelete(cluster_name):
-    datadogLog(
+    datadogEvent(
         title="Deleted Cluster",
         text=f"Cluster {cluster}",
         tags=[CLUSTER_DELETION, SUCCESS, CLUSTER_NAME_F.format(cluster_name=cluster_name)],
@@ -123,6 +136,57 @@ def datadogEvent_containerLifecycle(container_name):
     will fractalLog an error with a corresponding message.
 
     Args:
-        container_name ([str]): The name of the container whose lifecycle we are observing the end for.
+        container_name (str): The name of the container whose lifecycle we are observing the end for.
     """
-    pass
+    dt = 100
+    d, max_d = 100, 100 * 10
+
+    end_time, start_time = time.time(), end_time - dt
+    deletion_date = end_time
+
+    while d < max_d:
+        events = api.Event.query(
+            start=start_time,
+            end=end_time,
+            # sources ?
+            tags=[CONTAINER_NAME_F.format(container_name=container_name)],
+            unaggregated=True
+        )
+
+        try:
+            # https://docs.datadoghq.com/api/v1/events/
+            event = max(events, key=lambda event: event["date_happened"])
+
+            creation_date = event["date_happened"]
+
+            # find everything as a string so we can embedd it in a format string
+            runtime = str(deletion_date - creation_date)
+            creation_date, deletion_date = str(creation_date), str(deletion_date)
+
+            datadogEvent(
+                title="Cluster Lifecycle Ended",
+                text=f"Cluster {cluster} has been deleted, completing its lifecycle. " + 
+                     f"Creation time was {creation_date} and deletion time was {deletion_date}. " + 
+                     f"Change in time was {runtime}.",
+                tags=[
+                    CONTAINER_LIFECYCLE, 
+                    SUCCESS,
+                    CONTAINER_NAME_F.format(container_name=container_name)
+                ],
+            )
+        except:
+            d += dt
+            end_time, start_time = start_time, start_time - dt
+
+
+    fractalLog(
+            function="datadogEvent_containerLifecycle",
+            label=None,
+            logs=f"Failed to find creation event for cluster {cluster_name}",
+            level=logging.ERROR,
+        )
+    # we will not raise an exception since this may be a common occurence
+    # for long-lived containers and/or when we beign to use this functionality
+    # unlike datadogEvent where we absolutely want valid tags and the exception will
+    # never be invoked except in local testing (where we want to except), here this
+    # could be invoked in a real environment and do not want to break the server
