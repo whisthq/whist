0ca3dda4c67d509d735e9fe6a783823d37555db0
philippemnoel <philippe_noel@college.harvard.edu>
2020-11-08 10:56:04 -0500

    policy tested locally, need to test on GHA

--- container-images/apply_ecr_lifecycle_policy.sh
+++ container-images/apply_ecr_lifecycle_policy.sh
@@ -6,4 +6,4 @@ REPO=$1
 LIFECYCLE_POLICY='{"rules":[{"rulePriority":1,"description":"Removes outdated images.","selection":{"tagStatus":"any","countType":"imageCountMoreThan","countNumber":10},"action":{"type": "expire"}}]}'
 
 # push Lifecycle policy
-aws ecr put-lifecycle-policy --region ${AWS_REGION} --repository-name ${REPO} --lifecycle-policy-text ${LIFECYCLE_POLICY} || echo "Failed to put lifecycle policy on ${REPO} repo"
+aws ecr put-lifecycle-policy --region "${AWS_REGION}" --repository-name "fractal/${REPO}" --lifecycle-policy-text "${LIFECYCLE_POLICY}" || echo "Failed to put lifecycle policy on ${REPO} repo"
