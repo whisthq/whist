b9e84dd060e30e9d283eb4dd5e820f789392a3b4
philippemnoel <philippe_noel@college.harvard.edu>
2020-11-08 10:40:35 -0500

    lifecycle policies

---
+++ container-images/apply_ecr_lifecycle_policy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# define vars
+AWS_REGION=$2
+REPO=$1
+LIFECYCLE_POLICY='{"rules":[{"rulePriority":1,"description":"Removes outdated images.","selection":{"tagStatus":"any","countType":"imageCountMoreThan","countNumber":10},"action":{"type": "expire"}}]}'
+
+# push Lifecycle policy
+aws ecr put-lifecycle-policy --region ${AWS_REGION} --repository-name ${REPO} --lifecycle-policy-text ${LIFECYCLE_POLICY} || echo "Failed to put lifecycle policy on ${REPO} repo"
