822d5f65a1bdeee9d6d2c90b0e5e747049b06674
mingy98 <ming@fractalcomputers.com>
2020-11-09 11:26:57 -0500

    fixed when no token

--- main-webserver/app/helpers/blueprint_helpers/admin/hasura_get.py
+++ main-webserver/app/helpers/blueprint_helpers/admin/hasura_get.py
@@ -9,7 +9,7 @@ from app.helpers.utils.general.logs import fractalLog
 
 def authHelper(token):
     if not token:
-        return {"X-Hasura-Role": "anonymous", "X-Hasura-User-Id": None}
+        return {"X-Hasura-Role": "anonymous", "X-Hasura-User-Id": ""}
     else:
         token = token.replace("Bearer ", "")
         current_user = ""
@@ -19,11 +19,11 @@ def authHelper(token):
             if decoded_key:
                 current_user = decoded_key["identity"]
         except Exception as e:
-            return {"X-Hasura-Role": "anonymous", "X-Hasura-User-Id": "anonymous"}
+            return {"X-Hasura-Role": "anonymous", "X-Hasura-User-Id": ""}
 
         user = None if not current_user else User.query.get(current_user)
 
         if user and current_user:
             return {"X-Hasura-Role": "user", "X-Hasura-User-Id": current_user}
 
-        return {"X-Hasura-Role": "anonymous", "X-Hasura-User-Id": "anonymous"}
+        return {"X-Hasura-Role": "anonymous", "X-Hasura-User-Id": ""}
