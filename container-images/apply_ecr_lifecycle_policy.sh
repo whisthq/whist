0fd8fce5d27d390cbd22011b752705c5ef8cbc8a
philippemnoel <philippe_noel@college.harvard.edu>
2020-11-08 22:17:44 -0500

    okay, should work with bash vars in yaml now?

--- container-images/apply_ecr_lifecycle_policy.sh
+++ container-images/apply_ecr_lifecycle_policy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # define vars
-AWS_REGION=$2
 REPO=$1
+AWS_REGION=$2
 LIFECYCLE_POLICY='{"rules":[{"rulePriority":1,"description":"Removes outdated images.","selection":{"tagStatus":"any","countType":"imageCountMoreThan","countNumber":10},"action":{"type": "expire"}}]}'
 
 # push Lifecycle policy
-aws ecr put-lifecycle-policy --region "${AWS_REGION}" --repository-name "fractal/${REPO}" --lifecycle-policy-text "${LIFECYCLE_POLICY}" || echo "Failed to put lifecycle policy on ${REPO} repo"
+output=$(aws ecr put-lifecycle-policy --region ${AWS_REGION} --repository-name fractal/${REPO} --lifecycle-policy-text "${LIFECYCLE_POLICY}")
+if [ -z "$output" ];
+then
+    echo "Successfully put lifecycle policy in fractal/${REPO} ECR repository in AWS region ${AWS_REGION}."
+else
+    echo "Failed to put lifecycle policy on fractal/${REPO} ECR repository in AWS region ${AWS_REGION}."
+fi
