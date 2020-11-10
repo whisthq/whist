31994a97b472afa2ab76cd86fd6b342f0106b825
Roshan Padaki <roshan@tryfractal.com>
2020-11-10 13:31:34 -0500

    nix debug symbols and streamline protocol build

--- container-images/build_protocol.sh
+++ container-images/build_protocol.sh
@@ -3,6 +3,11 @@
 set -Eeuo pipefail
 
 git_hash=$(git rev-parse --short HEAD)
+if [[ ${1:-''} == release ]]; then
+    release_tag='-DCMAKE_BUILD_TYPE=Release'
+else
+    release_tag=''
+fi
 
 # build protocol
 # note: we clean build to prevent cmake caching issues, for example when
@@ -10,10 +15,10 @@ git_hash=$(git rev-parse --short HEAD)
 ( cd base/protocol && ./docker-create-builder.sh )
 base/protocol/docker-run-builder-shell.sh \
     $(pwd) \
-    ''' \
+    " \
     cd base/protocol && \
     git clean -dfx && \
-    cmake . && \
+    cmake FractalServer ${release_tag} && \
     make clang-format && \
     make -j \
-'''
+"
