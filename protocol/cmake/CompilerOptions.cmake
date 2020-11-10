c89b9e3c1e5209c97ecc94545f4110d4526913d1
Nicholas Pipitone <npip99@gmail.com>
2020-11-10 12:19:52 -0500

    Get rid of debug symbols on release, and statically link sentry

--- protocol/cmake/CompilerOptions.cmake
+++ protocol/cmake/CompilerOptions.cmake
@@ -1,12 +1,12 @@
 if (MSVC) # Windows MSVC compiler base flags
     set(CMAKE_C_FLAGS_INIT "-DWIN32 -DWIN32_LEAN_AND_MEAN -DUNICODE")
-    set(CMAKE_C_FLAGS_DEBUG_INIT "/W4 /MP /MTd /O2i" )
-    set(CMAKE_C_FLAGS_RELEASE_INIT "/W4 /MP /MTd /O2i" )
-    set(CMAKE_C_FLAGS_CI_INIT "/W4 /WX /MP /MTd /O2i" )
+    set(CMAKE_C_FLAGS_DEBUG_INIT "/W4 /MP /MTd /Od" )
+    set(CMAKE_C_FLAGS_RELEASE_INIT "/W4 /MP /MT /O2" )
+    set(CMAKE_C_FLAGS_CI_INIT "/W4 /WX /MP /MTd /Od" )
 else() # GCC and Clang base flags
     set(CMAKE_C_FLAGS_INIT "")
     set(CMAKE_C_FLAGS_DEBUG_INIT "-Og -g -Wall -Wextra -Wno-missing-braces -Wno-unused-value -Wno-unused-parameter -O0 -pthread")
-    set(CMAKE_C_FLAGS_RELEASE_INIT "-g -Wall -Wextra -Wno-missing-braces -Wno-unused-value -Wno-unused-parameter -O3 -pthread")
+    set(CMAKE_C_FLAGS_RELEASE_INIT "-Wall -Wextra -Wno-missing-braces -Wno-unused-value -Wno-unused-parameter -O3 -pthread")
     set(CMAKE_C_FLAGS_CI_INIT "-Og -g -Wall -Wextra -Wno-missing-braces -Wno-unused-value -Werror -Wno-unused-parameter -O0 -pthread")
-    set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-rdynamic -Og -g")
+    set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-rdynamic")
 endif()
