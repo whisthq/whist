fbce2e3e928bcce8be5f7c05e6cae3f6ae91ec5c
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2020-11-17 11:56:29 -0500

    clang-tidy run once

--- protocol/run-clang-tidy.sh
+++ protocol/run-clang-tidy.sh
@@ -5,8 +5,8 @@
 # array of all folders to be ignored (some paths are roundabout accessed)
 declare -a excludeFolders=(
     "include"
-    "include/ffmpeg/libavcodec/../libavutil/../../"
-    "include/ffmpeg/libavformat/../libavcodec/../libavutil/../../"
+    "include/ffmpeg/libavcodec/../libavutil/../.."
+    "include/ffmpeg/libavformat/../libavcodec/../libavutil/../.."
     "lib"
     "fractal/video/nvidia-linux"
 )
@@ -14,6 +14,7 @@ declare -a excludeFolders=(
 # array of all folders to be checked and modified
 declare -a includeFolders=(
     "fractal"
+    "desktop"
 )
 
 # iterate through exclusion folders and option to only read the 9999999th line of each of these files
@@ -32,20 +33,37 @@ fileFilterString="${fileFilterString} {\"name\":\"c\"}, {\"name\":\"h\"}]"
 yamlFolder="fixes"
 mkdir $yamlFolder
 
+# echo $fileFilterString
+
+filesToFix="desktop/main.c server/main.c"
 
 for folder in "${includeFolders[@]}"
 do
     for cFilePath in $(find $folder -type f -regex ".*\.\c")
     do
-        # run clang-tidy and output to clang-tidy-fixes.yaml
-        clang-tidy -header-filter=.* "${fileFilterString}" $cFilePath --export-fixes=${yamlFolder}/clang-tidy-fixes.yaml
+        if [[ "$cFilePath" != *"desktop/main.c"* ]]
+        then
+            filesToFix="${filesToFix} ${cFilePath}"
+        fi
+        # # run clang-tidy and output to clang-tidy-fixes.yaml
+        # clang-tidy -header-filter=.* "${fileFilterString}" $cFilePath --export-fixes=${yamlFolder}/clang-tidy-fixes.yaml
 
-        # run clang-tidy noted replacements
-        clang-apply-replacements $yamlFolder
+        # # run clang-tidy noted replacements
+        # clang-apply-replacements $yamlFolder
 
-        sleep 1
+        # sleep 1
     done
 done
+
+# run clang-tidy and output suggested fixes to clang-tidy-fixes.yaml
+clang-tidy -header-filter=.* "${fileFilterString}" --quiet --export-fixes=${yamlFolder}/clang-tidy-fixes.yaml $filesToFix
+
+# old .clang-tidy
+# Checks: '-*,readability-identifier-naming'
+# CheckOptions: [
+#     {key: readability-identifier-naming.FunctionCase, value: lower_case},
+#     {key: readability-identifier-naming.VariableCase, value: lower_case}
+# ]
 
 # cleanup
 rm -rf $yamlFolder
