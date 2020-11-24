2f84479528d2f1d542ec140140e2a20d2a7d5415
Changalang1729 <eric.c1729@gmail.com>
2020-11-24 13:52:37 -0800

    remove all references to git

--- client-applications/desktop/publish.sh
+++ client-applications/desktop/publish.sh
@@ -41,12 +41,7 @@ else
      python3 setVersion.gyp $bucket $version
 
      rm -rf .protocol
-     # git clone --depth 1 https://github.com/fractal/protocol .protocol
-     # cd .protocol
      cd ../../protocol
-     git reset --hard
-     git fetch --depth 25 origin $branch:$branch 
-     git checkout $branch
      cmake . -DCMAKE_BUILD_TYPE=Release
      make FractalClient
      cd ..
