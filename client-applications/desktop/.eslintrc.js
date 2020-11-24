2aeef0c187c3bbbd69c363f288d299724c92e753
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2020-11-24 17:51:31 -0500

    lint client-app app/shared folder

--- client-applications/desktop/.eslintrc.js
+++ client-applications/desktop/.eslintrc.js
@@ -8,14 +8,17 @@ module.exports = {
         // disable the below checks
         "@typescript-eslint/no-explicit-any": 0,
         "global-require": 0,
-        "object-shorthand": 0,
         "no-param-reassign": 0,
+        "object-shorthand": 0,
         "prefer-destructuring": 0,
         "react/destructuring-assignment": 0,
         "react/jsx-indent": 0, // we use prettier's indent rules
         "react/jsx-indent-props": 0, // we use prettier's indent rules
+        "react/jsx-key": 0,
         "vars-on-top": 0,
+
         // demote the below checks to warning instead of error
+        "import/prefer-default-export": 1,
         "no-shadow": 1,
         "react/jsx-props-no-spreading": 1,
         "react/jsx-one-expression-per-line": 1,
