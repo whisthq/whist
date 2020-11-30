4fb9936103b9729a84dfe727a62fafae31e2bae7
mingy98 <ming@fractalcomputers.com>
2020-11-29 22:04:49 -0500

    client app login pages

--- client-applications/desktop/app/pages/login/login.tsx
+++ client-applications/desktop/app/pages/login/login.tsx
@@ -2,38 +2,16 @@ import React, { useState, useEffect } from "react"
 import { connect } from "react-redux"
 import styles from "styles/login.css"
 import Titlebar from "react-electron-titlebar"
-import { parse } from "url"
 
 import BackgroundView from "pages/login/views/backgroundView"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-    faCircleNotch,
-    faUser,
-    faLock,
-} from "@fortawesome/free-solid-svg-icons"
-// import { FaGoogle } from "react-icons/fa"
+import LoginView from "pages/login/views/loginView"
 
-import { updateClient, updateAuth } from "store/actions/pure"
+import { updateClient } from "store/actions/pure"
 import { findDPI } from "pages/login/constants/helpers"
-import { setAWSRegion } from "shared/utils/exec"
-import { openExternal } from "shared/utils/helpers"
-import { config } from "shared/constants/config"
 
 const Login = (props: any) => {
     const { dispatch, os, loginWarning } = props
-    const [loggingIn, setLoggingIn] = useState(false)
     const [version, setVersion] = useState("1.0.0")
-
-    const handleLoginUser = () => {
-        setLoggingIn(true)
-        setAWSRegion().then((region) => {
-            dispatch(updateClient({ region: region }))
-            dispatch(updateAuth({ loginWarning: false, loginMessage: null }))
-            openExternal(
-                config.url.FRONTEND_URL + "/auth/bypass?callback=fractal://"
-            )
-        })
-    }
 
     useEffect(() => {
         const appVersion = require("../../package.json").version
@@ -72,42 +50,7 @@ const Login = (props: any) => {
                 <div style={{ marginTop: loginWarning ? 0 : 50 }}>
                     <div className={styles.loginContainer}>
                         <BackgroundView />
-                        <div className={styles.welcomeBack}>Welcome Back!</div>
-                        <div style={{ marginBottom: 20 }}>
-                            {loggingIn && !loginWarning ? (
-                                <button
-                                    type="button"
-                                    className={styles.processingButton}
-                                    id="login-button"
-                                    style={{
-                                        opacity: 0.6,
-                                        textAlign: "center",
-                                    }}
-                                >
-                                    <FontAwesomeIcon
-                                        icon={faCircleNotch}
-                                        spin
-                                        style={{
-                                            color: "#555555",
-                                            width: 12,
-                                            marginRight: 5,
-                                            position: "relative",
-                                            top: 0.5,
-                                        }}
-                                    />{" "}
-                                    Processing
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={handleLoginUser}
-                                    type="button"
-                                    className={styles.loginButton}
-                                    id="login-button"
-                                >
-                                    LOG IN
-                                </button>
-                            )}
-                        </div>
+                        <LoginView />
                     </div>
                 </div>
             </div>
