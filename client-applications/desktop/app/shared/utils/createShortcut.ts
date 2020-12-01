2287a48dce49b603a6e0f4c6f24b085864dd8626
mingy98 <ming@fractalcomputers.com>
2020-12-01 16:24:09 -0500

    windows desktop shortcut works in packaged

--- client-applications/desktop/app/shared/utils/createShortcut.ts
+++ client-applications/desktop/app/shared/utils/createShortcut.ts
@@ -9,11 +9,20 @@ export default async (app: any) => {
         console.log("linux shortcuts not yet implemented")
     } else if (platform === "win32") {
         console.log("Windows detected")
+        var path =
+            require("electron").remote.app.getAppPath() +
+            "\\node_modules\\@amdglobal\\create-desktop-shortcuts\\src\\windows.vbs"
+        path = path.replace("app.asar", "app.asar.unpacked")
 
-        const createDesktopShortcut = require("create-desktop-shortcuts")
+        console.log(path)
+
+        const createDesktopShortcut = require("@amdglobal/create-desktop-shortcuts")
 
         const shortcutsCreated = createDesktopShortcut({
-            windows: { filePath: "C:\\Windows\\system32\\notepad.exe" },
+            windows: {
+                filePath: "C:\\Windows\\system32\\notepad.exe",
+                vbsPath: process.env.NODE_ENV === "development" ? null : path,
+            },
         })
     } else {
         console.log(`no suitable os found, instead got ${platform}`)
