9d3bfe856eff360d41797550ee31d9acba501ef4
Cidney Weng <cidneyweng@berkeley.edu>
2020-12-01 17:21:03 -0500

    fix stripe race conditions (from adriano) and tests

--- main-webserver/tests/conftest.py
+++ main-webserver/tests/conftest.py
@@ -230,7 +230,7 @@ def user(request):
     """
 
     if "_retrieve_user" not in request.fixturenames:
-        u = User(user_id=f"test-user-{uuid.uuid4()}", password="")
+        u = User(user_id=f"test-user+{uuid.uuid4()}@tryfractal.com", password="")
 
         db.session.add(u)
         db.session.commit()
