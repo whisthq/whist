aee7ee22e9a3300143927a548f269d34e1d9c5d7
mingy98 <ming@fractalcomputers.com>
2020-12-01 15:20:10 -0500

    works in dev

--- client-applications/desktop/app/shared/utils/createShortcut.ts
+++ client-applications/desktop/app/shared/utils/createShortcut.ts
@@ -9,26 +9,12 @@ export default async (app: any) => {
         console.log("linux shortcuts not yet implemented")
     } else if (platform === "win32") {
         console.log("Windows detected")
-        const sm = require("windows-shortcut-maker")
 
-        // Creates an object to store any and all parameters to be passed to the Windows API
-        const options = {
-            filepath: "C:\\Windows\\system32\\notepad.exe",
-        }
+        const createDesktopShortcut = require("create-desktop-shortcuts")
 
-        // It creates a "GIMP" shortcut file in the desktop
-        try {
-            sm.makeSync(options)
-        } catch (error) {
-            console.log(error)
-        }
-
-        // ws.create(`C:\\Users\\ming_\\Desktop\\test.lnk`, {
-        //     target: app_url,
-        //     desc: app.description,
-        //     // icon, iconIndex
-        // })
-        // console.log(`C:\\Users\\ming_\\Desktop\\test.lnk`)
+        const shortcutsCreated = createDesktopShortcut({
+            windows: { filePath: "C:\\Windows\\system32\\notepad.exe" },
+        })
     } else {
         console.log(`no suitable os found, instead got ${platform}`)
     }
