5d7b0bcaf8336155fb00d150a7eeff74c2a66939
Roshan Padaki <roshan@tryfractal.com>
2020-12-01 16:36:12 +0000

    bump fractal to fractal/fractal for monorepo

--- ecs-host-service/fractalwebserver/webserver.go
+++ ecs-host-service/fractalwebserver/webserver.go
@@ -12,7 +12,7 @@ import (
 	// forget to send a message via sentry.  For the same reason, we make sure
 	// not to import the fmt package either, instead separating required
 	// functionality in this imported package as well.
-	logger "github.com/fractal/ecs-host-service/fractallogger"
+	logger "github.com/fractal/fractal/ecs-host-service/fractallogger"
 )
 
 const devHost = "https://dev-webserver.tryfractal.com"
