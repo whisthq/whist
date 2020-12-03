c5bdac7cda153791e5f556c6e033fe6d517c6f5f
mingy98 <ming@fractalcomputers.com>
2020-12-03 12:14:51 -0500

    linting wip

---
+++ client-applications/desktop/app/shared/enums/input.ts
@@ -0,0 +1,8 @@
+export enum FractalKey {
+    ARROW_LEFT = "ArrowLeft",
+    ARROW_RIGHT = "ArrowRight",
+    ARROW_DOWN = "ArrowDown",
+    ARROW_UP = "ArrowUp",
+    ENTER = "Enter",
+    ESCAPE = "Escape",
+}
