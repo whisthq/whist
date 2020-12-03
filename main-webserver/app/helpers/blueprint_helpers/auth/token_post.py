720f7498af3a816dd0cb4d6fa5725294800b04c4
Roshan Padaki <roshan@tryfractal.com>
2020-12-03 22:25:58 +0000

    fix pylint warnings

--- main-webserver/app/helpers/blueprint_helpers/auth/token_post.py
+++ main-webserver/app/helpers/blueprint_helpers/auth/token_post.py
@@ -1,8 +1,5 @@
-from flask import jsonify
-from flask_jwt_extended import decode_token
 from flask_jwt_extended import get_jwt_identity
 
-from app.constants.http_codes import SUCCESS, UNAUTHORIZED
 from app.helpers.blueprint_helpers.auth.account_get import fetch_user_helper
 
 
