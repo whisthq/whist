50d17ef6d3b01a4b8d991988ddead1d868aa482c
18adrianoh <adrianohernandez2000gmail.com>
2020-12-04 11:21:08 -0800

    better docs in test components to help users

--- client-applications/desktop/app/pages/dashboard/constants/adminApp.tsx
+++ client-applications/desktop/app/pages/dashboard/constants/adminApp.tsx
@@ -6,7 +6,7 @@ export const adminApp = {
     category: "Test",
     description: "Test app for Fractal admins",
     long_description:
-        "You can use the admin app to test if you are a Fractal admin (i.e. your email ends with @tryfractal). Go to settings and where the admin settings are set a task ARN, webserver (dev | local | staging | prod | a url), and region (us-east-1 | us-west-1 | ca-central-1). In any field you can enter reset to reset it to null. If you try to launch without setting then it will not work since it will be null (or your previous settings if you change one). Cluster is null | string.",
+        "You can use the admin app to test if you are a Fractal admin. Go to settings and where the admin settings are set a task ARN, webserver, cluster (optional), and an AWS region. In any field you can enter reset to reset it to the defaults (respectively, fractal-browsers-chrome, dev, null, us-east-1).",
     url: "tryfractal.com",
     tos: "https://www.tryfractal.com/termsofservice",
     active: true, // not used yet
