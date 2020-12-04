b6225229665debf66cb0043340f381e7290946da
mingy98 <ming@fractalcomputers.com>
2020-12-04 11:58:19 -0500

    app install working!

--- client-applications/desktop/app/shared/utils/__tests__/helpers.test.ts
+++ client-applications/desktop/app/shared/utils/__tests__/helpers.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-import { searchArrayByKey } from "shared/utils/helpers"
+import { searchArrayByKey, updateArrayByKey } from "shared/utils/helpers"
 import { FractalAppLocalState } from "shared/types/ui"
 
 /* Define test variables here */
@@ -15,8 +15,9 @@ let blender = {
 
 let appArray = [chrome, figma, blender]
 
-// Begin tests
+/* Begin tests */
 describe("helpers.ts", () => {
+    // searchArrayByKey() tests
     test("searchArrayByKey(): If valid object is found", () => {
         const { value, index } = searchArrayByKey(appArray, "app_id", "Blender")
         expect(value).toEqual(blender)
@@ -31,5 +32,32 @@ describe("helpers.ts", () => {
         expect(() => {
             searchArrayByKey(appArray, "nonexistent_key", "Adobe")
         }).toThrow()
+    })
+
+    // updateArrayByKey() tests
+    test("updateArrayByKey(): If valid object is replaced with a valid key/value pair", () => {
+        const { array, index } = updateArrayByKey(
+            appArray,
+            "app_id",
+            "Blender",
+            {
+                localState: FractalAppLocalState.INSTALLED,
+            }
+        )
+        // Check to see that new array is correctly modified
+        expect(index).toEqual(2)
+        expect(array[index].localState).toEqual(FractalAppLocalState.INSTALLED)
+        // Check to see that original array was not modified
+        expect(appArray[index].localState).toEqual(
+            FractalAppLocalState.NOT_INSTALLED
+        )
+    })
+    test("updateArrayByKey(): If an invalid key is searched for", () => {
+        // Check to see that new array is correctly modified
+        expect(() =>
+            updateArrayByKey(appArray, "invalidKey", "Blender", {
+                localState: FractalAppLocalState.INSTALLED,
+            })
+        ).toThrow()
     })
 })
