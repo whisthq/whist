8d73b09ea605a3eb8026ed9d92a6c42faa3e6e23
mingy98 <ming@fractalcomputers.com>
2020-12-05 17:47:41 -0500

    png resizing

--- client-applications/desktop/app/shared/utils/shortcuts.ts
+++ client-applications/desktop/app/shared/utils/shortcuts.ts
@@ -28,8 +28,7 @@ export class SVGConverter {
     _init() {
         this.canvas = document.createElement("canvas")
         this.imgPreview = document.createElement("img")
-        this.imgPreview.style =
-            "position: absolute; top: -9999px; width: 64px; height: 64px;"
+        this.imgPreview.style = "position: absolute; top: -9999px;"
 
         document.body.appendChild(this.imgPreview)
         this.canvasCtx = this.canvas.getContext("2d")
@@ -44,12 +43,22 @@ export class SVGConverter {
         let _this = this
         this.imgPreview.onload = function () {
             const img = new Image()
-            _this.canvas.width = _this.imgPreview.clientWidth
-            _this.canvas.height = _this.imgPreview.clientHeight
+            _this.canvas.width = 64
+            _this.canvas.height = 64
             img.crossOrigin = "anonymous"
             img.src = _this.imgPreview.src
             img.onload = function () {
-                _this.canvasCtx.drawImage(img, 0, 0)
+                _this.canvasCtx.drawImage(
+                    img,
+                    0,
+                    0,
+                    img.width,
+                    img.height,
+                    0,
+                    0,
+                    _this.canvas.width,
+                    _this.canvas.height
+                )
 
                 let imgData = _this.canvas.toDataURL("image/png")
                 callback(imgData)
