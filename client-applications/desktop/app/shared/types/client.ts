dde0a9482622b679b3838e97abd06d246981011b
mingy98 <ming@fractalcomputers.com>
2020-12-06 01:04:41 -0500

    code is pretty!

--- client-applications/desktop/app/shared/types/client.ts
+++ client-applications/desktop/app/shared/types/client.ts
@@ -1,4 +1,7 @@
 const homeDir = require("os").homedir()
+const remote = require("electron").remote
+    ? require("electron").remote.app.getAppPath()
+    : ""
 
 export enum OperatingSystem {
     WINDOWS = "win32",
@@ -13,7 +16,7 @@ export enum FractalAppName {
 export class FractalWindowsDirectory {
     static START_MENU = `${homeDir}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\`
     static DESKTOP = `${homeDir}\\Desktop\\`
-    static ROOT_DIRECTORY = `${require("electron").remote.app.getAppPath()}\\`
+    static ROOT_DIRECTORY = `${remote}\\`
         .replace("\\resources\\app.asar", "")
         .replace("\\app\\", "\\")
 }
