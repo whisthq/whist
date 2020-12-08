e55cd96a0a837aca4fe4e6da32e606a7e075eaa1
djsavvy <sraghuvanshi@college.harvard.edu>
2020-12-08 17:31:47 +0000

    Revert the last three commits: - "In container, read FRACTAL_DPI from the file written by host service" - "Completely relax all file permissions in the host service (for now)" - "Attempt to mount cloud storage dirs to fractal home directory"

--- container-images/base/scripts/docker-entrypoint.sh
+++ container-images/base/scripts/docker-entrypoint.sh
@@ -12,6 +12,12 @@ then
     echo $WEBSERVER_URL > /usr/share/fractal/private/webserver_url
 fi
 
+# if FRACTAL_DPI is set, then create file
+if [ -n "${FRACTAL_DPI+1}" ]
+then
+    echo $FRACTAL_DPI > /usr/share/fractal/private/dpi
+fi
+
 # make sure this environment variable does not leak in any way (probably
 # redundant, but still good practice)
 unset FRACTAL_AES_KEY
