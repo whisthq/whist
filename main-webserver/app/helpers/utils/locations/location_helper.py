04af25861f225734d28185e70569c2e6bece93b0
18adrianoh <adrianohernandez2000gmail.com>
2020-12-09 13:41:07 -0800

    Fixes a couple testing issues: get loc from ip failed for local + only admin could use test endpoints (want devs). Now both are as they should.

--- main-webserver/app/helpers/utils/locations/location_helper.py
+++ main-webserver/app/helpers/utils/locations/location_helper.py
@@ -2,6 +2,10 @@ import requests
 
 
 def get_loc_from_ip(ip):
+    # necessary for testing, the endpoint will return "Not Found" otherwise for longitude etc
+    if ip[0:4] == "172.":
+        return "us-east-1"
+    
     response = requests.get("https://geolocation-db.com/json/{}&position=true".format(ip)).json()
     long = abs(response["longitude"])
     if long > 90:
@@ -13,3 +17,4 @@ def get_loc_from_ip(ip):
 if __name__ == "__main__":
     print(get_loc_from_ip("73.167.118.123"))
     print(get_loc_from_ip("149.142.201.252"))
+    print(get_loc_from_ip("172.18.0.1"))
