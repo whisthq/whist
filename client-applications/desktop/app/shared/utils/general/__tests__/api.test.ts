a4f46df6821e45889922ee82a765911927043f4e
Roshan Padaki <roshan@tryfractal.com>
2020-12-12 15:15:13 -0500

    Add additional jest tests

---
+++ client-applications/desktop/app/shared/utils/general/__tests__/api.test.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+import { FractalAPI } from "shared/types/api"
+import { apiGet, apiPost } from "shared/utils/general/api"
+
+/* Define test variables here */
+const USERNAME = "not-a-valid-username"
+const PASSWORD = "not-a-valid-password"
+const ACCESS_TOKEN = "not-a-valid-access-token"
+
+/* Begin tests */
+describe("api.ts", () => {
+    // searchArrayByKey() tests
+    test("apiGet(): Send a GET request to Fractal webserver", async () => {
+        const { json, success } = await apiGet(
+            FractalAPI.TOKEN.VALIDATE,
+            ACCESS_TOKEN
+        )
+        expect(success).toEqual(false)
+        expect(json).toMatchObject({ msg: "Not enough segments" })
+    })
+    test("apiPost(): Send a POST request to Fractal webserver", async () => {
+        const { json, success } = await apiPost(
+            FractalAPI.ACCOUNT.LOGIN,
+            {
+                username: USERNAME,
+                password: PASSWORD,
+            },
+            ACCESS_TOKEN
+        )
+        expect(success).toEqual(true)
+        expect(json).toBeTruthy()
+    })
+})
