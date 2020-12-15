3c6b5b3cd03d711bb4ef5e4281f3b4102a7411a9
djsavvy <sraghuvanshi@college.harvard.edu>
2020-12-15 18:40:45 +0000

    awesomewm: Try to hint wm to not put windows offscreen

--- container-images/base/config/awesome-rc.lua
+++ container-images/base/config/awesome-rc.lua
@@ -165,7 +165,8 @@ awful.rules.rules = {
                      keys = clientkeys,
                      buttons = clientbuttons,
                      screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
+                     placement = awful.placement.no_offscreen,
+                     attach = true,
      }
     },
 
@@ -218,12 +219,7 @@ client.connect_signal("manage", function (c)
     -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
 
-    if awesome.startup
-      and not c.size_hints.user_position
-      and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c)
-    end
+    awful.placement.no_offscreen(c)
 end)
 
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
