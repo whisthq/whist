7f0f859e9930319c05516ca2991a0583c510180b
djsavvy <sraghuvanshi@college.harvard.edu>
2020-12-15 03:43:45 +0000

    awesomewm: remove default terminal and editor

--- container-images/base/config/awesome-rc.lua
+++ container-images/base/config/awesome-rc.lua
@@ -51,11 +51,6 @@ end
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
--- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
-editor_cmd = terminal .. " -e " .. editor
-
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
