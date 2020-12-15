3250a783b423ec1cc54e779ffc5828cc199da581
djsavvy <sraghuvanshi@college.harvard.edu>
2020-12-15 03:34:37 +0000

    awesomewm: remove ability to resize or drag window with modkey+mouse

--- container-images/base/config/awesome-rc.lua
+++ container-images/base/config/awesome-rc.lua
@@ -261,14 +261,6 @@ clientkeys = gears.table.join(
 clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
-    end),
-    awful.button({ modkey }, 1, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.move(c)
-    end),
-    awful.button({ modkey }, 3, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.resize(c)
     end)
 )
 
