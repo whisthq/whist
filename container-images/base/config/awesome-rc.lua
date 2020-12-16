d9cbe08b66c5728ee9fe718a33ff4cf8ebc873d7
djsavvy <sraghuvanshi@college.harvard.edu>
2020-12-16 17:20:48 +0000

    awesomewm: Remove right-click functionality on taskbar

--- container-images/base/config/awesome-rc.lua
+++ container-images/base/config/awesome-rc.lua
@@ -59,9 +59,6 @@ local tasklist_buttons = gears.table.join(
                                                   {raise = true}
                                               )
                                           end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
                      awful.button({ }, 4, function ()
                                               awful.client.focus.byidx(1)
                                           end),
