c233d774382824b03dcf150c4e654e1269e4ba4f
djsavvy <sraghuvanshi@college.harvard.edu>
2020-12-16 23:45:51 +0000

    awesomewm: Add helper functions

--- container-images/base/config/awesome-rc.lua
+++ container-images/base/config/awesome-rc.lua
@@ -179,6 +179,38 @@ show_master_window = function (c)
                     text = m.name })
 end
 
+notify = function (title, text)
+  naughty.notify( { preset = naughty.config.presets.normal,
+                    title = "" .. title,
+                    text = "" .. text })
+end
+
+function serializeTable(val, name, skipnewlines, depth)
+    skipnewlines = skipnewlines or false
+    depth = depth or 0
+
+    local tmp = string.rep(" ", depth)
+    if name then tmp = tmp .. name .. " = " end
+
+    if type(val) == "table" then
+        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+        for k, v in pairs(val) do
+            tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+        end
+
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif type(val) == "number" then
+        tmp = tmp .. tostring(val)
+    elseif type(val) == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif type(val) == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
+    end
+
+    return tmp
+end
 
 client.connect_signal("manage", function (c)
     -- Set the windows at the slave,
