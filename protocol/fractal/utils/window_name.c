3fdf4e6c36dbe1882ea8fc31c8b7a463738fd756
Anton-Cao <cao.anton@gmail.com>
2020-12-17 01:09:01 +0000

    Prevent reinitialization of window name listener if already initialized

--- protocol/fractal/utils/window_name.c
+++ protocol/fractal/utils/window_name.c
@@ -24,17 +24,19 @@ void get_focused_window_name(char* name) {}
 #include "../core/fractal.h"
 #include "window_name.h"
 
-static SDL_Thread* window_name_listener_thread;
 static SDL_mutex* window_name_mutex;
 static char window_name[WINDOW_NAME_MAXLEN];  // protected by window_name_mutex
+static SDL_Thread* window_name_listener_thread;
 
 int window_name_listener(void* opaque);
 
 void init_window_name_listener() {
-    // TODO(anton) do nothing if already initialized
-    window_name_mutex = SDL_CreateMutex();
-    window_name_listener_thread =
-        SDL_CreateThread(window_name_listener, "window_name_listener", NULL);
+    if (!window_name_listener_thread) {
+        // do nothing if already initialized
+        window_name_mutex = SDL_CreateMutex();
+        window_name_listener_thread =
+            SDL_CreateThread(window_name_listener, "window_name_listener", NULL);
+    }
 }
 
 void get_focused_window_name(char* name_return) {
