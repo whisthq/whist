44d07c702dd0984c833675b487d54989bd3c1fc9
18adrianoh <adrianohernandez2000gmail.com>
2020-12-18 15:38:51 -0800

    lint??

--- main-webserver/app/blueprints/aws/aws_container_blueprint.py
+++ main-webserver/app/blueprints/aws/aws_container_blueprint.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from flask.json import jsonify
 from flask_jwt_extended import jwt_required
-from app.helpers.utils.general.logs import fractal_log
 
 from app import fractal_pre_process
 from app.celery.aws_ecs_creation import (
