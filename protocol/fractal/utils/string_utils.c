b3b751008cffb1f8551d18a8c7e0de7df089c2a0
Anton-Cao <cao.anton@gmail.com>
2020-12-22 18:08:38 -0800

    Create string_utils and implement safe_strncpy

---
+++ protocol/fractal/utils/string_utils.c
@@ -0,0 +1,41 @@
+/**
+ * Copyright Fractal Computers, Inc. 2020
+ * @file string_utils.h
+ * @brief This file contains code for working with strings.
+============================
+Usage
+============================
+
+char buf[10];
+safe_strncpy(buf, some_string, 10);
+*/
+
+#include "string_utils.h"
+
+char* safe_strncpy(char* destination, char* source, size_t num) {
+    /*
+     * Safely copy a string from source to destination.
+     *
+     * Copies at most `num` bytes * after the first null character of `source` are not copied.
+     * If no null character is encountered within the first `num` bytes
+     * of `source`, `destination[num - 1]` will be manually set to zero,
+     * so `destination` is guaranteed to be null terminated, unless
+     * `num` is zero, in which case the `destination` is left unchanged.
+     *
+     * Arguments:
+     *     destination: Address to copy to. Should have at least `num` bytes available.
+     *     source: Address to copy from.
+     *     num: Number of bytes to copy.
+     *
+     * Return:
+     *     `destination` is returned.
+     */
+    if (num > 0) {
+        size_t i;
+        for (i = 0; i < num - 1 && source[i] != '\0'; i++) {
+            destination[i] = source[i];
+        }
+        destination[i] = '\0';
+    }
+    return destination;
+}
