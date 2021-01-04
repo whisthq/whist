700ff2a639b082c86e17402dd55c59d6895e7b06
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-01-04 17:05:14 -0500

    use S3 SDL binaries and includes

--- protocol/download-binaries.sh
+++ protocol/download-binaries.sh
@@ -15,4 +15,16 @@ cp share/64/Windows/* server/build64
 # copy OSX FFmpeg which is in lib/
 cp lib/64/ffmpeg/Darwin/* desktop/build64/Darwin/
 
+# Get SDL2 includes
+curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/fractal-sdl2-headers.tar.gz" | tar -xzv -C include/SDL2
+
+# Pull all SDL2 include files up a level and delete encapsulating folder
+mv include/SDL2/include/* include/SDL2/
+rm -rf include/SDL2/include
+
+# Get SDL2 shared libraries
+curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/fractal-macos-sdl2-static-lib.tar.gz" | tar xzv -C lib/64/SDL2/Darwin
+curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/fractal-linux-sdl2-static-lib.tar.gz" | tar xzv -C lib/64/SDL2/Linux
+curl -s "https://fractal-protocol-shared-libs.s3.amazonaws.com/fractal-windows-sdl2-static-lib.tar.gz" | tar xzv -C lib/64/SDL2/Windows
+
 echo "Done"
