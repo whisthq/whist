b5243643b445b4a3bbb3983a3cd8bf055336f070
Roshan Padaki <roshan@tryfractal.com>
2021-01-05 00:22:00 +0000

    implement ghcr pull and push with AWS authentication for taskdefs and corresponding workflow changes

--- container-images/push_container_image.sh
+++ container-images/push_container_image.sh
@@ -5,7 +5,6 @@ set -Eeuo pipefail
 git_hash=$(git rev-parse HEAD)
 local_name=fractal/$1
 local_tag=current-build
-region=${2:-us-east-1}
 ghcr_uri=ghcr.io
 
 echo $GH_PAT | docker login --username $GH_USERNAME --password-stdin $ghcr_uri
