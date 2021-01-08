9290b8fdad2d3cb673cf090cd1c5878978782a93
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-01-08 18:57:43 -0500

    document desktop/sdl_event_handler.c

--- protocol/desktop/sdl_event_handler.c
+++ protocol/desktop/sdl_event_handler.c
@@ -11,6 +11,12 @@ handleSDLEvent() must be called on any SDL event that occurs. Any action
 trigged an SDL event must be triggered in sdl_event_handler.c
 */
 
+/*
+============================
+Includes
+============================
+*/
+
 #include "sdl_event_handler.h"
 
 #include "../fractal/utils/logging.h"
@@ -41,6 +47,12 @@ extern volatile CodecType output_codec_type;
 
 extern MouseMotionAccumulation mouse_state;
 
+/*
+============================
+Private Functions
+============================
+*/
+
 int handle_window_size_changed(SDL_Event *event);
 int handle_mouse_left_window(SDL_Event *event);
 int handle_key_up_down(SDL_Event *event);
@@ -49,78 +61,23 @@ int handle_mouse_wheel(SDL_Event *event);
 int handle_mouse_button_up_down(SDL_Event *event);
 int handle_multi_gesture(SDL_Event *event);
 
-int try_handle_sdl_event(void) {
-    SDL_Event event;
-    if (SDL_PollEvent(&event)) {
-        if (handle_sdl_event(&event) != 0) {
-            return -1;
-        }
-    }
-    return 0;
-}
-
-int handle_sdl_event(SDL_Event *event) {
-    switch (event->type) {
-        case SDL_WINDOWEVENT:
-            if (event->window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
-                if (handle_window_size_changed(event) != 0) {
-                    return -1;
-                }
-            } else if (event->window.event == SDL_WINDOWEVENT_LEAVE) {
-                if (handle_mouse_left_window(event) != 0) {
-                    return -1;
-                }
-            }
-            break;
-        case SDL_AUDIODEVICEADDED:
-        case SDL_AUDIODEVICEREMOVED:
-            SDL_DetachThread(
-                SDL_CreateThread(multithreaded_reinit_audio, "MultithreadedReinitAudio", NULL));
-            break;
-        case SDL_KEYDOWN:
-        case SDL_KEYUP:
-#ifdef __APPLE__
-            // On Mac, map cmd to ctrl
-            if (event->key.keysym.scancode == FK_LGUI) {
-                event->key.keysym.scancode = (SDL_Scancode)FK_LCTRL;
-                event->key.keysym.sym = SDLK_LCTRL;
-            }
-#endif
-
-            if (handle_key_up_down(event) != 0) {
-                return -1;
-            }
-            break;
-        case SDL_MOUSEMOTION:
-            if (handle_mouse_motion(event) != 0) {
-                return -1;
-            }
-            break;
-        case SDL_MOUSEBUTTONDOWN:
-        case SDL_MOUSEBUTTONUP:
-            if (handle_mouse_button_up_down(event) != 0) {
-                return -1;
-            }
-            break;
-        case SDL_MOUSEWHEEL:
-            if (handle_mouse_wheel(event) != 0) {
-                return -1;
-            }
-            break;
-        case SDL_MULTIGESTURE:
-            if (handle_multi_gesture(event) != 0) {
-                return -1;
-            }
-            break;
-        case SDL_QUIT:
-            LOG_INFO("Forcefully Quitting...");
-            exiting = true;
-            break;
-    }
-    return 0;
-}
+/*
+============================
+Private Function Implementations
+============================
+*/
 
 int handle_window_size_changed(SDL_Event *event) {
+    /*
+        Handle the SDL window size change event
+
+        Arguments:
+            event (SDL_Event*): SDL event for window change
+
+        Return:
+            (int): 0 on success
+    */
+
     // Let video thread know about the resizing to
     // reinitialize display dimensions
     set_video_active_resizing(false);
@@ -148,6 +105,16 @@ int handle_window_size_changed(SDL_Event *event) {
 }
 
 int handle_mouse_left_window(SDL_Event *event) {
+    /*
+        Handle the SDL event for mouse leaving window
+
+        Arguments:
+            event (SDL_Event*): SDL event for mouse leaving window
+
+        Return:
+            (int): 0 on success
+    */
+
     UNUSED(event);
     FractalClientMessage fmsg = {0};
     fmsg.type = MESSAGE_MOUSE_INACTIVE;
@@ -156,6 +123,16 @@ int handle_mouse_left_window(SDL_Event *event) {
 }
 
 int handle_key_up_down(SDL_Event *event) {
+    /*
+        Handle the SDL key press or release event
+
+        Arguments:
+            event (SDL_event*): SDL event for key press/release
+
+        Return:
+            (int): 0 on success
+    */
+
     FractalKeycode keycode =
         (FractalKeycode)SDL_GetScancodeFromName(SDL_GetKeyName(event->key.keysym.sym));
     bool is_pressed = event->key.type == SDL_KEYDOWN;
@@ -215,11 +192,21 @@ int handle_key_up_down(SDL_Event *event) {
     return 0;
 }
 
-// Relative motion is the delta x and delta y from last
-// mouse position Absolute mouse position is where it is
-// on the screen We multiply by scaling factor so that
-// integer division doesn't destroy accuracy
 int handle_mouse_motion(SDL_Event *event) {
+    /*
+        Handle the SDL mouse motion event
+
+        Arguments:
+            event (SDL_Event*): SDL event for mouse motion
+
+        Result:
+            (int): 0 on success, -1 on failure
+    */
+
+    // Relative motion is the delta x and delta y from last
+    // mouse position Absolute mouse position is where it is
+    // on the screen We multiply by scaling factor so that
+    // integer division doesn't destroy accuracy
     bool is_relative = SDL_GetRelativeMouseMode() == SDL_TRUE;
 
     if (is_relative && !mouse_state.is_relative) {
@@ -245,6 +232,16 @@ int handle_mouse_motion(SDL_Event *event) {
 }
 
 int handle_mouse_button_up_down(SDL_Event *event) {
+    /*
+        Handle the SDL mouse button press/release event
+
+        Arguments:
+            event (SDL_Event*): SDL event for mouse button press/release event
+
+        Result:
+            (int): 0 on success
+    */
+
     FractalClientMessage fmsg = {0};
     fmsg.type = MESSAGE_MOUSE_BUTTON;
     // Record if left / right / middle button
@@ -259,6 +256,16 @@ int handle_mouse_button_up_down(SDL_Event *event) {
 }
 
 int handle_mouse_wheel(SDL_Event *event) {
+    /*
+        Handle the SDL mouse wheel event
+
+        Arguments:
+            event (SDL_Event*): SDL event for mouse wheel event
+
+        Result:
+            (int): 0 on success
+    */
+
     FractalClientMessage fmsg = {0};
     fmsg.type = MESSAGE_MOUSE_WHEEL;
     fmsg.mouseWheel.x = event->wheel.x;
@@ -274,5 +281,96 @@ int handle_multi_gesture(SDL_Event *event) {
     fmsg.multigestureData = event->mgesture;
     send_fmsg(&fmsg);
 
+    return 0;
+}
+
+/*
+============================
+Public Function Implementations
+============================
+*/
+
+int try_handle_sdl_event(void) {
+    /*
+        Handle an SDL event if polled
+
+        Return:
+            (int): 0 on success, -1 on failure
+    */
+
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+        if (handle_sdl_event(&event) != 0) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int handle_sdl_event(SDL_Event *event) {
+    /*
+        Handle SDL event based on type
+
+        Return:
+            (int): 0 on success, -1 on failure
+    */
+
+    switch (event->type) {
+        case SDL_WINDOWEVENT:
+            if (event->window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+                if (handle_window_size_changed(event) != 0) {
+                    return -1;
+                }
+            } else if (event->window.event == SDL_WINDOWEVENT_LEAVE) {
+                if (handle_mouse_left_window(event) != 0) {
+                    return -1;
+                }
+            }
+            break;
+        case SDL_AUDIODEVICEADDED:
+        case SDL_AUDIODEVICEREMOVED:
+            SDL_DetachThread(
+                SDL_CreateThread(multithreaded_reinit_audio, "MultithreadedReinitAudio", NULL));
+            break;
+        case SDL_KEYDOWN:
+        case SDL_KEYUP:
+#ifdef __APPLE__
+            // On Mac, map cmd to ctrl
+            if (event->key.keysym.scancode == FK_LGUI) {
+                event->key.keysym.scancode = (SDL_Scancode)FK_LCTRL;
+                event->key.keysym.sym = SDLK_LCTRL;
+            }
+#endif
+
+            if (handle_key_up_down(event) != 0) {
+                return -1;
+            }
+            break;
+        case SDL_MOUSEMOTION:
+            if (handle_mouse_motion(event) != 0) {
+                return -1;
+            }
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+        case SDL_MOUSEBUTTONUP:
+            if (handle_mouse_button_up_down(event) != 0) {
+                return -1;
+            }
+            break;
+        case SDL_MOUSEWHEEL:
+            if (handle_mouse_wheel(event) != 0) {
+                return -1;
+            }
+            break;
+        case SDL_MULTIGESTURE:
+            if (handle_multi_gesture(event) != 0) {
+                return -1;
+            }
+            break;
+        case SDL_QUIT:
+            LOG_INFO("Forcefully Quitting...");
+            exiting = true;
+            break;
+    }
     return 0;
 }
