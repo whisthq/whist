6dc7c21a28c68c484c4012c73df0e1ccbe964543
jmather625 <jatinm2@illinois.edu>
2021-01-08 14:54:50 -0600

    added hella docstrings to endpoint functions

--- main-webserver/celery_worker.py
+++ main-webserver/celery_worker.py
@@ -1,7 +1,8 @@
 from app import celery_instance
-from app.factory import create_app, init_app
+from app.factory import create_app, register_blueprints
 from app.celery_utils import init_celery
 
+
 app = create_app()
-app = init_app(app)
+app = register_blueprints(app)
 init_celery(celery_instance, app)
