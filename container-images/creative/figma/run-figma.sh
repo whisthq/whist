142928ace5fcb1d56adc4b0a6dd17ca6556032f4
Philippe Noel <phil@tryfractal.com>
2021-01-08 15:43:34 -0500

    some docker cleanup

--- container-images/creative/figma/run-figma.sh
+++ container-images/creative/figma/run-figma.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# close all instances of chrome
-# (to simplify login redirection story)
+# close all instances of Google Chrome (to simplify login redirection story)
 if pgrep chrome; then
     pkill chrome
 fi
 
-# open figma
+# open Figma
 pushd "/opt/figma/" >/dev/null || exit 1
 exec ./electron app.asar $@
 popd >/dev/null || exit 1
