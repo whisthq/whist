854379a9088ec95f75d2aed25446a54f927fa7df
Philippe Noel <phil@tryfractal.com>
2021-01-08 15:02:23 -0500

    clarified/documented .github folder

--- .github/workflows/helpers/protocol/install_cuda_windows.ps1
+++ .github/workflows/helpers/protocol/install_cuda_windows.ps1
@@ -2,7 +2,7 @@
 ## Constants
 ## -------------------
 
-# Dictionary of known cuda versions and thier download URLS, which do not follow a consistent pattern :(
+# Dictionary of known CUDA versions and thier download URLS, which do not follow a consistent pattern :(
 $CUDA_KNOWN_URLS = @{
     "8.0.44" = "http://developer.nvidia.com/compute/cuda/8.0/Prod/network_installers/cuda_8.0.44_win10_network-exe";
     "8.0.61" = "http://developer.nvidia.com/compute/cuda/8.0/Prod2/network_installers/cuda_8.0.61_win10_network-exe";
@@ -17,15 +17,13 @@ $CUDA_KNOWN_URLS = @{
     "11.0.167" = "http://developer.download.nvidia.com/compute/cuda/11.0.1/network_installers/cuda_11.0.1_win10_network.exe"
 }
 
-# @todo - change this to be based on _MSC_VER instead, or invert it to be CUDA keyed instead?
 $VISUAL_STUDIO_MIN_CUDA = @{
     "2019" = "10.1";
-    "2017" = "10.0"; # Depends on which version of 2017! 9.0 to 10.0 depending on  version
-    "2015" = "8.0"; # might support older, unsure.
+    "2017" = "10.0"; # Depends on which version of 2017! 9.0 to 10.0 depending on version
+    "2015" = "8.0";
 }
 
 # cuda_runtime.h is in nvcc <= 10.2, but cudart >= 11.0
-# @todo - make this easier to vary per CUDA version.
 $CUDA_PACKAGES_IN = @(
     "nvcc";
     "visual_studio_integration";
@@ -34,18 +32,17 @@ $CUDA_PACKAGES_IN = @(
     "cudart";
 )
 
-
 ## -------------------
 ## Select CUDA version
 ## -------------------
 
-# Get the cuda version from the environment as env:cuda.
+# Get the CUDA version from the environment as env:cuda.
 $CUDA_VERSION_FULL = $env:cuda
 # Make sure CUDA_VERSION_FULL is set and valid, otherwise error.
 
 # Validate CUDA version, extracting components via regex
 $cuda_ver_matched = $CUDA_VERSION_FULL -match "^(?<major>[1-9][0-9]*)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)$"
-if(-not $cuda_ver_matched){
+if (-Not $cuda_ver_matched) {
     Write-Output "Invalid CUDA version specified, <major>.<minor>.<patch> required. '$CUDA_VERSION_FULL'."
     exit 1
 }
@@ -56,11 +53,11 @@ $CUDA_PATCH=$Matches.patch
 ## ---------------------------
 ## Visual studio support check
 ## ---------------------------
-# Exit if visual studio is too new for the cuda version.
+# Exit if visual studio is too new for the CUDA version.
 $VISUAL_STUDIO = $env:visual_studio.trim()
-if ($VISUAL_STUDIO.length -ge 4) {
-$VISUAL_STUDIO_YEAR = $VISUAL_STUDIO.Substring($VISUAL_STUDIO.Length-4)
-    if ($VISUAL_STUDIO_YEAR.length -eq 4 -and $VISUAL_STUDIO_MIN_CUDA.containsKey($VISUAL_STUDIO_YEAR)){
+if ($VISUAL_STUDIO.length -Ge 4) {
+    $VISUAL_STUDIO_YEAR = $VISUAL_STUDIO.Substring($VISUAL_STUDIO.Length-4)
+    if ($VISUAL_STUDIO_YEAR.length -eq 4 -and $VISUAL_STUDIO_MIN_CUDA.containsKey($VISUAL_STUDIO_YEAR)) {
         $MINIMUM_CUDA_VERSION = $VISUAL_STUDIO_MIN_CUDA[$VISUAL_STUDIO_YEAR]
         if ([version]$CUDA_VERSION_FULL -lt [version]$MINIMUM_CUDA_VERSION) {
             Write-Output "Error: Visual Studio $($VISUAL_STUDIO_YEAR) requires CUDA >= $($MINIMUM_CUDA_VERSION)"
@@ -86,30 +83,30 @@ $CUDA_PACKAGES = ""
 
 Foreach ($package in $CUDA_PACKAGES_IN) {
     # Make sure the correct package name is used for nvcc.
-    if($package -eq "nvcc" -and [version]$CUDA_VERSION_FULL -lt [version]"9.1"){
+    if ($package -Eq "nvcc" -And [version]$CUDA_VERSION_FULL -lt [version]"9.1") {
         $package="compiler"
-    } elseif($package -eq "compiler" -and [version]$CUDA_VERSION_FULL -ge [version]"9.1") {
+    } elseif ($package -eq "compiler" -And [version]$CUDA_VERSION_FULL -ge [version]"9.1") {
         $package="nvcc"
     }
     $CUDA_PACKAGES += " $($package)_$($CUDA_MAJOR).$($CUDA_MINOR)"
 
 }
 echo "$($CUDA_PACKAGES)"
+
 ## -----------------
 ## Prepare download
 ## -----------------
 
 # Select the download link if known, otherwise have a guess.
 $CUDA_REPO_PKG_REMOTE=""
-if($CUDA_KNOWN_URLS.containsKey($CUDA_VERSION_FULL)){
+if ($CUDA_KNOWN_URLS.containsKey($CUDA_VERSION_FULL)) {
     $CUDA_REPO_PKG_REMOTE=$CUDA_KNOWN_URLS[$CUDA_VERSION_FULL]
-} else{
-    # Guess what the url is given the most recent pattern (at the time of writing, 10.1)
+} else {
+    # Guess what the URL is given the most recent pattern (at the time of writing, 10.1)
     Write-Output "note: URL for CUDA ${$CUDA_VERSION_FULL} not known, estimating."
     $CUDA_REPO_PKG_REMOTE="http://developer.download.nvidia.com/compute/cuda/$($CUDA_MAJOR).$($CUDA_MINOR)/Prod/network_installers/cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
 }
 $CUDA_REPO_PKG_LOCAL="cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
-
 
 ## ------------
 ## Install CUDA
@@ -118,7 +115,7 @@ $CUDA_REPO_PKG_LOCAL="cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
 # Get CUDA network installer
 Write-Output "Downloading CUDA Network Installer for $($CUDA_VERSION_FULL) from: $($CUDA_REPO_PKG_REMOTE)"
 Invoke-WebRequest $CUDA_REPO_PKG_REMOTE -OutFile $CUDA_REPO_PKG_LOCAL | Out-Null
-if(Test-Path -Path $CUDA_REPO_PKG_LOCAL){
+if (Test-Path -Path $CUDA_REPO_PKG_LOCAL) {
     Write-Output "Downloading Complete"
 } else {
     Write-Output "Error: Failed to download $($CUDA_REPO_PKG_LOCAL) from $($CUDA_REPO_PKG_REMOTE)"
@@ -135,10 +132,11 @@ if (!$?) {
     exit 1
 }
 
-# Store the CUDA_PATH in the environment for the current session, to be forwarded in the action.
+# Store the CUDA_PATH in the environment for the current session, to be forwarded in the GitHub Action.
 $CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$($CUDA_MAJOR).$($CUDA_MINOR)"
 $CUDA_PATH_VX_Y = "CUDA_PATH_V$($CUDA_MAJOR)_$($CUDA_MINOR)"
-# Set environmental variables in this session
+
+# Set environment variables in this session
 $env:CUDA_PATH = "$($CUDA_PATH)"
 $env:CUDA_PATH_VX_Y = "$($CUDA_PATH_VX_Y)"
 Write-Output "CUDA_PATH $($CUDA_PATH)"
@@ -146,4 +144,4 @@ Write-Output "CUDA_PATH_VX_Y $($CUDA_PATH_VX_Y)"
 
 # PATH needs updating elsewhere, anything in here won't persist.
 # Append $CUDA_PATH/bin to path.
-# Set CUDA_PATH as an environmental variable
+# Set CUDA_PATH as an environment variable
