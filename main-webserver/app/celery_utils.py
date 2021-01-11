69a8df2985b678cd7347c6fb6f855f9b6e01179f
jmather625 <jatinm2@illinois.edu>
2021-01-11 13:27:37 -0600

    added guards against redis ssl/tls timeouts and fixed app.json to use a redis tls heroku instance for CI

--- main-webserver/app/celery_utils.py
+++ main-webserver/app/celery_utils.py
@@ -1,5 +1,7 @@
 import ssl
 
+from app.helpers.utils.general.logs import fractal_log
+
 
 def init_celery(celery, app):
     celery.conf.update(
@@ -8,12 +10,6 @@ def init_celery(celery, app):
         result_accept_content=["json"],
         worker_hijack_root_logger=True,
         celery_redis_max_connections=40,
-        broker_use_ssl={
-            "ssl_cert_reqs": ssl.CERT_NONE,
-        },
-        redis_backend_use_ssl={
-            "ssl_cert_reqs": ssl.CERT_NONE,
-        },
     )
     TaskBase = celery.Task  # pylint: disable=invalid-name
 
