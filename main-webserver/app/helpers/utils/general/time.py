37aa5bfacd0b73387e40b1a068dfc012fcb0d8d8
Jatin Mathur <jatinm2@illinois.edu>
2021-01-11 15:03:08 -0600

    Jmather625/main webserver/allow rediss (#404)
    Integrated Redis+TLS into flask app, celery worker, and tests. Current approach is to bypass actual TLS authentication. Switched local development to use madflojo/redis-tls instead of generic redis container. Using a locally generated certificate and key with instructions on how to do it in README. Tested against local Redis+TLS docker image and Heroku's Redis+TLS instance using CI.
    
    Co-authored-by: mingy98 <ming@fractalcomputers.com>

--- main-webserver/app/helpers/utils/general/time.py
+++ main-webserver/app/helpers/utils/general/time.py
@@ -1,4 +1,8 @@
 import datetime
+from functools import wraps
+import errno
+import os
+import signal
 
 from dateutil.relativedelta import relativedelta
 
@@ -37,3 +41,43 @@ def shift_unix_by_week(utc, num_weeks):
 def shift_unix_by_minutes(utc, num_minutes):
     date = unix_to_date(utc)
     return round(date_to_unix(date + relativedelta(minutes=num_minutes)))
+
+
+# from https://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish
+class TimeoutError(Exception):
+    """
+    Generic Timeout class
+    """
+
+
+# from https://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish
+def timeout(seconds, error_message=os.strerror(errno.ETIMEDOUT)):
+    """
+    Decorator to make a timeout for long-running functions. This is necessary
+    because Python's SSL library seems to not handle handshake timeouts (see:
+    https://stackoverflow.com/questions/19938593/web-app-hangs-for-several-hours-in-ssl-py-at-self-sslobj-do-handshake
+    )
+    Unfortunately, this means we need to do the timeout ourselves.
+
+    Usage:
+    @timeout(seconds=NUM_SEC, error_message="Whatever you want")
+    def long_running_func()
+    """
+
+    def timer_decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wrapper
+
+    return timer_decorator
