d6872f43200c898056cdc933628d10e1c0d9daf4
Roshan Padaki <roshan@fractal.co>
2021-01-12 12:57:04 -0500

    push to test-4k repos

--- container-images/push_container_image.sh
+++ container-images/push_container_image.sh
@@ -9,5 +9,5 @@ ghcr_uri=ghcr.io
 
 echo $GH_PAT | docker login --username $GH_USERNAME --password-stdin $ghcr_uri
 
-docker tag $local_name:$local_tag $ghcr_uri/$local_name:$git_hash
-docker push $ghcr_uri/$local_name:$git_hash
+docker tag $local_name:$local_tag $ghcr_uri/$local_name/test-4k:$git_hash
+docker push $ghcr_uri/$local_name/test-4k:$git_hash
