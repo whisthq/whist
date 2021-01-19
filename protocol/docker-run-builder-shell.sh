f8f5330dd7214bf2866e07bc3a48e5ca3bdbad63
djsavvy <sraghuvanshi@college.harvard.edu>
2021-01-19 00:41:40 +0000

    Fix the mounted aws credentials to build the protocol

--- protocol/docker-run-builder-shell.sh
+++ protocol/docker-run-builder-shell.sh
@@ -9,7 +9,7 @@ then
 fi
 docker run $docker_run_flags \
     --rm \
-    -v ~/.aws:/home/fractal/.aws:ro \
+    -v ~/.aws:/home/fractal-builder/.aws:ro \
     --mount type=bind,source=$mount_directory,destination=/workdir \
     --name $container_name \
     --user $username \
