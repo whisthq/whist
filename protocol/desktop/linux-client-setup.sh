cda25862f361519b9afc1cc7fb5b39bc9b164fc5
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-01-19 15:08:40 +0000

    download binaries for server outside of container and don't install aws cli

--- protocol/desktop/linux-client-setup.sh
+++ protocol/desktop/linux-client-setup.sh
@@ -2,9 +2,3 @@
 
 sudo apt-get install libssl-dev libavcodec-dev libavdevice-dev libx11-dev libxtst-dev xclip x11-xserver-utils libasound2-dev libxdamage-dev libcurl4-openssl-dev zlib1g-dev -y
 
-# install aws cli
-sudo apt-get install curl unzip -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-sudo apt-get remove curl unzip -y
