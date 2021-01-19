9f14a617f1e5d24419a8f7a7c51e6559e8b9090e
djsavvy <sraghuvanshi@college.harvard.edu>
2021-01-19 01:21:11 +0000

    Revert "Remove awscli credentials pass-through"
    This reverts commit cc34f2fbd4b2a5177a9730b2ee79bd2d585873b6.

--- protocol/docker-run-builder-shell.sh
+++ protocol/docker-run-builder-shell.sh
@@ -9,6 +9,7 @@ then
 fi
 docker run $docker_run_flags \
     --rm \
+    -v ~/.aws:/home/fractal-builder/.aws:ro \
     --mount type=bind,source=$mount_directory,destination=/workdir \
     --name $container_name \
     --user $username \
