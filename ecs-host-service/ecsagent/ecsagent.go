38980d3abe2b1a63e097d28602c62630f5bddeb6
djsavvy <sraghuvanshi@college.harvard.edu>
2021-01-21 17:48:16 +0000

    Work around a crash in the ecs-agent

--- ecs-host-service/ecsagent/ecsagent.go
+++ ecs-host-service/ecsagent/ecsagent.go
@@ -7,10 +7,18 @@ import (
 
 	ecsapp "github.com/fractal/ecs-agent/agent/app"
 	ecslogger "github.com/fractal/ecs-agent/agent/logger"
+
+	logger "github.com/fractal/fractal/ecs-host-service/fractallogger"
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+
+	// If we don't make this directory, the ecs-agent crashes.
+	err := os.MkdirAll("/var/log/ecs/data/", 0755)
+	if err != nil {
+		logger.Panicf("Could not make directory /var/log/ecs/data: Error: %s", err)
+	}
 }
 
 func ECSAgentMain() {
