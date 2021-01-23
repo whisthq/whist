ad2fa235e7bd6346c887b71d4af8b2b6c6786d5e
Mia <sturzu@college.harvard.edu>
2021-01-23 00:19:38 -0500

    last commit of the night

--- ecs-host-setup/build_query.sh
+++
@@ -1,8 +0,0 @@
-Q1='{"query":"query get_orig_ami {hardware_region_to_ami(where: {region_name: {_eq: \"'
-Q2='\"}}) {ami_id}}","variables":{}}'
-QUERY=${Q1}${1}${Q2}
-SOURCE_AMI=$(curl --location --request POST ${2}\
-                --header 'Content-Type: application/json' \
-                --header "x-hasura-admin-secret: ${3}" \
-                --data-raw "$QUERY")
-echo $SOURCE_AMI
