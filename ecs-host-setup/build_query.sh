f307180f67e723347efb5fc1fa78f1973e86aad4
Mia <sturzu@college.harvard.edu>
2021-01-22 23:12:12 -0500

    pulled in all pieces of query

--- ecs-host-setup/build_query.sh
+++ ecs-host-setup/build_query.sh
@@ -1,4 +1,8 @@
 Q1='{"query":"query get_orig_ami {hardware_region_to_ami(where: {region_name: {_eq: \"'
 Q2='\"}}) {ami_id}}","variables":{}}'
-QUERY="${Q1}${1}${Q2}"
-echo $QUERY
+QUERY=${Q1}${1}${Q2}
+SOURCE_AMI=$(curl --location --request POST ${2}\
+                --header 'Content-Type: application/json' \
+                --header "x-hasura-admin-secret: ${3}" \
+                --data-raw "$QUERY" | grep -oE 'ami-[0-9a-z]+')
+echo $SOURCE_AMI
