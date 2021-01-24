01fd72534cc3cfe221fdf6fcfb409cc0a1022fd0
Philippe Noel <phil@tryfractal.com>
2021-01-23 21:55:24 -0500

    done with ecs-task-definitions

--- ecs-task-definitions/generate-taskdef.sh
+++ ecs-task-definitions/generate-taskdef.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# This script generates an ECS task definition JSON file for a specific application
+# passed in as a parameter to the script (i.e. chrome).
+
+# Warn on misconfigured arguments
+if [ $# -eq 0 ] || [ $# -gt 1 ]; then
+    echo "Invalid number of arguments"
+    echo "This script takes in exactly 1 argument, the name of the application to generate a task definition for"
+    exit 0
+fi
+
 # Currently, all apps have the same task definition parameters,
-# so we only need to update the family tag.
+# so we only need to update the family tag in fractal-taskdef-template.json
 #
-# When there are app-specific parameters, we can update those
-# using an if or switch block, or by storing diffs in json format
+# When/if there are app-specific parameters, we can update those
+# using an if or switch block, or by storing diffs in JSON format.
 
-app=$1 # TODO: warn on misconfigured args
+app=$1
 
-# it's important to use fractal-template here, else we get stdio issues
+# Generate the task definition JSON for the app
+# It's important to use fractal-taskdef-template here, else we get stdio issues
 # rewriting fractal-base.json, from the base image, which we did before
-cat fractal-template.json | jq '.family |= "'$app'"' > $app.json
-echo $app.json # we echo so that the workflow can easily get the app name
+
+cat fractal-taskdef-template.json | jq '.family |= "'$app'"' > $app.json
+
+# Echo the task definition filename so the GitHub Actions CI workflow can get the app name
+echo $app.json
