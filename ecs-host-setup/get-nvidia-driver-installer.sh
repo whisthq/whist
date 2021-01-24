25afb6f9eeb105c48991c48fc7b8831838e09aa5
Philippe Noel <phil@tryfractal.com>
2021-01-24 13:22:44 -0500

    being overly pedantic about extra-documenting the host-service, part 1

--- ecs-host-setup/get-nvidia-driver-installer.sh
+++ ecs-host-setup/get-nvidia-driver-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# This script retrieves the Nvidia GRID drivers from AWS S3 and runs the installer.
+# This script retrieves the Nvidia GRID drivers from AWS S3.
 
 aws s3 cp s3://ec2-linux-nvidia-drivers/grid-11.2/NVIDIA-Linux-x86_64-450.89-grid-aws.run nvidia-driver-installer.run
