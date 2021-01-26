c5f84e077fdb73131eff4384d12b880f1c8ac83c
Philippe Noel <phil@tryfractal.com>
2021-01-25 21:39:52 -0500

    some more dockerfiles, and some bash scripts documented

--- container-images/find_images_in_git_repo.sh
+++ container-images/find_images_in_git_repo.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# get relative subfolder path
+# This script walks the folder tree it is run in, finds all Dockerfile.20 it contains and 
+# formats their path properly for pushing to GitHub Container Registry.
+
+# Retrieve relative subfolder path
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-# Find all the Dockerfiles
+# Find all the Dockerfiles in subfolder
 dockerfile_paths=()
 find . -name "Dockerfile.20" -print0 >tmpfile
 while IFS=  read -r -d $'\0'; do
@@ -13,7 +16,7 @@ while IFS=  read -r -d $'\0'; do
 done <tmpfile
 rm -f tmpfile
 
-# Parse Dockerfiles to get the proper format for GitHub Container Registry
+# Parse Dockerfile paths to get the proper format for GitHub Container Registry
 apps=()
 for path in "${dockerfile_paths[@]}"
 do
@@ -23,5 +26,5 @@ do
     apps+=($path)
 done
 
-# output images in repo
+# Output images to stdout, for our continuous integratin pipeline
 printf '%s ' "${apps[@]}"
