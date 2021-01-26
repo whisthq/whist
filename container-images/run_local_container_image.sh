0be2abe64b0bf6e660f59d238a976866e4310375
Philippe Noel <phil@tryfractal.com>
2021-01-25 21:39:52 -0500

    some more dockerfiles, and some bash scripts documented

--- container-images/run_local_container_image.sh
+++ container-images/run_local_container_image.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# This script runs a container image that exists locally on the machine where the script is run. For 
+# it to work with the Fractal containers, this script needs to be run directly on a Fractal-enabled (see /ecs-host-setup)
+# AWS EC2 instance, via SSH, after the container image was locally built.
+
 set -Eeuo pipefail
 
+# Parameters of the local container image to run
 app_path=${1%/}
 image=fractal/$app_path:current-build
 mount=${2:-}
 
+# Run the container image stored locally
 ./run_container_image.sh $image $mount
--- container-images/run_remote_container_image.sh
+++ container-images/run_remote_container_image.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# This script runs a container image stored in GitHub Container Registry (GHCR). For  it to 
+# work with the Fractal containers, this script needs to be run directly on a Fractal-enabled 
+# (see /ecs-host-setup) AWS EC2 instance, via SSH, without needing to build a container image beforehand.
+
 set -Eeuo pipefail
 
+# Parameters of the GHCR-stored container image to run
 app_path=${1%/}
 repo_name=fractal/$app_path
 remote_tag=$2
@@ -9,8 +14,11 @@ mount=${3:-}
 ghcr_uri=ghcr.io
 image=$ghcr_uri/$repo_name:$remote_tag
 
+# Authenticate to GHCR
 echo $GH_PAT | docker login --username $GH_USERNAME --password-stdin $ghcr_uri
 
+# Download the container image from GHCR
 docker pull $image
 
+# Run the container image retrieved from GHCR
 ./run_container_image.sh $image $mount
