05a1983e3063986a80c598777b0bdd81e121828b
Philippe Noel <phil@tryfractal.com>
2021-01-26 11:08:24 -0500

    some service files, helper scripts and ecs-host-service done

--- container-images/browsers/chrome/install-extensions.sh
+++ container-images/browsers/chrome/install-extensions.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# This script defines a general-purpose function to install any Google Chrome extension
+# by passing in the extension ID and name
+
+# Google Chrome extension installer
 install_chrome_extension () {
+  # Extensions folder and format
   preferences_dir_path="/opt/google/chrome/extensions"
   pref_file_path="$preferences_dir_path/$1.json"
+
+  # Extension external URL
   upd_url="https://clients2.google.com/service/update2/crx"
+
+  # Create extensions folder structure and retrieve extension
   mkdir -p "$preferences_dir_path"
   echo "{" > "$pref_file_path"
-  echo "  \"external_update_url\": \"$upd_url\"" >> "$pref_file_path"
+  echo "\"external_update_url\": \"$upd_url\"" >> "$pref_file_path"
   echo "}" >> "$pref_file_path"
   echo Added \""$pref_file_path"\" ["$2"]
 }
