d51c6575ab569b9ed1e30a64cfd6de759863dc8c
Philippe Noel <phil@tryfractal.com>
2021-01-26 23:33:53 -0500

    testing-scripts readme, start protocol, and nodejs texturelab package typo fix

--- container-images/base/scripts/start-fractal-server.sh
+++ container-images/base/scripts/start-fractal-server.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
+# This script starts the Fractal protocol server, and assumes that it is
+# being run within a Fractal Docker container.
 
-# This script starts the Fractal protocol server executable, on Linux Ubuntu, 
-
-
-
+# Set/Retrieve Container parameters 
 CONTAINER_ID=$(basename $(cat /proc/1/cpuset))
 FRACTAL_MAPPINGS_DIR=/fractal/containerResourceMappings
 IDENTIFIER_FILENAME=hostPort_for_my_32262_tcp
@@ -12,30 +11,33 @@ PRIVATE_KEY_FILENAME=/usr/share/fractal/private/aes_key
 WEBSERVER_URL_FILENAME=/usr/share/fractal/private/webserver_url
 SENTRY_ENV_FILENAME=/usr/share/fractal/private/sentry_env
 
+# Define a string-format identifier for this container
 IDENTIFIER=$(cat $FRACTAL_MAPPINGS_DIR/$CONTAINER_ID/$IDENTIFIER_FILENAME)
 
+# Create list of command-line arguments to pass to the Fractal protocol server
 OPTIONS=""
 
-# send in private key if set
+# Send in AES private key, if set
 if [ -f "$PRIVATE_KEY_FILENAME" ]; then
     export FRACTAL_AES_KEY=$(cat $PRIVATE_KEY_FILENAME)
     OPTIONS="$OPTIONS --private-key=$FRACTAL_AES_KEY"
     # use --private-key without argument to make it read the private key from env variable
 fi
 
-# send in webserver url if set
+# Send in Fractal webserver url, if set
 if [ -f "$WEBSERVER_URL_FILENAME" ]; then
     export WEBSERVER_URL=$(cat $WEBSERVER_URL_FILENAME)
     OPTIONS="$OPTIONS --webserver=$WEBSERVER_URL"
 fi
 
-# send in sentry environment if set
+# Send in Sentry environment, if set
 if [ -f "$SENTRY_ENV_FILENAME" ]; then
     export SENTRY_ENV=$(cat $SENTRY_ENV_FILENAME)
     OPTIONS="$OPTIONS --environment=$SENTRY_ENV"
 fi
 
+# Symlink the Google Drive cloud storage folder to the container home directory
 ln -sf /fractal/cloudStorage/$IDENTIFIER/google_drive /home/fractal/
 
+# Start the Fractal protocol server
 /usr/share/fractal/FractalServer --identifier=$IDENTIFIER $OPTIONS
-
