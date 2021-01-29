3db090b25606c9cedb4dc5f39ba974eefe20d6ce
jmather625 <jatinm2@illinois.edu>
2021-01-29 14:04:02 -0600

    removing old comment

--- main-webserver/tests/conftest.py
+++ main-webserver/tests/conftest.py
@@ -132,7 +132,6 @@ def celery_config():
             "redis_backend_use_ssl": {
                 "ssl_cert_reqs": ssl.CERT_NONE,
             },
-            # "broker_pool_limit": None,
         }
 
     elif redis_url[:5] == "redis":
@@ -140,7 +139,6 @@ def celery_config():
         return {
             "broker_url": redis_url,
             "result_backend": redis_url,
-            # "broker_pool_limit": None,
         }
 
     # unexpected input, fail out
