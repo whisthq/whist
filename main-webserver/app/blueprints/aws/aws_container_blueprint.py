4e965443baeb8f1fe076cf7f0b5cdce2a9b752b1
Owen Niles <owen@fractal.co>
2021-01-29 20:30:28 -0500

    Fixed missing indentation in /aws_container/assign_container.

--- main-webserver/app/blueprints/aws/aws_container_blueprint.py
+++ main-webserver/app/blueprints/aws/aws_container_blueprint.py
@@ -288,7 +288,7 @@ def aws_container_assign(**kwargs):
                 webserver_url=kwargs["webserver_url"],
                 dpi=dpi,
             )
-        response = jsonify({"ID": task.id}), ACCEPTED
+            response = jsonify({"ID": task.id}), ACCEPTED
 
     return response
 
