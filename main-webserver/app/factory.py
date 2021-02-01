784e9cc306534b383c99425bc230698e1dc249bb
FishmanL <leor@fractal.co>
2021-02-01 12:37:59 -0500

    lint

--- main-webserver/app/factory.py
+++ main-webserver/app/factory.py
@@ -106,8 +106,8 @@ def register_blueprints(app):
 
     from .blueprints.oauth import oauth_bp
 
-    bp = build_hirefire_blueprint(os.environ["HIREFIRE_TOKEN"], ["app.factory.WorkerProc"])
-    app.register_blueprint(bp)
+    hirefire_bp = build_hirefire_blueprint(os.environ["HIREFIRE_TOKEN"], ["app.factory.WorkerProc"])
+    app.register_blueprint(hirefire_bp)
 
     app.register_blueprint(account_bp)
     app.register_blueprint(token_bp)
