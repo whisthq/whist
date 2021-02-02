4711cc2994c8a49062494176ba7fc20ffbf7071d
jmather625 <jatinm2@illinois.edu>
2021-02-02 14:09:52 -0600

    local launch works

--- main-webserver/celery_worker.py
+++ main-webserver/celery_worker.py
@@ -1,7 +1,10 @@
-from app import celery_instance
-from app.factory import create_app, register_blueprints
+from app import make_celery
+from app.factory import create_app
 from app.celery_utils import init_celery
 
-
-app = create_app()
+# same as app/waitress.py, but can't be shared as hirefire thinks
+# it is initializing twice and overlapping workers
+celery_instance = make_celery()
+celery_instance.set_default()
+app = create_app(celery=celery_instance)
 init_celery(celery_instance, app)
