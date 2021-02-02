03ac43d1312f079fd1132c26238e1b7a618225a2
jmather625 <jatinm2@illinois.edu>
2021-02-02 14:12:25 -0600

    not using waitress.py

--- main-webserver/app/waitress.py
+++
@@ -1,8 +0,0 @@
-
-from app import make_celery, create_app
-
-celery_instance = make_celery()
-
-celery_instance.set_default()
-
-app = create_app(celery=celery_instance)
--- main-webserver/celery_worker.py
+++ main-webserver/celery_worker.py
@@ -1,10 +1,4 @@
-from app import make_celery
-from app.factory import create_app
+from app import celery_instance, app
 from app.celery_utils import init_celery
 
-# same as app/waitress.py, but can't be shared as hirefire thinks
-# it is initializing twice and overlapping workers
-celery_instance = make_celery()
-celery_instance.set_default()
-app = create_app(celery=celery_instance)
 init_celery(celery_instance, app)
