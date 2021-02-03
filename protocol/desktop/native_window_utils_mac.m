1988f7b2bc1fae05b28a576e66bf9a1d3a0574f7
Roshan Padaki <roshan@fractal.co>
2021-02-03 14:45:32 -0500

    formatting

--- protocol/desktop/native_window_utils_mac.m
+++ protocol/desktop/native_window_utils_mac.m
@@ -41,31 +41,30 @@ int set_native_window_color(SDL_Window *window, RGBColor color) {
         Returns:
             (int): 0 on success, -1 on failure.
     */
-    
+
     SDL_SysWMinfo sys_info = {0};
     SDL_GetWindowWMInfo(window, &sys_info);
     NSWindow *native_window = sys_info.info.cocoa.window;
 
     if (color_requires_dark_text(color)) {
         // dark font color for titlebar
-        [native_window setAppearance: [NSAppearance appearanceNamed:NSAppearanceNameVibrantLight]];
+        [native_window setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantLight]];
     } else {
         // light font color for titlebar
-        [native_window setAppearance: [NSAppearance appearanceNamed:NSAppearanceNameVibrantDark]];
+        [native_window setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantDark]];
     }
 
     // RGBA; in this case, alpha is just 1.0.
-    const CGFloat components[4] = {
-        (float) color.red / 255., 
-        (float) color.green / 255., 
-        (float) color.blue / 255.,
-        1.0
-    };
+    const CGFloat components[4] = {(float)color.red / 255., (float)color.green / 255.,
+                                   (float)color.blue / 255., 1.0};
 
     // We technically only need to set this the first time
-    [native_window setTitlebarAppearsTransparent: true];
+    [native_window setTitlebarAppearsTransparent:true];
 
     // Use color space for current monitor for perfect color matching
-    [native_window setBackgroundColor: [NSColor colorWithColorSpace: [[native_window screen] colorSpace]  components: &components[0] count: 4]];
+    [native_window
+        setBackgroundColor:[NSColor colorWithColorSpace:[[native_window screen] colorSpace]
+                                             components:&components[0]
+                                                  count:4]];
     return 0;
 }
