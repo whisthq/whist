be3ea71ce71f7db1c9e5dd9e2f2b23f0a9ac3d68
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-02-03 17:41:39 -0500

    change taskdef name based on deploy environment

--- ecs-task-definitions/generate-taskdef.sh
+++ ecs-task-definitions/generate-taskdef.sh
@@ -2,6 +2,8 @@
 
 # This script generates an ECS task definition JSON file for a specific application
 # passed in as a parameter to the script (i.e. chrome).
+# Arguments: $1 - app name (e.g. fractal-browsers-chrome) $2 - deploy environment (prod, staging, or dev)
+#   $3 - Sentry environment (prod, staging, or none)
 
 # Warn on misconfigured arguments
 if [ $# -eq 0 ] || [ $# -gt 1 ]; then
@@ -11,20 +13,23 @@ if [ $# -eq 0 ] || [ $# -gt 1 ]; then
 fi
 
 # Currently, all apps have the same task definition parameters,
-# so we only need to update the family and Sentry environment tags (prod, staging, or dev)
+# so we only need to update the family and Sentry environment tags (prod, staging, or empty string)
 # in fractal-taskdef-template.json
 #
 # When/if there are app-specific parameters, we can update those
 # using an if or switch block, or by storing diffs in JSON format.
 
 app=$1
-sentry_env=$2
+deploy_env=$2
+sentry_env=$3
+
+json_filepath=$deploy_env-$1.json # e.g. prod-fractal-browsers-chrome.json
 
 # Generate the task definition JSON for the app
 # It's important to use fractal-taskdef-template here, else we get stdio issues
 # rewriting fractal-base.json, from the base image, which we did before
 
-cat fractal-taskdef-template.json | jq '.family |= "'$app'"' | jq '.environment[2] |= "'$sentry_env'"' > $app.json
+cat fractal-taskdef-template.json | jq '.family |= "'$app'"' | jq '.environment[2] |= "'$sentry_env'"' > $json_filepath
 
 # Echo the task definition filename so the GitHub Actions CI workflow can get the app name
-echo $app.json
+echo $json_filepath
