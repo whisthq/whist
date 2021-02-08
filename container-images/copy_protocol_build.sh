e46b39f9b8b8771488a37b4dbf8a0a87c986d85a
djsavvy <sraghuvanshi@college.harvard.edu>
2021-02-08 23:01:20 +0000

    Move copy_protocol_build.sh into container-images root and make it callable from anywhere

--- container-images/copy_protocol_build.sh
+++ container-images/copy_protocol_build.sh
@@ -2,8 +2,12 @@
 
 set -Eeuo pipefail
 
+# Retrieve source directory of this script
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
 # Create temporary directory for build dependencies
-mkdir -p "base/build-temp"
 mkdir -p "base/build-temp/protocol"
 
 # Copy protocol build into build-temp
