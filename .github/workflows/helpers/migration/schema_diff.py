c925bae35f4e892c4ccb8b74170517f08b1c48bf
Neil Hansen <neil.hansen.31@gmail.com>
2021-02-09 01:47:58 -0500

    no ignoring

--- .github/workflows/helpers/migration/schema_diff.py
+++ .github/workflows/helpers/migration/schema_diff.py
@@ -16,8 +16,8 @@ diff = completed.stdout.decode("utf-8")
 
 # filter out ignored schema patters
 # includes version differences and random uid gens
-for ln in ignore_filter(diff.splitlines()):
-    print(ln)
-
-# for ln in diff.splitlines():
+# for ln in ignore_filter(diff.splitlines()):
 #     print(ln)
+
+for ln in diff.splitlines():
+    print(ln)
