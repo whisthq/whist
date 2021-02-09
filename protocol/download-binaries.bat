124703fda774ed80394f5f119cbdbae3b7427590
djsavvy <sraghuvanshi@college.harvard.edu>
2021-02-09 00:04:21 +0000

    Make all aws s3 cp commands only show errors to avoid lots of output

--- protocol/download-binaries.bat
+++ protocol/download-binaries.bat
@@ -12,7 +12,7 @@ REM ###############################
 REM # Download Protocol Shared Libs
 REM ###############################
 
-aws s3 cp s3://fractal-protocol-shared-libs/shared-libs.tar.gz - | tar xzf -
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/shared-libs.tar.gz - | tar xzf -
 
 REM Copy files to the right location
 xcopy /e /Y /c share\64\Windows\* desktop\build64\Windows\
@@ -24,7 +24,7 @@ REM # Download SDL2 headers
 REM ###############################
 
 mkdir include\SDL2
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-sdl2-headers.tar.gz - | tar xzf - -C include\SDL2
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-sdl2-headers.tar.gz - | tar xzf - -C include\SDL2
 
 REM Pull all SDL2 include files up a level and delete encapsulating folder
 move include\SDL2\include\* include\SDL2
@@ -37,15 +37,15 @@ REM ###############################
 REM macOS
 mkdir lib\64\SDL2
 mkdir lib\64\SDL2\Darwin
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-macos-sdl2-static-lib.tar.gz - | tar xzf - -C lib\64\SDL2\Darwin
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-macos-sdl2-static-lib.tar.gz - | tar xzf - -C lib\64\SDL2\Darwin
 
 REM Linux Ubuntu
 mkdir lib\64\SDL2\Linux
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-linux-sdl2-static-lib.tar.gz - | tar xzf - -C lib\64\SDL2\Linux
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-linux-sdl2-static-lib.tar.gz - | tar xzf - -C lib\64\SDL2\Linux
 
 REM Windows
 mkdir lib\64\SDL2\Windows
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-windows-sdl2-static-lib.tar.gz - | tar xzf - -C lib\64\SDL2\Windows
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-windows-sdl2-static-lib.tar.gz - | tar xzf - -C lib\64\SDL2\Windows
 
 REM ###############################
 REM # Download OpenSSL libraries
@@ -54,7 +54,7 @@ REM ###############################
 REM Emscripten
 REM mkdir lib\64\openssl
 REM mkdir lib\64\openssl\Emscripten
-REM aws s3 cp s3://fractal-protocol-shared-libs/fractal-emscripten-libcrypto.tar.gz - | tar xzf - -C lib\64\openssl\Emscripten
+REM aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-emscripten-libcrypto.tar.gz - | tar xzf - -C lib\64\openssl\Emscripten
 
 REM ###############################
 ECHO "Download Completed"
--- protocol/download-binaries.sh
+++ protocol/download-binaries.sh
@@ -15,7 +15,7 @@ mkdir -p desktop/build64/Darwin
 # Download Protocol Shared Libs
 ###############################
 
-aws s3 cp s3://fractal-protocol-shared-libs/shared-libs.tar.gz - | tar xz
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/shared-libs.tar.gz - | tar xz
 
 # Copy Windows files
 cp share/64/Windows/* desktop/build64/Windows/
@@ -29,7 +29,7 @@ cp lib/64/ffmpeg/Darwin/* desktop/build64/Darwin/
 ###############################
 
 mkdir -p include/SDL2
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-sdl2-headers.tar.gz - | tar -xz -C include/SDL2
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-sdl2-headers.tar.gz - | tar -xz -C include/SDL2
 
 # Pull all SDL2 include files up a level and delete encapsulating folder
 mv include/SDL2/include/* include/SDL2/
@@ -42,15 +42,15 @@ rm -rf include/SDL2/include
 # macOS
 mkdir -p lib/64/SDL2
 mkdir -p lib/64/SDL2/Darwin
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-macos-sdl2-static-lib.tar.gz - | tar xz -C lib/64/SDL2/Darwin
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-macos-sdl2-static-lib.tar.gz - | tar xz -C lib/64/SDL2/Darwin
 
 # Linux Ubuntu
 mkdir -p lib/64/SDL2/Linux
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-linux-sdl2-static-lib.tar.gz - | tar xz -C lib/64/SDL2/Linux
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-linux-sdl2-static-lib.tar.gz - | tar xz -C lib/64/SDL2/Linux
 
 # Windows
 mkdir -p lib/64/SDL2/Windows
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-windows-sdl2-static-lib.tar.gz - | tar xz -C lib/64/SDL2/Windows
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-windows-sdl2-static-lib.tar.gz - | tar xz -C lib/64/SDL2/Windows
 
 ###############################
 # Download OpenSSL libraries
@@ -59,7 +59,7 @@ aws s3 cp s3://fractal-protocol-shared-libs/fractal-windows-sdl2-static-lib.tar.
 # Emscripten
 mkdir -p lib/64/openssl
 mkdir -p lib/64/openssl/Emscripten
-aws s3 cp s3://fractal-protocol-shared-libs/fractal-emscripten-libcrypto.tar.gz - | tar xz -C lib/64/openssl/Emscripten
+aws s3 cp --only-show-errors s3://fractal-protocol-shared-libs/fractal-emscripten-libcrypto.tar.gz - | tar xz -C lib/64/openssl/Emscripten
 
 ###############################
 echo "Download Completed"
