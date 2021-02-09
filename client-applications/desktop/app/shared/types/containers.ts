97f4336c1d804e38642d7003d47e7188a1ed0a1c
Ming <ming@fractal.co>
2021-02-09 00:05:08 -0500

    Electron compatibility with one window (#651)
    * stdin writing and reading works!
    
    * giving up
    
    * good stopping point for suriya
    
    * remove stringio package
    
    * fixed missing import
    
    * pass arguments via pipe to protocol
    
    * receive and properly process piped arguments
    
    * move SDL renderer to after parse_args, but before spin
    
    * read in piped arguments after video has begun (loading screen)
    
    * add newline to end of files
    
    * stdin writing and reading works!
    
    * giving up
    
    * good stopping point for suriya
    
    * remove stringio package
    
    * fixed missing import
    
    * pass arguments via pipe to protocol
    
    * receive and properly process piped arguments
    
    * move SDL renderer to after parse_args, but before spin
    
    * read in piped arguments after video has begun (loading screen)
    
    * modifications to make login screen work
    
    * client app handles container/assign errors wip
    
    * wip
    
    * wip
    
    * working protocol set to useState
    
    * removing comments
    
    * wip
    
    * wip
    
    * aws ping swappedo ut
    
    * fixing timeout and force quit
    
    * everything working for first time on one window
    
    * moved region fetching to loading screen to improve load times
    
    * linted
    
    Co-authored-by: mingy98 <ming@fractalcomputers.com>
    Co-authored-by: Suriya Kandaswamy <suriya.kandaswamy@gmail.com>

--- client-applications/desktop/app/shared/types/containers.ts
+++ client-applications/desktop/app/shared/types/containers.ts
@@ -1,6 +1,7 @@
 // State of container creation from GraphQL
 export enum FractalAppState {
     PENDING = "PENDING", // existing container being assigned
+    NO_TASK = "NO_TASK",
     READY = "READY", // successfully spun up, ready to connect
     FAILURE = "FAILURE", // failed to spin up, cannot be connected to
     CANCELLED = "CANCELLED", // has been cancelled (treated as a FAILURE effectivelly)
