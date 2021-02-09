9839bb53c5ff3fcde2b5adc73439e2bda4e5b6e9
FishmanL <leor@fractal.co>
2021-02-09 10:03:47 -0500

    fixed our celery errors being completely unreadable

--- main-webserver/app/celery_utils.py
+++ main-webserver/app/celery_utils.py
@@ -9,10 +9,12 @@ def init_celery(celery, app):
     """
     celery.conf.update(
         task_track_started=True,
-        accept_content=["json"],
-        result_accept_content=["json"],
+        accept_content=["json", "pickle"],
+        result_accept_content=["json", "pickle"],
         worker_hijack_root_logger=True,
         celery_redis_max_connections=40,
+        task_serializer='pickle',
+        result_serializer='pickle',
     )
     TaskBase = celery.Task  # pylint: disable=invalid-name
 
