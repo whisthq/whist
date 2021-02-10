72d4c9bedbcd3aa52dffe94becb296b02380b548
djsavvy <sraghuvanshi@college.harvard.edu>
2021-02-10 17:09:38 +0000

    Refactor helper scripts into their own directory in container-images

--- container-images/helper-scripts/copy_protocol_build.sh
+++ container-images/helper-scripts/copy_protocol_build.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 # Retrieve source directory of this script
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
+cd "$DIR/.."
 
 # Create temporary directory for build dependencies
 mkdir -p "base/build-temp/protocol"
