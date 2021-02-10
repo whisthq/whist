8273beca64714aa87794e4ffd06a879b3c88189d
mingy98 <ming@fractalcomputers.com>
2021-02-09 21:53:45 -0500

    mail client wip
    remove old stripe mail endpoints
    
    wip
    
    mail tests wip

--- main-webserver/app/blueprints/mail/mail_blueprint.py
+++ main-webserver/app/blueprints/mail/mail_blueprint.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
+from flask import jsonify, current_app
 
 from app import fractal_pre_process
 from app.helpers.blueprint_helpers.mail.mail_post import mail_helper
 
 mail_bp = Blueprint("mail_bp", __name__)
-
 
 @mail_bp.route("/mail", methods=["POST"])
 @fractal_pre_process
@@ -22,9 +22,10 @@ def mail(**kwargs):
     body = kwargs.pop("body")
     args = (
         body.pop("email_id"),
-        body.pop("from_email"),
-        body.pop("to_emails"),
-        body.pop("email_args")
+        body.pop("from_email", current_app.config["SENDGRID_DEFAULT_FROM"]),
+        body.pop("to_email"),
+        body.pop("email_args"),
     )
 
-    mail_helper(*args)
+    output = mail_helper(*args)
+    return jsonify(output), output["status"]
