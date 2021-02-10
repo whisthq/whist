2825fa25c904490f8c9b2035f8661fc5f4f9c020
Nicholas Pipitone <npip99@gmail.com>
2021-02-10 12:07:50 -0500

    Simplify find_images_in_git_repo.sh

--- container-images/find_images_in_git_repo.sh
+++ container-images/find_images_in_git_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script walks the folder tree it is run in, finds all Dockerfile.20 it contains and 
+# This script walks the folder tree it is run in, finds all Dockerfile.20 it contains and
 # formats their path properly for pushing to GitHub Container Registry.
 
 # Retrieve relative subfolder path
@@ -8,23 +8,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-# Find all the Dockerfiles in subfolder
-dockerfile_paths=()
-find . -name "Dockerfile.20" -print0 >tmpfile
-while IFS=  read -r -d $'\0'; do
-    dockerfile_paths+=("$REPLY")
-done <tmpfile
-rm -f tmpfile
+# Find all the Dockerfiles in this directory, match to "./[PATH]/Dockerfile.20", and replace '\n' with ' '
+find . -name "Dockerfile.20" | sed 's/^\.\/\(.*\)\/Dockerfile\.20$/\1/g' | tr '\n' ' '
 
-# Parse Dockerfile paths to get the proper format for GitHub Container Registry
-apps=()
-for path in "${dockerfile_paths[@]}"
-do
-    :
-    path=${path%/Dockerfile.20}
-    path=${path#"./"}
-    apps+=($path)
-done
-
-# Output images to stdout, for our continuous integratin pipeline
-printf '%s ' "${apps[@]}"
