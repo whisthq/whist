1b6323172f1e2cf3457be398e2cb9b50f274e030
Owen Niles <owen@fractal.co>
2021-02-11 16:12:49 -0500

    Delete an unmaintained container_serial test.

--- main-webserver/tests/aws/test_aws_container.py
+++ main-webserver/tests/aws/test_aws_container.py
@@ -1,26 +1,18 @@
-import copy
 import logging
 import os
 import uuid
 
-from collections import defaultdict
-
 import pytest
-import celery
-from celery import shared_task
 
 from app.celery import aws_ecs_creation
 from app.celery.aws_ecs_modification import update_cluster
+from app.helpers.utils.aws.base_ecs_client import ECSClient
 from app.helpers.utils.general.logs import fractal_log
 from app.helpers.utils.general.sql_commands import fractal_sql_commit
 from app.models import ClusterInfo, db, UserContainer, RegionToAmi
 
-from ..helpers.general.progress import fractalJobRunner, queryStatus
+from ..helpers.general.progress import queryStatus
 from ..patches import function
-
-from app.helpers.utils.aws.base_ecs_client import ECSClient
-from app.celery.aws_ecs_modification import manual_scale_cluster
-
 
 pytest.cluster_name = f"test-cluster-{uuid.uuid4()}"
 pytest.container_name = None
@@ -423,124 +415,3 @@ def test_delete_cluster(client, cluster=pytest.cluster_name):
         )
         assert False
     assert True
-
-
-@pytest.mark.skipif(
-    "CHOOSE_CLUSTER_TEST" not in os.environ,
-    reason="This test is slow and reource heavy; run only upon explicit request",
-)
-def test_cluster_assignment():
-    check_test_database(input_token, admin_token)
-    fractal_log(
-        function="test_cluster_assignment",
-        label="cluster/assign",
-        logs="First deleting all clusters in database",
-    )
-
-    def delete_cluster_helper(cluster):
-        fractal_log(
-            function="test_cluster_assignment",
-            label="cluster/delete",
-            logs="Deleting cluster {} from database".format(cluster),
-        )
-        cluster_info = ClusterInfo.query.get(cluster)
-        fractal_sql_commit(db, lambda db, x: db.session.delete(x), cluster_info)
-
-        if ClusterInfo.query.get(cluster):
-            fractal_log(
-                function="test_cluster_assignment",
-                label="cluster/delete",
-                logs="Cluster was not deleted in database",
-                level=logging.ERROR,
-            )
-            assert False
-        assert True
-
-    all_clusters = ClusterInfo.query.all()
-    clusters = [
-        cluster_info.cluster for cluster_info in all_clusters if "cluster_" in cluster_info.cluster
-    ]
-    if all_clusters:
-        fractal_log(
-            function="test_cluster_assignment",
-            label="cluster/delete",
-            logs="Found {num_clusters} cluster(s) already in database. Starting to delete...".format(
-                num_clusters=str(len(clusters))
-            ),
-        )
-        fractalJobRunner(delete_cluster_helper, clusters)
-
-    else:
-        fractal_log(
-            function="test_cluster_assignment",
-            label="cluster/delete",
-            logs="No clusters found in database",
-        )
-
-    first_cluster = f"test-cluster-{uuid.uuid4()}"
-    test_create_cluster(input_token, admin_token, max_containers=2, cluster_name=first_cluster)
-    clusters_to_containers = defaultdict(list)
-    container_cluster = first_cluster
-    container_cluster_info = ClusterInfo.query.filter_by(cluster=container_cluster).first()
-
-    while True:
-        pytest.cluster_name = None
-        container_info = test_create_container(input_token, admin_token, cluster_name=None)
-        new_container, new_container_cluster = (
-            container_info["container_id"],
-            container_info["cluster"],
-        )
-        clusters_to_containers[new_container_cluster].append(new_container)
-        old_info = copy.copy(container_cluster_info)
-        db.session.expire(container_cluster_info)
-        container_cluster_info = ClusterInfo.query.get(new_container_cluster)
-
-        if (
-            old_info.registeredContainerInstancesCount < old_info.maxContainers
-            or old_info.maxMemoryRemainingPerInstance > 8500
-        ):
-            assert container_cluster == new_container_cluster
-            assert (
-                container_cluster_info.maxMemoryRemainingPerInstance
-                <= old_info.maxMemoryRemainingPerInstance
-                or container_cluster_info.registeredContainerInstancesCount
-                > old_info.registeredContainerInstancesCount
-            )
-        else:
-            assert container_cluster != new_container_cluster
-            assert (
-                container_cluster_info.maxMemoryRemainingPerInstance
-                >= old_info.maxMemoryRemainingPerInstance
-            )
-            db.session.expire(container_cluster_info)
-            break
-
-        container_cluster = new_container_cluster
-    first_cluster_info = ClusterInfo.query.get(first_cluster)
-    old_first_cluster_info = copy.copy(first_cluster_info)
-    container_to_delete = clusters_to_containers[first_cluster].pop()
-    test_delete_container(input_token, admin_token, container_to_delete)
-    db.session.refresh(first_cluster_info)
-    new_first_cluster_info = copy.copy(first_cluster_info)
-    assert (
-        new_first_cluster_info.maxMemoryRemainingPerInstance
-        >= old_first_cluster_info.maxMemoryRemainingPerInstance
-    )
-
-    pytest.cluster_name = None
-    container_info = test_create_container(input_token, admin_token, cluster_name=None)
-    new_container, assigned_cluster = container_info["container_id"], container_info["cluster"]
-    clusters_to_containers[assigned_cluster].append(new_container)
-    assert assigned_cluster == first_cluster
-    db.session.refresh(first_cluster_info)
-    assigned_cluster_info = copy.copy(first_cluster_info)
-    assert (
-        assigned_cluster_info.maxMemoryRemainingPerInstance
-        <= new_first_cluster_info.maxMemoryRemainingPerInstance
-    )
-    db.session.expire(first_cluster_info)
-
-    for cluster in clusters_to_containers.keys():
-        for container in clusters_to_containers[cluster]:
-            test_delete_container(input_token, admin_token, container)
-        test_delete_cluster(input_token, admin_token, cluster)
