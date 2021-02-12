fcef769cee83f82c5f544555e9a6203558760263
djsavvy <sraghuvanshi@college.harvard.edu>
2021-02-12 14:17:48 -0500

    Remove extraneous "\n" from prints in db_setup.sh

--- main-webserver/db_setup/db_setup.sh
+++ main-webserver/db_setup/db_setup.sh
@@ -13,14 +13,14 @@ cd "$DIR"
 # has an initialized user/database. CI uses URI connection method.
 IN_CI=${CI:=false} # default: false
 if [ $IN_CI == "true" ]; then
-  echo "=== Initializing db for CI ===\n"
+  echo "=== Initializing db for CI ==="
 
   # copy (slightly modified; see modify_ci_db_schema.py) schema
   psql -d $POSTGRES_LOCAL_URI -f db_schema.sql
-  echo "===   Errors are ok as long as the db was made   ===\n"
+  echo "===   Errors are ok as long as the db was made   ==="
 
   # copy select data
-  echo "===             Putting data into db             ===\n"
+  echo "===             Putting data into db             ==="
   psql -d $POSTGRES_LOCAL_URI -f db_data.sql
 
   exit 0
@@ -28,8 +28,8 @@ fi
 
 POSTGRES_LOCAL_PORT=${POSTGRES_LOCAL_PORT:=9999} # default: 9999
 
-echo "=== Make sure to update the schema periodically ===\n"
-echo "===            Initializing db                  ===\n"
+echo "=== Make sure to update the schema periodically ==="
+echo "===            Initializing db                  ==="
 
 # make user. initially just "postgres" user exists
 cmds="CREATE ROLE $POSTGRES_LOCAL_USER WITH LOGIN CREATEDB;\q"
@@ -40,9 +40,9 @@ psql -h $POSTGRES_LOCAL_HOST -p $POSTGRES_LOCAL_PORT -U $POSTGRES_LOCAL_USER -d 
 
 # copy schema
 psql -h $POSTGRES_LOCAL_HOST -p $POSTGRES_LOCAL_PORT -U $POSTGRES_LOCAL_USER -d $POSTGRES_LOCAL_DB -f db_schema.sql
-echo "===   Errors are ok as long as the db was made   ===\n"
+echo "===   Errors are ok as long as the db was made   ==="
 
 # copy select data
-echo "===             Putting data into db             ===\n"
+echo "===             Putting data into db             ==="
 psql -h $POSTGRES_LOCAL_HOST -p $POSTGRES_LOCAL_PORT -U $POSTGRES_LOCAL_USER -d $POSTGRES_LOCAL_DB -f db_data.sql
 
