620ab5676332ccbfc51d7daf5c320cf4d7ad1f28
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2021-02-12 19:19:31 -0500

    Try it with the Python script now!

--- .github/workflows/helpers/migration/main.py
+++ .github/workflows/helpers/migration/main.py
@@ -45,6 +45,7 @@ diff = postgres.schema_diff(DB_CONFIG_CURRENT, DB_CONFIG_MERGING)
 
 # If there's no schema diff, exit with code 0.
 if not diff:
+    print("No schema changes for this PR!")
     sys.exit(0)
 
 # If there's a diff, we need to test it against the current database
@@ -63,9 +64,15 @@ postgres.sql_commands(SCHEMA_PATH_DIFFING, **DB_CONFIG_CURRENT)
 test_diff = postgres.schema_diff(DB_CONFIG_CURRENT, DB_CONFIG_MERGING)
 
 if not test_diff:
+    print("There's some changes to be made to the schema!")
+    print("Running these SQL commands will perform the migration:\n\n")
     print(diff)
     sys.exit(2)
 else:
+    print("This migration might not go properly.")
+    print("Here's the diff between schemas:\n\n")
     print(diff)
+    print("\n\n")
+    print("Here's what didn't make it through the diff test:\n\n")
     print(test_diff)
     sys.exit(4)
