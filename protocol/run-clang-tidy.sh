c46c0784e301d736af58026f7c1e82dd932c2fab
Nicholas Pipitone <npip99@gmail.com>
2021-02-16 13:03:33 -0500

    Don't fail if clang-tidy fails

--- protocol/run-clang-tidy.sh
+++ protocol/run-clang-tidy.sh
@@ -65,7 +65,8 @@ done
 # header files to be included in clang-tidy (we don't want to include third-party folders, only our code)
 headerFilter="desktop/|fractal/|server/"
 
-clang-tidy --header-filter=$headerFilter --quiet --export-fixes=$yamlFolder/$fixesFilename $filesToFix
+# Run clang-tidy, with || true so that the bash script doesn't exit if clang-tidy fails
+clang-tidy --header-filter=$headerFilter --quiet --export-fixes=$yamlFolder/$fixesFilename $filesToFix || true
 
 # ---- clean up yaml file before running replacements ----
 
