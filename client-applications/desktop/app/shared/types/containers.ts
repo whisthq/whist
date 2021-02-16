681b53eb0b5a45a0b3c142a4a83aae530a173d74
djsavvy <sraghuvanshi@college.harvard.edu>
2021-02-16 13:24:21 -0500

    Clarify comment

--- client-applications/desktop/app/shared/types/containers.ts
+++ client-applications/desktop/app/shared/types/containers.ts
@@ -1,6 +1,6 @@
 // State of container creation from GraphQL
 export enum FractalAppState {
-    PENDING = "PENDING", // container being assigned
+    PENDING = "PENDING", // existing container being assigned
     READY = "READY", // successfully spun up, ready to connect
     FAILURE = "FAILURE", // failed to spin up, cannot be connected to
     CANCELLED = "CANCELLED", // has been cancelled (treated as a FAILURE effectivelly)
