11b87f312176fd24f8af190bb111a5a11398be49
Steve Li <lithafnium2000@gmail.com>
2021-02-17 14:49:58 -0500

    cdocumentation

--- main-webserver/app/models/limiter.py
+++ main-webserver/app/models/limiter.py
@@ -1,6 +1,7 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+# initializes flask rate limiter
 limiter = Limiter(key_func=get_remote_address)
 
 LIMIT = "10 per minute"
