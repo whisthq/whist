a713ad57a5a3d0d76c1a32fca58f495d4c4de0e3
Roshan Padaki <roshan@fractal.co>
2021-02-18 17:10:45 -0500

    Send the container delete message AFTER sending logs to avoid a webserver race condition, by moving responsibility from protocol to the container image.

--- .github/workflows/helpers/ecs-host-setup/poll_webserver_update_region_celery.sh
+++ .github/workflows/helpers/ecs-host-setup/poll_webserver_update_region_celery.sh
@@ -19,6 +19,7 @@ state=PENDING
 echo "ID: $2"
 while [[ $state =~ PENDING ]] || [[ $state =~ STARTED ]]; do
     status=$(curl -L -X GET -H "Authorization: Bearer ${3}" "${1}status/${2}")
+    sleep 0.1
     state=$(echo $status | jq -e ".state")
     echo "Status: $status"
 done
