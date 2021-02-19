556cfe46c746a626c072387f60f3419a030ec499
Jannie Zhong <24399232+janniezhong@users.noreply.github.com>
2021-02-19 08:42:12 -0800

    New subscription based electron auth (#715)
    * add initial graphql queries
    
    rebase from dev
    
    * new auth flow implementation. still needs webserver docs and tests
    
    * added webserver tests
    
    cleanup
    
    * linting
    
    * code cleanup
    
    * Update README.md
    
    * use docker/.env variables
    
    * change Login to X-Hasura-Login-Token
    
    * code cleanup
    
    Co-authored-by: Jannie <jz3191@columbia.edu>

--- main-webserver/app/blueprints/admin/hasura_blueprint.py
+++ main-webserver/app/blueprints/admin/hasura_blueprint.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify, request
 from app import fractal_pre_process
 from app.constants.http_codes import SUCCESS
 
-from app.helpers.blueprint_helpers.admin.hasura_get import auth_helper
+from app.helpers.blueprint_helpers.admin.hasura_get import auth_token_helper, login_token_helper
 
 from app.helpers.utils.general.logs import fractal_log
 
@@ -13,8 +13,23 @@ hasura_bp = Blueprint("hasura_bp", __name__)
 @hasura_bp.route("/hasura/auth", methods=["GET"])
 @fractal_pre_process
 def hasura_auth_get(**kwargs):  # pylint: disable=unused-argument
-    token = request.headers.get("Authorization")
-    output = auth_helper(token)
+    """The endpoint for the hasura auth hook.
+
+    All headers passed through GraphQL queries will end up here for processing.
+
+    Returns:
+        A dictionary containing key value pairs for "X-Hasura-Role", "X-Hasura-User-Id",
+        and "X-Hasura-Login-Token" that will be returned to Hasura for permissions checking.
+
+    """
+    auth_token = request.headers.get("Authorization")
+    auth_output = auth_token_helper(auth_token)
+
+    login_token = request.headers.get("X-Hasura-Login-Token")
+    login_output = login_token_helper(login_token)
+
+    output = dict(auth_output)
+    output.update(login_output)
 
     fractal_log("", "", str(output))
 
