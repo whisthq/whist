cf4282bd493c689f21372016c8195d8a6645e534
Jannie <jz3191@columbia.edu>
2021-02-18 19:09:24 -0800

    parameterize mail tests
    lint code
    
    more linting

--- main-webserver/app/helpers/blueprint_helpers/mail/mail_post.py
+++ main-webserver/app/helpers/blueprint_helpers/mail/mail_post.py
@@ -1,10 +1,27 @@
 from flask import current_app
 
 from app.constants.http_codes import SUCCESS, BAD_REQUEST
-from app.helpers.utils.mail.mail_client import MailClient, SendGridException
+from app.helpers.utils.mail.mail_client import MailClient, SendGridException, TemplateNotFound
 
 
 def mail_helper(email_id, from_email, to_email, email_args):
+    """
+    Helper function for the /mail route.
+
+    Calls the send_email function, and catches possible exceptions.
+
+    Parameters:
+        email_id (str): ID of the email being sent (ex. EMAIL_VERIFICATION, PASSWORD_RESET,
+                        etc.)
+        from_email (str): email being sent from
+        to_email (str): email being sent to
+        email_args (dict): dict of Jinja arguments to pass into render_template()
+
+    Returns:
+        A dictionary object with the single key "status", representing the HTTP response to the
+        request.
+    """
+
     try:
         mail_client = MailClient(current_app.config["SENDGRID_API_KEY"])
         mail_client.send_email(
@@ -12,4 +29,6 @@ def mail_helper(email_id, from_email, to_email, email_args):
         )
         return {"status": SUCCESS}
     except SendGridException:
+        return {"status": BAD_REQUEST}
+    except TemplateNotFound:
         return {"status": BAD_REQUEST}
