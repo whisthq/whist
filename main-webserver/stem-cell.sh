8387ea1dcf4091ab4c3c5ce1b8478cb329e269c0
FishmanL <leor.fishman@gmail.com>
2021-02-19 12:46:04 -0500

    Revert "Add additional documentation to stem-cell.sh"
    This reverts commit 490bce793b67b8bddde5f8f55cf3f1eea169a536.

--- main-webserver/stem-cell.sh
+++ main-webserver/stem-cell.sh
@@ -3,17 +3,6 @@
 # Used as the entrypoint by Dockerfile to determine if the instance
 # should be running as `web` or `celery`.
 
-# The web and celery services in our docker-compose stack are so similar that
-# they share the same Dockerfile. The web service only differs from the celery
-# service because the commands specified for each service in
-# docker/docker-compose.yml, is different. This file is the entrypoint for
-# both containers. See https://docs.docker.com/engine/reference/builder/#cmd to
-# learn about the interaction between Dockerfile entrypoints and commands.
-
-# This file is named "stem-cell.sh" because just as stem cells can grow up to
-# become any kind of cell, this file can execute either a web or celery process
-# depending on its argument.
-
 # TODO use this as the entrypoint for the Procfile as well, e.g.
 # Procfile
 #   web: ./stem-cell.sh web
