0fdbd624eb0fb98d6ca886bd8f36e9e105eb9ab5
FishmanL <leor@fractal.co>
2021-02-22 16:19:41 -0500

    fix no new variables

--- ecs-host-service/ecs-host-service.go
+++ ecs-host-service/ecs-host-service.go
@@ -555,8 +555,8 @@ func handleStartValuesRequest(req *httpserver.SetContainerStartValuesRequest) er
 	}
 
 	// Actually write ID information to file
-	filename := datadir + "ID"
-	err := writeAssignmentToFile(filename, req.ContainerID)
+	id_filename := datadir + "ID"
+	err := writeAssignmentToFile(id_filename, req.ContainerID)
 	if err != nil {
 		return logger.MakeError("Could not write value %v to ID file %v. Error: %s", req.ContainerID, filename, err)
 	}
