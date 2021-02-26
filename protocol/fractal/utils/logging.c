88bc8e873c5f1a3c7a333edcb552d4623bcdfbb6
Nicholas Pipitone <npip99@gmail.com>
2021-02-26 16:34:08 -0500

    Clean logging

--- protocol/fractal/utils/logging.c
+++ protocol/fractal/utils/logging.c
@@ -8,12 +8,15 @@ Usage
 ============================
 We have several levels of logging.
 - NO_LOG: self explanatory
-- ERROR_LEVEL: only log errors. Errors are conditions that cause the program to
-               terminate or lead to an irrecoverable state.
+- ERROR_LEVEL: only log errors. Errors are problems that must be addressed,
+               as they indicate a fundamental probem with the protocol,
+               but they represent a status that can be recovered from.
 - WARNING_LEVEL: log warnings and above (warnings and errors). Warnings are when
-                 things do not work as expected, but we can recover.
+                 something did not work as expected, but do not necessarily imply
+                 that the protocol is at fault, as it may be an issue with
+                 their environment (No audio device found, packet loss during handshake, etc).
 - INFO_LEVEL: log info and above. Info is just for logs that provide additional
-              information on state. e.g decode time
+              information on the state of the protocol. e.g. decode time
 - DEBUG_LEVEL: log debug and above. For use when actively debugging a problem,
                but for things that don't need to be logged regularly
 The log level defaults to DEBUG_LEVEL, but it can also be passed as a compiler
@@ -21,10 +24,6 @@ flag, as it is in the root CMakesList.txt, which sets it to DEBUG_LEVEL for
 Debug builds and WARNING_LEVEL for release builds.
 Note that these macros do not need an additional \n character at the end of your
 format strings.
-We also have a LOG_IF(condition, format string) Macro which only logs if the
-condition is true. This can be used for debugging or if we want to more
-aggressively log something when a flag changes. For example in this file you
-could #define LOG_AUDIO True and then use LOG_IF(LOG_AUDIO, "my audio logging").
 */
 
 #if defined(_WIN32)
