e5818b78d302582c65d270350f8305ef3185b5db
jmather625 <jatinm2@illinois.edu>
2021-02-26 12:11:55 -0600

    added flag for local deploy with dev db

--- main-webserver/docker/local_deploy.sh
+++ main-webserver/docker/local_deploy.sh
@@ -12,9 +12,16 @@ cd "$DIR"
 # Allow passing `--down` to spin down the docker-compose stack, instead of
 # having to cd into this directory and manually run the command.
 if [[ $* =~ [:space:]*--down[:space:]* ]]; then
-  echo "Running \"docker-compose down\"..."
+  echo "Running \"docker-compose down\". Ignore any warnings about unset variables."
   docker-compose down
   exit 0
+fi
+
+USE_DEV_DB=false
+# Allow passing `--use-dev-db` to use the dev database
+if [[ $* =~ [:space:]*--use-dev-db[:space:]* ]]; then
+  echo "WARNING: Using the dev db."
+  USE_DEV_DB=true
 fi
 
 # Make sure .env file exists
@@ -22,10 +29,6 @@ if [ ! -f .env ]; then
     echo "Did not find docker/.env file. Make sure you have run docker/retrieve_config.sh!"
     exit 1
 fi
-
-# Make sure we have dummy SSL certificates. Note that we don't care if we
-# overwrite existing ones.
-../dummy_certs/create_dummy_certs.sh
 
 # add env vars to current env. these tell us the host, db, role, pwd
 export $(cat .env | xargs)
@@ -36,30 +39,47 @@ export POSTGRES_REMOTE_USER=$POSTGRES_USER
 export POSTGRES_REMOTE_PASSWORD=$POSTGRES_PASSWORD
 export POSTGRES_REMOTE_DB=$POSTGRES_DB
 
-if [ -f ../db_setup/db_schema.sql ]; then
-    echo "Found existing schema and data sql scripts. Skipping fetching db."
+if [ $USE_DEV_DB == "true" ]; then
+    export POSTGRES_LOCAL_URI=postgres://${POSTGRES_REMOTE_USER}:${POSTGRES_REMOTE_PASSWORD}@${POSTGRES_REMOTE_HOST}/${POSTGRES_REMOTE_DB}
+
+    # launch all images but dev db
+    docker-compose up --build -d redis web celery # don't spin up postgres_db
+
 else
-    bash ../db_setup/fetch_db.sh
+    # first fetch the current dev db schema
+    if [ -f ../db_setup/db_schema.sql ]; then
+        echo "Found existing schema and data sql scripts. Skipping fetching db."
+    else
+        bash ../db_setup/fetch_db.sh
+    fi
+
+    # Make sure we have dummy SSL certificates. Note that we don't care if we
+    # overwrite existing ones.
+    ../dummy_certs/create_dummy_certs.sh
+
+    # eph db configurations
+    export POSTGRES_LOCAL_HOST="localhost"
+    export POSTGRES_LOCAL_PORT="9999"
+    # we don't need a pwd because local db trusts all incoming connections
+    export POSTGRES_LOCAL_USER=$POSTGRES_REMOTE_USER
+    export POSTGRES_LOCAL_DB=$POSTGRES_REMOTE_DB
+
+    export POSTGRES_LOCAL_URI=postgres://${POSTGRES_LOCAL_USER}@postgres_db/${POSTGRES_LOCAL_DB}
+
+    # launch images with ephemeral db
+    docker-compose up -d --build
+
+    # let ephemeral db prepare. Check connections using psql.
+    echo "Trying to connect to local db..."
+    cmds="\q"
+    while ! (psql -h $POSTGRES_LOCAL_HOST -p $POSTGRES_LOCAL_PORT -U postgres -d postgres <<< $cmds) &> /dev/null
+    do
+        echo "Connection failed. Retrying in 2 seconds..."
+        sleep 2
+    done
+    
+    # set up the ephemeral  db
+    bash ../db_setup/db_setup.sh
 fi
-
-# local deploy uses localhost db
-export POSTGRES_LOCAL_HOST="localhost"
-export POSTGRES_LOCAL_PORT="9999"
-# we don't need a pwd because local db trusts all incoming connections
-export POSTGRES_LOCAL_USER=$POSTGRES_REMOTE_USER
-export POSTGRES_LOCAL_DB=$POSTGRES_REMOTE_DB
-# launch images using above LOCAL env vars
-docker-compose up -d --build
-
-# let db prepare. Check connections using psql.
-echo "Trying to connect to local db..."
-cmds="\q"
-while ! (psql -h $POSTGRES_LOCAL_HOST -p $POSTGRES_LOCAL_PORT -U postgres -d postgres <<< $cmds) &> /dev/null
-do
-    echo "Connection failed. Retrying in 2 seconds..."
-    sleep 2
-done
-
-bash ../db_setup/db_setup.sh
 
 echo "Success! Teardown when you are done with: docker/local_deploy.sh --down"
