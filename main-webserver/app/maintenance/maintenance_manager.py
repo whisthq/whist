e26d303854287356c171ef602c007462c436bef7
jmather625 <jatinm2@illinois.edu>
2021-03-01 11:50:56 -0600

    cleaned logs, removed newline

--- main-webserver/app/maintenance/maintenance_manager.py
+++ main-webserver/app/maintenance/maintenance_manager.py
@@ -143,17 +143,23 @@ def try_start_update(region_name: str) -> Tuple[bool, str]:
     success = False
     return_msg = None
     tasks = _REDIS_CONN.lrange(tasks_key, 0, -1)
+
     if tasks is None or not tasks:
         # no tasks, we can start the update
         success = True
-        _REDIS_CONN.set(update_key, 1)
-
-        fractal_log(
-            "try_start_update",
-            None,
-            f"putting webserver into maintenance mode in region {region_name}",
-        )
-        return_msg = f"Put webserver into maintenance mode in region {region_name}."
+        # use an atomic setnx operation to start the update. The return is True iff this thread
+        # created the update key. This reduces log cluttering as only the thread that
+        # starts the update ends up printing.
+        update_started = _REDIS_CONN.setnx(update_key, 1)
+        if update_started:
+            fractal_log(
+                "try_start_update",
+                None,
+                f"putting webserver into maintenance mode in region {region_name}",
+            )
+            return_msg = f"Put webserver into maintenance mode in region {region_name}."
+        else:
+            return_msg = f"Webserver is already in maintenance mode in region {region_name}."
 
     else:
         # no tasks, we cannot start the update. However, we set update_key so no new tasks start.
@@ -162,7 +168,7 @@ def try_start_update(region_name: str) -> Tuple[bool, str]:
 
         log = (
             "cannot start update, but stopping new tasks from running."
-            f" Waiting on {len(tasks)} task to finish. Task IDs:\n{tasks}."
+            f" Waiting on {len(tasks)} task to finish. Task IDs:{tasks}."
         )
         fractal_log(
             "try_start_update",
@@ -393,11 +399,11 @@ def maintenance_track_task(func: Callable):
         # pre-function logic: register the task
         if not try_register_task(region_name=region_name, task_id=self_obj.request.id):
             msg = (
-                "Could not register a tracked task. Debug info:\n"
-                f"Celery Task ID: {self_obj.request.id}\n"
-                f"Function: {func.__name__}\n"
-                f"Args: {args}\n"
-                f"Kwargs: {kwargs}\n"
+                "Could not register a tracked task. "
+                f"Celery Task ID: {self_obj.request.id}. "
+                f"Function: {func.__name__}. "
+                f"Args: {args}. "
+                f"Kwargs: {kwargs}."
             )
             fractal_log(
                 "maintenance_track_task",
@@ -405,6 +411,10 @@ def maintenance_track_task(func: Callable):
                 msg,
             )
             raise ValueError("MAINTENANCE ERROR. Failed to register task.")
+
+        fractal_log(
+            "maintenance_track_task", None, f"Registered tracked task: {self_obj.request.id}."
+        )
 
         exception = None
         try:
@@ -418,12 +428,11 @@ def maintenance_track_task(func: Callable):
             task_id=self_obj.request.id,
         ):
             msg = (
-                f"CRITICAL! Could not deregister a tracked task.\n"
-                "Debug info:\n"
-                f"Celery Task ID: {self_obj.request.id}\n"
-                f"Function: {func.__name__}\n"
-                f"Args: {args}\n"
-                f"Kwargs: {kwargs}\n"
+                f"CRITICAL! Could not deregister a tracked task. "
+                f"Celery Task ID: {self_obj.request.id}. "
+                f"Function: {func.__name__}. "
+                f"Args: {args}. "
+                f"Kwargs: {kwargs}."
             )
             fractal_log(
                 "maintenance_track_task",
@@ -433,6 +442,10 @@ def maintenance_track_task(func: Callable):
             )
             # exception can be None here
             raise ValueError("MAINTENANCE ERROR. Failed to deregister task.") from exception
+
+        fractal_log(
+            "maintenance_track_task", None, f"Deregistered tracked task: {self_obj.request.id}."
+        )
 
         # raise any exception to caller
         if exception is not None:
