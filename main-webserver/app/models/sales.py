8508aa5bd8d821e5b6564f5cf0052ef2745239b9
Jannie <jz3191@columbia.edu>
2021-03-01 10:29:11 -0800

    add docs + other cleanup

--- main-webserver/app/models/sales.py
+++ main-webserver/app/models/sales.py
@@ -2,6 +2,13 @@ from ._meta import db
 
 
 class EmailTemplates(db.Model):
+    """Stores all email templates that could be sent out to users. Use for the MailClient
+    in app/helpers/utils/mail.py.
+
+    Args:
+        db (SQLAlchemy db): Implements db methods to communicate with the physical infra.
+    """
+
     __tablename__ = "email_templates"
     __table_args__ = {"schema": "sales", "extend_existing": True}
 
