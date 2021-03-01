6d0db8042ab6ed17831ae18129d192c00b6d8091
mstur <sturzu@college.harvard.edu>
2021-03-01 11:04:36 -0500

    Fixes Slack Notification Formatting for Hanging Resources (#1047)
    * switched to matrix var
    
    * testing new line
    
    * list formatting
    
    * forgot to press save
    
    * backslashes
    
    * remove sed
    
    * default not there
    
    * default not there
    
    * default not there
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing env var instead
    
    * commenting
    
    * putting cron back
    
    * forgot to remove debugging stuff

--- .github/workflows/helpers/aws/resource_cleanup.py
+++ .github/workflows/helpers/aws/resource_cleanup.py
@@ -5,19 +5,31 @@ import sys
 # Uses AWS CLI to return all Auto Scaling Groups in AWS for a specified region
 def get_all_aws_asgs(region):
     all_asgs, _ = subprocess.Popen(
-        ["aws", "autoscaling", "describe-auto-scaling-groups", "--no-paginate", "--region", region],
+        [
+            "aws",
+            "autoscaling",
+            "describe-auto-scaling-groups",
+            "--no-paginate",
+            "--region",
+            region,
+        ],
         stdout=subprocess.PIPE,
     ).communicate()
-    all_asgs = [asg["AutoScalingGroupARN"] for asg in json.loads(all_asgs)["AutoScalingGroups"]]
+    all_asgs = [
+        asg["AutoScalingGroupARN"] for asg in json.loads(all_asgs)["AutoScalingGroups"]
+    ]
     return all_asgs
+
 
 # Uses AWS CLI to return all clusters in AWS for a specified region
 def get_all_aws_clusters(region):
     all_cluster_arns, _ = subprocess.Popen(
-        ["aws", "ecs", "list-clusters", "--no-paginate", "--region", region], stdout=subprocess.PIPE
+        ["aws", "ecs", "list-clusters", "--no-paginate", "--region", region],
+        stdout=subprocess.PIPE,
     ).communicate()
     all_cluster_arns = json.loads(all_cluster_arns)["clusterArns"]
     return all_cluster_arns
+
 
 # Takes a Cluster ARN and maps it to its Auto Scaling Group(s) via its associated Capacity Provider(s)
 def cluster_to_asgs(cluster_arn, region):
@@ -34,7 +46,9 @@ def cluster_to_asgs(cluster_arn, region):
         ],
         stdout=subprocess.PIPE,
     ).communicate()
-    capacity_providers = json.loads(capacity_providers)["clusters"][0]["capacityProviders"]
+    capacity_providers = json.loads(capacity_providers)["clusters"][0][
+        "capacityProviders"
+    ]
     if len(capacity_providers) > 0:
         asgs, _ = subprocess.Popen(
             [
@@ -58,6 +72,7 @@ def cluster_to_asgs(cluster_arn, region):
     else:
         return []
 
+
 # Queries specified db for all clusters in a specified region
 def get_db_clusters(url, secret, region):
     clusters, _ = subprocess.Popen(
@@ -80,6 +95,7 @@ def get_db_clusters(url, secret, region):
     clusters = json.loads(clusters)["data"]["hardware_cluster_info"]
     return [list(cluster.values())[0] for cluster in clusters]
 
+
 # Compares list of ASGs in AWS to list of ASGs associated with Clusters in AWS
 def get_hanging_asgs(region):
     cluster_asgs = [
@@ -89,15 +105,20 @@ def get_hanging_asgs(region):
         if cluster_arn is not None
     ]
     all_asgs = get_all_aws_asgs(region)
-    
-    # return list of ASGs not assocated a clusters (note: could be more than one ASG per cluster)
-    return list(set(all_asgs) - set(cluster_asgs))
+
+    # return names (not ARNs) of ASGs not assocated a cluster (could be more than one ASG per cluster)
+
+    return [asg.split("/")[-1] for asg in list(set(all_asgs) - set(cluster_asgs))]
+
 
 # Compares list of clusters in AWS to list of clusters in all DBs
 def get_hanging_clusters(urls, secrets, region):
-    # parses names of clusters from ARNs
-    aws_clusters = set([cluster.split("/")[-1] for cluster in get_all_aws_clusters(region)])
-    aws_clusters.remove('default')
+    # parses names of clusters from ARNs since only the names are stored in the DB
+    aws_clusters = set(
+        [cluster.split("/")[-1] for cluster in get_all_aws_clusters(region)]
+    )
+    if "default" in aws_clusters:
+        aws_clusters.remove("default")
 
     # loop through each of dev, staging, and prod; add clusters to db_clusters
     db_clusters = set()
@@ -107,16 +128,23 @@ def get_hanging_clusters(urls, secrets, region):
     # return list of cluster names in aws but not in db, ignoring the 'default' cluster
     return list(aws_clusters - db_clusters)
 
+
 def main():
     component = sys.argv[1]
     region = sys.argv[2]
     urls = sys.argv[3:6]
     secrets = sys.argv[6:9]
 
+    # format as bulleted list for Slack notification
     if component == "ASGs":
-        print([str(x) for x in get_hanging_asgs(region)])
+        asgs = get_hanging_asgs(region)
+        if len(asgs) > 0:
+            print("\\n- \\`" + "\\`\\n- \\`".join([str(x) for x in asgs]) + "\\`")
     elif component == "Clusters":
-        print([str(x) for x in get_hanging_clusters(urls, secrets, region)])
+        clusters = get_hanging_clusters(urls, secrets, region)
+        if len(clusters) > 0:
+            print("\\n- \\`" + "\\`\\n- \\`".join([str(x) for x in clusters]) + "\\`")
+
 
 if __name__ == "__main__":
     main()
