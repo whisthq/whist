de7f854b1965c67e1e262787061ec7ac65d0cc9a
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-01 12:32:44 -0500

    Refactor footer component to use tailwind

--- website/src/shared/components/footer/components/pressCoverage/components/coverageBox.tsx
+++ website/src/shared/components/footer/components/pressCoverage/components/coverageBox.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 
-import "shared/components/footer/components/pressCoverage/components/coverageBox.css"
-
 export const CoverageBox = (props: {
     image: string
     text: string
@@ -12,11 +10,16 @@ export const CoverageBox = (props: {
     return (
         <div style={{ position: "relative" }}>
             <a href={link} target="_blank" rel="noopener noreferrer">
-                <div className="boxWrapper">
-                    <img src={image} className="boxImage" alt="coverage" />
+                <div className="w-full h-full bg-white bg-opacity-10 rounded-lg">
+                    <img src={image}
+                         className="w-full max-h-60 rounded-lg"
+                         style={{ filter: "grayscale(0.8)" }}
+                         alt="coverage" />
                 </div>
-                {/* <div className="overlay">
-                    <div className="overlayBox">{text}</div>
+                {/* <div className="opacity-0 hover:opacity-1 hover:text-black hover:cursor-pointer absolute top-0 left-0 h-full transition duration-1000 text-base tracking-wide">
+                    <div className="rounded-md w-full h-full px-1 py-1.5" bg-green-200>
+                        {text}
+                    </div>
                 </div> */}
             </a>
         </div>
