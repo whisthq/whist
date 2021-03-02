4cfcb0a2a72d915be2f903c6dba1cb028fae71cf
FishmanL <leor@fractal.co>
2021-03-02 10:15:59 -0500

    fixing rate limit tests

--- main-webserver/tests/misc/test_rate_limiter.py
+++ main-webserver/tests/misc/test_rate_limiter.py
@@ -1,8 +1,11 @@
 import pytest
 from flask import g
+from app.helpers.utils.general import limiter
+
 
 # @pytest.fixture
 def test_rate_limiter(client):
+    limiter.enabled = True
     for i in range(10):
         resp = client.post("/newsletter/post")
         assert resp.status_code == 200
