a4778459b7f5d94d1d98e666aee021e1aa7f530d
jmather625 <jatinm2@illinois.edu>
2021-03-02 18:01:30 -0600

    adding exceptions file

---
+++ main-webserver/app/celery/aws_celery_exceptions.py
@@ -0,0 +1,7 @@
+from app.exceptions import _FractalError
+
+
+class ContainerNotAvailableError(_FractalError):
+    params = ("task_arn", "num_tries")
+    message = "AWS task {task_arn} failed to become available after {num_tries}."
+    
