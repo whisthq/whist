554774d791dde8d6d6c156411f9f3328f87aa41f
Owen Niles <owen@fractal.co>
2021-03-03 14:24:20 -0500

    Remove the sales.stripe_products table from the database.

--- main-webserver/app/models/__init__.py
+++ main-webserver/app/models/__init__.py
@@ -13,4 +13,3 @@ from .hardware import (
 from .logs import LoginHistory
 from .oauth import Credential
 from .public import User
-from .sales import StripeProduct
--- main-webserver/app/models/sales.py
+++
@@ -1,9 +0,0 @@
-from ._meta import db
-
-
-class StripeProduct(db.Model):
-    __tablename__ = "stripe_products"
-    __table_args__ = {"schema": "sales", "extend_existing": True}
-
-    stripe_product_id = db.Column(db.String(250), primary_key=True)
-    product_name = db.Column(db.String(250), nullable=False)
