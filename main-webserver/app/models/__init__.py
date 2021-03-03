2c3ff325b79b89de27cbaea504bab7c0d6d7b866
Owen Niles <owen@fractal.co>
2021-03-03 14:22:00 -0500

    Remove the sales.main_newsletter table from the database.

--- main-webserver/app/models/__init__.py
+++ main-webserver/app/models/__init__.py
@@ -13,4 +13,4 @@ from .hardware import (
 from .logs import LoginHistory
 from .oauth import Credential
 from .public import User
-from .sales import MainNewsletter, StripeProduct
+from .sales import StripeProduct
