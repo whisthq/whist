0a614fce35cf2165aae325d8c1eacde7fa1d0562
FishmanL <leor@fractal.co>
2021-03-03 11:26:12 -0500

    savvy's desired renames

--- main-webserver/app/celery/aws_ecs_creation.py
+++ main-webserver/app/celery/aws_ecs_creation.py
@@ -123,7 +123,7 @@ def _mount_cloud_storage(user, container):
         )
 
 
-def _pass_start_values_to_instance(ip, container_id, port, dpi, user_id, token):
+def _pass_start_values_to_instance(ip, container_id, port, dpi, user_id, config_encryption_token):
     """
     Send the instance start values to the host service.
 
@@ -133,7 +133,7 @@ def _pass_start_values_to_instance(ip, container_id, port, dpi, user_id, token):
         port: The port on the instance to which port 32262 within the container has been mapped.
         dpi: The DPI of the client display.
         user_id: The container's assigned user's user ID
-        token: the encryption token for the user's config
+        config_encryption_token: the encryption token for the user's config
     """
 
     try:
@@ -144,7 +144,7 @@ def _pass_start_values_to_instance(ip, container_id, port, dpi, user_id, token):
                 "container_ARN": container_id,
                 "dpi": dpi,
                 "user_id": user_id,
-                "token": token,
+                "config_encryption_token": config_encryption_token,
                 "auth_secret": current_app.config["HOST_SERVICE_SECRET"],
             },
             verify=False,
@@ -313,7 +313,7 @@ def assign_container(
     self,
     username,
     task_definition_arn,
-    token="",
+    config_encryption_token="",
     region_name="us-east-1",
     cluster_name=None,
     dpi=96,
@@ -325,7 +325,7 @@ def assign_container(
     :param self: the celery instance running the task
     :param username: the username of the requesting user
     :param task_definition_arn: which taskdef the user needs a container for
-    :param token: the encryption token for a user's app config
+    :param config_encryption_token: the encryption token for a user's app config
     :param region_name: which region the user needs a container for
     :param cluster_name: which cluster the user needs a container for, only used in test
     :param dpi: the user's DPI
@@ -336,7 +336,14 @@ def assign_container(
     does not exist for functions with celery decorators like this one.
     """
     return _assign_container(
-        self, username, task_definition_arn, token, region_name, cluster_name, dpi, webserver_url
+        self,
+        username,
+        task_definition_arn,
+        config_encryption_token,
+        region_name,
+        cluster_name,
+        dpi,
+        webserver_url,
     )
 
 
@@ -344,7 +351,7 @@ def _assign_container(
     self,
     username,
     task_definition_arn,
-    token,
+    config_encryption_token,
     region_name="us-east-1",
     cluster_name=None,
     dpi=96,
@@ -570,7 +577,7 @@ def _assign_container(
         base_container.port_32262,
         base_container.dpi,
         user.user_id,
-        token,
+        config_encryption_token,
     )
     time.sleep(1)
 
