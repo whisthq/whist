5b460d2f8c9a6398ef6182abd39a259f2bbd3810
jmather625 <jatinm2@illinois.edu>
2021-03-05 14:58:01 -0600

    fix

--- main-webserver/app/config.py
+++ main-webserver/app/config.py
@@ -175,6 +175,7 @@ class DeploymentConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STRIPE_SECRET = property(getter("STRIPE_SECRET"))
     AWS_TASKS_PER_INSTANCE = property(getter("AWS_TASKS_PER_INSTANCE"))
+    SENTRY_DSN = property(getter("AWS_TASKS_PER_INSTANCE"))
 
     @property
     def config_table(self):
