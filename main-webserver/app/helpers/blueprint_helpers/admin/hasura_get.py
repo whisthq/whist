0b3d2a13007972e20ae470e3bdccad34e4a62649
Jannie <jz3191@columbia.edu>
2021-03-09 11:26:41 -0800

    implement full endpoint w/ get_jwt_identity
    cleanup

---
+++ main-webserver/app/helpers/blueprint_helpers/admin/hasura_get.py
@@ -0,0 +1,39 @@
+from flask_jwt_extended import get_jwt_identity
+
+
+def auth_token_helper(auth_token):
+    """A helper function that processes any tokens passed to the /hasura/auth
+    endpoint through the Authorization header
+
+    Args:
+        auth_token: A string that is either empty or contains "Bearer some_auth_token"
+    Returns:
+        A dictionary containing key value pairs for "X-Hasura-Role" and "X-Hasura-User-Id"
+    """
+    hasura_role = {"X-Hasura-Role": "anonymous", "X-Hasura-User-Id": ""}
+
+    if auth_token:
+        auth_token = auth_token.replace("Bearer ", "")
+
+        username = get_jwt_identity()
+        if username:
+            hasura_role = {"X-Hasura-Role": "user", "X-Hasura-User-Id": username}
+
+    return hasura_role
+
+
+def login_token_helper(login_token):
+    """A helper function that processes any tokens passed to the /hasura/auth endpoint
+    through the Login header
+
+    Args:
+        login: A string that is either empty or contains a login token generated from the
+        client-application
+    Returns:
+        A dictionary containing a single key "X-Hasura-Login-Token".
+    """
+    hasura_login = {"X-Hasura-Login-Token": ""}
+    if login_token:
+        hasura_login = {"X-Hasura-Login-Token": login_token}
+
+    return hasura_login
