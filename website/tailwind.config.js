d08ddb501960946c3edd2d606c6dc9bad6b41402
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-08 19:58:18 -0500

    Rewrite website typewriter effect, drop dependency

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -61,6 +61,21 @@ module.exports = {
                 "250px": "250px",
                 "300px": "300px",
             },
+            keyframes: {
+                bounce: {
+                    "0%, 20%, 50%, 80%, 100%": { transform: "translateY(0)" },
+                    "30%": { transform: "translateY(-5px)" },
+                    "60%": { transform: "translateY(-8px)" },
+                },
+                blink: {
+                    "0%, 100%": { opacity: 1 },
+                    "50%": { opacity: 0 },
+                },
+            },
+            animation: {
+                bounce: "bounce 5s infinite",
+                blink: "blink 1s step-end infinite",
+            },
         },
     },
     variants: {
