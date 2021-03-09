f0cd7234cb7d2b760cc9850fa260d747b1cd24a4
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-08 19:00:06 -0500

    Copy core-ts files into monorepo

---
+++ core-ts/src/http/index.ts
@@ -0,0 +1,42 @@
+import { fetchBase } from "./fetchBase"
+import { withCatch } from "./withCatch"
+import { withGet } from "./withGet"
+import { withJSON } from "./withJSON"
+import { withPost } from "./withPost"
+import { withServer } from "./withServer"
+import { withURL } from "./withURL"
+import { withTokenRefresh } from "./withTokenRefresh"
+import { withHandleAuth } from "./withHandleAuth"
+import { ConfigHTTP } from "../types/api"
+import { decorate, decorateDebug } from "../utilities"
+import { identity } from "lodash"
+
+export const configGet = (c: ConfigHTTP) => {
+    const decFn = c.debug ? decorateDebug : decorate
+
+    return decFn(
+        fetchBase,
+        withGet,
+        withCatch,
+        withServer(c.server),
+        withHandleAuth(c.handleAuth || identity),
+        withTokenRefresh(c.endpointRefreshToken || ""),
+        withURL,
+        withJSON
+    )
+}
+
+export const configPost = (c: ConfigHTTP) => {
+    const decFn = c.debug ? decorateDebug : decorate
+
+    return decFn(
+        fetchBase,
+        withPost,
+        withCatch,
+        withServer(c.server),
+        withHandleAuth(c.handleAuth || identity),
+        withTokenRefresh(c.endpointRefreshToken || ""),
+        withURL,
+        withJSON
+    )
+}
---
+++ core-ts/src/http/withURL.ts
@@ -0,0 +1,46 @@
+import { isString } from "lodash"
+import { ServerDecorator } from "../types/api"
+
+/*
+ * Builds a url using a server string and an endpoint string.
+ *
+ * @param server - a string representing a HTTP server
+ * @param endpoint - a string representing a HTTP endpoint
+ * @returns a string representing a HTTP request URL
+ */
+const serverUrl = (server: string, endpoint: string): string =>
+    `${server}${endpoint}`
+
+/*
+ * Ensures that a url is present on a ServerRequest object.
+ * If a url is present, it passes on the object unchanged.
+ * If no url is present, it attempts to build one using the
+ * server and endpoint keys.
+ * Without valid url, server, or endpoint keys, it will
+ * stop the HTTP request and return a error ServerResponse.
+ *
+ * @param fn - The downstream decorator function
+ * @param req - a ServerRequest object
+ * @returns a ServerResponse wrapped in a Promise
+ */
+export const withURL: ServerDecorator = async (fn, req) => {
+    const { url, server, endpoint } = req
+
+    if (isString(url)) {
+        return await fn(req)
+    } else {
+        if (isString(server) && isString(endpoint)) {
+            return await fn({
+                ...req,
+                url: serverUrl(server || "", endpoint || ""),
+            })
+        } else {
+            return {
+                request: req,
+                error: Error(
+                    "Server API not configured with: url or [server, endpoint]."
+                ),
+            }
+        }
+    }
+}
