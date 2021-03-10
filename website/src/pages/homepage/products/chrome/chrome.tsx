9f114de9fdaf79c08be45f5d29184d40b36c5270
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-10 14:34:19 -0500

    Update homepage to match style guide

--- website/src/pages/homepage/products/chrome/chrome.tsx
+++ website/src/pages/homepage/products/chrome/chrome.tsx
@@ -16,7 +16,7 @@ const DarkModeIcon = (props: {dark: boolean, onClick: () => void }) => (
                         onClick={props.onClick}
                         className={classNames("fixed top-16 md:top-24 right-2 md:right-12 dark:bg-transparent",
                                               "border border-transparent dark:border-gray-400 bg-blue-lightest",
-                                              "text-gray dark:text-gray-300 p-2 rounded cursor-pointer")}
+                                              "text-gray dark:text-gray-300 p-2 rounded cursor-pointer z-50")}
                     >
                         <div>
                            {props.dark ? <FaSun /> : <FaMoon/>}
