8ae0421e0ae9af347acd94d5c6d462707617774c
djsavvy <sraghuvanshi@college.harvard.edu>
2021-03-10 20:00:31 +0000

    Start fractalcontainer package

---
+++ ecs-host-service/fractalcontainer/transport_protocol.go
@@ -0,0 +1,41 @@
+package fractalcontainer // import "github.com/fractal/fractal/ecs-host-service/fractalcontainer"
+
+import (
+	"strings"
+
+	logger "github.com/fractal/fractal/ecs-host-service/fractallogger"
+)
+
+const (
+	TransportProtocolTCP transportProtocol = "tcp"
+	TransportProtocolUDP transportProtocol = "udp"
+)
+
+// A transportProtocol is either TCP or UDP. It is used for port mappings of containers.
+type transportProtocol string
+
+// UnmarshalJSON for transportProtocol determines whether to use TCP or UDP,
+// setting TCP as the zero-value but treating other unrecognized values as
+// errors
+func (tp *transportProtocol) UnmarshalJSON(b []byte) error {
+	if strings.ToLower(string(b)) == "null" {
+		*tp = TransportProtocolTCP
+		logger.Errorf("Unmarshalled nil transportProtocol as TCP")
+		return nil
+	}
+	switch string(b) {
+	case `"tcp"`:
+		*tp = TransportProtocolTCP
+	case `"udp"`:
+		*tp = TransportProtocolUDP
+	default:
+		*tp = TransportProtocolTCP
+		return logger.MakeError("TransportProtocol must be \"tcp\" or \"udp\"; Got " + string(b))
+	}
+	return nil
+}
+
+// MarshalJSON overrides the logic for JSON-encoding the transportProtocol type
+func (tp transportProtocol) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + string(tp) + `"`), nil
+}
