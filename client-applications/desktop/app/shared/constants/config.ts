f2dd3ded5e9f69327a9c89702cea725dda6f11c6
Jannie <jz3191@columbia.edu>
2021-03-12 16:54:40 -0800

    add graphql connection

--- client-applications/desktop/app/shared/constants/config.ts
+++ client-applications/desktop/app/shared/constants/config.ts
@@ -4,6 +4,10 @@ import {
     FractalNodeEnvironment,
     FractalCIEnvironment,
 } from "shared/types/config"
+/*
+    Host service port
+*/
+export const HOST_SERVICE_PORT = "4678"
 
 /*
     Webserver URLs
