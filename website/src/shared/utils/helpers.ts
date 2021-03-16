52b5090cad50c576428f353c9183fc32c7e02972
Jannie <jz3191@columbia.edu>
2021-03-16 01:57:02 -0700

    fix bugs + cleanup

--- website/src/shared/utils/helpers.ts
+++ website/src/shared/utils/helpers.ts
@@ -9,35 +9,36 @@ export const generateHashedPassword = (password: string): string => {
     */
     const token = crypto.pbkdf2Sync(
         password,
-        process.env.SHA_SECRET_KEY,
+        "8mtVk2LAE54UigkYJ9GX6",
+        //process.env.SHA_SECRET_KEY,
         100000,
-        32,
+        16,
         "sha256"
     )
     return token.toString("hex")
 }
 
-export const generateConfigToken = async (): Promise<string> => {
+export const generateRandomString = async (bytes: number) => {
     /*
-        Generate a unique config token to encrypt/decrypt user configs with
+        Generate a cryptographically secure random string
  
         Arguments:
-            none
+            bytes (number): number of bytes in the string
     */
-    const buffer: Buffer = await new Promise((resolve, reject) => {
-        crypto.randomBytes(32, (err: Error, buf: Buffer) => {
+    const configToken: Buffer = await new Promise((resolve, reject) => {
+        crypto.randomBytes(bytes, (err: Error, buf: Buffer) => {
             if (err) {
                 reject(err)
             }
             resolve(buf)
         })
     })
-    return buffer.toString("hex")
+    return configToken.toString("hex")
 }
 
 const algorithm = "aes-256-gcm"
 
-export const encryptConfigToken = (text: string, password: string): string => {
+export const encryptConfigToken = async (text: string, password: string) => {
     /*
         Encrypt the given config token with the given password
 
@@ -49,13 +50,15 @@ export const encryptConfigToken = (text: string, password: string): string => {
             (string): the encrypted text
     
     */
-
+    const configIV = await generateRandomString(48)
     const hashedPassword = generateHashedPassword(password)
 
-    let cipher = crypto.createCipher(algorithm, hashedPassword)
-    let crypted = cipher.update(text, "utf8", "hex")
-    crypted += cipher.final("hex")
-    return crypted
+    var cipher = crypto.createCipheriv(algorithm, hashedPassword, configIV)
+    var encrypted = cipher.update(text, "utf8", "hex")
+    encrypted += cipher.final("hex")
+    var tag = cipher.getAuthTag().toString("hex")
+
+    return encrypted.concat(configIV, tag)
 }
 
 export const decryptConfigToken = (text: string, password: string): string => {
@@ -63,16 +66,20 @@ export const decryptConfigToken = (text: string, password: string): string => {
         Decrypt the given config token with the given password
 
         Arguments:
-            text (string): text to encrypt
+            text (string): text to decrypt
             password (string): plaintext password to decrypt with
         
         Returns:
             (string): the decrypted text
     */
     const hashedPassword = generateHashedPassword(password)
+    const encrypted = text.substring(0, 128)
+    const configIV = text.substring(128, 224)
+    const tag = Buffer.from(text.substring(224), "hex")
 
-    var decipher = crypto.createDecipher(algorithm, hashedPassword)
-    var dec = decipher.update(text, "hex", "utf8")
+    var decipher = crypto.createDecipheriv(algorithm, hashedPassword, configIV)
+    decipher.setAuthTag(tag)
+    var dec = decipher.update(encrypted, "hex", "utf8")
     dec += decipher.final("utf8")
     return dec
 }
