a58703263b4d985b68a82312449080f3f97e2f2c
djsavvy <sraghuvanshi@college.harvard.edu>
2021-03-17 06:43:35 +0000

    Start implementing ECS agent with contexts

--- ecs-host-service/ecsagent/ecsagent.go
+++ ecs-host-service/ecsagent/ecsagent.go
@@ -2,10 +2,10 @@ package ecsagent // import "github.com/fractal/fractal/ecs-host-service/ecsagent
 
 import (
 	"bufio"
-	"math/rand"
+	"context"
 	"os"
 	"strings"
-	"time"
+	"sync"
 
 	ecsapp "github.com/fractal/ecs-agent/agent/app"
 	ecslogger "github.com/fractal/ecs-agent/agent/logger"
@@ -15,29 +15,25 @@ import (
 	"github.com/cihub/seelog"
 )
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-
+// Main is the function that actually starts the ecsagent. It also contains the
+// main function from fractal/ecs-agent.
+func Main(globalCtx context.Context, globalCancel context.CancelFunc, goroutineTracker *sync.WaitGroup) {
 	// If we don't make this directory, the ecs-agent crashes.
 	err := os.MkdirAll("/var/lib/ecs/data/", 0755)
 	if err != nil {
-		fractallogger.Panicf("Could not make directory /var/lib/ecs/data: Error: %s", err)
+		fractallogger.Panicf(globalCancel, "Could not make directory /var/lib/ecs/data: Error: %s", err)
 	}
 
 	err = os.MkdirAll("/var/log/ecs/", 0755)
 	if err != nil {
-		fractallogger.Panicf("Could not make directory /var/log/ecs: Error: %s", err)
+		fractallogger.Panicf(globalCancel, "Could not make directory /var/log/ecs: Error: %s", err)
 	}
 
 	err = os.MkdirAll("/etc/ecs/", 0755)
 	if err != nil {
-		fractallogger.Panicf("Could not make directory /etc/ecs: Error: %s", err)
+		fractallogger.Panicf(globalCancel, "Could not make directory /etc/ecs: Error: %s", err)
 	}
-}
 
-// Main is the function that actually starts the ecsagent. It also contains the
-// main function from fractal/ecs-agent.
-func Main() {
 	// We want to set the relevant environment variables here so that the ECS
 	// agent is configured correctly before it actually runs.
 
@@ -45,7 +41,7 @@ func Main() {
 	ecsConfigFilePath := "/etc/ecs/ecs.config"
 	file, err := os.Open(ecsConfigFilePath)
 	if err != nil {
-		fractallogger.Panicf("Couldn't open ECS Config File %s. Error: %s", ecsConfigFilePath, err)
+		fractallogger.Panicf(globalCancel, "Couldn't open ECS Config File %s. Error: %s", ecsConfigFilePath, err)
 	}
 	defer file.Close()
 
@@ -59,13 +55,13 @@ func Main() {
 		// We split on the first `=` only
 		substrs := strings.SplitN(line, "=", 2)
 		if len(substrs) != 2 {
-			fractallogger.Panicf("Couldn't parse ECS Config file line: %s", line)
+			fractallogger.Panicf(globalCancel, "Couldn't parse ECS Config file line: %s", line)
 		}
 
 		// Actually set the environment variable
 		err := os.Setenv(substrs[0], substrs[1])
 		if err != nil {
-			fractallogger.Panicf("Couldn't set environment variable %s to value %s. Error: %s", substrs[0], substrs[1], err)
+			fractallogger.Panicf(globalCancel, "Couldn't set environment variable %s to value %s. Error: %s", substrs[0], substrs[1], err)
 		} else {
 			fractallogger.Infof("Set environment variable %s to value %s.", substrs[0], substrs[1])
 		}
@@ -75,12 +71,17 @@ func Main() {
 
 	seelogBridge, err := seelog.LoggerFromCustomReceiver(&trivialSeeLogReceiver{})
 	if err != nil {
-		fractallogger.Panicf("Error initializing seelogBridge: %s", err)
+		fractallogger.Panicf(globalCancel, "Error initializing seelogBridge: %s", err)
 	}
 
-	// Below is the original main() body from the ECS agent itself. Note that we
-	// modify `ecsapp.Run()` to take no arguments instead of reading in
-	// `os.Args[1:]`.
+	// Below is (a heavily modified version of) the original main() body from the
+	// ECS agent itself. Note that we modify `ecsapp.Run()` to take no arguments
+	// instead of reading in `os.Args[1:]`.
 	ecslogger.InitSeelog(seelogBridge)
-	os.Exit(ecsapp.Run([]string{}))
+	exitCode := ecsapp.Run([]string{})
+
+	// TODO: pass in the context and cancel functions into the actual ecsapp.Run() command
+
+	// If we got here, then that means that the ecsagent has exited for some reason.
+	fractallogger.Panicf(globalCancel, "ECS Agent exited with code %d", exitCode)
 }
