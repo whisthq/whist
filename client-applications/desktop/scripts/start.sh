0457295495dd5de86523e857111d813cb6aa96d3
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-17 18:23:49 -0500

    Update build scripts

--- client-applications/desktop/scripts/start.sh
+++ client-applications/desktop/scripts/start.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# nodemon \
-#     --watch "./src/main" \
-#     --watch "./src/utils" \
-#     --ext js,jsx,ts,tsx \
-#     --exec "yarn compile-both"
 concurrently \
     "snowpack dev" \
     "electron build/dist/main"
