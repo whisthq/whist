539fcf4f8a182c59b3ed71311b40de223aa0252f
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-18 12:11:50 -0500

    Add ipcRenderer removeListener method + doc

--- client-applications/desktop/public/preload.js
+++ client-applications/desktop/public/preload.js
@@ -1,9 +1,14 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
+// Many of the properties of ipcRenderer are non-enumerable properties
+// because they are inherited from EventEmitter. This requires them to
+// be explicity named passed in the object below.
+
 contextBridge.exposeInMainWorld(
     "ipcRenderer",
     {
         on: ipcRenderer.on,
         send: ipcRenderer.send
+        removeListener: ipcRenderer.removeListener
     }
 )
