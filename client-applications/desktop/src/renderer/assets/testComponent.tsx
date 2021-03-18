a1e128a261a494d8bf1785f47183867811c9f240
mingy98 <ming@fractal.co>
2021-03-18 15:34:50 -0400

    getting rid of test component

--- client-applications/desktop/src/renderer/assets/testComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Test = () => {
-    return <>TEST COMPONENT</>
-}
-
-export default Test
--- client-applications/desktop/src/renderer/pages/auth/shared/components/logo.tsx
+++ client-applications/desktop/src/renderer/pages/auth/shared/components/logo.tsx
@@ -1,11 +1,9 @@
 import React from "react"
 import classNames from "classnames"
 import LogoPurple from "@app/renderer/assets/logoPurple.svg"
-import Test from "@app/renderer/assets/testComponent"
 
 export const Logo = (props: { className?: string }) => (
     <div className={classNames("text-center", props.className)}>
         <img src={LogoPurple} className="w-12 h-12 m-auto" alt="Logo" />
-        <Test />
     </div>
 )
