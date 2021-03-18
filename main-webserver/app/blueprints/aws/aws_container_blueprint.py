f2075ae8404a8f2b2662317280d5defa6a02d83a
Owen Niles <owen@fractal.co>
2021-03-18 10:29:38 -0400

    Don't pass config_encryption_token to assign_container

--- main-webserver/app/blueprints/aws/aws_container_blueprint.py
+++ main-webserver/app/blueprints/aws/aws_container_blueprint.py
@@ -359,7 +359,6 @@ def aws_container_assign(**kwargs):
     response = jsonify({"status": NOT_FOUND}), NOT_FOUND
     try:
         user = body.pop("username")
-        config_encryption_token = body.get("encryptionToken", "")
         app = body.pop("app")
         region = body.pop("region")
         dpi = body.get("dpi", 96)
@@ -385,7 +384,6 @@ def aws_container_assign(**kwargs):
                 user,
                 task_arn,
                 task_version,
-                config_encryption_token,
                 region_name=region,
                 webserver_url=kwargs["webserver_url"],
                 dpi=dpi,
