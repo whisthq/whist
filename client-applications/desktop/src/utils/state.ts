1930f810bb0f4d53feeeedcaab26d4b0a0424d0a
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-18 12:11:36 -0500

    Add state error if no ipcRenderer

--- client-applications/desktop/src/utils/state.ts
+++ client-applications/desktop/src/utils/state.ts
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react"
 import { IpcRenderer, IpcRendererEvent } from "electron"
 
 export type State = any
-export type Event = (f: (state: Partial<State>) => void) => void
+export type Event = (setState: (state: Partial<State>) => void) => void
 export type Effect = (state: State) => void
 
 export const StateChannel = "MAIN_STATE_CHANNEL"
 
+const ipcError = [
+    "A ipcRenderer must be attached to the window object to commmunicate",
+    "with the main process.",
+    "You need to attach it in a Electron preload.js file using",
+    "contextBridge.exposeInMainWorld. You must explicity attach the 'on' and",
+    "'send' methods for them to be exposed.",
+].join(" ")
+
 export const useMainState = (ipcRenderer: IpcRenderer) => {
+    const ipc = window.ipcRenderer
+    if (!(ipc && ipc.on && ipc.send)) throw new Error("Y")
+
     const [mainState, setState] = useState(null)
 
     useEffect(() => {
