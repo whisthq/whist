7dc112075636673b8efcf250ab9a44b60a20edb1
Nicholas Pipitone <npip99@gmail.com>
2021-03-19 18:04:18 +0000

    Remove unneeded png.h/png.c functions

--- protocol/fractal/utils/png.h
+++ protocol/fractal/utils/png.h
@@ -31,13 +31,9 @@ Public Functions
 ============================
 */
 
-char* read_file(const char* filename, size_t* char_nb);
-
 int bmp_to_png(unsigned char* bmp, unsigned int size, AVPacket* pkt);
 
 int png_char_to_bmp(char* png, int size, AVPacket* pkt);
-
-int png_file_to_bmp(char* png, AVPacket* pkt);
 
 /**
  * @brief                          Load a PNG file to an SDL surface using lodepng.
