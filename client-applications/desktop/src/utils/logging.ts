eaaa10253cf5abe12cb8f3854ea589a656fb1cca
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-19 14:22:19 -0500

    Add new utils

---
+++ client-applications/desktop/src/utils/logging.ts
@@ -0,0 +1,9 @@
+import { Logger } from "tslog"
+
+export const log = new Logger({
+    displayLoggerName: false,
+    displayDateTime: false,
+    displayRequestId: false,
+    displayLogLevel: false,
+    displayFilePath: "hidden",
+})
