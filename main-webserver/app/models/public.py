49798f048529b6f1b473c566eeb24aff2ba9f8be
Owen Niles <owen@fractal.co>
2021-03-19 13:11:39 -0400

    Increase the size of the encrypted_config_token column to 256 bytes.

--- main-webserver/app/models/public.py
+++ main-webserver/app/models/public.py
@@ -21,7 +21,11 @@ class User(db.Model):
             when a user creates an account. It is sent in an email to the email address associated
             with the account. The email verification server endpoint compares the email
             verification token it receives to this token.
-        encrypted_config_token (String):  the user's app config token, stored encrypted
+        encrypted_config_token (String): The random string used to encrypt the Fractal user's
+            streamed app configuration data (e.g. Google Chrome configuration settings),
+            AES-256-encrypted by the website using the user's plaintext password to derive the
+            encryption key. Note that a valid encrypted app config encryption token will always be
+            a string of 256 hexadecimal digits.
         password (String): Hashed password
         stripe_customer_id (String): A pointer to a customer record on Fractal's Stripe account.
         reason_for_signup (String): How users heard about Fractal
@@ -35,7 +39,7 @@ class User(db.Model):
     user_id = db.Column(db.String(250), nullable=False, unique=True, primary_key=True)
     name = db.Column(db.String(250))
     token = db.Column(db.String(250))
-    encrypted_config_token = db.Column(db.String(250))
+    encrypted_config_token = db.Column(db.String(256), nullable=False)
     password = db.Column(db.String(250), nullable=False)
     stripe_customer_id = db.Column(db.String(250))
     reason_for_signup = db.Column(Text)
