7ad72f8ed7f3935e457ba752ca2e11738e9a5edc
Steve Li <lithafnium2000@gmail.com>
2021-03-19 01:12:55 -0400

    pwd testing

--- main-webserver/.profile.d/set_git_info.sh
+++ main-webserver/.profile.d/set_git_info.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+echo $PWD
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+echo $PWD
 echo $DIR
 echo $(ls)
 
@@ -9,7 +10,7 @@ echo "-----> fuck you leonard"
 
 
 
-exec 6< "${GIT_DIR}/git_info.txt"
+exec 6< "${DIR}/bin/git_info.txt"
 read BRANCH <&6
 read COMMIT <&6
 
