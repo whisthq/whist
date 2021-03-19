f90c456e55f4556164a696024d656ffd0b6b6fac
Steve Li <lithafnium2000@gmail.com>
2021-03-19 00:57:43 -0400

    more debuggin

--- main-webserver/.profile.d/set_git_info.sh
+++ main-webserver/.profile.d/set_git_info.sh
@@ -2,9 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+echo $DIR
 GIT_DIR="${DIR}/../../bin"
 
 echo "-----> fuck you leonard"
+
+
 
 exec 6< "${GIT_DIR}/git_info.txt"
 read BRANCH <&6
