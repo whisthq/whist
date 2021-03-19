d5e105e4808271274a7b0ff5e9177d1e5eb81dfe
Steve Li <lithafnium2000@gmail.com>
2021-03-19 00:27:58 -0400

    trying writing to a file and getting the contents in profile script

--- main-webserver/.profile.d/set_git_info.sh
+++ main-webserver/.profile.d/set_git_info.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd -
+
+cd /bin
+
 echo "-----> Configuring git branch and commit hash to enviornment variables"
-export BRANCH=$(git branch --show-current)
-export COMMIT=$(git rev-parse --short HEAD)
+
+exec 6< git_info.txt
+read BRANCH <&6
+read COMMIT <&6
+
+export BRANCH=$BRANCH
+export COMMIT=$COMMIT
 
 echo $BRANCH
 echo $COMMIT
