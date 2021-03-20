2b3835c445d1b21db1129a8607740c8ad07f04a9
Philippe Noel <phil@fractal.co>
2021-03-20 13:27:37 -0400

    prettify

--- client-applications/desktop/build/afterSign.js
+++ client-applications/desktop/build/afterSign.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-const { notarize } = require('electron-notarize')
-const fs = require('fs')
-const { exec } = require("child_process");
+require("dotenv").config()
+const { notarize } = require("electron-notarize")
+const fs = require("fs")
+const { exec } = require("child_process")
 
 exports.default = async function afterSign(context) {
     const { electronPlatformName, appOutDir } = context
-    if (electronPlatformName !== 'darwin') {
+    if (electronPlatformName !== "darwin") {
         return
     }
 
@@ -29,31 +29,37 @@ exports.default = async function afterSign(context) {
     )
 
     // codesign the two external binaries defined above, Fractal and FractalLauncher (all binaries need to be codesigned for notarizing to work)
-    exec("codesign -f -v -s 'Fractal Computers, Inc.' \"${appOutDir}/${appName}.app/Contents/MacOS/FractalLauncher\"", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+    exec(
+        "codesign -f -v -s 'Fractal Computers, Inc.' \"${appOutDir}/${appName}.app/Contents/MacOS/FractalLauncher\"",
+        (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`)
+                return
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`)
+                return
+            }
+            console.log(`stdout: ${stdout}`)
         }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
+    )
+    exec(
+        "codesign -f -v -s 'Fractal Computers, Inc.' \"${appOutDir}/${appName}.app/Contents/MacOS/Fractal\"",
+        (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`)
+                return
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`)
+                return
+            }
+            console.log(`stdout: ${stdout}`)
         }
-        console.log(`stdout: ${stdout}`);
-    });
-    exec("codesign -f -v -s 'Fractal Computers, Inc.' \"${appOutDir}/${appName}.app/Contents/MacOS/Fractal\"", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+    )
 
     return await notarize({
-        appBundleId: 'com.fractalcomputers.fractal',
+        appBundleId: "com.fractalcomputers.fractal",
         appPath: `${appOutDir}/${appName}.app`,
         appleId: `phil@fractal.co`,
         appleIdPassword: `seoy-fnou-zjro-xicr`,
