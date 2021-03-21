46fcefdbf5a9361ad5628b8b391b60940a86f01f
Alex Wendland <me@alexwendland.com>
2021-03-20 17:46:19 -0700

    feat(webserver): introduce logz-based metrics recording framework

---
+++ main-webserver/app/helpers/utils/metrics/keys_dims.py
@@ -0,0 +1,9 @@
+"""
+A central store of all dimensions keys to support easier auditability of potential
+metric dimensions being recorded.
+"""
+
+ENVIRONMENT = "environment"
+HOST_SERVER = "host_server"
+APP_GIT_COMMIT = "app_git_commit"
+APP_NAME = "app_name"
