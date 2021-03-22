1a140eb12c9f6bdf20c1ee71f97bb5b950a57312
Steve Li <lithafnium2000@gmail.com>
2021-03-22 10:40:10 -0400

    changed back to git

--- main-webserver/.profile.d/set_git_info.sh
+++ main-webserver/.profile.d/set_git_info.sh
@@ -5,12 +5,15 @@ echo "-----> Reading git branch and commit info from txt file "
 
 echo $(ls)
 
-cd bin 
+# cd bin 
 
-echo $(ls)
-exec 6< "bin/git_info.txt"
-read BRANCH <&6
-read COMMIT <&6
+# echo $(ls)
+# exec 6< "bin/git_info.txt"
+# read BRANCH <&6
+# read COMMIT <&6
+BRANCH=$(git branch --show-current)
+COMMIT=$(git rev-parse --short HEAD)
+
 
 export BRANCH=$BRANCH
 export COMMIT=$COMMIT
