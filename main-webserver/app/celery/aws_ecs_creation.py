66585acdc09f50baff898bdf6a417233e3181cf3
Steve Li <lithafnium2000@gmail.com>
2021-03-23 16:37:32 -0400

    debugging

--- main-webserver/app/celery/aws_ecs_creation.py
+++ main-webserver/app/celery/aws_ecs_creation.py
@@ -1033,8 +1033,11 @@ def _create_new_cluster(
             max_size=max_size,
             availability_zones=availability_zones,
         )
+        fractal_logger.info(f"Created cluster {cluster_name}")
 
         cluster_usage = ecs_client.get_clusters_usage(clusters=[cluster_name])[cluster_name]
+
+        fractal_logger.info(f"Cluster info: {dict(cluster_usage)}")
         cluster_usage_info = ClusterInfo(
             cluster=cluster_name, location=region_name, **cluster_usage
         )
