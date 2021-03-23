7cc29a6d0650d110e1b97b52a1f2a71d067d5ce5
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-22 20:07:46 -0500

    Remove tslog references

--- client-applications/desktop/src/utils/logging.ts
+++
@@ -1,9 +0,0 @@
-// import { Logger } from "tslog"
-
-// export const log = new Logger({
-//     displayLoggerName: false,
-//     displayDateTime: false,
-//     displayRequestId: false,
-//     displayLogLevel: false,
-//     displayFilePath: "hidden",
-// })
