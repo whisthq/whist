2772ebb210127a0202f8437104f00ecfd95ab384
FishmanL <leor@fractal.co>
2021-03-23 14:37:20 -0400

    add heartbeat handling

--- main-webserver/app/blueprints/host_service/host_service_blueprint.py
+++ main-webserver/app/blueprints/host_service/host_service_blueprint.py
@@ -8,6 +8,7 @@ from app.models import UserContainerState
 from app.constants.http_codes import SUCCESS, ACCEPTED, BAD_REQUEST, FORBIDDEN
 from app.helpers.blueprint_helpers.host_service.host_service_post import (
     initial_instance_auth_helper,
+    instance_heartbeat_helper,
 )
 
 host_service_bp = Blueprint("host_service_bp", __name__)
@@ -43,10 +44,10 @@ def host_service(**kwargs):
 @fractal_pre_process
 def host_service_auth(**kwargs):
     body = kwargs.pop("body")
-    address = kwargs.pop("received_from")
+    address: str = kwargs.pop("received_from")
 
     try:
-        instance_id = body.pop("InstanceID")
+        instance_id: str = body.pop("InstanceID")
     except KeyError:
         return jsonify({"status": BAD_REQUEST}), BAD_REQUEST
 
@@ -61,25 +62,20 @@ def host_service_heartbeat(**kwargs):
     address = kwargs.pop("received_from")
 
     try:
-        auth_token = body.pop("AuthToken")
-        instance_id = body.pop("InstanceID")
+        auth_token: str = body.pop("AuthToken")
+        instance_id: str = body.pop("InstanceID")
     except KeyError:
         return jsonify({"status": BAD_REQUEST}), BAD_REQUEST
-
-    # TODO: replace with a helper checking db for instance_id, address, auth_token
-    valid_auth = True
-    if not valid_auth:
-        return jsonify({"status": FORBIDDEN}), FORBIDDEN
 
     try:
         timestamp = body.pop("Timestamp")
         heartbeat_number = body.pop("HeartbeatNumber")
-        instance_type = body.pop("InstanceType")
+        instance_type: str = body.pop("InstanceType")
         total_ram_kb = body.pop("TotalRAMinKB")
-        free_ram_kb = body.pop("FreeRAMinKB")
+        free_ram_kb: int = body.pop("FreeRAMinKB")
         available_ram_kb = body.pop("AvailRAMinKB")
         dying_heartbeat = body.pop("IsDyingHeartbeat")
     except KeyError:
         return jsonify({"status": BAD_REQUEST}), BAD_REQUEST
 
-    return jsonify({"status": ACCEPTED}), ACCEPTED
+    return instance_heartbeat_helper(auth_token, instance_id, free_ram_kb, instance_type)
