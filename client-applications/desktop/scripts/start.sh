392a3357cfd75b26fd60d46c381487fd84856247
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-25 13:59:15 -0500

    better start script

--- client-applications/desktop/scripts/start.sh
+++ client-applications/desktop/scripts/start.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+if [[ -f "protocol-build/client/Fractal" ]]; then
+    echo "Protocol built, starting..."
+else
+    echo "Protocol not built! Installing it first..."
+    sh ./scripts/publish.sh
+fi
 
 snowpack dev
