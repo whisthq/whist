7c6a3ea43b614c0b4a972bccc528f0f72f9e42b3
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-25 12:54:14 -0500

    Only persist state if there's a value with key

--- client-applications/desktop/src/utils/persist.ts
+++ client-applications/desktop/src/utils/persist.ts
@@ -2,10 +2,13 @@ import Store from "electron-store"
 
 export const store = new Store({ watch: true })
 
-export const persistKeys = <T>(obj: T, ...keys: Array<keyof T>) => {
-    for (let key of keys)
-        if (obj[key] !== store.get(key as string))
+export const persistKeys = <T>(obj: T, ...keys: Array<keyof T & string>) => {
+    for (let key of keys) {
+        if (obj[key] && obj[key] !== store.get(key)) {
             store.set(key as string, obj[key])
+            // store.delete(key)
+        }
+    }
 }
 
 export const persistClear = () => {
