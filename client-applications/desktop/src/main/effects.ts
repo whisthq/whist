21a9cb381614e44750cd754f51f77e0a534c482c
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-25 12:53:30 -0500

    Add app config crypto functions

--- client-applications/desktop/src/main/effects.ts
+++ client-applications/desktop/src/main/effects.ts
@@ -1,22 +1,44 @@
 import { webContents, BrowserWindow } from "electron"
+import { emailLogin } from "@app/utils/api"
+import { decryptConfigToken } from "@app/utils/crypto"
 import { createAuthWindow } from "@app/utils/windows"
+import { fractalLoginWarning } from "@app/utils/constants"
 import { Effect, StateChannel, isLoggedIn } from "@app/utils/state"
 import { persistKeys } from "@app/utils/persist"
 import * as proto from "@app/utils/protocol"
 
+export const handleLogin: Effect = async function* (state) {
+    if (!state.loginRequest) return
+    if (state.loginLoading) return
+    yield { loginLoading: true, loginWarning: "" }
+    const { json } = await emailLogin(state.email, state.password)
+    const loginWarning =
+        json && json.access_token ? "" : fractalLoginWarning.INVALID
+    yield { loginLoading: false, loginWarning }
+
+    return {
+        password: "",
+        loginRequest: false,
+        accessToken: json.access_token || "",
+        refreshToken: json.refresh_token || "",
+        configToken: json.encrypted_config_token
+            ? decryptConfigToken(state.password, json.encrypted_config_token)
+            : "",
+    }
+}
+
 export const launchAuthWindow: Effect = function* (state) {
-    if (isLoggedIn()) return state
-    if (!state.appWindowRequested) return state
+    if (!state.appWindowRequest) return
+    if (isLoggedIn(state)) return { appWindowRequest: false }
     if (BrowserWindow.getAllWindows().length === 0) {
         createAuthWindow()
     }
-    return { ...state, appWindowRequested: false }
+    return { appWindowRequest: false }
 }
 
 export const closeAllWindows: Effect = function* (state) {
-    if (!isLoggedIn()) return state
+    if (!isLoggedIn(state)) return
     for (let win of BrowserWindow.getAllWindows()) win.close()
-    return state
 }
 
 export const broadcastState: Effect = function* (state) {
@@ -26,24 +48,27 @@ export const broadcastState: Effect = function* (state) {
             ? c.on("did-finish-load", () => c.send(StateChannel, state))
             : c.send(StateChannel, state)
     })
-    return state
 }
 
 export const launchProtocol: Effect = async function* (state) {
-    if (!isLoggedIn()) return state
-    yield { ...state, protocolLoading: true }
+    if (!isLoggedIn(state)) return
+    if (state.protocolLoading) return
+    if (state.protocolProcess) return
+    yield { protocolLoading: true }
     const taskID = await proto.createContainer(state.email, state.accessToken)
     const info = await proto.waitUntilReady(taskID, state.accessToken)
-    yield { ...state, protocolLoading: false }
-    return { ...state, protocolProcess: proto.launchProtocol(info.output) }
+    return {
+        protocolLoading: false,
+        protocolProcess: proto.launchProtocol(info.output),
+    }
 }
 
 export const persistState: Effect = function* (state) {
     persistKeys(state, "email", "accessToken")
-    return state
 }
 
+export const handleConfigToken: Effect = function* (state) {}
+
 export const logState: Effect = function* (state) {
-    console.log(state)
-    return state
+    console.log("STATE", state)
 }
