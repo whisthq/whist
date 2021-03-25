f0b6fe5bba88c1cab488fe96d66a35913445792e
djsavvy <sraghuvanshi@college.harvard.edu>
2021-03-25 15:13:13 +0000

    Get run_container_image.sh working again

--- container-images/helper-scripts/run_container_image.sh
+++ container-images/helper-scripts/run_container_image.sh
@@ -35,38 +35,74 @@ dpi=${FRACTAL_DPI:-96}
 user_id=${FRACTAL_USER_ID:-''}
 
 
+# Helper function to kill a locally running Docker container
+# Args: container_id
+kill_container() {
+    docker kill $1 > /dev/null || true
+    docker rm $1 > /dev/null || true
+}
+
+# Helper function to print error message and then kill a locally running Docker container
+# Args: container_id, error_message
+print_error_and_kill_container() {
+    echo "$2" && kill_container $1 && exit 1
+}
+
+# Check if host service is even running
+# Args: none
+check_if_host_service_running() {
+    sudo lsof -i :4678 | grep ecs-host > /dev/null \
+        || (echo "Cannot start container because the ecs-host-service is not listening on port 4678. Is it running successfully?" && exit 1)
+}
+
 # Send a start values request to the Fractal ECS host service HTTP server running on localhost
 # This is necessary for the Fractal server protocol to think that it is ready to start. In production,
 # the webserver would send this request to the Fractal host service, but for local development we need
 # to send it manually until our development pipeline is fully built
-# Args: DPI, user_id
+# Args: container_id, DPI, user_id
 send_start_values_request() {
+    echo "Sending DPI/container-ready request to container $1!"
     # Send the DPI/container-ready request
     response=$(curl --insecure --silent --location --request PUT 'https://localhost:4678/set_container_start_values' \
             --header 'Content-Type: application/json' \
             --data-raw '{
       "auth_secret": "testwebserverauthsecretdev",
       "host_port": 32262,
-      "dpi": '"$1"',
-      "user_id": "'"${2:-}"'"
+      "dpi": '"$2"',
+      "user_id": "'"${3:-}"'"
     }') \
-        || (echo "DPI/container-ready request to the host service failed!" && killall ecs-host-service && exit 1)
-    echo "Sent DPI/container-ready request!"
+        || (print_error_and_kill_container $1 "DPI/container-ready request to the host service failed!")
     echo "Response to DPI/container-ready request from host service: $response"
+}
+
+# Send a spin_up_container request to the host service (which must be running with `make run`).
+# Args: app_name, app_image, mount_command. Modifies container_id
+send_spin_up_container_request() {
+    # Send the request
+    echo "Sending spin up request to host service!"
+    response=$(curl --insecure --silent --location --request PUT 'https://localhost:4678/spin_up_container' \
+            --header 'Content-Type: application/json' \
+            --data-raw '{
+      "auth_secret": "testwebserverauthsecretdev",
+      "app_name": "'"$1"'",
+      "app_image": "'"$2"'",
+      "mount_command": "'"$3"'"
+    }') \
+        || (echo "spin up request to the host service failed!" && exit 1)
+    echo "Response to spin up request from host service: $response"
+    container_id=$(jq -r '.result' <<< "$response")
+    echo "Container ID: $container_id"
 }
 
 
 # Main executing thread
-
-# Wait some time for host service to start, then send start values request
-(sleep 3 && send_start_values_request $dpi $user_id) &
-
-# Start host service with appropriate environment variables
-(cd ../ecs-host-service && APP_NAME="$app_name" APP_IMAGE="$image" MOUNT_COMMAND="$mount_protocol" PRINT_DOCKER_CREATE_COMMAND="true" make run > .host_service_output)
-
+container_id=""
+check_if_host_service_running
+send_spin_up_container_request "$app_name" "$image" "$mount_protocol"
+send_start_values_request $container_id $dpi $user_id
 
 # Run bash inside the Docker container
-# docker exec -it $container_id /bin/bash || true
+docker exec -it $container_id /bin/bash || true
 
-# Kill the container and host service we exit the run bash
-# killall ecs-host-service
+# Kill the container once we're done
+kill_container $container_id
