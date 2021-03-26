fb8f0798a2f96da6d00c43993fc2801498943f09
djsavvy <sraghuvanshi@college.harvard.edu>
2021-03-26 17:58:52 +0000

    Move fractallogger to ecsagent as well

--- ecs-host-service/ecsagent/seelog_bridge.go
+++ ecs-host-service/ecsagent/seelog_bridge.go
@@ -1,7 +1,7 @@
 package ecsagent // import "github.com/fractal/fractal/ecs-host-service/ecsagent"
 
 import (
-	fractallogger "github.com/fractal/fractal/ecs-host-service/fractallogger"
+	fractallogger "github.com/fractal/ecs-agent/agent/fractallogger"
 
 	"github.com/cihub/seelog"
 )
