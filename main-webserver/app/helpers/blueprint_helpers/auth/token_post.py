b1bcf87dc64e9368257cb3bdf6576af9e04515ca
FishmanL <leor@fractal.co>
2021-03-30 13:57:46 -0400

    more types

--- main-webserver/app/helpers/blueprint_helpers/auth/token_post.py
+++ main-webserver/app/helpers/blueprint_helpers/auth/token_post.py
@@ -1,9 +1,16 @@
+from typing import Dict, Union, Optional
+
 from flask_jwt_extended import get_jwt_identity
 
 from app.helpers.blueprint_helpers.auth.account_get import fetch_user_helper
 
 
-def validate_token_helper():
+def validate_token_helper() -> Dict[str, Union[Optional[str], int]]:
+    """
+    Confirms the bearer token sent by the client app and returns the matching user object
+    Returns: a json containing user information
+
+    """
     current_user = get_jwt_identity()
     output = fetch_user_helper(current_user)
     return output
