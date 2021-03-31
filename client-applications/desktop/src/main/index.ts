a7696d195a3927431f915da134568576bdfeab54
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-31 15:54:58 -0500

    Move broadcast effect to index.js

--- client-applications/desktop/src/main/index.ts
+++ client-applications/desktop/src/main/index.ts
@@ -1,2 +1,23 @@
-import "@app/main/controller"
+// import "@app/main/controller"
+// import "@app/main/debug"
+import "@app/main/container"
 import "@app/main/debug"
+import "@app/main/protocol"
+import "@app/main/user"
+
+import { ipcState } from "@app/main/events"
+import { loginLoading, loginWarning } from "@app/main/login"
+
+import { ipcBroadcast } from "@app/utils/state"
+import { getWindows } from "@app/utils/windows"
+import { combineLatest } from "rxjs"
+import { startWith } from "rxjs/operators"
+
+// Broadcast state to all renderer windows.
+combineLatest(
+    ipcState,
+    loginLoading.pipe(startWith(false)),
+    loginWarning.pipe(startWith(null))
+).subscribe(([state, loginLoading, loginWarning]) =>
+    ipcBroadcast({ ...state, loginLoading, loginWarning }, getWindows())
+)
