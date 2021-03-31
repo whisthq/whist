5cfd129c44568e92c41d41da52d2e84263bb5461
Steve Li <lithafnium2000@gmail.com>
2021-03-31 15:57:34 -0400

    heroku test run commit version

--- main-webserver/tests/aws/test_aws_container.py
+++ main-webserver/tests/aws/test_aws_container.py
@@ -33,7 +33,7 @@ from app.celery.aws_ecs_modification import manual_scale_cluster
 
 # print(os.environ)
 branch = os.environ["HEROKU_TEST_RUN_BRANCH"].replace("/", "-")
-commit = os.environ["HEROKU_TEST_RUN_COMMIT"][0:7]
+commit = os.environ["HEROKU_TEST_RUN_COMMIT_VERSION"][0:7]
 pytest.cluster_name = f"test-cluster-{branch}-{commit}-{uuid.uuid4()}"
 pytest.container_name = None
 
