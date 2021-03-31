a668fd51b8ee9d6e34bda9215e3c8629c376473d
Neil Hansen <neil.hansen.31@gmail.com>
2021-03-31 15:54:31 -0500

    Move rxjs functions to seperate files

---
+++ client-applications/desktop/src/main/protocol.ts
@@ -0,0 +1,49 @@
+import { spawn, ChildProcess } from "child_process"
+import {
+    protocolLaunch,
+    protocolStreamInfo,
+    protocolStreamKill,
+} from "@app/utils/protocol"
+import {
+    containerInfoIP,
+    containerInfoPorts,
+    containerInfoSecretKey,
+} from "@app/utils/container"
+import {
+    containerAssignRequest,
+    containerAssignSuccess,
+    containerAssignFailure,
+} from "@app/main/container"
+import { merge, zip, of, from } from "rxjs"
+import { mapTo, map, share, exhaustMap, switchMap } from "rxjs/operators"
+
+export const protocolLaunchRequest = containerAssignRequest.pipe(
+    exhaustMap(() => of(protocolLaunch())),
+    share()
+)
+export const protocolLaunchLoading = protocolLaunchRequest.pipe(
+    exhaustMap(() => merge(of(true), containerAssignSuccess.pipe(mapTo(false))))
+)
+
+export const protocolLaunchSuccess = containerAssignSuccess.pipe(
+    map((res) => ({
+        ip: containerInfoIP(res),
+        secret_key: containerInfoSecretKey(res),
+        ports: containerInfoPorts(res),
+    }))
+)
+
+export const protocolLaunchFailure = containerAssignFailure
+
+// Stream the ip, secret_key, and ports info to the protocol when we
+// when we receive a successful container status response.
+zip(
+    protocolLaunchRequest,
+    protocolLaunchSuccess
+).subscribe(([protocol, info]) => protocolStreamInfo(protocol, info))
+
+// If we have an error, close the protocol.
+zip(
+    protocolLaunchRequest,
+    protocolLaunchFailure
+).subscribe(([protocol, _error]) => protocolStreamKill(protocol))
