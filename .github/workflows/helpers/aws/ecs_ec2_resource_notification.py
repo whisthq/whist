c51474e551ee43d5e25825df17461534c22d380e
Steve Li <lithafnium2000@gmail.com>
2021-04-01 14:24:47 -0400

    only check overdue to debug us-east-1

--- .github/workflows/helpers/aws/ecs_ec2_resource_notification.py
+++ .github/workflows/helpers/aws/ecs_ec2_resource_notification.py
@@ -97,12 +97,12 @@ def flag_instances(region):
             overdue, days = compare_days(launch_time)
             if overdue:
                 message += f"     - \\`{name}\\` - id: \\`{instance_id}\\` - UPTIME: {days} days \n"
-            elif test:
-                if compare_hours(launch_time):
-                    message += f"     - \\`{name}\\` - id: \\`{instance_id}\\` - TEST INSTANCE OVERDUE \n"
+            # elif test:
+            #     if compare_hours(launch_time):
+            #         message += f"     - \\`{name}\\` - id: \\`{instance_id}\\` - TEST INSTANCE OVERDUE \n"
 
-            elif len(name) == 0:
-                message += f"     - \\'{instance_id}\\' - UNTAGGED/UNNAMED \n"
+            # elif len(name) == 0:
+            #     message += f"     - \\'{instance_id}\\' - UNTAGGED/UNNAMED \n"
 
     return message
 
