b01bad004eeaf77e0f146f701482c42ca9f10848
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-02 11:14:43 -0500

    Clean up persist utils file

--- client-applications/desktop/src/utils/persist.ts
+++ client-applications/desktop/src/utils/persist.ts
@@ -11,20 +11,11 @@ export const persist = (obj: any) => {
 
 export const persistKeys = <T>(obj: T, ...keys: Array<keyof T & string>) => {
     for (let key of keys) {
-        if (obj[key] && obj[key] !== store.get(key)) {
+        if (obj[key] && obj[key] !== store.get(key))
             store.set(key as string, obj[key])
-            // store.delete(key)
-        }
     }
 }
 
 export const persistClear = () => {
     store.clear()
 }
-
-export const persistLoad = () => ({
-    ...store.store,
-    email: store.store["email"] as string,
-    accessToken: store.store["accessToken"] as string,
-    configToken: store.store["configToken"] as string,
-})
