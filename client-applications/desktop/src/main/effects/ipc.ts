79e15499906fdaf4b3251a75edc4792354abdf77
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-03 09:31:19 -0600

    Add subscribe map + docs to effects/ipc.ts

--- client-applications/desktop/src/main/effects/ipc.ts
+++ client-applications/desktop/src/main/effects/ipc.ts
@@ -4,18 +4,35 @@
  * @brief This file contains subscriptions to Observables related to state persistence.
  */
 import { eventIPC } from "@app/main/events/ipc"
+import { mapValues } from "lodash"
 import { ipcBroadcast } from "@app/utils/state"
 import { combineLatest } from "rxjs"
-import { startWith, mapTo } from "rxjs/operators"
+import { startWith, mapTo, withLatestFrom } from "rxjs/operators"
 import { WarningLoginInvalid } from "@app/utils/constants"
 import { getWindows } from "@app/utils/windows"
 import { loginLoading, loginWarning } from "@app/main/observables/login"
 
-// Broadcast state to all renderer windows.
+// This file is responsible for broadcasting state to all renderer windows.
+// We use a single object and IPC for all windows, so here we set up a single
+// observable subscription that calls ipcBroadcast whenever it emits.
+//
+// ipcBroadcast takes the latest object sent back from the renderer thread,
+// and merges it with the latest data from the observables in the "subscribed"
+// map below.
+//
+// Note that combineLatest doesn't emit until each of its observable arguments
+// emits an initial value. To get the state broacasting right away, we pipe
+// all the subscribed observables through startWith(undefined).
+
+const subscribed = {
+    loginLoading: loginLoading,
+    loginWarning: loginWarning.pipe(mapTo(WarningLoginInvalid)),
+}
+
 combineLatest(
-    eventIPC,
-    loginLoading.pipe(startWith(false)),
-    loginWarning.pipe(mapTo(WarningLoginInvalid), startWith(null))
-).subscribe(([state, loginLoading, loginWarning]) =>
-    ipcBroadcast({ ...state, loginLoading, loginWarning }, getWindows())
+    mapValues(subscribed, (o: any): any => o.pipe(startWith(undefined)))
 )
+    .pipe(withLatestFrom(eventIPC))
+    .subscribe(([subs, state]) =>
+        ipcBroadcast({ ...state, ...subs }, getWindows())
+    )
