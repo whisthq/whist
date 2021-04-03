80d37e1f848c021de1949a2befe6b01448e722fd
Mia Sturzu <sturzu@college.harvard.edu>
2021-04-03 11:26:59 -0400

    Extend Heroku CI Testing to Include Local Deploy (#1419)
    * run local deploy in heroku CI
    
    * final commit
    
    * pretty
    
    * exit code
    
    * first pass
    
    * added branch trigger
    
    * more test
    
    * changed directory
    
    * path fix?
    
    * seems to work
    
    * minor
    
    * forgot import
    
    * removed import
    
    * better logging:
    
    * maybe this?
    
    * maybe sleep?
    
    Co-authored-by: jmather625 <jatinm2@illinois.edu>

--- main-webserver/app/celery/dummy.py
+++ main-webserver/app/celery/dummy.py
@@ -2,41 +2,17 @@ import time
 
 from celery import shared_task
 
-from app.constants.http_codes import SUCCESS
 from app.helpers.utils.general.logs import fractal_logger
 
 
 @shared_task(bind=True)
 def dummy_task(self):
-    self.update_state(
-        state="STARTED",
-        meta={
-            "msg": "Running performance tests. This could take a few extra minutes.",
-            "progress": 10,
-        },
-    )
+    """
+    A dummy task deployed with our webserver. Useful for scripted testing and manually making
+    sure celery is working. Ex: test_celery_sigterm, .github/workflows/main-webserver-check-pr.yml
+    """
+    self.update_state(state="STARTED")
     fractal_logger.info("Running dummy_task...")
 
+    # beware of removing this. test_celery_sigterm uses it and the sleep is critical to the test.
     time.sleep(10)
-
-    self.update_state(
-        state="STARTED",
-        meta={
-            "msg": "Spinning the wheels.",
-            "progress": 40,
-        },
-    )
-
-    time.sleep(15)
-
-    self.update_state(
-        state="STARTED",
-        meta={
-            "msg": "Greasing the gears.",
-            "progress": 70,
-        },
-    )
-
-    time.sleep(2)
-
-    return {"status": SUCCESS}
