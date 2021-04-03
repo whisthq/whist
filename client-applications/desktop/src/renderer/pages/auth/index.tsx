6acf3df4463e0d4e756056b236f6dc687e602f97
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-03 09:04:07 -0600

    Remove development credentials ...again

--- client-applications/desktop/src/renderer/pages/auth/index.tsx
+++ client-applications/desktop/src/renderer/pages/auth/index.tsx
@@ -12,9 +12,9 @@ const Auth = () => {
     */
 
     const [mainState, setMainState] = useMainState()
-    const [email, setEmail] = useState("fakeemail@gmail.com")
-    const [password, setPassword] = useState("Password1234")
-    const [confirmPassword, setConfirmPassword] = useState("Password1234")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
 
 
     const clearPassword = () => {
