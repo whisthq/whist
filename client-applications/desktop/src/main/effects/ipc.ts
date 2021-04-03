14a4dfb587a425f224fc54259ba57e51aeae4cad
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-03 09:52:37 -0600

    Fix up effects/ipc documentation

--- client-applications/desktop/src/main/effects/ipc.ts
+++ client-applications/desktop/src/main/effects/ipc.ts
@@ -13,15 +13,15 @@ import { getWindows } from "@app/utils/windows"
 import { loginLoading, loginWarning } from "@app/main/observables/login"
 
 // This file is responsible for broadcasting state to all renderer windows.
-// We use a single object and IPC for all windows, so here we set up a single
-// observable subscription that calls ipcBroadcast whenever it emits.
+// We use a single object and IPC channel for all windows, so here we set up a
+// single observable subscription that calls ipcBroadcast whenever it emits.
 //
-// ipcBroadcast takes the latest object sent back from the renderer thread,
+// This subscription takes the latest object sent back from the renderer thread,
 // and merges it with the latest data from the observables in the "subscribed"
-// map below.
+// map below. It sends the result to all windows with ipcBroadcast.
 //
 // Note that combineLatest doesn't emit until each of its observable arguments
-// emits an initial value. To get the state broacasting right away, we pipe
+// emits an initial value. To get the state broadcasting right away, we pipe
 // all the subscribed observables through startWith(undefined).
 //
 // We can only send serializable values over IPC, so the subscribed map is
