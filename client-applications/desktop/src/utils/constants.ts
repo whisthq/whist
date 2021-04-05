5406fec76e0548e71e54644ab0110eb0cc3dc4c3
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-05 03:20:21 -0500

    Add HostService constant

--- client-applications/desktop/src/utils/constants.ts
+++ client-applications/desktop/src/utils/constants.ts
@@ -1,13 +1,4 @@
-// export const fractalLoginWarning = {
-//     NONE: "",
-//     INVALID: "Invalid email or password",
-// }
-
-// export const fractalSignupWarning = {
-//     NONE: "",
-//     ACCOUNT_EXISTS: "Email already registered. Please log in instead.",
-// }
-//
+export const HostServicePort = 4678
 
 export const WarningSignupInvalid =
     "Email already registered, please log in instead"
