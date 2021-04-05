79087a2b920af66b9793e476b46161c3f712d6e7
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-04 22:37:38 -0500

    Resubscribe persist functions to user observables

--- client-applications/desktop/src/main/observables/user.ts
+++ client-applications/desktop/src/main/observables/user.ts
@@ -1,16 +1,3 @@
-// This file is home to observables of user data. This data should be sourced
-// directly from persisted storage, which is the "beginning" of the reactive
-// data flow in the application.
-//
-// It may be tempting to pull user data from other observables in the
-// application, such as getting the userAccessToken directly from
-// loginSuccess, but that can create a circular dependency.
-//
-// Instead, it's expected that data returned from observables like loginSuccess
-// or signupSuccess will go through the "persistence cycle" and be first saved
-// to local storage. The observables in this file then "listen" to local storage
-// to receive the updated data.
-
 import { fromEventIPC } from "@app/main/events/ipc"
 import { loginSuccess } from "@app/main/observables/login"
 import { signupRequest, signupSuccess } from "@app/main/observables/signup"
