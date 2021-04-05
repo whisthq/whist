2c28975b1dccfc6975bf4256191f749e7c1c8987
FishmanL <leor@fractal.co>
2021-04-05 11:14:01 -0400

    quickfix for polling

--- main-webserver/app/celery/aws_ecs_creation.py
+++ main-webserver/app/celery/aws_ecs_creation.py
@@ -46,7 +46,7 @@ from app.exceptions import StartValueException
 
 
 MAX_MOUNT_CLOUD_STORAGE_AND_PASS_START_VALUES_RETRIES = 3
-MAX_POLL_ITERATIONS = 20
+MAX_POLL_ITERATIONS = 45
 user_container_schema = UserContainerSchema()
 user_cluster_schema = ClusterInfoSchema()
 
