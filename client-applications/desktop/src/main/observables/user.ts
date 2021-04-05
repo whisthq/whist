fc104087e005ef364f9d520c13b901ac5530584a
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-04 20:33:02 -0500

    Pull user email+tokens direct from signup/login

--- client-applications/desktop/src/main/observables/user.ts
+++ client-applications/desktop/src/main/observables/user.ts
@@ -11,15 +11,38 @@
 // to local storage. The observables in this file then "listen" to local storage
 // to receive the updated data.
 
-import { fromEventPersist } from "@app/main/events/persist"
-import { map } from "rxjs/operators"
+import { fromEventIPC } from "@app/main/events/ipc"
+import { loginSuccess } from "@app/main/observables/login"
+import { signupRequest, signupSuccess } from "@app/main/observables/signup"
+import { merge, from } from "rxjs"
+import { map, switchMap, withLatestFrom, sample } from "rxjs/operators"
+import {
+    emailLoginConfigToken,
+    emailLoginAccessToken,
+    emailLoginRefreshToken,
+    emailSignupAccessToken,
+    emailSignupRefreshToken,
+} from "@app/utils/api"
 
-export const userEmail = fromEventPersist("userEmail").pipe(
-    map((s) => s as string)
+export const userEmail = merge(
+    loginSuccess.pipe(sample(fromEventIPC("loginRequest", "email"))),
+    signupSuccess.pipe(sample(fromEventIPC("signupRequest", "email")))
 )
-export const userConfigToken = fromEventPersist("userConfigToken").pipe(
-    map((s) => s as string)
+
+export const userConfigToken = merge(
+    loginSuccess.pipe(
+        withLatestFrom(fromEventIPC("loginRequest", "password")),
+        switchMap((args) => from(emailLoginConfigToken(...args)))
+    ),
+    signupRequest.pipe(map(([_email, _password, token]) => token))
 )
-export const userAccessToken = fromEventPersist("userAccessToken").pipe(
-    map((s) => s as string)
+
+export const userAccessToken = merge(
+    loginSuccess.pipe(map(emailLoginAccessToken)),
+    signupSuccess.pipe(map(emailSignupAccessToken))
+)
+
+export const userRefreshToken = merge(
+    loginSuccess.pipe(map(emailLoginRefreshToken)),
+    signupSuccess.pipe(map(emailSignupRefreshToken))
 )
