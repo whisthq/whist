c60286252bf388d53aa9a8a27133b9ba4809dd29
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-04 22:37:38 -0500

    Resubscribe persist functions to user observables

--- client-applications/desktop/src/main/effects/persist.ts
+++ client-applications/desktop/src/main/effects/persist.ts
@@ -4,19 +4,16 @@
  * @brief This file contains subscriptions to Observables related to state persistence.
  */
 
-import { mapValues, identity } from "lodash"
+import { mapValues } from "lodash"
 import { persist } from "@app/utils/persist"
-import { eventIPC } from "@app/main/events/ipc"
-import { zip, merge, combineLatest } from "rxjs"
-import { startWith, map, pluck, filter, switchMap } from "rxjs/operators"
-import { createConfigToken } from "@app/utils/crypto"
+import { combineLatest } from "rxjs"
+import { startWith } from "rxjs/operators"
 import {
-    emailLoginConfigToken,
-    emailLoginAccessToken,
-    emailSignupAccessToken,
-} from "@app/utils/api"
-import { loginSuccess } from "@app/main/observables/login"
-import { signupSuccess } from "@app/main/observables/signup"
+    userEmail,
+    userConfigToken,
+    userAccessToken,
+    userRefreshToken,
+} from "@app/main/observables/user"
 
 // Persistence
 // We create observables for each piece of state we want to persist.
@@ -24,32 +21,14 @@ import { signupSuccess } from "@app/main/observables/signup"
 // state updates.
 //
 // They are combined into a dictionary, which is persisted to local storage.
-const persistUserEmail = eventIPC.pipe(pluck("signupRequest", "email"))
 
-const persistUserAccessToken = merge(
-    loginSuccess.pipe(map(emailLoginAccessToken)),
-    signupSuccess.pipe(map(emailSignupAccessToken))
-)
+const subscribed = {
+    userEmail,
+    userConfigToken,
+    userAccessToken,
+    userRefreshToken,
+}
 
-const persistUserConfigToken = merge(
-    eventIPC.pipe(
-        filter((req: any) => req.signupRequest),
-        map(() => createConfigToken())
-    ),
-    zip(
-        eventIPC.pipe(pluck("signupRequest", "password"), filter(identity))
-    ).pipe(switchMap((args) => emailLoginConfigToken(...args)))
-)
-
-const persistState = combineLatest(
-    mapValues(
-        {
-            userEmail: persistUserEmail,
-            userAccessToken: persistUserAccessToken,
-            userConfigToken: persistUserConfigToken,
-        },
-        (o: any): any => o.pipe(startWith(undefined))
-    )
-)
-
-persistState.subscribe((state) => persist(state))
+combineLatest(
+    mapValues(subscribed, (o: any): any => o.pipe(startWith(undefined)))
+).subscribe((state) => persist(state))
