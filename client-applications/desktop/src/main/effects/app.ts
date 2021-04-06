3b7ddc7aa107d90fa9ef0b36b7abd0f3df02bcd4
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-06 12:11:41 -0500

    Fix window opening bug

--- client-applications/desktop/src/main/effects/app.ts
+++ client-applications/desktop/src/main/effects/app.ts
@@ -8,8 +8,9 @@ import { app } from "electron"
 import { autoUpdater } from "electron-updater"
 import { eventUpdateDownloaded } from "@app/main/events/autoupdate"
 
-import { eventAppReady, eventAppQuit } from "@app/main/events/app"
+import { eventAppReady, eventWindowsAllClosed } from "@app/main/events/app"
 import { merge, race } from "rxjs"
+import { takeUntil } from "rxjs/operators"
 import {
     closeWindows,
     createAuthWindow,
@@ -17,6 +18,7 @@ import {
 } from "@app/utils/windows"
 import { loginSuccess } from "@app/main/observables/login"
 import { signupSuccess } from "@app/main/observables/signup"
+import { errorWindowRequest } from "@app/main/observables/error"
 import {
     autoUpdateAvailable,
     autoUpdateNotAvailable,
@@ -26,13 +28,18 @@ import {
 // appReady only fires once, at the launch of the application.
 eventAppReady.subscribe(() => createAuthWindow((win: any) => win.show()))
 
+// Application closing
+eventWindowsAllClosed
+    .pipe(takeUntil(merge(loginSuccess, signupSuccess, errorWindowRequest)))
+    .subscribe(() => app.quit())
+
 // Window closing
 // If we have have successfully authorized, close the existing windows.
+// It's important to put this effect after the application closing effect.
+// If not, the filters on the application closing observable don't run.
+// This causes the app to close on every loginSuccess, before the protocol
+// can launch.
 merge(loginSuccess, signupSuccess).subscribe(() => closeWindows())
-
-// Application closing
-eventAppReady.subscribe(() => app.quit())
-eventAppQuit.subscribe(() => app.quit())
 
 // If the update is downloaded, quit the app and install the update
 eventUpdateDownloaded.subscribe(() => autoUpdater.quitAndInstall())
