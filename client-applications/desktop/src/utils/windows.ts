67f2e2136334ff0890b2f866dbebf9e69e18db21
mingy98 <ming@fractal.co>
2021-04-07 15:08:53 -0400

    fixing lint errors wip

--- client-applications/desktop/src/utils/windows.ts
+++ client-applications/desktop/src/utils/windows.ts
@@ -18,21 +18,25 @@ const base = {
   titleBarStyle: 'hidden'
 }
 
-const wXs = { width: 16 * 24 }
-const wSm = { width: 16 * 32 }
-const wMd = { width: 16 * 40 }
-const wLg = { width: 16 * 56 }
-const wXl = { width: 16 * 64 }
-const w2Xl = { width: 16 * 80 }
-const w3Xl = { width: 16 * 96 }
+const screenWidth = {
+  wXs: { width: 16 * 24 },
+  wSm: { width: 16 * 32 },
+  wMd: { width: 16 * 40 },
+  wLg: { width: 16 * 56 },
+  wXl: { width: 16 * 64 },
+  w2Xl: { width: 16 * 80 },
+  w3Xl: { width: 16 * 96 }
+}
 
-const hXs = { height: 16 * 20 }
-const hSm = { height: 16 * 32 }
-const hMd = { height: 16 * 40 }
-const hLg = { height: 16 * 56 }
-const hXl = { height: 16 * 64 }
-const h2Xl = { height: 16 * 80 }
-const h3Xl = { height: 16 * 96 }
+const screenHeight = {
+  hXs: { height: 16 * 20 },
+  hSm: { height: 16 * 32 },
+  hMd: { height: 16 * 40 },
+  hLg: { height: 16 * 56 },
+  hXl: { height: 16 * 64 },
+  h2Xl: { height: 16 * 80 },
+  h3Xl: { height: 16 * 96 }
+}
 
 type CreateWindowFunction = (
   onReady?: (win: BrowserWindow) => any,
@@ -63,9 +67,9 @@ export const createWindow = (
   }
 
   win.webContents.on('did-finish-load', () =>
-    (onReady != null) ? onReady(win) : win.show()
+    onReady != null ? onReady(win) : win.show()
   )
-  win.on('close', () => (onClose != null) && onClose(win))
+  win.on('close', () => onClose != null && onClose(win))
 
   return win
 }
@@ -73,34 +77,34 @@ export const createWindow = (
 export const createAuthWindow: CreateWindowFunction = () =>
   createWindow(WindowHashAuth, {
     ...base,
-    ...wSm,
-    ...hMd
+    ...screenWidth.wSm,
+    ...screenHeight.hMd
   } as BrowserWindowConstructorOptions)
 
 export const createAuthErrorWindow: CreateWindowFunction = () =>
   createWindow(WindowHashAuthError, {
     ...base,
-    ...wMd,
-    ...hXs
+    ...screenWidth.wMd,
+    ...screenHeight.hXs
   } as BrowserWindowConstructorOptions)
 
 export const createContainerErrorWindow: CreateWindowFunction = () =>
   createWindow(WindowHashContainerError, {
     ...base,
-    ...wMd,
-    ...hXs
+    ...screenWidth.wMd,
+    ...screenHeight.hXs
   } as BrowserWindowConstructorOptions)
 
 export const createProtocolErrorWindow: CreateWindowFunction = () =>
   createWindow(WindowHashProtocolError, {
     ...base,
-    ...wMd,
-    ...hXs
+    ...screenWidth.wMd,
+    ...screenHeight.hXs
   } as BrowserWindowConstructorOptions)
 
 export const createUpdateWindow: CreateWindowFunction = () =>
   createWindow(WindowHashUpdate, {
     ...base,
-    ...wSm,
-    ...hMd
+    ...screenWidth.wSm,
+    ...screenHeight.hMd
   } as BrowserWindowConstructorOptions)
