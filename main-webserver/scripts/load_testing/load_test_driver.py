a03157bfca16d5634bd52d43d84bb336d4e8e4e5
jmather625 <jatinm2@illinois.edu>
2021-04-07 14:32:58 -0500

    ready to try

--- main-webserver/scripts/load_testing/load_test_driver.py
+++ main-webserver/scripts/load_testing/load_test_driver.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.join(os.getcwd(), os.path.dirname(__file__), "../.."))
 from scripts.load_testing.load_test_utils import OUTFOLDER, CSV_PREFIX
 
 
-def run_local_load_test(web_url: str, admin_token: str, num_reqs: int):
+def run_local_load_test(web_url: str, admin_token: str, num_users: int):
     """
     Run a local load test using locust.
 
@@ -23,14 +23,12 @@ def run_local_load_test(web_url: str, admin_token: str, num_reqs: int):
         web_url: URL of the webserver to load test
         admin_token: needed to the scripts perform admin-level actions on the webserver.
             See load_test.py:load_test_single_user for a full explanation.
-        num_reqs: total number of requests to make
+        num_users: total number of users to simulate
 
     Returns:
-        Calls `locust` utility, which creates four files:
-        - <OUTFOLDER>/<CSV_PREFIX>_exceptions.csv
-        - <OUTFOLDER>/<CSV_PREFIX>_failures.csv
-        - <OUTFOLDER>/<CSV_PREFIX>_stats.csv
-        - <OUTFOLDER>/<CSV_PREFIX>_stats_history.csv
+        Calls `locust` utility on `locust_load_test.py`. It will exit with nonzero exit
+        code if any user cannot get a container. If that happens, this function will
+        also exit with error.
     """
     if os.path.exists(OUTFOLDER):
         print(f"Cleaning existing {OUTFOLDER}")
@@ -47,8 +45,8 @@ def run_local_load_test(web_url: str, admin_token: str, num_reqs: int):
     # --csv: path to save results
     # --only-summary: only print result summaries
     cmd = (
-        f"locust -f locust_load_test.py -u {num_reqs} -r 10 --host {web_url} "
-        f"--headless --csv={csv_path} --only-summary"
+        f"locust -f locust_load_test.py -u {num_users} -r 10 --host {web_url} "
+        f"--headless --only-summary"
     )
     ret = subprocess.run(cmd, shell=True)
     assert ret.returncode == 0, "Failed to run a local load test."
