678c00bf56d7b310bd26c8086bdc1506120dc787
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-07 18:12:43 -0500

    Unused imports in utils/observables

--- client-applications/desktop/src/utils/observables.ts
+++ client-applications/desktop/src/utils/observables.ts
@@ -1,5 +1,5 @@
 import { Observable, ObservableInput, merge, race, interval, from } from "rxjs"
-import { mapTo, startWith, switchMap, share } from "rxjs/operators"
+import { mapTo, startWith, switchMap } from "rxjs/operators"
 
 export const loadingFrom = (
     request: Observable<any>,
@@ -9,6 +9,7 @@ export const loadingFrom = (
         request.pipe(mapTo(true), startWith(false)),
         race(...ends.map((o) => o.pipe(mapTo(false))))
     )
+
 export const pollMap = <T>(
     step: number,
     func: (...args: any[]) => ObservableInput<T>
