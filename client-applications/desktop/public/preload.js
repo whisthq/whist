21c042b84e7427a813ba82407366daa4b3cde62b
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-09 16:57:38 -0500

    Hardcode development environment in client app

--- client-applications/desktop/public/preload.js
+++ client-applications/desktop/public/preload.js
@@ -4,7 +4,7 @@ const { contextBridge, ipcRenderer } = require("electron")
 // because they are inherited from EventEmitter. This requires that they
 // be wrapped in a new function object so they can correctly be passed
 // to the renderer process.
-process.env.PRODUCTION_ENV = "development"
+process.env.PRODUCTION_ENV = "dev"
 process.env.VERSION = "v2.1.0"
 
 contextBridge.exposeInMainWorld("ipcRenderer", {
