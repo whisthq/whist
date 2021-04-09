2c937f0ba2454a3132077e194ac01de1dd4d6321
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-09 11:06:06 -0500

    Log application config (no keys) at app start

--- client-applications/desktop/src/main/effects/logging.ts
+++ client-applications/desktop/src/main/effects/logging.ts
@@ -1,9 +1,11 @@
 import { eventIPC } from "@app/main/events/ipc"
 
 import { pluck } from "rxjs/operators"
+import { omit } from "lodash"
 
 import { logDebug, logError, LogLevel } from "@app/utils/logging"
 import { Observable, combineLatest } from "rxjs"
+import config from "@app/utils/config"
 
 import * as e from "@app/main/events/app"
 import * as h from "@app/main/observables/host"
@@ -11,6 +13,9 @@ import * as c from "@app/main/observables/container"
 import * as p from "@app/main/observables/protocol"
 import * as s from "@app/main/observables/signup"
 import * as l from "@app/main/observables/login"
+
+// Printing out app config, omitting the api keys.
+logDebug("APP CONFIG", "printing utils/config.ts", omit(config, ["keys"]))
 
 // Commented out for the moment, as we'll use the logging in debug.ts
 // for the first release of this. The extra visibility will be helpful
