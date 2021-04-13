d56d37820efd12f052afd25acbe01be0300a0316
Mia <sturzu@college.harvard.edu>
2021-04-13 12:08:19 -0400

    clean

--- container-images/browsers/firefox/start-firefox.sh
+++ container-images/browsers/firefox/start-firefox.sh
@@ -6,4 +6,5 @@ set -Eeuo pipefail
 # Start Firefox with custom profile and set as default browser
 exec firefox \
         -profile /home/fractal/.mozilla/firefox/fractal_profile \
-        -setDefaultBrowser
+        -setDefaultBrowser \
+        -url "google.com"
