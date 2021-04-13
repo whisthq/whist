a83116f6d9a3f84f739f6fe4bd39cdfa71110a18
jmather625 <jatinm2@illinois.edu>
2021-04-13 14:43:41 -0500

    migrated notifs to slack

--- .github/workflows/helpers/notifications/slack_bot.py
+++ .github/workflows/helpers/notifications/slack_bot.py
@@ -5,53 +5,7 @@ from functools import wraps
 import requests
 
 
-def with_slack(func):
-    """A decorator to pass a configured Slack client to functions
-
-    Passes a default Slack client to decorated functions as the
-    'client=' kwarg.
-
-    Args:
-        func: A function that accepts a Slack client as its first argument.
-    Returns:
-        The input function, partially applied with the Slack client object.
-    """
-
-    @wraps(func)
-    def with_slack_wrapper(*args, **kwargs):
-
-        slack_username = os.environ.get("SLACK_USERNAME", "Fractal Bot")
-        slack_url = os.environ.get("SLACK_URL")
-
-        if not slack_url:
-            raise Exception("No SLACK_URL environment variable provided.")
-
-        kwargs["url"] = kwargs.get("url", slack_url)
-        kwargs["username"] = kwargs.get("username", slack_username)
-
-        return func(*args, **kwargs)
-
-    return with_slack_wrapper
-
-
-def validate_slack_config(func):
-    @wraps(func)
-    def validate_slack_config_wrapper(*args, **kwargs):
-        username = kwargs.get("username")
-        if not isinstance(username, str):
-            raise Exception(
-                "Slack client must be configured with "
-                + "a 'username=' keyword argument as a string. "
-                + f"Received: {username}")
-
-        return func(*args, **kwargs)
-
-    return validate_slack_config_wrapper
-
-
-@with_slack
-@validate_slack_config
-def create_post(channel, body, url=None, username=None, text=None):
+def create_post(channel, body, slack_webhook, username):
     """
     Accepts a text= keyword argument, because the Slack SDK warns without
     passing it.
@@ -62,9 +16,10 @@ def create_post(channel, body, url=None, username=None, text=None):
             "blocks": [{"type": "section",
                        "text": {"type": "mrkdwn",
                                 "text": body}}],
-            "text": text}
+            "link_names": 1, # @channel, @fractal_employee work
+            }
 
-    r = requests.post(url,
+    r = requests.post(slack_webhook,
                       data=json.dumps(data),
                       headers={"Content-Type": "application/json"})
 
