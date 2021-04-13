c0bd1204c3c7117178b204a38a76125d6a993a44
jmather625 <jatinm2@illinois.edu>
2021-04-13 11:11:12 -0500

    fix

--- main-webserver/app/blueprints/celery/celery_status_blueprint.py
+++ main-webserver/app/blueprints/celery/celery_status_blueprint.py
@@ -34,12 +34,15 @@ def celery_status(task_id, **kwargs):  # pylint: disable=unused-argument
         abort(make_response({"error": "Invalid task ID"}, BAD_REQUEST))
 
     try:
-        result = current_app.AsyncResult(task_id)
-        if result.status == "SUCCESS":
-            response = {"state": result.status, "output": result.result}
+        # methods like .status, .result of AsyncResult are actually properties that are fetched
+        # on each request. This led to a race where we mix returns from a non-SUCCESS state
+        # and a SUCCESS state.
+        result = current_app.AsyncResult(task_id)._get_task_meta()
+        if result["status"] == "SUCCESS":
+            response = {"state": result["status"], "output": result["result"]}
             return make_response(jsonify(response), SUCCESS)
-        elif result.status == "FAILURE":
-            if "MAINTENANCE ERROR" in result.result:
+        elif result["status"] == "FAILURE":
+            if "MAINTENANCE ERROR" in result["result"]:
                 # this is a special case: the request was accepted but the celery task
                 # started after the webserver was put into maintenance mode. We return
                 # that webserver is in maintenance mode so client app knows what happened.
@@ -58,15 +61,15 @@ def celery_status(task_id, **kwargs):  # pylint: disable=unused-argument
             msg = None
             if check_developer():
                 # give traceback to developers
-                msg = f"Experienced an error. Error trace: {result.traceback}"
+                msg = f"Experienced an error. Error trace: {result.get('traceback')}"
             else:
                 # do not give details to non-developers
                 msg = "Experienced an error. Please try the request again."
 
-            response = {"state": result.status, "output": msg}
+            response = {"state": result["status"], "output": msg}
             return make_response(jsonify(response), SUCCESS)
         else:
-            output = result.info
+            output = result["result"]
             # if isinstance(result.info, dict):
             #     if "msg" in result.info.keys():
             #         output = result.info["msg"]
