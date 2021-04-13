c979e3459588ced6c699c000b178129355acb8e0
Steve Li <lithafnium2000@gmail.com>
2021-04-13 12:19:21 -0400

    margin auto and fixed logo

--- website/src/pages/home/index.tsx
+++ website/src/pages/home/index.tsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
-import { FaMoon, FaSun } from 'react-icons/fa'
-import classNames from 'classnames'
+import React, { useState } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import classNames from "classnames";
 
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from "react-router-dom";
 
-import withTracker from '@app/shared/utils/withTracker'
+import withTracker from "@app/shared/utils/withTracker";
 
-import Header from '@app/shared/components/header'
-import Footer from '@app/shared/components/footer'
-import Top from './top'
-import Middle from './middle'
-import Features from './features'
-import ActionPrompt from './bottom'
+import Header from "@app/shared/components/header";
+import Footer from "@app/shared/components/footer";
+import Top from "./top";
+import Middle from "./middle";
+import Features from "./features";
+import ActionPrompt from "./bottom";
 
-const DarkModeIcon = (props: {dark: boolean, onClick: () => void }) => (
-                <div className="absolute">
-                    <div
-                        onClick={props.onClick}
-                        className={classNames('fixed top-16 md:top-24 right-2 md:right-12 dark:bg-transparent',
-                          'border border-transparent dark:border-gray-400 bg-blue-lightest',
-                          'text-gray dark:text-gray-300 p-2 rounded cursor-pointer z-50')}
-                    >
-                        <div>
-                           {props.dark ? <FaSun /> : <FaMoon/>}
-                        </div>
-                    </div>
-                </div>
-)
+const DarkModeIcon = (props: { dark: boolean; onClick: () => void }) => (
+  <div className="absolute">
+    <div
+      onClick={props.onClick}
+      className={classNames(
+        "fixed top-16 md:top-24 right-2 md:right-12 dark:bg-transparent",
+        "border border-transparent dark:border-gray-400 bg-blue-lightest",
+        "text-gray dark:text-gray-300 p-2 rounded cursor-pointer z-50"
+      )}
+    >
+      <div>{props.dark ? <FaSun /> : <FaMoon />}</div>
+    </div>
+  </div>
+);
 
 export const Chrome = () => {
   /*
@@ -34,25 +34,27 @@ export const Chrome = () => {
 
         Arguments: none
     */
-  const [dark, setDark] = useState(true)
+  const [dark, setDark] = useState(true);
 
   return (
-        <div
-            className={classNames('overflow-x-hidden',
-              dark ? 'dark bg-blue-darkest' : 'bg-white')}
-        >
-            <div className="pb-20 px-12 max-w-screen-2xl overflow-x-hidden" >
-                <DarkModeIcon dark={dark} onClick={() => setDark(!dark)} />
-                <Header dark={dark} />
-                <Top />
-                <Middle />
-                <Features />
-                <ActionPrompt />
-            </div>
-            <Footer />
-        </div>
-  )
-}
+    <div
+      className={classNames(
+        "overflow-x-hidden",
+        dark ? "dark bg-blue-darkest" : "bg-white"
+      )}
+    >
+      <div className="pb-20 px-12 max-w-screen-2xl m-auto overflow-x-hidden">
+        <DarkModeIcon dark={dark} onClick={() => setDark(!dark)} />
+        <Header dark={dark} />
+        <Top />
+        <Middle />
+        <Features />
+        <ActionPrompt />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 const Products = () => {
   /*
@@ -62,16 +64,16 @@ const Products = () => {
     */
 
   return (
-        <div>
-            <Switch>
-                <Route exact path="/" component={withTracker(Chrome)} />
-                {/* <Route
+    <div>
+      <Switch>
+        <Route exact path="/" component={withTracker(Chrome)} />
+        {/* <Route
                     path="/dashboard/settings"
                     component={withTracker(Settings)}
                 /> */}
-            </Switch>
-        </div>
-  )
-}
+      </Switch>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
--- website/src/shared/components/links.tsx
+++ website/src/shared/components/links.tsx
@@ -1,130 +1,122 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import classNames from 'classnames'
-import LogoBlack from '@app/assets/icons/logoBlack.svg'
-import LogoWhite from '@app/assets/icons/logoWhite.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import LogoBlack from "@app/assets/icons/logoBlack.svg";
+import LogoWhite from "@app/assets/icons/logoWhite.svg";
 
 export const AboutLink = (props: { className: string }) => (
-    <Link className={props.className} to="/about" id="about">
-        About
-    </Link>
-)
+  <Link className={props.className} to="/about" id="about">
+    About
+  </Link>
+);
 
 export const SupportLink = (props: { className: string }) => (
-    <a
-        className={props.className}
-        href="mailto: support@fractal.co"
-        id="support"
-    >
-        Support
-    </a>
-)
+  <a className={props.className} href="mailto: support@fractal.co" id="support">
+    Support
+  </a>
+);
 
 export const CareersLink = (props: { className: string }) => (
-    <a
-        className={props.className}
-        href="mailto: careers@fractal.co"
-        id="careers"
-    >
-        Careers
-    </a>
-)
+  <a className={props.className} href="mailto: careers@fractal.co" id="careers">
+    Careers
+  </a>
+);
 
 export const MyAccountLink = (props: { className?: string }) => (
-    <Link className={props.className} to="/dashboard">
-        My Account
-    </Link>
-)
+  <Link className={props.className} to="/dashboard">
+    My Account
+  </Link>
+);
 
-export const LogoLink = (props: { className?: string, dark?: boolean }) => (
-    <Link
-        className={classNames(props.className)}
-        style={{ minWidth: '1em' }}
-        to="/"
-    >
-        <img
-            src={props.dark !== undefined ? LogoWhite : LogoBlack}
-            className="w-full h-full"
-            alt="Logo"
-        />
-    </Link>
-)
+export const LogoLink = (props: { className?: string; dark?: boolean }) => (
+  <Link
+    className={classNames(props.className)}
+    style={{ minWidth: "1em" }}
+    to="/"
+  >
+    <img
+      src={props.dark ? LogoWhite : LogoBlack}
+      className="w-full h-full"
+      alt="Logo"
+    />
+  </Link>
+);
 
 export const WordmarkLink = (props: { className?: string }) => (
-    <Link
-        className={classNames(
-          props.className,
-          'outline-none font-semibold hover:no-underline tracking-fractal'
-        )}
-        to="/"
-    >
-        FRACTAL
-    </Link>
-)
+  <Link
+    className={classNames(
+      props.className,
+      "outline-none font-semibold hover:no-underline tracking-fractal"
+    )}
+    to="/"
+  >
+    FRACTAL
+  </Link>
+);
 
 export const SignInLink = (props: { className?: string }) => (
-    <Link id="signin" to="/auth" className={props.className}>
-        Sign In
-    </Link>
-)
+  <Link id="signin" to="/auth" className={props.className}>
+    Sign In
+  </Link>
+);
 
 export const SignOutLink = (props: {
-  className?: string
-  onClick?: Function
+  className?: string;
+  onClick?: Function;
 }) => (
-    <button
-        className={props.className}
-        onClick={() => (props?.onClick != null) && props.onClick()}
-    >
-        Sign Out
-    </button>
-)
+  <button
+    className={props.className}
+    onClick={() => props?.onClick != null && props.onClick()}
+  >
+    Sign Out
+  </button>
+);
 
-export const HomeLink = (props: { className?: string, onClick?: Function }) => (
-    <Link className={props.className} to="/dashboard">
-        Home
-    </Link>
-)
+export const HomeLink = (props: { className?: string; onClick?: Function }) => (
+  <Link className={props.className} to="/dashboard">
+    Home
+  </Link>
+);
 
 export const SettingsLink = (props: {
-  className?: string
-  onClick?: Function
+  className?: string;
+  onClick?: Function;
 }) => (
-    <Link to="/dashboard/settings" className={props.className}>
-        Settings
-    </Link>
-)
+  <Link to="/dashboard/settings" className={props.className}>
+    Settings
+  </Link>
+);
 
 export const SalesLink = (props: any) => (
-    <a {...props} href="mailto: sales@fractal.co">
-        Sales
-    </a>
-)
+  <a {...props} href="mailto: sales@fractal.co">
+    Sales
+  </a>
+);
 
 export const BlogLink = (props: any) => (
-    /* Temporary eslint-disable due to eslint bug */
-    /* Can remove on eslint-plugin-react@7.21.6 release */
-    /* eslint-disable react/jsx-no-target-blank */
-    <a
-        {...props}
-        href="https://medium.com/@fractal"
-        target="_blank"
-        rel="noopener noreferror"
-    >
-        Blog
-    </a>
-)
+  /* Temporary eslint-disable due to eslint bug */
+  /* Can remove on eslint-plugin-react@7.21.6 release */
+  /* eslint-disable react/jsx-no-target-blank */
+  <a
+    {...props}
+    href="https://medium.com/@fractal"
+    target="_blank"
+    rel="noopener noreferror"
+  >
+    Blog
+  </a>
+);
 
 export const DiscordLink = (props: any) => (
-    /* Temporary eslint-disable due to eslint bug */
-    /* Can remove on eslint-plugin-react@7.21.6 release */
-    /* eslint-disable react/jsx-no-target-blank */
-    <a
-        {...props}
-        href="https://medium.com/@fractal"
-        target="_blank"
-        rel="noopener noreferror"
-    >
-        Join our Discord
-    </a>
-)
+  /* Temporary eslint-disable due to eslint bug */
+  /* Can remove on eslint-plugin-react@7.21.6 release */
+  /* eslint-disable react/jsx-no-target-blank */
+  <a
+    {...props}
+    href="https://medium.com/@fractal"
+    target="_blank"
+    rel="noopener noreferror"
+  >
+    Join our Discord
+  </a>
+);
