ce1fadc271cf9118d3b9a555da555e3f6475d065
Roshan Padaki <roshan@fractal.co>
2021-04-14 19:39:44 -0400

    Replace lint command with roundabout call to cli

--- client-applications/desktop/scripts/start.js
+++ client-applications/desktop/scripts/start.js
@@ -11,14 +11,17 @@ const cmakeBuildDir = "build-clientapp"
 fs.mkdirSync(path.join("../../protocol", cmakeBuildDir), { recursive: true })
 
 if (process.platform === "win32") {
-    // On Windows, cmake wants rc.exe but is provided node_modules/.bin/rc.js
-    // Hence, we must modify the path to pop out the node_modules entries.
-    const pathArray = process.env.Path.split(";")
-    while (["node_modules", "yarn", "Yarn"].some(v => pathArray[0].includes(v))) pathArray.shift()
-    const path = pathArray.join(";")
-    execCommand(`cmake -S . -B ${cmakeBuildDir} -G Ninja`, "../../protocol", { Path: path })
+  // On Windows, cmake wants rc.exe but is provided node_modules/.bin/rc.js
+  // Hence, we must modify the path to pop out the node_modules entries.
+  const pathArray = process.env.Path.split(";")
+  while (["node_modules", "yarn", "Yarn"].some((v) => pathArray[0].includes(v)))
+    pathArray.shift()
+  const path = pathArray.join(";")
+  execCommand(`cmake -S . -B ${cmakeBuildDir} -G Ninja`, "../../protocol", {
+    Path: path,
+  })
 } else {
-    execCommand(`cmake -S . -B ${cmakeBuildDir}`, "../../protocol")
+  execCommand(`cmake -S . -B ${cmakeBuildDir}`, "../../protocol")
 }
 
 execCommand(
@@ -30,16 +33,25 @@ console.log("Copying over the built protocol...")
 
 const protocolBuildDir = path.join("protocol-build", "client")
 rimrafSync("protocol-build")
-fse.copySync(path.join("../../protocol", cmakeBuildDir, "client/build64"), protocolBuildDir)
+fse.copySync(
+  path.join("../../protocol", cmakeBuildDir, "client/build64"),
+  protocolBuildDir
+)
 
 const ext = process.platform === "win32" ? ".exe" : ""
 const oldExecutable = "FractalClient"
 const newExecutable = process.platform === "darwin" ? "_Fractal" : "Fractal"
 
-fse.moveSync(path.join(protocolBuildDir, `${oldExecutable}${ext}`), path.join(protocolBuildDir, `${newExecutable}${ext}`))
+fse.moveSync(
+  path.join(protocolBuildDir, `${oldExecutable}${ext}`),
+  path.join(protocolBuildDir, `${newExecutable}${ext}`)
+)
 
 rimrafSync("./loading")
-fse.moveSync(path.join("protocol-build/client", "loading"), path.join("loading"))
+fse.moveSync(
+  path.join("protocol-build/client", "loading"),
+  path.join("loading")
+)
 
 console.log("Building CSS with tailwind...")
 execCommand("tailwindcss build -o public/css/tailwind.css", ".")
