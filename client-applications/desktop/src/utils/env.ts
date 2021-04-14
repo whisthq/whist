e2b5e0bd0bfdc854807c2b7afb8dafd0a0887ad7
mingy98 <ming@fractal.co>
2021-04-14 18:43:02 -0400

    making env.ts prettier

--- client-applications/desktop/src/utils/env.ts
+++ client-applications/desktop/src/utils/env.ts
@@ -3,19 +3,14 @@ import fs from 'fs'
 import { app } from 'electron'
 
 const getEnv = () => {
-  if (app.isPackaged) {
-    const envFile = path
-      .join(app.getAppPath(), 'env.json')
-      .replace('build/dist/main/', '')
-      .replace('Resources/app.asar/', '')
+  if (!app.isPackaged) return {}
 
-    const rawJSON = fs.readFileSync(envFile, 'utf-8')
-    return JSON.parse(rawJSON)
-  } else {
-    return {}
-  }
+  const envFile = path
+    .join(app.getAppPath(), 'env.json')
+    .replace('build/dist/main/', '')
+    .replace('Resources/app.asar/', '')
+
+  return JSON.parse(fs.readFileSync(envFile, 'utf-8'))
 }
 
-const env = getEnv()
-
-export default env
+export default getEnv()
