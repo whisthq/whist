f97ec8c21bb3bd31aa529b82def26c50f3c8ca01
Mia <sturzu@college.harvard.edu>
2021-04-14 12:49:02 -0400

    more hw acc

--- container-images/browsers/firefox/fractal_profile/user.js
+++ container-images/browsers/firefox/fractal_profile/user.js
@@ -1,7 +1,14 @@
-// Enable Hardware Acceleration
-user_pref("layers.acceleration.force-enabled", true);
-user_pref("media.ffmpeg.vaapi.enabled", true);
-
 // Suppress startup pages
 user_pref("browser.startup.page", 0);
 user_pref("datareporting.policy.firstRunURL", "");
+
+// Enable General Hardware Acceleration
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("gfx.webrender.all", true);
+
+// Enable Hardware Video Acceleration
+// https://wiki.archlinux.org/index.php/Firefox#Hardware_video_acceleration
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.ffvpx.enabled", false);
+user_pref("media.rdd-vpx.enabled", false);
+user_pref("media.navigator.mediadatadecoder_vpx_enabled", true);
