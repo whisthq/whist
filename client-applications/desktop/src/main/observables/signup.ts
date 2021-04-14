ad46ed274ec8d0ee56d41c2e64ef371562e07fd3
Jannie <jz3191@columbia.edu>
2021-04-14 10:34:09 -0700

    file reorg wip

--- client-applications/desktop/src/main/observables/signup.ts
+++ client-applications/desktop/src/main/observables/signup.ts
@@ -10,18 +10,22 @@
 // "listen" to local storage, and update their values based on local
 // storage changes.
 
-import { fromEventIPC } from '@app/main/events/ipc'
-import { from } from 'rxjs'
-import { loadingFrom } from '@app/utils/observables'
-import { emailSignup, emailSignupValid, emailSignupError } from '@app/utils/api'
-import { createConfigToken, encryptConfigToken } from '@app/utils/crypto'
-import { filter, map, share, exhaustMap, switchMap } from 'rxjs/operators'
+import { fromEventIPC } from "@app/main/events/ipc"
+import { from } from "rxjs"
+import { loadingFrom } from "@app/utils/observables"
+import {
+  emailSignup,
+  emailSignupValid,
+  emailSignupError,
+} from "@app/utils/signup"
+import { createConfigToken, encryptConfigToken } from "@app/utils/crypto"
+import { filter, map, share, exhaustMap, switchMap } from "rxjs/operators"
 
-export const signupRequest = fromEventIPC('signupRequest').pipe(
+export const signupRequest = fromEventIPC("signupRequest").pipe(
   filter(
     (req) =>
-      ((req?.email as string) ?? '') !== '' &&
-      ((req?.password as string) ?? '') !== ''
+      ((req?.email as string) ?? "") !== "" &&
+      ((req?.password as string) ?? "") !== ""
   ),
   switchMap((req) =>
     from(
---
+++ client-applications/desktop/src/utils/login.ts
@@ -0,0 +1,31 @@
+import { post } from "@app/utils/api"
+import { AsyncReturnType } from "@app/utils/types"
+import { createConfigToken, decryptConfigToken } from "@app/utils/crypto"
+
+export const emailLogin = async (username: string, password: string) =>
+  post({
+    endpoint: "/account/login",
+    body: { username, password },
+  })
+
+type ResponseAuth = AsyncReturnType<typeof emailLogin>
+
+export const emailLoginValid = (response: ResponseAuth) =>
+  (response?.json?.access_token ?? "") !== ""
+
+export const emailLoginError = (response: ResponseAuth) =>
+  response.status !== 200
+
+export const emailLoginAccessToken = (response: ResponseAuth) =>
+  response.json?.access_token
+
+export const emailLoginRefreshToken = (response: ResponseAuth) =>
+  response.json?.refresh_token
+
+export const emailLoginConfigToken = async (
+  response: ResponseAuth,
+  password: string
+) =>
+  (response?.json?.encrypted_config_token ?? "") !== ""
+    ? decryptConfigToken(response.json.encrypted_config_token, password)
+    : await createConfigToken()
