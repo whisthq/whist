8bdcadc2492dcedf7955c2d8822257d1051dcac4
mingy98 <ming@fractal.co>
2021-04-15 12:41:19 -0400

    publishing should be fixed

--- client-applications/desktop/scripts/package.sh
+++ client-applications/desktop/scripts/package.sh
@@ -2,6 +2,6 @@
 
 set -Eeuo pipefail
 
-publish=${publish:-never}
+publish=${2:-never}
 
 set DEBUG=electron-builder && VERSION=$(git describe --abbrev=0) snowpack build && electron-builder build --config electron-builder.config.js  --publish $publish
