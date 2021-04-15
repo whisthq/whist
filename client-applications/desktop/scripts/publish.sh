cd6b5786e9a8ec6f3ad3957606650a3b5cb4c517
Roshan Padaki <roshan@fractal.co>
2021-04-15 16:06:29 -0400

    Clean up script names; tested on macOS

--- client-applications/desktop/scripts/publish.sh
+++ client-applications/desktop/scripts/publish.sh
@@ -108,7 +108,7 @@ else
     # Initialize yarn first
     yarn cache clean
     yarn install
-    yarn prepackage -- ${env}
+    yarn package:set-env ${env}
 
     if [[ "$publish" == "true" ]]; then
         # Package the application and upload to AWS S3 bucket
