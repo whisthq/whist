b06c8db942f1c166c2800ade7ecdce5f54f0b531
Ming <ming@fractal.co>
2021-04-15 14:32:32 -0400

    App should actually now publish (#1769)
    * publishing should be fixed
    
    * adding phils package.json changes
    
    * Switch from package.sh to package.js
    
    * Allow CI builds to retrieve version via 'git describe'
    
    Co-authored-by: Roshan Padaki <roshan@fractal.co>

--- client-applications/desktop/scripts/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-publish=${publish:-never}
-
-set DEBUG=electron-builder && VERSION=$(git describe --abbrev=0) snowpack build && electron-builder build --config electron-builder.config.js  --publish $publish
