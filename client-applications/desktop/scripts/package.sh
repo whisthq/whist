2b4b613db48ee8cdefd06777dfe2f0ec9e87d445
Roshan Padaki <roshan@fractal.co>
2021-04-15 13:17:46 -0400

    Switch from package.sh to package.js

--- client-applications/desktop/scripts/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-publish=${1:-never}
-
-set DEBUG=electron-builder && VERSION=$(git describe --abbrev=0) snowpack build && electron-builder build --config electron-builder.config.js  --publish $publish
