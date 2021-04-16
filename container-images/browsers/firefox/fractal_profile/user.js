4c0bd68b16c645b81f67f9a1eb7a3c710ce63d32
Mia <sturzu@college.harvard.edu>
2021-04-16 12:12:42 -0400

    remove webrender to test

--- container-images/browsers/firefox/fractal_profile/user.js
+++ container-images/browsers/firefox/fractal_profile/user.js
@@ -4,7 +4,7 @@ user_pref("datareporting.policy.firstRunURL", "");
 
 // Enable General Hardware Acceleration
 user_pref("layers.acceleration.force-enabled", true);
-user_pref("gfx.webrender.all", true);
+// user_pref("gfx.webrender.all", true);
 
 // Enable Hardware Video Acceleration
 // https://wiki.archlinux.org/index.php/Firefox#Hardware_video_acceleration
