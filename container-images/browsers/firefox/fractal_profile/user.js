1b2347d14bedb2c7a89dd5b4d1023ade8fa8917d
Mia <sturzu@college.harvard.edu>
2021-04-20 13:13:41 -0400

    comment, remove debug output

--- container-images/browsers/firefox/fractal_profile/user.js
+++ container-images/browsers/firefox/fractal_profile/user.js
@@ -6,11 +6,9 @@ user_pref("datareporting.policy.firstRunURL", "");
 // user_pref("layers.acceleration.force-enabled", true);
 user_pref("gfx.webrender.all", true);
 
-
 // Enable Hardware Video Acceleration
 // https://wiki.archlinux.org/index.php/Firefox#Hardware_video_acceleration
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.ffmpeg.vaapi-drm-display.enabled", true);
 user_pref("media.ffvpx.enabled", false);
 user_pref("media.rdd-vpx.enabled", false);
-// user_pref("media.navigator.mediadatadecoder_vpx_enabled", true);
--- container-images/browsers/firefox/start-firefox.sh
+++ container-images/browsers/firefox/start-firefox.sh
@@ -3,6 +3,7 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
+# Use X11 (Wayland not supported by native NVIDIA driver yet) 
 export MOZ_X11_EGL=1
 
 # Start Firefox with custom profile, set as default browser, and open Google
@@ -10,5 +11,3 @@ exec firefox \
         -profile /home/fractal/.mozilla/firefox/fractal_profile \
         -setDefaultBrowser \
         -url "google.com" \
-        -MOZ_LOG="PlatformDecoderModule:5" \
-        -MOZ_LOG_FILE="/home/fractal/firefox_log.txt"
