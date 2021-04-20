8aa66eb19f0d1658b20152fe14b380ff3ac4b48f
Katz <mykatz01@gmail.com>
2021-04-20 00:04:53 -0700

    Fix linux packaging, clear packaged and dev caches

--- client-applications/desktop/scripts/clearAppData.js
+++ client-applications/desktop/scripts/clearAppData.js
@@ -4,8 +4,10 @@ const fs = require('fs-extra');
 const appName = app.getName();
 
 // Get app directory
-const appPath = path.join(app.getPath('appData'), appName);
+const devAppPath = path.join(app.getPath('appData'), "Electron");
+const prodAppPath = path.join(app.getPath('appData'), "Fractal");
 
 // Recursively delete all app files
-fs.rmdirSync(appPath, { recursive: true });
+fs.rmdirSync(devAppPath, { recursive: true });
+fs.rmdirSync(prodAppPath, { recursive: true });
 app.exit();
