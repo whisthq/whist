9abe3a132ea560955588555ada2c9e61d9c4fb83
Mia Sturzu <sturzu@college.harvard.edu>
2021-04-20 13:49:50 -0400

    Enable hardware acceleration in Firefox (#1818)

--- container-images/browsers/firefox/start-firefox.sh
+++ container-images/browsers/firefox/start-firefox.sh
@@ -3,6 +3,9 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
+# Use X11 (Wayland not supported by native NVIDIA driver yet) 
+export MOZ_X11_EGL=1
+
 # Start Firefox with custom profile, set as default browser, and open Google
 exec firefox \
         -profile /home/fractal/.mozilla/firefox/fractal_profile \
