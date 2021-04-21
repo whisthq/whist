6bed21c6a468acfed4b229f6021786b49a82575c
Steve Li <lithafnium2000@gmail.com>
2021-04-21 13:44:06 -0400

    fixed routes with snowpack (#1827)

--- website/snowpack.config.js
+++ website/snowpack.config.js
@@ -1,16 +1,17 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-    mount: {
-        public: { url: "/", static: true },
-        src: { url: "/dist" },
-    },
-    alias: {
-        "@app/assets": "./public/assets",
-        "@app": "./src"
-    },
-    plugins: [
-        "@snowpack/plugin-react-refresh",
-        "@snowpack/plugin-dotenv",
-        "@snowpack/plugin-typescript",
-    ],
-}
+  mount: {
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
+  },
+  alias: {
+    "@app/assets": "./public/assets",
+    "@app": "./src",
+  },
+  plugins: [
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+  ],
+  routes: [{ match: "routes", src: ".*", dest: "/index.html" }],
+};
