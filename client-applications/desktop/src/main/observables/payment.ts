6d91c9cda89556758bf78260f501522d48758f94
Steve Li <lithafnium2000@gmail.com>
2021-04-21 12:00:54 -0400

    added window listener, loading page, and customer portal functionality

--- client-applications/desktop/src/main/observables/payment.ts
+++ client-applications/desktop/src/main/observables/payment.ts
@@ -1,25 +1,33 @@
-import { fromEventIPC } from '@app/main/events/ipc'
-import { from } from 'rxjs'
-import { debugObservables, errorObservables } from '@app/utils/logging'
+import { fromEventIPC } from "@app/main/events/ipc"
+import { from, merge } from "rxjs"
+import { debugObservables, errorObservables } from "@app/utils/logging"
 import {
-  stripeCreateCheckout,
+  stripeCheckoutCreate,
   stripeCheckoutValid,
-  stripeCheckoutError
-} from '@app/utils/payment'
-import { filter, map, share, exhaustMap } from 'rxjs/operators'
+  stripeCheckoutError,
+  stripePortalCreate,
+  stripePortalValid,
+  stripePortalError,
+} from "@app/utils/payment"
+import { filter, map, share, exhaustMap } from "rxjs/operators"
 
-export const stripeCheckoutRequest = fromEventIPC('stripeCheckoutRequest').pipe(
+export const stripeCheckoutRequest = fromEventIPC("stripeCheckoutRequest").pipe(
   filter(
-    (req) => (req?.customerId ?? '') !== '' && (req?.priceId ?? '') !== ''
+    (req) => (req?.customerId ?? "") !== "" && (req?.priceId ?? "") !== ""
   ),
-  map(({ customerId, priceId }) => [customerId, priceId]),
+  map(({ customerId, priceId, successUrl, cancelUrl }) => [
+    customerId,
+    priceId,
+    successUrl,
+    cancelUrl,
+  ]),
   share()
 )
 
 export const stripeCheckoutProcess = stripeCheckoutRequest.pipe(
   map(
-    async ([customerId, priceId]) =>
-      await stripeCreateCheckout(customerId, priceId)
+    async ([customerId, priceId, successUrl, cancelUrl]) =>
+      await stripeCheckoutCreate(customerId, priceId, successUrl, cancelUrl)
   ),
   exhaustMap((req) => from(req)),
   share()
@@ -33,10 +41,53 @@ export const stripeCheckoutFailure = stripeCheckoutProcess.pipe(
   filter((res) => stripeCheckoutError(res))
 )
 
-debugObservables(
-  [stripeCheckoutRequest, 'stripeCheckoutRequest'],
-  [stripeCheckoutProcess, 'stripeCheckoutProcess'],
-  [stripeCheckoutSuccess, 'stripeCheckoutSuccess']
+export const stripePortalRequest = fromEventIPC("stripePortalRequest").pipe(
+  filter(
+    (req) => (req?.customerId ?? "") !== "" && (req?.returnUrl ?? "") !== ""
+  ),
+  map(({ customerId, returnUrl }) => [customerId, returnUrl]),
+  share()
 )
 
-errorObservables([stripeCheckoutFailure, 'stripeCheckoutFailure'])
+export const stripePortalProcess = stripePortalRequest.pipe(
+  map(
+    async ([customerId, returnUrl]) =>
+      await stripePortalCreate(customerId, returnUrl)
+  ),
+  exhaustMap((req) => from(req)),
+  share()
+)
+
+export const stripePortalSuccess = stripePortalProcess.pipe(
+  filter((res) => stripePortalValid(res))
+)
+
+export const stripePortalFailure = stripePortalProcess.pipe(
+  filter((res) => stripePortalError(res))
+)
+
+export const stripeAction = merge(
+  stripeCheckoutSuccess.pipe(
+    map((req) => ({ action: "CHECKOUT", stripeCheckoutId: req.json.sessionId }))
+  ),
+  stripePortalSuccess.pipe(
+    map((req) => ({ action: "PORTAL", stripePortalUrl: req.json.url }))
+  )
+)
+
+// IDEA: merge the two successes together
+
+debugObservables(
+  [stripeCheckoutRequest, "stripeCheckoutRequest"],
+  [stripeCheckoutProcess, "stripeCheckoutProcess"],
+  [stripeCheckoutSuccess, "stripeCheckoutSuccess"],
+  [stripePortalRequest, "stripePortalRequest"],
+  [stripePortalProcess, "stripePortalProcess"],
+  [stripePortalSuccess, "stripePortalSuccess"],
+  [stripeAction, "stripeAction"]
+)
+
+errorObservables(
+  [stripeCheckoutFailure, "stripeCheckoutFailure"],
+  [stripePortalFailure, "stripePortalFailure"]
+)
--- website/src/shared/constants/routes.ts
+++ website/src/shared/constants/routes.ts
@@ -1,16 +1,17 @@
 export const routes = {
-  EMPTY: '/',
-  ABOUT: '/about',
-  COOKIES: '/cookies',
-  PRIVACY: '/privacy',
-  TOS: '/termsofservice',
-  AUTH: '/auth/:first?',
-  VERIFY: '/verify',
-  RESET: '/reset',
-  AUTH_CALLBACK: '/callback',
-  DASHBOARD: '/dashboard',
-  LANDING: '/:first?/:second?',
-  PRODUCTS: '/products'
-}
+  EMPTY: "/",
+  ABOUT: "/about",
+  COOKIES: "/cookies",
+  PRIVACY: "/privacy",
+  TOS: "/termsofservice",
+  AUTH: "/auth/:first?",
+  VERIFY: "/verify",
+  RESET: "/reset",
+  AUTH_CALLBACK: "/callback",
+  DASHBOARD: "/dashboard",
+  LANDING: "/:first?/:second?",
+  PRODUCTS: "/products",
+  LOADING: "/loading",
+};
 
-export default routes
+export default routes;
