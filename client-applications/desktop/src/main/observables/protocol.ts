b8e3cd90705228af28fc0a57263906e3e7386e18
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-22 12:14:37 -0500

    Add utils/formatters to client app

--- client-applications/desktop/src/main/observables/protocol.ts
+++ client-applications/desktop/src/main/observables/protocol.ts
@@ -5,26 +5,26 @@
 // Many of these observables emit the protocol ChildProcess object, which
 // carries important data about the state of the protocol process.
 
-import { protocolLaunch } from "@app/utils/protocol"
+import { protocolLaunch } from '@app/utils/protocol'
 import {
   containerInfoIP,
   containerInfoPorts,
-  containerInfoSecretKey,
-} from "@app/utils/container"
-import { debugObservables, errorObservables } from "@app/utils/logging"
+  containerInfoSecretKey
+} from '@app/utils/container'
+import { debugObservables, errorObservables } from '@app/utils/logging'
 import {
-  containerCreateRequest,
+  containerAssignRequest,
   containerAssignSuccess,
-  containerAssignFailure,
-} from "@app/main/observables/container"
-import { hostConfigFailure } from "@app/main/observables/host"
-import { loadingFrom } from "@app/utils/observables"
-import { zip, of, fromEvent, merge } from "rxjs"
-import { map, filter, share, mergeMap, take } from "rxjs/operators"
-import { EventEmitter } from "events"
+  containerAssignFailure
+} from '@app/main/observables/container'
+import { hostConfigFailure } from '@app/main/observables/host'
+import { loadingFrom } from '@app/utils/observables'
+import { zip, of, fromEvent, merge } from 'rxjs'
+import { map, filter, share, mergeMap } from 'rxjs/operators'
+import { EventEmitter } from 'events'
+import { formatChildProcess } from '@app/utils/formatters'
 
-export const protocolLaunchProcess = containerCreateRequest.pipe(
-  take(1),
+export const protocolLaunchProcess = containerAssignRequest.pipe(
   map(() => protocolLaunch()),
   share()
 )
@@ -33,7 +33,7 @@ export const protocolLaunchSuccess = containerAssignSuccess.pipe(
   map((res) => ({
     ip: containerInfoIP(res),
     secret_key: containerInfoSecretKey(res),
-    ports: containerInfoPorts(res),
+    ports: containerInfoPorts(res)
   }))
 )
 
@@ -49,9 +49,7 @@ export const protocolLoading = loadingFrom(
 )
 
 export const protocolCloseRequest = protocolLaunchProcess.pipe(
-  mergeMap((protocol) =>
-    zip(of(protocol), fromEvent(protocol as EventEmitter, "close"))
-  )
+  mergeMap((protocol) => zip(of(protocol), fromEvent(protocol as EventEmitter, 'close')))
 )
 
 export const protocolCloseFailure = protocolCloseRequest.pipe(
@@ -59,16 +57,21 @@ export const protocolCloseFailure = protocolCloseRequest.pipe(
 )
 
 export const protocolCloseSuccess = protocolCloseRequest.pipe(
-  filter(([protocol]) => !protocol.killed)
+  filter(([protocol]) => !(protocol.killed))
 )
 
 // Logging
 
 debugObservables(
-  [protocolLaunchProcess, "protocolLaunchProcess"],
-  [protocolLaunchSuccess, "protocolLaunchSuccess"],
-  [protocolLoading, "protocolLaunchLoading"],
-  [protocolCloseRequest, "protocolCloseRequest"]
+  [protocolLaunchProcess, 'protocolLaunchProcess'],
+  [protocolLaunchSuccess, 'protocolLaunchSuccess'],
+  [protocolLoading, 'protocolLaunchLoading'],
+  [
+    protocolCloseRequest.pipe(
+      map(([protocol, event]) => [formatChildProcess(protocol), event])
+    ),
+    'protocolCloseRequest',
+  ]
 )
 
-errorObservables([protocolLaunchFailure, "protocolLaunchFailure"])
+errorObservables([protocolLaunchFailure, 'protocolLaunchFailure'])
