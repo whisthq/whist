bf6db48540176fe33e096bef3029a4cf6b8ae528
Steve Li <lithafnium2000@gmail.com>
2021-04-22 15:43:40 -0400

    removed body arguments

--- client-applications/desktop/src/renderer/shared/payment.tsx
+++ client-applications/desktop/src/renderer/shared/payment.tsx
@@ -52,12 +52,6 @@ export const StripeProvider = ({ children }: any) => {
     successUrl: string,
     cancelUrl: string
   ) => {
-    const body = {
-      priceId: "plan_J632kqGTMkcro6",
-      customerId: "cus_JKiqEsRwvk9gds",
-      successUrl: "https://www.fractal.co/",
-      cancelUrl: "https://www.fractal.co/",
-    }
     setMainState({
       stripeCheckoutRequest: { priceId, customerId, successUrl, cancelUrl },
       stripeAction: {},
@@ -65,10 +59,6 @@ export const StripeProvider = ({ children }: any) => {
   }
 
   const getPortalSession = async (customerId: string, returnUrl: string) => {
-    const body = {
-      customerId: "cus_JKiqEsRwvk9gds",
-      returnUrl: "https://www.fractal.co/",
-    }
     setMainState({
       stripePortalRequest: { customerId, returnUrl },
       stripeAction: {},
