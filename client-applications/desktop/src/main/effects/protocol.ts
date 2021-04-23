d29481d12682f1b9d84d0c41041478fee5d9866e
Jannie <jz3191@columbia.edu>
2021-04-23 15:30:59 -0700

    lint
    clean up
    
    done

--- client-applications/desktop/src/main/effects/protocol.ts
+++ client-applications/desktop/src/main/effects/protocol.ts
@@ -3,15 +3,15 @@
  * @file app.ts
  * @brief This file contains subscriptions to Observables related to protocol launching.
  */
-import { zip, merge } from 'rxjs'
+import { zip, merge } from "rxjs"
 
-import { protocolStreamInfo, protocolStreamKill } from '@app/utils/protocol'
+import { protocolStreamInfo, protocolStreamKill } from "@app/utils/protocol"
 import {
   protocolLaunchProcess,
   protocolLaunchSuccess,
-  protocolLaunchFailure
-} from '@app/main/observables/protocol'
-import { quitRequest, signoutRequest } from '@app/main/observables/tray'
+  protocolLaunchFailure,
+} from "@app/main/observables/protocol"
+import { quitRequest, signoutRequest } from "@app/main/observables/tray"
 
 // The current implementation of the protocol process shows its own loading
 // screen while a container is created and configured. To do this, we need it
