146ba751af9cbb2795e7a27d1aebbf67f989826e
Steve Li <lithafnium2000@gmail.com>
2021-04-23 13:03:31 -0400

    removed prettier script from lint.js

--- client-applications/desktop/scripts/lint.js
+++ client-applications/desktop/scripts/lint.js
@@ -1,7 +1,7 @@
 /* scripts/lint.js
  *
  * This script triggers lint checking and/or fixing for all of
- * our source files, using ESLint and Prettier.
+ * our source files, using ESLint.
  *
  * This should be invoked via:
  *   $ yarn lint:check
@@ -9,22 +9,16 @@
  *   $ yarn lint:fix
  * from your command line.
  *
- * The format in package.json is:
- *  $ node scripts/lint.js <prettier flag> <eslint flags>
  */
 
 const { execute } = require('../node_modules/eslint/lib/cli')
 const execCommand = require('./execCommand').execCommand
 
-// The third argument is the flag for prettier, either --check or --write
-// execCommand(`yarn prettier ${process.argv.slice(2, 3)} .`, '.')
-
 // The first two arguments here are simply placeholders, as ESLint CLI expects to be called in this way
-// wWe start on the 4th argument as the first 3 are reserved for placeholders/prettier
 execute([
     'yarn',
     'eslint',
-    ...process.argv.slice(3),
+    ...process.argv.slice(2),
     '--max-warnings=0',
     './src/**/*.{js,jsx,ts,tsx}',
 ]).then((ret) => process.exit(ret))
