cc350934add526754eed85d5deb338b271cd4cde
Katz <mykatz01@gmail.com>
2021-04-22 22:13:07 -0700

    build and scripts config

--- client-applications/desktop/scripts/clearAppData.js
+++ client-applications/desktop/scripts/clearAppData.js
@@ -1,13 +1,39 @@
+/* 
+  Clears electron user data cache
+
+  To be called via:
+  $ yarn cache:clear -- [all|dev|prod]
+*/
+
 const path = require('path');
 const { app } = require('electron');
 const fs = require('fs-extra');
-const appName = app.getName();
+const { userDataFolderNames } = require('../config/environment')
 
-// Get app directory -- "Electron" for local development, "Fractal" for packaged
-const devAppPath = path.join(app.getPath('appData'), "Electron");
-const prodAppPath = path.join(app.getPath('appData'), "Fractal");
+let foldersToDelete;
 
-// Recursively delete all app files
-fs.rmdirSync(devAppPath, { recursive: true });
-fs.rmdirSync(prodAppPath, { recursive: true });
-app.exit();
+const args = process.argv.slice(2)
+switch(args[0]) {
+  case '--all':
+    foldersToDelete = Object.values(userDataFolderNames)
+    break
+  case '--prod':
+    foldersToDelete = [userDataFolderNames.production]
+    break
+  case '--dev':
+    foldersToDelete = [userDataFolderNames.development]
+    break
+}
+
+if(!foldersToDelete) {
+  console.log('Must specify a flag: --[all|dev|prod]')
+  app.exit()
+}
+
+foldersToDelete.forEach(folder => {
+  const appPath = path.join(app.getPath('appData'), folder)
+  console.log('Clearing', appPath)
+  fs.rmdirSync(appPath, { recursive: true });
+})
+
+app.exit()
