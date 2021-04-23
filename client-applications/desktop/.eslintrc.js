0f6a687e69e1429dc3af37a65bb511b7b3a23d26
Steve Li <lithafnium2000@gmail.com>
2021-04-23 11:59:24 -0400

    added prettier linter to client app

--- client-applications/desktop/.eslintrc.js
+++ client-applications/desktop/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
--- client-applications/desktop/scripts/lint.js
+++ client-applications/desktop/scripts/lint.js
@@ -1,7 +1,7 @@
 /* scripts/lint.js
  *
  * This script triggers lint checking and/or fixing for all of
- * our source files, using ESLint.
+ * our source files, using ESLint and Prettier.
  *
  * This should be invoked via:
  *   $ yarn lint:check
@@ -9,15 +9,22 @@
  *   $ yarn lint:fix
  * from your command line.
  *
+ * The format in package.json is:
+ *  $ node scripts/lint.js <prettier flag> <eslint flags>
  */
 
-const { execute } = require("../node_modules/eslint/lib/cli")
+const { execute } = require('../node_modules/eslint/lib/cli')
+const execCommand = require('./execCommand').execCommand
+
+// The third argument is the flag for prettier, either --check or --write
+execCommand(`yarn prettier ${process.argv.slice(2, 3)} .`, '.')
 
 // The first two arguments here are simply placeholders, as ESLint CLI expects to be called in this way
+// wWe start on the 4th argument as the first 3 are reserved for placeholders/prettier
 execute([
-  "yarn",
-  "eslint",
-  ...process.argv.slice(2),
-  "--max-warnings=0",
-  "./src/**/*.{js,jsx,ts,tsx}",
+    'yarn',
+    'eslint',
+    ...process.argv.slice(3),
+    '--max-warnings=0',
+    './src/**/*.{js,jsx,ts,tsx}',
 ]).then((ret) => process.exit(ret))
