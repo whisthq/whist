b44bd0c677a2fe987a45691faeb3981a7ae57ef3
djsavvy <sraghuvanshi@college.harvard.edu>
2021-04-26 12:34:02 -0400

    Make client-app packaging work cross-platform, and organize packaging into package:local, package:notarize, and package:publish

---
+++ client-applications/desktop/scripts/package-local.js
@@ -0,0 +1,14 @@
+// Package the app for local testing using snowpack and electron-builder
+
+const helpers = require("./build-package-helpers")
+
+helpers.buildAndCopyProtocol()
+helpers.buildTailwind()
+helpers.configureCodeSigning(false)
+
+// For testing, we just hardcode the environment to dev
+helpers.setPackageEnv("dev")
+
+// For package-local, we don't want to increment the version so we use existing version
+helpers.snowpackBuild(helpers.getCurrentClientAppVersion())
+helpers.electronBuild()
