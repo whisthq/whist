e4574242a8eaabbc0fd018896a49a27510f873a5
Nicholas Pipitone <npip99@gmail.com>
2021-04-27 13:13:58 -0400

    Cleanup function names, add doxygen comments

--- protocol/fractal/core/fractal.h
+++ protocol/fractal/core/fractal.h
@@ -668,25 +668,133 @@ void* safe_malloc(size_t size);
 bool safe_strncpy(char* destination, const char* source, size_t num);
 
 // Dummy typedef for block allocator since only pointers are used anyway
+// See fractal.c for the real BlockAllocator struct
 typedef char BlockAllocator;
 
+/**
+ * @brief                          Creates a block allocator that will create and free blocks of the
+ *                                 given block_size. The block allocator will _not_ interfere with
+ *                                 the malloc heap.
+ *
+ * @param block_size               The size of blocks that this block allocator will allocate/free
+ *
+ * @returns                        The block allocator
+ */
 BlockAllocator* create_block_allocator(size_t block_size);
 
 /**
- * @brief                          Allocates a large block of memory, without fragmenting our heap
+ * @brief                          Allocates a block using the given block allocator
+ *
+ * @param block_allocator          The block allocator to use for allocating the block
+ *
+ * @returns                        The new block
  */
-void* allocate_block(BlockAllocator* BlockAllocator);
+void* allocate_block(BlockAllocator* block_allocator);
 
 /**
- * @brief                          Frees data allocated by allocate_block
+ * @brief                          Frees a block allocated by allocate_block
+ *
+ * @param block_allocator          The block allocator that the block was allocated from
+ *
+ * @param block                    The block to free
  */
-void free_block(BlockAllocator* BlockAllocator, void* block);
+void free_block(BlockAllocator* block_allocator, void* block);
 
-void* allocate_custom_block(size_t block_size);
-void mark_unused_custom_block(void* block);
-void mark_used_custom_block(void* block);
-void* realloc_custom_block(void* block, size_t block_size);
-void free_custom_block(void* block);
+/*
+==================
+Region Allocator
+==================
+
+This is a thin wrapper around mmap/VirtualAlloc, and will give us fine-grained
+control over regions of memory and whether or not the OS will allocate actual pages of RAM for us.
+(Pages are usually size 4096, but are in-general OS-defined)
+
+This gives us fine-grained control over the "reported memory usage", which represents pages of
+RAM that we demand must be allocated, and must not be written-to/read-from by any other processes.
+This is what controls when the OS is "out of RAM", and what is reported by the OS's "Task Manager".
+
+The OS by-default uses lazy allocation. So we won't actually use RAM until we read or write
+to a page allocated by allocate_region. All fresh pages we read from will be 0-ed out first
+
+Once we read/write to a page, that page becomes allocated in RAM. If you're done using it,
+and you want to tell the OS that the data in the region isn't important to you anymore, call the
+`mark_unused_region` function. That will decrease our reported memory usage by the size of the
+region. However, the OS won't actually take those pages from us until it's low on RAM.
+
+If we want to tell the OS that the region is important to us again, call
+`mark_used_region`. Just like allocate_region, this won't actually increase reported memory usage,
+until we read/write to those pages. However, our pages will usually be left exactly as they were
+prior to `mark_unused_region`. They'll only be zero-ed out if the OS needed those pages in the
+meantime. This is much more efficient than having it be zero-ed out every time, it'll
+only be zero-ed out if it was actually necessary to zero it out.
+
+If we want to completely give the region back to the OS, call `deallocate_region`,
+and the RAM + address space will be freed.
+
+*/
+
+/**
+ * @brief                          Allocates a region of memory, of the requested size.
+ *                                 This region will be allocated independently of
+ *                                 the malloc heap or any block allocator.
+ *                                 Will be zero-initialized.
+ *
+ *                                 This will only increase reported memory usage,
+ *                                 once the pages are actually read or written to
+ *
+ * @param region_size              The size of the region to create
+ *
+ * @returns                        The new region of memory
+ */
+void* allocate_region(size_t region_size);
+
+/**
+ * @brief                          Marks the region as unused (for now).
+ *                                 This will let other processes use it if they desire.
+ *
+ *                                 This will decrease the reported memory usage,
+ *                                 by the size of the region that was used
+ *
+ * @param region                   The region to mark as unused
+ */
+void mark_unused_region(void* region);
+
+/**
+ * @brief                          Marks the region as used again.
+ *                                 This will grab new memory regions from the OS,
+ *                                 only give other processes have taken the
+ *                                 memory while it was unused
+ *
+ *                                 This will only increase the reported memory usage,
+ *                                 once the pages are used
+ *
+ * @param region                   The region to mark as used now
+ */
+void mark_used_region(void* region);
+
+/**
+ * @brief                          Resize the memory region.
+ *
+ *                                 This will try to simply grow the region,
+ *                                 but if it can't, it will allocate a new region
+ *                                 and copy everything over
+ *
+ * @param region                   The region to resize
+ *
+ * @param new_region_size          The new size of the region
+ *
+ * @returns                        The new pointer to the region
+ *                                 (Will be different from the given region,
+ *                                 if an allocation + copy was necessary)
+ */
+void* realloc_region(void* region, size_t new_region_size);
+
+/**
+ * @brief                          Give the region back to the OS
+ *
+ * @param region                   The region to deallocate
+ */
+void deallocate_region(void* region);
 
 /**
  * @brief                          Returns a short string representing the current git commit
