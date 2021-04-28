208396d8e8c8eac48bae5017deb524c5b9f6890b
Steve Li <lithafnium2000@gmail.com>
2021-04-28 12:50:46 -0400

    added formatters for user tokens

--- client-applications/desktop/src/utils/formatters.ts
+++ client-applications/desktop/src/utils/formatters.ts
@@ -1,5 +1,5 @@
 import { ChildProcess } from "child_process"
-import { omit, pick } from "lodash"
+import { omit, pick, truncate } from "lodash"
 import { merge, Observable } from "rxjs"
 import { map } from "rxjs/operators"
 
@@ -23,26 +23,36 @@ export const formatChildProcess = (process: ChildProcess) =>
     "_handle",
   ])
 
-export const formatLogin = (res: object) => omit(res, [...omitJson])
+export const formatLogin = (res: any) => omit(res, [...omitJson])
 
-export const formatContainer = (res: object) =>
-  omit(res, [
-    "statusText",
-    "response",
-    "status",
-    "json.output.dpi",
-    "json.output.ip",
-    "json.output.location",
-    "json.output.port_32262",
-    "json.output.port_32263",
-    "json.output.port_32273",
-    "json.output.secret_key",
-    "json.output.task_definition",
-    "json.output.task_version",
-    "json.output.user_id",
-    "json.output.last_pinged",
-  ])
+export const formatContainer = (res: any) => {
+  if (res.state !== "POLLING") {
+    return omit(res, [...omitJson, "json.output"])
+  } else
+    return omit(res, [
+      "statusText",
+      "response",
+      "status",
+      "json.output.dpi",
+      "json.output.ip",
+      "json.output.location",
+      "json.output.port_32262",
+      "json.output.port_32263",
+      "json.output.port_32273",
+      "json.output.secret_key",
+      "json.output.task_definition",
+      "json.output.task_version",
+      "json.output.user_id",
+      "json.output.last_pinged",
+    ])
+}
 
 export const formatHostConfig = (res: object) => pick(res, ["json", "status"])
 
 export const formatHostInfo = (res: object) => omit(res, [...omitJson])
+
+export const formatTokens = (token: string) => truncate(token, { length: 15 })
+
+export const formatTokensArray = (res: [string, string]) => {
+  return [res[0], truncate(res[1], { length: 15 })]
+}
