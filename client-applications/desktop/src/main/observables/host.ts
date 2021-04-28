b6c6fa79e1304fee3f9335c7ef9cf60231b81538
Neil Hansen <neil.hansen.31@gmail.com>
2021-04-28 18:39:29 -0500

    Linter

--- client-applications/desktop/src/main/observables/host.ts
+++ client-applications/desktop/src/main/observables/host.ts
@@ -1,13 +1,13 @@
 import {
   userEmail,
   userAccessToken,
-  userConfigToken
-} from '@app/main/observables/user'
+  userConfigToken,
+} from "@app/main/observables/user"
 import {
   containerAssignPolling,
-  containerAssignFailure
-} from '@app/main/observables/container'
-import { loadingFrom, pollMap } from '@app/utils/observables'
+  containerAssignFailure,
+} from "@app/main/observables/container"
+import { loadingFrom, pollMap } from "@app/utils/observables"
 import {
   hostServiceInfo,
   hostServiceInfoValid,
@@ -17,10 +17,10 @@ import {
   hostServiceInfoSecret,
   hostServiceConfig,
   hostServiceConfigValid,
-  hostServiceConfigError
-} from '@app/utils/host'
-import { debugObservables, errorObservables } from '@app/utils/logging'
-import { from } from 'rxjs'
+  hostServiceConfigError,
+} from "@app/utils/host"
+import { debugObservables, errorObservables } from "@app/utils/logging"
+import { from } from "rxjs"
 import {
   map,
   take,
@@ -31,17 +31,17 @@ import {
   takeWhile,
   takeUntil,
   exhaustMap,
-  withLatestFrom
-} from 'rxjs/operators'
+  withLatestFrom,
+} from "rxjs/operators"
 import {
   formatHostConfig,
   formatHostInfo,
   formatObservable,
-  formatTokensArray
-} from '@app/utils/formatters'
+  formatTokensArray,
+} from "@app/utils/formatters"
 
 export const hostInfoRequest = containerAssignPolling.pipe(
-  skipWhile((res) => res?.json.state !== 'PENDING'),
+  skipWhile((res) => res?.json.state !== "PENDING"),
   take(1),
   withLatestFrom(userEmail, userAccessToken),
   map(([_, email, token]) => [email, token]),
@@ -56,7 +56,7 @@ export const hostInfoPolling = hostInfoRequest.pipe(
 )
 
 hostInfoPolling.subscribe((res) =>
-  console.log('host poll', res?.status, res?.json)
+  console.log("host poll", res?.status, res?.json)
 )
 
 export const hostInfoSuccess = hostInfoPolling.pipe(
@@ -79,7 +79,7 @@ export const hostConfigRequest = hostInfoSuccess.pipe(
   map((res) => [
     hostServiceInfoIP(res),
     hostServiceInfoPort(res),
-    hostServiceInfoSecret(res)
+    hostServiceInfoSecret(res),
   ]),
   withLatestFrom(userEmail, userConfigToken),
   map(([[ip, port, secret], email, token]) => [ip, port, secret, email, token])
@@ -109,14 +109,14 @@ export const hostConfigLoading = loadingFrom(
 // Logging
 
 debugObservables(
-  [formatObservable(hostInfoRequest, formatTokensArray), 'hostInfoRequest'],
-  [formatObservable(hostInfoSuccess, formatHostInfo), 'hostInfoSuccess'],
-  [hostConfigRequest, 'hostConfigRequest'],
-  [formatObservable(hostConfigProcess, formatHostConfig), 'hostConfigProcess'],
-  [formatObservable(hostConfigSuccess, formatHostConfig), 'hostConfigSuccess']
+  [formatObservable(hostInfoRequest, formatTokensArray), "hostInfoRequest"],
+  [formatObservable(hostInfoSuccess, formatHostInfo), "hostInfoSuccess"],
+  [hostConfigRequest, "hostConfigRequest"],
+  [formatObservable(hostConfigProcess, formatHostConfig), "hostConfigProcess"],
+  [formatObservable(hostConfigSuccess, formatHostConfig), "hostConfigSuccess"]
 )
 
 errorObservables(
-  [hostInfoFailure, 'hostInfoFailure'],
-  [hostConfigFailure, 'hostConfigFailure']
+  [hostInfoFailure, "hostInfoFailure"],
+  [hostConfigFailure, "hostConfigFailure"]
 )
--- client-applications/desktop/src/utils/logging.ts
+++ client-applications/desktop/src/utils/logging.ts
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Linter
 import { app } from "electron"
 import { tap } from "rxjs/operators"
 import { identity, truncate } from "lodash"
@@ -10,6 +13,7 @@ import logzio from "logzio-nodejs"
 import { merge, Observable } from "rxjs"
 import stringify from "json-stringify-safe"
 
+<<<<<<< HEAD
 import config, { loggingBaseFilePath } from "@app/config/environment"
 =======
 import { app } from 'electron'
@@ -25,12 +29,15 @@ import stringify from 'json-stringify-safe'
 
 import config, { loggingBaseFilePath } from '@app/utils/config'
 >>>>>>> linter
+=======
+import config, { loggingBaseFilePath } from "@app/utils/config"
+>>>>>>> Linter
 
 // Logging base function
 export enum LogLevel {
-  DEBUG = 'DEBUG',
-  WARNING = 'WARNING',
-  ERROR = 'ERROR',
+  DEBUG = "DEBUG",
+  WARNING = "WARNING",
+  ERROR = "ERROR",
 }
 
 // Initialize logz.io SDK
@@ -38,7 +45,7 @@ const logzLogger = logzio.createLogger({
   token: config.keys.LOGZ_API_KEY,
   bufferSize: 1,
   addTimestampWithNanoSecs: true,
-  sendIntervalMs: 50
+  sendIntervalMs: 50,
 })
 
 // Disable until we configure new logz.io endpoint
@@ -48,8 +55,8 @@ logzLogger.close()
 // Create the loggingBaseFilePath directory if it does not exist.
 const openLogFile = () => {
   fs.mkdirSync(loggingBaseFilePath, { recursive: true })
-  const logPath = path.join(loggingBaseFilePath, 'debug.log')
-  return fs.createWriteStream(logPath, { flags: 'a' })
+  const logPath = path.join(loggingBaseFilePath, "debug.log")
+  return fs.createWriteStream(logPath, { flags: "a" })
 }
 
 const logFile = openLogFile()
@@ -78,19 +85,19 @@ const logBase = (
   */
 
   const template = `DEBUG: ${title} -- \n ${
-    data !== undefined ? stringify(data, null, 2) : ''
+    data !== undefined ? stringify(data, null, 2) : ""
   }`
 
   const debugLog = truncate(template, {
     length: 1000,
-    omission: '...**logBase only prints 1000 characters per log**'
+    omission: "...**logBase only prints 1000 characters per log**",
   })
 
   if (app.isPackaged) {
     /* stop sending to logz until we configure new logz endpoint
     logzLogger.log({
       message: debugLog,
-      level: level
+      level: level,
     })
     */
   } else {
@@ -113,7 +120,7 @@ export const uploadToS3 = async (email: string) => {
 
   logBase(
     logFile,
-    'Logs upload to S3',
+    "Logs upload to S3",
     { s3FileName: s3FileName },
     LogLevel.DEBUG
   )
@@ -121,11 +128,11 @@ export const uploadToS3 = async (email: string) => {
   const uploadHelper = async (localFilePath: string) => {
     const accessKey = config.keys.AWS_ACCESS_KEY
     const secretKey = config.keys.AWS_SECRET_KEY
-    const bucketName = 'fractal-protocol-logs'
+    const bucketName = "fractal-protocol-logs"
 
     const s3 = new AWS.S3({
       accessKeyId: accessKey,
-      secretAccessKey: secretKey
+      secretAccessKey: secretKey,
     })
     // Read file into buffer
     const fileContent = fs.readFileSync(localFilePath)
@@ -133,7 +140,7 @@ export const uploadToS3 = async (email: string) => {
     const params = {
       Bucket: bucketName,
       Key: s3FileName,
-      Body: fileContent
+      Body: fileContent,
     }
     // Upload files to the bucket
     return await new Promise((resolve, reject) => {
@@ -150,9 +157,9 @@ export const uploadToS3 = async (email: string) => {
   const uploadPromises: Array<Promise<any>> = []
 
   const logLocations = [
-    path.join(loggingBaseFilePath, 'log-dev.txt'),
-    path.join(loggingBaseFilePath, 'log-staging.txt'),
-    path.join(loggingBaseFilePath, 'log.txt')
+    path.join(loggingBaseFilePath, "log-dev.txt"),
+    path.join(loggingBaseFilePath, "log-staging.txt"),
+    path.join(loggingBaseFilePath, "log.txt"),
   ]
 
   logLocations.forEach((filePath: string) => {
@@ -185,10 +192,10 @@ export const logObservable = (
     */
 
   return tap<any>({
-    next (value) {
+    next(value) {
       const data = identity(value)
       logBase(logFile, title, data, level)
-    }
+    },
   })
 }
 
