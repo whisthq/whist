029e9d38a7e42ab2c09a21e0b51190cdf9778090
Nicholas Pipitone <npip99@gmail.com>
2021-04-28 03:59:24 -0400

    Add comments to png.h

--- protocol/fractal/utils/png.h
+++ protocol/fractal/utils/png.h
@@ -21,20 +21,42 @@ Public Functions
 ============================
 */
 
+/**
+ * @brief                          Converts a given bmp into a png
+ *
+ * @param bmp_data                 The bmp data to convert
+ * @param bmp_size                 The size of the bmp data
+ * @param png_data                 *png_data will be the png data
+ * @param png_size                 *png_size will be the png's size
+ *
+ * @returns                        0 on success, -1 on failure
+ */
 int bmp_to_png(char* bmp_data, int bmp_size, char** png_data, int* png_size);
 
 /**
  * @brief                          Converts a given png into a bmp
- * 
- * @param png_data                 The png data
+ *
+ * @param png_data                 The png data to convert
  * @param png_size                 The size of the png data
- * @param bmp_data                 *bmp_data will be set to the bmp data
- * @param bmp_size                 *bmp_size will be set to the bmp's size
- * @returns 
+ * @param bmp_data                 *bmp_data will be the bmp data
+ * @param bmp_size                 *bmp_size will be the bmp's size
+ *
+ * @returns                        0 on success, -1 on failure
  */
 int png_to_bmp(char* png_data, int png_size, char** bmp_data, int* bmp_size);
 
+/**
+ * @brief                          Will free png_data returned by bmp_to_png
+ *
+ * @param png_data                 The png data to free
+ */
 void free_png(char* png_data);
+
+/**
+ * @brief                          Will free bmp_data returned by png_to_bmp
+ *
+ * @param bmp_data                 The bmp data to free
+ */
 void free_bmp(char* bmp_data);
 
 #endif
