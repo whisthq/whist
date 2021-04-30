78f87f058502821f1d5b00526f3daec4a6e8c281
djsavvy <sraghuvanshi@college.harvard.edu>
2021-04-30 14:55:06 -0400

    Use path.join correctly to find env.json in env.ts

--- client-applications/desktop/src/utils/env.ts
+++ client-applications/desktop/src/utils/env.ts
@@ -5,10 +5,8 @@ import { app } from "electron"
 const getEnv = () => {
   if (!app.isPackaged) return {}
 
-  const envFile = path
-    .join(app.getAppPath(), "env.json")
-    .replace("build/dist/main/", "")
-    .replace("app.asar/", "")
+  // If packaged, then app.getAppPath() points to app.asar
+  const envFile = path.join(app.getAppPath(), "..", "env.json")
 
   return JSON.parse(fs.readFileSync(envFile, "utf-8"))
 }
