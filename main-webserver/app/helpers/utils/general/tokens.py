3d366209f2d4c0e4218f107bdc1ee81eccab7658
Owen Niles <owen@fractal.co>
2021-04-30 12:23:31 -0400

    Remove all user management and authentication code from the web server.

--- main-webserver/app/helpers/utils/general/tokens.py
+++ main-webserver/app/helpers/utils/general/tokens.py
@@ -1,23 +1,7 @@
-import secrets
-
 from flask_jwt_extended import create_access_token, create_refresh_token
-
-
-def generate_private_key():
-    return secrets.token_hex(16)
 
 
 def get_access_tokens(username):
     access_token = create_access_token(identity=username, expires_delta=False)
     refresh_token = create_refresh_token(identity=username, expires_delta=False)
     return (access_token, refresh_token)
-
-
-def generate_token(username):
-    token = create_access_token(identity=username)
-    if len(token) > 15:
-        token = token[-15:]
-    else:
-        token = token[-1 * len(token) :]
-
-    return token
