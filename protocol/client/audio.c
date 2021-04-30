9cd948347ac9e3d1b9dd9255ea500e7299e257c0
jmather625 <jatinm2@illinois.edu>
2021-04-30 12:46:15 -0400

    streamlined more code

--- protocol/client/audio.c
+++ protocol/client/audio.c
@@ -331,9 +331,7 @@ void update_audio() {
         }
     }
 
-    int next_to_play_id;
-
-    next_to_play_id = last_played_id + 1;
+    int next_to_play_id = last_played_id + 1;
 
     if (next_to_play_id % MAX_NUM_AUDIO_INDICES != 0) {
         LOG_WARNING("NEXT TO PLAY ISN'T AT START OF AUDIO FRAME!");
