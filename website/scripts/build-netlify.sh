db74d39ad5bf482ae0533ca636b9f9f3d2a5740f
Roshan Padaki <roshan@fractal.co>
2021-04-30 21:48:13 -0400

    Remove netlify.toml ignore, add logging for future debugging

--- website/scripts/build-netlify.sh
+++ website/scripts/build-netlify.sh
@@ -2,4 +2,8 @@
 
 set -Eeuo pipefail
 
+echo $CACHED_COMMIT_REF $COMMIT_REF $PWD
+git diff $CACHED_COMMIT_REF $COMMIT_REF ||:
+echo $URL $BRANCH $HEAD $PULL_REQUEST
+
 export REACT_APP_VERSION=$COMMIT_REF && npm run build && rm -rf /opt/build/cache
