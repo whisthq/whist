69ec45e46fcf0654a9f062640ec637bf18b3d739
mingy98 <ming@fractal.co>
2021-05-01 01:30:45 -0400

    swap out links

--- website/src/shared/constants/config.ts
+++ website/src/shared/constants/config.ts
@@ -6,102 +6,96 @@ import type {
 } from "@app/shared/types/config"
 
 const environment: FractalEnvironment = {
-    local: {
-        url: {
-            WEBSERVER_URL: "http://127.0.0.1:7730",
-            FRONTEND_URL: "http://localhost:3000",
-            GRAPHQL_HTTP_URL: "https://dev-database.fractal.co/v1/graphql",
-            GRAPHQL_WS_URL: "wss://dev-database.fractal.co/v1/graphql",
-            TYPEFORM_URL: "https://tryfractal.typeform.com/to/nRa1zGFa",
-        },
-        keys: {
-            STRIPE_PUBLIC_KEY: import.meta.env
-                .REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
-            GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
-            GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
-        },
-        sentry_env: "development",
-        client_download_urls: {
-            macOS:
-                "https://fractal-chromium-macos-dev.s3.amazonaws.com/Fractal.dmg",
-            Windows:
-                "https://fractal-chromium-windows-dev.s3.amazonaws.com/Fractal.exe",
-        },
-        payment_enabled: true,
-        video_enabled: true,
+  local: {
+    url: {
+      WEBSERVER_URL: "http://127.0.0.1:7730",
+      FRONTEND_URL: "http://localhost:3000",
+      GRAPHQL_HTTP_URL: "https://dev-database.fractal.co/v1/graphql",
+      GRAPHQL_WS_URL: "wss://dev-database.fractal.co/v1/graphql",
+      TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
     },
-    development: {
-        url: {
-            WEBSERVER_URL: "https://dev-server.fractal.co",
-            FRONTEND_URL: "https://dev.fractal.co",
-            GRAPHQL_HTTP_URL: "https://dev-database.fractal.co/v1/graphql",
-            GRAPHQL_WS_URL: "wss://dev-database.fractal.co/v1/graphql",
-            TYPEFORM_URL: "https://tryfractal.typeform.com/to/nRa1zGFa",
-        },
-        keys: {
-            STRIPE_PUBLIC_KEY: import.meta.env
-                .REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
-            GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
-            GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
-        },
-        sentry_env: "development",
-        client_download_urls: {
-            macOS:
-                "https://fractal-chromium-macos-dev.s3.amazonaws.com/Fractal.dmg",
-            Windows:
-                "https://fractal-chromium-windows-dev.s3.amazonaws.com/Fractal.exe",
-        },
-        payment_enabled: true,
-        video_enabled: true,
+    keys: {
+      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
+      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
+      GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
     },
-    staging: {
-        url: {
-            WEBSERVER_URL: "https://staging-server.fractal.co",
-            FRONTEND_URL: "https://staging.fractal.co",
-            GRAPHQL_HTTP_URL: "https://staging-database.fractal.co/v1/graphql",
-            GRAPHQL_WS_URL: "wss://staging-database.fractal.co/v1/graphql",
-            TYPEFORM_URL: "https://tryfractal.typeform.com/to/nRa1zGFa",
-        },
-        keys: {
-            STRIPE_PUBLIC_KEY: import.meta.env
-                .REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
-            GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
-            GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
-        },
-        sentry_env: "staging",
-        client_download_urls: {
-            macOS:
-                "https://fractal-chromium-macos-staging.s3.amazonaws.com/Fractal.dmg",
-            Windows:
-                "https://fractal-chromium-windows-staging.s3.amazonaws.com/Fractal.exe",
-        },
-        video_enabled: false,
-        payment_enabled: false,
+    sentry_env: "development",
+    client_download_urls: {
+      macOS: "https://fractal-chromium-macos-dev.s3.amazonaws.com/Fractal.dmg",
+      Windows:
+        "https://fractal-chromium-windows-dev.s3.amazonaws.com/Fractal.exe",
     },
-    production: {
-        url: {
-            WEBSERVER_URL: "https://prod-server.fractal.co",
-            FRONTEND_URL: "https://fractal.co",
-            GRAPHQL_HTTP_URL: "https://prod-database.fractal.co/v1/graphql",
-            GRAPHQL_WS_URL: "wss://prod-database.fractal.co/v1/graphql",
-            TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
-        },
-        keys: {
-            STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_PROD_PUBLIC_KEY,
-            GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
-            GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
-        },
-        sentry_env: "production",
-        client_download_urls: {
-            macOS:
-                "https://fractal-chromium-macos-prod.s3.amazonaws.com/Fractal.dmg",
-            Windows:
-                "https://fractal-chromium-windows-base.s3.amazonaws.com/Fractal.exe",
-        },
-        payment_enabled: false,
-        video_enabled: false,
+    payment_enabled: true,
+    video_enabled: true,
+  },
+  development: {
+    url: {
+      WEBSERVER_URL: "https://dev-server.fractal.co",
+      FRONTEND_URL: "https://dev.fractal.co",
+      GRAPHQL_HTTP_URL: "https://dev-database.fractal.co/v1/graphql",
+      GRAPHQL_WS_URL: "wss://dev-database.fractal.co/v1/graphql",
+      TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
     },
-}
+    keys: {
+      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
+      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
+      GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
+    },
+    sentry_env: "development",
+    client_download_urls: {
+      macOS: "https://fractal-chromium-macos-dev.s3.amazonaws.com/Fractal.dmg",
+      Windows:
+        "https://fractal-chromium-windows-dev.s3.amazonaws.com/Fractal.exe",
+    },
+    payment_enabled: true,
+    video_enabled: true,
+  },
+  staging: {
+    url: {
+      WEBSERVER_URL: "https://staging-server.fractal.co",
+      FRONTEND_URL: "https://staging.fractal.co",
+      GRAPHQL_HTTP_URL: "https://staging-database.fractal.co/v1/graphql",
+      GRAPHQL_WS_URL: "wss://staging-database.fractal.co/v1/graphql",
+      TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
+    },
+    keys: {
+      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
+      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
+      GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
+    },
+    sentry_env: "staging",
+    client_download_urls: {
+      macOS:
+        "https://fractal-chromium-macos-staging.s3.amazonaws.com/Fractal.dmg",
+      Windows:
+        "https://fractal-chromium-windows-staging.s3.amazonaws.com/Fractal.exe",
+    },
+    video_enabled: false,
+    payment_enabled: false,
+  },
+  production: {
+    url: {
+      WEBSERVER_URL: "https://prod-server.fractal.co",
+      FRONTEND_URL: "https://fractal.co",
+      GRAPHQL_HTTP_URL: "https://prod-database.fractal.co/v1/graphql",
+      GRAPHQL_WS_URL: "wss://prod-database.fractal.co/v1/graphql",
+      TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
+    },
+    keys: {
+      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_PROD_PUBLIC_KEY,
+      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
+      GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
+    },
+    sentry_env: "production",
+    client_download_urls: {
+      macOS: "https://fractal-chromium-macos-prod.s3.amazonaws.com/Fractal.dmg",
+      Windows:
+        "https://fractal-chromium-windows-base.s3.amazonaws.com/Fractal.exe",
+    },
+    payment_enabled: false,
+    video_enabled: false,
+  },
+};
 
 const VALID_ENVS = new Set(["local", "development", "staging", "production"])
 
