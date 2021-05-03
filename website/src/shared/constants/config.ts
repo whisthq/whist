06bb72c2342a0112e541184cb37c06c96adab8bb
Roshan Padaki <roshan@fractal.co>
2021-05-03 14:14:18 -0400

    Fix/clean up environment setting in website

--- website/src/shared/constants/config.ts
+++ website/src/shared/constants/config.ts
@@ -6,39 +6,11 @@ import type {
 } from "@app/shared/types/config"
 
 const environment: FractalEnvironment = {
-  local: {
-    url: {
-      WEBSERVER_URL: "http://127.0.0.1:7730",
-      FRONTEND_URL: "http://localhost:3000",
-      GRAPHQL_HTTP_URL: "https://dev-database.fractal.co/v1/graphql",
-      GRAPHQL_WS_URL: "wss://dev-database.fractal.co/v1/graphql",
-      TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
-    },
-    keys: {
-      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
-      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
-      GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
-    },
-    sentry_env: "development",
-    client_download_urls: {
-      macOS: "https://fractal-chromium-macos-dev.s3.amazonaws.com/Fractal.dmg",
-      Windows:
-        "https://fractal-chromium-windows-dev.s3.amazonaws.com/Fractal.exe",
-    },
-    payment_enabled: true,
-    video_enabled: true,
-  },
   development: {
     url: {
-      WEBSERVER_URL: "https://dev-server.fractal.co",
-      FRONTEND_URL: "https://dev.fractal.co",
-      GRAPHQL_HTTP_URL: "https://dev-database.fractal.co/v1/graphql",
-      GRAPHQL_WS_URL: "wss://dev-database.fractal.co/v1/graphql",
       TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
     },
     keys: {
-      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
-      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
       GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
     },
     sentry_env: "development",
@@ -47,20 +19,12 @@ const environment: FractalEnvironment = {
       Windows:
         "https://fractal-chromium-windows-dev.s3.amazonaws.com/Fractal.exe",
     },
-    payment_enabled: true,
-    video_enabled: true,
   },
   staging: {
     url: {
-      WEBSERVER_URL: "https://staging-server.fractal.co",
-      FRONTEND_URL: "https://staging.fractal.co",
-      GRAPHQL_HTTP_URL: "https://staging-database.fractal.co/v1/graphql",
-      GRAPHQL_WS_URL: "wss://staging-database.fractal.co/v1/graphql",
       TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
     },
     keys: {
-      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_STAGING_PUBLIC_KEY,
-      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
       GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
     },
     sentry_env: "staging",
@@ -70,20 +34,12 @@ const environment: FractalEnvironment = {
       Windows:
         "https://fractal-chromium-windows-staging.s3.amazonaws.com/Fractal.exe",
     },
-    video_enabled: false,
-    payment_enabled: false,
   },
   production: {
     url: {
-      WEBSERVER_URL: "https://prod-server.fractal.co",
-      FRONTEND_URL: "https://fractal.co",
-      GRAPHQL_HTTP_URL: "https://prod-database.fractal.co/v1/graphql",
-      GRAPHQL_WS_URL: "wss://prod-database.fractal.co/v1/graphql",
       TYPEFORM_URL: "https://tryfractal.typeform.com/to/RsOsBSSu",
     },
     keys: {
-      STRIPE_PUBLIC_KEY: import.meta.env.REACT_APP_STRIPE_PROD_PUBLIC_KEY,
-      GOOGLE_CLIENT_ID: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
       GOOGLE_ANALYTICS_TRACKING_CODES: ["UA-180615646-1"],
     },
     sentry_env: "production",
@@ -92,30 +48,12 @@ const environment: FractalEnvironment = {
       Windows:
         "https://fractal-chromium-windows-base.s3.amazonaws.com/Fractal.exe",
     },
-    payment_enabled: false,
-    video_enabled: false,
   },
-};
-
-const VALID_ENVS = new Set(["local", "development", "staging", "production"])
-
-const LIVE_ENV = (() => {
-    if (!import.meta.env.REACT_APP_ENVIRONMENT) return "development"
-    if (!VALID_ENVS.has(import.meta.env.REACT_APP_ENVIRONMENT.toString()))
-        throw new Error(
-            "Environment variable REACT_APP_ENVIRONMENT must be one of: " +
-                Array.from(VALID_ENVS).join(", ") +
-                ". Received: " +
-                import.meta.env.REACT_APP_ENVIRONMENT
-        )
-    return import.meta.env.REACT_APP_ENVIRONMENT.toString()
-})()
+}
 
 export const config: FractalConfig = (() => {
-    if (import.meta.env.NODE_ENV !== "development")
-        return environment[LIVE_ENV as keyof FractalEnvironment]
-
-    if (import.meta.env.REACT_APP_USE_DOCKER) return environment.local
+    if (import.meta.env.MODE !== "development")
+        return environment[import.meta.env.FRACTAL_ENVIRONMENT as keyof FractalEnvironment]
 
     return environment.development
 })()
--- website/src/shared/types/config.ts
+++ website/src/shared/types/config.ts
@@ -1,14 +1,8 @@
 export interface FractalConfig {
   url: {
-    WEBSERVER_URL: string
-    FRONTEND_URL: string
-    GRAPHQL_HTTP_URL: string
-    GRAPHQL_WS_URL: string
     TYPEFORM_URL: string
   }
   keys: {
-    STRIPE_PUBLIC_KEY: string | undefined
-    GOOGLE_CLIENT_ID: string | undefined
     GOOGLE_ANALYTICS_TRACKING_CODES: string[]
   }
   sentry_env: string
@@ -16,12 +10,9 @@ export interface FractalConfig {
     macOS: string
     Windows: string
   }
-  payment_enabled: boolean
-  video_enabled: boolean
 }
 
 export interface FractalEnvironment {
-  local: FractalConfig
   development: FractalConfig
   staging: FractalConfig
   production: FractalConfig
