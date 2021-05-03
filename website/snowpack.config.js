3d977f8fc78d324050750a435534d1fc7e191213
Roshan Padaki <roshan@fractal.co>
2021-05-03 14:34:16 -0400

    Fix typos/coaelescing not being available on Node 12

--- website/snowpack.config.js
+++ website/snowpack.config.js
@@ -15,17 +15,29 @@ const buildEnv = {
     COMMIT_REF: process.env.COMMIT_REF,
 }
 
-const getBaseUrl = () => {
+const getBuildOptions = () => {
     // Return `{ baseUrl: "https://the.base.url" }` if the URL environment variable is
     // defined and this is not a Netlify deploy preview. Otherwise, return `{}`.
-    // Intended to be splatted as `...getBaseUrl()` into the `buildOptions`.
+    // Intended to used to generate `buildOptions` below.
     const netlifyUrl = buildEnv.URL
     const isNetlifyPreview = buildEnv.CONTEXT === "deploy-preview"
     
     if (!isNetlifyPreview && netlifyUrl) {
+        // `baseUrl` controls what replaces `%PUBLIC_URL%` in index.html; the default is `/`.
+        // Having a full path is important in order for OpenGraph previews to display the
+        // thumbnail, as OpenGraph requires a full path to assets.
         return { baseUrl: netlifyUrl }
     } else {
         return {}
+    }
+}
+
+const getFractalEnv = () => {
+    // Return environment variables to bake into our application as a dictionary;
+    // these can be imported via `import.meta.env`.
+    return {
+        FRACTAL_ENVIRONMENT: process.env.FRACTAL_ENVIRONMENT ? process.env.FRACTAL_ENVIRONMENT : "development",
+        FRACTAL_VERSION: buildEnv.COMMIT_REF ? buildEnv.COMMIT_REF : "local"
     }
 }
 
@@ -34,27 +46,24 @@ module.exports = {
         public: { url: "/", static: true },
         src: { url: "/dist" },
     },
+
+    // This simplifies import syntax in our React app
     alias: {
         "@app/assets": "./public/assets",
         "@app": "./src",
     },
+
     plugins: [
         "@snowpack/plugin-react-refresh",
         "@snowpack/plugin-dotenv",
         "@snowpack/plugin-typescript",
     ],
-    buildOptions: {
-        // This controls what replaces `%PUBLIC_URL%` in index.html; the default is `/`.
-        // Having a full path is important in order for OpenGraph previews to display the
-        // thumbnail, as OpenGraph requires a full path to assets.
-        ...getBaseUrl()
-    },
-    // This controls environment variables which can be used in the app via
-    // `import.meta.env`.
-    env: {
-        FRACTAL_ENVIRONMENT: process.env.FRACTAL_ENVIRONMENT ?? "development",
-        FRACTAL_VERSION: buildEnv.COMMIT_REF ?? "local",
-    },
 
+    buildOptions: getBuildOptions(),
+
+    // This controls environment variables which can be used in the app via `import.meta.env`
+    env: getFractalEnv(),
+
+    // This points all routes to our single-page app rooted at index.html
     routes: [{ match: "routes", src: ".*", dest: "/index.html" }],
 }
