5ffe99b39e7ea3b38e2cae27ec6a64d41c07b527
FishmanL <leor@fractal.co>
2021-05-03 09:24:54 -0400

    adding reasoning for py userdata file

--- main-webserver/app/helpers/utils/aws/ec2_userdata/no_ecs_userdata.py
+++ main-webserver/app/helpers/utils/aws/ec2_userdata/no_ecs_userdata.py
@@ -1,3 +1,9 @@
+"""
+This file/layer of indirection over the .sh file
+exists so the userdata can be brought in via an import
+(similar to the rest of our constants)
+"""
+
 import os
 
 with open(os.path.join(os.path.dirname(__file__), "no_ecs_userdata.sh"), "r") as f:
