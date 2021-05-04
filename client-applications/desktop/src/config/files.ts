d9314d900c4d7a2c7fe26cb650349655784bda3d
Jannie <jz3191@columbia.edu>
2021-05-04 15:43:36 -0700

    fix tray icon file paths in packaged app, and smol fix for client app version

--- client-applications/desktop/src/config/files.ts
+++ client-applications/desktop/src/config/files.ts
@@ -1,13 +1,11 @@
 import path from "path"
 import { app } from "electron"
+import config from "@app/config/environment"
+const { buildRoot } = config
 
-const deskTopFilePath = app.isPackaged
-  ? path.join(app.getAppPath(), "../..")
-  : path.join(app.getAppPath(), "../../..")
-
-const trayIconMac = "public/assets/images/trayIconBlackTemplate.png"
-const trayIconWindows = "public/assets/images/trayIconPurple.ico"
+const trayIconMac = "assets/images/trayIconBlackTemplate.png"
+const trayIconWindows = "assets/images/trayIconPurple.ico"
 export const trayIconPath =
   process.platform === "win32"
-    ? path.join(deskTopFilePath, trayIconWindows)
-    : path.join(deskTopFilePath, trayIconMac)
+    ? path.join(buildRoot, trayIconWindows)
+    : path.join(buildRoot, trayIconMac)
