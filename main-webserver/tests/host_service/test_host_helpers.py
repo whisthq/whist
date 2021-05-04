e93a8887a5223b37729266d18355a6f9c82a83be
FishmanL <leor@fractal.co>
2021-05-04 12:23:30 -0400

    modifying heartbeats per 2072

--- main-webserver/tests/host_service/test_host_helpers.py
+++ main-webserver/tests/host_service/test_host_helpers.py
@@ -15,7 +15,9 @@ def test_initial_auth_good():
     """
     Tests whether auth works on an empty db
     """
-    resp, resp_code = initial_instance_auth_helper("1.1.1.1", "test_instance_id", "us-east-1")
+    resp, resp_code = initial_instance_auth_helper(
+        "1.1.1.1", "test_instance_id", "g3.4x_large", "us-east-1", "test"
+    )
     resp_dict = resp.get_json()
     assert "AuthToken" in resp_dict.keys()
     assert resp_code == SUCCESS
@@ -28,7 +30,9 @@ def test_initial_auth_exists(bulk_instance):
     Tests whether auth fails if the instance already exists
     """
     bulk_instance(instance_name="test_instance_id")
-    resp, resp_code = initial_instance_auth_helper("1.1.1.1", "test_instance_id", "us-east-1")
+    resp, resp_code = initial_instance_auth_helper(
+        "1.1.1.1", "test_instance_id", "g3.4x_large", "us-east-1", "test"
+    )
     resp_dict = resp.get_json()
     assert (resp_dict, resp_code) == ({"status": BAD_REQUEST}, BAD_REQUEST)
 
@@ -39,9 +43,7 @@ def test_heartbeat_no_update(bulk_instance):
     """
     bulk_instance(instance_name="test_instance_id", auth_token="test_auth")
     time.sleep(3)
-    resp, resp_code = instance_heartbeat_helper(
-        "test_auth", "test_instance_id", 1024000, "test", False
-    )
+    resp, resp_code = instance_heartbeat_helper("test_auth", "test_instance_id", 1024000, False)
     resp_dict = resp.get_json()
     assert (resp_dict, resp_code) == ({"status": SUCCESS}, SUCCESS)
     inst = InstanceInfo.query.get("test_instance_id")
@@ -55,9 +57,7 @@ def test_heartbeat_wrong_key(bulk_instance):
     """
     bulk_instance(instance_name="test_instance_id", auth_token="test_auth")
     time.sleep(3)
-    resp, resp_code = instance_heartbeat_helper(
-        "test_aut", "test_instance_id", 1024000, "test", False
-    )
+    resp, resp_code = instance_heartbeat_helper("test_aut", "test_instance_id", 1024000, False)
     resp_dict = resp.get_json()
     assert (resp_dict, resp_code) == ({"status": NOT_FOUND}, NOT_FOUND)
     inst = InstanceInfo.query.get("test_instance_id")
@@ -71,9 +71,7 @@ def test_heartbeat_no_exist(bulk_instance):
     """
     bulk_instance(instance_name="test_instance_id", auth_token="test_auth")
     time.sleep(3)
-    resp, resp_code = instance_heartbeat_helper(
-        "test_auth", "test_instance_id2", 1024, "test", False
-    )
+    resp, resp_code = instance_heartbeat_helper("test_auth", "test_instance_id2", 1024, False)
     resp_dict = resp.get_json()
     assert (resp_dict, resp_code) == ({"status": NOT_FOUND}, NOT_FOUND)
     inst = InstanceInfo.query.get("test_instance_id")
@@ -85,9 +83,7 @@ def test_heartbeat_dying(bulk_instance):
     Tests that a dying heartbeat actually deletes the instance
     """
     bulk_instance(instance_name="test_instance_id", auth_token="test_auth")
-    resp, resp_code = instance_heartbeat_helper(
-        "test_auth", "test_instance_id", 1024000, "test", True
-    )
+    resp, resp_code = instance_heartbeat_helper("test_auth", "test_instance_id", 1024000, True)
     resp_dict = resp.get_json()
     assert (resp_dict, resp_code) == ({"status": SUCCESS}, SUCCESS)
     assert InstanceInfo.query.filter_by(instance_id="test_instance_id").one_or_none() is None
@@ -99,9 +95,7 @@ def test_heartbeat_updates(bulk_instance):
     """
     bulk_instance(instance_name="test_instance_id", auth_token="test_auth")
     time.sleep(3)
-    resp, resp_code = instance_heartbeat_helper(
-        "test_auth", "test_instance_id", 1025000, "test", False
-    )
+    resp, resp_code = instance_heartbeat_helper("test_auth", "test_instance_id", 1025000, False)
     resp_dict = resp.get_json()
     assert (resp_dict, resp_code) == ({"status": SUCCESS}, SUCCESS)
     inst = InstanceInfo.query.get("test_instance_id")
