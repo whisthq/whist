4856561fccb63a653bf3c8d3f85280d9d2d3b3c9
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-05-04 12:32:18 +0000

    add drag-tab support for sidekick and brave

--- container-images/base/config/awesome-rc.lua
+++ container-images/base/config/awesome-rc.lua
@@ -399,7 +399,7 @@ client.connect_signal("unmanage", function (c)
     -- If last closed client was Chrome, open a new tab to its window.
     --     By sending the new tab command to the window, we prevent a new
     --     instance of Chrome opening if the user has hit the tab "x".
-    if numclients == 0 and c.class == "Google-chrome" then
+    if numclients == 0 and (c.class == "Google-chrome" or c.class == "Brave-browser" or c.class == "Sidekick-browser") then
       os.execute("xdotool key --window " .. c.window .. " ctrl+t")
     end
     manage_taskbar_visibility()
