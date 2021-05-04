f2d34943a65d57f18f12f9ccdeeff71aaca0cd5f
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-05-04 16:20:04 +0000

    fix alt to ctrl

--- container-images/base/config/awesome-rc.lua
+++ container-images/base/config/awesome-rc.lua
@@ -396,11 +396,11 @@ end)
 
 client.connect_signal("unmanage", function (c)
     local numclients = compute_table_length(awful.screen.focused().all_clients)
-    -- If last closed client was Chrome, open a new tab to its window.
-    --     By sending the new tab command to the window, we prevent a new
+    -- If last closed client was Chrome, focus on the first (and only) tab in its window.
+    --     By sending the focus tab 1 command to the window, we prevent a new
     --     instance of Chrome opening if the user has hit the tab "x".
     if numclients == 0 and (c.class == "Google-chrome" or c.class == "Brave-browser" or c.class == "Sidekick-browser") then
-      os.execute("xdotool key --window " .. c.window .. " alt+1")
+      os.execute("xdotool key --window " .. c.window .. " ctrl+1")
     end
     manage_taskbar_visibility()
     ensure_client_is_not_offscreen(awful.client.getmaster())
