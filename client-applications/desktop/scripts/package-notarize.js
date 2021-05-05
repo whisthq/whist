ddcbf17cff2be2ec28495d668f35573ee202cdf5
djsavvy <sraghuvanshi@college.harvard.edu>
2021-05-04 20:50:28 -0400

    Fix almost all eslint suggestions

--- client-applications/desktop/scripts/package-notarize.js
+++ client-applications/desktop/scripts/package-notarize.js
@@ -3,21 +3,20 @@
 // version-handling logic.
 
 const helpers = require("./build-package-helpers")
-const yargs = require('yargs')
+const yargs = require("yargs")
 
 // We require the version argument for notarization-level testing so that at
 // least some of our argument handling is covered by CI as well.
 const argv = yargs(process.argv.slice(2))
   .version(false) // necessary to prevent mis-parsing of the `--version` arg we pass in
-  .option('version', {
-    description: "Set the version number of the client app for notarization testing.",
-    type: "string", 
+  .option("version", {
+    description:
+      "Set the version number of the client app for notarization testing.",
+    type: "string",
     requiresArg: true,
     demandOption: true,
   })
-  .help()
-  .argv
-
+  .help().argv
 
 helpers.reinitializeYarn()
 helpers.buildAndCopyProtocol()
