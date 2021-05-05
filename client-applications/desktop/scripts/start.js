9cde7c1649068e58c1b9d6d5780c77a4a43ee33a
djsavvy <sraghuvanshi@college.harvard.edu>
2021-05-04 20:50:28 -0400

    Fix almost all eslint suggestions

--- client-applications/desktop/scripts/start.js
+++ client-applications/desktop/scripts/start.js
@@ -1,6 +1,5 @@
 // Build the protocol, build tailwind, and run `snowpack dev`
 
-const { execCommand } = require("./execCommand")
 const helpers = require("./build-package-helpers")
 
 helpers.buildAndCopyProtocol()
