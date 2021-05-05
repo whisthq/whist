fe9515dd7ed01544a16adbf1f0dee312b8f5055c
Jatin Mathur <jatinm2@illinois.edu>
2021-05-05 14:47:40 -0400

    Protocol Client Audio Refactor (#1835)

--- protocol/cmake/CompilerOptions.cmake
+++ protocol/cmake/CompilerOptions.cmake
@@ -17,6 +17,7 @@ else() # GCC and Clang base flags
     "-Wno-missing-field-initializers"
     "-Wno-implicit-fallthrough" # We use switch/case fallthrough intentionally a lot, it should be allowed
     "-fno-common" # Error when two global variables have the same name, which would overlap them
+    "$<$<COMPILE_LANGUAGE:C>:-Wincompatible-pointer-types>"
     "$<$<STREQUAL:$<TARGET_PROPERTY:LINKER_LANGUAGE>,C>:-Werror=implicit-function-declaration>" # Error on implicit function declaration with C
     "$<$<BOOL:${CI}>:-Werror>"
     "$<$<CONFIG:DEBUG>:-Og;-g;-O0>"
