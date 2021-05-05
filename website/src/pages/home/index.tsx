c79005d224b32a700a2479e19237c9ccaf23a735
Jannie <jz3191@columbia.edu>
2021-05-05 12:54:48 -0700

    add dark mode button

--- website/src/pages/home/index.tsx
+++ website/src/pages/home/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FaMoon, FaSun } from 'react-icons/fa'
 import classNames from 'classnames'
 import { Switch, Route } from 'react-router-dom'
 
@@ -8,25 +7,11 @@ import { withContext } from '@app/shared/utils/context'
 
 import Header from '@app/shared/components/header'
 import Footer from '@app/shared/components/footer'
+import { DarkModeIcon } from '@app/shared/components/darkModeIcon'
 import Top from './top'
 import Middle from './middle'
 import Features from './features'
 import ActionPrompt from './bottom'
-
-const DarkModeIcon = (props: { dark: boolean, onClick: () => void }) => (
-  <div className="absolute">
-    <div
-      onClick={props.onClick}
-      className={classNames(
-        'fixed top-16 md:top-24 right-2 md:right-12 dark:bg-transparent',
-        'border border-transparent dark:border-gray-400 bg-blue-lightest',
-        'text-gray dark:text-gray-300 p-2 rounded cursor-pointer z-50'
-      )}
-    >
-      <div>{props.dark ? <FaSun /> : <FaMoon />}</div>
-    </div>
-  </div>
-)
 
 export const Chrome = () => {
   /*
