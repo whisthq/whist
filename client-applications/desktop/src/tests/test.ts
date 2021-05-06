8059cbbd011a2a8a80f83fe0e3e9c16f2ec70c6a
Steve Li <lithafnium2000@gmail.com>
2021-05-06 12:09:34 -0400

    configured babel and jest on client app

---
+++ client-applications/desktop/src/tests/test.ts
@@ -0,0 +1,14 @@
+import { filter, map, share, exhaustMap } from "rxjs/operators"
+import { loginProcess } from "@app/main/observables/login"
+
+// example test using observable, doesn't do anything right now
+
+describe("login tests", () => {
+  beforeEach(() => {
+    console.log("before each")
+  })
+  test("loginRequest test", (done) => {
+    loginProcess.subscribe((data) => console.log(data))
+    done()
+  })
+})
