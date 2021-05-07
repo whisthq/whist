b8620f5a3bdc926616f7a9808139cd22daf19a23
Neil Hansen <neil.hansen.31@gmail.com>
2021-05-07 06:59:26 -0700

    tailwindcss documentation

--- client-applications/desktop/tailwind.config.js
+++ client-applications/desktop/tailwind.config.js
@@ -1,12 +1,19 @@
 const colors = require("tailwindcss/colors")
 
 module.exports = {
+  // This "purge" list should reference any files that call our .css files by
+  // name. This includes html, and any js files that 'import' css.
+  // This will allow tailwind to analyze and "purge" unused css.
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  // We may be able to remove "darkModeVariant" soon with tailwind JIT.
   experimental: {
     darkModeVariant: true,
   },
   darkMode: "class",
+  // The theme section is where we define anything related to visual design.
   theme: {
+    // The extend sections lets us add extra "variants" to utilities. We also
+    // override some of the default utilities.
     extend: {
       fontFamily: {
         sans: ["Josefin Sans"],
@@ -33,6 +40,7 @@ module.exports = {
       boxShadow: {
         bright: "0px 4px 50px rgba(255,255,255,0.25)",
       },
+      // These "literals" below can be removed soom with tailwind JIT.
       top: {
         "100px": "100px",
         "150px": "150px",
@@ -61,6 +69,9 @@ module.exports = {
         "250px": "250px",
         "300px": "300px",
       },
+      // We can add custom animation utilities using similiar syntax to
+      // CSS animation definitions. First we definite custom keyframe ranges,
+      // and we can reference these later in the actual animation utiltities.
       keyframes: {
         bounce: {
           "0%, 20%, 50%, 80%, 100%": { transform: "translateY(0)" },
@@ -72,6 +83,8 @@ module.exports = {
           "50%": { opacity: 0 },
         },
       },
+      // These are the utilities that we'll actually call in the program.
+      // Note how they reference the classes defined in the keyframes section.
       animation: {
         bounce: "bounce 5s infinite",
         blink: "blink 1s step-end infinite",
