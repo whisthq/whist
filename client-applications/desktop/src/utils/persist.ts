ad1e9e84e7426f7b154334f052550cb1a42abd21
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2021-05-07 13:43:15 -0700

    Client App utils docs for windows + ipc + persist + observables (#2198)
    * utils/ipc docs
    
    * utils/persist docs
    
    * utils/observables docs
    
    * utils/windows docs
    
    * typo
    
    * typo

--- client-applications/desktop/src/utils/persist.ts
+++ client-applications/desktop/src/utils/persist.ts
@@ -1,3 +1,13 @@
+// This file manages the low-level details of how we'll persist user data in
+// between Electron sessions. From the perspective of the rest of the app,
+// we should just able to call these functions and not worry about any of the
+// implementation details.
+//
+// Persistence requires access to the file system, so it can only be used by
+// the main process.
+//
+// Only serializable data can be persisted. Essentially, anything that can be
+// converted to JSON.
 import Store from "electron-store"
 import { get } from "lodash"
 
@@ -5,6 +15,9 @@ import { StateIPC } from "@app/@types/state"
 
 export const store = new Store({ watch: true })
 
+// This is the function that should most often be used to persist data.
+// Just pass it an object, and as long as its values are serializable to JSON
+// it will save.
 export const persist = (obj: Partial<StateIPC>) => {
   for (const key in obj) {
     const value = get(obj, key, "")
@@ -12,6 +25,10 @@ export const persist = (obj: Partial<StateIPC>) => {
   }
 }
 
+// This is a helper function to use when you want to save only a subset of an
+// object. It's useful when dealing with an object that may have null or
+// undefined values. electron-store prefers that you delete keys instead of
+// assigning them to undefined.
 export const persistKeys = (obj: Partial<StateIPC>, ...keys: string[]) => {
   for (const key of keys) {
     const value = get(obj, key, "")
@@ -21,6 +38,8 @@ export const persistKeys = (obj: Partial<StateIPC>, ...keys: string[]) => {
   }
 }
 
+// With no arguments, delete everything in persistence. We should extend this
+// function to accept arguments so that we can delete specific keys.
 export const persistClear = () => {
   store.clear()
 }
--- client-applications/desktop/src/utils/windows.ts
+++ client-applications/desktop/src/utils/windows.ts
@@ -1,3 +1,6 @@
+// This file manages creation of renderer process windows. It is called from the
+// main process, and passes all the configuration needed to load files into
+// Electron renderer windows.
 import path from "path"
 import { app, BrowserWindow, BrowserWindowConstructorOptions } from "electron"
 import {
@@ -14,6 +17,11 @@ import config, { FractalCIEnvironment } from "@app/config/environment"
 
 const { buildRoot } = config
 
+// Because we need to make windows of all kinds of shapes and sizes, we're
+// predefining some sizes here to help with consistency. The actual windows that
+// we create will be compositions of the "base" config + "width" and "height"
+// values. There's no particular reason why we multiply these values by 16,
+// it just makes the sizing scale a little more readable.
 const base = {
   webPreferences: { preload: path.join(buildRoot, "preload.js") },
   resizable: false,
@@ -71,6 +79,9 @@ export const getWindowTitle = () => {
   return `Fractal (${deployEnv})`
 }
 
+// This is a "base" window creation function. It won't be called directly from
+// the application, instead we'll use it to contain the common functionality
+// that we want to share between all windows.
 export const createWindow = (
   show: string,
   options: Partial<BrowserWindowConstructorOptions>,
@@ -78,10 +89,22 @@ export const createWindow = (
   onClose?: (win: BrowserWindow) => any
 ) => {
   const { title } = config
+  // We don't want to show a new BrowserWindow right away. We should manually
+  // show the window after it has finished loading.
   const win = new BrowserWindow({ ...options, show: false, title })
 
+  // Electron doesn't have a API for passing data to renderer windows. We need
+  // to pass "init" data for a variety of reasons, but mainly so we can decide
+  // which React component to render into the window. We're forced to do this
+  // using query parameters in the URL that we pass. The alternative would
+  // be to use separate index.html files for each window, which we want to avoid.
   const params = "?show=" + show
 
+  // We need to load index.html differently if we're developing. We want to
+  // use Snowpack's development server to manage our reload, so we load from
+  // the server's localhost port as opposed to our build folder.
+  // Electron's API for passing query parameters is inconsistent, so we must
+  // be careful to pass them correctly for each environment.
   if (app.isPackaged) {
     win
       .loadFile("build/index.html", { search: params })
@@ -90,11 +113,16 @@ export const createWindow = (
     win
       .loadURL("http://localhost:8080" + params)
       .then(() => {
+        // We manually open devTools, because we want to make sure that
+        // both the main/renderer processes are in a "ready" state before we
+        // show.
         win.webContents.openDevTools({ mode: "undocked" })
       })
       .catch((err) => console.error(err))
   }
 
+  // We accept some callbacks in case the caller needs to run some additional
+  // functions on open/close.
   win.webContents.on("did-finish-load", () =>
     onReady != null ? onReady(win) : win.show()
   )
@@ -102,6 +130,14 @@ export const createWindow = (
 
   return win
 }
+
+// These functions below are the ones we'll actually use to create windows in
+// the application. They're preconfigured with sizing settings, as well as the
+// parameters required to render the correct React component. We pass params
+// through the "WindowHash..." objects that we've imported in this file.
+// This allows us to contain the complexity of window configuration to this file.
+// The rest of the application doesn't need to know anything about how to
+// configure an Electron window.
 
 export const createAuthWindow: CreateWindowFunction = () =>
   createWindow(WindowHashAuth, {
