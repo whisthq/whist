62ae0a392a49ae4d9fd75e44237de9daac42c50a
Neil Hansen <neil.hansen.31@gmail.com>
2021-05-10 16:34:37 -0700

    Update container logging message

--- client-applications/desktop/src/main/observables/container.ts
+++ client-applications/desktop/src/main/observables/container.ts
@@ -19,64 +19,83 @@ import {
   userConfigToken,
 } from "@app/main/observables/user"
 import { eventUpdateNotAvailable } from "@app/main/events/autoupdate"
-import { containerPollingTimeout } from "@app/utils/constants"
-import { loadingFrom, pollMap } from "@app/utils/observables"
-import { from, of, zip, combineLatest } from "rxjs"
+import { loadingFrom } from "@app/utils/observables"
+import { from, of, zip, combineLatest, interval, merge } from "rxjs"
 import {
   map,
-  delay,
   takeUntil,
-  withLatestFrom,
-  takeWhile,
   switchMap,
+  take,
+  repeatWhen,
+  withLatestFrom,
 } from "rxjs/operators"
-import { gates } from "@app/utils/gates"
+import { gates, Flow } from "@app/utils/gates"
+
+const containerCreateGates: Flow = (name, trigger) =>
+  gates(
+    name,
+    trigger.pipe(
+      switchMap(([email, token]) => from(containerCreate(email, token)))
+    ),
+    {
+      success: (req: { json: { ID: string } }) => (req?.json?.ID ?? "") !== "",
+      failure: (req: { json: { ID: string } }) => (req?.json?.ID ?? "") === "",
+    }
+  )
+
+const containerInfoGates: Flow = (name, trigger) =>
+  gates(
+    name,
+    trigger.pipe(switchMap(([id, token]) => from(containerInfo(id, token)))),
+    {
+      success: (result) => containerInfoSuccess(result),
+      pending: (result) => containerInfoPending(result),
+      failure: (result) => containerInfoError(result),
+    }
+  )
+
+const containerPollingInner: Flow = (name, trigger) => {
+  const tick = trigger.pipe(repeatWhen(() => interval(1000)))
+  const poll = containerInfoGates(name, tick)
+
+  return {
+    pending: poll.pending.pipe(takeUntil(merge(poll.success, poll.failure))),
+    success: poll.success.pipe(take(1)),
+    failure: poll.failure.pipe(take(1)),
+  }
+}
+
+const containerPollingFlow: Flow = (name, trigger) => {
+  const poll = trigger.pipe(
+    map(([res, token]) => [res.json.id, token]),
+    map((args) => containerPollingInner(name, of(args)))
+  )
+
+  const success = poll.pipe(switchMap((inner) => inner.success))
+  const failure = poll.pipe(switchMap((inner) => inner.success))
+  const pending = poll.pipe(switchMap((inner) => inner.pending))
+  const loading = loadingFrom(trigger, success, failure)
+
+  return { success, failure, pending, loading }
+}
 
 export const {
   success: containerCreateSuccess,
   failure: containerCreateFailure,
   warning: containerCreateWarning,
-} = gates(
+} = containerCreateGates(
   `containerCreate`,
   combineLatest([
     zip(userEmail, userAccessToken, userConfigToken),
     eventUpdateNotAvailable,
-  ]).pipe(
-    map(([auth]) => auth), //discard the eventUpdateNotAvailable
-    switchMap(([email, token]) => from(containerCreate(email, token)))
-  ),
-  {
-    success: (req: { json: { ID: string } }) => (req?.json?.ID ?? "") !== "",
-    failure: (req: { json: { ID: string } }) => (req?.json?.ID ?? "") === "",
-  }
+  ])
 )
 
 export const {
   success: containerPollingSuccess,
   failure: containerPollingFailure,
-} = gates(
+  loading: containerPollingLoading,
+} = containerPollingFlow(
   `containerPolling`,
-  containerCreateSuccess.pipe(
-    withLatestFrom(userAccessToken),
-    pollMap(
-      1000,
-      async ([response, token]) => await containerInfo(response.json.ID, token)
-    ),
-    takeWhile((res) => containerInfoPending(res), true),
-    takeWhile((res) => !containerInfoError(res), true),
-    takeUntil(of(true).pipe(delay(containerPollingTimeout)))
-  ),
-  {
-    success: (res) => containerInfoSuccess(res),
-    failure: (res) =>
-      containerInfoError(res) ||
-      containerInfoPending(res) ||
-      !containerInfoSuccess(res),
-  }
-)
-
-export const containerPollingLoading = loadingFrom(
-  containerCreateSuccess.pipe(withLatestFrom(userAccessToken)),
-  containerPollingSuccess,
-  containerPollingFailure
+  containerCreateSuccess.pipe(withLatestFrom(userAccessToken))
 )
