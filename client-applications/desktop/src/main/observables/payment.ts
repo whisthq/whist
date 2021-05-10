2405f6900ec4363ed9c3308077f9582819243ec9
Steve Li <lithafnium2000@gmail.com>
2021-05-10 13:04:28 -0400

    started using action for payments
    changed payments to work with action
    
    fixed linting errors

--- client-applications/desktop/src/main/observables/payment.ts
+++ client-applications/desktop/src/main/observables/payment.ts
@@ -1,4 +1,7 @@
-import { fromEventIPC } from "@app/main/events/ipc"
+// This file contains all the observables that manage the stripe payment logic
+// in the renderer threadId. They listen to IPC events and communicate with
+// the webserver to gain the necessary stripe info to relay back to the renderer thread.
+
 import { from, merge } from "rxjs"
 import { debugObservables, errorObservables } from "@app/utils/logging"
 import {
@@ -9,9 +12,15 @@ import {
   stripePortalValid,
   stripePortalError,
 } from "@app/utils/payment"
-import { filter, map, share, exhaustMap } from "rxjs/operators"
+import { filter, map, share, exhaustMap, tap } from "rxjs/operators"
+import {
+  stripeCheckoutAction,
+  stripePortalAction,
+} from "@app/main/events/actions"
 
-export const stripeCheckoutRequest = fromEventIPC("stripeCheckoutRequest").pipe(
+// Listens for a checkout portal request from the main IPC channel
+export const stripeCheckoutRequest = stripeCheckoutAction.pipe(
+  tap((req) => console.log(req)),
   filter(
     (req) => (req?.customerId ?? "") !== "" && (req?.priceId ?? "") !== ""
   ),
@@ -24,6 +33,8 @@ export const stripeCheckoutRequest = fromEventIPC("stripeCheckoutRequest").pipe(
   share()
 )
 
+// Waits for a webserver call to get the stripe checkout id to send
+// back to the renderer thread
 export const stripeCheckoutProcess = stripeCheckoutRequest.pipe(
   map(
     async ([customerId, priceId, successUrl, cancelUrl]) =>
@@ -33,15 +44,18 @@ export const stripeCheckoutProcess = stripeCheckoutRequest.pipe(
   share()
 )
 
+// Emits upon webserver call checkout portal success
 export const stripeCheckoutSuccess = stripeCheckoutProcess.pipe(
   filter((res) => stripeCheckoutValid(res))
 )
 
+// Emits upon webserver call checkout portal failure
 export const stripeCheckoutFailure = stripeCheckoutProcess.pipe(
   filter((res) => stripeCheckoutError(res))
 )
 
-export const stripePortalRequest = fromEventIPC("stripePortalRequest").pipe(
+// Listens for a customer portal request from the main IPC channel
+export const stripePortalRequest = stripePortalAction.pipe(
   filter(
     (req) => (req?.customerId ?? "") !== "" && (req?.returnUrl ?? "") !== ""
   ),
@@ -49,6 +63,8 @@ export const stripePortalRequest = fromEventIPC("stripePortalRequest").pipe(
   share()
 )
 
+// Waits for a webserver call to get the customer portal URL to send
+// back to the renderer thread.
 export const stripePortalProcess = stripePortalRequest.pipe(
   map(
     async ([customerId, returnUrl]) =>
@@ -58,14 +74,19 @@ export const stripePortalProcess = stripePortalRequest.pipe(
   share()
 )
 
+// Emits upon webserver call customer portal success
 export const stripePortalSuccess = stripePortalProcess.pipe(
   filter((res) => stripePortalValid(res))
 )
 
+// Emits upon webserver call customer portal failure
 export const stripePortalFailure = stripePortalProcess.pipe(
   filter((res) => stripePortalError(res))
 )
 
+// Combines both checkout and customer portal action into a single observable
+// to send to the renderer thread using merge(), which allows the renderer
+// thread to parse the returned object accordingly.
 export const stripeAction = merge(
   stripeCheckoutSuccess.pipe(
     map((req) => ({ action: "CHECKOUT", stripeCheckoutId: req.json.sessionId }))
@@ -74,8 +95,6 @@ export const stripeAction = merge(
     map((req) => ({ action: "PORTAL", stripePortalUrl: req.json.url }))
   )
 )
-
-// IDEA: merge the two successes together
 
 debugObservables(
   [stripeCheckoutRequest, "stripeCheckoutRequest"],
