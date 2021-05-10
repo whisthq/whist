951ce9ee97f0940b1066458eef205145d8d9f9fe
FishmanL <leor@fractal.co>
2021-05-10 17:12:13 -0400

    adding docs

--- main-webserver/app/helpers/helpers_tests/aws_tests/test_ec2_client.py
+++ main-webserver/app/helpers/helpers_tests/aws_tests/test_ec2_client.py
@@ -11,15 +11,24 @@ def test_single() -> None:
     Returns:
         None
     """
+    # first, we test that instances start as expected
     ec2_client = EC2Client()
     up_start = time.time()
     ids = ec2_client.start_instances("ami-037b96e43364db32c")
     # it takes a moment for AWS to recognize these instances exist
     time.sleep(5)
+
+    # check that only one instance started, and that the req is async
     assert len(ids) == 1
     assert time.time() - up_start < 20, "start should not be blocking"
+
+    # Now, poll til it's live
     ec2_client.spin_til_instances_up(ids)
+
+    # And check that the instance is live
     assert ids[0] in ec2_client.get_ip_of_instances(ids).keys()
+
+    # Now, we test stop in the same way we tested start
     down_start = time.time()
     ec2_client.stop_instances(ids)
     assert time.time() - down_start < 20, "stop should not be blocking"
