3b4a914252db319cb31aab824e37b505bb3cc249
FishmanL <leor@fractal.co>
2021-05-10 11:30:31 -0400

    writing up basic tests

---
+++ main-webserver/app/helpers/helpers_tests/aws_tests/test_ec2_client.py
@@ -0,0 +1,27 @@
+import time
+
+import pytest
+
+from app.helpers.utils.aws.base_ec2_client import EC2Client
+
+
+def test_single() -> None:
+    """
+    Tests that starting an instance works as expected
+    Returns:
+        None
+    """
+    ec2_client = EC2Client()
+    up_start = time.time()
+    ids = ec2_client.start_instances("ami-037b96e43364db32c")
+    assert len(ids) == 1
+    assert time.time() - up_start < 20, "start should not be blocking"
+    ec2_client.spin_til_instances_up(ids)
+    assert ids[0] in ec2_client.get_ip_of_instances(ids).keys()
+    down_start = time.time()
+    ec2_client.stop_instances(ids)
+    assert time.time() - down_start < 20, "stop should not be blocking"
+    ec2_client.spin_til_instances_down(ids)
+    assert ec2_client.check_if_instances_down(ids)
+    with pytest.raises(Exception, match="instances must be up to check IPs"):
+        ec2_client.get_ip_of_instances(ids)
