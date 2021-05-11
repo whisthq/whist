401130e9effb494b2406f7025e5c6fa7f23dadf6
Neil Hansen <neil.hansen.31@gmail.com>
2021-05-11 11:57:13 -0700

    Pushing the flows file

---
+++ client-applications/desktop/src/utils/flows.ts
@@ -0,0 +1,49 @@
+import { Observable } from "rxjs"
+import { filter, share, tap } from "rxjs/operators"
+import { mapValues, truncate } from "lodash"
+import stringify from "json-stringify-safe"
+
+const logFormat = (...args: any[]) => {
+  let [title, message, value] = args
+  if (value === undefined) {
+    value = message
+    message = undefined
+  }
+  if (value === undefined && message === undefined) {
+    value = title
+    title = undefined
+  }
+  title = title ? `${title} -- ` : ""
+  message = message ? `${message} -- ` : ""
+
+  let output = truncate(stringify(value, null, 2), {
+    length: 1000,
+    omission: "...**only printing 1000 characters per log**",
+  })
+  return `${title}${message}${output}`
+}
+
+const logDebug = (...args: any[]) => {
+  console.log(`DEBUG: ${logFormat(...args)}`)
+}
+
+export const fork = <T>(
+  source: Observable<T>,
+  filters: { [gate: string]: (result: T) => boolean }
+) => {
+  const shared = source.pipe(share())
+  return mapValues(filters, (fn) => shared.pipe(filter(fn)))
+}
+export const flow = <A>(
+  name: string,
+  fn: (
+    childName: string,
+    trigger: Observable<A>
+  ) => { [key: string]: Observable<any> }
+) => (childName: string, trigger: Observable<A>) =>
+  mapValues(fn(`${name}.${childName}`, trigger), (obs, key) =>
+    obs.pipe(
+      tap((value) => logDebug(`${name}.${childName}.${key}`, value)),
+      share()
+    )
+  )
