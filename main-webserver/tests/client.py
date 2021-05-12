38b9c573e5d48dbabb9f25fc06de62522785c635
Owen Niles <owen@fractal.co>
2021-05-12 11:16:54 -0400

    Add an admin argument to the FractalAPITestClient.login() method.

--- main-webserver/tests/client.py
+++ main-webserver/tests/client.py
@@ -7,17 +7,23 @@ from flask_jwt_extended import create_access_token
 class FractalAPITestClient(FlaskClient):
     """A custom test client class that makes it easy to authenticate requests."""
 
-    def login(self, username):
+    def login(self, username, *, admin=False):
         """Authenticate all future requests as the user with the specified username.
 
         Args:
             username: The user ID of the user as whom to authenticate all future requests.
+            admin: Optional. A boolean indicating whether or not the authenticated user should have
+                have access to administrative endpoints.
 
         Returns:
             None
         """
 
-        access_token = create_access_token(identity=username, expires_delta=False)
+        additional_claims = {"scope": "admin"} if admin else {}
+        access_token = create_access_token(
+            identity=username, additional_claims=additional_claims, expires_delta=False
+        )
+
         self.environ_base["HTTP_AUTHORIZATION"] = f"Bearer {access_token}"
 
     def logout(self):
