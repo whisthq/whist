1b26e713e276fb0b42c07dd3dc2ce04ed7121a74
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2021-05-18 15:58:25 -0400

    Revert "only send pinch events if not scrolling"
    This reverts commit ccfdd8456f592b69251b483e672c29c92254e647.

--- protocol/client/sdl_event_handler.c
+++ protocol/client/sdl_event_handler.c
@@ -53,8 +53,6 @@ extern bool multigesture_active;
 
 extern bool audio_refresh;
 
-bool active_scroll = false;
-
 /*
 ============================
 Private Functions
@@ -325,8 +323,6 @@ int handle_mouse_wheel(SDL_Event *event) {
             (int): 0 on success
     */
 
-    active_scroll = true;
-
     FractalClientMessage fmsg = {0};
     fmsg.type = MESSAGE_MOUSE_WHEEL;
     fmsg.mouseWheel.x = event->wheel.x;
@@ -351,15 +347,14 @@ int handle_multi_gesture(SDL_Event *event) {
     // LOG_INFO("multigesture detected!! d_theta: %f d_dist: %f, x: %d, y: %d, num_fingers: %u",
     //     event->mgesture.dTheta, event->mgesture.dDist, event->mgesture.x, event->mgesture.y, event->mgesture.numFingers);
 
-    // If not scrolling and detected pinch, then populate fmsg to send pinch event to server
-    if (!active_scroll && fabs(event->mgesture.dDist) > 10.0 / ((float) output_width)) {
+    if (fabs(event->mgesture.dDist) > 10.0 / ((float) output_width)) {
         multigesture_active = true;
         if (event->mgesture.dDist > 0) {
             current_gesture_type = PINCH_OPEN;
-            LOG_INFO("START PINCH OPEN - %f > %f, %d, %d", event->mgesture.dDist, 10.0 / ((float) output_width), output_width, output_height);
+            LOG_INFO("START PINCH OPEN - %f, %d, %d", 10.0 / ((float) output_width), output_width, output_height);
         } else {
             current_gesture_type = PINCH_CLOSE;
-            LOG_INFO("START PINCH CLOSE - %f > %f, %d, %d", event->mgesture.dDist, 10.0 / ((float) output_width), output_width, output_height);
+            LOG_INFO("START PINCH CLOSE");
         }
     // } else if (!multigesture_active) {
     } else {
@@ -395,11 +390,10 @@ int handle_touch_up(SDL_Event *event) {
 
     send_fmsg(&fmsg);
 
-    // the multigesture or scroll has ended
-    if (multigesture_active || active_scroll) {
+    // the multigesture has ended
+    if (multigesture_active) {
         LOG_INFO("RELEASED GESTURE");
         multigesture_active = false;
-        active_scroll = false;
     }
 
     return 0;
