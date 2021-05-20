36d2019b1745693cefb9853eccc929297ba3c64e
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2021-05-20 11:45:16 -0400

    Lint/format python code in container-images (#2377)

--- container-images/build_container_image.sh
+++ container-images/build_container_image.sh
@@ -18,19 +18,19 @@ if [[ "$mount" == "mount" ]]; then
     # If no protocol exists, copy the current one, so that the base Dockerfile doesn't complain about it not existing
     # We intentionally don't update this, when mounting, so that the Docker image doesn't forcefully rebuild
     if [[ ! -d "base/build-temp/protocol" ]]; then
-        ./helper-scripts/copy_protocol_build.sh
+        ./helper_scripts/copy_protocol_build.sh
     fi
 else
     # Otherwise, we copy_protocol_build.sh directly into the docker file
 
     # If copying the protocol build fails,
     # try compiling first and then copying again
-    if ./helper-scripts/copy_protocol_build.sh ; then
+    if ./helper_scripts/copy_protocol_build.sh ; then
         echo "A protocol build exists, though it is not guaranteed to be up-to-date."
     else
         echo "Attempting to copy an existing protocol build failed with the above error. Building ourselves a fresh copy..."
         ../protocol/build_protocol.sh Release
-        ./helper-scripts/copy_protocol_build.sh
+        ./helper_scripts/copy_protocol_build.sh
     fi
 fi
 
@@ -38,7 +38,7 @@ fi
 mkdir -p "base/build-temp/nvidia-driver"
 ../ecs-host-setup/get-nvidia-driver-installer.sh && mv nvidia-driver-installer.run base/build-temp/nvidia-driver
 
-python3 ./helper-scripts/build_container_image.py "$@"
+python3 ./helper_scripts/build_container_image.py "$@"
 
 echo "Cleaning up..."
 rm -rf "base/build-temp"
