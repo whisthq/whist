f0f5f3bcb5a26e2c6e7d802f97ea0e2df66d89ae
djsavvy <sraghuvanshi@college.harvard.edu>
2021-05-19 22:18:28 -0400

    Rename variables to make pylint happy

--- container-images/testing_scripts/connection_tester.py
+++ container-images/testing_scripts/connection_tester.py
@@ -8,7 +8,7 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 # TCP_ports = range(32273, 32282)
 # conversation_port = 32262
 
-test_port = 32263
+TEST_PORT = 32263
 
 
 def server():
@@ -16,28 +16,27 @@ def server():
     # conversation_socket.bind(('', conversation_port))
 
     test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    test_socket.bind(("", test_port))
-    startTime = time.time()
+    test_socket.bind(("", TEST_PORT))
+    start_time = time.time()
 
-    while time.time() - startTime < 30.0:
+    while time.time() - start_time < 30.0:
         data, address = test_socket.recvfrom(4096)
         print("Received %d bytes from %s" % (len(data), address))
         if len(data) > 0:
-            timeDelay = current_milli_time() - int.from_bytes(data, byteorder="big")
-            print("Time delay is %d" % timeDelay)
+            time_delay = current_milli_time() - int.from_bytes(data, byteorder="big")
+            print("Time delay is %d" % time_delay)
 
     print("Been doing this for 30 seconds, retiring")
     test_socket.close()
-    return
 
 
 def client(dest):
     test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    startTime = time.time()
-    while time.time() - startTime < 12.0:
+    start_time = time.time()
+    while time.time() - start_time < 12.0:
         print("Sending data...")
         test_socket.sendto(
-            current_milli_time().to_bytes(8, byteorder="big"), (dest, test_port)
+            current_milli_time().to_bytes(8, byteorder="big"), (dest, TEST_PORT)
         )
         time.sleep(1)
 
