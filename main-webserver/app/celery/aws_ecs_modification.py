5c3fb26ea9acc7122a623cb30255eed404b7da0d
FishmanL <leor@fractal.co>
2021-05-24 14:38:36 -0400

    don't update test clusters

--- main-webserver/app/celery/aws_ecs_modification.py
+++ main-webserver/app/celery/aws_ecs_modification.py
@@ -186,11 +186,11 @@ def update_region(
     )
 
     all_clusters = list(ClusterInfo.query.filter_by(location=region_name).all())
-    all_clusters = [
-        cluster
-        for cluster in all_clusters
-        if "cluster" in cluster.cluster and "test" not in cluster.cluster
-    ]
+    all_clusters = [cluster for cluster in all_clusters if "cluster" in cluster.cluster]
+
+    if not current_app.testing:
+        # we don't want to update people's personal test clusters
+        all_clusters = [cluster for cluster in all_clusters if "test" not in cluster.cluster]
 
     if len(all_clusters) == 0:
         fractal_logger.warning(
