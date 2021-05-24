6b9666a8971a471eac82a4a3946763a392e72008
mingy98 <ming@fractal.co>
2021-05-24 13:16:15 -0400

    added comments to flows.ts

--- client-applications/desktop/src/utils/flows.ts
+++ client-applications/desktop/src/utils/flows.ts
@@ -16,6 +16,18 @@ export const fork = <T>(
   source: Observable<T>,
   filters: { [gate: string]: (result: T) => boolean }
 ): { [gate: string]: Observable<T> } => {
+  /*
+    Description: 
+      Takes in an observable and map of filter functions and outputs a map of filtered observables
+    
+    Arguments: 
+      source (Observable<T>): An observable whos emissions are desired to be filtered
+      filters: Map of filter functions
+
+    Returns: 
+      Map of filtered observables
+  */
+
   const shared = source.pipe(share())
   return mapValues(filters, (fn) => shared.pipe(filter(fn)))
 }
@@ -25,6 +37,19 @@ export const flow =
     name: string,
     fn: (t: Observable<T>) => { [key: string]: Observable<any> }
   ): ((t: Observable<T>) => { [key: string]: Observable<any> }) =>
+  /*
+    Description: 
+      A function that returns a function which, when a trigger is activated, will run
+      and return a map of observables
+    
+    Arguments: 
+      name (string): Flow name
+      fn: Function that takes in an observable and returns a map of observables
+
+    Returns: 
+      Map of observables
+  */
+
   (trigger: Observable<T>) => {
     const channels = fn(trigger)
 
@@ -34,6 +59,18 @@ export const flow =
   }
 
 export const createTrigger = <A>(name: string, obs: Observable<A>) => {
+  /*
+    Description: 
+      Creates a Trigger from an observable
+    
+    Arguments: 
+      name (string): Trigger name
+      obs (Observable<A>): Observable to be turned into a Trigger
+
+    Returns: 
+      Original observable
+  */
+
   obs.subscribe((x: A) => {
     TriggerChannel.next({ name: `${name}`, payload: x } as Trigger)
   })
@@ -42,6 +79,17 @@ export const createTrigger = <A>(name: string, obs: Observable<A>) => {
 }
 
 export const fromTrigger = (name: string): Observable<any> => {
+  /*
+    Description: 
+      Returns a Trigger by name
+    
+    Arguments: 
+      name (string): Trigger name
+
+    Returns: 
+      Observable
+  */
+
   return TriggerChannel.pipe(
     // Filter out triggers by name. Note this allows for partial, case-insensitive string matching,
     // so filtering for "failure" will emit every time any trigger with "failure" in the name fires.
