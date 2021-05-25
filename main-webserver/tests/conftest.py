fa8b9041157a806a98f81a94fe12768b15840417
FishmanL <leor@fractal.co>
2021-05-25 14:27:35 -0400

    fixing sequential IDs

--- main-webserver/tests/conftest.py
+++ main-webserver/tests/conftest.py
@@ -283,7 +283,6 @@ def bulk_instance():
         db.session.commit()
         for _ in range(associated_containers):
             new_container = ContainerInfo(
-                container_id=randint(0, 100000),
                 instance_id=new_instance.instance_id,
                 user_id="test-user",
                 status="Running",
