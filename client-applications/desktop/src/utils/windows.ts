bd7f420f54f3a8febc789700ace6f0c82b0b7f39
Neil Hansen <neil.hansen.31@gmail.com>
2021-05-28 17:31:27 -0400

    Change window size

--- client-applications/desktop/src/utils/windows.ts
+++ client-applications/desktop/src/utils/windows.ts
@@ -122,8 +122,8 @@ export const createAuthWindow = () => {
     WindowHashAuth,
     {
       ...base,
-      ...width.sm,
-      ...height.md,
+      ...width.xs,
+      height: 16 * 37,
     } as BrowserWindowConstructorOptions,
     authenticationURL
   )
