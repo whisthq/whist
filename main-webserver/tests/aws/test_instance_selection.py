3c26f83a4012c048454a022ec6389d1847a1726e
FishmanL <leor@fractal.co>
2021-05-28 12:22:11 -0400

    bunch of small fixes

--- main-webserver/tests/aws/test_instance_selection.py
+++ main-webserver/tests/aws/test_instance_selection.py
@@ -50,49 +50,36 @@ def test_no_find_full_small_instance(bulk_instance):
     assert find_instance("us-east-1") is None
 
 
-@pytest.fixture
-def test_assignment_replacement_code(bulk_instance):
-    """
-    generates a function which, given a region, returns a multistage test
-    of the instance finding code
-    """
-
-    def _test_find_unassigned(location):
-        """
-        tests 4 properties of our find unassigned algorithm:
-        1) we don't return an overfull instance in the requested region
-        2) we don't return an overfull instance in a replacement region
-        3) we return an available instance in a replacement region
-        4) if available, we use the requested region rather than a replacement one
-        these properties are tested in order
-        """
-        replacement_region = bundled_region[location][0]
-        bulk_instance(associated_containers=10, location=location)
-        assert (
-            find_instance(location) is None
-        ), f"we assigned an already full instance in the main region, {location}"
-        bulk_instance(associated_containers=10, location=replacement_region)
-        assert (
-            find_instance(location) is None
-        ), f"we assigned an already full instance in the secondary region, {replacement_region}"
-        bulk_instance(location=replacement_region, instance_name="replacement-container")
-        assert find_instance(location) is not None, (
-            f"we failed to find the available instance "
-            f"in the replacement region {replacement_region}"
-        )
-        bulk_instance(location=location, instance_name="main-container")
-        instance = find_instance(location)
-        assert (
-            instance is not None and instance == "main-container"
-        ), f"we failed to find the available instance in the main region {location}"
-
-    return _test_find_unassigned
-
-
 @pytest.mark.parametrize(
     "location",
     bundled_region.keys(),
 )
-def test_assignment_logic(test_assignment_replacement_code, location):
+def test_assignment_logic(bulk_instance, location):
     # ensures that replacement will work for all regions
-    test_assignment_replacement_code(location)
+    """
+    tests 4 properties of our find unassigned algorithm:
+    1) we don't return an overfull instance in the requested region
+    2) we don't return an overfull instance in a replacement region
+    3) we return an available instance in a replacement region
+    4) if available, we use the requested region rather than a replacement one
+    these properties are tested in order
+    """
+    replacement_region = bundled_region[location][0]
+    bulk_instance(associated_containers=10, location=location)
+    assert (
+        find_instance(location) is None
+    ), f"we assigned an already full instance in the main region, {location}"
+    bulk_instance(associated_containers=10, location=replacement_region)
+    assert (
+        find_instance(location) is None
+    ), f"we assigned an already full instance in the secondary region, {replacement_region}"
+    bulk_instance(location=replacement_region, instance_name="replacement-container")
+    assert find_instance(location) is not None, (
+        f"we failed to find the available instance "
+        f"in the replacement region {replacement_region}"
+    )
+    bulk_instance(location=location, instance_name="main-container")
+    instance = find_instance(location)
+    assert (
+        instance is not None and instance == "main-container"
+    ), f"we failed to find the available instance in the main region {location}"
