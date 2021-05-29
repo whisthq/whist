7538e65c3d1f40d56d566924bd78a20a4803d479
Roshan Padaki <roshan@fractal.co>
2021-05-29 06:47:10 +0000

    Fix packaging on Windows (#2491)
    - Our SVG proxy snowpack plugin assumed POSIX path separators
    - We need to patch snowpack pending a merge and release of snowpackjs/rollup-plugin-polyfill-node#24

--- client-applications/desktop/scripts/postinstall.js
+++ client-applications/desktop/scripts/postinstall.js
@@ -1,5 +1,6 @@
 // Build core-ts and electron-notarize after the normal install procedures.
 const execCommand = require("./execCommand").execCommand
+const fs = require("fs")
 const path = require("path")
 
 console.log("Building `@fractal/core-ts`...")
@@ -11,3 +12,20 @@ console.log("Building `electron-notarize`...")
 execCommand("yarn install", path.join("node_modules", "electron-notarize"))
 execCommand("yarn build", path.join("node_modules", "electron-notarize"))
 console.log("Success building `electron-notarize`!")
+
+console.log(
+  "Patching `snowpack` to apply https://github.com/snowpackjs/rollup-plugin-polyfill-node/pull/24..."
+)
+const snowpackLibIndex = path.join(
+  "node_modules",
+  "snowpack",
+  "lib",
+  "index.js"
+)
+const oldContents = fs.readFileSync(snowpackLibIndex, "utf8")
+const newContents = oldContents.replace(
+  /\\0polyfill-node:/g,
+  "\\0polyfill-node."
+)
+fs.writeFileSync(snowpackLibIndex, newContents, "utf8")
+console.log("Success patching `snowpack`!")
