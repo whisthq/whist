a7a0b577db66deb2c92b869b764dffe2a5c4acdd
djsavvy <sraghuvanshi@college.harvard.edu>
2021-06-01 22:51:30 +0000

    Update actual host service code using latest version of Docker

--- ecs-host-service/ecs-host-service.go
+++ ecs-host-service/ecs-host-service.go
@@ -38,6 +38,7 @@ import (
 	dockerclient "github.com/docker/docker/client"
 	dockernat "github.com/docker/go-connections/nat"
 	dockerunits "github.com/docker/go-units"
+	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 func init() {
@@ -236,7 +237,12 @@ func SpinUpContainer(globalCtx context.Context, globalCancel context.CancelFunc,
 	re := regexp.MustCompile(`[^a-zA-Z0-9_.-]`)
 	containerName = re.ReplaceAllString(containerName, "-")
 
-	dockerBody, err := dockerClient.ContainerCreate(fc.GetContext(), &config, &hostConfig, nil, containerName)
+	platform := &specs.Platform{
+		Architecture: "amd64",
+		OS:           "linux",
+	}
+
+	dockerBody, err := dockerClient.ContainerCreate(fc.GetContext(), &config, &hostConfig, nil, platform, containerName)
 	if err != nil {
 		logAndReturnError("Error running `docker create` for %s:\n%s", fc.GetFractalID(), err)
 		return
