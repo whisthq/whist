c09e4bc165e282e124ba311c212780e0d753c9e5
Serina Hu <serina_hu@college.harvard.edu>
2021-06-02 13:01:18 -0400

    Explained why we are initializing to 0

--- protocol/client/main.c
+++ protocol/client/main.c
@@ -167,7 +167,9 @@ void init_update() {
 
     start_timer((clock*)&update_data.last_tcp_check_timer);
     start_timer((clock*)&latency_timer);
-    latency = 0;
+
+    // we initialize latency here because on macOS, latency would not initialize properly to 0 in its declaration above.
+    latency = 0.0;
     ping_id = 1;
     ping_failures = -2;
 
