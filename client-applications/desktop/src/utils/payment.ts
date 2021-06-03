c7dd2da1143e52cfe09ddd715c2394f76f37f3aa
Jannie <jz3191@columbia.edu>
2021-06-03 13:36:54 -0700

    rewrite client-app code following effects refactor
    works, need to confirm interaction with webserver (auth issues)
    
    add accessToken + fix typo

--- client-applications/desktop/src/utils/payment.ts
+++ client-applications/desktop/src/utils/payment.ts
@@ -1,64 +1,26 @@
 import { post } from "@app/utils/api"
-import { AsyncReturnType } from "@app/@types/state"
-import { string1To1000 } from "aws-sdk/clients/customerprofiles"
+import { FractalCallbackUrls } from "@app/config/urls"
+import { store } from "@app/utils/persist"
 
-export const stripeCheckoutCreate = async (
-  /*
-    Description: 
-      Makes a webserver call to get a stripe checkout id 
-    
-    Arguments: 
-      successUrl (str): URL to redirect to upon success 
-      cancelUrl (str): URL to redirect to upon cancelation
-    
-    Returns: 
-      JSON request containing checkout id 
-  */
-  successUrl: string,
-  cancelUrl: string1To1000
-) =>
-  post({
-    endpoint: "/stripe/create_checkout_session",
-    body: {
-      success_url: successUrl,
-      cancel_url: cancelUrl,
-    },
-  })
-
-export const stripeBillingPortalCreate = async (
+const stripeBillingPortalCreate = (accessToken: any) =>
   /*
     Description: 
       Makes a webserver call to get a stripe customer portal url 
     
     Arguments: 
-      returnUrl (str): URL to redirect to upon after leaving the portal
+      accessToken (str): access token for the user
     
     Returns: 
-      JSON request containing stripe customer portal url 
+      JSON response containing stripe customer portal url 
   */
-  returnUrl: string
-) =>
   post({
     endpoint: "/stripe/customer_portal",
+    accessToken,
     body: {
-      return_url: returnUrl,
+      return_url: FractalCallbackUrls,
     },
   })
 
-// Error checking for checkout portal creation
-type CheckoutResponseAuth = AsyncReturnType<typeof stripeCheckoutCreate>
-
-export const stripeCheckoutValid = (response: CheckoutResponseAuth) =>
-  (response?.json?.session_id ?? "") !== ""
-
-export const stripeCheckoutError = (response: CheckoutResponseAuth) =>
-  response.status !== 200
-
-// Error checking for customer portal creation
-type PortalResponseAuth = AsyncReturnType<typeof stripeBillingPortalCreate>
-
-export const stripePortalValid = (response: PortalResponseAuth) =>
-  (response?.json?.url ?? "") !== ""
-
-export const stripePortalError = (response: PortalResponseAuth) =>
-  response.status !== 200
+const accessToken = store.get("accessToken") ?? ""
+export const billingPortalURL =
+  stripeBillingPortalCreate(accessToken)?.json?.url
