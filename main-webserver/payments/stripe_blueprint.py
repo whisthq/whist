d03bca8507e3f16e3fbc4a93cf95a0595612e9eb
Owen Niles <owen@fractal.co>
2021-06-03 16:52:00 -0400

    Register the Stripe blueprint with the Flask application.

--- main-webserver/payments/stripe_blueprint.py
+++ main-webserver/payments/stripe_blueprint.py
@@ -1,62 +1,16 @@
-from flask import Blueprint, jsonify, request, current_app
+from flask import Blueprint
 from flask_jwt_extended import jwt_required
 
 from app import fractal_pre_process
-
-# from app.constants.config import ENDPOINT_SECRET
-from app.constants.http_codes import BAD_REQUEST, SUCCESS
-
-from payments.stripe_helpers import (
-    checkout_helper,
-    billing_portal_helper,
-    has_access_helper,
-    webhook_helper,
-)
+from app.constants.http_codes import BAD_REQUEST
 from app.helpers.utils.general.limiter import limiter, RATE_LIMIT_PER_MINUTE
 
+from payments.stripe_helpers import (
+    get_checkout_id,
+    get_billing_portal_url,
+)
+
 stripe_bp = Blueprint("stripe_bp", __name__)
-
-## TODO
-# in the future we may want to re-add endpoints to let them add products
-# basically, there are products which they can buy multiple units of
-# right now, not really so it's not implemented
-# there should be some scaffolding you can use in the client to make it happen
-
-
-@stripe_bp.route("/stripe/can_access_product", methods=["POST"])
-@limiter.limit(RATE_LIMIT_PER_MINUTE)
-@fractal_pre_process
-@jwt_required()
-def can_access_product(**kwargs):
-    """Returns a boolean with whether the given customer can access Fractal
-
-    Args:
-        customer_id (str): the stripe id of the user (different than email)
-
-    Returns:
-        json, int: JSON with whether the user has access, status code. The JSON is in the format
-            {
-                "subscribed": returns whether the user has access
-            }
-                or
-
-            {
-                "error": {
-                    "message": error message
-                }
-            }
-                if the customer does not exist
-    """
-    body = kwargs["body"]
-
-    try:
-        customer_id = body["customer_id"]
-    except:
-        return {"error": "The request body is incorrectly formatted."}, BAD_REQUEST
-
-    # TODO: use jwt _+ Auth0 to figure out a user's stripe customer_id
-
-    return has_access_helper(customer_id)
 
 
 @stripe_bp.route("/stripe/create_checkout_session", methods=["POST"])
@@ -96,7 +50,7 @@ def create_checkout_session(**kwargs):
 
     # TODO: use jwt _+ Auth0 to figure out a user's stripe customer_id
 
-    return checkout_helper(success_url, cancel_url, customer_id)
+    return get_checkout_id(success_url, cancel_url, customer_id)
 
 
 @stripe_bp.route("/stripe/customer_portal", methods=["POST"])
@@ -135,37 +89,4 @@ def customer_portal(**kwargs):
 
     # TODO: use jwt _+ Auth0 to figure out a user's stripe customer_id
 
-    return billing_portal_helper(customer_id, return_url)
-
-
-@fractal_pre_process
-@stripe_bp.route("/stripe/webhook", methods=["POST"])
-def webhook():
-    """
-    Endpoint for receiving all stripe webhook calls
-    """
-    event = None
-    try:
-        event = webhook_helper(request)
-    except Exception as e:
-        # ValueError: Invalid payload
-        # stripe.error.SignatureVerificationError: Invalid signature
-        return jsonify({"error": {"message": str(e)}}), BAD_REQUEST
-
-    # TODO: uncomment this once the webhook is in the stripe dashboard and STRIPE_WEBHOOK_SECRET has been populated
-    # if event.type == "invoice.created":
-    #     print("invoice created")
-    # elif event.type == "invoice.payment_failed":
-    #     print("payment failed")
-    # else:
-    #     print("Unhandled event type {}".format(event.type))
-
-    # TODO: delete this once the webhook is in the stripe dashboard and STRIPE_WEBHOOK_SECRET has been populated
-    if event["type"] == "invoice.created":
-        print("invoice created")
-    elif event["type"] == "invoice.payment_failed":
-        print("payment failed")
-    else:
-        print("Unhandled event type {}".format(event.type))
-
-    return "OK", SUCCESS
+    return get_billing_portal_url(customer_id, return_url)
