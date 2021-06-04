0dcdca2ce22be9689c1ef84100a1a181355d75e1
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2021-06-04 15:57:21 -0400

    AWS refactor container allocation (#2261)
    * fixing sequential IDs
    
    * bunch of small fixes
    
    * init
    
    * naive implementation
    
    * data validation
    
    * wip
    
    * add pydantic
    
    * delete extra file
    
    * Remove schemas
    
    * Add leors find_instance function
    
    * Lose choose_instance function
    
    * Add validation file
    
    * Add test
    
    * Delete draft file
    
    * Copy to mandelbox/assign
    
    * Add pydantic to requirements.txt
    
    * linting
    
    * Add pydantic to pylint extension-whitelist
    
    * Fix remaining linter errors
    
    * lint rest of file
    
    * fixing Neil's tests
    
    Co-authored-by: FishmanL <leor@fractal.co>

---
+++ main-webserver/app/validation.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from pydantic import BaseModel
+
+
+# With pydantic, we can define our query/body datastructures with
+# "one-time use" classes like this. They need to inherit from the pydantic
+# BaseModel, but no other boilerplate after that. The type signatures are
+# fully mypy compatible.
+class MandelboxAssignBody(BaseModel):
+    region: str
+    username: str
+    dpi: int
+
+
+# flask-pydantic ships a decorator called validate().
+# When validate() is used, flask-pydantic can populate the arguments to your
+# route function automatically.
+#
+# It's important to note that flask-pydantic actually uses the name of the
+# parameter to decide which data to pass to it. So "body" must be used to
+# access the request body, "query" must be used to access the query parameters.
+# It must also have a type signature of a class thats inherited from BaseModel.
+#
+# That's it! No more boilerplate, the rest of the validation comes for free.
+#
+# If any of the required parameters to MandelboxAssignBody are missing,
+# the response will automatically 400 BAD REQUEST, and the response json
+# will be something like the following:
+#
+# {
+#    'validation_error': {
+#        'query_params': [
+#           {
+#             'loc': ['dpi'],
+#             'msg': ['field required'],
+#             'type': ['value_error.missing'],
+#           }
+#        ]
+#    }
+# }
