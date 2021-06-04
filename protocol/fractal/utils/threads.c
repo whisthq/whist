af947e8c89bcdb38a7f7e1d1c7a446659ae85de1
Nicholas Pipitone <npip99@gmail.com>
2021-06-04 19:29:48 -0400

    Add comments to scheduling code

--- protocol/fractal/utils/threads.c
+++ protocol/fractal/utils/threads.c
@@ -27,19 +27,28 @@ void fractal_wait_thread(FractalThread thread, int *ret) { SDL_WaitThread(thread
 
 void fractal_set_thread_priority(FractalThreadPriority priority) {
 #ifdef __linux__
+    // When requesting _REALTIME on linux, use pthread calls directly
+    // for the highest possible priority
+    // (SCHED_RR being higher than any standard nice value, which is SCHED_OTHER)
     if (priority == FRACTAL_THREAD_PRIORITY_REALTIME) {
         char err[1024];
+        // Get the identifier for the thread that called this function
         pthread_t this_thread = pthread_self();
         struct sched_param params;
         errno = 0;
+        // Get the maximum possible priority that exists
         params.sched_priority = sched_get_priority_max(SCHED_RR);
+        // Check for error
         if (params.sched_priority == -1 && errno != 0) {
+            // Try _HIGH instead of _REALTIME on logged error
             strerror_r(errno, err, sizeof(err));
             LOG_ERROR("Failure calling sched_get_priority_max(): %s", err);
             fractal_set_thread_priority(FRACTAL_THREAD_PRIORITY_HIGH);
             return;
         }
+        // Set the priority to the maximum possible priority
         if (pthread_setschedparam(this_thread, SCHED_RR, &params) != 0) {
+            // Try _HIGH instead of _REALTIME on logged error
             LOG_ERROR("Failure calling pthread_setschedparam()");
             fractal_set_thread_priority(FRACTAL_THREAD_PRIORITY_HIGH);
             return;
@@ -47,8 +56,10 @@ void fractal_set_thread_priority(FractalThreadPriority priority) {
         return;
     }
 #endif
+    // Use SDL to set the thread priority
     if (SDL_SetThreadPriority((SDL_ThreadPriority)priority) < 0) {
         LOG_ERROR("Failure setting thread priority: %s", SDL_GetError());
+        // Try _HIGH instead of _REALTIME on logged failure to set _REALTIME
         if (priority == FRACTAL_THREAD_PRIORITY_REALTIME) {
             fractal_set_thread_priority(FRACTAL_THREAD_PRIORITY_HIGH);
         }
