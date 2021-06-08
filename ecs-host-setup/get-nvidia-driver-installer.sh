88ddab1a3ab77e9d67b6875d6e3cb4f798a0c354
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-06-08 16:37:26 +0000

    update to use latest nvidia driver

--- ecs-host-setup/get-nvidia-driver-installer.sh
+++ ecs-host-setup/get-nvidia-driver-installer.sh
@@ -5,5 +5,5 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
-aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/grid-11.2/NVIDIA-Linux-x86_64-450.89-grid-aws.run nvidia-driver-installer.run
+aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/latest/NVIDIA-Linux-x86_64-460.73.01-grid-aws.run nvidia-driver-installer.run
 chmod +x ./nvidia-driver-installer.run
