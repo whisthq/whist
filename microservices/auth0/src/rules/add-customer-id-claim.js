c70f884a9bbf0e6d4724c5abeee8864df731ec1d
Owen Niles <owen@fractal.co>
2021-06-09 10:44:41 -0400

    Write an Auth0 Rule that adds Stripe customer IDs to access tokens.

---
+++ microservices/auth0/src/rules/add-customer-id-claim.js
@@ -0,0 +1,17 @@
+/* add-customer-id-claim.js
+
+   Add the authenticated user's Stripe customer ID as the value of the custom
+   https://api.fractal.co/stripe_customer_id access token claim. By adding the
+   user's customer ID as a claim in the access token, the Webserver can read
+   the authenticated user's customer ID from the access token once it verifies
+   the token's signature.
+ */
+
+function addCustomerIdClaim(user, context, callback) {
+  const app_metadata = user.app_metadata || {};
+  const stripe_customer_id = app_metadata.stripe_customer_id || null;
+
+  context.accessToken["https://api.fractal.co/stripe_customer_id"] = stripe_customer_id;
+
+  return callback(null, user, context);
+}
