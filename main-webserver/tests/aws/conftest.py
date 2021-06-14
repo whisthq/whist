7315743369b2fe987bf3e48ce82ad89b7aa19f6b
Leor Barak Fishman <leor.fishman@gmail.com>
2021-06-14 13:05:09 -0400

    Basic instance scaling logic (#2625)
    * starting instance scaling logic
    
    adding basic scale_up/scale_down functions
    
    phil's simpler scaling logic
    
    phil's simpler scaling logic
    
    adding namegen
    
    adding rows to DB
    
    test fix
    
    more test fixes
    
    Neil fix
    
    * scaffolding out tests
    
    helpful pytest test fixtures
    
    scaleing up tests
    
    scaledown tests (single)
    
    * finishing up generic scaling tests
    
    * test fix
    
    * buffer tests
    
    * adding in scale_up thread start
    
    * test fix
    
    * adding scaledown helper + tests
    
    * adding a bunch of documentation and splitting out fixtures into conftest
    
    * lint fix
    
    * adding scaling harness
    
    * fixing scaling harnes
    
    * update col name
    
    * update col name
    
    * fix
    
    * savvy's changes
    
    * savvy's changes
    
    * more savvy changes
    
    * rename
    
    * adding in mutexes for scaling down
    
    * some test fixes/logic fixes
    
    * final savvy fix
    
    * fixing logic/tests
    
    * fixing app context issue
    
    * modifying scaling test to test draining
    
    * modifying scaling test to test draining
    
    * logic fixes + phil fixes

---
+++ main-webserver/tests/aws/conftest.py
@@ -0,0 +1,59 @@
+import pytest
+
+import app.helpers.blueprint_helpers.aws.aws_instance_post as aws_funcs
+from app.helpers.utils.aws.base_ec2_client import EC2Client
+from app.models import db
+
+
+@pytest.fixture
+def hijack_ec2_calls(monkeypatch):
+    """
+    This fixture mocks any calls to EC2client side-effecting endpoints
+    and just puts arguments to those calls into a list.
+    Use it to confirm that things interfacing with the EC2 client are
+    doing so correctly, _without_ risk of side effects.
+    """
+    call_list = []
+
+    def _helper(*args, **kwargs):
+        call_list.append({"args": args, "kwargs": kwargs})
+
+    monkeypatch.setattr(EC2Client, "start_instances", _helper)
+    monkeypatch.setattr(EC2Client, "stop_instances", _helper)
+    yield call_list
+
+
+@pytest.fixture
+def mock_get_num_new_instances(monkeypatch):
+    """
+    This fixture mocks the _get_num_new_instances function
+    from aws_instance_post, making it easy to set what that function should return.
+    """
+
+    def _patcher(return_value):
+        monkeypatch.setattr(
+            aws_funcs, "_get_num_new_instances", (lambda *args, **kwargs: return_value)
+        )
+
+    yield _patcher
+
+
+@pytest.fixture
+def hijack_db(monkeypatch):
+    """
+    This fixture mocks any calls to DB side effecting endpoints,
+    putting added objects into a list.
+    Use it to confirm that things interfacing with the DB in generic ways
+    do so correctly, without risk of side effects.
+    """
+    call_list = []
+
+    def _helper(*args, **kwargs):
+        call_list.append({"args": args, "kwargs": kwargs})
+
+    def _empty():
+        return
+
+    monkeypatch.setattr(db.session, "add", _helper)
+    monkeypatch.setattr(db.session, "commit", _empty)
+    yield call_list
