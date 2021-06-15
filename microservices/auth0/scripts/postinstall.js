3707d3ce9f35d79b51c533f3cc941ee2e629b0aa
Owen Niles <owen@fractalcomputers.com>
2021-06-15 23:05:02 +0000

    Remove unnecessary Auth0 microservice scripts and dependencies. (#2710)

--- microservices/auth0/scripts/postinstall.js
+++
@@ -1,7 +0,0 @@
-const path = require("path")
-const execCommand = require("../../../client-applications/desktop/scripts/execCommand").execCommand
-
-console.log("Building `@fractal/core-ts`...")
-execCommand("npm install", path.join("node_modules", "@fractal/core-ts"))
-execCommand("npm run build", path.join("node_modules", "@fractal/core-ts"))
-console.log("Success building `@fractal/core-ts`!")
