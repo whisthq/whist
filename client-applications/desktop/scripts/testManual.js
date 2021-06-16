4d7cf39dad94208f66de28a0ea0b6c95954a47f2
Neil Hansen <neil.hansen.31@gmail.com>
2021-06-16 14:14:09 -0400

    rename command variables

--- client-applications/desktop/scripts/testManual.js
+++ client-applications/desktop/scripts/testManual.js
@@ -9,7 +9,7 @@ const helpers = require("./build-package-helpers")
 
 const args = process.argv.slice(2)
 
-const withTesting = args.reduce((result, value) => {
+const schemaNames = args.reduce((result, value) => {
   if (result.length === 0) return `${value}`
   return `${result}, ${value}`
 }, "")
@@ -18,6 +18,5 @@ helpers.buildAndCopyProtocol()
 helpers.buildTailwind()
 helpers.snowpackDev({
   VERSION: helpers.getCurrentClientAppVersion(),
-  WITH_MOCKS: withTesting,
-  MANUAL_TEST: "true",
+  TEST_MANUAL_SCHEMAS: schemaNames,
 })
