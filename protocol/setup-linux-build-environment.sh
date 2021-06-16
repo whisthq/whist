3954f9bcf0cb8078043ce7b731d0243b0c52db84
Cathrach <serina_hu@college.harvard.edu>
2021-06-16 16:35:06 -0400

    Fix Linux server linking issues (#2735)
    * Make our copy protocol script copy everything in server/build64
    
    * Remove install of ffmpeg from Dockerfile, and make our base Dockerfile install the contents of ./setup-linux-build-environment.sh
    
    * Use --no-install-recommends when install dependencies
    
    * tinyfix
    
    * Install dependencies a bit earlier
    
    * rebased onto dev, cmake now copies shared ffmpeg libraries into server build
    
    * Print clang-format version
    
    * Add -o to build_container_images.sh so that GHA issues can be better diagnosed
    
    * Fix dockerfile folder convention
    
    * Clean some bash scripts
    
    * Try installing the normal clang-format
    
    * Make README use snap instead of apt-get, it's easier
    
    Co-authored-by: Nicholas Pipitone <npip99@gmail.com>
    Co-authored-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>

--- protocol/setup-linux-build-environment.sh
+++ protocol/setup-linux-build-environment.sh
@@ -4,12 +4,19 @@
 set -Eeuo pipefail
 
 # Install ffmpeg dependencies
-sudo apt-get install -y \
-    libmfx1 libvo-amrwbenc0 libopencore-amrnb0 libopencore-amrwb0 libx264-155 libx265-179 libfdk-aac1 libva-drm2 libvdpau1
+sudo apt-get install --no-install-recommends -y \
+    libmfx1 \
+    libvo-amrwbenc0 \
+    libopencore-amrnb0 \
+    libopencore-amrwb0 \
+    libx264-155 \
+    libx265-179 \
+    libfdk-aac1 \
+    libva-drm2 \
+    libvdpau1
 
 # Install Fractal dependencies
-
-sudo apt-get install -y \
+sudo apt-get install --no-install-recommends -y \
     libssl-dev \
     libx11-dev \
     libxtst-dev \
