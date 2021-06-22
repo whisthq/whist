371db67aa96838db0a71d82430e85161bfdab6e1
Ming <ming@fractal.co>
2021-06-22 10:43:07 -0400

    option chain dock function (#2800)

--- client-applications/desktop/src/utils/dock.ts
+++ client-applications/desktop/src/utils/dock.ts
@@ -2,14 +2,14 @@ import { app } from "electron"
 
 export const showAppDock = () => {
   // On MacOS, the regular option creates an app with an app dock
-  app?.setActivationPolicy("regular")
+  app?.setActivationPolicy?.("regular")
   // In case it's hidden, show the app dock
   app?.dock?.show().catch((err) => console.error(err))
 }
 
 export const hideAppDock = () => {
   // On MacOS, the accessory option removes the app dock
-  app?.setActivationPolicy("accessory")
+  app?.setActivationPolicy?.("accessory")
   // Hide the app dock just in case
   app?.dock?.hide()
 }
