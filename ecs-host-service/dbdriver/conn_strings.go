398d7df1178ca602e00c58fd4919cfdb9f3dc45a
djsavvy <sraghuvanshi@college.harvard.edu>
2021-06-23 08:47:53 +0000

    Build dev,staging,prod database URLs into the host service

--- ecs-host-service/dbdriver/conn_strings.go
+++ ecs-host-service/dbdriver/conn_strings.go
@@ -5,16 +5,27 @@ import (
 )
 
 // Fractal database connection strings
-// TODO: figure out the rest of these
-const localdevFractalDB = "user=uap4ch2emueqo9 host=localhost port=9999 dbname=d9rf2k3vd6hvbm"
+
+const localDevDatabaseURL = "user=uap4ch2emueqo9 host=localhost port=9999 dbname=d9rf2k3vd6hvbm"
+
+// The dev, staging, and prod ones are filled in by the linker
+var devDatabaseURL string
+var stagingDatabaseURL string
+var prodDatabaseURL string
 
 func getFractalDBConnString() string {
 	switch metadata.GetAppEnvironment() {
 	case metadata.EnvLocalDev:
 		return "fake string to throw error if used, since EnvLocalDev should not connect to a database"
 	case metadata.EnvLocalDevWithDB:
-		return localdevFractalDB
+		return localDevDatabaseURL
+	case metadata.EnvDev:
+		return devDatabaseURL
+	case metadata.EnvStaging:
+		return stagingDatabaseURL
+	case metadata.EnvProd:
+		return prodDatabaseURL
 	default:
-		return localdevFractalDB
+		return localDevDatabaseURL
 	}
 }
