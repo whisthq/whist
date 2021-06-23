dc7df87376a3c10b9f9ef22b5b22acb7f5272728
Leor Barak Fishman <leor.fishman@gmail.com>
2021-06-23 16:31:10 -0400

    Only allow users to have 1 container at a time (#2827)
    * adding function for checking user activity
    
    tests for the function
    
    * hehehaha i can definitely code
    
    * we shouldn't have neil's user info in our webserver
    
    * slight test fix?
    
    * no more unused import
    
    * changing the function name from absolute path in the code to the location of the import of the function
    
    Co-authored-by: Raviteja Guttula <raviteja@fractal.co>

---
+++ main-webserver/app/helpers/blueprint_helpers/aws/aws_container_assign_post.py
@@ -0,0 +1,13 @@
+from app.models import ContainerInfo
+
+
+def is_user_active(username: str) -> bool:
+    """
+    This function determines whether a given user already has a container running.
+    Is it's own function for ease of testing
+    Args:
+        username: Which user to check for
+
+    Returns: True if and only if the user has a container in the DB
+    """
+    return ContainerInfo.query.filter_by(user_id=username).one_or_none() is not None
