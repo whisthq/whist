dc0e0e7c9e51731c6aa678a37be8dc4da9611142
djsavvy <sraghuvanshi@college.harvard.edu>
2021-06-23 06:24:51 +0000

    Add VerifyContainer skeleton

--- ecs-host-service/dbdriver/containers.go
+++ ecs-host-service/dbdriver/containers.go
@@ -6,6 +6,20 @@ import (
 	"github.com/fractal/fractal/ecs-host-service/utils"
 )
 
+// VerifyContainer verifies that this host service is indeed expecting a
+// container for the given user.
+func VerifyContainer(ctx context.Context) error {
+	if !enabled {
+		return nil
+	}
+	if dbpool == nil {
+		return utils.MakeError("VerifyContainer() called but dbdriver is not initialized!")
+	}
+
+	logger.Errorf("Unimplemented")
+	return nil
+}
+
 // Check if container exists, etc.
 func RegisterContainer(ctx context.Context) error {
 	if !enabled {
