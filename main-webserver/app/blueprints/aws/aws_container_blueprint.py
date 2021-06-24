169708edba2a2db8a0e6182e016a9e529065a94a
Leor Barak Fishman <leor.fishman@gmail.com>
2021-06-24 11:59:29 -0400

    get rid of all user_container_state code (#2836)
    * get rid of all user_container_state code
    
    * bugfix
    
    * lint

--- main-webserver/app/blueprints/aws/aws_container_blueprint.py
+++ main-webserver/app/blueprints/aws/aws_container_blueprint.py
@@ -2,21 +2,14 @@ from threading import Thread
 import uuid
 from flask import Blueprint, current_app
 from flask.json import jsonify
-from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_jwt_extended import jwt_required
 from flask_pydantic import validate
 from app.validation import MandelboxAssignBody
 
 from app import fractal_pre_process, log_request
-from app.helpers.blueprint_helpers.aws.container_state import (
-    set_container_state,
-)
-from app.constants.container_state_values import CANCELLED
 from app.constants.http_codes import (
     ACCEPTED,
-    BAD_REQUEST,
-    NOT_FOUND,
     RESOURCE_UNAVAILABLE,
-    SUCCESS,
 )
 from app.helpers.blueprint_helpers.aws.aws_instance_post import do_scale_up_if_necessary
 from app.helpers.blueprint_helpers.aws.aws_container_assign_post import is_user_active
@@ -42,37 +35,6 @@ def regions():
     allowed_regions = RegionToAmi.query.filter_by(allowed=True)
 
     return jsonify([region.region_name for region in allowed_regions])
-
-
-@aws_container_bp.route("/container_state/<action>", methods=["POST"])
-@fractal_pre_process
-@jwt_required()
-def container_state(action, **kwargs):
-    """This is an endpoint which accesses our container_state_values table.
-    This table hides much of the inner workings of container spinup from users
-    and simply provides information regarding whether a user's app/container is
-    ready, cancelled, failed, etc. It is meant to be listened on through
-    a GraphQL websocket (subscription) for the loading page in the client app.
-
-    Args:
-        action (str): The string at the end of the URL which signifies
-        which action we want to take. Currently, only the cancel endpoint is provided,
-        which allows us to easily cancel a spinning up container and clean up our redux state.
-
-    Returns:
-        json, int: A json HTTP response and status code.
-    """
-    user = get_jwt_identity()
-
-    if action == "cancel":
-        body = kwargs.pop("body")
-        try:
-            task = body.pop("task")
-            set_container_state(keyuser=user, keytask=task, state=CANCELLED)
-        except:
-            return jsonify({"status": BAD_REQUEST}), BAD_REQUEST
-        return jsonify({"status": SUCCESS}), SUCCESS
-    return jsonify({"error": NOT_FOUND}), NOT_FOUND
 
 
 @aws_container_bp.route("/mandelbox/assign", methods=("POST",))
