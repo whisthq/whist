8bd5b013a340c848d4002b05ecb144bd05636408
Roshan Padaki <roshan@fractal.co>
2021-06-24 11:59:11 -0400

    Fix and simplifying Auth0 deploy hooks (#2829)

--- microservices/auth0/scripts/list-dependencies.js
+++ microservices/auth0/scripts/list-dependencies.js
@@ -1,15 +1,15 @@
 // List dependencies for a bundled hook
 // Usage: `yarn list-dependencies ./dist/[hook-name]`
 
-const fs = require('fs')
+const fs = require("fs");
 
-const path = process.argv[2]
-const data = fs.readFileSync(path, 'utf8')
+const path = process.argv[2];
+const data = fs.readFileSync(path, "utf8");
 
-const re = /require\("(.*?)"\)/g
-const matches = new Set()
+const re = /require\("(.*?)"\)/g;
+const matches = new Set();
 let match;
-while((match = re.exec(data))) {
-  matches.add(match[1])
+while ((match = re.exec(data))) {
+  matches.add(match[1]);
 }
-matches.forEach(match => console.log(match))
+matches.forEach((match) => console.log(match));
