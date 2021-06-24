ba611dc660aec61df6882b39856fe9257486d7e0
Leor Barak Fishman <leor.fishman@gmail.com>
2021-06-24 11:59:29 -0400

    get rid of all user_container_state code (#2836)
    * get rid of all user_container_state code
    
    * bugfix
    
    * lint

--- main-webserver/app/serializers/hardware.py
+++ main-webserver/app/serializers/hardware.py
@@ -3,7 +3,6 @@ from app.models import (
     InstanceInfo,
     RegionToAmi,
     SupportedAppImages,
-    UserContainerState,
 )
 
 
@@ -20,9 +19,3 @@ class RegionToAmiSchema(ma.SQLAlchemyAutoSchema):  # type: ignore[name-defined]
 class InstanceInfoSchema(ma.SQLAlchemyAutoSchema):  # type: ignore[name-defined]
     class Meta:
         model = InstanceInfo
-
-
-class UserContainerStateSchema(ma.SQLAlchemyAutoSchema):  # type: ignore[name-defined]
-    class Meta:
-        model = UserContainerState
-        include_fk = True
