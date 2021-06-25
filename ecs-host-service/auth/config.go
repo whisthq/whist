7e96c5df810241ef0031b2e3800c206e014a15d2
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2021-06-25 12:36:40 -0400

    Hotfix Host Service JWT Validation (#2853)
    I have no idea how this ever worked, even for the few days that it did (or did it ever work)?
    
    We had some Auth0-provided keys that were hardcoded in the host service which could rotate. We theorize that this happened yesterday, which is why all three of dev, staging, and prod went down simultaneously. We also were not validating the User IDs for requests correctly --- this has been fixed. Finally, the error messages on auth validation failure were very lacking in detail, and this has been addressed. Now we should have far more insight into future auth issues simply from host service logs.

--- ecs-host-service/auth/config.go
+++ ecs-host-service/auth/config.go
@@ -1,8 +1,7 @@
 package auth
 
 import (
-	"os"
-	"strings"
+	"github.com/fractal/fractal/ecs-host-service/metadata"
 )
 
 type authConfig struct {
@@ -10,62 +9,34 @@ type authConfig struct {
 	Aud string
 	// JWT issuer. The issuing server.
 	Iss string
-	// PEM-formatted public key used to verify JWT signer.
-	VerifyKey string
+}
+
+func (a authConfig) getJwksURL() string {
+	return a.Iss + ".well-known/jwks.json"
 }
 
 var authConfigDev = authConfig{
 	Aud: "https://api.fractal.co",
 	Iss: "https://fractal-dev.us.auth0.com/",
-	VerifyKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv9vqBRqFJZZOvc3Zoq60
-i8NRwYuTSiRnWjw4qLEPtHtNG0JVSDwtdcgcy2txR+/T7OA58wHQypMjXcd2A1Wq
-ufLXuKW8n7z8fioa45cdAhTLMYnsiimGwTM4Q504GFo8FntZDzjfxkFft0Exf/G8
-f15+6tHd5UyHGOehRRly1OzewQhvey+GbUitTG/x9Q34Zw80cbuF/4/+peyb0zAH
-G2DGzIZEOArfPTxoeFeyzNCvoq5dM6KEzu1nY1EhXxCON1nBMN6iCPFaQv15Yo8Z
-DM2uoZfTzKYVj5PJn3KbUS7oRhsA2u+ZArChmmgymzKgOFeTeBoCzwFF2Xf8pj4S
-2wIDAQAB
------END PUBLIC KEY-----
-`,
 }
 
 var authConfigStaging = authConfig{
 	Aud: "https://api.fractal.co",
 	Iss: "https://fractal-staging.us.auth0.com/",
-	VerifyKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtBHeP2sS+YqGs7hLqxxw
-0ebUQ+8E85+OJsGIu1PXQpqBybN2hMpUMHIxCPzipPfGPlu+fb5tIL/Qvect9ocT
-HcUIAaUNOHIk9rZ/ZtHrf740RrzegVNe+HlLU+7kTV60wwp9sMdbTbxGMqWAUj4d
-eZXHo4hq+zYSvAAcAJbFrR7GaYivsqRirjHksPkJHkiOiC4KSfKSX9UPRp+gtq/L
-jze3BhAswBchJ9EDE1TevXOBB9397O0Mz6IuE7uC1HpQxNhTnx/gZDkwsmN1giEW
-qSDfH3aQNrDkDvgM8rCXTxnCePndcHNrEHo+RrhEjPsRkmuMjJYnyladKrgz1vkS
-bwIDAQAB
------END PUBLIC KEY-----`,
 }
 
 var authConfigProd = authConfig{
 	Aud: "https://api.fractal.co",
 	Iss: "https://fractal-prod.us.auth0.com/",
-	VerifyKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzBiFN5J3Q7DeSKR6TRtk
-2z906kFX1/grZNy8o2OqUVGD6WPU9VlNh531TBYi40K+rLUV4WQNscZZ5V9Sri+k
-U+gMbeYAlhI2hxIYrQWHjJBnI+1V/EZmlBdJ2uhEiFroLKwTb6qaGijUc0AAqxeq
-j46M2D+EDa11Kuk08hsDNUys4wCAsXGeDh3yjIWjCu4xtDGCy0oaztSY0ZEd7J+D
-Pa1lINCpNB5NcFCyH5SGp55nBsEO3FmIwHELUCwg4LZHsQ09UC57da58LDlvrYIg
-7TDrDnikaVAG160AI5cPlRTBqU8ehH13zIgAAxJfvpQxPViP55hp81CdnEe9fEdn
-CQIDAQAB
------END PUBLIC KEY-----
-`,
 }
 
 func getAuthConfig() authConfig {
-	env := strings.ToLower(os.Getenv("APP_ENV"))
-	switch env {
-	case "development", "dev":
+	switch metadata.GetAppEnvironment() {
+	case metadata.EnvDev:
 		return authConfigDev
-	case "staging":
+	case metadata.EnvStaging:
 		return authConfigStaging
-	case "production", "prod":
+	case metadata.EnvProd:
 		return authConfigProd
 	default:
 		return authConfigDev
