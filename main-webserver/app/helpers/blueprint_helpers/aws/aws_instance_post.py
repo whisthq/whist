bfb2f314a7490d7a6a90a4bd10277686fb13f638
Raviteja Guttula <raviteja@fractal.co>
2021-06-25 11:19:36 -0700

    accomodation for local dev workflow for client-app

--- main-webserver/app/helpers/blueprint_helpers/aws/aws_instance_post.py
+++ main-webserver/app/helpers/blueprint_helpers/aws/aws_instance_post.py
@@ -17,6 +17,7 @@ from app.helpers.utils.db.db_utils import set_local_lock_timeout
 from app.helpers.utils.aws.base_ec2_client import EC2Client
 from app.helpers.utils.general.name_generation import generate_name
 from app.constants.instance_state_values import PRE_CONNECTION, DRAINING
+from app.constants.env_names import DEVELOPMENT
 
 bundled_region = {
     "us-east-1": ["us-east-2"],
@@ -38,12 +39,24 @@ def find_instance(region: str, client_commit_hash: str) -> Optional[str]:
     """
     # 5sec arbitrarily decided as sufficient timeout when using with_for_update
     set_local_lock_timeout(5)
-    avail_instance: Optional[InstanceSorted] = (
-        InstanceSorted.query.filter_by(location=region, commit_hash=client_commit_hash)
-        .limit(1)
-        .with_for_update(skip_locked=True)
-        .one_or_none()
-    )
+    # This condition below is to accomodate the worflow for developers of client_apps to test their changes
+    #  without needing to update the development database with commit_hashes on their local machines.
+    #  TODO: move the `local_dev` to the mono-repo config as this needs to be a shared secret between
+    #  client_app and main-webserver. 
+    if current_app.config['ENVIRONMENT'] == DEVELOPMENT and client_commit_hash == 'local_dev':
+        avail_instance: Optional[InstanceSorted] = (
+            InstanceSorted.query.filter_by(location=region)
+            .limit(1)
+            .with_for_update(skip_locked=True)
+            .one_or_none()
+        )
+    else:
+        avail_instance: Optional[InstanceSorted] = (
+            InstanceSorted.query.filter_by(location=region, commit_hash=client_commit_hash)
+            .limit(1)
+            .with_for_update(skip_locked=True)
+            .one_or_none()
+        )
     if avail_instance is None:
         # check each replacement region for available containers
         for bundlable_region in bundled_region.get(region, []):
