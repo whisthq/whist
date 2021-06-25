70cf6d7b86a2c4077afcbf9bd291bbc82e1ba64d
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2021-06-25 16:31:05 -0400

    Client app changes for aws refactor (#2842)

--- client-applications/desktop/src/testing/schemas/index.ts
+++ client-applications/desktop/src/testing/schemas/index.ts
@@ -27,4 +27,3 @@
 // application will react to the authFlow failure.
 export { default as loginError } from "./loginError"
 export { default as mandelboxCreateError } from "./mandelboxCreateError"
-export { default as mandelboxPollingError } from "./mandelboxPollingError"
