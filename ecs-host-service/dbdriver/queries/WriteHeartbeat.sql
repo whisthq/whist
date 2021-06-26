1fa8f8b31b47239c2cde5734dc8a8e1ea9fde42f
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2021-06-26 11:14:45 -0400

    Add database capabilities to the host service (#2810)

---
+++ ecs-host-service/dbdriver/queries/WriteHeartbeat.sql
@@ -0,0 +1,5 @@
+-- name: WriteHeartbeat :exec
+UPDATE hardware.instance_info
+  SET (memory_remaining_kb, nanocpus_remaining, gpu_vram_remaining_kb, last_updated_utc_unix_ms)
+  = (pggen.arg('memoryRemainingKB'), pggen.arg('nanoCPUsRemainingKB'), pggen.arg('gpuVramRemainingKb'), pggen.arg('lastUpdatedUtcUnixMs'))
+  WHERE instance_name = pggen.arg('instanceName');
