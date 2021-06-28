e5ddc49f1822cff687d0ee12137aee9e44de09c9
Raviteja V Guttula <2667498+thisisgvrt@users.noreply.github.com>
2021-06-28 11:20:31 -0700

    Added Flask commands for AMI upgrade (#2757)
    * Added Flask commands for maintenance mode
    
    * Fixes to the bug where instances won't boot up as the new AMIs are marked to be disabled during maintenance mode
    
    * addressed most of the review comments
    
    * replacing fractal_sql_commit with db.session.commit
    
    * Fixing issues found in local testing
    
    * Adding db_data.sql temporarily to make the CI happy during breaking changes to region_to_ami table
    
    * Linting fixes for py files with black
    
    * pylint fixes
    
    * more pylint fixes
    
    * linting fixes from black
    
    * fixing issues found with test cases
    
    * fixing issues with schema
    
    * fixes to test cases for changes as part of maintence mode removal
    
    * formatting fixes from black
    
    * Fixing typo in the drain_and_shutdown endpoint
    
    * refactor to improve testability along with test cases to test the enabled flag in region_to_ami table
    
    * added test case for launch_new_ami_buffer
    
    * linting fixes for black
    
    * fixing fixture as per changes to region_to_ami table
    
    * fixing the cleanup for newly added test cases
    
    * fixing instance type for production
    
    * fixing name for insert_disabled_amis function
    
    * making client_commit_hash as a constant
    
    * changing disable_ami, enabled_ami to accept a list of amis
    
    * linting fixes from black
    
    * refactor to test the ami_upgrade method with the test case
    
    * mocking fetch_current_running_instances to include testing of drain and shutdown functionality
    
    * linting fixes from black
    
    * fixed _mock_instance_info_query to return only running instances
    
    * fix for hijack_db when using with bulk_instance method
    
    * changes to the worflow to support the new workflow for AMI upgrade
    
    * making sure we only get each region once
    
    * renamed allowed in regionToAmi to region_enabled for clarity
    
    * fixed the breaking test case due to maintenance mode additions
    
    * renaming enabled column in RegionToAmi to ami_active for clarity
    
    * linting fixes from black
    
    * linting fixes for prettier
    
    * Addressing the comments for missing docstrings
    
    * linting fixes for black
    
    * Fixed failing test case
    
    * correction
    
    * Moving instance_type_to_be_launched to a central location
    
    * Fixed typo
    
    * accomodation for local dev workflow for client-app
    
    * fixed typo in docstring for fetch_current_running_instances
    
    * linting fixes for black
    
    * Locking active instances rows in InstanceInfo before we mark them as draining
    
    * created config for DEFAULT_INSTANCE_BUFFER  and fixed issues with locking when hosts are being marked as DRAINING
    
    * linting fixes
    
    * Fixing PR comments from savvy
    
    * fixed variable name in regions endpoints for clarity
    
    * fixed typo in comment for local_deploy.sh
    
    * fixed inconsistency in capitalization for AMIs
    
    * small correction
    
    * removing unnecessary db commits in a test fixture.
    
    * Fixed inconsistency of AMIs in deploy.yml
    
    * Small grammar fixes
    
    * Remove ecs-task-definitions directory ✂
    
    * fixing variable names and docstring for the move to instance_name as a primary identifier
    
    * Added docstring for test cases for maintenance mode
    
    * Fixing the missing DB commit when the host service is unresponsive
    
    * Remove a couple straggler instance_id variables
    
    * Remove references to task definitions from build and deploy
    
    * Update comment wording
    
    * Rename tables to correspond with view names (#2879)
    
    * Remove all references to auth_token column in instance_info table (#2881)
    
    * fixing the issue with mandelbox assign in dev
    
    Co-authored-by: FishmanL <leor@fractal.co>
    Co-authored-by: djsavvy <sraghuvanshi@college.harvard.edu>
    Co-authored-by: Leor Barak Fishman <leor.fishman@gmail.com>

---
+++ main-webserver/app/helpers/command_helpers/ami_upgrade.py
@@ -0,0 +1,216 @@
+from threading import Thread
+from typing import Dict, List
+import requests
+from flask import current_app
+from sqlalchemy import or_
+
+from app.models import db, RegionToAmi, InstanceInfo
+from app.helpers.utils.db.db_utils import set_local_lock_timeout
+from app.helpers.utils.general.logs import fractal_logger
+from app.helpers.blueprint_helpers.aws.aws_instance_post import do_scale_up_if_necessary
+from app.helpers.blueprint_helpers.aws.aws_instance_state import _poll
+from app.constants.instance_state_values import (
+    DRAINING,
+    HOST_SERVICE_UNRESPONSIVE,
+    ACTIVE,
+    PRE_CONNECTION,
+)
+
+
+def insert_new_amis(
+    client_commit_hash: str, region_to_ami_id_mapping: Dict[str, str]
+) -> List[RegionToAmi]:
+    """
+    Inserts new AMIs (but doesn't mark them as active) into the RegionToAmi table.
+    Will be invoked from the Flask CLI command to upgrade the AMIs for regions.
+    We should mark the new AMIs as active only after we cleanup the existing instances and AMIs.
+    Args:
+        client_commit_hash: Commit hash of the client that is compatible with the AMIs
+                            that are going to be passed in as the other argument.
+        region_to_ami_id_mapping: Dict<Region, AMI> Dict of regions to AMIs that are compatible
+                                    with the <client_commit_hash>.
+
+    Returns:
+        A list of the created RegionToAmi objects that are created.
+    """
+    new_amis = []
+    for region_name, ami_id in region_to_ami_id_mapping.items():
+        new_ami = RegionToAmi(
+            region_name=region_name,
+            ami_id=ami_id,
+            client_commit_hash=client_commit_hash,
+            ami_active=False,
+            region_enabled=True,
+        )
+        new_amis.append(new_ami)
+    db.session.add_all(new_amis)
+    db.session.commit()
+    return new_amis
+
+
+def launch_new_ami_buffer(region_name: str, ami_id: str, flask_app):
+    """
+    This function will be invoked from the Flask CLI command to upgrade the AMIs for regions.
+    Inserts new AMIs into the RegionToAmi table and blocks until the instance is started, and the
+    host service is active and has marked the instance as active in the database.
+
+    Args:
+        region_name: Name of the region in which new instances neeed to be launched.
+        ami_id: AMI for the instances that need to be launched.
+        flask_app: Needed for providing the context need for interacting with the database.
+
+    Returns:
+        None.
+    """
+    fractal_logger.debug(f"launching_instances in {region_name} with ami: {ami_id}")
+    with flask_app.app_context():
+        force_buffer = flask_app.config["DEFAULT_INSTANCE_BUFFER"]
+        new_instances = do_scale_up_if_necessary(region_name, ami_id, force_buffer)
+        for new_instance in new_instances:
+            fractal_logger.debug(
+                f"Waiting for instance with name: {new_instance.instance_name} to be marked online"
+            )
+            _poll(new_instance.instance_name)
+
+
+def mark_instance_for_draining(active_instance: InstanceInfo) -> None:
+    """
+    Marks the instance for draining by calling the drain_and_shutdown endpoint of the host service
+    and marks the instance as draining. If the endpoint errors out with an unexpected status code,
+    we are going to mark the instance as unresponsive. We are not going to kill the instance as at
+    this point in time, we won't be sure if all the users have left the instance. Once the instance
+    is marked as draining, we won't launch associate a "mandelbox" running on this instance to an user.
+
+    Note that we shouldn't call this function on a single instance multiple times. In particular,
+    the http server in a host service is shut down soon after the endpoint is called, which means
+    that future requests are always going to fail, unfairly marking the host service as
+    unresponsive.
+
+    Args:
+        active_instance: InstanceInfo object for the instance that need to be marked as draining.
+    """
+    try:
+        base_url = f"http://{active_instance.ip}:{current_app.config['HOST_SERVICE_PORT']}"
+        requests.post(f"{base_url}/drain_and_shutdown")
+        # Host service would be setting the state in the DB once we call the drain endpoint.
+        # However, there is no downside to us setting this as well.
+        active_instance.status = DRAINING
+    except requests.exceptions.RequestException:
+        active_instance.status = HOST_SERVICE_UNRESPONSIVE
+    finally:
+        db.session.commit()
+
+
+def fetch_current_running_instances(active_amis: List[str]) -> List[InstanceInfo]:
+    """
+    Fetches the instances that are either
+        ACTIVE - Instances that were launched and have potentially users running their applications on it.
+        PRE_CONNECTION - Instances that are launched few instants ago but haven't
+                        marked themselves active in the database through host service.
+    Args:
+        active_amis -> List of active AMIs before the upgrade started. We will be using this list to differntiate
+        between the instances launched from new AMIs that we are going to upgrade to and the current/older AMIs.
+        We can just mark all the running instances as DRAINING before we start the instances with new AMI but that
+        is going to increase the length of our unavailable window for users. This reduces the window by the time
+        it takes to spin up an AWS instance which can be anywhere from seconds to few minutes.
+    Returns:
+        List[InstanceInfo] -> List of instances that are currently running.
+    """
+    return (
+        db.session.query(InstanceInfo)
+        .filter(
+            or_(
+                InstanceInfo.status.like(ACTIVE),
+                InstanceInfo.status.like(PRE_CONNECTION),
+                InstanceInfo.aws_ami_id.in_(active_amis),
+            )
+        )
+        .with_for_update()
+        .all()
+    )
+
+
+def perform_upgrade(client_commit_hash: str, region_to_ami_id_mapping: str) -> None:
+    """
+    Performs upgrade of the AMIs in the regions that are passed in as the keys of the region_to_ami_id_mapping
+    This happens in the following steps:
+        - Get current active AMIs in the database, these will be marked as inactive once
+        we have sufficient buffer capacity from the new AMIs
+        - Insert the new AMIs that are passed in as an argument to this function and associate them with the
+        client_commit_hash.
+        - Launch new instances in regions with the new AMIs and wait until they are up and mark themselves as
+        active in the instances table. Since this launching the instances is going to take time, we will be using
+        a thread per each region to parallelize the process.
+        - Once the instances with the new AMIs are up and running, we will mark the instances that are running with
+        an older AMI version (i.e the current active AMI versions) as DRAINING to stop associating users with mandelboxes
+        on the instances.
+        - Once all the instances across all the regions are up, mark the current active AMIs as inactive and the
+        new AMIs as active.
+
+        Edge cases:
+            - What if the argument region_to_ami_id_mapping has more regions than we currently support. This should be fine
+            as we use the regions passed in as the argument as a reference to spin up instances in new regions.
+            - What if the argument region_to_ami_id_mapping has less number of regions than we currently support. Then, we
+            would update the AMIs in the regions that are passed in as the argument and mark the AMIs in missing regions as disabled.
+            Also, we would be marking all the current running instances for draining. So, this would essentially purge the regions
+            that are missing in the passed in arguments.
+
+        Args:
+            client_commit_hash: Commit hash of the client that is compatible with the AMIs
+                            that are going to be passed in as the other argument.
+            region_to_ami_id_mapping: String representation of Dict<Region, AMI>. Dict of regions to AMIs that are compatible
+                                        with the <client_commit_hash>.
+
+        Returns:
+            None
+    """
+    region_current_active_ami_map = {}
+    current_active_amis = RegionToAmi.query.filter_by(ami_active=True).all()
+    for current_active_ami in current_active_amis:
+        region_current_active_ami_map[current_active_ami.region_name] = current_active_ami
+
+    new_amis = insert_new_amis(client_commit_hash, region_to_ami_id_mapping)
+
+    region_wise_upgrade_threads = []
+    for region_name, ami_id in region_to_ami_id_mapping.items():
+        region_wise_upgrade_thread = Thread(
+            target=launch_new_ami_buffer,
+            args=(
+                region_name,
+                ami_id,
+            ),
+            # current_app is a proxy for app object, so `_get_current_object` method
+            # should be used to fetch the application object to be passed to the thread.
+            kwargs={"flask_app": current_app._get_current_object()},
+        )
+        region_wise_upgrade_threads.append(region_wise_upgrade_thread)
+        region_wise_upgrade_thread.start()
+
+    for region_wise_upgrade_thread in region_wise_upgrade_threads:
+        region_wise_upgrade_thread.join()
+
+    current_active_amis_str = [
+        current_active_ami.ami_id for current_active_ami in current_active_amis
+    ]  # Fetching the AMI strings for instances running with current/older AMIs.
+    # This will be used to select only the instances with current/older AMIs
+
+    current_running_instances = fetch_current_running_instances(current_active_amis_str)
+    for active_instance in current_running_instances:
+        # At this point, we should still have the lock that we grabbed when we invoked
+        # the `fetch_current_running_instances` function. Using this lock, we mark the
+        # instances as DRAINING to prevent a container being assigned to the instances.
+        active_instance.status = DRAINING
+    db.session.commit()
+
+    for active_instance in current_running_instances:
+        # At this point, the instance is marked as DRAINING in the database. But we need
+        # to inform the HOST_SERVICE that we have marked the instance as draining.
+        mark_instance_for_draining(active_instance)
+
+    for new_ami in new_amis:
+        new_ami.ami_active = True
+
+    for current_ami in current_active_amis:
+        current_ami.ami_active = False
+
+    db.session.commit()
