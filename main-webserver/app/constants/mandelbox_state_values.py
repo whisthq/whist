0a559a3f01dfa627a90e7d0e5aa7f63e9314af84
djsavvy <sraghuvanshi@college.harvard.edu>
2021-06-28 20:00:48 +0000

    Replace some other instances of 'container'

--- main-webserver/app/constants/mandelbox_state_values.py
+++ main-webserver/app/constants/mandelbox_state_values.py
@@ -4,4 +4,4 @@ SPINNING_UP_NEW = "SPINNING_UP_NEW"  # making a new container
 WAITING_FOR_CLIENT_APP = "WAITING_FOR_CLIENT_APP"  # found container, passed info to client app
 READY = "READY"  # successfully spun up, ready to connect
 FAILURE = "FAILURE"  # failed to spin up, cannot be connected to
-CANCELLED = "CANCELLED"  # has been cancelled (treated as a FAILURE effectivelly)
+CANCELLED = "CANCELLED"  # has been cancelled (treated as a FAILURE effectively)
