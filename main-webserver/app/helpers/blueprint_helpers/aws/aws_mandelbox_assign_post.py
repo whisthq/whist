81a611dffeeb66da3d49a4b7b4c5e58bc2119494
djsavvy <sraghuvanshi@college.harvard.edu>
2021-06-28 20:22:57 +0000

    more files

--- main-webserver/app/helpers/blueprint_helpers/aws/aws_mandelbox_assign_post.py
+++ main-webserver/app/helpers/blueprint_helpers/aws/aws_mandelbox_assign_post.py
@@ -3,11 +3,11 @@ from app.models import MandelboxInfo
 
 def is_user_active(username: str) -> bool:
     """
-    This function determines whether a given user already has a container running.
-    Is it's own function for ease of testing
+    This function determines whether a given user already has a mandelbox running.
+    Is it's own function for ease of testing.
     Args:
         username: Which user to check for
 
-    Returns: True if and only if the user has a container in the DB
+    Returns: True if and only if the user has a mandelbox in the DB
     """
     return MandelboxInfo.query.filter_by(user_id=username).one_or_none() is not None
