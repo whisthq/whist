1535d1c25687427b9a1804ab068ffe95b65f1440
djsavvy <sraghuvanshi@college.harvard.edu>
2021-06-28 19:00:53 +0000

    Update run_mandelbox_image.py

--- mandelbox-images/helper_scripts/run_mandelbox_image.py
+++ mandelbox-images/helper_scripts/run_mandelbox_image.py
@@ -14,7 +14,7 @@ import requests
 
 
 DESCRIPTION = """
-This script runs a Fractal container by calling the `SpinUpMandelbox` endpoint
+This script runs a Fractal mandelbox by calling the `SpinUpMandelbox` endpoint
 on the host service and emulating the client-app (but not the client protocol).
 It is used by the scripts `run_local_mandelbox_image.sh` and
 `run_remote_mandelbox_image.sh` and is usually not called directly.
@@ -33,7 +33,7 @@ reset_working_directory()
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument(
     "image",
-    help="Fractal container to run. Defaults to 'fractal/base:current-build'.",
+    help="Fractal mandelbox to run. Defaults to 'fractal/base:current-build'.",
     default="fractal/base:current-build",
 )
 parser.add_argument(
@@ -42,7 +42,7 @@ parser.add_argument(
     default=False,
     help=(
         "This flag causes the locally-built server protocol to be copied into "
-        "the running container. Not passing in this flag causes the container to use "
+        "the running mandelbox. Not passing in this flag causes the mandelbox to use "
         "the version of the server protocol already built into its image. By "
         "default, run_local_mandelbox_image.sh enables this flag, but "
         "run_remote_mandelbox_image.sh does not."
@@ -52,7 +52,7 @@ parser.add_argument(
     "--dpi",
     type=int,
     default=96,
-    help="DPI to use for the X server inside the container. Defaults to 96.",
+    help="DPI to use for the X server inside the mandelbox. Defaults to 96.",
 )
 parser.add_argument(
     "--protocol-timeout",
@@ -60,7 +60,7 @@ parser.add_argument(
     default=60,
     help=(
         "This arg sets the timeout for the protocol server inside the "
-        "container, in seconds. Defaults to 60."
+        "mandelbox, in seconds. Defaults to 60."
     ),
 )
 parser.add_argument(
@@ -84,7 +84,7 @@ docker_client = docker.from_env()
 HOST_SERVICE_URL = "https://127.0.0.1:4678/"
 HOST_SERVICE_CERT_PATH = "/fractalprivate/cert.pem"
 protocol_build_path = os.path.abspath("../protocol/build-docker/server/build64")
-container_server_path = os.path.abspath("/usr/share/fractal/bin")
+mandelbox_server_path = os.path.abspath("/usr/share/fractal/bin")
 PortBindings = namedtuple(
     "PortBindings", ["host_port_32262tcp", "host_port_32263udp", "host_port_32273tcp"]
 )
@@ -118,18 +118,25 @@ def ensure_host_service_is_running():
     if not running:
         sys.exit(
             (
-                "Cannot start container because the ecs-host-service is not "
+                "Cannot start mandelbox because the ecs-host-service is not "
                 "listening on port 4678. Is it running successfully?"
             )
         )
 
 
 def kill_container(cont):
+    """
+    Takes in a Container object and kills it.
+    """
     cont.kill()
     cont.remove()
 
 
 def copy_locally_built_protocol(cont):
+    """
+    Takes in a Container object and copies the locally built version of the
+    protocol into the corresponding Mandelbox.
+    """
     # The docker python API is stupid, so we first need to tar the protocol directory
     fileobj = io.BytesIO()
     with tarfile.open(fileobj=fileobj, mode="w") as tar:
@@ -137,14 +144,14 @@ def copy_locally_built_protocol(cont):
         tar.add(".")
         reset_working_directory()
 
-    cont.put_archive(container_server_path, fileobj.getvalue())
+    cont.put_archive(mandelbox_server_path, fileobj.getvalue())
 
 
 def send_spin_up_mandelbox_request():
     """
     Sends the host service a SpinUpMandelbox request and returns a Container
-    object corresponding to that container, along with its identifying host
-    port (i.e. host port corresponding to tcp/32262 in the container),
+    object corresponding to that mandelbox, along with its identifying host
+    port (i.e. host port corresponding to tcp/32262 in the mandelbox),
     aeskey, and mandelboxID.
     """
     print("Sending SpinUpMandelbox request to host service!")
@@ -168,7 +175,7 @@ def send_spin_up_mandelbox_request():
     host_port_32273tcp = response["result"]["port_32273"]
     key = response["result"]["aes_key"]
 
-    # Find the Container object corresponding to the container that was just created
+    # Find the Container object corresponding to the mandelbox that was just created
     matching_containers = docker_client.containers.list(
         filters={
             "publish": f"{host_port_32262tcp}/tcp",
@@ -185,7 +192,7 @@ def send_spin_up_mandelbox_request():
 
 def write_protocol_timeout(mandelbox_id):
     """
-    Takes in a Mandelbox ID, and writes the protocol timeout to the corresponding container.
+    Takes in a Mandelbox ID, and writes the protocol timeout to the corresponding mandelbox.
     """
     with open(f"/fractal/{mandelbox_id}/mandelboxResourceMappings/timeout", "w") as timeout_file:
         timeout_file.write(f"{args.protocol_timeout}")
@@ -206,15 +213,15 @@ if __name__ == "__main__":
         raise err
 
     print(
-        f"""Successfully started container with identifying hostPort {host_ports.host_port_32262tcp}.
-To connect to this container using the client protocol, run one of the following commands, depending on your platform:
+        f"""Successfully started mandelbox with identifying hostPort {host_ports.host_port_32262tcp}.
+To connect to this mandelbox using the client protocol, run one of the following commands, depending on your platform:
 
     windows:        .\\build\\fclient.bat {get_public_ipv4_addr()} -p32262:{host_ports.host_port_32262tcp}.32263:{host_ports.host_port_32263udp}.32273:{host_ports.host_port_32273tcp} -k {aeskey}
     linux/macos:    ./fclient {get_public_ipv4_addr()} -p32262:{host_ports.host_port_32262tcp}.32263:{host_ports.host_port_32263udp}.32273:{host_ports.host_port_32273tcp} -k {aeskey}
 
-Name of resulting container:
+Name of resulting mandelbox:
 {container.name}
-Docker ID of resulting container:"""
+Docker ID of resulting mandelbox:"""
     )
 
     # Note that ./run_mandelbox_image.sh relies on the dockerID being printed
