a0c15b3285e700d9b733ec2104e2289fc21c8c9c
Yoel Hawa <yoelhawa@gmail.com>
2021-06-29 15:01:05 -0400

    Fixes setup_localdev_dependencies.sh to install python dependencies after rest of setup

--- ecs-host-setup/setup_localdev_dependencies.sh
+++ ecs-host-setup/setup_localdev_dependencies.sh
@@ -9,16 +9,23 @@ set -Eeuo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
+# This part sets up the instance, and is necessary for both engineer dev instances and production instances
+./setup_ubuntu20_host.sh
+
 # This part installs things that are ONLY necessary for engineer dev instances, NOT for production instances
+echo "================================================"
+echo "Installing python dependencies..."
+echo "================================================"
 sudo apt-get install -y python3-pip
 cd ..
 find ./container-images -name 'requirements.txt' | sed 's/^/-r /g' | xargs sudo pip3 install
 cd ecs-host-setup
 
-# This part sets up the instance, and is necessary for both engineer dev instances and production instances
-./setup_ubuntu20_host.sh
-
 echo "================================================"
 echo "Creating symlink for root user to get aws credentials..."
 echo "================================================"
 sudo ln -sf /home/ubuntu/.aws /root/.aws
+
+echo
+echo "Install complete. Please 'sudo reboot' before continuing."
+echo
