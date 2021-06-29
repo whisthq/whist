15ee05437ba9edd048db45e109f397a90ebb4fe5
Philippe Noel <phil@fractal.co>
2021-06-29 16:04:25 -0400

    do codecov upload straight from Heroku

--- main-webserver/tests/run_tests.sh
+++ main-webserver/tests/run_tests.sh
@@ -48,3 +48,7 @@ export TESTING=true
 # pass args to pytest, including Codecov flags, and ignore the scripts/ folder as it's irrelevant 
 # to unit/integration testing
 (cd .. && pytest --ignore=scripts/ "$@" --cov=./ --cov-report=xml)
+
+# Upload the Codecov coverage report to Codecov, using the CODECOV_TOKEN stored as a 
+# Heroku config variable
+bash <(curl -s https://codecov.io/bash)
