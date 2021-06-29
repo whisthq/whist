07b5e8ab726d020783b37eae852a3d1bbca85287
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2021-06-29 11:30:38 -0400

    Use Heroku to get DB URL for host service (#2908)

--- ecs-host-service/dbdriver/connection_strings.go
+++ ecs-host-service/dbdriver/connection_strings.go
@@ -2,30 +2,27 @@ package dbdriver // import "github.com/fractal/fractal/ecs-host-service/dbdriver
 
 import (
 	"github.com/fractal/fractal/ecs-host-service/metadata"
+	"github.com/fractal/fractal/ecs-host-service/metadata/heroku"
+	"github.com/fractal/fractal/ecs-host-service/utils"
 )
 
 // Fractal database connection strings
 
 const localDevDatabaseURL = "user=uap4ch2emueqo9 host=localhost port=9999 dbname=d9rf2k3vd6hvbm"
 
-// The dev, staging, and prod ones are filled in by the linker
-var devDatabaseURL string
-var stagingDatabaseURL string
-var prodDatabaseURL string
-
-func getFractalDBConnString() string {
-	switch metadata.GetAppEnvironment() {
-	case metadata.EnvLocalDev:
-		return "fake string to throw error if used, since EnvLocalDev should not connect to a database"
-	case metadata.EnvLocalDevWithDB:
-		return localDevDatabaseURL
-	case metadata.EnvDev:
-		return devDatabaseURL
-	case metadata.EnvStaging:
-		return stagingDatabaseURL
-	case metadata.EnvProd:
-		return prodDatabaseURL
-	default:
-		return localDevDatabaseURL
+func getFractalDBConnString() (string, error) {
+	if metadata.IsLocalEnv() {
+		return localDevDatabaseURL, nil
 	}
+
+	config, err := heroku.GetConfig()
+	if err != nil {
+		return "", utils.MakeError("Couldn't get DB connection string: %s", err)
+	}
+	result, ok := config["DATABASE_URL"]
+	if !ok {
+		return "", utils.MakeError("Couldn't get DB connection string: couldn't find DATABASE_URL in Heroku environment.")
+	}
+
+	return result, nil
 }
