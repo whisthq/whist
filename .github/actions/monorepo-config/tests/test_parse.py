c0124121ef3b76a18a40f2a00ba82384a68e5e39
Neil Hansen <neil.hansen.31@gmail.com>
2021-06-29 14:28:50 -0400

    Remove ununsed

--- .github/actions/monorepo-config/tests/test_parse.py
+++ .github/actions/monorepo-config/tests/test_parse.py
@@ -8,18 +8,6 @@ from pathlib import Path
 from contextlib import contextmanager
 from helpers.utils import temporary_fs
 from helpers.validate import ValidationError
-from tests.generators import schemas, non_dict
-from hypothesis import given, example
-from hypothesis.strategies import (
-    text,
-    just,
-    lists,
-    one_of,
-    dictionaries,
-    composite,
-    integers,
-    sampled_from,
-)
 
 
 def flatten_child(profiles, child):
@@ -27,36 +15,6 @@ def flatten_child(profiles, child):
         if profile in child:
             return child[profile]
     return child
-
-
-# def test_parse( ,):
-
-
-# def test_profile_load():
-#     all_loaded = "all keys and values should be in loaded dictionary"
-
-#     with temporary_fs(mock_data.config_simple_fs) as tempdir:
-#         expected = {"env": ["dev", "stg", "prd"]}
-#         profile_path = tempdir.joinpath("config", "profiles.yml")
-#         assert parse.profile_load(profile_path) == expected, all_loaded
-
-#     bad_parent_format = {**mock_data.config_simple_fs}
-#     bad_parent_format["config"]["profiles.yml"] = yaml.dump([{"env": ["dev"]}])
-
-#     with temporary_fs(bad_parent_format) as tempdir:
-#         profile_path = tempdir.joinpath("config", "profiles.yml")
-#         with pytest.raises(ValidationError):
-#             parse.profile_load(profile_path)
-
-#     bad_child_format = {**mock_data.config_simple_fs}
-#     bad_child_format["config"]["profiles.yml"] = yaml.dump(
-#         {"env": {"profile": "abc"}}
-#     )
-
-#     with temporary_fs(bad_child_format) as tempdir:
-#         profile_path = tempdir.joinpath("config", "profiles.yml")
-#         with pytest.raises(ValidationError):
-#             parse.profile_load(profile_path)
 
 
 @pytest.mark.skip()
@@ -108,55 +66,8 @@ def test_parse_secrets_profiles():
         )
 
 
-# @given(schemas(), non_dict())
-# def test_resolve(schema, random_value):
-#     profiles, _source = schema
-
-#     base1 = parse.parse.resolve(profiles, random_value)
-#     assert (
-#         base1 == random_value
-#     ), "a value that wasn't a dictionary was returned changed."
-
-
-# @given(schemas())
-# @example((mock_data.example_profiles, mock_data.example_nested_map))
-# def test_resolve_flattening(schema):
-#     profiles, source = schema
-#     result = parse.parse.resolve(profiles, source)
-#     for key, child in source.items():
-#         if isinstance(child, dict):
-#             if any(p in child for p in profiles):
-#                 for profile in reversed(profiles):
-#                     if profile in child:
-#                         assert result[key] == child[profile], (
-#                             "resolved child value must match"
-#                             + " value of highest index profile"
-#                         )
-
-#                         break
-#             else:
-#                 assert (
-#                     result[key] == child
-#                 ), "child value must be unchanged if no profile match is found"
-
-
-# @given(schemas())
-# @example((mock_data.example_profiles, mock_data.example_nested_map))
-# def test_resolve_same_keys(schema):
-#     profiles, source = schema
-#     result = parse.parse.resolve(profiles, source)
-#     assert (
-#         result.keys() == source.keys()
-#     ), "result must have same keys as source"
-
-
 a = {
     "dev": {"macos": "dev-mac", "win32": "dev-win", "linux": "dev-lnx"},
     "stg": {"macos": "stg-mac", "win32": "stg-win", "linux": "stg-lnx"},
     "prd": {"macos": "prd-mac", "win32": "prd-win", "linux": "prd-lnx"},
 }
-
-
-@pytest.mark.skip()
-def test_parse2():
-    assert parse.parse2(a) == a
--- .github/actions/monorepo-config/tests/test_utils.py
+++ .github/actions/monorepo-config/tests/test_utils.py
@@ -3,22 +3,7 @@ import pytest
 import itertools
 import toolz
 import helpers.utils as utils
-from functools import partial
-from collections.abc import MutableMapping
 from pathlib import Path
-from hypothesis import given, example
-from hypothesis.strategies import (
-    text,
-    just,
-    lists,
-    one_of,
-    dictionaries,
-    composite,
-    integers,
-    sampled_from,
-    functions,
-    recursive,
-)
 
 example_dict1 = {
     "dev": {"x": "t", "y": "u", "z": "v"},
@@ -39,21 +24,6 @@ example_dict3 = {
     },
     "x": {"y": 5, "z": 6},
 }
-
-
-@composite
-def nested_dict(draw):
-    return draw(
-        dictionaries(
-            text(), dictionaries(text(), text(), max_size=10), max_size=10
-        )
-    )
-
-
-def recursive_dicts():
-    return recursive(
-        dictionaries(text(), text()), lambda x: dictionaries(text(), x)
-    )
 
 
 def test_temporary_fs():
@@ -80,91 +50,6 @@ def test_temporary_fs():
             paths.append(path)
     for pth in paths:
         assert not pth.exists(), fs_delete
-
-
-# @given(recursive_dicts())
-# def test_walk_keys(dct):
-#     has_keys = "all keys in outputs paths must exist in input dict"
-
-#     for path in utils.walk_keys(dct):
-#         subdct = dct
-#         for key in path[:-1]:
-#             assert key in subdct, has_keys
-#             subdct = subdct[key]
-#         assert not isinstance(path[-1], dict)
-
-
-# @given(integers(max_value=10), text(max_size=100), text(max_size=5))
-# @example(10, "longword", "...")
-# def test_truncated(limit, string, trail):
-#     has_trail = "result must include trail"
-#     no_change = "result under limit must be returned unchanged"
-#     be_string = "result must be a string"
-#     limited = "result must have length greater than 0, less or equal to limit"
-#     includes = "result must include input up to (limit - len(trail)) chars"
-
-#     clamped = max(limit, 0)
-
-#     assert isinstance(string, str), be_string
-
-#     result = utils.truncated(limit, string, trail)
-#     result_default_trail = utils.truncated(limit, string)
-
-#     if len(string) > limit:
-#         assert result_default_trail.endswith("..."[:clamped]), has_trail
-#         assert result.endswith(trail[:clamped]), has_trail
-#     else:
-#         assert result == string, no_change
-
-#     assert len(result) <= clamped, limited
-#     assert result.startswith(string[: len(result) - clamped]), includes
-
-
-# # result is dict with trunctated values, or same values
-# @given(dictionaries(text(), text()))
-# def test_truncated_children(dct):
-#     truncates = "all values must be truncated up to limit"
-
-#     result = utils.truncated_children(dct)
-#     assert all(v == "..." for v in result.values()), truncates
-
-
-# def test_nested_keys():
-#     assert sorted(utils.nested_keys(example_dict3)) == list("abcdefghijxyz")
-
-
-# def test_all_child_keys():
-#     all_children = "fn must be called with all keys below top-level of dict"
-#     no_top_level = "fn must not be called with top-level keys"
-#     children = set("bcdefghijyz")
-#     parents = set("ax")
-#     assert utils.all_child_keys(
-#         lambda x: x in children, example_dict3
-#     ), all_children
-#     assert utils.all_child_keys(
-#         lambda x: x not in parents, example_dict3
-#     ), no_top_level
-
-
-# def test_all_items_in_set_partial():
-#     superset = [1, 2, 3, 4, 5]
-#     applied = utils.all_items_in_set_partial(superset)
-#     assert applied([1, 2, 3])
-#     assert not applied([4, 5, 6])
-
-
-# def test_all_child_keys_in_set_partial():
-#     all_child = "true when all child keys are in set"
-#     no_child = "false when some child keys not in set"
-#     no_parent = "parent keys should not be tested for inclusion"
-
-#     children = set("bcdefghijyz")
-#     parents = set("ax")
-#     in_children = utils.all_child_keys_in_set_partial(children)
-#     in_parents = utils.all_child_keys_in_set_partial(parents)
-#     assert in_children(example_dict3), all_child
-#     assert not in_children(example_dict1), no_child
-#     assert not in_parents(example_dict3), no_parent
 
 
 def test_find_duplicate():
