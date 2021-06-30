fbb022ea337ca2edae2682ef35c012762b2939f2
FishmanL <leor@fractal.co>
2021-06-30 13:59:15 -0400

    grab locks correctly

--- main-webserver/app/helpers/blueprint_helpers/aws/aws_instance_post.py
+++ main-webserver/app/helpers/blueprint_helpers/aws/aws_instance_post.py
@@ -272,14 +272,11 @@ def try_scale_down_if_necessary_all_regions() -> None:
         ).all()
     ]
     for region, ami in region_and_ami_list:
-        if (
-            RegionToAmi.query.filter_by(region_name=region, ami_id=ami)
-            .with_for_update(skip_locked=True)
-            .one_or_none()
-            is not None
-        ):
-            db.session.commit()
-            try_scale_down_if_necessary(region, ami)
+        # grab a lock on this region/ami pair
+        RegionToAmi.query.filter_by(region_name=region, ami_id=ami).with_for_update()
+        try_scale_down_if_necessary(region, ami)
+        # and release it after scaling
+        db.session.commit()
 
 
 def repeated_scale_down_harness(time_delay: int, flask_app=current_app) -> None:
