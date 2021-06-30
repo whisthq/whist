64671864322ec226c309c55d374b93addfe6113c
djsavvy <sraghuvanshi@college.harvard.edu>
2021-06-30 13:21:18 -0400

    Shorten polling iterations

--- main-webserver/app/helpers/blueprint_helpers/aws/aws_instance_state.py
+++ main-webserver/app/helpers/blueprint_helpers/aws/aws_instance_state.py
@@ -10,8 +10,8 @@ from app.constants.instance_state_values import (
     ACTIVE,
 )
 
-MAX_POLL_ITERATIONS = 90
-POLL_SLEEP_INTERVAL = 10  # seconds
+MAX_POLL_ITERATIONS = 60
+POLL_SLEEP_INTERVAL = 4 # seconds
 
 
 def _poll(instance_name: str) -> bool:
