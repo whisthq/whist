edf9a98cc2cb12cc0e29670092b0e5f4cb8ebf41
FishmanL <leor@fractal.co>
2021-07-01 10:34:16 -0400

    fixing the damn schema

--- main-webserver/db_migration/schema.sql
+++ main-webserver/db_migration/schema.sql
@@ -261,7 +261,7 @@ CREATE VIEW hardware.instances_with_room_for_mandelboxes AS
                     instance_info.commit_hash,
                     instance_info.mandelbox_capacity
                    FROM hardware.instance_info
-                   WHERE (instance_info.status)::text <> 'DRAINING'::text)) instances
+                   WHERE instance_info.status::text <> 'DRAINING'::text) instances
              LEFT JOIN ( SELECT count(*) AS count,
                     mandelbox_info.instance_name AS cont_inst
                    FROM hardware.mandelbox_info
