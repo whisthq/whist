455d5fce82e3561e79ca04c839003d7fc83f1df5
Neil Hansen <neil.hansen.31@gmail.com>
2021-07-01 01:53:01 -0400

    Add action entrypoint files

---
+++ .github/actions/monorepo-config/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# In the GitHub Actions runner, this script will be run with the monorepo
+# root as the working directory. Our file path to the Python script must be
+# relative to the monorepo root folder.
+
+# We forward all the arguments passed to the docker run proces to this script.
+# You can see the arguments that will be passed to this script in actions.yml
+# for this GitHub Action.
+STATE_CONFIG=$(python .github/actions/monorepo-config/main.py "$@")
+
+echo "$STATE_CONFIG"
