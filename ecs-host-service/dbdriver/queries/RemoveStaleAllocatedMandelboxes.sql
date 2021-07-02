27f2a8c99da7f657a41996fa32e79bd95729a03e
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2021-07-02 11:39:07 -0400

    Pull changes from testing branch onto main project branch (#3026)
    Co-authored-by: FishmanL <leor@fractal.co>

--- ecs-host-service/dbdriver/queries/RemoveStaleAllocatedMandelboxes.sql
+++
@@ -1,5 +0,0 @@
--- name: RemoveStaleAllocatedMandelboxes :exec
-DELETE FROM hardware.mandelbox_info
-  WHERE instance_name = pggen.arg('instanceName')
-    AND status = pggen.arg('status')
-    AND creation_time_utc_unix_ms < pggen.arg('creationTimeThreshold');
