4314964bbf1bbc98dc23d01113d923a5225d1cde
djsavvy <sraghuvanshi@college.harvard.edu>
2021-07-02 14:49:30 +0000

    Mark a container as allocated once it starts up

--- ecs-host-service/dbdriver/mandelboxes.go
+++ ecs-host-service/dbdriver/mandelboxes.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/jackc/pgtype"
+	"github.com/jackc/pgx/v4"
 
 	"github.com/fractal/fractal/ecs-host-service/dbdriver/queries"
 	logger "github.com/fractal/fractal/ecs-host-service/fractallogger"
@@ -25,13 +26,15 @@ type MandelboxStatus string
 
 // These represent the currently-defined statuses for mandelboxes.
 const (
-	MandelboxStatusAllocated MandelboxStatus = "ALLOCATED"
-	MandelboxStatusRunning   MandelboxStatus = "RUNNING"
-	MandelboxStatusDying     MandelboxStatus = "DYING"
+	MandelboxStatusAllocated  MandelboxStatus = "ALLOCATED"
+	MandelboxStatusConnecting MandelboxStatus = "CONNECTING"
+	MandelboxStatusRunning    MandelboxStatus = "RUNNING"
+	MandelboxStatusDying      MandelboxStatus = "DYING"
 )
 
 // VerifyAllocatedMandelbox verifies that this host service is indeed expecting
-// the provided mandelbox for the given user.
+// the provided mandelbox for the given user, and if found marks it as
+// connecting.
 func VerifyAllocatedMandelbox(userID types.UserID, mandelboxID types.FractalID) error {
 	if !enabled {
 		return nil
@@ -40,12 +43,21 @@ func VerifyAllocatedMandelbox(userID types.UserID, mandelboxID types.FractalID) 
 		return utils.MakeError("VerifyAllocatedMandelbox() called but dbdriver is not initialized!")
 	}
 
+	// Create a transaction to register the instance, since we are querying and
+	// writing separately.
+	tx, err := dbpool.BeginTx(context.Background(), pgx.TxOptions{IsoLevel: pgx.Serializable})
+	if err != nil {
+		return utils.MakeError("Couldn't register instance: unable to begin transaction: %s", err)
+	}
+	// Safe to do even if committed -- see tx.Rollback() docs.
+	defer tx.Rollback(context.Background())
+
 	instanceName, err := aws.GetInstanceName()
 	if err != nil {
 		return utils.MakeError("Couldn't verify mandelbox for user %s: couldn't get instance name: %s", userID, err)
 	}
 
-	q := queries.NewQuerier(dbpool)
+	q := queries.NewQuerier(tx)
 	rows, err := q.FindMandelboxesForUser(context.Background(), string(instanceName), string(userID))
 	if err != nil {
 		return utils.MakeError("Couldn't verify mandelbox for user %s: couldn't verify query: %s", userID, err)
@@ -63,6 +75,20 @@ func VerifyAllocatedMandelbox(userID types.UserID, mandelboxID types.FractalID) 
 	} else if rows[0].MandelboxID.String != string(mandelboxID) {
 		return utils.MakeError(`Couldn't verify mandelbox for user %s: found an allocated mandelbox row in the database, but it has the wrong fractalID. Expected "%s", got "%s".`, userID, rows[0].MandelboxID.String, mandelboxID)
 	}
+
+	// Mark the container as connecting. We can't just use WriteMandelboxStatus
+	// since we want to do it in a single transaction.
+	result, err := q.WriteMandelboxStatus(context.Background(), pgtype.Varchar{
+		String: string(MandelboxStatusConnecting),
+		Status: pgtype.Present,
+	}, string(mandelboxID))
+	if err != nil {
+		return utils.MakeError("Couldn't write status %s for mandelbox %s: error updating existing row in table `hardware.mandelbox_info`: %s", MandelboxStatusConnecting, mandelboxID, err)
+	}
+	logger.Infof("Updated status in database for mandelbox %s to %s: %s", mandelboxID, MandelboxStatusConnecting, result)
+
+	// Finish the transaction
+	tx.Commit(context.Background())
 
 	return nil
 }
