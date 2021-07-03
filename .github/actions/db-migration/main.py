5b5b023635b7dae38c8876e1957ebe7c4bb35a07
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2021-07-02 21:50:13 -0400

    Move migration workflow to GitHub Action (#3003)

---
+++ .github/actions/db-migration/main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# The exit codes for this script will mimic the migra diff tool
+# that it depends on.
+#
+# migra uses the following exit codes:
+# 0 is a successful run, producing no diff (identical schemas)
+# 1 is a error
+# 2 is a successful run, producing a diff (non-identical schemas)
+# 3 is a successful run, but producing no diff, meaning the diff is "unsafe"
+#
+# We'll introduce our own exit codes to mark certain issues:
+# 4 is a successful run, but producing a diff that does not result in
+#   identical databases upon application.
+
+import os
+import sys
+import helpers.messages as msg
+
+sys.path.append(".github/workflows/helpers")
+
+from resources import heroku_config
+from helpers.utils import exec_sql, dump_schema, diff_schema, replace_postgres
+from rich import print, traceback
+
+traceback.install()
+
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+GITHUB_ISSUE = os.environ.get("GITHUB_ISSUE")
+GITHUB_BRANCH = os.environ.get("GITHUB_BRANCH")
+GITHUB_REPO = "fractal/fractal"
+GITHUB_PR_URL = "https://github.com/fractal/fractal/pull/"
+HEROKU_API_TOKEN = os.environ.get("HEROKU_DEVELOPER_API_KEY")
+SERVER_DEV = "fractal-dev-server"
+SERVER_STAGING = "fractal-staging-server"
+SERVER_PROD = "fractal-prod-server"
+PATH_BRANCH_SCHEMA = "main-webserver/db_migration/schema.sql"
+COMMENT_IDENTIFIER = "AUTOMATED_DB_MIGRATION_MESSAGE"
+
+
+def db_diff(sql_a, sql_b, db_a="postgresql:///a", db_b="postgresql:///b"):
+    exec_sql(db_a, sql_a)
+    exec_sql(db_b, sql_b)
+
+    code, diff = diff_schema(db_a, db_b)
+
+    if diff:
+        exec_sql(db_a, diff)
+
+        _, check_diff = diff_schema(db_a, db_b)
+
+    if check_diff:
+        return 4, check_diff
+    return code, diff
+
+
+if __name__ == "__main__":
+    if GITHUB_BRANCH == "prod":
+        config = heroku_config(GITHUB_BRANCH)
+    elif GITHUB_BRANCH == "staging":
+        config = heroku_config(SERVER_STAGING)
+    else:
+        config = heroku_config(SERVER_DEV)
+
+    db_url = replace_postgres(config["DATABASE_URL"])
+
+    with open(PATH_BRANCH_SCHEMA, "r") as f:
+        branch_schema = f.read()
+
+    return_code, schema_diff = db_diff(dump_schema(db_url), branch_schema)
+
+    if schema_diff:
+        print(schema_diff)
+    sys.exit(return_code)
