79d4e1c06c775acf04b90f920ba7315f36267687
Savvy Raghuvanshi <sraghuvanshi@college.harvard.edu>
2021-07-06 12:57:56 -0400

    Remove old code related to task definitions (#2882, plus some more)

--- main-webserver/app/models/_meta.py
+++ main-webserver/app/models/_meta.py
@@ -6,17 +6,6 @@ It's necessary to instantiate SQLAlchemy here so that all of the models can
 subclass db.Model without creating circular imports.
 """
 
-from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(engine_options={"pool_pre_ping": True})
-
-
-def secret_key():
-    """Retrieve the secret key from the Flask application's configuration.
-
-    Returns:
-        The value of the SECRET_KEY configuration variable, a string.
-    """
-
-    return current_app.secret_key
