4d2266d6ed18fb6fb8530db19399aa93759c9d53
djsavvy <sraghuvanshi@college.harvard.edu>
2021-07-09 15:07:35 -0400

    Default to LOCAL environment

--- client-applications/desktop/src/config/environment.ts
+++ client-applications/desktop/src/config/environment.ts
@@ -20,9 +20,9 @@ const getDevelopmentConfig = () => {
       return configs.PRODUCTION
     default:
       console.warn(
-        `Got an unrecognized DEVELOPMENT_ENV: ${devEnv}. Defaulting to ${FractalEnvironments.DEVELOPMENT}`
+        `Got an unrecognized DEVELOPMENT_ENV: ${devEnv}. Defaulting to ${FractalEnvironments.LOCAL}`
       )
-      return configs.DEVELOPMENT
+      return configs.LOCAL
   }
 }
 
