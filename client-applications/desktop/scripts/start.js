f324ce9416e17313439f5cd98f067bf98f3cf3bf
Roshan Padaki <roshan@fractal.co>
2021-07-09 12:18:01 -0400

    Add `--show-protocol-logs` option (#3135)

--- client-applications/desktop/scripts/start.js
+++ client-applications/desktop/scripts/start.js
@@ -1,6 +1,7 @@
 // Build the protocol, build tailwind, and run `snowpack dev`
 
 const helpers = require("./build-package-helpers")
+const yargs = require("yargs")
 
 const start = (env, ..._args) => {
   helpers.buildAndCopyProtocol()
@@ -11,5 +12,12 @@ const start = (env, ..._args) => {
 module.exports = start
 
 if (require.main === module) {
-  start()
+  const argv = yargs(process.argv.slice(2))
+    .boolean("show-protocol-logs")
+    .alias("show-protocol-logs", ["P"])
+    .describe("show-protocol-logs", "Print the client protocol logs to stdout.")
+    .help()
+    .version().argv
+
+  start({ SHOW_PROTOCOL_LOGS: argv.showProtocolLogs ? "true" : "false" })
 }
