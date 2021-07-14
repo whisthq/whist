d5b3a8ee9a8425c56fc1f0fec09864f89f1bd63a
Nicholas Pipitone <npip99@gmail.com>
2021-07-14 22:07:01 +0000

    Replace printf with LOG_

--- protocol/fractal/video/nvidia-linux/NvFBCUtils.c
+++ protocol/fractal/video/nvidia-linux/NvFBCUtils.c
@@ -31,6 +31,7 @@
 #include <sys/time.h>
 
 #include "NvFBCUtils.h"
+#include <fractal/core/fractal.h>
 
 #define BITMAP_ROW_SIZE(width) (((width * Bpp) + 3) & ~3)
 #define BITMAP_INDEX(x, y, rowSize) ((y * rowSize) + (x * Bpp))
@@ -103,13 +104,13 @@ static int NvFBCUtilsSaveBitmap(const char *filename, const unsigned char *data,
     FILE *fd = NULL;
 
     if (data == NULL) {
-        fprintf(stderr, "%s: There is no data to write\n", __FUNCTION__);
+        LOG_ERROR("%s: There is no data to write\n", __FUNCTION__);
         return 1;
     }
 
     fd = fopen(filename, "wb");
     if (fd == NULL) {
-        fprintf(stderr, "%s: Unable to open file '%s'\n", __FUNCTION__, filename);
+        LOG_ERROR("%s: Unable to open file '%s'\n", __FUNCTION__, filename);
         return 1;
     }
 
@@ -357,7 +358,7 @@ int NvFBCUtilsSaveFrame(NVFBC_BUFFER_FORMAT format, const char *filename, unsign
         case NVFBC_BUFFER_FORMAT_YUV444P:
             return NvFBCUtilsSaveYUVPlanar(format, filename, data, width, height);
         default:
-            fprintf(stderr, "%s: Unknown buffer format\n", __FUNCTION__);
+            LOG_ERROR("%s: Unknown buffer format\n", __FUNCTION__);
             return 1;
     }
 }
@@ -377,28 +378,28 @@ void NvFBCUtilsPrintStatus(NVFBC_GET_STATUS_PARAMS *status) {
         return;
     }
 
-    printf("Status:\n");
-    printf("- NvFBC library API version: %u.%u\n", status->dwNvFBCVersion >> 8 & 0xf,
-           status->dwNvFBCVersion & 0xf);
-    printf("- This system supports FBC: %s\n", status->bIsCapturePossible ? "Yes" : "No");
-    printf("- Curently capturing: %s\n", status->bCurrentlyCapturing ? "Yes" : "No");
-    printf("- Can create an FBC instance: %s\n", status->bCanCreateNow ? "Yes" : "No");
-    printf("- X screen (framebuffer) size: %ux%u\n", status->screenSize.w, status->screenSize.h);
-    printf("- XrandR extension available: %s\n", status->bXRandRAvailable ? "Yes" : "No");
+    LOG_INFO("Status:\n");
+    LOG_INFO("- NvFBC library API version: %u.%u\n", status->dwNvFBCVersion >> 8 & 0xf,
+             status->dwNvFBCVersion & 0xf);
+    LOG_INFO("- This system supports FBC: %s\n", status->bIsCapturePossible ? "Yes" : "No");
+    LOG_INFO("- Curently capturing: %s\n", status->bCurrentlyCapturing ? "Yes" : "No");
+    LOG_INFO("- Can create an FBC instance: %s\n", status->bCanCreateNow ? "Yes" : "No");
+    LOG_INFO("- X screen (framebuffer) size: %ux%u\n", status->screenSize.w, status->screenSize.h);
+    LOG_INFO("- XrandR extension available: %s\n", status->bXRandRAvailable ? "Yes" : "No");
 
     if (status->bXRandRAvailable) {
         int i;
 
-        printf("- Connected RandR outputs with CRTC:\n");
+        LOG_INFO("- Connected RandR outputs with CRTC:\n");
 
         for (i = 0; i < (int)status->dwOutputNum; i++) {
             NVFBC_RANDR_OUTPUT_INFO output;
 
             output = status->outputs[i];
 
-            printf("  * '%s' (id: 0x%x), CRTC: %ux%u+%u+%u\n", output.name, output.dwId,
-                   output.trackedBox.w, output.trackedBox.h, output.trackedBox.x,
-                   output.trackedBox.y);
+            LOG_INFO("  * '%s' (id: 0x%x), CRTC: %ux%u+%u+%u\n", output.name, output.dwId,
+                     output.trackedBox.w, output.trackedBox.h, output.trackedBox.x,
+                     output.trackedBox.y);
         }
     }
 }
@@ -439,7 +440,7 @@ uint32_t NvFBCUtilsGetOutputId(NVFBC_RANDR_OUTPUT_INFO *outputs, uint32_t output
 }
 
 void NvFBCUtilsPrintVersions(const unsigned int appVersion) {
-    printf("Application version: %u\n", appVersion);
-    printf("NvFBC API version: %u.%u\n", NVFBC_VERSION_MAJOR, NVFBC_VERSION_MINOR);
-    printf("\n");
+    LOG_INFO("Application version: %u\n", appVersion);
+    LOG_INFO("NvFBC API version: %u.%u\n", NVFBC_VERSION_MAJOR, NVFBC_VERSION_MINOR);
+    LOG_INFO("\n");
 }
