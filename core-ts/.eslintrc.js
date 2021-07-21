0f896cbd9cd8825b6ed45741fd908fd684326dff
Philippe Noël <phil@fractal.co>
2021-07-21 12:42:34 -0400

    properly configure tsconfig and eslintrc

--- core-ts/.eslintrc.js
+++ core-ts/.eslintrc.js
@@ -1,32 +1,28 @@
 module.exports = {
     env: {
-      browser: true,
-      es2021: true,
+        browser: true,
+        es2021: true,
+        node: true
     },
     extends: [
-      "plugin:react/recommended",
-      "standard-with-typescript",
-      "plugin:prettier/recommended",
+        "standard",
+        "prettier",
+        "prettier/prettier",
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: "module",
-      project: "./tsconfig.json",
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: "module"
     },
-    plugins: ["react", "@typescript-eslint", "prettier"],
+    plugins: [
+        ["@typescript-eslint", "prettier"]
+    ],
     rules: {
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/consistent-type-assertions": "off",
-      "prettier/prettier": "error",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/consistent-type-assertions": "off",
+        "prettier/prettier": "error",
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  };
-  
+}
