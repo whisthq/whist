30c691afbe4d72ed840ad60a1cc2c5097f8a0e7a
Philippe Noël <phil@fractal.co>
2021-07-21 12:36:23 -0400

    standardize core-ts settings

--- core-ts/.eslintrc.js
+++ core-ts/.eslintrc.js
@@ -1,26 +1,32 @@
 module.exports = {
     env: {
-        browser: true,
-        es2021: true,
-        node: true
+      browser: true,
+      es2021: true,
     },
     extends: [
-        "standard",
-        "prettier",
-        "prettier/prettier",
+      "plugin:react/recommended",
+      "standard-with-typescript",
+      "plugin:prettier/recommended",
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12,
-        sourceType: "module"
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: "module",
+      project: "./tsconfig.json",
     },
-    plugins: [
-        "@typescript-eslint"
-    ],
+    plugins: ["react", "@typescript-eslint", "prettier"],
     rules: {
-        quotes: [2, "double", { avoidEscape: true }]
-    }
-}
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/consistent-type-assertions": "off",
+      "prettier/prettier": "error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  };
+  
