879f30ddd05473a817b24b57f5d9c4cc6fca7d5d
Roshan Padaki <roshan@fractal.co>
2021-07-21 12:43:27 -0400

    Upgrade to latest g4 GRID driver

--- ecs-host-setup/get-nvidia-driver-installer.sh
+++ ecs-host-setup/get-nvidia-driver-installer.sh
@@ -6,5 +6,5 @@
 set -Eeuo pipefail
 
 # Hardcode a GRID driver version to avoid server/client version mismatch. This driver version needs to be updated periodically.
-aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/g4/grid-11.2/NVIDIA-Linux-x86_64-450.89-grid-aws.run nvidia-driver-installer.run
+aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/g4/grid-12.0/NVIDIA-Linux-x86_64-460.32.03-grid-aws.run nvidia-driver-installer.run
 chmod +x ./nvidia-driver-installer.run
