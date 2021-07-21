7a252fcb7cc719cbac69c2c74b790905a4028f51
Philippe Noël <phil@fractal.co>
2021-07-21 10:52:00 -0400

    use same Prettier settings across client-apps and website

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:prettier/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -11,16 +15,17 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: "module",
-    project: "./tsconfig.json"
+    project: "./tsconfig.json",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    "no-use-before-define": "off",
-    "@typescript-eslint/explicit-function-return-type": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/consistent-type-assertions": "off",
+    "prettier/prettier": "error",
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
-};
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
