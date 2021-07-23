cae965c7d554c2d657d66819cbbfd747ed5f35ab
Neil Hansen <neil.hansen.31@gmail.com>
2021-07-23 10:03:28 -0700

    Add payment and auth functions

---
+++ core-ts/src/types/data.ts
@@ -0,0 +1,25 @@
+// auth
+export type accessToken = { accessToken: string }
+export type refreshToken = { refreshToken: string }
+export type configToken = { configToken: string }
+export type jwtIdentity = { jwtIdentity: string }
+
+export type authCallbackURL = { authCallbackURL: string }
+
+// mandelbox
+export type mandelboxIP = { mandelboxIP: string }
+export type mandelboxPorts = {
+    mandelboxPorts: {
+        port_32262: number
+        port_32263: number
+        port_32273: number
+    }
+}
+export type mandelboxSecret = { mandelboxSecret: string }
+export type regionAWS = { regionAWS: string }
+
+// user
+export type userEmail = { userEmail: string }
+export type userPassword = { userPassword: string }
+
+export type paymentPortalURL = { paymentPortalURL: string }
