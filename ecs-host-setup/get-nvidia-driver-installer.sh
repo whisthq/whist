01b4c6d06dfdc4926b130ad3e550fa6256ee0e6f
Roshan Padaki <roshan@fractal.co>
2021-07-26 16:35:12 -0400

    Use 12.2 not 12.0

--- ecs-host-setup/get-nvidia-driver-installer.sh
+++ ecs-host-setup/get-nvidia-driver-installer.sh
@@ -9,5 +9,5 @@ set -Eeuo pipefail
 # versions later than 11.0 to g4/latest, but will keep version 11.0 and the earlier versions specific to G4dn under g4/latest.
 
 # Hardcode a GRID driver version to avoid server/client version mismatch. This driver version needs to be updated periodically.
-aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/grid-12.0/NVIDIA-Linux-x86_64-460.32.03-grid-aws.run nvidia-driver-installer.run
+aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/grid-12.2/NVIDIA-Linux-x86_64-460.73.01-grid-aws.run nvidia-driver-installer.run
 chmod +x ./nvidia-driver-installer.run
