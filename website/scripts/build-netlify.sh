5fbf9e42940a7879142103eb83867dd825fc4aa4
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2021-07-26 16:40:05 -0400

    Specify proper Nvidia drivers and add documentation (#3302)
    * specify proper nvidia driver and add documentation
    
    * beautysh
    
    * Use 12.2 not 12.0
    
    Co-authored-by: Roshan Padaki <roshan@fractal.co>

--- website/scripts/build-netlify.sh
+++ website/scripts/build-netlify.sh
@@ -4,9 +4,9 @@ set -Eeuo pipefail
 
 echo "Logging Netlify Build Environment (to debug netlify.toml)"
 printf "COMMIT_REF %s | CACHED_COMMIT_REF %s | PWD %s\n" \
-        $COMMIT_REF     $CACHED_COMMIT_REF     $PWD
+    $COMMIT_REF     $CACHED_COMMIT_REF     $PWD
 printf "URL %s | BRANCH %s | HEAD %s | PULL_REQUEST %s\n" \
-        $URL     $BRANCH     $HEAD     $PULL_REQUEST
+    $URL     $BRANCH     $HEAD     $PULL_REQUEST
 
 yarn build && rm -rf /opt/build/cache
 
