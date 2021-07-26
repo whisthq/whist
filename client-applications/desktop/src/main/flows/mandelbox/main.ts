05fbd659c933373fdfc21023693660b36814f550
Neil Hansen <neil.hansen.31@gmail.com>
2021-07-26 15:55:42 -0700

    Add retry to mandelboxFlow

--- client-applications/desktop/src/main/flows/mandelbox/main.ts
+++ client-applications/desktop/src/main/flows/mandelbox/main.ts
@@ -1,7 +1,9 @@
 import { screen } from "electron"
-import { merge, Observable, zip } from "rxjs"
+import { head } from "lodash"
+import { merge, zip } from "rxjs"
 import { map } from "rxjs/operators"
 import { mandelboxCreateFlow } from "@app/main/flows/mandelbox/create"
+import { authRefreshFlow } from "@app/main/flows/auth"
 import { hostSpinUpFlow } from "@app/main/flows/mandelbox/host"
 import { flow } from "@app/utils/flows"
 import { AWSRegion } from "@app/@types/aws"
@@ -9,10 +11,10 @@ import { AWSRegion } from "@app/@types/aws"
 export const mandelboxFlow = flow<{
     subClaim: string
     accessToken: string
+    refreshToken: string
     configToken: string
     region?: AWSRegion
 }>("mandelboxFlow", (trigger) => {
-    trigger.subscribe((x) => console.log("mSubscribe", x))
     const dpiStream = trigger.pipe(
         map(() => Math.round(screen.getPrimaryDisplay().scaleFactor * 96))
     )
@@ -28,8 +30,21 @@ export const mandelboxFlow = flow<{
         )
     )
 
+    const refreshedAuthInfo = authRefreshFlow(
+        zip([trigger, create.expired]).pipe(map(head))
+    )
+
+    const retry = mandelboxCreateFlow(
+        zip([refreshedAuthInfo.success, trigger]).pipe(
+            map(([authInfo, { region }]) => ({
+                ...authInfo,
+                region: region,
+            }))
+        )
+    )
+
     const host = hostSpinUpFlow(
-        zip([trigger, create.success, dpiStream]).pipe(
+        zip([trigger, merge(create.success, retry.success), dpiStream]).pipe(
             map(([t, c, dpi]) => ({
                 ip: c.ip,
                 dpi: dpi,
@@ -43,6 +58,6 @@ export const mandelboxFlow = flow<{
 
     return {
         success: host.success,
-        failure: merge(create.failure, host.failure),
+        failure: merge(create.failure, refreshedAuthInfo.failure, host.failure),
     }
 })
