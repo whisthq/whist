791ea7a54e624e2d5139a20024ee6a7052430184
Neil Hansen <neil.hansen.31@gmail.com>
2021-07-26 16:01:00 -0700

    Add refresh token to flow index type

--- client-applications/desktop/src/main/flows/index.ts
+++ client-applications/desktop/src/main/flows/index.ts
@@ -40,6 +40,7 @@ const launchTrigger = fromTrigger(TRIGGER.authFlowSuccess).pipe(
 ) as Observable<{
     subClaim: string
     accessToken: string
+    refreshToken: string
     configToken: string
     region?: AWSRegion
 }>
