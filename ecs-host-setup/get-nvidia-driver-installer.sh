66d34f397d49b6a2021c89f076659dbda93c387f
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2021-07-26 16:40:05 -0400

    Specify proper Nvidia drivers and add documentation (#3302)
    * specify proper nvidia driver and add documentation
    
    * beautysh
    
    * Use 12.2 not 12.0
    
    Co-authored-by: Roshan Padaki <roshan@fractal.co>

--- ecs-host-setup/get-nvidia-driver-installer.sh
+++ ecs-host-setup/get-nvidia-driver-installer.sh
@@ -5,6 +5,9 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
+# From AWS: Starting with GRID version 11.0, you can use the driver packages under latest for both G3 and G4dn instances. We will not add
+# versions later than 11.0 to g4/latest, but will keep version 11.0 and the earlier versions specific to G4dn under g4/latest.
+
 # Hardcode a GRID driver version to avoid server/client version mismatch. This driver version needs to be updated periodically.
-aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/g4/grid-12.0/NVIDIA-Linux-x86_64-460.32.03-grid-aws.run nvidia-driver-installer.run
+aws s3 cp --only-show-errors s3://ec2-linux-nvidia-drivers/grid-12.2/NVIDIA-Linux-x86_64-460.73.01-grid-aws.run nvidia-driver-installer.run
 chmod +x ./nvidia-driver-installer.run
--- mandelbox-images/browsers/firefox/start-firefox.sh
+++ mandelbox-images/browsers/firefox/start-firefox.sh
@@ -3,11 +3,11 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
-# Use X11 (Wayland not supported by native NVIDIA driver yet) 
+# Use X11 (Wayland not supported by native NVIDIA driver yet)
 export MOZ_X11_EGL=1
 
 # Start Firefox with custom profile, set as default browser, and open Google
 exec firefox \
-        -profile /home/fractal/.mozilla/firefox/fractal_profile \
-        -setDefaultBrowser \
-        -url "google.com"
+    -profile /home/fractal/.mozilla/firefox/fractal_profile \
+    -setDefaultBrowser \
+    -url "google.com"
