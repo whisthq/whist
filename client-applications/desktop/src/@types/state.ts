d04a90eb1d9d792de20489cd1493a4d238a19d08
Neil Hansen <neil.hansen.31@gmail.com>
2021-07-29 21:06:02 -0700

    lint

--- client-applications/desktop/src/@types/state.ts
+++ client-applications/desktop/src/@types/state.ts
@@ -1,8 +1,7 @@
 import { Trigger } from "@app/utils/flows"
 
-export type AsyncReturnType<
-    T extends (...args: any) => Promise<any>
-> = T extends (...args: any) => Promise<infer R> ? R : any
+export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
+    T extends (...args: any) => Promise<infer R> ? R : any
 
 export interface StateIPC {
     userEmail: string // User email returned by Auth0
