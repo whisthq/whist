4b3d47bef06e407d6dfe0ef1b1cf2e2c532160b5
Serina Hu <serina@fractal.co>
2021-08-03 12:59:33 -0400

    clang-format

--- protocol/client/main.c
+++ protocol/client/main.c
@@ -452,8 +452,8 @@ int main(int argc, char* argv[]) {
 
         // Create thread to receive all packets and handle them as needed
         run_sync_udp_packets = true;
-        SDL_Thread* sync_udp_packets_thread =
-            SDL_CreateThread(multithreaded_sync_udp_packets, "ReceivePackets", &packet_receive_context);
+        SDL_Thread* sync_udp_packets_thread = SDL_CreateThread(
+            multithreaded_sync_udp_packets, "ReceivePackets", &packet_receive_context);
 
         // Create thread to send and receive TCP packets
         run_sync_tcp_packets = true;
