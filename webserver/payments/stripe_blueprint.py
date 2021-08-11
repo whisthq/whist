08b6c4974a9c94dd8a6cb7def67ce1f1b26e1e6e
Owen Niles <owen@fractalcomputers.com>
2021-08-11 01:02:05 -0400

    Return either a Stripe checkout or customer portal session URL from /payment_portal_url (#3498)
    * Return either a Stripe checkout or customer portal session URL.
    
    When a user with an ongoing Fractal Stripe subscription calls the
    /payment_portal_url endpoint, they get back a Stripe customer portal session
    URL. When a user without a Fractal Stripe subscription calls the endpoint, they
    get back a Stripe checkout session URL.
    
    * Call the new /payment_portal_url endpoint from the desktop application.

--- webserver/payments/stripe_blueprint.py
+++ webserver/payments/stripe_blueprint.py
@@ -1,51 +1,61 @@
-from http import HTTPStatus
+from typing import Any
 
-from flask import Blueprint
+import stripe
+from flask import Blueprint, current_app, jsonify
 from flask_jwt_extended import jwt_required
 
-from app import fractal_pre_process
-from app.helpers.utils.general.limiter import limiter, RATE_LIMIT_PER_MINUTE
-from payments import get_stripe_customer_id
-from payments.stripe_helpers import (
-    get_billing_portal_url,
-)
+from payments import get_stripe_customer_id, get_subscription_status
 
 stripe_bp = Blueprint("stripe_bp", __name__)
 
 
-@stripe_bp.route("/stripe/customer_portal", methods=["POST"])
-@limiter.limit(RATE_LIMIT_PER_MINUTE)
-@fractal_pre_process
+@stripe_bp.route("/payment_portal_url")
 @jwt_required()
-def customer_portal(**kwargs):
-    """
-    Returns billing portal url.
+def payment_portal() -> Any:
+    """Creates a Stripe checkout or customer portal session an returns the URL.
 
-    Args:
-        return_url (str): the url to redirect to upon leaving the billing portal
+    If the authenticated user already has an active Fractal subscription, this endpoint creates a
+    Stripe customer portal session and returns its URL. By navigating to the URL, the user can use
+    a Stripe-hosted form to manage their subscription and billing information.
+
+    If the authenticated user does not currently have an active Fractal subscription, either
+    because they have never signed up for a Fractal plan before or because their subscription has
+    been automatically (due to a lack of payment) or manually cancelled, this endpoint creates a
+    Stripe checkout session and returns its URL. By navigating to the URL, the user can use a
+    Stripe-hosted form to sign up for a new Fractal subscription.
 
     Returns:
-        json, int: JSON containing billing url and status code. The JSON is in the format
-            {
-                "url": the billing portal url
-            }
-                or
-
-            {
-                "error": {
-                    "message": error message
-                }
-            }
-                if the creation fails
+        A JSON object containing a single key "url", whose value is the URL of a Stripe checkout
+        or customer portal session.
     """
 
-    body = kwargs["body"]
-    try:
-        customer_id = get_stripe_customer_id()
-        return_url = body["return_url"]
-    except:
-        return {
-            "error": "The request body is incorrectly formatted, or the user is not authorized."
-        }, HTTPStatus.BAD_REQUEST
+    customer_id = get_stripe_customer_id()
 
-    return get_billing_portal_url(customer_id, return_url)
+    # If the Stripe customer ID is not present in the access token, something has gone horribly
+    # wrong.
+    assert customer_id
+
+    if get_subscription_status() in (None, "canceled", "incomplete_expired", "unpaid"):
+        # Any user subscriptions that the user might have are in the cancelled state and cannot be
+        # renewed. The user must sign up for a new subscription.
+        session = stripe.checkout.Session.create(
+            customer=customer_id,
+            line_items=[
+                {
+                    "price": current_app.config["STRIPE_PRICE_ID"],
+                    "quantity": 1,
+                }
+            ],
+            payment_method_types=("card",),
+            success_url="http://localhost/callback/payment?success=true",
+            cancel_url="http://localhost/callback/payment?success=false",
+            mode="subscription",
+        )
+    else:
+        # The user has a non-cancelled subscription.
+        session = stripe.billing_portal.Session.create(
+            customer=customer_id,
+            return_url="http://localhost/callback/payment",
+        )
+
+    return jsonify(url=session.url)
