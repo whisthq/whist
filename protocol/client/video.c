0f85d2b3693f95c84015152c499dc59f8d17c576
Serina Hu <serina@fractal.co>
2021-08-10 23:34:36 -0400

    good bitrate

--- protocol/client/video.c
+++ protocol/client/video.c
@@ -705,9 +705,9 @@ void calculate_statistics() {
     // Update mbps every 5 seconds
     if (get_timer(t) > 5.0) {
         // Note: Uncomment to oscillate between bitrates every 5 seconds
-        if (max_bitrate > STARTING_BITRATE) {
-            max_bitrate = STARTING_BITRATE;
-            max_burst_bitrate = STARTING_BURST_BITRATE;
+        if (max_bitrate > GOOD_STARTING_BITRATE) {
+            max_bitrate = GOOD_STARTING_BITRATE;
+            max_burst_bitrate = GOOD_STARTING_BURST_BITRATE;
         } else {
             // max_bitrate += 1000000;
             // max_burst_bitrate += 1000000;
