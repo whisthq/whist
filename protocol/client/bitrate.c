b51e20e910fcb3604dd7f07c45725d68efbada31
Serina Hu <serina@fractal.co>
2021-08-11 01:40:51 -0400

    documentation

--- protocol/client/bitrate.c
+++ protocol/client/bitrate.c
@@ -1,14 +1,49 @@
+/**
+ * Copyright Fractal Computers, Inc. 2021
+ * @file bitrate.h
+ * @brief This file contains the client's adaptive bitrate code. Any algorithms we are using for
+predicting bitrate should be stored here.
+============================
+Usage
+============================
+The client should periodically call calculate_new_bitrate with any data rrequested, such as
+throughput or nack data. This will update max_bitrate and max_burst_bitrate if needed.
+*/
+
+/*
+============================
+Includes
+============================
+*/
 #include "bitrate.h"
+
 volatile int max_bitrate = STARTING_BITRATE;
 volatile int max_burst_bitrate = STARTING_BURST_BITRATE;
 #define BAD_BITRATE 10400000
 #define BAD_BURST_BITRATE 31800000
 
+/*
+============================
+Private Functions
+============================
+*/
+void fallback_bitrate(int num_nacks_per_second);
+
+/*
+============================
+Private Function Implementations
+============================
+*/
 void fallback_bitrate(int num_nacks_per_second) {
-    // our first dumb algorithm:
-    // if we're nacking a lot, default to the old 10mbps/30mbps
-    // and if we're on the old bad bitrate and not nacking, keep adding mbps until we hit the 16mbps
-    // threshold
+    /*
+        Switches between two sets of bitrate/burst bitrate: the default of 16mbps/100mbps and a
+       fallback of 10mbps/30mbps. We fall back if we've nacked a lot in the last second. Otherwise,
+       we add 1mbps to the bitrate, up to 16mbps, at which point we switch back to 16/100.
+
+        Arguments:
+            num_nacks_per_second (int): the average number of nacks per second since the last time
+       this function was called
+    */
     if (num_nacks_per_second > 6 && max_bitrate != BAD_BITRATE) {
         max_bitrate = BAD_BITRATE;
         max_burst_bitrate = BAD_BURST_BITRATE;
@@ -23,8 +58,26 @@ void fallback_bitrate(int num_nacks_per_second) {
     }
 }
 
+/*
+============================
+Public Function Implementations
+============================
+*/
 bool calculate_new_bitrate(int num_nacks_per_second) {
+    /*
+        Update max_bitrate and max_burst_bitrate (if necessary) using client data. Returns whether
+       or not the client should request bitrate changes from the server.
+
+        Arguments:
+            num_nacks_per_second (int): number of nacks per second since this function was last
+       called
+
+        Returns:
+            (bool): whether or not the client should request a bitrate change from the server
+    */
     int old_bitrate = max_bitrate;
     fallback_bitrate(num_nacks_per_second);
-    return old_bitrate != max_bitrate;
+    bool should_update = (old_bitrate != max_bitrate) &&
+                         (max_bitrate == STARTING_BITRATE || max_bitrate == BAD_BITRATE);
+    return should_update;
 }
--- protocol/client/bitrate.h
+++ protocol/client/bitrate.h
@@ -1,7 +1,39 @@
 #ifndef CLIENT_BITRATE_H
 #define CLIENT_BITRATE_H
+/**
+ * Copyright Fractal Computers, Inc. 2021
+ * @file bitrate.h
+ * @brief This file contains the client's adaptive bitrate code. Any algorithms we are using for
+predicting bitrate should be stored here.
+============================
+Usage
+============================
+The client should periodically call calculate_new_bitrate with any data rrequested, such as
+throughput or nack data. This will update max_bitrate and max_burst_bitrate if needed.
+*/
+
+/*
+============================
+Includes
+============================
+*/
 
 #include <fractal/core/fractal.h>
 
+/*
+============================
+Public Functions
+============================
+*/
+
+/**
+ * @brief       Update max_bitrate and max_burst_bitrate with the latest client data. Return whether
+ * or not those bitrates have actually changed. This function does not trigger an update bitrate
+ * message to the server.
+ *
+ * @param num_nacks         The number of nacks that occurred in the last secod
+ *
+ * @return      Whether or not we changed max_bitrate and max_burst_bitrate
+ */
 bool calculate_new_bitrate(int num_nacks);
 #endif
