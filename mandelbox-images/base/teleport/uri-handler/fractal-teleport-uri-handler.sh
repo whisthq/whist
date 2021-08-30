d4c43be39c7b112cde0f64830d12ad94692f28fa
Roshan Padaki <roshan@fractal.co>
2021-08-30 22:21:11 +0000

    trigger 'Open URI' server events from the URI handler script

--- mandelbox-images/base/teleport/uri-handler/fractal-teleport-uri-handler.sh
+++ mandelbox-images/base/teleport/uri-handler/fractal-teleport-uri-handler.sh
@@ -2,6 +2,10 @@
 
 set -Eeuo pipefail
 
-# TODO: We need to do something here which triggers a protocol `FractalServerMessage`
-# that includes the uri in "$1"
-echo "$1"
+# Only write if there isn't a URL currently being handled
+# Note that writes of size 4096 and under are atomic on
+# Linux, so the only edge case is if there's a really long
+# URL, in which case it's possible for it to be truncated.
+if [[ ! -f /home/fractal/.teleport/handled-uri ]]; then
+    echo -n "$1" > /home/fractal/.teleport/handled-uri
+fi
