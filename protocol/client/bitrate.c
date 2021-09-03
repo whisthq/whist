0d626efd44c554a0cf66b8127e185d3fb77d3475
Suriya Kandaswamy <suriya@tryfractal.com>
2021-09-03 16:12:33 -0400

    Gradual adaptive bitrate (#3667)
    * basic throughput calculation
    
    * fix var name
    
    * gradual determination of bitrate
    
    * adapt burst bitrate based on skipped frames
    
    * limit saved throughput to maximum and minimum bitrates
    
    * clang-format
    
    * fix assignments
    
    * implement increased threshold bitrate fallback
    
    * clang-format
    
    * remove LOG_INFO statements
    
    * move around some intialization
    
    * clang-format
    
    * cast const?
    
    * change some consts to enum
    
    * try to trick compiler
    
    * nolint enum
    
    * NOLINT PLS WTF
    
    * change some naming and logic to make more sense
    
    * define STATISTICS_SECONDS constant
    
    * clang-format
    
    * add tick

--- protocol/client/bitrate.c
+++ protocol/client/bitrate.c
@@ -2,15 +2,21 @@
  * Copyright Fractal Computers, Inc. 2021
  * @file bitrate.h
  * @brief This file contains the client's adaptive bitrate code. Any algorithms we are using for
-predicting bitrate should be stored here.
+ *        predicting bitrate should be stored here.
 ============================
 Usage
 ============================
 Place to put any predictive/adaptive bitrate algorithms. In the current setup, each algorithm is a
 function that takes in inputs through a BitrateStatistics struct. The function is responsible for
 maintaining any internal state necessary for the algorithm (possibly through the use of custom types
-or helpers), and should update max_bitrate when necessary. To change the algorithm the client uses,
-set calculate_new_bitrate to the desired algorithm.
+or helpers), and should update max_bitrate when necessary.
+
+To change the algorithm the client uses, set `calculate_new_bitrate` to the desired algorithm.
+The function assigned to `calculate_new_bitrate` should have signature such that it takes
+one argument of type `BitrateStatistics` and returns type `Bitrates`.
+
+For more details about the individual algorithms, please visit
+https://www.notion.so/tryfractal/Adaptive-Bitrate-Algorithms-a6ea0987adc04e8d84792fc9dcbc7fc7
 */
 
 /*
@@ -32,6 +38,7 @@ Private Functions
 */
 Bitrates fallback_bitrate(BitrateStatistics stats);
 Bitrates ewma_bitrate(BitrateStatistics stats);
+Bitrates ewma_ratio_bitrate(BitrateStatistics stats);
 
 /*
 ============================
@@ -41,7 +48,7 @@ Private Function Implementations
 Bitrates fallback_bitrate(BitrateStatistics stats) {
     /*
         Switches between two sets of bitrate/burst bitrate: the default of 16mbps/100mbps and a
-       fallback of 10mbps/30mbps. We fall back if we've nacked a lot in the last second.
+        fallback of 10mbps/30mbps. We fall back if we've nacked a lot in the last second.
 
         Arguments:
             stats (BitrateStatistics): struct containing the average number of nacks per second
@@ -61,7 +68,7 @@ Bitrates fallback_bitrate(BitrateStatistics stats) {
 Bitrates ewma_bitrate(BitrateStatistics stats) {
     /*
         Keeps an exponentially weighted moving average of the throughput per second the client is
-       getting, and uses that to predict a good bitrate to ask the server for.
+        getting, and uses that to predict a good bitrate to ask the server for.
 
         Arguments:
             stats (BitrateStatistics): struct containing the throughput per second of the client
@@ -84,10 +91,191 @@ Bitrates ewma_bitrate(BitrateStatistics stats) {
     return bitrates;
 }
 
+Bitrates ewma_ratio_bitrate(BitrateStatistics stats) {
+    /*
+        Keeps an exponentially weighted moving average of the throughput per second the client is
+        getting, and uses that to predict a good bitrate to ask the server for. The throughput
+        per second that the client is getting is estimated by the ratio of successful packets to
+        total packets (successful + NACKed) multiplied by the active throughput. Due to the usage
+        of a heuristic for finding current throughput, every `same_throughput_count_threshold`
+        periods, we boost the throughput estimate if the calculated throughput has remained the
+        same for `same_throughput_count_threshold` periods. If the boosted throughput results in
+        NACKs, then we revert to the previous throughput and increase the value of
+        `same_throughput_count_threshold` so that the successful throughput stays constant
+        for longer and longer periods of time before we try higher bitrates. We follow a similar
+        logic flow for the burst bitrate, except we use skipped renders instead of NACKs.
+
+        Arguments:
+            stats (BitrateStatistics): struct containing the throughput per second of the client
+    */
+    static const double alpha = 0.8;
+    // Because the max bitrate of the encoder is usually larger than the actual amount of data we
+    //     get from the server
+    static const double bitrate_throughput_ratio = 1.25;
+    // Multiplier when boosting throughput/bitrate after continuous success
+    static const double boost_multiplier = 1.05;
+
+    // Hacky way of allowing constant assignment to static variable (cannot assign `const` to
+    //     `static`)
+    // In order:
+    //     > the minimum setting for threshold for meeting throughput expectations before boosting
+    //     > the multiplier for threshold after successfully meeting throughput expectations
+    //     > the maximum setting for threshold for meeting throughput expectations before boosting
+    enum {
+        meet_expectations_min = 5,         // NOLINT(readability-identifier-naming)
+        meet_expectations_multiplier = 2,  // NOLINT(readability-identifier-naming)
+        meet_expectations_max = 1024       // NOLINT(readability-identifier-naming)
+    };
+
+    // Target throughput (used to calculate bitrate)
+    static int expected_throughput = -1;
+    // How many times in a row that the real throughput has met expectations
+    static int met_throughput_expectations_count = 0;
+    // The threshold that the current throughput needs to meet to be boosted
+    static int meet_throughput_expectations_threshold = meet_expectations_min;
+    // The latest throughput that met expectations up to threshold
+    static int latest_successful_throughput = -1;
+    // The threshold that the latest successful throughput needs to meet to be boosted
+    static int latest_successful_throughput_threshold = meet_expectations_min;
+
+    // How many times in a row that the burst has met expectations
+    static int met_burst_expectations_count = 0;
+    // The threshold that the current burst needs to meet to be boosted
+    static int meet_burst_expectations_threshold = meet_expectations_min;
+    // The latest burst that met expectations up to threshold
+    static int latest_successful_burst = -1;
+    // The threshold that the latest successful burst needs to meet to be boosted
+    static int latest_successful_burst_threshold = meet_expectations_min;
+
+    static Bitrates bitrates;
+    if (expected_throughput == -1) {
+        expected_throughput = (int)(STARTING_BITRATE / bitrate_throughput_ratio);
+        bitrates.burst_bitrate = max_burst_bitrate;
+    }
+
+    // Make sure throughput is not negative and also that the client has received frames at all
+    //     i.e., we don't want to recalculate bitrate if the video is static and server is sending
+    //      zero packets
+    if (stats.num_received_packets_per_second + stats.num_nacks_per_second > 0) {
+        int real_throughput =
+            (int)(expected_throughput * (1.0 * stats.num_received_packets_per_second) /
+                  (1.0 * (stats.num_nacks_per_second + stats.num_received_packets_per_second)));
+
+        if (real_throughput == expected_throughput) {
+            // If this throughput meets expectations, then increment the met expectation count
+            met_throughput_expectations_count++;
+
+            // If we have met expectations for the current threshold number of iterations,
+            //     then set the latest successful throughput to the active throughput and boost the
+            //     expected throughput to test a higher bitrate.
+            if (met_throughput_expectations_count >= meet_throughput_expectations_threshold) {
+                latest_successful_throughput = real_throughput;
+                met_throughput_expectations_count = 0;
+                latest_successful_throughput_threshold = meet_throughput_expectations_threshold;
+                meet_throughput_expectations_threshold = meet_expectations_min;
+                expected_throughput = (int)(expected_throughput * boost_multiplier);
+            }
+        } else {
+            if (expected_throughput > latest_successful_throughput &&
+                latest_successful_throughput != -1) {
+                // If the expected throughput that yielded lost packets was higher than the
+                //     last continuously successful throughput, then set the expected
+                //     throughput to the last continuously successful throughput to try it
+                //     again for longer. Increase the threshold for boosting the expected throughput
+                //     after continuous success because it is more likely that we have found
+                //     our successful throughput for now.
+                expected_throughput = latest_successful_throughput;
+                latest_successful_throughput_threshold =
+                    min(latest_successful_throughput_threshold * meet_expectations_multiplier,
+                        meet_expectations_max);
+                meet_throughput_expectations_threshold = latest_successful_throughput_threshold;
+            } else {
+                // If the expected throughput that yielded lost packets was lower than the last
+                //     continuously successful throughput, then set the expected throughput to an
+                //     EWMA-calculated new value and reset the threshold for boosting
+                //     the expected throughput after meeting threshold success.
+                expected_throughput =
+                    (int)(alpha * expected_throughput + (1 - alpha) * real_throughput);
+                meet_throughput_expectations_threshold = meet_expectations_min;
+            }
+
+            met_throughput_expectations_count = 0;
+        }
+
+        bitrates.bitrate = (int)(bitrate_throughput_ratio * expected_throughput);
+
+        if (bitrates.bitrate > MAXIMUM_BITRATE) {
+            bitrates.bitrate = MAXIMUM_BITRATE;
+            expected_throughput = (int)(MAXIMUM_BITRATE / bitrate_throughput_ratio);
+        } else if (bitrates.bitrate < MINIMUM_BITRATE) {
+            bitrates.bitrate = MINIMUM_BITRATE;
+            expected_throughput = (int)(MINIMUM_BITRATE / bitrate_throughput_ratio);
+        }
+    }
+
+    // Make sure that frames have been recieved before trying to update the burst bitrate
+    if (stats.num_rendered_frames_per_second + stats.num_skipped_frames_per_second > 0) {
+        int current_burst_heuristic =
+            (int)(bitrates.burst_bitrate * (1.0 * stats.num_rendered_frames_per_second) /
+                  (1.0 *
+                   (stats.num_skipped_frames_per_second + stats.num_rendered_frames_per_second)));
+
+        if (current_burst_heuristic == bitrates.burst_bitrate) {
+            // If this burst bitrate is the same as the burst bitrate from the last period, then
+            //     increment the same burst bitrate count
+            met_burst_expectations_count++;
+
+            // If we have had the same burst bitrate for the current threshold number of iterations,
+            //     then set the latest successful burst bitrate to the active burst bitrate and
+            //     boost the burst bitrate to test a higher burst bitrate. Reset the threshold for
+            //     boosting the burst bitrate after continuous success since we may have entered an
+            //     area with higher bandwidth.
+            if (met_burst_expectations_count >= meet_burst_expectations_threshold) {
+                latest_successful_burst = bitrates.burst_bitrate;
+                met_burst_expectations_count = 0;
+                latest_successful_burst_threshold = meet_throughput_expectations_threshold;
+                meet_burst_expectations_threshold = meet_expectations_min;
+                bitrates.burst_bitrate = (int)(bitrates.burst_bitrate * boost_multiplier);
+            }
+        } else {
+            if (bitrates.burst_bitrate > latest_successful_burst && latest_successful_burst != -1) {
+                // If the burst bitrate that yielded lost packets was higher than the last
+                //     continuously successful burst bitrate, then set the burst bitrate to
+                //     the last continuously successful burst bitrate to try it again for longer.
+                //     Increase the threshold for boosting the burst bitrate after continuous
+                //     success because it is more likely that we have found our successful
+                //     burst bitrate for now.
+                bitrates.burst_bitrate = latest_successful_burst;
+                latest_successful_burst_threshold =
+                    min(latest_successful_burst_threshold * meet_expectations_multiplier,
+                        meet_expectations_max);
+                meet_burst_expectations_threshold = latest_successful_burst_threshold;
+            } else {
+                // If the burst bitrate that yielded lost packets was lower than the last
+                //     continuously successful burst bitrate, then set the burst bitrate to an
+                //     EWMA-calculated new value and reset the threshold for boosting
+                //     the burst bitrate after continuous success.
+                bitrates.burst_bitrate =
+                    (int)(alpha * bitrates.burst_bitrate + (1 - alpha) * current_burst_heuristic);
+                meet_burst_expectations_threshold = meet_expectations_min;
+            }
+
+            met_burst_expectations_count = 0;
+        }
+
+        if (bitrates.burst_bitrate > STARTING_BURST_BITRATE) {
+            bitrates.burst_bitrate = STARTING_BURST_BITRATE;
+        } else if (bitrates.burst_bitrate < MINIMUM_BITRATE) {
+            bitrates.burst_bitrate = MINIMUM_BITRATE;
+        }
+    }
+    return bitrates;
+}
+
 /*
 ============================
 Public Function Implementations
 ============================
 */
 
-BitrateCalculator calculate_new_bitrate = fallback_bitrate;
+BitrateCalculator calculate_new_bitrate = ewma_ratio_bitrate;
