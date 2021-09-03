89cfc3681f6cfde32af726bbac60e32aad92fa1c
Nicholas Pipitone <npip99@gmail.com>
2021-09-03 18:20:24 +0000

    Swap from varlog to usrshare

--- mandelbox-images/base/display/fractal-display.sh
+++ mandelbox-images/base/display/fractal-display.sh
@@ -7,7 +7,7 @@ set -Eeuo pipefail
 nohup /usr/bin/xinit -- /usr/bin/Xorg :${fractal_display_number} -noreset +extension GLX +extension RANDR +extension RENDER -logfile ${fractal_xorg_log_file} -config ${fractal_xorg_conf_file} vt${fractal_tty_number} &
 
 # Wait for xinit-pid to be written to, which happens at the bottom of xinitrc
-while [[ ! -f /var/log/fractal/xinit-pid ]]; do
+while [[ ! -f /usr/share/fractal/xinit-pid ]]; do
   sleep 0.01;
 done
 
