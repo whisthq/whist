ce50fe3073593dc25d1673250b8397814292eac3
mingy98 <ming@fractal.co>
2021-09-14 16:34:25 -0400

    wip testing

--- client-applications/src/main/effects/windows.ts
+++ client-applications/src/main/effects/windows.ts
@@ -59,6 +59,9 @@ const quit = () => {
 }
 
 const allWindowsClosed = fromTrigger("windowInfo").pipe(
+  takeUntil(
+    fromTrigger("installUpdate")
+  ),
   filter(
     (args: {
       crashed: boolean
@@ -69,75 +72,75 @@ const allWindowsClosed = fromTrigger("windowInfo").pipe(
   )
 )
 
-// fromTrigger("windowsAllClosed")
-//   .pipe(
-//     takeUntil(
-//       fromTrigger("installUpdate")
-//     )
-//   )
-//   .subscribe((evt: IpcMainEvent) => {
-//     evt?.preventDefault()
-//   })
+fromTrigger("windowsAllClosed")
+  .pipe(
+    takeUntil(
+      fromTrigger("installUpdate")
+    )
+  )
+  .subscribe((evt: IpcMainEvent) => {
+    evt?.preventDefault()
+  })
 
-// allWindowsClosed
-//   .pipe(
-//     withLatestFrom(fromTrigger("mandelboxFlowSuccess").pipe(startWith({}))),
-//     withLatestFrom(
-//       fromTrigger("mandelboxFlowFailure").pipe(mapTo(true), startWith(false))
-//     )
-//   )
-//   .subscribe(
-//     ([[args, info], mandelboxFailure]: [
-//       [
-//         {
-//           crashed: boolean
-//           numberWindowsRemaining: number
-//           hash: string
-//           event: string
-//         },
-//         any
-//       ],
-//       boolean
-//     ]) => {
-//       // If they didn't crash out and didn't fill out the exit survey, show it to them
-//       if (
-//         persistGet("exitTypeformSubmitted", "data") === undefined &&
-//         !mandelboxFailure &&
-//         !args.crashed &&
-//         !exitSurveyShown &&
-//         args.hash === WindowHashProtocol
-//       ) {
-//         createExitTypeform()
-//         exitSurveyShown = true
-//         // If all windows were successfully closed, quit
-//       } else if (
-//         args.hash !== WindowHashProtocol ||
-//         (args.hash === WindowHashProtocol && !args.crashed)
-//       ) {
-//         quit()
-//         // If the protocol crashed out, try to reconnect
-//       } else if (
-//         args.hash === WindowHashProtocol &&
-//         args.crashed &&
-//         args.event === "close" &&
-//         protocolLaunchRetries < MAX_RETRIES
-//       ) {
-//         protocolLaunchRetries = protocolLaunchRetries + 1
-//         createProtocolWindow()
-//           .then(() => {
-//             protocolStreamInfo(info)
-//             rebootNotification?.show()
-//             setTimeout(() => {
-//               rebootNotification?.close()
-//             }, 6000)
-//           })
-//           .catch((err) => console.error(err))
-//         // If we've already tried several times to reconnect, just show the protocol error window
-//       } else {
-//         createErrorWindow(PROTOCOL_ERROR)
-//       }
-//     }
-//   )
+allWindowsClosed
+  .pipe(
+    withLatestFrom(fromTrigger("mandelboxFlowSuccess").pipe(startWith({}))),
+    withLatestFrom(
+      fromTrigger("mandelboxFlowFailure").pipe(mapTo(true), startWith(false))
+    )
+  )
+  .subscribe(
+    ([[args, info], mandelboxFailure]: [
+      [
+        {
+          crashed: boolean
+          numberWindowsRemaining: number
+          hash: string
+          event: string
+        },
+        any
+      ],
+      boolean
+    ]) => {
+      // If they didn't crash out and didn't fill out the exit survey, show it to them
+      if (
+        persistGet("exitTypeformSubmitted", "data") === undefined &&
+        !mandelboxFailure &&
+        !args.crashed &&
+        !exitSurveyShown &&
+        args.hash === WindowHashProtocol
+      ) {
+        createExitTypeform()
+        exitSurveyShown = true
+        // If all windows were successfully closed, quit
+      } else if (
+        args.hash !== WindowHashProtocol ||
+        (args.hash === WindowHashProtocol && !args.crashed)
+      ) {
+        quit()
+        // If the protocol crashed out, try to reconnect
+      } else if (
+        args.hash === WindowHashProtocol &&
+        args.crashed &&
+        args.event === "close" &&
+        protocolLaunchRetries < MAX_RETRIES
+      ) {
+        protocolLaunchRetries = protocolLaunchRetries + 1
+        createProtocolWindow()
+          .then(() => {
+            protocolStreamInfo(info)
+            rebootNotification?.show()
+            setTimeout(() => {
+              rebootNotification?.close()
+            }, 6000)
+          })
+          .catch((err) => console.error(err))
+        // If we've already tried several times to reconnect, just show the protocol error window
+      } else {
+        createErrorWindow(PROTOCOL_ERROR)
+      }
+    }
+  )
 
 fromTrigger("networkUnstable")
   .pipe(throttle(() => interval(1000))) // Throttle to 1s so we don't flood the main thread
