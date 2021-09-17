718311674318d50aa46e31e8c9c3691439481440
Aaron Choo <aaronchoo9@gmail.com>
2021-09-17 12:09:44 -0400

    Aaron/workflow/identify unresponsive or unregistered ec2 instances (#3893)
    * Add option to get region specific aws instance that are not for development
    
    * Add support to get instance ids from database
    
    * Add option to get instances with status HOST_SERVICE_UNRESPONSIBLE
    
    * linting
    
    * linting
    
    * determine personal dev instance by checking for ec2 and testing-instance not in name and refactored function names
    
    * update file name and execute_query name for clarity
    
    * Add slack notification on instances that are hanging or are marked as HOST_SERVICE_UNRESPONSIVE
    
    New steps were added to webserver-hanging-resource-notification to notify #alert-dev when an instance is hanging (resource not registered in the dbs) or the instance status is set to HOST_SERVICE_UNRESPONSIVE in the db. These step checks dev, staging, and prod databases.
    
    * linting
    
    * Fixed improper syntax and indentations + updated db_helpers function arguments
    
    * add region to reduce db query result
    
    * removed returning region from get_host_service_unresponsive_instance_ids as it is already in the parameter
    
    * batch unresponsiveness instance ids notif based on dev/staging/prod + get_host_service_unresponsive_instances -> get_host_service_unresponsive_instance_ids

---
+++ .github/workflows/helpers/resources/database_helpers.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import psycopg2
+
+
+# add the current directory to the path no matter where this is called from
+sys.path.append(os.path.join(os.getcwd(), os.path.dirname(__file__), "."))
+
+
+def execute_db_query(database_url, path, query):
+    """
+    Executes a given query based on the database url and path
+
+    Args:
+        database_url (str): current database url
+        path (str): the search path
+        query (str): query to be executed
+
+    Returns:
+        arr: result of executing query
+    """
+    conn = psycopg2.connect(database_url, sslmode="require")
+
+    cur = conn.cursor()
+    cur.execute("SET search_path TO %s;" % path)
+    cur.execute("%s;" % query)
+
+    return cur.fetchall()
+
+
+def get_instance_ids(database_url, region):
+    """
+    Gets all aws instance ids using the database url and region
+
+    Args:
+        database_url (str): current database url
+        region (str): current region
+
+    Returns:
+        arr: array of instance ids
+    """
+    query = "SELECT cloud_provider_id FROM instance_info WHERE location='%s';" % region
+    ids = execute_db_query(database_url, "hardware", query)
+
+    return [id[0] for id in ids]
+
+
+def get_host_service_unresponsive_instance_ids(database_url, region):
+    """
+    Gets all aws instance ids using the database url and region which have the status HOST_SERVICE_UNRESPONSIVE
+
+    Args:
+        database_url (str): current database url
+        region (str): current region
+
+    Returns:
+        arr: array of instance ids
+    """
+
+    query = (
+        "SELECT cloud_provider_id FROM instance_info WHERE status='HOST_SERVICE_UNRESPONSIVE' AND location = '%s';"
+        % region
+    )
+    ids = execute_db_query(database_url, "hardware", query)
+
+    return [id[0] for id in ids]
