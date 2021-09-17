f20fc91215c26797fc2be7ae69356bf57be76868
mingy98 <ming@fractal.co>
2021-09-17 14:38:35 -0400

    spawning protocol from entrypoint wip

--- client-applications/src/entrypoint/index.ts
+++ client-applications/src/entrypoint/index.ts
@@ -1,1 +1,31 @@
-require("@app/utils/mandelbox")
+import { spawn } from "child_process"
+import path from "path"
+
+const protocolPath = path.join(
+  __dirname,
+  "../../../",
+  "protocol-build",
+  "client",
+  "./_Fractal"
+)
+
+const protocolParameters = {
+  environment: "development",
+}
+
+const protocolArguments = [
+  ...Object.entries(protocolParameters)
+    .map(([flag, arg]) => [`--${flag}`, arg])
+    .flat(),
+  "--read-pipe",
+]
+
+const protocol = spawn(protocolPath, protocolArguments, {
+  detached: false,
+  // options are for [stdin, stdout, stderr]. pipe creates a pipe, ignore will ignore the
+  // output. We pipe stdin since that's how we send args to the protocol. We pipe stdout
+  // and later use that pipe to write logs to `protocol.log` and potentially stdout.
+  stdio: ["pipe", "pipe", "ignore"],
+})
+
+console.log("Spawned", protocol)
