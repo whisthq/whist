74f5eab71e59ff8ce1642a04af42849b685cfea5
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2021-09-20 16:15:49 +0200

    nit5

--- protocol/fractal/utils/window_name.c
+++ protocol/fractal/utils/window_name.c
@@ -56,9 +56,9 @@ int convert_string_to_UTF8_format(char* string_output, char *string_input) {
 
     for (; index_in<len; index_in++) {
 
-        unsigned int codepoint = (unsigned int) string_input[index_in];
+        uint32_t codepoint = (uint32_t) string_input[index_in];
 
-        LOG_WARNING("Encoding string_input[%i]=%c (codepoint=%i), index_out=%i", index_in, string_input[index_in], codepoint, index_out);
+        LOG_WARNING("Encoding string_input[%i]=%c (codepoint=%lu), index_out=%i", index_in, string_input[index_in], codepoint, index_out);
         
         if (codepoint <= ONE_BYTE_MAX_UNICODE_CODEPOINT) {
             if (index_out+1 > WINDOW_NAME_MAXLEN) {
