459afaee0dc63f3217153c3977ac6818f4722c44
Mauricio Araujo <mauricio@fractal.co>
2021-09-20 09:52:18 -0500

    Make GPU allocation state more robust (#3914)
    * [WIP] Make GPU allocation state more robust
    
    * Add cleanup while freeing GPU.
    
    * Revert cleanup
    
    * Revert ids
    
    * Fix comments in added functions and struct
    
    * Remove unnecesary import
    
    * Fix whitespace at the end of file
    
    * Change to MandelboxID type, add comments and remove SliceContains
    
    * Fix SliceRemove docstring

--- host-service/utils/slices.go
+++ host-service/utils/slices.go
@@ -9,3 +9,24 @@ func SliceContains(slice []interface{}, val interface{}) bool {
 	}
 	return false
 }
+
+// SliceRemove deletes the first occurence of val in s.
+// This represents a small memory leak.
+// https://github.com/golang/go/wiki/SliceTricks#delete-without-preserving-order
+func SliceRemove(s []interface{}, val interface{}) []interface{} {
+	var i int
+	var found bool
+	for index := range s {
+		if s[index] == val {
+			i = index
+			found = true
+			break
+		}
+	}
+
+	if found {
+		s[i] = s[len(s)-1]
+		s = s[:len(s)-1]
+	}
+	return s
+}
