43771e808275c22006cd70c4b864f65bc64b7460
Aaron Choo <aaron@fractal.co>
2021-09-21 14:23:03 -0400

    add script to copy helpers modules andset python path

---
+++ .github/workflows/helpers/setup_helpers_folder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configure GHA to import the AWS and resources packages.
+
+# Create a temporary folder, and copy the aws and resources modules to that folder.
+# We copy instead of move to avoid mutating our repository.
+# The point is to ensure that this package can be imported even if our git repo changes.
+tmpfolder=`mktemp -d`
+cp -r aws $tmpfolder
+cp -r resources $tmpfolder
+
+# Add the temp folder to our Python path, so we can import from aws and resources.
+# The fancy syntax means "$PYTHONPATH:" if it's defined, else ""
+echo "PYTHONPATH=${PYTHONPATH:+${PYTHONPATH}:}$tmpfolder" >> $GITHUB_ENV
