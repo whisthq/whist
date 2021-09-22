c7d2ff20d3a91ecda40f453a358da98533389551
Aaron Choo <aaron@fractal.co>
2021-09-22 13:18:40 -0400

    Set up resources and aws helper folders to be accessible by GHA (#3969)
    * add script to register resources into python path
    
    * add script to register aws helper into python path
    
    * removed responsibilty of copying aws helper to tempt folder
    
    * add steps to set up helpers aws and resources
    
    * removed working-directory and fixed path to script
    
    * fixed summary of script

--- .github/workflows/helpers/notifications/setup_notifications.sh
+++ .github/workflows/helpers/notifications/setup_notifications.sh
@@ -32,7 +32,6 @@ fi
 # The point is to ensure that this package can be imported even if our git repo changes.
 tmpfolder=`mktemp -d`
 cp -r notifications $tmpfolder
-cp -r aws $tmpfolder
 
 # Add the temp folder to our Python path, so we can import from notifications.
 # The fancy syntax means "$PYTHONPATH:" if it's defined, else ""
