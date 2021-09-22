e0c55ebae3cef0bcf5f8ad1c1c93d11de06f6386
Aaron Choo <aaron@fractal.co>
2021-09-22 12:17:02 -0400

    add script to register resources into python path

---
+++ .github/workflows/helpers/resources/setup_resources.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Configure GHA to import the AWS and resources packages.
+
+set -Eeuo pipefail
+
+# Retrieve relative subfolder path
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Working directory is fractal/.github/workflows/helpers
+cd "$DIR/.."
+
+# Create a temporary folder, and copy the resources modules to that folder.
+# We copy instead of move to avoid mutating our repository.
+# The point is to ensure that this package can be imported even if our git repo changes.
+tmpfolder=`mktemp -d`
+cp -r resources $tmpfolder
+
+# Add the temp folder to our Python path, so we can import from resources.
+# The fancy syntax means "$PYTHONPATH:" if it's defined, else ""
+echo "PYTHONPATH=${PYTHONPATH:+${PYTHONPATH}:}$tmpfolder" >> $GITHUB_ENV
