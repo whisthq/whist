8f2fc952b3622ed493ec19444d25caa7789308f4
Roshan Padaki <roshan@fractal.co>
2021-09-23 18:28:29 -0400

    add test for "color.h"

--- protocol/test/ClientTest.cpp
+++ protocol/test/ClientTest.cpp
@@ -3,6 +3,7 @@
 
 extern "C" {
     #include "client/client_utils.h"
+    #include "fractal/utils/color.h"
 }
 // Include paths should be relative to the protocol folder
 //      Examples:
@@ -36,8 +37,35 @@ TEST(ClientTest, ArgParsingEmptyArgsTest) {
     EXPECT_EQ(ret_val,-1);
 }
 
+TEST(ClientTest, FractalColorTest) {
+    FractalRGBColor cyan = {0, 255, 255};
+    FractalRGBColor magenta = {255, 0, 255};
+    FractalRGBColor dark_gray = {25, 25, 25};
+    FractalRGBColor light_gray = {150, 150, 150};
+    FractalRGBColor fractal_purple_rgb = {79, 53, 222};
+    FractalYUVColor fractal_purple_yuv = {85, 198, 127};
+
+    // equality works
+    EXPECT_EQ(rgb_compare(cyan, cyan), 0);
+    EXPECT_EQ(rgb_compare(magenta, magenta), 0);
+
+    // inequality works
+    EXPECT_EQ(rgb_compare(cyan, magenta), 1);
+    EXPECT_EQ(rgb_compare(magenta, cyan), 1);
+
+    // dark color wants light text
+    EXPECT_FALSE(color_requires_dark_text(dark_gray));
+
+    // light color wants dark text
+    EXPECT_TRUE(color_requires_dark_text(light_gray));
+
+    // yuv conversion works (with some fuzz)
+    EXPECT_NEAR(yuv_to_rgb(fractal_purple_yuv).red, fractal_purple_rgb.red, 2);
+    EXPECT_NEAR(yuv_to_rgb(fractal_purple_yuv).green, fractal_purple_rgb.green, 2);
+    EXPECT_NEAR(yuv_to_rgb(fractal_purple_yuv).blue, fractal_purple_rgb.blue, 2);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
