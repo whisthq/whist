de9a4d52946a69c1280ded4702a3f7815026e516
Philippe Noël <phil@fractal.co>
2021-09-24 02:28:43 -0400

    more git oopsies smh

--- protocol/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/phil/Downloads/fractal/protocol/_deps/googletest-src
-# Build directory: /Users/phil/Downloads/fractal/protocol/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
