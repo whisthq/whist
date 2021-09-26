1697f14a3a49a94a7fefd75c2299dcb616c9f8b5
Aaron Choo <aaron@fractal.co>
2021-09-26 18:34:57 -0400

    moved client commit hash dev override to own file

---
+++ config/constants.py
@@ -0,0 +1,6 @@
+# A preshared client commit hash that is accepted along with the
+# client commit hash corresponding to the active AMI in the database
+# in dev environment. Used for enabling client app developers to be able to
+# connect to dev environment without updating entries in the development
+# database to match their local commit hash values.
+CLIENT_COMMIT_HASH_DEV_OVERRIDE = "local_dev"
