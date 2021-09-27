ee9ae2c59467162b55602088813a1fe8b04afd9e
mingy98 <ming@fractal.co>
2021-09-27 13:19:11 -0400

    wip

--- webserver/tests/aws/test_instance_selection.py
+++ webserver/tests/aws/test_instance_selection.py
@@ -84,14 +84,14 @@ def test_assignment_logic(bulk_instance, location):
     replacement_region = bundled_region[location][0]
     bulk_instance(associated_mandelboxes=10, location=location)
     assert (
-        find_instance(location, CLIENT_COMMIT_HASH_FOR_TESTING) is None
+        find_instance(location, CLIENT_COMMIT_HASH_FOR_TESTING) is (None, None)
     ), f"we assigned an already full instance in the main region, {location}"
     bulk_instance(associated_mandelboxes=10, location=replacement_region)
     assert (
-        find_instance(location, CLIENT_COMMIT_HASH_FOR_TESTING) is None
+        find_instance(location, CLIENT_COMMIT_HASH_FOR_TESTING) is (None, None)
     ), f"we assigned an already full instance in the secondary region, {replacement_region}"
     bulk_instance(location=replacement_region, instance_name="replacement-mandelbox")
-    assert find_instance(location, CLIENT_COMMIT_HASH_FOR_TESTING) is not None, (
+    assert find_instance(location, CLIENT_COMMIT_HASH_FOR_TESTING) is not (None, None), (
         f"we failed to find the available instance "
         f"in the replacement region {replacement_region}"
     )
