1d53f13407be2574788a3417dab1785f76547bec
Daniel Kang <daniel.d.kang@gmail.com>
2021-09-28 14:56:40 -0700

    Add end-to-end speed test (#4030)
    * functional benchmarking
    
    * clean up tests
    
    * linting
    
    * review fixes
    
    * comments, prettier
    
    * comment

---
+++ mandelboxes/development/benchmark/start-chrome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit on subcommand errors
+set -Eeuo pipefail
+
+# Load Chrome extensions and start Google Chrome
+# flag-switches{begin,end} are no-ops but it's nice convention to use them to surround chrome://flags features
+exec google-chrome \
+  --use-gl=desktop \
+  --load-extensions=/opt/google/chrome/extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm \
+  --flag-switches-begin \
+  --enable-gpu-rasterization \
+  --enable-zero-copy \
+  --enable-features=VaapiVideoDecoder,Vulkan \
+  --disable-smooth-scrolling \
+  --disable-font-subpixel-positioning \
+  --flag-switches-end \
+  /home/fractal/1080p_60fps.html
