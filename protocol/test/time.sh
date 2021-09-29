54b576f70aecefd173541d5b25bdfc7d2b558fa3
Daniel Kang <daniel.d.kang@gmail.com>
2021-09-29 11:27:16 -0700

    Fix end-to-end protocol tests (#4063)
    * Fix tests
    
    * Remove video download from time script

--- protocol/test/time.sh
+++ protocol/test/time.sh
@@ -3,13 +3,6 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
-# Download the video
-# This script expects to be run on the dev machine, so the paths are hard coded
-cd ~/fractal/mandelboxes/development/benchmark
-if [ ! -f bbb-1080p-75.mp4 ]; then
-  curl https://fractal-test-assets.s3.amazonaws.com/bbb-1080p-75.mp4 -o bbb-1080p-75.mp4
-fi
-
 # Build the benchmark container
 cd ~/fractal/mandelboxes
 ./build_local_mandelbox_image.sh development/benchmark
