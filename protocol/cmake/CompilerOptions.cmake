e027c11ba6b0d390aceb698899d7b5d89820cddd
Philippe Noël <phil@fractal.co>
2021-09-30 15:33:43 -0400

    only run codecov in CI, and via Gtest

--- protocol/cmake/CompilerOptions.cmake
+++ protocol/cmake/CompilerOptions.cmake
@@ -3,7 +3,7 @@ if(MSVC) # Windows MSVC compiler base flags
   # better stack traces.  MT is for release, MTd is for debug, and for RELEASE
   # we should compile against the release version.
   add_compile_options(
-    "$<$<OR:$<COMPILE_LANGUAGE:C>,$<COMPILE_LANGUAGE:CXX>>:-DWIN32;-DWIN32_LEAN_AND_MEAN;-DUNICODE;-COVERAGE;/W4;/wd4189;/wd4100;/wd4127;/wd4244;/MP;$<$<BOOL:${CI}>:/WX;>;$<$<CONFIG:DEBUG>:/MTd;/Od>;$<$<CONFIG:RELEASE>:/MT;/O2>>"
+    "$<$<OR:$<COMPILE_LANGUAGE:C>,$<COMPILE_LANGUAGE:CXX>>:-DWIN32;-DWIN32_LEAN_AND_MEAN;-DUNICODE;/W4;/wd4189;/wd4100;/wd4127;/wd4244;/MP;$<$<BOOL:${CI}>:/WX;>;$<$<CONFIG:DEBUG>:/MTd;/Od>;$<$<CONFIG:RELEASE>:/MT;/O2>>"
   )
 else() # GCC and Clang base flags
   add_compile_options(
@@ -17,11 +17,10 @@ else() # GCC and Clang base flags
     "-Wno-missing-field-initializers"
     "-Wno-implicit-fallthrough" # We use switch/case fallthrough intentionally a lot, it should be allowed
     "-fno-common" # Error when two global variables have the same name, which would overlap them
-    "--coverage" # for Codecov
     "$<$<COMPILE_LANGUAGE:C>:-Wincompatible-pointer-types>"
     "$<$<STREQUAL:$<TARGET_PROPERTY:LINKER_LANGUAGE>,C>:-Werror=implicit-function-declaration>" # Error on implicit function declaration with C
     "$<$<BOOL:${CI}>:-Werror>"
     "$<$<CONFIG:DEBUG>:-Og;-g;-O0>"
     "$<$<CONFIG:RELEASE>:-O3>")
-  add_link_options("-pthread" "-rdynamic" "--coverage")
+  add_link_options("-pthread" "-rdynamic")
 endif()
