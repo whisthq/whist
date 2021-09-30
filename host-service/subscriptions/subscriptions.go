423a269acfe39391ad263d9770a35ed67252da3d
Mauricio Araujo <mauricio@fractal.co>
2021-09-30 15:46:38 -0500

    Add setup client function, remove conflicting tests

--- host-service/subscriptions/subscriptions.go
+++ host-service/subscriptions/subscriptions.go
@@ -12,15 +12,12 @@ import (
 	graphql "github.com/hasura/go-graphql-client" // We use hasura's own graphql client for Go
 )
 
-// Run is responsible for starting the subscription client, as well as
-// subscribing to the subscriptions defined in queries.go
-func Run(instanceName string, done chan bool) error {
-	// We set up the client with auth and logging parameters
-	url := "http://localhost:8080/v1/graphql"
+// SetUpHasuraClient does all the necessary work to set up the Hasura client
+func SetUpHasuraClient(url string, adminSecret string) *graphql.SubscriptionClient {
 	client := graphql.NewSubscriptionClient(url).
 		WithConnectionParams(map[string]interface{}{
 			"headers": map[string]string{
-				"x-hasura-admin-secret": "hasura",
+				"x-hasura-admin-secret": adminSecret,
 			},
 		}).WithLog(log.Println).
 		WithoutLogTypes(graphql.GQL_DATA, graphql.GQL_CONNECTION_KEEP_ALIVE).
@@ -28,8 +25,19 @@ func Run(instanceName string, done chan bool) error {
 			log.Print("err", err)
 			return err
 		})
+	return client
+}
 
-	id, err := SubscribeDrainingStatus(instanceName, client, done)
+// Run is responsible for starting the subscription client, as well as
+// subscribing to the subscriptions defined in queries.go
+func Run(instanceName string, done chan bool) error {
+	// We set up the client with auth and logging parameters
+	url := "http://localhost:8080/v1/graphql"
+	secret := "hasura"
+	client := SetUpHasuraClient(url, secret)
+
+	// Subscribe to the DRAINING status
+	id, err := SubscribeStatus(instanceName, "DRAINING", client, done)
 	if err != nil {
 		// handle subscription error
 		return err
@@ -50,13 +58,13 @@ func Run(instanceName string, done chan bool) error {
 	return nil
 }
 
-// SubscribeDrainingStatus handles the hasura subscription to detect changes on instance instanceName
-// to the status DRAINING in the database.
-func SubscribeDrainingStatus(instanceName string, client *graphql.SubscriptionClient, done chan<- bool) (string, error) {
+// SubscribeStatus handles the hasura subscription to detect changes on instance instanceName
+// to the given status in the database.
+func SubscribeStatus(instanceName string, status string, client *graphql.SubscriptionClient, done chan<- bool) (string, error) {
 	// variables holds the values needed to run the graphql subscription
 	variables := map[string]interface{}{
 		"instance_name": graphql.String(instanceName),
-		"status":        graphql.String("DRAINING"),
+		"status":        graphql.String(status),
 	}
 	// This subscriptions fires when the running instance status changes to draining on the database
 	id, err := client.Subscribe(SubscriptionInstanceStatus, variables, func(data *json.RawMessage, err error) error {
@@ -68,9 +76,16 @@ func SubscribeDrainingStatus(instanceName string, client *graphql.SubscriptionCl
 		var result SubscriptionStatusResult
 		json.Unmarshal(*data, &result)
 
+		var instance Instance
 		// If the result array returned by Hasura is not empty, it means
 		// there was a change in the database row
 		if len(result.Hardware_instance_info) > 0 {
+			instance = result.Hardware_instance_info[0]
+		} else {
+			return nil
+		}
+
+		if (instance.Instance_name == instanceName) && (instance.Status == status) {
 			// We notify via the done channel to start the drain and shutdown process
 			done <- true
 		}
