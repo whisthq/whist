286d521f869da875995c7e0ac4317573cc8cea6c
Roshan Padaki <roshan@fractal.co>
2021-10-02 11:19:32 -0400

    Fix SingletonLock issue where Chrome would refuse to start (#4113)

--- mandelboxes/browsers/chrome/start-chrome.sh
+++ mandelboxes/browsers/chrome/start-chrome.sh
@@ -3,11 +3,22 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
-# Load Chrome extensions and start Google Chrome
+# Under certain (bad) circumstances, SingletonLock might be saved into the user's config. This is an issue,
+# as this prevents Chrome from running forevermore! Hence, we should remove this file when we launch the
+# browser for the first time each session. You can think of this as effectively moving the locking mechansim
+# out of the backed-up Chrome config folder and into a location that will not persist when the instance dies.
+GOOGLE_CHROME_SINGLETON_LOCK=/home/fractal/.config/google-chrome/SingletonLock
+FRACTAL_CHROME_SINGLETON_LOCK=/home/fractal/.config/FractalChromeSingletonLock
+
+if [[ ! -f $FRACTAL_CHROME_SINGLETON_LOCK ]]; then
+    touch $FRACTAL_CHROME_SINGLETON_LOCK
+    rm -f $GOOGLE_CHROME_SINGLETON_LOCK
+fi
+
+# Start Chrome
 # flag-switches{begin,end} are no-ops but it's nice convention to use them to surround chrome://flags features
 exec google-chrome \
   --use-gl=desktop \
-  --load-extensions=/opt/google/chrome/extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm \
   --flag-switches-begin \
   --enable-gpu-rasterization \
   --enable-zero-copy \
