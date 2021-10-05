883ae42e7768c8a6dbb4ce412f0716e4570a074d
djsavvy <sraghuvanshi@college.harvard.edu>
2021-10-05 11:29:49 -0400

    fix imports

--- host-service/metrics/metrics.go
+++ host-service/metrics/metrics.go
@@ -6,11 +6,11 @@ package metrics // import "github.com/fractal/fractal/host-service/metrics"
 
 import (
 	"math/rand"
-	"os"
 	"sync"
 	"time"
 
 	logger "github.com/fractal/fractal/host-service/fractallogger"
+	"github.com/fractal/fractal/host-service/metadata"
 	"github.com/fractal/fractal/host-service/utils"
 
 	"github.com/NVIDIA/go-nvml/pkg/nvml"
