807d58b24b68cd580f4bd94a9c216a40992594c7
Abraham Cohen <abe@fractal.co>
2021-10-08 13:23:27 -0700

    Formatting the code

--- protocol/fractal/cursor/linuxcursor.c
+++ protocol/fractal/cursor/linuxcursor.c
@@ -34,7 +34,7 @@ Includes
 #define WAIT_PROGRESS_CURSOR_HASH 635125873
 
 /*
-        The wait cursor in Ubuntu is a spinning wheel. This is 
+        The wait cursor in Ubuntu is a spinning wheel. This is
         just a combination of a bunch of different wheel images.
 
         All these macros define all the hash various wait wheel cursor images
@@ -78,7 +78,6 @@ Includes
 #define HAND_GRABBING_CURSOR_HASH 3674173946
 #define CROSSHAIR_CURSOR_HASH 1236176635
 
-
 static Display* disp;
 /*
 ============================
@@ -92,88 +91,65 @@ void init_cursors() {
     */
 
     disp = XOpenDisplay(NULL);
-
 }
 
 /**
  * @brief Matches the cursor image from the screen to a FractalCursorID
- * 
+ *
  * TODO: (abecohen) X11 cursor fonts are overridden by the running application.
  * In this case, chrome will override the cursor library and use its own. If
  * we can find where that cursor file is located, get X11 to use that as its theme,
- * we may be able to just match the cursor via the name parameter in ci. 
- * 
- * @param ci the cursor image from XFixesGetCursorImage 
- * @return FractalCursorID, INVALID when no matching FractalCursorID found 
+ * we may be able to just match the cursor via the name parameter in ci.
+ *
+ * @param ci the cursor image from XFixesGetCursorImage
+ * @return FractalCursorID, INVALID when no matching FractalCursorID found
  */
 FractalCursorID get_cursor_id(XFixesCursorImage* ci) {
-        FractalCursorID id = INVALID;
+    FractalCursorID id = INVALID;
 
-        //Need to multiply the size by 4, as the width*height describes 
-        //number of pixels, which are 32 bit, so 4 bytes each.
-        uint32_t cursor_hash = hash(ci->pixels, 4 * ci->width * ci->height);
-        if(cursor_hash == ARROW_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_ARROW;
-        }
-        else if(cursor_hash == IBEAM_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_IBEAM;
-        }
-        else if(cursor_hash == WAIT_PROGRESS_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_WAITARROW;
-        }
-        else if(cursor_hash == WAIT_CURSOR_HASH1 ||
-                cursor_hash == WAIT_CURSOR_HASH2 ||
-                cursor_hash == WAIT_CURSOR_HASH3 ||
-                cursor_hash == WAIT_CURSOR_HASH4 ||
-                cursor_hash == WAIT_CURSOR_HASH5 ||
-                cursor_hash == WAIT_CURSOR_HASH6 ||
-                cursor_hash == WAIT_CURSOR_HASH7 ||
-                cursor_hash == WAIT_CURSOR_HASH8 ||
-                cursor_hash == WAIT_CURSOR_HASH9 ||
-                cursor_hash == WAIT_CURSOR_HASH10 ||
-                cursor_hash == WAIT_CURSOR_HASH11 ||
-                cursor_hash == WAIT_CURSOR_HASH12 ||
-                cursor_hash == WAIT_CURSOR_HASH13 ||
-                cursor_hash == WAIT_CURSOR_HASH14 ||
-                cursor_hash == WAIT_CURSOR_HASH15 ||
-                cursor_hash == WAIT_CURSOR_HASH16 ||
-                cursor_hash == WAIT_CURSOR_HASH17 ||
-                cursor_hash == WAIT_CURSOR_HASH18 ||
-                cursor_hash == WAIT_CURSOR_HASH19 ||
-                cursor_hash == WAIT_CURSOR_HASH20 ||
-                cursor_hash == WAIT_CURSOR_HASH21 ||
-                cursor_hash == WAIT_CURSOR_HASH22 ||
-                cursor_hash == WAIT_CURSOR_HASH23 ||
-                cursor_hash == WAIT_CURSOR_HASH24
-                ) {
+    // Need to multiply the size by 4, as the width*height describes
+    // number of pixels, which are 32 bit, so 4 bytes each.
+    uint32_t cursor_hash = hash(ci->pixels, 4 * ci->width * ci->height);
+    if (cursor_hash == ARROW_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_ARROW;
+    } else if (cursor_hash == IBEAM_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_IBEAM;
+    } else if (cursor_hash == WAIT_PROGRESS_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_WAITARROW;
+    } else if (cursor_hash == WAIT_CURSOR_HASH1 || cursor_hash == WAIT_CURSOR_HASH2 ||
+               cursor_hash == WAIT_CURSOR_HASH3 || cursor_hash == WAIT_CURSOR_HASH4 ||
+               cursor_hash == WAIT_CURSOR_HASH5 || cursor_hash == WAIT_CURSOR_HASH6 ||
+               cursor_hash == WAIT_CURSOR_HASH7 || cursor_hash == WAIT_CURSOR_HASH8 ||
+               cursor_hash == WAIT_CURSOR_HASH9 || cursor_hash == WAIT_CURSOR_HASH10 ||
+               cursor_hash == WAIT_CURSOR_HASH11 || cursor_hash == WAIT_CURSOR_HASH12 ||
+               cursor_hash == WAIT_CURSOR_HASH13 || cursor_hash == WAIT_CURSOR_HASH14 ||
+               cursor_hash == WAIT_CURSOR_HASH15 || cursor_hash == WAIT_CURSOR_HASH16 ||
+               cursor_hash == WAIT_CURSOR_HASH17 || cursor_hash == WAIT_CURSOR_HASH18 ||
+               cursor_hash == WAIT_CURSOR_HASH19 || cursor_hash == WAIT_CURSOR_HASH20 ||
+               cursor_hash == WAIT_CURSOR_HASH21 || cursor_hash == WAIT_CURSOR_HASH22 ||
+               cursor_hash == WAIT_CURSOR_HASH23 || cursor_hash == WAIT_CURSOR_HASH24) {
+        id = FRACTAL_CURSOR_WAIT;
+    } else if (cursor_hash == NWSE_CURSOR_HASH || cursor_hash == NW_CURSOR_HASH ||
+               cursor_hash == SE_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_SIZENWSE;
+    } else if (cursor_hash == NESW_CURSOR_HASH || cursor_hash == NE_CURSOR_HASH ||
+               cursor_hash == SW_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_SIZENESW;
+    } else if (cursor_hash == EW_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_SIZEWE;
+    } else if (cursor_hash == NS_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_SIZENS;
+    } else if (cursor_hash == NOT_ALLOWED_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_NO;
+    } else if (cursor_hash == CROSSHAIR_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_CROSSHAIR;
+    } else if (cursor_hash == HAND_POINT_CURSOR_HASH || cursor_hash == HAND_GRAB_CURSOR_HASH ||
+               cursor_hash == HAND_GRABBING_CURSOR_HASH) {
+        id = FRACTAL_CURSOR_HAND;
+    }
 
-                id = FRACTAL_CURSOR_WAIT;
-        }
-        else if(cursor_hash == NWSE_CURSOR_HASH || cursor_hash == NW_CURSOR_HASH || cursor_hash == SE_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_SIZENWSE;
-        }
-        else if(cursor_hash == NESW_CURSOR_HASH || cursor_hash == NE_CURSOR_HASH || cursor_hash == SW_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_SIZENESW;
-        }
-        else if(cursor_hash == EW_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_SIZEWE;
-        }
-        else if(cursor_hash == NS_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_SIZENS;
-        }
-        else if(cursor_hash == NOT_ALLOWED_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_NO;
-        }
-        else if(cursor_hash == CROSSHAIR_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_CROSSHAIR;
-        }
-        else if(cursor_hash == HAND_POINT_CURSOR_HASH || cursor_hash == HAND_GRAB_CURSOR_HASH || cursor_hash == HAND_GRABBING_CURSOR_HASH) {
-                id = FRACTAL_CURSOR_HAND;
-        }
-
-        return id;
+    return id;
 }
-
 
 void get_current_cursor(FractalCursorImage* image) {
     /*
@@ -187,7 +163,6 @@ void get_current_cursor(FractalCursorImage* image) {
     image->cursor_id = FRACTAL_CURSOR_ARROW;
     image->cursor_state = CURSOR_STATE_VISIBLE;
     if (disp) {
-
         XFixesCursorImage* ci = XFixesGetCursorImage(disp);
 
         if (ci->width > MAX_CURSOR_WIDTH || ci->height > MAX_CURSOR_HEIGHT) {
@@ -197,18 +172,18 @@ void get_current_cursor(FractalCursorImage* image) {
                 ci->width, ci->height, MAX_CURSOR_WIDTH, MAX_CURSOR_HEIGHT);
         }
 
-        if(INVALID == (image->cursor_id = get_cursor_id(ci))) {
-                image->using_bmp = true;
-                image->bmp_width = min(MAX_CURSOR_WIDTH, ci->width);
-                image->bmp_height = min(MAX_CURSOR_HEIGHT, ci->height);
-                image->bmp_hot_x = ci->xhot;
-                image->bmp_hot_y = ci->yhot;
+        if (INVALID == (image->cursor_id = get_cursor_id(ci))) {
+            image->using_bmp = true;
+            image->bmp_width = min(MAX_CURSOR_WIDTH, ci->width);
+            image->bmp_height = min(MAX_CURSOR_HEIGHT, ci->height);
+            image->bmp_hot_x = ci->xhot;
+            image->bmp_hot_y = ci->yhot;
 
-                for(int k = 0; k < image->bmp_width * image->bmp_height; ++k) {
-                        // we need to do this in case ci->pixels uses 8 bytes per pixel
-                        uint32_t argb = (uint32_t)ci->pixels[k];
-                        image->bmp[k] = argb;
-                }
+            for (int k = 0; k < image->bmp_width * image->bmp_height; ++k) {
+                // we need to do this in case ci->pixels uses 8 bytes per pixel
+                uint32_t argb = (uint32_t)ci->pixels[k];
+                image->bmp[k] = argb;
+            }
         }
 
         XFree(ci);
