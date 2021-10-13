bc8bd446d2d84f6694114a8309556e222b386d8e
Aaron Choo <aaron@fractal.co>
2021-10-13 16:11:57 -0400

    get cookies and decrypt values

--- client-applications/src/utils/browsers.ts
+++ client-applications/src/utils/browsers.ts
@@ -20,8 +20,142 @@ import * as fs from "fs"
 import tempfile from "tempfile"
 import { homedir } from "os"
 import { dirname } from "path"
+import * as pbkdf2 from 'pbkdf2'
+import database from 'better-sqlite3'
+import { AES, enc } from 'crypto-js'
+
+interface Cookie {
+  [key: string]: Buffer | string | number
+}
 
 const bus = dbus.sessionBus()
+
+const getCookies = async (browser: string): Promise<Array<Cookie>> => {
+  const encryptedCookies = await getCookiesFromFile(browser)
+  const encryptKey = await getCookieEncryptionKey(browser)
+
+  let cookies = decryptCookies(encryptedCookies, encryptKey)  
+
+  return cookies
+}
+
+
+const decryptCookies = async (encryptedCookies: Array<Cookie>, encryptKey: string): Promise<Array<Cookie>> => {
+  let cookies: Array<Cookie> = []
+
+  const numOfCookise = encryptedCookies.length
+  for (let i = 0; i < numOfCookise; i++) {
+    cookies.push(await decryptCookie(encryptedCookies[i], encryptKey))
+  }
+
+  return cookies
+}
+
+const decryptCookie = async (cookie: Cookie, encryptKey: string): Promise<Cookie> => {
+  if (typeof cookie['value'] === 'string' && cookie['value'].length > 0) {
+    return cookie
+  }
+
+  const encryptionPrefix = cookie['encrypted_value'].toString().substring(0,3)
+  if (encryptionPrefix != 'v10' && encryptionPrefix != 'v11') {
+    return cookie
+  }
+
+  cookie["encrypted_prefix"] = encryptionPrefix
+
+
+  const encryptedValue = cookie['encrypted_value'].toString().substring(3)
+  const bytes = AES.decrypt(encryptedValue, encryptKey)
+  const originalText = bytes.toString(enc.Utf8);
+
+  cookie['encrypted_value'] = originalText
+
+  return cookie
+
+}
+
+
+const getCookiesFromFile = async (browser: string): Promise<Array<Cookie>> => {
+  const cookieFile = getExpandedCookieFilePath(browser)
+  const tempFile = createLocalCopy(cookieFile)
+  const db = new database(tempFile, { verbose: console.log })
+
+  let rows: Array<Cookie>
+  try {
+    const query = await db.prepare("SELECT creation_utc, top_frame_site_key, host_key, name, value, encrypted_value, path, expires_utc, secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_scheme, source_port, is_same_party FROM cookies;")
+    rows = query.all();
+  } catch {
+    const query = await db.prepare("SELECT creation_utc, top_frame_site_key, host_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_scheme, source_port, is_same_party FROM cookies;")       
+    rows = query.all();
+  } 
+
+  return rows
+
+}
+
+class ChromiumBased {
+    // Super class for all Chromium based browser
+    length = 16
+    browserName: string = "Chromium"
+    cookieFile: string = ""
+    domainName: string = ""
+    key: any = ""
+    keyFile: any
+    browserDetails: any = {}
+    tmpCookieFIle: string = ""
+
+    constructor(init?:Partial<ChromiumBased>) {
+        Object.assign(this, init);
+        this.addKeyAndCookieFile()
+    }
+
+}
+
+
+const getExpandedCookieFilePath = (browser: string): string => {
+  let os = ""
+  switch(process.platform) {
+    case "darwin": {
+      os = "osx"
+      break
+    } case "linux": {
+      os = "linux"
+      break
+    } default:
+        throw("OS not recognized. Works on OSX or linux.")
+  }
+
+  return expandPaths(getCookieFilePath(browser), os)
+}
+
+
+const getCookieEncryptionKey = async (browser: string): Promise<string> => {
+  const salt: Buffer = Buffer.from('saltysalt', 'base64')
+  const iv: Buffer = Buffer.from('                ', 'base64')
+  switch(process.platform) {
+      case "darwin": {
+          let myPass = await keytar.getPassword(getKeyUser(browser), getOsCryptName(browser))
+          
+          if (myPass === null) {
+            myPass = "peanuts"
+          }
+
+          let iterations = 1003 // number of pbkdf2 iterations on mac
+          const key =  await pbkdf2.pbkdf2Sync(myPass, salt, iterations, length)
+          return key.toString()
+      } case "linux": {
+          const myPass = await getLinuxCookieEncryptionKey(getOsCryptName(browser))
+          
+          let iterations = 1 
+          const key = pbkdf2.pbkdf2Sync(myPass, salt, iterations, length)
+          return key.toString()
+          
+      } default:
+          throw("OS not recognized. Works on OSX or linux.")
+  }
+}
+
+
 
 const createLocalCopy = (cookieFile: string): string => {
   if (fs.existsSync(cookieFile)) {
@@ -56,8 +190,10 @@ const expandPaths = (paths: string[], osName: string): string => {
 }
 
 const getLinuxCookieEncryptionKey = async (
-  osCryptName: string
+  browser: string
 ): Promise<string> => {
+  const osCryptName = getOsCryptName(browser)
+
   const proxyToSecret = await bus.getProxyObject(BusSecretName, BusSecretPath)
 
   const secretService = await proxyToSecret.getInterface(SecretServiceName)
@@ -84,7 +220,7 @@ const getLinuxCookieEncryptionKey = async (
       variant
     )
 
-    const secretObj: { string: Array<string | any> } =
+    const secretObj: { [key: string]: Array<string | any> } =
       await secretService.GetSecrets(secretLocations, sessionPath)
 
     bus.disconnect()
@@ -99,9 +235,9 @@ const getLinuxCookieEncryptionKey = async (
   } else {
     bus.disconnect()
 
-    const capitalizedOsCryptName = `${osCryptName
-      .charAt(0)
-      .toUpperCase()}${osCryptName.slice(1)}`
+    const capitalizedOsCryptName = `${
+      osCryptName.charAt(0).toUpperCase()
+    }${osCryptName.slice(1)}`
     const cookieEncryptionKey = keytar.getPassword(
       `${capitalizedOsCryptName} Keys`,
       `${capitalizedOsCryptName} Safe Storage`
@@ -114,44 +250,67 @@ const getLinuxCookieEncryptionKey = async (
   return "peanuts"
 }
 
+const getKeyUser = (browser: string): string => {
+  switch (browser.toLowerCase()) {
+    case "chrome": {
+      return "Chrome"
+    } case "opera": {
+      return "Opera"
+    } case "edge": {
+      return "Microsoft Edge"
+    } case "chromium": {
+      return "Chromium"
+    } case "brave": {
+      return "Brave"
+    }
+  }
+  return ""
+}
+
+
+
+const getOsCryptName = (browser: string): string => {
+  switch (browser.toLowerCase()) {
+    case "chrome": {
+      return "chrome"
+    } case "opera": {
+    } case "edge": {
+    } case "chromium": {
+    } case "brave": {
+      return "chromium"
+    }
+  }
+  return ""
+}
+
 const getCookieFilePath = (browser: string): string[] => {
   switch (process.platform) {
     case "darwin": {
       switch (browser.toLowerCase()) {
         case "chrome": {
           return ChromeOSXCookieFiles
-        }
-        case "opera": {
+        } case "opera": {
           return OperaOSXCookieFiles
-        }
-        case "edge": {
+        } case "edge": {
           return EdgeOSXCookieFiles
-        }
-        case "chromium": {
+        } case "chromium": {
           return ChromiumOSXCookieFiles
-        }
-        case "brave": {
+        } case "brave": {
           return BraveOSXCookieFiles
         }
       }
-
       break
-    }
-    case "linux": {
+    } case "linux": {
       switch (browser.toLowerCase()) {
         case "chrome": {
           return ChromeLinuxCookieFiles
-        }
-        case "opera": {
+        } case "opera": {
           return OperaLinuxCookieFiles
-        }
-        case "edge": {
+        } case "edge": {
           return EdgeLinuxCookieFiles
-        }
-        case "chromium": {
+        } case "chromium": {
           return ChromiumLinuxCookieFiles
-        }
-        case "brave": {
+        } case "brave": {
           return BraveLinuxCookieFiles
         }
       }
