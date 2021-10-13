baef790d2b87bab3a405a51fff95cc8afb24cf08
Aaron Choo <aaron@fractal.co>
2021-10-13 10:14:45 -0400

    get cookie file path from given browser

---
+++ client-applications/src/utils/browsers.ts
@@ -0,0 +1,41 @@
+import { BraveLinuxCookieFiles, BraveOSXCookieFiles, ChromeLinuxCookieFiles, ChromeOSXCookieFiles, ChromiumLinuxCookieFiles, ChromiumOSXCookieFiles, EdgeLinuxCookieFiles, EdgeOSXCookieFiles, OperaLinuxCookieFiles, OperaOSXCookieFiles } from "./constants";
+
+const getCookieFilePath = (browser: string): Array<string> => {
+    switch(process.platform) {
+        case "darwin": {
+            switch(browser.toLowerCase()) {
+                case "chrome": {
+                    return ChromeOSXCookieFiles
+                } case "opera": {
+                    return OperaOSXCookieFiles
+                } case "edge": {
+                    return EdgeOSXCookieFiles
+                } case "chromium": {
+                    return ChromiumOSXCookieFiles
+                } case "brave": {
+                    return BraveOSXCookieFiles
+                }
+            }
+
+            break
+        } case "linux": {
+            switch(browser.toLowerCase()) {
+                case "chrome": {
+                    return ChromeLinuxCookieFiles
+                } case "opera": {
+                    return OperaLinuxCookieFiles
+                } case "edge": {
+                    return EdgeLinuxCookieFiles
+                } case "chromium": {
+                    return ChromiumLinuxCookieFiles
+                } case "brave": {
+                    return BraveLinuxCookieFiles
+                }
+            }
+            break
+        }
+    }
+
+    return []
+
+}
