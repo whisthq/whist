bf536c1d65b36cbdd5a0ba76f6532bea62c40900
Aaron Choo <aaron@fractal.co>
2021-10-13 11:48:36 -0400

    linting

--- client-applications/src/utils/constants.ts
+++ client-applications/src/utils/constants.ts
@@ -11,28 +11,43 @@ export const HostServicePort = 4678
 
 // File paths for each browser
 export const ChromeLinuxCookieFiles = [
-    "~/.config/google-chrome/Default/Cookies",
-    "~/.config/google-chrome-beta/Default/Cookies"
-  ]
-export const ChromeOSXCookieFiles = ["~/Library/Application Support/Google/Chrome/Default/Cookies"]
+  "~/.config/google-chrome/Default/Cookies",
+  "~/.config/google-chrome-beta/Default/Cookies",
+]
+export const ChromeOSXCookieFiles = [
+  "~/Library/Application Support/Google/Chrome/Default/Cookies",
+]
 export const ChromiumLinuxCookieFiles = ["~/.config/chromium/Default/Cookies"]
-export const ChromiumOSXCookieFiles = ["~/Library/Application Support/Chromium/Default/Cookies"]
+export const ChromiumOSXCookieFiles = [
+  "~/Library/Application Support/Chromium/Default/Cookies",
+]
 export const OperaLinuxCookieFiles = ["~/.config/opera/Cookies"]
-export const OperaOSXCookieFiles = ["~/Library/Application Support/com.operasoftware.Opera/Cookies"]
+export const OperaOSXCookieFiles = [
+  "~/Library/Application Support/com.operasoftware.Opera/Cookies",
+]
 export const BraveLinuxCookieFiles = [
-    "~/.config/BraveSoftware/Brave-Browser/Default/Cookies",
-    "~/.config/BraveSoftware/Brave-Browser-Beta/Default/Cookies"
-  ]
+  "~/.config/BraveSoftware/Brave-Browser/Default/Cookies",
+  "~/.config/BraveSoftware/Brave-Browser-Beta/Default/Cookies",
+]
 export const BraveOSXCookieFiles = [
-    "~/Library/Application Support/BraveSoftware/Brave-Browser/Default/Cookies",
-    "~/Library/Application Support/BraveSoftware/Brave-Browser-Beta/Default/Cookies"
-  ]
+  "~/Library/Application Support/BraveSoftware/Brave-Browser/Default/Cookies",
+  "~/Library/Application Support/BraveSoftware/Brave-Browser-Beta/Default/Cookies",
+]
 export const EdgeLinuxCookieFiles = [
-    "~/.config/microsoft-edge/Default/Cookies",
-    "~/.config/microsoft-edge-dev/Default/Cookies"
-  ]
-export const EdgeOSXCookieFiles = ["~/Library/Application Support/Microsoft Edge/Default/Cookies"]
+  "~/.config/microsoft-edge/Default/Cookies",
+  "~/.config/microsoft-edge-dev/Default/Cookies",
+]
+export const EdgeOSXCookieFiles = [
+  "~/Library/Application Support/Microsoft Edge/Default/Cookies",
+]
 
+// Linux DBus / Secret Session
+export const BusSecretName = "org.freedesktop.secrets"
+export const BusSecretPath = "/org/freedesktop/secrets"
+export const SecretServiceName = "org.freedesktop.Secret.Service"
+
+export const ALGORITHM_PLAIN = "plain"
+// add later: const ALGORITHM_DH = 'dh-ietf1024-sha256-aes128-cbc-pkcs7'
 
 // The Electron BrowserWindow API can be passed a hash parameter as data.
 // We use this so that renderer threads can decide which view component to
