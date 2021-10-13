aa0a7c6ab143df83c2bacc154d8707d035af2092
Aaron Choo <aaron@fractal.co>
2021-10-13 11:06:17 -0400

    add func to get linux cookie encrypt key

--- client-applications/src/utils/browsers.ts
+++ client-applications/src/utils/browsers.ts
@@ -1,41 +1,60 @@
-import { BraveLinuxCookieFiles, BraveOSXCookieFiles, ChromeLinuxCookieFiles, ChromeOSXCookieFiles, ChromiumLinuxCookieFiles, ChromiumOSXCookieFiles, EdgeLinuxCookieFiles, EdgeOSXCookieFiles, OperaLinuxCookieFiles, OperaOSXCookieFiles } from "./constants";
+import {
+  BraveLinuxCookieFiles,
+  BraveOSXCookieFiles,
+  ChromeLinuxCookieFiles,
+  ChromeOSXCookieFiles,
+  ChromiumLinuxCookieFiles,
+  ChromiumOSXCookieFiles,
+  EdgeLinuxCookieFiles,
+  EdgeOSXCookieFiles,
+  OperaLinuxCookieFiles,
+  OperaOSXCookieFiles,
+} from "./constants"
 
-const getCookieFilePath = (browser: string): Array<string> => {
-    switch(process.platform) {
-        case "darwin": {
-            switch(browser.toLowerCase()) {
-                case "chrome": {
-                    return ChromeOSXCookieFiles
-                } case "opera": {
-                    return OperaOSXCookieFiles
-                } case "edge": {
-                    return EdgeOSXCookieFiles
-                } case "chromium": {
-                    return ChromiumOSXCookieFiles
-                } case "brave": {
-                    return BraveOSXCookieFiles
-                }
-            }
-
-            break
-        } case "linux": {
-            switch(browser.toLowerCase()) {
-                case "chrome": {
-                    return ChromeLinuxCookieFiles
-                } case "opera": {
-                    return OperaLinuxCookieFiles
-                } case "edge": {
-                    return EdgeLinuxCookieFiles
-                } case "chromium": {
-                    return ChromiumLinuxCookieFiles
-                } case "brave": {
-                    return BraveLinuxCookieFiles
-                }
-            }
-            break
+const getCookieFilePath = (browser: string): string[] => {
+  switch (process.platform) {
+    case "darwin": {
+      switch (browser.toLowerCase()) {
+        case "chrome": {
+          return ChromeOSXCookieFiles
         }
+        case "opera": {
+          return OperaOSXCookieFiles
+        }
+        case "edge": {
+          return EdgeOSXCookieFiles
+        }
+        case "chromium": {
+          return ChromiumOSXCookieFiles
+        }
+        case "brave": {
+          return BraveOSXCookieFiles
+        }
+      }
+
+      break
     }
+    case "linux": {
+      switch (browser.toLowerCase()) {
+        case "chrome": {
+          return ChromeLinuxCookieFiles
+        }
+        case "opera": {
+          return OperaLinuxCookieFiles
+        }
+        case "edge": {
+          return EdgeLinuxCookieFiles
+        }
+        case "chromium": {
+          return ChromiumLinuxCookieFiles
+        }
+        case "brave": {
+          return BraveLinuxCookieFiles
+        }
+      }
+      break
+    }
+  }
 
-    return []
-
+  return []
 }
