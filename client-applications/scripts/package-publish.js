012cbd7402b6c47a95fdbe1bc1f18fc33384eba5
Philippe Noël <phil@fractal.co>
2021-10-16 20:38:06 -0400

    clean up the code

--- client-applications/scripts/package-publish.js
+++ client-applications/scripts/package-publish.js
@@ -38,27 +38,20 @@ const packageNotarize = (env, config, version, environment, commit) => {
   })
 
   const getBucketName = () => {
-    let bucketName
     switch (process.platform) {
       case "darwin":
         // on macOS, we have buckets for Intel silicon (X86_64) and Apple silicon (ARM64)
         switch (config.keys.MACOS_ARCH) {
           case "x64":
-            bucketName = `fractal-chromium-macos-${environment}`
-            break
+            return `fractal-chromium-macos-${environment}`
           case "arm64":
-            bucketName = `fractal-chromium-macos-arm64-${environment}`
-            break
+            return `fractal-chromium-macos-arm64-${environment}`
         }
-        break
       case "win32":
-        bucketName = `fractal-chromium-windows-${environment}`
-        break
+        return `fractal-chromium-windows-${environment}`
       case "linux":
-        bucketName = `fractal-chromium-ubuntu-${environment}`
-        break
+        return `fractal-chromium-ubuntu-${environment}`
     }
-    return bucketName
   }
 
   helpers.electronPublish(getBucketName())
