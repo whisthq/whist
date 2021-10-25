3d6cc7caaf59ae5c70efb27e90282dce0b045f1e
Mauricio Araujo <mauricio@fractal.co>
2021-10-25 14:17:59 -0500

    Properly return when there are no user configs to decrypt

--- host-service/mandelbox/user_configs.go
+++ host-service/mandelbox/user_configs.go
@@ -108,9 +108,17 @@ func (c *mandelboxData) PopulateUserConfigs() error {
 // DecryptUserConfigs decrypts and unpacks the previously downloaded
 // s3 config using the encryption token received through JSON transport.
 func (c *mandelboxData) DecryptUserConfigs() error {
+	// Lock directory to avoid cleanup
+	c.rwlock.Lock()
+	defer c.rwlock.Unlock()
 
 	if len(c.configEncryptionToken) == 0 {
 		return utils.MakeError("Cannot get user configs for MandelboxID %s since ConfigEncryptionToken is empty", c.mandelboxID)
+	}
+
+	if c.configBuffer == nil {
+		logger.Infof("Not decrypting user configs because the config buffer is empty.")
+		return nil
 	}
 
 	logger.Infof("Decrypting user config for mandelbox %s", c.mandelboxID)
@@ -130,10 +138,6 @@ func (c *mandelboxData) DecryptUserConfigs() error {
 
 	logger.Infof("Finished decrypting user config for mandelbox %s", c.mandelboxID)
 	logger.Infof("Decompressing user config for mandelbox %s", c.mandelboxID)
-
-	// Lock directory to avoid cleanup
-	c.rwlock.Lock()
-	defer c.rwlock.Unlock()
 
 	// Make directory for user configs
 	configDir := c.getUserConfigDir()
