9ca57a4926b27972b052a3ffd91937c141ed18f0
Philippe Noël <phil@fractal.co>
2021-10-27 18:53:42 -0400

    add support for sync-ing repeat rates

--- client-applications/src/main/flows/mandelbox/index.ts
+++ client-applications/src/main/flows/mandelbox/index.ts
@@ -3,6 +3,18 @@ import { map } from "rxjs/operators"
 import mandelboxCreateFlow from "@app/main/flows/mandelbox/create"
 import hostSpinUpFlow from "@app/main/flows/mandelbox/host"
 import { flow } from "@app/utils/flows"
+import { isNumber } from "lodash"
+
+const execCommand = require("./execCommand").execCommand
+
+
+
+
+
+
+
+
+
 
 export default flow(
   "mandelboxFlow",
@@ -20,13 +32,20 @@ export default flow(
       )
     )
 
+    // Retrieve macOS keyboard repeat rate(s) to send them to the server
+    const initialKeyRepeat = execCommand(`defaults read NSGlobalDomain InitialKeyRepeat`)
+    const keyRepeat = execCommand(`defaults read NSGlobalDomain KeyRepeat`)
+
     const host = hostSpinUpFlow(
       zip([trigger, create.success]).pipe(
         map(([t, c]) => ({
           ip: c.ip,
           config_encryption_token: t.configToken,
           jwt_access_token: t.accessToken,
-          json_data: JSON.stringify({}), // Data to send through the JSON transport
+          json_data: JSON.stringify({
+            initial_key_repeat: (isNumber(initialKeyRepeat) ? initialKeyRepeat : 68), // this fails if user hasn't modified the default value, which is 68
+            key_repeat: (isNumber(keyRepeat) ? keyRepeat : 6), // this fails if user hasn't modified the default value, which is 6
+          }), // Data to send through the JSON transport
         }))
       )
     )
