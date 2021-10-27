a59b8895e36805c227597c29599b374a226e7693
Philippe Noël <phil@fractal.co>
2021-10-27 18:54:32 -0400

    sync repeat rates

--- mandelboxes/base/utils/run-as-fractal-user.sh
+++ mandelboxes/base/utils/run-as-fractal-user.sh
@@ -2,6 +2,19 @@
 
 set -Eeuo pipefail
 
+# default values for the JSON transport client settings fron the client
+export INITIAL_KEY_REPEAT=68 # default value on macOS, options are 120, 94, 68, 35, 25, 15
+export KEY_REPEAT=6 # default value on macOS, options are 120, 90, 60, 30, 12, 6, 2
+
+FRACTAL_JSON_FILE=/fractal/resourceMappings/config.json
+if [[ -f $FRACTAL_JSON_FILE ]]; then
+  if [ "$( jq 'has("initial_key_repeat")' < $FRACTAL_JSON_FILE )" == "true" && "$( jq 'has("initial_key_repeat")' < $FRACTAL_JSON_FILE )" == "true" ]; then
+    INITIAL_KEY_REPEAT=$( jq -r '.initial_key_repeat' < $FRACTAL_JSON_FILE )
+    KEY_REPEAT=$( jq -r '.key_repeat' < $FRACTAL_JSON_FILE )
+    xset r rate $INITIAL_KEY_REPEAT $KEY_REPEAT
+  fi
+fi
+
 # Things running outside run_as_fractal are run as root.
 # Running with login means that we lose all environment variables, so we need to pass them in manually.
 exec runuser --login fractal -c \
