97331c8c7ab0b7d903c36ca652e6988d347bb281
mingy98 <ming@fractal.co>
2021-11-02 16:42:01 -0400

    only sentry log critical errors

--- client-applications/src/main/effects/monitoring.ts
+++ client-applications/src/main/effects/monitoring.ts
@@ -4,36 +4,14 @@ import * as Sentry from "@sentry/electron"
 
 import config from "@app/config/environment"
 import { appEnvironment, FractalEnvironments } from "../../../config/configs"
-import { fromTrigger } from "@app/utils/flows"
-import {
-  sessionID,
-  logzProtocolClientURL,
-  logzProtocolServerURL,
-  LOG_INTERVAL_IN_MINUTES,
-  SENTRY_DSN,
-} from "@app/utils/constants"
-import { persistGet } from "@app/utils/persist"
+import { LOG_INTERVAL_IN_MINUTES, SENTRY_DSN } from "@app/utils/constants"
 
 // Initialize and report Sentry errors in all packaged environments
-if (appEnvironment !== FractalEnvironments.LOCAL) {
+if (appEnvironment === FractalEnvironments.PRODUCTION) {
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: config.sentryEnv,
     release: `client-applications@${config.keys.COMMIT_SHA as string}`,
-  })
-
-  Sentry.setContext("Logz URLs", {
-    protocolClientLogs: logzProtocolClientURL(),
-    protocolServerLogs: logzProtocolServerURL(),
-  })
-
-  fromTrigger("protocolError").subscribe(() => {
-    Sentry.captureMessage(
-      `Protocol error detected for user ${
-        (persistGet("userEmail") ?? "") as string
-      } with session ID ${sessionID.toString()}. Investigate immediately.`,
-      Sentry.Severity.Error
-    )
   })
 }
 
--- client-applications/src/utils/constants.ts
+++ client-applications/src/utils/constants.ts
@@ -4,7 +4,7 @@
  * @brief This file contains constant values that are passed around to other parts of the application.
  */
 import { AWSRegion } from "@app/@types/aws"
-import { appEnvironment, FractalEnvironments } from "../../config/configs"
+// import { appEnvironment, FractalEnvironments } from "../../config/configs"
 
 export const allowPayments = true
 
@@ -45,38 +45,6 @@ export const defaultAllowedRegions = [
   AWSRegion.US_WEST_2,
   AWSRegion.CA_CENTRAL_1,
 ]
-
-// Base logz GET request to apply filters and queries
-const PROTOCOL_SERVER_LOGZ_BASE = `https://app.logz.io/#/dashboard/kibana/discover?_a=(columns:!(message),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'logzioCustomerIndex*',key:session_id,negate:!f,params:(query:'${sessionID}'),type:phrase),query:(match_phrase:(session_id:'${sessionID}'))),('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'logzioCustomerIndex*',key:component,negate:!f,params:(query:mandelbox),type:phrase),query:(match_phrase:(component:mandelbox)))),index:'logzioCustomerIndex*',interval:auto,query:(language:lucene,query:''),sort:!(!('@timestamp',desc)))&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15d,to:now))`
-const PROTOCOL_CLIENT_LOGZ_BASE = `https://app.logz.io/#/dashboard/kibana/discover?_a=(columns:!(message),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'logzioCustomerIndex*',key:session_id,negate:!f,params:(query:'${sessionID}'),type:phrase),query:(match_phrase:(session_id:'${sessionID}'))),('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'logzioCustomerIndex*',key:component,negate:!f,params:(query:clientapp),type:phrase),query:(match_phrase:(component:clientapp)))),index:'logzioCustomerIndex*',interval:auto,query:(language:lucene,query:''),sort:!(!('@timestamp',desc)))&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15d,to:now))`
-
-// GET params to switch logz accounts (dev/staging/prod)
-const LOGZ_PROD_PARAMS = "&accountIds=162757&switchToAccountId=162757"
-const LOGZ_STAGING_PARAMS = "&switchToAccountId=319255&accountIds=319255"
-const LOGZ_DEV_PARAMS = "&accountIds=167279&switchToAccountId=167279"
-
-// Final URL
-export const logzProtocolServerURL = () => {
-  switch (appEnvironment) {
-    case FractalEnvironments.PRODUCTION:
-      return `${PROTOCOL_SERVER_LOGZ_BASE}${LOGZ_PROD_PARAMS}`
-    case FractalEnvironments.STAGING:
-      return `${PROTOCOL_SERVER_LOGZ_BASE}${LOGZ_STAGING_PARAMS}`
-    default:
-      return `${PROTOCOL_SERVER_LOGZ_BASE}${LOGZ_DEV_PARAMS}`
-  }
-}
-
-export const logzProtocolClientURL = () => {
-  switch (appEnvironment) {
-    case FractalEnvironments.PRODUCTION:
-      return `${PROTOCOL_CLIENT_LOGZ_BASE}${LOGZ_PROD_PARAMS}`
-    case FractalEnvironments.STAGING:
-      return `${PROTOCOL_CLIENT_LOGZ_BASE}${LOGZ_STAGING_PARAMS}`
-    default:
-      return `${PROTOCOL_CLIENT_LOGZ_BASE}${LOGZ_DEV_PARAMS}`
-  }
-}
 
 // How often to send heartbeat logs to Amplitude
 export const LOG_INTERVAL_IN_MINUTES = 5
