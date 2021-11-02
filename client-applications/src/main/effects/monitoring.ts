65525a322fc039da5830781a4b1af58aea6c2e87
mingy98 <ming@fractal.co>
2021-11-02 15:13:30 -0400

    working

--- client-applications/src/main/effects/monitoring.ts
+++ client-applications/src/main/effects/monitoring.ts
@@ -1,7 +1,41 @@
 import { logBase } from "@app/utils/logging"
 import { interval } from "rxjs"
+import * as Sentry from "@sentry/electron"
 
-const LOG_INTERVAL_IN_MINUTES = 5
+import config from "@app/config/environment"
+import { appEnvironment, FractalEnvironments } from "../../../config/configs"
+import { fromTrigger } from "@app/utils/flows"
+import {
+  sessionID,
+  logzProtocolClientURL,
+  logzProtocolServerURL,
+  LOG_INTERVAL_IN_MINUTES,
+  SENTRY_DSN,
+} from "@app/utils/constants"
+import { persistGet } from "@app/utils/persist"
+
+// Initialize and report Sentry errors in all packaged environments
+if (appEnvironment !== FractalEnvironments.LOCAL) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: config.sentryEnv,
+    release: `client-applications@${config.keys.COMMIT_SHA as string}`,
+  })
+
+  Sentry.setContext("Logz URLs", {
+    protocolClientLogs: logzProtocolClientURL(),
+    protocolServerLogs: logzProtocolServerURL(),
+  })
+
+  fromTrigger("protocolError").subscribe(() => {
+    Sentry.captureMessage(
+      `Protocol error detected for user ${
+        (persistGet("userEmail") ?? "") as string
+      } with session ID ${sessionID.toString()}. Investigate immediately.`,
+      Sentry.Severity.Error
+    )
+  })
+}
 
 interval(LOG_INTERVAL_IN_MINUTES * 60 * 1000).subscribe(() => {
   logBase("heartbeat", {})
