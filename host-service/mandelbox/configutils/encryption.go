981440f4e55c561b37c7e39146343acc967597b3
Yanchen Ma <yanchen@whist.com>
2021-11-10 17:42:55 -0500

    rewrite config encryption

--- host-service/mandelbox/configutils/encryption.go
+++ host-service/mandelbox/configutils/encryption.go
@@ -3,108 +3,140 @@ package configutils // import "github.com/fractal/fractal/host-service/mandelbox
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/sha256" // getConfigTokenHash returns a hash of the config token.
+	"crypto/rand"
+	"io"
 
 	"github.com/fractal/fractal/host-service/utils"
-	"golang.org/x/crypto/pbkdf2"
+	"golang.org/x/crypto/argon2"
 )
 
 const (
-	aes256KeyLength = 32
-	aes256IVLength  = 16
+	aes256KeyLength      = 32
+	aes256GCMNonceLength = 12
+	defaultSaltLength    = 16
 
-	// This is the default iteration value used by openssl -pbkdf2 flag
-	pbkdf2Iterations = 10000
-
-	// This is openssl's "magic" salt header value
-	opensslSaltHeader = "Salted__"
+	saltHeader  = "salt_"
+	nonceHeader = "nonce_"
 )
 
-// getSaltAndDataFromOpenSSLEncryptedFile takes OpenSSL encrypted data
-// and returns the salt used to encrypt and the encrypted data itself.
-func getSaltAndDataFromOpenSSLEncryptedFile(file []byte) (salt, data []byte, err error) {
-	if len(file) < aes.BlockSize {
-		return nil, nil, utils.MakeError("file is too short to have been encrypted by openssl")
+// EncryptAES256GCM takes a password and plaintext data and encrypts the data using GCM.
+// AES-GCM is an AEAD (authenticated encryption with associated data) method that
+// no only provides data confidentiality but also a way to verify authenticity/integrity
+// of the data. GCM is also highly performant and fully-parallelizable.
+//
+// See: https://en.wikipedia.org/wiki/Galois/Counter_Mode
+// Based on example code from: https://golang.org/src/crypto/cipher/example_test.go
+func EncryptAES256GCM(password string, data []byte) ([]byte, error) {
+	// Generate a random salt to use for key generation
+	salt, err := generateRandomBytes(defaultSaltLength)
+	if err != nil {
+		return nil, utils.MakeError("could not generate salt: %v", err)
 	}
 
-	// All OpenSSL encrypted files that use a salt have a 16-byte header
-	// The first 8 bytes are the string "Salted__"
-	// The next 8 bytes are the salt itself
-	// See: https://security.stackexchange.com/questions/29106/openssl-recover-key-and-iv-by-passphrase
-	saltHeader := file[:aes.BlockSize]
-	byteHeaderLength := len([]byte(opensslSaltHeader))
-	if string(saltHeader[:byteHeaderLength]) != opensslSaltHeader {
-		return nil, nil, utils.MakeError("file does not appear to have been encrypted by openssl: wrong salt header")
-	}
+	// We need to generate a cryptographically secure key from the password
+	key := generateKeyFromPasswordAndSalt(password, salt, aes256KeyLength)
 
-	salt = saltHeader[byteHeaderLength:]
-	data = file[aes.BlockSize:]
-
-	return
-}
-
-// getKeyAndIVFromPassAndSalt uses pbkdf2 to generate a key, IV pair
-// given a known password and salt.
-func getKeyAndIVFromPassAndSalt(password, salt []byte) (key, iv []byte) {
-	derivedKey := pbkdf2.Key(password, salt, pbkdf2Iterations, aes256KeyLength+aes256IVLength, sha256.New)
-
-	// First 32 bytes of the derived key are the key and the next 16 bytes are the IV
-	key = derivedKey[:aes256KeyLength]
-	iv = derivedKey[aes256KeyLength : aes256KeyLength+aes256IVLength]
-
-	return
-}
-
-// decryptAES256CBC takes a key, IV, and AES-256 CBC encrypted data and decrypts
-// the data in place.
-// Based on example code from: https://golang.org/src/crypto/cipher/example_test.go.
-func decryptAES256CBC(key, iv, data []byte) error {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return utils.MakeError("could not create aes cipher block: %v", err)
+		return nil, utils.MakeError("could not create aes cipher block: %v", err)
 	}
 
-	// Validate that encrypted data follows the correct format
-	// This is because CBC encryption always works in whole blocks
-	if len(data)%aes.BlockSize != 0 {
-		return utils.MakeError("given data is not a multiple of the block size")
-	}
-
-	mode := cipher.NewCBCDecrypter(block, iv)
-	mode.CryptBlocks(data, data)
-
-	// Remove PKCS7 padding
-	data, err = unpadPKCS7(data)
+	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return utils.MakeError("could not remove PKCS7 padding: %v", err)
+		return nil, utils.MakeError("could not create gcm cipher mode: %v", err)
 	}
 
-	return nil
+	// Generate a random nonce to use for encryption
+	nonce, err := generateRandomBytes(gcm.NonceSize())
+	if err != nil {
+		return nil, utils.MakeError("could not generate nonce: %v", err)
+	}
+
+	// We want to store the salt and nonce as a prefix to the encrypted data
+	prefix := append([]byte(saltHeader), salt...)
+	prefix = append(prefix, []byte(nonceHeader)...)
+	prefix = append(prefix, nonce...)
+
+	// Encrypt the data and append it to the prefix
+	encryptedData := gcm.Seal(prefix, nonce, data, nil)
+	return encryptedData, nil
 }
 
-// unpadPKCS7 takes AES-256 CBC decrypted data and undoes
-// the PKCS#7 padding that was applied before encryption.
-// See: https://en.wikipedia.org/wiki/Padding_(cryptography)#PKCS#5_and_PKCS#7
-func unpadPKCS7(data []byte) ([]byte, error) {
-	if len(data) < 1 {
-		return nil, utils.MakeError("no data to unpad")
+// DecryptAES256GCM takes a password and AES256 GCM encrypted data
+// and returns the decrypted data or an error.
+//
+// Based on example code from: https://golang.org/src/crypto/cipher/example_test.go
+func DecryptAES256GCM(password string, data []byte) ([]byte, error) {
+	// Extract the salt and nonce from the encrypted data
+	salt, nonce, encryptedData, err := getSaltNonceAndDataFromEncryptedFile(data, defaultSaltLength, aes256GCMNonceLength)
+
+	// Generate the encryption key used from the password and salt
+	key := generateKeyFromPasswordAndSalt(password, salt, aes256KeyLength)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, utils.MakeError("could not create aes cipher block: %v", err)
 	}
 
-	// The last byte of the data will always be the number of bytes to unpad
-	padLength := int(data[len(data)-1])
-
-	// Validate the padding length is valid
-	if padLength > len(data) || padLength > aes.BlockSize || padLength < 1 {
-		return nil, utils.MakeError("invalid padding length of %d is longer than data size (%d), AES block length (%d), or less than one", padLength, len(data), aes.BlockSize)
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, utils.MakeError("could not create gcm cipher mode: %v", err)
 	}
 
-	// Validate each byte of the padding to check correctness
-	for _, v := range data[len(data)-padLength:] {
-		if int(v) != padLength {
-			return nil, utils.MakeError("invalid padding byte %d, expected %d", v, padLength)
-		}
+	decryptedData, err := gcm.Open(nil, nonce, encryptedData, nil)
+	if err != nil {
+		return nil, utils.MakeError("could not decrypt data: %v", err)
 	}
 
-	// Return data minus padding
-	return data[:len(data)-padLength], nil
+	return decryptedData, nil
+}
+
+// getSaltNonceAndDataFromEncryptedFile takes an encrypted file and
+// attempts to extract the salt and nonce used to encrypt the file.
+func getSaltNonceAndDataFromEncryptedFile(file []byte, saltLength, nonceLength int) (salt, nonce, data []byte, err error) {
+	// The salt and nonce should be prefixed to the data
+	saltHeaderLength := len(saltHeader)
+	nonceHeaderLength := len(nonceHeader)
+
+	totalSaltLength := saltLength + saltHeaderLength
+	totalNonceLength := nonceLength + nonceHeaderLength
+
+	prefixLength := totalSaltLength + totalNonceLength
+
+	if len(file) < prefixLength {
+		return nil, nil, nil, utils.MakeError("encrypted file is too short")
+	}
+
+	// Verify salt header and extract salt
+	extractedSaltHeader := string(file[:saltHeaderLength])
+	if extractedSaltHeader != saltHeader {
+		return nil, nil, nil, utils.MakeError("encrypted file does not have a valid salt header")
+	}
+	salt = file[saltHeaderLength:totalSaltLength]
+
+	// Verify nonce header and extract nonce
+	extractedNonceHeader := string(file[totalSaltLength : totalSaltLength+nonceHeaderLength])
+	if extractedNonceHeader != nonceHeader {
+		return nil, nil, nil, utils.MakeError("encrypted file does not have a valid nonce header")
+	}
+	nonce = file[totalSaltLength+nonceHeaderLength : prefixLength]
+
+	// The remaining is encrypted data
+	data = file[prefixLength:]
+	return
+}
+
+// generateKeyFromPasswordAndSalt takes a password and salt and returns
+// a cryptographically secure key using argon2id with default parameters.
+func generateKeyFromPasswordAndSalt(password string, salt []byte, length uint32) []byte {
+	return argon2.IDKey([]byte(password), salt, 1, 64*1024, 8, length)
+}
+
+// generateRandomBytes takes a length and returns a slice of random bytes of that length.
+func generateRandomBytes(length int) ([]byte, error) {
+	randomData := make([]byte, length)
+	if _, err := io.ReadFull(rand.Reader, randomData); err != nil {
+		return nil, utils.MakeError("could not generate random bytes: %v", err)
+	}
+	return randomData, nil
 }
