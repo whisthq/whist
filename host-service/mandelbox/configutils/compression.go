a5bca62effa0877fc7a20a7765fa57cd57423191
Yanchen Ma <yanchen@whist.com>
2021-11-11 14:54:12 -0500

    refactor compression code

---
+++ host-service/mandelbox/configutils/compression.go
@@ -0,0 +1,230 @@
+package configutils // import "github.com/fractal/fractal/host-service/mandelbox/configutils"
+
+import (
+	"archive/tar"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	logger "github.com/fractal/fractal/host-service/fractallogger"
+	"github.com/fractal/fractal/host-service/utils"
+	"github.com/pierrec/lz4/v4"
+)
+
+// ExtractTarLz4 extracts a tar.lz4 file in memory to the specified directory.
+func ExtractTarLz4(file []byte, dir string) error {
+	// Create the destination directory if it doesn't exist.
+	_, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				return utils.MakeError("failed to create directory %s: %v", dir, err)
+			}
+		} else {
+			return utils.MakeError("failed to stat directory %s: %v", dir, err)
+		}
+	}
+
+	// Extracting the tar.lz4 file involves:
+	// 1. Decompress the the lz4 file into a tar
+	// 2. Untar the config to the target directory
+	dataReader := bytes.NewReader(file)
+	lz4Reader := lz4.NewReader(dataReader)
+	tarReader := tar.NewReader(lz4Reader)
+
+	for {
+		// Read the header for the next file in the tar
+		header, err := tarReader.Next()
+
+		// If we reach EOF, that means we are done untaring
+		switch {
+		case err == io.EOF:
+			break
+		case err != nil:
+			return utils.MakeError("error reading tar file: %v", err)
+		}
+
+		// Not certain why this case happens, but causes segfaults if removed
+		if header == nil {
+			break
+		}
+
+		path := filepath.Join(dir, header.Name)
+
+		// Check that the path is within the destination directory
+		// to avoid the zip slip vulnerability
+		// See: https://snyk.io/research/zip-slip-vulnerability
+		if !strings.HasPrefix(path, dir) {
+			return utils.MakeError("destination path %s is illegal", path)
+		}
+
+		info := header.FileInfo()
+
+		// Create directory if it doesn't exist, otherwise go next
+		if info.IsDir() {
+			if err = os.MkdirAll(path, info.Mode()); err != nil {
+				return utils.MakeError("error creating directory: %v", err)
+			}
+			continue
+		}
+
+		// Create file and copy contents
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+		if err != nil {
+			return utils.MakeError("error opening file: %v", err)
+		}
+
+		if _, err := io.Copy(file, tarReader); err != nil {
+			file.Close()
+			return utils.MakeError("error copying data to file: %v", err)
+		}
+
+		// Manually close file instead of defer otherwise files are only
+		// closed when ALL files are done unpacking
+		if err := file.Close(); err != nil {
+			logger.Warningf("Failed to close file %s: %v", path, err)
+		}
+	}
+
+	return nil
+}
+
+// CompressTarLz4 takes a directory and compresses it into a tar.lz4 file.
+func CompressTarLz4(dir string) ([]byte, error) {
+	// Check that the directory exists and is a directory
+	info, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, utils.MakeError("directory %s does not exist", dir)
+		}
+		return nil, utils.MakeError("failed to stat directory %s: %v", dir, err)
+	}
+	if !info.IsDir() {
+		return nil, utils.MakeError("%s is not a directory", dir)
+	}
+
+	// Create a buffer to write the tar.lz4 file to
+	buf := new(bytes.Buffer)
+
+	// Compressing the tar.lz4 file involves:
+	// 1. Create a tar of the directory
+	// 2. Compress the tar into a lz4 file
+	lz4Writer := lz4.NewWriter(buf)
+	defer lz4Writer.Close()
+	tarWriter := tar.NewWriter(lz4Writer)
+	defer tarWriter.Close()
+
+	// Walk the directory and add all files to the tar
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return utils.MakeError("error walking directory: %v", err)
+		}
+
+		// Create header for the file
+		header, err := tar.FileInfoHeader(info, info.Name())
+		if err != nil {
+			return utils.MakeError("error creating tar header for file %s: %v", path, err)
+		}
+
+		// Convert the path to a relative path from the base directory.
+		// This is required because we need the header name to be the
+		// full relative path from the base directory, info.Name()
+		// only gives the base name of the file.
+		// See: https://pkg.go.dev/archive/tar#FileInfoHeader
+		relPath, err := filepath.Rel(dir, path)
+		if err != nil {
+			return utils.MakeError("error converting path %s to relative path: %v", path, err)
+		}
+		header.Name = relPath
+
+		// Write the header to the tar
+		if err := tarWriter.WriteHeader(header); err != nil {
+			return utils.MakeError("error writing tar header for file %s: %v", path, err)
+		}
+
+		// Skip directories and symlinks since there is no data to tar
+		// source: https://medium.com/@komuw/just-like-you-did-fbdd7df829d3
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		// Open the file and copy its contents to the tar
+		file, err := os.Open(path)
+		if err != nil {
+			return utils.MakeError("error opening file %s: %v", path, err)
+		}
+
+		if _, err := io.Copy(tarWriter, file); err != nil {
+			file.Close()
+			return utils.MakeError("error copying data from file %s: %v", path, err)
+		}
+
+		// Manually close file instead of defer otherwise files are only
+		// closed when ALL files are done packing
+		if err := file.Close(); err != nil {
+			logger.Warningf("Failed to close file %s: %v", path, err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, utils.MakeError("error tarring directory: %v", err)
+	}
+
+	return buf.Bytes(), nil
+}
+
+// ValidateDirectoryContents checks if all directories and files in the
+// old directory are present in the new directory and have the same contents.
+func ValidateDirectoryContents(oldDir, newDir string) error {
+	return filepath.Walk(oldDir, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relativePath, err := filepath.Rel(oldDir, filePath)
+		if err != nil {
+			return utils.MakeError("error getting relative path for %s: %v", filePath, err)
+		}
+
+		newFilePath := filepath.Join(newDir, relativePath)
+		matchingFile, err := os.Open(newFilePath)
+		if err != nil {
+			return utils.MakeError("error opening matching file %s: %v", newFilePath, err)
+		}
+		defer matchingFile.Close()
+
+		matchingFileInfo, err := matchingFile.Stat()
+		if err != nil {
+			return utils.MakeError("error reading stat for file: %s: %v", newFilePath, err)
+		}
+
+		// If one is a directory, both should be
+		if info.IsDir() {
+			if !matchingFileInfo.IsDir() {
+				return utils.MakeError("expected %s to be a directory", newFilePath)
+			}
+		} else {
+			testFileContents, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				return utils.MakeError("error reading test file %s: %v", filePath, err)
+			}
+
+			matchingFileBuf := bytes.NewBuffer(nil)
+			_, err = matchingFileBuf.ReadFrom(matchingFile)
+			if err != nil {
+				return utils.MakeError("error reading matching file %s: %v", newFilePath, err)
+			}
+
+			// Check contents match
+			if string(testFileContents) != string(matchingFileBuf.Bytes()) {
+				return utils.MakeError("file contents don't match for file %s: '%s' vs '%s'", filePath, testFileContents, matchingFileBuf.Bytes())
+			}
+		}
+
+		return nil
+	})
+}
