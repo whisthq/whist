7d98b85a961ac76ec8ceddef4393b5bfba89d27a
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2021-11-11 15:43:55 -0500

    updated spin_up_dev_client_container.sh

--- mandelboxes/development/client/spin_up_dev_client_container.sh
+++ mandelboxes/development/client/spin_up_dev_client_container.sh
@@ -14,6 +14,14 @@
 # Exit on subcommand errors
 set -Eeuo pipefail
 
+# Ensure that the /fractal folder exists
+if [ -d "/fractal" ]; then
+  echo "/fractal folder exists!"
+else
+  echo "/fractal folder does not exist"
+  exit 1
+fi
+
 # TODO: 0. Find tag of matching Docker image
 ## App name will be fractal/development/client:current-build; We need to look for images with a tag that matches either
 ## one of the following regexes [fractal/development/client:current-build:current-build fractal/development/client:current-build]
@@ -31,14 +39,59 @@ if [[ $CMD_OUTPUT == "no" ]]; then
 	fi
 fi
 
-# TODO: 1. Download any necessary user configs onto the container
-# TODO: 2. Apply port bindings/forwarding if needed
-# TODO: 3. Pass config variables such as FRACTAL_AES_KEY, which will be saved to file by the startup/entrypoint.sh script, in order for the container to be able to access them later and exported them as environment variables by the `run-fractal-client.sh` script. These config variables will have to be passed as parameters to the FractalClient executable, which will run in non-root mode in the container (username = fractal).
-# TODO: 4. Create the Docker container, and start it
-docker create -e SERVER_IP_ADDRESS=${1} -e SERVER_PORT_32262=${2} -e SERVER_PORT_32263=${3} -e SERVER_PORT_32273=${4} -e FRACTAL_AES_KEY=${5} $DOCKER_IMAGE_NAME 
-docker run -i -a stdin -a stdout -a stderr $DOCKER_IMAGE_NAME
-# TODO: 5. Decrypt user configs within the docker container, if needed
+# Generate string with ENVS
+CONTAINER_ENVS="-e SERVER_IP_ADDRESS=${1} \
+-e SERVER_PORT_32262=${2} \
+-e SERVER_PORT_32263=${3} \
+-e SERVER_PORT_32273=${4} \
+-e FRACTAL_AES_KEY=${5} \
+-e NVIDIA_DRIVER_CAPABILITIES=all \
+-e NVIDIA_VISIBLE_DEVICES=all"
+
+random_str_cmd=`echo $RANDOM | md5sum | head -c 20`
+random_str="$random_str_cmd"
+fake_mandelboxID="dev_client_${random_str}"
+
+# Generate the Resource Mappings folder
+mkdir -p /fractal/$fake_mandelboxID/containerResourceMappings
+
+DOCKER_CREATE_CMD=`docker create -it \
+    -v "/sys/fs/cgroup:/sys/fs/cgroup:ro" \
+    -v "/fractal/$fake_mandelboxID/containerResourceMappings:/fractal/resourceMappings:ro" \
+    -v "/fractal/temp/$fake_mandelboxID/sockets:/tmp/sockets" \
+    -v "/run/udev/data:/run/udev/data:ro" \
+    -v "/fractal/$fake_mandelboxID/userConfigs/unpacked_configs:/fractal/userConfigs:rshared" \
+    --tmpfs /run \
+    --tmpfs /run/lock \
+    --gpus all \
+    $CONTAINER_ENVS \
+    --shm-size=2147483648 \
+    --cap-drop ALL \
+    --cap-add CAP_SETPCAP \
+    --cap-add CAP_MKNOD \
+    --cap-add CAP_AUDIT_WRITE \
+    --cap-add CAP_CHOWN \
+    --cap-add CAP_NET_RAW \
+    --cap-add CAP_DAC_OVERRIDE \
+    --cap-add CAP_FOWNER \
+    --cap-add CAP_FSETID \
+    --cap-add CAP_KILL \
+    --cap-add CAP_SETGID \
+    --cap-add CAP_SETUID \
+    --cap-add CAP_NET_BIND_SERVICE \
+    --cap-add CAP_SYS_CHROOT \
+    --cap-add CAP_SETFCAP \
+    --cap-add SYS_NICE \
+    $DOCKER_IMAGE_NAME`
+
+CONTAINER_ID="$DOCKER_CREATE_CMD | tail -1"
+
+docker start $CONTAINER_ID
 # TODO: 6. Write the config.json file if we want to test JSON transport related features
-docker exec $DOCKER_IMAGE_NAME bash -c "touch config.json"
+docker exec $CONTAINER_ID /bin/bash -c "touch config.json"
 # TODO: 7. Write the .ready file to trigger `base/startup/fractal-startup.sh`
-docker exec $DOCKER_IMAGE_NAME bash -c "echo .ready >> .ready"
+docker exec $CONTAINER_ID /bin/bash -c "echo .ready >> .ready"
+
+
+
+
