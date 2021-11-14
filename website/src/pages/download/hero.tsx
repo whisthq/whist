d8b09e4d9e41f0a9376a3a2264d571621b2fda8e
mingy98 <ming@fractal.co>
2021-11-14 15:21:54 -0500

    website has calendly

--- website/src/pages/download/hero.tsx
+++ website/src/pages/download/hero.tsx
@@ -10,7 +10,9 @@ import { ScreenSize } from "@app/shared/constants/screenSizes"
 import { withContext } from "@app/shared/utils/context"
 import { config } from "@app/shared/constants/config"
 
-const Hero = (props: { allowDownloads: boolean }) => {
+const CALENDLY_URL = "https://calendly.com/whist-ming/onboarding?month=2021-11"
+
+const AllowDownloads = () => {
   /*
         Colorful box component with Get Started prompt
 
@@ -36,56 +38,43 @@ const Hero = (props: { allowDownloads: boolean }) => {
         </div>
         {width > ScreenSize.SMALL ? (
           <>
-            {props.allowDownloads ? (
-              <div className="flex justify-center">
-                <a href={config.client_download_urls.macOS_x64} download>
-                  <FractalButton
-                    className="mt-12 mx-2"
-                    contents={
-                      <div className="flex">
-                        <FaApple className="relative mr-3 top-0.5" />
-                        macOS (Intel)
-                      </div>
-                    }
-                    state={FractalButtonState.DEFAULT}
-                  />
-                </a>
-                <a href={config.client_download_urls.macOS_arm64} download>
-                  <FractalButton
-                    className="mt-12 mx-2"
-                    contents={
-                      <div className="flex">
-                        <FaApple className="relative mr-3 top-0.5" />
-                        macOS (M1)
-                      </div>
-                    }
-                    state={FractalButtonState.DEFAULT}
-                  />
-                </a>
+            {" "}
+            <div className="flex justify-center">
+              <a href={config.client_download_urls.macOS_x64} download>
                 <FractalButton
                   className="mt-12 mx-2"
                   contents={
                     <div className="flex">
-                      <FaWindows className="relative mr-3 top-0.5" />
-                      Coming Soon
+                      <FaApple className="relative mr-3 top-0.5" />
+                      macOS (Intel)
                     </div>
                   }
-                  state={FractalButtonState.DISABLED}
-                />
-              </div>
-            ) : (
-              <a
-                href="https://whist.typeform.com/to/RsOsBSSu"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FractalButton
-                  className="mt-12 mx-2"
-                  contents={<div className="flex">Join Waitlist</div>}
                   state={FractalButtonState.DEFAULT}
                 />
               </a>
-            )}
+              <a href={config.client_download_urls.macOS_arm64} download>
+                <FractalButton
+                  className="mt-12 mx-2"
+                  contents={
+                    <div className="flex">
+                      <FaApple className="relative mr-3 top-0.5" />
+                      macOS (M1)
+                    </div>
+                  }
+                  state={FractalButtonState.DEFAULT}
+                />
+              </a>
+              <FractalButton
+                className="mt-12 mx-2"
+                contents={
+                  <div className="flex">
+                    <FaWindows className="relative mr-3 top-0.5" />
+                    Coming Soon
+                  </div>
+                }
+                state={FractalButtonState.DISABLED}
+              />
+            </div>
           </>
         ) : (
           <div className="flex justify-center">
@@ -105,6 +94,46 @@ const Hero = (props: { allowDownloads: boolean }) => {
       </div>
     </div>
   )
+}
+
+const DontAllowDownloads = () => {
+  /*
+        Colorful box component with Get Started prompt
+
+        Arguments:
+            none
+    */
+  return (
+    <div className="mb-24 text-center pt-36">
+      <div className="px-0 md:px-12">
+        <div className="text-6xl md:text-7xl tracking-wide leading-snug text-gray dark:text-gray-300 mb-4">
+          Try the <span className="text-mint">alpha release</span>
+        </div>
+        <div className="text-md md:text-lg text-gray tracking-wide dark:text-gray-400 max-w-xl m-auto">
+          If you meet the{" "}
+          <Link to="requirements" spy={true} smooth={true}>
+            <span className="text-mint cursor-pointer">requirements below</span>
+          </Link>{" "}
+          then we would love for you to try Whist. Please reserve an onboarding
+          time to get started!
+        </div>
+        <div>
+          <a href={CALENDLY_URL} target="_blank" rel="noreferrer">
+            <FractalButton
+              className="mt-12 mx-2"
+              contents={"Reserve an onboarding session"}
+              state={FractalButtonState.DEFAULT}
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Hero = (props: { allowDownloads: boolean }) => {
+  if (props.allowDownloads) return <AllowDownloads />
+  return <DontAllowDownloads />
 }
 
 export default Hero
