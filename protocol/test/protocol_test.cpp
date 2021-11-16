6842e788d562b7288fd806d38bf41189b772f442
Philippe Noël <phil@whist.com>
2021-11-16 01:43:33 -0500

    fix CI warnings

--- protocol/test/protocol_test.cpp
+++ protocol/test/protocol_test.cpp
@@ -21,6 +21,10 @@ Includes
 ============================
 */
 
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS  // stupid Windows warnings
+#endif
+
 #include <iostream>
 #include <fstream>
 #include <algorithm>
