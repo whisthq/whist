32417d20cde29270f6e2165cfc59aa2494417f82
mingy98 <ming@fractal.co>
2021-11-18 16:07:31 -0500

    add logging for testing

--- protocol/client/sdlscreeninfo.c
+++ protocol/client/sdlscreeninfo.c
@@ -40,7 +40,7 @@ int get_window_virtual_height(SDL_Window *window) {
 
 int get_virtual_screen_width(SDL_Window *window) {
     SDL_DisplayMode dm;
-    //    int res = SDL_GetCurrentDisplayMode(0, &DM);
+    LOG_INFO("The display index is %d", SDL_GetWindowDisplayIndex(window));
     int res = SDL_GetDesktopDisplayMode(SDL_GetWindowDisplayIndex(window), &dm);
     if (res) LOG_WARNING("SDL_GetCurrentDisplayMode failed: %s", SDL_GetError());
     return dm.w;
@@ -48,7 +48,7 @@ int get_virtual_screen_width(SDL_Window *window) {
 
 int get_virtual_screen_height(SDL_Window *window) {
     SDL_DisplayMode dm;
-    //    int res = SDL_GetCurrentDisplayMode(0, &DM);
+    LOG_INFO("The display index is %d", SDL_GetWindowDisplayIndex(window));
     int res = SDL_GetDesktopDisplayMode(SDL_GetWindowDisplayIndex(window), &dm);
     if (res) LOG_WARNING("SDL_GetCurrentDisplayMode failed: %s", SDL_GetError());
     return dm.h;
