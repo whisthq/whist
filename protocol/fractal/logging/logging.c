dd6686d85adb5a3e534f26e93e4f05628d48b1c9
Suriya Kandaswamy <suriya.kandaswamy@gmail.com>
2021-11-19 15:41:57 -0500

    reset signal handler to default after printing stacktrace

--- protocol/fractal/logging/logging.c
+++ protocol/fractal/logging/logging.c
@@ -537,7 +537,11 @@ LONG WINAPI windows_exception_handler(EXCEPTION_POINTERS* ExceptionInfo) {  // N
 void unix_crash_handler(int sig) {
     fprintf(stdout, "\nError: signal %d:%s\n", sig, strsignal(sig));
     print_stacktrace();
-    _exit(-1);
+    // We reset the signal handler to default to allow Sentry to handle the crash without
+    //     getting stuck in an infinite loop of crash handling
+    signal(sig, SIG_DFL);
+    // If we exit, then the program doesn't crash and Sentry does not receive the event
+    // _exit(-1);
 }
 #endif
 
