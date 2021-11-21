6b839c4f9b55304bedac153e0d78c48a9e32ea75
Aaron Choo <aaron@whist.com>
2021-11-21 14:10:20 -0500

    add func to get current commit hash

--- webserver/app/helpers/aws/aws_instance_post.py
+++ webserver/app/helpers/aws/aws_instance_post.py
@@ -505,21 +505,24 @@ def check_and_handle_lingering_instances() -> None:
         drain_instance(instance_info)
 
 
+def get_current_commit_hash() -> str:
+    """
+    Returns a current commit hash
+    """
+    commit_hash = RegionToAmi.query.filter_by(ami_active=True).first().client_commit_hash
+    return str(commit_hash) if commit_hash else ""
+
+
 def check_and_handle_instances_with_old_commit_hash() -> None:
     """
-    Drain all instances assoicated with old commit hash
+    Drain all instances assoicated with old commit hashes
     Returns:
         None
     """
     # Normally instances with inactive ami are set to draining when swapiover_amis is invoked
-    # but in the case where it does fail we want to be sure to clean up the mess
-    # All active ami must have the same commit hash
-    current_commit_hash = (
-        RegionToAmi.query.distinct(RegionToAmi.client_commit_hash)
-        .filter_by(ami_active=True)
-        .first()
-        .client_commit_hash
-    )
+    # but in the case where it does fail we want to be sure to clean up the mess.
+    # All active ami must have the same commit hash.
+    current_commit_hash = get_current_commit_hash()
 
     instances_with_old_commit_hash = InstanceInfo.query.filter(
         InstanceInfo.commit_hash != current_commit_hash,
