b35108f68a3a0833dd1015fcb84df688bb9b4b71
Aaron Choo <aaron@whist.com>
2021-11-25 02:17:27 -0500

    add tests

--- host-service/httpserver_test.go
+++ host-service/httpserver_test.go
@@ -216,6 +216,173 @@ func TestHttpServerIntegration(t *testing.T) {
 	t.Log("server goroutine ended")
 }
 
+// TestSendRequestResultErr checks if an error result is handled properly
+func TestSendRequestResultErr(t *testing.T) {
+	reqResult := requestResult{
+		Err: utils.MakeError("test error")
+	}
+	res := httptest.NewRecorder()
+
+	// A 406 error should arise
+	reqResult.send(*res)
+
+	if res.Result().StatusCode != http.StatusNotAcceptable {
+		t.Fatalf("error sending request results with error. Expected status code %v, got %v", http.StatusNotAcceptable, res.Result().StatusCode)
+	}
+}
+
+// TestSendRequestResult tests if a valid request resolves successfully
+func TestSendRequestResultErr(t *testing.T) {
+	reqResult := requestResult{
+		Result: "test result"
+	}
+	res := httptest.NewRecorder()
+
+	// A 200 status code should be set
+	reqResult.send(*res)
+
+	if res.Result().StatusCode != http.StatusOK {
+		t.Fatalf("error sending a valid request results. Expected status code %v, got %v", http.StatusOK, res.Result().StatusCode)
+	}
+}
+
+// TestProcessJSONDataRequestWrongType checks if the wrong request type fails silently
+func TestProcessJSONDataRequestWrongType(t *testing.T) {
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(httptest.MethodPost)
+	testChan := make(chan ServerRequest)
+
+	// With the wrong request type processJSONDataRequest will fail quietly
+	processJSONDataRequest(res, req, testChan)
+
+	select {
+		case result := <-testChan:
+			t.Fatalf("error processing json data request with wrong request type. Expected test server request chan to be empty")
+		default:
+	}
+}
+
+// TestProcessJSONDataRequestEmptyBody checks if an empty body fails silently
+func TestProcessJSONDataRequestEmptyBody(t *testing.T) {
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(httptest.MethodPut)
+	testChan := make(chan ServerRequest)
+
+	// With an empty request body processJSONDataRequest will fail quietly (will not be able to unmarshal)
+	processJSONDataRequest(res, req, testChan)
+
+	select {
+		case result := <-testChan:
+			t.Fatalf("error processing json data request with empty. Expected test server request chan to be empty")
+		default:
+	}
+}
+
+// TestHandleJSONTransportRequest checks if valid fields will successfully send json transport request
+func TestHandleJSONTransportRequest(t *testing.T) {
+	testJSONTransportRequest := JSONTransportRequest{
+		ConfigEncryptionToken: "testToken1234",
+		JwtAccessToken:        "test_jwt_token",
+		MandelboxID:           mandelboxtypes.MandelboxID(utils.PlaceholderTestUUID()),
+		JSONData:              "test_json_data",
+		resultChan:            make(chan requestResult),
+	}
+
+	testmux := &sync.Mutex{}
+	testTransportRequestMap := make(map[mandelboxtypes.MandelboxID]chan *JSONTransportRequest)
+
+	handleJSONTransportRequest(&testJSONTransportRequest, testTransportRequestMap, testmux)
+
+	select {
+		case result := <-testTransportRequestMap[req.MandelboxID]:
+			return
+		default:
+			t.Fatalf("error handling json transport requests. Expected a request from the request chan")
+	}
+}
+
+// TestGetJSONTransportRequestChannel checks if JSON transport request is created for the mandelboxID
+func TestGetJSONTransportRequestChannel(t *testing.T) {
+	mandelboxID := mandelboxtypes.MandelboxID(utils.PlaceholderTestUUID())
+	testmux := &sync.Mutex{}
+	testTransportRequestMap := make(map[mandelboxtypes.MandelboxID]chan *JSONTransportRequest)
+
+	// Should create a new JSONTransportRequest chan
+	testJsonChan := getJSONTransportRequestChannel(mandelboxID, testTransportRequestMap, testmux)
+
+	if _, ok := interface{}(testJsonChan).(chan *JSONTransportRequest); !ok {
+		t.Fatalf("error getting json transport request channel")
+	}
+}
+
+
+// TestGetAppNameEmpty will check if default AppName is browser/chrome
+func TestGetAppNameEmpty(t *testing.T) {
+	mandelboxID := mandelboxtypes.MandelboxID(utils.PlaceholderTestUUID())
+	testmux := &sync.Mutex{}
+	testTransportRequestMap := make(map[mandelboxtypes.MandelboxID]chan *JSONTransportRequest)
+	
+	// Should default name to browser/chrome
+	req, appName := getAppName(mandelboxID, testTransportRequestMap, testmux)
+	
+	if appName != mandelboxtypes.AppName("browsers/chrome") {
+		t.Fatalf("error getting app name. Expected %v, got %v", mandelboxtypes.AppName("browsers/chrome"), appName)
+	}
+
+}
+
+// TestGetAppName will set appName to json request app name
+func TestGetAppName(t *testing.T) {generateTestJSONTransportRequest
+	testJSONTransportRequest := JSONTransportRequest{
+		AppName:		mandelboxtypes.AppName("test_app_name")
+	}
+
+	mandelboxID := mandelboxtypes.MandelboxID(utils.PlaceholderTestUUID())
+	testmux := &sync.Mutex{}
+	testTransportRequestMap := make(map[mandelboxtypes.MandelboxID]chan *JSONTransportRequest)
+	
+	// Assign JSONTRansportRequest
+	testTransportRequestMap[MandelboxID] = make(chan *JSONTransportRequest, 1)
+	testTransportRequestMap[MandelboxID] <- testJSONTransportRequest
+
+	// Should be set to test_app_name
+	req, appName := getAppName(mandelboxID, testTransportRequestMap, testmux)
+	
+	if appName != testJSONTransportRequest.AppName {
+		t.Fatalf("error getting app name. Expected %v, got %v", testJSONTransportRequest.AppName, appName)
+	}
+}
+
+// TestVerifyRequestWrongType will create a request method that does not match the expected method
+func TestVerifyRequestWrongType(t *testing.T) {
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(httptest.MethodPost)
+	
+	// Verify request type will catch request with wrong method and return an error
+	if err := verifyRequestType(res, req, http.PUT); err == nil {
+		t.Fatal("error verifying request type when the request method does not match. Expected an error, got nil")
+	}
+
+	if res.Result().StatusCode != http.StatusBadRequest {
+		t.Fatalf("error verifying request type when the request method does not match. Expected status code %v, got %v", http.StatusBadRequest, res.Result().StatusCode)
+	}
+}
+
+// TestVerifyRequestTypeNilRequest checks if nil requests are handled properly
+func TestVerifyRequestTypeNilRequest(t *testing.T) {
+	res := httptest.NewRecorder()
+
+	// Verify request type will catch nil request and return an error
+	if err := verifyRequestType(res, nil, httptest.MethodPut); err == nil {
+		t.Fatal("error verifying request type when request is nil. Expected an error, got nil")
+	}
+
+	if res.Result().StatusCode != http.StatusBadRequest {
+		t.Fatalf("error verifying request type when request is nil. Expected status code %v, got %v", http.StatusBadRequest, res.Result().StatusCode)
+	}
+	
+}
+
 // generateTestJSONTransportRequest takes a request body and creates an
 // HTTP PUT request for /json_transport
 func generateTestJSONTransportRequest(requestBody JSONTransportRequest) (*http.Request, error) {
