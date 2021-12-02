a6baeed1236ee1b94542de0cb9ea1055d0d2ad41
Ming Ying <ming@whist.com>
2021-12-02 13:46:34 -0500

    new onboarding flow, no more calendly

--- website/src/pages/download/typeform.tsx
+++ website/src/pages/download/typeform.tsx
@@ -1,27 +1,8 @@
-import React, { useState } from "react"
+import React from "react"
 import { Widget } from "@typeform/embed-react"
 
-const Typeform = (props: { id: string; onSubmit: () => void }) => {
-  const [submitted, setSubmitted] = useState(false)
-
-  const onSubmit = () => {
-    setSubmitted(true)
-    props.onSubmit()
-  }
-
-  if (!submitted) {
-    return (
-      <Widget id={props.id} className="w-full h-full" onSubmit={onSubmit} />
-    )
-  } else {
-    return (
-      <div className="relative bg-gray-900">
-        <div className="flex justify-center items-center m-auto pt-72">
-          <div className="mt-12 animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue"></div>
-        </div>
-      </div>
-    )
-  }
+const Typeform = (props: { id: string }) => {
+  return <Widget id={props.id} className="w-full h-full" />
 }
 
 export default Typeform
