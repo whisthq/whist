b87923af09846d39eb43c5d030488b6d83b44844
Aaron Choo <aaron@whist.com>
2021-12-03 17:47:03 -0500

    adding test to auth

--- host-service/auth/auth_test.go
+++ host-service/auth/auth_test.go
@@ -11,6 +11,18 @@ import (
 )
 
 func TestUnmarshalScopes(t *testing.T) {
+	// Ensure that UnmarshalJSON returns an error when given invalid data
+	t.Run("InvalidJSON", func(t *testing.T) {
+		t.Parallel()
+
+		scope := make(auth.Scopes, 0)
+		err := scope.UnmarshalJSON([]byte{})
+
+		if err == nil {
+			t.Fatal("UmarshalJSON should have an err")
+		}
+	})
+
 	// Ensure that UnmarshalJSON unmarshals an empty scopes type when passed an
 	// empty byte slice.
 	t.Run("EmptyString", func(t *testing.T) {
@@ -122,6 +134,61 @@ func TestUnmarshalScopes(t *testing.T) {
 	})
 }
 
+
+func TestUnmarshalAudience(t *testing.T) {
+	// Ensure that UnmarshalJSON returns an error when it cannot unmarshal to string
+	t.Run("InvalidJSON", func(t *testing.T) {
+		t.Parallel()
+
+		audience := make(auth.Audience, 0)
+		// UnmarshalJSON will throw an error as the byte array does not contain a string/string splice
+		err := audience.UnmarshalJSON([]byte(`{"test":"not string"}`))
+
+		if _, ok := err.(*json.UnmarshalTypeError); !ok || err == nil {
+			t.Fatal("UmarshalJSON should have an err")
+		}
+	})
+}
+
+func TestVerify(t *testing.T) {
+	// Ensure an invalid token will result in an error
+	t.Run("InvalidToken", func(t *testing.T) {
+		t.Parallel()
+
+		token := "not_a_real_token"
+		// Verify should return an error as the token is invalid
+		claim, err := Verify(token)
+
+		if err == nil {
+			t.Fatal("Verify should have returned an err")
+		}
+
+		if claim != nil {
+			t.Fatalf("Verify should have set claim to nil but got %v", claim)
+		}
+	})
+
+	// Ensure a token with missing Iss will result in an error
+	t.Run("TokenWIthoutIss", func(t *testing.T) {
+		t.Parallel()
+
+		// rawToken is an H256-signed JWT whose payload is {"aud": "foo"}
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJmb28ifQ.h44sAymzUhQYXbdOqrCkr7aEf0HkU-wbgWQdQ74Q288"
+		// Verify should return an error as the token is invalid
+		claim, err := Verify(token)
+
+		if err == nil {
+			t.Fatal("Verify should have returned an err because Iss is missing")
+		}
+
+		if claim != nil {
+			t.Fatalf("Verify should have set claim to nil but got %v", claim)
+		}
+	})
+
+	// nneed to pass the ParseWithClaims but fail next then need one to pass all 
+}
+
 func TestParseToken(t *testing.T) {
 	// Ensure that tokens whose "aud" claim is a string are parsed correctly.
 	t.Run("OneAudience", func(t *testing.T) {
@@ -205,3 +272,4 @@ func TestVerifyScope(t *testing.T) {
 		}
 	})
 }
+
