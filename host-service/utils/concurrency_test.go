269d206561130071450c83fbc9fc3d42e1c3f6c5
Aaron Choo <aaron@whist.com>
2021-12-03 13:24:44 -0500

    add test for concurrency

---
+++ host-service/utils/concurrency_test.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"time"
+	"testing"
+)
+
+// TestStopAndDrainTimer will confirm no deadlocks occurred and timerChan is not empty
+func TestStopAndDrainTimer(t *testing.T) {
+	// Simulate the actual usage of this function
+	timerChan := make(chan interface{})
+	sleepTime := 0
+	timer := time.AfterFunc(time.Duration(sleepTime)*time.Millisecond, func() { timerChan <- struct{}{} })
+
+	// Does nothing as timer has been stopped
+	StopAndDrainTimer(timer)
+
+	select {
+	case <-timerChan:
+		return
+	default:
+		t.Fatal("error stopping and draining timer")
+	}
+}
+
+// TestStopAndDrainTimerAlreadyStopped will confirm no deadlocks occurred and timerChan is empty
+func TestStopAndDrainTimerAlreadyStopped(t *testing.T) {
+	// Simulate the actual usage of this function
+	timerChan := make(chan interface{})
+	sleepTime := 60000
+	timer := time.AfterFunc(time.Duration(sleepTime)*time.Millisecond, func() { timerChan <- struct{}{} })
+
+	//  Will stop timer
+	StopAndDrainTimer(timer)
+
+	select {
+	case <-timerChan:
+		t.Fatal("error stopping and draining timer when it is already stopped")
+	default:
+		return
+	}
+}
