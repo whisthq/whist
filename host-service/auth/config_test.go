839640da187a5baad5c105dd846ba9a7c0f73bb5
Aaron Choo <aaron@whist.com>
2021-12-03 17:46:47 -0500

    test all cases of appEnv

---
+++ host-service/auth/config_test.go
@@ -0,0 +1,37 @@
+package auth
+
+import (
+	"testing"
+
+	"github.com/fractal/fractal/host-service/metadata"
+)
+
+// TestGetAuthConfigDev will get the dev app environmet
+func TestGetAuthConfigDev(t *testing.T) {
+	// getAuthConfig should return authConfigDev
+	config := getAuthConfig(metadata.EnvDev)
+
+	if config.Aud != authConfigDev.Aud || config.Iss != authConfigDev.Iss {
+		t.Fatalf("error getting auth config for dev. Expected %v, got %v", authConfigDev, config)
+	}
+}
+
+// TestGetAuthConfigStaging will get the dev app environmet
+func TestGetAuthConfigStaging(t *testing.T) {
+	// getAuthConfig should return authConfigStaging
+	config := getAuthConfig(metadata.EnvStaging)
+
+	if config.Aud != authConfigStaging.Aud || config.Iss != authConfigStaging.Iss {
+		t.Fatalf("error getting auth config for staging. Expected %v, got %v", authConfigStaging, config)
+	}
+}
+
+// TestGetAuthConfigProd will get the dev app environmet
+func TestGetAuthConfigProd(t *testing.T) {
+	// getAuthConfig should return authConfigProd
+	config := getAuthConfig(metadata.EnvProd)
+
+	if config.Aud != authConfigProd.Aud || config.Iss != authConfigProd.Iss {
+		t.Fatalf("error getting auth config for prod. Expected %v, got %v", authConfigProd, config)
+	}
+}
