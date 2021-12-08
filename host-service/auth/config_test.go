15b2821cba22ebf11ecde4b92894f2b0a6b1961e
Aaron Choo <aaron@whist.com>
2021-12-08 11:47:22 -0500

    update path to metadata

--- host-service/auth/config_test.go
+++ host-service/auth/config_test.go
@@ -3,7 +3,7 @@ package auth
 import (
 	"testing"
 
-	"github.com/fractal/fractal/host-service/metadata"
+	"github.com/fractal/whist/host-service/metadata"
 )
 
 // TestGetAuthConfigDev will get the dev app environmet
