ef34740f8cdae6e3aad9c3a429dd347fb2d90657
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2021-12-11 14:36:08 -0500

    remove unused frame_data var

--- protocol/client/video.c
+++ protocol/client/video.c
@@ -830,7 +830,6 @@ int render_video() {
     WhistRGBColor window_color = {0};
     WhistCursorImage cursor_image = {0};
     bool has_cursor_image = false;
-    FrameData frame_data;
     timestamp_us server_timestamp = 0;
     timestamp_us client_input_timestamp = 0;
 
