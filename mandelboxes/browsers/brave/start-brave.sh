4130149e38568976829b6ca75600409c24228783
Roshan Padaki <roshan@whist.com>
2021-12-16 12:35:36 -0500

    fix browsers dockerfiles

--- mandelboxes/browsers/brave/start-brave.sh
+++ mandelboxes/browsers/brave/start-brave.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Enable Sentry bash error handler, this will catch errors if `set -e` is set in a Bash script
+# This is called via `./run-as-whist-user.sh`, which passes sentry environment in.
+case $(cat $SENTRY_ENVIRONMENT) in
+  dev|staging|prod)
+    export SENTRY_ENVIRONMENT=${SENTRY_ENV}
+    eval "$(sentry-cli bash-hook)"
+    ;;
+  *)
+    echo "Sentry environment not set, skipping Sentry error handler"
+    ;;
+esac
+
 # Exit on subcommand errors
 set -Eeuo pipefail
 
--- mandelboxes/browsers/chrome/start-chrome.sh
+++ mandelboxes/browsers/chrome/start-chrome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Enable Sentry bash error handler, this will catch errors if `set -e` is set in a Bash script
-SENTRY_ENV_FILENAME=/usr/share/whist/private/sentry_env
-case $(cat $SENTRY_ENV_FILENAME) in
+# This is called via `./run-as-whist-user.sh`, which passes sentry environment in.
+case $(cat $SENTRY_ENVIRONMENT) in
   dev|staging|prod)
     export SENTRY_ENVIRONMENT=${SENTRY_ENV}
     eval "$(sentry-cli bash-hook)"
