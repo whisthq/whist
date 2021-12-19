cbfdc07cc2205a6233577837e6ea056c5ce29841
Ming Ying <ming@whist.com>
2021-12-19 14:59:49 -0500

    script is smart about creating/deleting the teams folder

--- frontend/website/scripts/pull-assets.js
+++ frontend/website/scripts/pull-assets.js
@@ -11,20 +11,25 @@
  */
 
 const { Client } = require("@notionhq/client")
+const http = require("http")
+const https = require("https")
+const Stream = require("stream").Transform
+const fs = require("fs")
 
 // Don't worry, this secret key has read-only permissions to our Team Directory
 const NOTION_API_KEY = "secret_IWKwfhedzKrvbiWtCF7XcbxuX9emMwyfWNoAXfnQXNZ"
 // ID of our Team Directory Notion database
 const NOTION_DATABASE_ID = "f023b39bd511459d822fadbe2cf5b605"
+// Team photos directory, relative to root of the website subrepo
+const TEAM_PHOTOS_DIRECTORY = "public/assets/team"
 // Initialize Notion SDK
 const notion = new Client({ auth: NOTION_API_KEY })
-
-const downloadImageLocally = (imageUrl, name) => {
-  const http = require("http")
-  const https = require("https")
-  const Stream = require("stream").Transform
-  const fs = require("fs")
-
+// Function to save image locally
+const downloadImageLocally = (
+  imageUrl,
+  name,
+  directory = TEAM_PHOTOS_DIRECTORY
+) => {
   const url = new URL(imageUrl)
   const client = url.protocol === "https:" ? https : http
 
@@ -37,13 +42,14 @@ const downloadImageLocally = (imageUrl, name) => {
       })
 
       response.on("end", function () {
-        fs.writeFileSync(`./public/assets/team/${name}.jpg`, data.read())
-        console.log(`Created file ${name}.jpg`)
+        fs.writeFileSync(`./${directory}/${name}.jpg`, data.read())
+        console.log(`Saved image ${directory}/${name}.jpg`)
       })
     })
     .end()
 }
 
+// Query Notion API and pull all data
 ;(async () => {
   const database = await notion.databases.query({
     database_id: NOTION_DATABASE_ID,
@@ -54,6 +60,14 @@ const downloadImageLocally = (imageUrl, name) => {
       },
     ],
   })
+
+  // Before looping through the database entries, create a clean folder
+  // to store the team photos
+  const teamPhotosDirectoryExists = fs.existsSync(TEAM_PHOTOS_DIRECTORY)
+  if (teamPhotosDirectoryExists)
+    fs.rmSync(TEAM_PHOTOS_DIRECTORY, { recursive: true })
+  fs.mkdirSync(TEAM_PHOTOS_DIRECTORY)
+  console.log(`Directory ${TEAM_PHOTOS_DIRECTORY} created`)
 
   database?.results?.forEach(async (row) => {
     const pageId = row?.id
@@ -68,7 +82,7 @@ const downloadImageLocally = (imageUrl, name) => {
     // Team member title e.g. Roman Emperor
     const title = pageContents?.properties?.Title?.rich_text?.[0]?.plain_text
 
-    // Download image locally
+    // Now we have pulled all the data, it's time to save it locally
     downloadImageLocally(imageUrl, name.replace(/\s/g, ""))
   })
 })()
