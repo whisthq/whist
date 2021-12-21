177504447d9ea58867fdae0ca3ca1087404a1099
Mark Thompson <mark@whist.com>
2021-12-21 18:16:48 +0000

    Improve atomic support and test coverage
    Fix seq-cst-ness of MSVC stores (was previously only right for relaxed),
    along with a silly copy-paste error in xor.
    
    Expand the test coverage and add a lot more explanation of what is going
    on there.

--- protocol/whist/utils/atomic.h
+++ protocol/whist/utils/atomic.h
@@ -22,14 +22,20 @@ typedef struct {
 
 static inline void atomic_init(volatile atomic_int *object, int value) { object->value = value; }
 
-static inline void atomic_store(volatile atomic_int *object, int desired) {
+static inline int atomic_load(volatile atomic_int *object) {
     // Windows guarantees that simple loads and stores to aligned word-size
     // variables are atomic (that is: the compiler doesn't do anything funny
     // and Windows doesn't run on platforms where that wouldn't be sufficient).
-    object->value = desired;
+    // For loads, this is fine.
+    return object->value;
 }
 
-static inline int atomic_load(volatile atomic_int *object) { return object->value; }
+static inline void atomic_store(volatile atomic_int *object, int desired) {
+    // For stores, it's not enough for sequentially-consistent atomics
+    // because loads can be reordered across it.  Use an exchange to
+    // ensure we have the expected behaviour.
+    InterlockedExchange(&object->value, desired);
+}
 
 static inline int atomic_exchange(volatile atomic_int *object, int desired) {
     return InterlockedExchange(&object->value, desired);
@@ -70,7 +76,7 @@ static inline int atomic_fetch_or(volatile atomic_int *object, int operand) {
 }
 
 static inline int atomic_fetch_xor(volatile atomic_int *object, int operand) {
-    return InterlockedXor(&object->value, -operand);
+    return InterlockedXor(&object->value, operand);
 }
 
 static inline int atomic_fetch_and(volatile atomic_int *object, int operand) {
@@ -80,7 +86,9 @@ static inline int atomic_fetch_and(volatile atomic_int *object, int operand) {
 #elif defined(__cplusplus) && __cplusplus >= 201103L
 
 // We are compiling C++, so we can use C++11 atomics with the same
-// semantics as C11.
+// semantics as C11.  Note that this section is after the MSVC atomics in
+// this header so that the MSVC atomics do get tested when compiled with
+// C++ as the protocol tests are.
 #include <atomic>
 
 // Allow using atomic_int without a namespace, as in C.
