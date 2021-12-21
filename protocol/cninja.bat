0e375f012ec5cdf20f8ced77a45d2c7a1dea9d58
Mark Thompson <mark@whist.com>
2021-12-21 15:10:42 +0000

    Don't unexpectedly change the current directory when building on Windows (#4938)
    Use pushd/popd to restore it at the end.

--- protocol/cninja.bat
+++ protocol/cninja.bat
@@ -2,10 +2,12 @@
 
 REM Script to build the Whist protocol on Windows
 
-REM cd to parent directory of this script
-cd %~dp0
+REM Move to parent directory of this script
+pushd %~dp0
 
 mkdir build
 cmake -S . -B build/ -G Ninja -D CMAKE_BUILD_TYPE=Debug
 cd build
 ninja
+
+popd
