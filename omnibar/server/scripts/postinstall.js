dbf0dc7a7fbb1219324f99e66b9bcad29de2e0f7
Ming Ying <ming@whist.com>
2021-12-28 13:08:37 -0500

    dont build electron builder in server

--- omnibar/server/scripts/postinstall.js
+++ omnibar/server/scripts/postinstall.js
@@ -1,20 +1,16 @@
 // Build core-ts and electron-notarize after the normal install procedures.
-const execCommand = require("./execCommand").execCommand;
-const path = require("path");
+const execCommand = require("./execCommand").execCommand
+const path = require("path")
 
 const postInstall = (_env, ..._args) => {
-  console.log("Building `@whist/shared`...");
-  execCommand("yarn install", path.join("node_modules", "@whist/shared"));
-  execCommand("yarn build", path.join("node_modules", "@whist/shared"));
-  console.log("Success building `@whist/shared`!");
+  console.log("Building `@whist/shared`...")
+  execCommand("yarn install", path.join("node_modules", "@whist/shared"))
+  execCommand("yarn build", path.join("node_modules", "@whist/shared"))
+  console.log("Success building `@whist/shared`!")
+}
 
-  // Recompile node native modules
-  console.log("Recompiling node modules for your computer...");
-  execCommand("electron-builder install-app-deps");
-};
-
-module.exports = postInstall;
+module.exports = postInstall
 
 if (require.main === module) {
-  postInstall();
+  postInstall()
 }
