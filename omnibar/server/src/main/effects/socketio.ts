a26ae15036ac44b00537846fa78999bf411e6c3c
Ming Ying <ming@whist.com>
2021-12-28 20:20:17 -0500

    test: go to yahoo if server receives connection from client

--- omnibar/server/src/main/effects/socketio.ts
+++ omnibar/server/src/main/effects/socketio.ts
@@ -1,10 +1,15 @@
-import { app } from "electron"
+import { app, BrowserWindow } from "electron"
 import express from "express"
 import http from "http"
 import { Server as Socketio } from "socket.io"
 
 import { MANDELBOX_PORT, LOCAL_PORT } from "@app/constants/networking"
 import { clientToServerDiscovery } from "@whist/shared"
+import {
+  createBrowserView,
+  attachBrowserView,
+  setTopBrowserView,
+} from "@app/utils/view"
 
 const expressServer = express()
 const server = http.createServer(expressServer)
@@ -13,7 +18,11 @@ const io = new Socketio(server)
 // Whenever any socketio-client connects, this function will fire
 io.on("connection", (socket) => {
   socket.on(clientToServerDiscovery(1234).name, () => {
-    console.log("Received client!")
+    console.log("Discovery request received")
+    const view = createBrowserView("https://yahoo.com")
+    const window = BrowserWindow.getAllWindows()?.[0]
+    attachBrowserView(view, window)
+    setTopBrowserView(view, window)
   })
 })
 
