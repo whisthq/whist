42b74cbb57a7f05734bd2f98f38ee0f8f5b330d9
Kevin Meng <kevin.meng.01@gmail.com>
2022-01-03 03:41:39 -0600

    format notification code, add docstrings

--- protocol/client/notifications.c
+++ protocol/client/notifications.c
@@ -1,4 +1,27 @@
+/**
+ * Copyright 2021 Whist Technologies, Inc., dba Whist
+ * @file notifications.c
+ * @brief Contains utilities to receive notifications from the server and display them.
+============================
+Usage
+============================
+
+TODO(kevin) write some summary thing here
+
+*/
+
 #include "notifications.h"
+
+#include <stdio.h>
+
+#include <whist/logging/logging.h>
+#include <whist/network/network.h>
+
+/*
+============================
+Public Function Implementations
+============================
+*/
 
 int display_notification(WhistPacket *packet) {
     LOG_INFO("GOT PACKET: %s\n", packet->data);
