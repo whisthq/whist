5056ee35f1b254cf92c455df4bce47cc5bf829a1
Nicholas Pipitone <npip99@gmail.com>
2022-01-04 17:23:05 -0500

    Add Mac Support for get_keyboard_layout

--- protocol/whist/utils/os_utils.c
+++ protocol/whist/utils/os_utils.c
@@ -6,26 +6,83 @@ Usage
 ============================
 */
 
-#include "os_utils.h"
-
 /*
 ============================
 Includes
 ============================
 */
 
+#include "os_utils.h"
 #include <whist/core/whist.h>
-#ifdef _WIN32
-#elif __APPLE__
-#else
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
 #endif
+
+/*
+============================
+Defines
+============================
+*/
 
 #define WHIST_KB_DEFAULT_LAYOUT "us"
 
+#ifdef __APPLE__
+// If this array gets any longer than 1000 or so,
+// we should probably swap to a C++ std::map<string, string>
+const char* apple_keyboard_mappings[][2] = {
+    {"com.apple.keylayout.USExtended", "us"},  {"com.apple.keylayout.US", "us"},
+    {"com.apple.keylayout.Italian-Pro", "it"}, {"com.apple.keylayout.Italian", "it"},
+    {"com.apple.keylayout.Arabic", "ara"},     {"com.apple.keylayout.ABC-QWERTZ", "de"},
+    {"com.apple.keylayout.German", "de"},      {"com.apple.keylayout.Canadian-CSA", "ca"},
+    {"com.apple.keylayout.ABC-AZERTY", "fr"},  {"com.apple.keylayout.French", "fr"},
+    {"com.apple.keylayout.SwissFrench", "fr"}, {"com.apple.keylayout.LatinAmerican", "latam"},
+    {"com.apple.keylayout.Spanish", "es"},     {"com.apple.keylayout.Hebrew", "il"},
+};
+
+#define NUM_APPLE_KEYBOARD_MAPPINGS \
+    ((int)sizeof(apple_keyboard_mappings) / (int)sizeof(apple_keyboard_mappings[0]))
+#endif
+
 WhistKeyboardLayout get_keyboard_layout() {
-    WhistKeyboardLayout ret = {0};
-    safe_strncpy(ret.layout_name, WHIST_KB_DEFAULT_LAYOUT, sizeof(ret.layout_name));
-    return ret;
+    WhistKeyboardLayout whist_layout = {0};
+    // Set the default layout, in-case the code below fails to set the keyboard layout
+    safe_strncpy(whist_layout.layout_name, WHIST_KB_DEFAULT_LAYOUT,
+                 sizeof(whist_layout.layout_name));
+
+#ifdef __APPLE__
+    TISInputSourceRef source = TISCopyCurrentKeyboardInputSource();
+    // get input source id - kTISPropertyInputSourceID
+    // get layout name - kTISPropertyLocalizedName
+    CFStringRef layout_id = TISGetInputSourceProperty(source, kTISPropertyInputSourceID);
+    char layout[128] = {0};
+    Boolean res = CFStringGetCString(layout_id, layout, sizeof(layout), kCFStringEncodingUTF8);
+    if (!res) {
+        LOG_ERROR("CFStringGetCString failed!");
+        return whist_layout;
+    }
+
+    bool found = false;
+    for (int i = 0; i < NUM_APPLE_KEYBOARD_MAPPINGS; i++) {
+        if (strncmp(apple_keyboard_mappings[i][0], layout, sizeof(layout)) == 0) {
+            // Copy new name
+            safe_strncpy(whist_layout.layout_name, apple_keyboard_mappings[i][1],
+                         sizeof(whist_layout.layout_name));
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        // Log the unrecognized keyboard layout,
+        // so we can add support for it if we see usage of it
+        LOG_ERROR("Mac Keyboard Layout was not recognized! %s", layout);
+    }
+#else
+    // Unimplemented on Windows/Linux, so always assuming US layout for now
+#endif
+
+    // Return the whist layout
+    return whist_layout;
 }
 
 void set_keyboard_layout(WhistKeyboardLayout requested_layout) {
