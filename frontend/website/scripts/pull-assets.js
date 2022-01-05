b71fa3066d5679d89c554e2cac1d4317b8ea3f5c
Ming Ying <ming@whist.com>
2022-01-05 18:26:33 -0500

    uploadgin to s3 works! just need to clean up now

--- frontend/website/scripts/pull-assets.js
+++ frontend/website/scripts/pull-assets.js
@@ -12,6 +12,8 @@
 
 const { Client } = require("@notionhq/client")
 const fs = require("fs")
+const AWS = require("aws-sdk")
+const request = require("request-promise")
 
 // Don't worry, this secret key has read-only permissions to our Team Directory
 const NOTION_API_KEY = "secret_IWKwfhedzKrvbiWtCF7XcbxuX9emMwyfWNoAXfnQXNZ"
@@ -23,6 +25,14 @@ const NOTION_INVESTOR_DATABASE_ID = "76ab133fe155432e9b1c7868b5d9d08a"
 const NOTION_DATA_DIRECTORY = "public/assets/metadata"
 // Initialize Notion SDK
 const notion = new Client({ auth: NOTION_API_KEY })
+// Initialize AWS SDk
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "AKIA24A776SSDGVALR5K",
+  secretAccessKey:
+    process.env.AWS_SECRET_KEY ?? "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",
+})
+const AWS_BUCKET_NAME = "whist-website-assets"
+
 // Reads and returns info from Notion Team Directory
 const pullTeamInfo = async () => {
   const savedTeamInfo = []
@@ -106,14 +116,77 @@ const pullInvestorInfo = async () => {
   return savedInvestorInfo
 }
 
+const emptyS3Directory = async (bucket, dir) => {
+  const listParams = {
+    Bucket: bucket,
+    Prefix: dir,
+  }
+
+  const listedObjects = await s3.listObjectsV2(listParams).promise()
+
+  if (listedObjects.Contents.length === 0) return
+
+  const deleteParams = {
+    Bucket: bucket,
+    Delete: { Objects: [] },
+  }
+
+  listedObjects.Contents.forEach(({ Key }) => {
+    deleteParams.Delete.Objects.push({ Key })
+  })
+
+  await s3.deleteObjects(deleteParams).promise()
+}
+
+const uploadUrlToS3 = async (url, filepath) => {
+  try {
+    const options = {
+      uri: url,
+      encoding: null,
+    }
+
+    const body = await request(options)
+
+    const { Location } = await s3
+      .upload({
+        Bucket: AWS_BUCKET_NAME,
+        Key: filepath,
+        Body: body,
+      })
+      .promise()
+
+    return Location
+  } catch (err) {
+    console.error(`Upload failed for ${filepath}:`, err)
+    return undefined
+  }
+}
+
 // The main function that actually runs
 // Queries Notion API and pull all data
 ;(async () => {
   console.log("Pulling assets from Notion database...")
 
-  const savedInvestorInfo = await pullInvestorInfo()
-  const savedTeamInfo = await pullTeamInfo()
+  const environment = process.env.WHIST_ENVIRONMENT ?? "local"
+
+  let savedInvestorInfo = await pullInvestorInfo()
+  let savedTeamInfo = await pullTeamInfo()
   const jsonDirectory = `./${NOTION_DATA_DIRECTORY}/notion.json`
+
+  await emptyS3Directory(AWS_BUCKET_NAME, "/")
+
+  for (let i = 0; i < savedTeamInfo.length; i++) {
+    const info = savedTeamInfo[i]
+    if ((info.imageUrl ?? "") !== "") {
+      const location = await uploadUrlToS3(
+        info.imageUrl,
+        `${environment}/team/${info.name}.png`
+      )
+      savedTeamInfo[i].imageUrl = location
+    }
+  }
+
+  console.log(savedTeamInfo)
 
   fs.writeFileSync(
     jsonDirectory,
