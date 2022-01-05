e3e55908ab324e9beacbf5b0cb56b87dbabe96dd
Roshan Padaki <roshan@whist.com>
2022-01-05 23:06:18 +0000

    ⛴ port over chrome extension code

---
+++ mandelboxes/browsers/chrome/whist-teleport/webpack/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path")
+const CopyPlugin = require("copy-webpack-plugin")
+const { ProvidePlugin } = require("webpack")
+
+const srcDir = path.join(__dirname, "..", "src")
+const outDir = path.join(__dirname, "..", "build", "js")
+
+module.exports = {
+    entry: {
+        downloadHandler: path.join(srcDir, "downloadHandler.ts"),
+    },
+    output: {
+        path: outDir,
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        fallback: {
+            path: require.resolve("path-browserify"),
+        },
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: "../", context: "public" }],
+        }),
+        new ProvidePlugin({
+            process: "process/browser",
+        }),
+    ],
+}
