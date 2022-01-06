42aa4ba54fa3af087169e1e1b49cc7d4f0239d24
Mauricio <mauricio@whist.com>
2022-01-06 11:46:11 -0600

    Update mutations

--- backend/services/subscriptions/mutations.go
+++ backend/services/subscriptions/mutations.go
@@ -6,26 +6,26 @@ import "github.com/hasura/go-graphql-client"
 var InsertInstances struct {
 	MutationResponse struct {
 		AffectedRows graphql.Int `graphql:"affected_rows"`
-	} `graphql:"insert_whist_hosts(objects: $objects)"`
+	} `graphql:"insert_whist_instances(objects: $objects)"`
 }
 
 // InsertOneInstance inserts one instance to the database.
 var InsertOneInstance struct {
 	MutationResponse struct {
 		AffectedRows graphql.Int `graphql:"affected_rows"`
-	} `graphql:"insert_whist_hosts_one(objects: $objects)"`
+	} `graphql:"insert_whist_instances_one(objects: $objects)"`
 }
 
 // UpdateInstanceStatus updates the status of an instance to the given status.
 var UpdateInstanceStatus struct {
 	MutationResponse struct {
 		AffectedRows graphql.Int `graphql:"affected_rows"`
-	} `graphql:"update_whist_hosts(where: {status: {_eq: $status}})"`
+	} `graphql:"update_whist_instances(where: {status: {_eq: $status}})"`
 }
 
 // DeleteInstanceStatusById deletes an instance that matches the given instance_name.
 var DeleteInstanceById struct {
 	MutationResponse struct {
 		AffectedRows graphql.Int `graphql:"affected_rows"`
-	} `graphql:"delete_whist_hosts(where: {id: {_eq: $id}})"`
+	} `graphql:"delete_whist_instances(where: {id: {_eq: $id}})"`
 }
