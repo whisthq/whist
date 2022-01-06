34c08ff0ec4c1b2a270d44de785ee0039989ddae
Ming Ying <ming@whist.com>
2022-01-05 19:11:44 -0500

    cleaned notion/aws website pulling code, not yet tested

---
+++ frontend/website/scripts/notion-helpers.js
@@ -0,0 +1,76 @@
+const { Client } = require("@notionhq/client")
+const fs = require("fs")
+
+const NOTION_API_KEY = "secret_IWKwfhedzKrvbiWtCF7XcbxuX9emMwyfWNoAXfnQXNZ" // This is read-only, don't worry
+const NOTION_TEAM_DATABASE_ID = "f023b39bd511459d822fadbe2cf5b605" // ID of our Team Directory Notion database
+const NOTION_INVESTOR_DATABASE_ID = "76ab133fe155432e9b1c7868b5d9d08a" // ID of our Investor Directory Notion database
+const NOTION_DATA_DIRECTORY = "public/assets/metadata" // Where to save .json file containing database info e.g. team member names
+
+// Initialize Notion SDK
+const notion = new Client({ auth: NOTION_API_KEY })
+
+module.exports = {
+  fetchNotionTeamData: () => {
+    const teamDatabase = await notion.databases.query({
+      database_id: NOTION_TEAM_DATABASE_ID,
+      sorts: [
+        {
+          property: "Name",
+          direction: "ascending",
+        },
+      ],
+    })
+
+    let savedTeamInfo = []
+
+    // Iterate through database entries
+    for (const row of teamDatabase?.results) {
+      const pageContents = await notion.pages.retrieve({
+        page_id: row?.id,
+      })
+      const imageUrl =
+        pageContents?.properties?.Photo?.files?.[0]?.file?.url ?? "" // e.g. http://link-to-image.png
+      const name = pageContents?.properties?.Name?.title?.[0]?.plain_text ?? "" // e.g. Marcus Aurelius
+      const title =
+        pageContents?.properties?.Title?.rich_text?.[0]?.plain_text ?? "" // e.g. Roman emperor
+
+      savedTeamInfo.push({ name, title, imageUrl })
+    }
+
+    return savedTeamInfo
+  },
+  fetchNotionInvestorData: () => {
+    const investorDatabase = await notion.databases.query({
+      database_id: NOTION_INVESTOR_DATABASE_ID,
+      sorts: [
+        {
+          property: "Name",
+          direction: "ascending",
+        },
+      ],
+    })
+
+    let savedInvestorInfo = []
+
+    // Iterate through database entries
+    for (const row of investorDatabase?.results) {
+      const pageId = row?.id
+      const pageContents = await notion.pages.retrieve({
+        page_id: pageId,
+      })
+      const imageUrl =
+        pageContents?.properties?.Photo?.files?.[0]?.file?.url ?? "" // e.g. http://link-to-image.png
+      const name = pageContents?.properties?.Name?.title?.[0]?.plain_text ?? "" // e.g. Sam Lessin
+      const website = pageContents?.properties?.Website?.url ?? "" // e.g. e.g. http://slow.co
+      const description =
+        pageContents?.properties?.Description?.rich_text?.[0]?.plain_text ?? "" // e.g. "Great Investor"
+
+      savedInvestorInfo.push({ name, imageUrl, website, description })
+    }
+  },
+  saveDataLocally: () => {
+    const jsonDirectory = `./${NOTION_DATA_DIRECTORY}/notion.json`
+    fs.writeFileSync(jsonDirectory, JSON.stringify(obj))
+    return savedInvestorInfo
+  },
+}
--- frontend/website/scripts/pull-assets.js
+++ frontend/website/scripts/pull-assets.js
@@ -9,191 +9,43 @@
  * from your command line.
  *
  */
+const notion = require("./notion-helpers")
+const aws = require("./aws-helpers")
 
-const { Client } = require("@notionhq/client")
-const fs = require("fs")
-const AWS = require("aws-sdk")
-const request = require("request-promise")
-
-// Don't worry, this secret key has read-only permissions to our Team Directory
-const NOTION_API_KEY = "secret_IWKwfhedzKrvbiWtCF7XcbxuX9emMwyfWNoAXfnQXNZ"
-// ID of our Team Directory Notion database
-const NOTION_TEAM_DATABASE_ID = "f023b39bd511459d822fadbe2cf5b605"
-// ID of our Investor Directory Notion database
-const NOTION_INVESTOR_DATABASE_ID = "76ab133fe155432e9b1c7868b5d9d08a"
-// Where to save .json file containing database info e.g. team member names
-const NOTION_DATA_DIRECTORY = "public/assets/metadata"
-// Initialize Notion SDK
-const notion = new Client({ auth: NOTION_API_KEY })
-// Initialize AWS SDk
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "AKIA24A776SSDGVALR5K",
-  secretAccessKey:
-    process.env.AWS_SECRET_KEY ?? "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",
-})
-const AWS_BUCKET_NAME = "whist-website-assets"
-
-// Reads and returns info from Notion Team Directory
-const pullTeamInfo = async () => {
-  const savedTeamInfo = []
-
-  const teamDatabase = await notion.databases.query({
-    database_id: NOTION_TEAM_DATABASE_ID,
-    sorts: [
-      {
-        property: "Name",
-        direction: "ascending",
-      },
-    ],
-  })
-
-  // Iterate through database entries
-  for (const row of teamDatabase?.results) {
-    const pageId = row?.id
-    const pageContents = await notion.pages.retrieve({
-      page_id: pageId,
-    })
-    // Team member AWS S3 hosted headshot URL
-    const imageUrl =
-      pageContents?.properties?.Photo?.files?.[0]?.file?.url ?? ""
-    // Team member name e.g. Marcus Aurelius
-    const name = pageContents?.properties?.Name?.title?.[0]?.plain_text ?? ""
-    // Team member title e.g. Roman Emperor
-    const title =
-      pageContents?.properties?.Title?.rich_text?.[0]?.plain_text ?? ""
-
-    // Finally, save the info
-    savedTeamInfo.push({
-      name,
-      title,
-      imageUrl,
-    })
-  }
-
-  return savedTeamInfo
-}
-
-// Reads and returns info from Notion Investor Directory
-const pullInvestorInfo = async () => {
-  const savedInvestorInfo = []
-
-  const investorDatabase = await notion.databases.query({
-    database_id: NOTION_INVESTOR_DATABASE_ID,
-    sorts: [
-      {
-        property: "Name",
-        direction: "ascending",
-      },
-    ],
-  })
-
-  // Iterate through database entries
-  for (const row of investorDatabase?.results) {
-    const pageId = row?.id
-    const pageContents = await notion.pages.retrieve({
-      page_id: pageId,
-    })
-    // Team member AWS S3 hosted headshot URL
-    const imageUrl =
-      pageContents?.properties?.Photo?.files?.[0]?.file?.url ?? ""
-    // Team member name e.g. Marcus Aurelius
-    const name = pageContents?.properties?.Name?.title?.[0]?.plain_text ?? ""
-    // Investor website e.g. https://stoicsrus.com
-    const website = pageContents?.properties?.Website?.url ?? ""
-    // Team member title e.g. Roman Emperor
-    const description =
-      pageContents?.properties?.Description?.rich_text?.[0]?.plain_text ?? ""
-
-    // Finally, save the info
-    savedInvestorInfo.push({
-      name,
-      imageUrl,
-      website,
-      description,
-    })
-  }
-
-  return savedInvestorInfo
-}
-
-const emptyS3Directory = async (bucket, dir) => {
-  const listParams = {
-    Bucket: bucket,
-    Prefix: dir,
-  }
-
-  const listedObjects = await s3.listObjectsV2(listParams).promise()
-
-  if (listedObjects.Contents.length === 0) return
-
-  const deleteParams = {
-    Bucket: bucket,
-    Delete: { Objects: [] },
-  }
-
-  listedObjects.Contents.forEach(({ Key }) => {
-    deleteParams.Delete.Objects.push({ Key })
-  })
-
-  await s3.deleteObjects(deleteParams).promise()
-}
-
-const uploadUrlToS3 = async (url, filepath) => {
-  try {
-    const options = {
-      uri: url,
-      encoding: null,
-    }
-
-    const body = await request(options)
-
-    const { Location } = await s3
-      .upload({
-        Bucket: AWS_BUCKET_NAME,
-        Key: filepath,
-        Body: body,
-      })
-      .promise()
-
-    return Location
-  } catch (err) {
-    console.error(`Upload failed for ${filepath}:`, err)
-    return undefined
-  }
-}
-
-// The main function that actually runs
-// Queries Notion API and pull all data
+// Queries Notion API, saves images to S3, and caches results
 ;(async () => {
   console.log("Pulling assets from Notion database...")
 
   const environment = process.env.WHIST_ENVIRONMENT ?? "local"
 
-  let savedInvestorInfo = await pullInvestorInfo()
-  let savedTeamInfo = await pullTeamInfo()
-  const jsonDirectory = `./${NOTION_DATA_DIRECTORY}/notion.json`
+  let savedInvestorInfo = await notion.fetchNotionInvestorData()
+  let savedTeamInfo = await notion.fetchNotionTeamData()
 
-  await emptyS3Directory(AWS_BUCKET_NAME, "/")
+  if (environment !== "local") {
+    const teamS3Directory = `${environment}/team`
+    const investorS3Directory = `${environment}/investors`
 
-  for (let i = 0; i < savedTeamInfo.length; i++) {
-    const info = savedTeamInfo[i]
-    if ((info.imageUrl ?? "") !== "") {
-      const location = await uploadUrlToS3(
+    await aws.emptyS3Directory(AWS_BUCKET_NAME, s3Directory)
+
+    savedTeamInfo.forEach((info, i) => {
+      savedTeamInfo[i].imageUrl = await aws.uploadUrlToS3(
         info.imageUrl,
-        `${environment}/team/${info.name}.png`
+        `${teamS3Directory}/${info.name}.png`
       )
-      savedTeamInfo[i].imageUrl = location
-    }
+    })
+
+    savedInvestorInfo.forEach((info, i) => {
+      savedInvestorInfo[i].imageUrl = await aws.uploadUrlToS3(
+        info.imageUrl,
+        `${investorS3Directory}/${info.name}.png`
+      )
+    })
   }
 
-  console.log(savedTeamInfo)
+  notion.saveDataLocally({
+    team: savedTeamInfo,
+    investors: savedInvestorInfo,
+  })
 
-  fs.writeFileSync(
-    jsonDirectory,
-    JSON.stringify({
-      team: savedTeamInfo,
-      investors: savedInvestorInfo,
-    })
-  )
-  console.log(`Retrieved Notion data and saved at ${jsonDirectory}`)
+  console.log("Assets pulled and cached successfully")
 })()
