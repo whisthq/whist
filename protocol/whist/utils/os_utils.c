1d131adf5690e3d3cc506469a2e2a75fec09e423
Kyle Schaefer <schkyles@gmail.com>
2022-01-07 10:18:56 -0800

    Adding additional keyboard layout mappings. (#5092)

--- protocol/whist/utils/os_utils.c
+++ protocol/whist/utils/os_utils.c
@@ -40,7 +40,8 @@ const char* apple_keyboard_mappings[][2] = {
     {"com.apple.keylayout.ABC-AZERTY", "fr"},  {"com.apple.keylayout.French", "fr"},
     {"com.apple.keylayout.SwissFrench", "fr"}, {"com.apple.keylayout.LatinAmerican", "latam"},
     {"com.apple.keylayout.Spanish", "es"},     {"com.apple.keylayout.Hebrew", "il"},
-    {"com.apple.keylayout.Canadian", "ca"},
+    {"com.apple.keylayout.Canadian", "ca"},    {"com.apple.keylayout.DVORAK-QWERTYCMD", "dvorak"},
+    {"com.apple.keylayout.ABC-India", "in"},
 };
 
 #define NUM_APPLE_KEYBOARD_MAPPINGS \
