7616a8b6e32dc68ffd0552a7d853372951b5f454
Philippe Noel <phil@whist.com>
2022-01-09 18:32:54 -0500

    comment out shellcheck, undo some quotes in Docker

--- protocol/build_protocol_targets.sh
+++ protocol/build_protocol_targets.sh
@@ -82,9 +82,9 @@ fi
 # We also mount entire ./whist directory so that git works for git revision
 docker run \
   --rm \
-  "$DOCKER_IT_FLAG" \
+  $DOCKER_IT_FLAG \
   --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --env AWS_DEFAULT_REGION --env AWS_DEFAULT_OUTPUT --env GITHUB_SHA --env CODECOV_TOKEN \
-  --mount type=bind,source="$(cd ..; pwd)",destination=/workdir \
+  --mount type=bind,source=$(cd ..; pwd),destination=/workdir \
   "$MOUNT_AWS" \
   --name whist-protocol-builder-$(date +"%s") \
   --user "$DOCKER_USER" \
