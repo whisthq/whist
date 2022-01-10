03a953e693886d1542562bbf370c84ec3b41975c
Roshan Padaki <roshan@whist.com>
2022-01-10 16:57:31 -0500

    Fix Website Netlify Deploys (#5130)
    * modernize some website code
    
    * fixed?
    
    * switcheroo
    
    * move
    
    * cleanup
    
    * build context

--- frontend/website/scripts/build-netlify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-yarn build && rm -rf /opt/build/cache
-
-# We need to let the React app handle all routing login.
-# Without this, directly navigating to whist.com/about fails.
-echo '/* /index.html 200' > build/_redirects
--- frontend/website/scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-yarn tailwind && yarn pull:assets && snowpack dev
