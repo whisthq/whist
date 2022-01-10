185046cfac49efe7b16088f64e31076d728db529
Kevin Meng <kevin@whist.com>
2022-01-10 22:13:26 +0000

    fix last-line \n and refactor notifications in client

---
+++ protocol/client/notifications/display_notifs.h
@@ -0,0 +1,24 @@
+#ifndef DISPLAY_NOTIFS_H
+#define DISPLAY_NOTIFS_H
+
+/**
+ * Copyright 2021 Whist Technologies, Inc.
+ * @file display_notifs.h
+ * @brief stuff
+============================
+Usage
+============================
+
+TODO(kevin) write
+
+*/
+
+/*
+============================
+Public Functions
+============================
+*/
+
+int native_show_notification(char *title, char *msg);
+
+#endif  // DISPLAY_NOTIFS_H
