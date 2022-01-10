c6a922e7bb071a3cd8ac860d02c1717c0281cef1
Kevin Meng <kevin@whist.com>
2022-01-10 22:17:49 +0000

    further reorganization of client-side notification files

--- protocol/client/notifications/display_notifs_windows.c
+++
@@ -1,8 +0,0 @@
-#include "notifications.h"
-
-#include <whist/logging/logging.h>
-
-int native_show_notification(WhistPacket *packet) {
-    LOG_WARNING("Notification display not implemented on windows");
-    return -1;
-}
