cc45f681e8156f4aa0244c17d6f48c48b91a6b00
Philippe Noel <phil@whist.com>
2022-01-11 16:17:38 -0500

    clang-tidy

--- protocol/whist/utils/x11_window_info.c
+++ protocol/whist/utils/x11_window_info.c
@@ -86,8 +86,8 @@ bool get_focused_window_name(char** name_return) {
         }
     } else {
         LOG_ERROR(
-            "XGetWMName returned status %d and failed to get the name of the focused window %lu",
-            s, w);
+            "XGetWMName returned status %d and failed to get the name of the focused window %lu", s,
+            w);
     }
     return false;
 }
