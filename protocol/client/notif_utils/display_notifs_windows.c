d1f522b42ef14c011757f4fd8ce2cc9753560085
Kevin Meng <kevin@whist.com>
2022-01-11 23:20:38 +0000

    docs for display notifications

--- protocol/client/notif_utils/display_notifs_windows.c
+++ protocol/client/notif_utils/display_notifs_windows.c
@@ -1,6 +1,27 @@
-#include "notifications.h"
+/**
+ * Copyright 2021 Whist Technologies, Inc.
+ * @file display_notifs_windows.c
+ * @brief Handles client-side notification display on Windows
+
+Interfaces with native libraries to display notifications thrown from the server application.
+
+*/
+
+/*
+============================
+Includes
+============================
+*/
+
+#include "display_notifs.h"
 
 #include <whist/logging/logging.h>
+
+/*
+============================
+Public Function Implementations
+============================
+*/
 
 int native_show_notification(WhistPacket *packet) {
     LOG_WARNING("Notification display not implemented on windows");
