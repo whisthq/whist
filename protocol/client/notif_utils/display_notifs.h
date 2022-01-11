4859d09ec291e12a194443c63482d0dd1d629c5c
Kevin Meng <kevin@whist.com>
2022-01-11 23:20:38 +0000

    docs for display notifications

--- protocol/client/notif_utils/display_notifs.h
+++ protocol/client/notif_utils/display_notifs.h
@@ -4,12 +4,14 @@
 /**
  * Copyright 2021 Whist Technologies, Inc.
  * @file display_notifs.h
- * @brief stuff
+ * @brief Handles client-side notification display. Integrated with different
+ * .c files to display on different platforms
 ============================
 Usage
 ============================
 
-TODO(kevin) write
+Interfaces with native libraries on various operating systems (OSX/Windows/Linux)
+to display notifications thrown from the server application.
 
 */
 
@@ -19,6 +21,17 @@ Public Functions
 ============================
 */
 
+/**
+ * @brief       Displays notification on the client side.
+ *              Specific implementation depends on operating system (OSX/Windows/Linux).
+ *
+ * @param title Notification title
+ * @param msg   Notification body
+ *
+ * @return int  0 if success, -1 if failure
+ *
+ * @note This function is ONLY implemented for MacOS. Linux and Windows impls are stubs.
+ */
 int native_show_notification(char *title, char *msg);
 
 #endif  // DISPLAY_NOTIFS_H
--- protocol/client/notif_utils/display_notifs_mac.m
+++ protocol/client/notif_utils/display_notifs_mac.m
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2021 Whist Technologies, Inc.
+ * @file display_notifs_mac.m
+ * @brief Handles client-side notification display on MacOS
+
+Interfaces with native libraries to display notifications thrown from the server application.
+
+*/
+
+/*
+============================
+Includes
+============================
+*/
+
 #import "display_notifs.h"
 
 #import <stdio.h>
@@ -6,6 +21,12 @@
 #import <objc/runtime.h>
 
 #import <whist/logging/logging.h>
+
+/*
+============================
+Public Function Implementations
+============================
+*/
 
 int native_show_notification(char *title, char *msg) {
     NSUserNotification *n = [[NSUserNotification alloc] init];
