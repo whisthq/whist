2c6c502420e3fb64cf8b4a6f581af93451d21a21
djsavvy <sraghuvanshi@college.harvard.edu>
2022-01-12 02:35:52 +0000

    Add GetMostRecentKey function

--- backend/services/host-service/mandelbox/configutils/storage.go
+++ backend/services/host-service/mandelbox/configutils/storage.go
@@ -3,11 +3,13 @@ package configutils // import "github.com/whisthq/whist/backend/services/host-se
 import (
 	"context"
 	"io"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/whisthq/whist/backend/services/utils"
 )
 
@@ -48,4 +50,36 @@ func UploadFileToBucket(client *s3.Client, bucket, key string, file io.Reader) (
 		Key:    aws.String(key),
 		Body:   file,
 	})
+}
+
+// GetMostRecentKey scans the provided S3 bucket for keys beginning with the
+// provided prefix and ending with the provided suffix. It returns the key
+// whose value was most recently modified.
+func GetMostRecentMatchingKey(client *s3.Client, bucket, prefix, suffix string) (*s3types.Object, error) {
+	var curMatch *s3types.Object = nil
+
+	// We deal with the case where there are more than 1000 subkeys for the
+	// user's app config with pagination. God forbid this ever happen, but we
+	// gotta write resilient software.
+	for paginator := s3.NewListObjectsV2Paginator(client, &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(prefix),
+	}); paginator.HasMorePages(); {
+		output, err := paginator.NextPage(context.Background())
+		if err != nil {
+			return curMatch, err
+		}
+
+		for i, v := range output.Contents {
+			if (curMatch == nil || curMatch.LastModified.Before(*v.LastModified)) && strings.HasSuffix(*v.Key, suffix) {
+				// DO NOT use `&v` instead of `&output.Contents[i]`, since Go reuses
+				// memory addresses for loop variables (see the link below). I learned
+				// this the hard way.
+				// https://www.evanjones.ca/go-gotcha-loop-variables.html
+				curMatch = &output.Contents[i]
+			}
+		}
+	}
+
+	return curMatch, nil
 }
