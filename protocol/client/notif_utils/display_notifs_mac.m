db25c4126b80cbc4c49a942ecf4b98e3475d2de3
Kevin Meng <kevin@whist.com>
2022-01-12 22:47:33 +0000

    fix copyright

--- protocol/client/notif_utils/display_notifs_mac.m
+++ protocol/client/notif_utils/display_notifs_mac.m
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Whist Technologies, Inc.
+ * Copyright 2022 Whist Technologies, Inc.
  * @file display_notifs_mac.m
  * @brief Handles client-side notification display on MacOS
 
