72737bfb5ae4129e13263cdb08a013e510ed2070
Kevin Meng <kevin@whist.com>
2022-01-12 18:45:38 +0000

    major refactoring of server-side notifications.c/h: use thread_init and thread_destroy, remove need for globals, etc.

--- protocol/server/notifications.h
+++ protocol/server/notifications.h
@@ -19,7 +19,8 @@ Includes
 ============================
 */
 
-#include <stdint.h>
+#include <event.h>
+#include "state.h"
 
 /*
 ============================
@@ -28,11 +29,20 @@ Public Functions
 */
 
 /**
- * @brief
+ * @brief           Initializes a notification processing thread.
  *
- * @param opaque
- * @return int32_t
+ * @param state     Server state. Must be specified to interface with the Whist protocol.
+ * @param eb        An event base to handle d-bus message listening.
  */
-int32_t listen_and_process_notifications(void *opaque);
+void init_notifications_thread(whist_server_state *state, struct event_base *eb);
+
+/**
+ * @brief       Destroys a notification processing thread by closing connections
+ *              and freeing memory.
+ *
+ * @param eb    An event base object (libevent). Used to identify and break the
+ *              libevent event loop.
+ */
+void destroy_notifications_thread(struct event_base *eb);
 
 #endif  // NOTIFICATIONS_H
