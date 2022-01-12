f988f91113c8960473c4d4013fe8f355b79aabe7
Roshan Padaki <roshan@whist.com>
2022-01-12 14:11:19 -0500

    factor out

--- protocol/test/codec_test.cpp
+++ protocol/test/codec_test.cpp
@@ -4,98 +4,15 @@
  * @brief This file contains unit tests for codecs in the /protocol codebase
  */
 
-#ifdef _WIN32
-#define _CRT_SECURE_NO_WARNINGS  // stupid Windows warnings
-#endif
-
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include <iostream>
-#include <fstream>
+#include "fixtures.hpp"
 
 extern "C" {
-#include <fcntl.h>
 #include "whist/video/codec/encode.h"
 #include "whist/video/codec/decode.h"
 }
 
-#define TEST_OUTPUT_DIRNAME "test_output"
-
-class CodecTest : public ::testing::Test {
-    /*
-        This class is a test fixture which redirects stdout to a file
-        for the duration of the test. The file is then read and compared
-        to registered expected output matchers on a line-by-line basis.
-    */
-   protected:
-    void SetUp() override {
-        /*
-            This function captures the output of stdout to a file for the current
-            test. The file is named after the test name, and is located in the
-            test/test_output directory. The file is overwritten if it already
-            exists.
-        */
-        old_stdout = safe_dup(STDOUT_FILENO);
-        safe_mkdir(TEST_OUTPUT_DIRNAME);
-        std::string filename = std::string(TEST_OUTPUT_DIRNAME) + "/" +
-                               ::testing::UnitTest::GetInstance()->current_test_info()->name() +
-                               ".log";
-        fd = safe_open(filename.c_str(), O_WRONLY | O_CREAT);
-        EXPECT_GE(fd, 0);
-        fflush(stdout);
-        safe_dup2(fd, STDOUT_FILENO);
-    }
-
-    void TearDown() override {
-        /*
-            This function releases the output of stdout captured by the `SetUp()`
-            function. We then open the file and read it line by line, comparing
-            the output to matchers we have registerd with `check_stdout_line()`.
-        */
-        fflush(stdout);
-        safe_dup2(old_stdout, STDOUT_FILENO);
-        safe_close(fd);
-        std::ifstream file(std::string(TEST_OUTPUT_DIRNAME) + "/" +
-                           ::testing::UnitTest::GetInstance()->current_test_info()->name() +
-                           ".log");
-
-        for (::testing::Matcher<std::string> matcher : line_matchers) {
-            std::string line;
-            std::getline(file, line);
-            EXPECT_THAT(line, matcher);
-        }
-
-        file.close();
-    }
-
-    void check_stdout_line(::testing::Matcher<std::string> matcher) {
-        /*
-            This function registers a matcher to be used to compare the output
-            of the current test to the expected output. For example, if we want
-            to check that the output contains the string "hello", we can do so
-            by calling `check_stdout_line(::testing::HasSubstr("hello"))`. If we
-            then want to verify that the next line of output contains the string
-            "world", we then call `check_stdout_line(::testing::HasSubstr("world"))`.
-
-            The matcher is added to a vector of matchers, which is used to
-            compare the output line-by-line. To add multiple matchers for a
-            single line, we can use `::testing::AllOf()` or `::testing::AnyOf()`
-            to combine multiple matchers into a single matcher.
-
-            Note that the matchers are not checked until the `TearDown()` function
-            is called.
-
-            Arguments:
-                matcher (::testing::Matcher<std::string>): The matcher to use
-                    to compare the output line.
-        */
-        line_matchers.push_back(matcher);
-    }
-
-    int old_stdout;
-    int fd;
-    std::vector<::testing::Matcher<std::string>> line_matchers;
-};
+class CodecTest : public CaptureStdoutFixture {};
 
 static void test_write_image(uint8_t *data, int width, int height, int pitch, int value) {
     // Encode a 16-bit number in binary using black and white patches in the video.
