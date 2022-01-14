62fdd86feef47f1586b8d775ac9e06e1b9008dd3
Kevin Meng <kevin@whist.com>
2022-01-14 20:04:57 +0000

    add windows/macos stubs for notifications - feature only avail on linux

--- protocol/server/main.c
+++ protocol/server/main.c
@@ -24,6 +24,7 @@ Includes
 
 #ifdef __linux__
 #include "notifications.h"
+#include <event.h>
 #endif
 
 /*
@@ -439,7 +440,6 @@ int main(int argc, char* argv[]) {
     init_window_info_getter();
 
     WhistTimer ack_timer;
-    clock ack_timer;
     start_timer(&ack_timer);
 
     WhistTimer window_name_timer;
