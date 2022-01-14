169b580841bc146955a6ceff4e669e2c0b3eae4c
Kevin Meng <kevin@whist.com>
2022-01-14 21:26:34 +0000

    remove backticks

--- mandelboxes/browsers/chrome/start-chrome.sh
+++ mandelboxes/browsers/chrome/start-chrome.sh
@@ -60,7 +60,7 @@ flags+=(--flag-switches-end)
 flags+=($INITIAL_URL)
 
 # Load D-Bus configurations from .xinitrc
-eval `cat /whist/dbus_config.txt`
+eval $(cat /whist/dbus_config.txt)
 echo "Loaded d-bus address in start-chrome.sh: $DBUS_SESSION_BUS_ADDRESS | pid: $DBUS_SESSION_BUS_PID"
 
 # Start Chrome
