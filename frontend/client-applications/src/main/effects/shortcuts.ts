adddb41f4184b140838eda97256b256a3903b0dd
Ming Ying <ming@whist.com>
2022-01-14 16:57:01 -0500

    create/destroy omnibar instsead of hiding

--- frontend/client-applications/src/main/effects/shortcuts.ts
+++ frontend/client-applications/src/main/effects/shortcuts.ts
@@ -2,13 +2,17 @@ import { fromTrigger } from "@app/utils/flows"
 import { createGlobalShortcut } from "@app/utils/shortcuts"
 
 import { WhistTrigger } from "@app/constants/triggers"
-import { getWindowByHash, showOmnibar, hideOmnibar } from "@app/utils/windows"
+import {
+  getWindowByHash,
+  createOmnibar,
+  destroyOmnibar,
+} from "@app/utils/windows"
 import { WindowHashOmnibar } from "@app/constants/windows"
 
 fromTrigger(WhistTrigger.mandelboxFlowSuccess).subscribe(() => {
   createGlobalShortcut("Option+W", () => {
     const win = getWindowByHash(WindowHashOmnibar)
 
-    win?.isVisible() ?? false ? hideOmnibar() : showOmnibar()
+    win === undefined ? createOmnibar() : destroyOmnibar()
   })
 })
