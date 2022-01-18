e9ced8278a1fc5b9a50381aaa146818f90c439d2
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-01-18 11:24:36 -0500

    moved parsing of client dpi from xinitrc to update-whist-dpi.sh

--- mandelboxes/base/display/update-whist-dpi.sh
+++ mandelboxes/base/display/update-whist-dpi.sh
@@ -7,29 +7,40 @@
 # Enable Sentry bash error handler, this will catch errors if `set -e` is set in a Bash script
 SENTRY_ENV_FILENAME=/usr/share/whist/private/sentry_env
 case $(cat $SENTRY_ENV_FILENAME) in
-  dev|staging|prod)
-    export SENTRY_ENVIRONMENT=${SENTRY_ENV}
-    eval "$(sentry-cli bash-hook)"
-    ;;
-  *)
-    echo "Sentry environment not set, skipping Sentry error handler"
-    ;;
+    dev|staging|prod)
+        export SENTRY_ENVIRONMENT=${SENTRY_ENV}
+        eval "$(sentry-cli bash-hook)"
+        ;;
+    *)
+        echo "Sentry environment not set, skipping Sentry error handler"
+        ;;
 esac
 
 # Exit on subcommand errors
 set -Eeuo pipefail
 
-# Takes the DPI as an input, or 192 as the default.
-# 96 is a reasonable default, but Macbooks these days
-# default to 192, so this is a hack until JSON transport
-# is ready!
-WHIST_DPI=${1:-192}
+# Takes the DPI as an input, or the initial value passed by the JSON transport protocol.
+# If that's not available, use 192 as the default. 96 is a reasonable default, but
+# Macbooks these days default to 192, so this is a hack until JSON transport is ready!
+
+
+# Obtain relevant config values from the JSON transport file
+WHIST_JSON_FILE=/whist/resourceMappings/config.json
+INITIAL_DPI=192
+if [[ -f $WHIST_JSON_FILE ]]; then
+    if [ "$( jq 'has("client_dpi")' < $WHIST_JSON_FILE )" == "true"  ]; then
+        INITIAL_DPI="$(jq -rc '.client_dpi' < $WHIST_JSON_FILE)"
+    fi
+fi
+
+
+WHIST_DPI=${1:-INITIAL_DPI}
 WHIST_DPI_CACHE_FILE=/usr/share/whist/dpi.cache
 
 # Don't do anything if the DPI didn't change!
 if [[ -f "$WHIST_DPI_CACHE_FILE" ]]; then
-  CACHED_DPI=$(cat $WHIST_DPI_CACHE_FILE)
-  [[ "$WHIST_DPI" == "$CACHED_DPI" ]] && exit
+    CACHED_DPI=$(cat $WHIST_DPI_CACHE_FILE)
+    [[ "$WHIST_DPI" == "$CACHED_DPI" ]] && exit
 fi
 
 # https://unix.stackexchange.com/a/640599
