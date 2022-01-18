f92e4bffaadd6bf44fc731afc2e68e7d1746251c
Mark Thompson <mark@whist.com>
2022-01-18 12:21:36 +0000

    Memory allocation overrides for FFmpeg
    If FFmpeg is built with `--malloc-prefix='whist_ffmpeg_'`, these
    functions override the normal allocation to provide the extra page
    alignment wanted for buffer use on macOS.
    
    Not included on other platforms.

---
+++ protocol/whist/utils/ffmpeg_memory.c
@@ -0,0 +1,28 @@
+/**
+ * @copyright Copyright (c) 2022 Whist Technologies, Inc.
+ * @file ffmpeg_memory.c
+ * @brief Memory allocation overrides for FFmpeg on macOS.
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+void *whist_ffmpeg_malloc(size_t size) {
+    size_t page_size = sysconf(_SC_PAGESIZE);
+    size_t align;
+    if (size >= page_size) {
+        // Align to page size so that buffers can be used with SDL.
+        align = page_size;
+    } else {
+        // Small buffers only need the largest-register alignment as
+        // normally required by FFmpeg.
+        align = 64;
+    }
+    void *ptr;
+    if (posix_memalign(&ptr, align, size)) ptr = NULL;
+    return ptr;
+}
+
+void *whist_ffmpeg_realloc(void *ptr, size_t size) { return realloc(ptr, size); }
+
+void whist_ffmpeg_free(void *ptr) { free(ptr); }
