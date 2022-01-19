90d537d8e5cbe188ae933dd1e2dcce1c7dca671f
djsavvy <sraghuvanshi@college.harvard.edu>
2022-01-19 16:26:58 +0000

    Rewrite end-to-end user configs test

--- backend/services/host-service/mandelbox/user_configs_test.go
+++ backend/services/host-service/mandelbox/user_configs_test.go
@@ -5,10 +5,11 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"sync"
 	"testing"
 
 	"github.com/whisthq/whist/backend/services/host-service/mandelbox/configutils"
-	mandelboxtypes "github.com/whisthq/whist/backend/services/types"
+	"github.com/whisthq/whist/backend/services/types"
 	"github.com/whisthq/whist/backend/services/utils"
 )
 
@@ -19,25 +20,27 @@ import (
 // decrypt, and uncompress the file and assert that the final
 // config is the same as the original.
 func TestUserConfigIntegration(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	testMandelboxData := mandelboxData{
-		ctx:                   ctx,
-		cancel:                cancel,
-		ID:                    mandelboxtypes.MandelboxID(utils.PlaceholderTestUUID()),
+	uCtx, uCancel := context.WithCancel(context.Background())
+	testToken := types.ConfigEncryptionToken("testEncryptionToken")
+	testInvalidToken := types.ConfigEncryptionToken("testInvalidEncryptionToken")
+	uploaderMandelboxData := mandelboxData{
+		ctx:                   uCtx,
+		cancel:                uCancel,
+		ID:                    types.MandelboxID(utils.PlaceholderTestUUID()),
 		appName:               "testApp",
 		userID:                "user_config_test_user",
-		configEncryptionToken: "testEncryptionToken",
+		configEncryptionToken: testToken,
 	}
 
 	// Start with a clean slate
 	os.RemoveAll(utils.WhistDir)
 
-	err := testMandelboxData.SetupUserConfigDirs()
+	err := uploaderMandelboxData.setupUserConfigDirs()
 	if err != nil {
 		t.Fatalf("failed to create user config directories: %v", err)
 	}
 
-	mandelboxDir := path.Join(utils.WhistDir, testMandelboxData.ID.String())
+	mandelboxDir := path.Join(utils.WhistDir, uploaderMandelboxData.ID.String())
 	sourceDir := path.Join(mandelboxDir, "testBase")
 	if err := os.MkdirAll(sourceDir, 0777); err != nil {
 		t.Fatalf("failed to create source directory: %v", err)
@@ -49,7 +52,7 @@ func TestUserConfigIntegration(t *testing.T) {
 	}
 	defer cleanupTestDirs()
 
-	unpackedConfigPath := path.Join(testMandelboxData.GetUserConfigDir(), UnpackedConfigsDirectoryName)
+	unpackedConfigPath := path.Join(uploaderMandelboxData.getUserConfigDir(), UnpackedConfigsDirectoryName)
 	if err := os.MkdirAll(unpackedConfigPath, 0777); err != nil {
 		t.Fatalf("failed to create config dir %s: %v", unpackedConfigPath, err)
 	}
@@ -61,45 +64,73 @@ func TestUserConfigIntegration(t *testing.T) {
 		t.Fatalf("error copying test directories: %v, output: %s", err, output)
 	}
 
-	if err := testMandelboxData.BackupUserConfigs(); err != nil {
+	if err := uploaderMandelboxData.BackupUserConfigs(); err != nil {
 		t.Fatalf("error backing up configs: %v", err)
 	}
 
-	t.Run("valid token", func(t *testing.T) {
-		// Delete the user config directory so it can be recreated
-		os.RemoveAll(unpackedConfigPath)
+	downloadTestNotNewToken := func(useOriginalToken bool, isNewToken bool) func(*testing.T) {
+		return func(t *testing.T) {
+			// Delete the user config directory so it can be recreated
+			os.RemoveAll(unpackedConfigPath)
 
-		if err := testMandelboxData.DownloadUserConfigs(); err != nil {
-			t.Fatalf("error populating configs: %v", err)
+			dCtx, dCancel := context.WithCancel(context.Background())
+			downloaderMandelboxData := mandelboxData{
+				ctx:     dCtx,
+				cancel:  dCancel,
+				ID:      types.MandelboxID(utils.PlaceholderTestUUID()),
+				appName: "testApp",
+				userID:  "user_config_test_user",
+			}
+
+			ctx, cancel := context.WithCancel(context.Background())
+			goroutineTracker := sync.WaitGroup{}
+			infoChan, errChan := downloaderMandelboxData.StartLoadingUserConfigs(ctx, cancel, &goroutineTracker)
+			defer close(infoChan)
+
+			tokenToUse := func() types.ConfigEncryptionToken {
+				if useOriginalToken {
+					return testToken
+				} else {
+					return testInvalidToken
+				}
+			}()
+
+			infoChan <- ConfigEncryptionInfo{
+				Token:                          tokenToUse,
+				IsNewTokenAccordingToClientApp: isNewToken,
+			}
+			close(infoChan)
+
+			// Verify errors (or their absence) depending on provided params
+			errCount := 0
+			for _ = range errChan {
+				errCount++
+			}
+
+			if isNewToken {
+				// We expect no errors if we have a new token
+				if errCount != 0 {
+					t.Fatalf("Got errors with new token configuration!")
+				}
+			} else if !useOriginalToken && errCount == 0 {
+				t.Fatalf("expected errors since invalid (not new) token supplied!")
+			}
+
+			// Verify that all files in original directory are still there and correct
+			destinationPath := path.Join(unpackedConfigPath, "testBase")
+			err = configutils.ValidateDirectoryContents(sourceDir, destinationPath)
+			if err != nil {
+				t.Fatalf("error validating directory contents: %v", err)
+			}
 		}
+	}
 
-		if err := testMandelboxData.DecryptUserConfigs(); err != nil {
-			t.Fatalf("error decrypting configs: %v", err)
-		}
+	t.Run("valid token", downloadTestNotNewToken(true, false))
+	t.Run("invalid token", downloadTestNotNewToken(false, false))
 
-		// Verify that all files in original directory are still there and correct
-		destinationPath := path.Join(unpackedConfigPath, "testBase")
-		err = configutils.ValidateDirectoryContents(sourceDir, destinationPath)
-		if err != nil {
-			t.Fatalf("error validating directory contents: %v", err)
-		}
-	})
-
-	t.Run("invalid token", func(t *testing.T) {
-		// Delete the user config directory so it can be recreated
-		os.RemoveAll(unpackedConfigPath)
-
-		// Change token to be invalid
-		testMandelboxData.configEncryptionToken = "invalidToken"
-
-		if err := testMandelboxData.DownloadUserConfigs(); err != nil {
-			t.Fatalf("error populating configs: %v", err)
-		}
-
-		if err := testMandelboxData.DecryptUserConfigs(); err == nil {
-			t.Fatalf("expected error decrypting configs but got nil")
-		}
-	})
+	// With a new token, anything is valid. But we want to make sure
+	t.Run("new repeated token", downloadTestNotNewToken(true, true))
+	t.Run("new non-repeated token", downloadTestNotNewToken(false, true))
 }
 
 // cleanupTestDirs removes the created directories created by the integration
