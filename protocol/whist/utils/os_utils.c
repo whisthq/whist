c487caf3432deeaa73a0168a4a95147ef33ef32f
Suriya Kandaswamy <suriya@tryfractal.com>
2022-01-25 13:22:04 -0500

    add uk keyboard layout (#5314)
    * add uk keyboard layout
    
    * lint
    
    Co-authored-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>

--- protocol/whist/utils/os_utils.c
+++ protocol/whist/utils/os_utils.c
@@ -42,7 +42,7 @@ static const char* const apple_keyboard_mappings[][2] = {
     {"com.apple.keylayout.Spanish", "es"},     {"com.apple.keylayout.Hebrew", "il"},
     {"com.apple.keylayout.Canadian", "ca"},    {"com.apple.keylayout.DVORAK-QWERTYCMD", "dvorak"},
     {"com.apple.keylayout.ABC-India", "us"},   {"com.apple.keylayout.Dvorak", "dvorak"},
-};
+    {"com.apple.keylayout.British", "uk"}};
 
 #define NUM_APPLE_KEYBOARD_MAPPINGS \
     ((int)sizeof(apple_keyboard_mappings) / (int)sizeof(apple_keyboard_mappings[0]))
@@ -51,7 +51,7 @@ static const char* const apple_keyboard_mappings[][2] = {
 #ifdef __linux__
 
 static const char linux_supported_layouts[][5] = {"us", "it",    "ara", "de", "fr",
-                                                  "es", "latam", "il",  "ca"};
+                                                  "es", "latam", "il",  "ca", "uk"};
 
 #define NUM_LINUX_SUPPORTED_LAYOUTS \
     ((int)sizeof(linux_supported_layouts) / (int)sizeof(linux_supported_layouts[0]))
