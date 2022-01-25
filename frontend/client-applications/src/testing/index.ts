f2218bd97b2ea8bfca55e469a157934358db771c
Ming Ying <ming@whist.com>
2022-01-25 14:40:19 -0500

    fix runtime error

--- frontend/client-applications/src/testing/index.ts
+++ frontend/client-applications/src/testing/index.ts
@@ -92,8 +92,11 @@ export const withMocking = <
   // Return channels unchanged if not found
   const mockFn = get(getMocks(), name) ?? undefined
 
+  if (mockFn === undefined) console.log("No mocking for", name)
+
   if (mockFn === undefined) return flowFn(trigger)
 
+  console.log("Yes mocking for", name)
   // We want any channels that are not defined in the DebugSchema to be
   // empty observables, so we don't cause errors for subscribers.
   return {
