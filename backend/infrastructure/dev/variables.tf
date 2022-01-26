a36ca3020a3db10d69c32d96fcb9c971952b3981
Mauricio Araujo <mauricio@whist.com>
2022-01-26 04:29:03 -0600

    Add IAM groups, remove IAM users, update policies

---
+++ backend/infrastructure/dev/variables.tf
@@ -0,0 +1,3 @@
+variable enabled_regions {
+    type = map
+}
--- backend/infrastructure/modules/aws_iam/main.tf
+++ backend/infrastructure/modules/aws_iam/main.tf
@@ -1,46 +1,47 @@
-resource "aws_iam_user" "whist-employees" {
-  for_each = var.employee_emails
-  name = each.value
+# IAM roles
+
+#IAM User groups
+
+resource "aws_iam_group" "whist-2FA" {
+  name = "Whist2FA"
 }
 
-resource "aws_iam_user" "whist-admins" {
-  for_each = var.admin_emails
-  name = each.value
+resource "aws_iam_group" "whist-admins" {
+  name = "WhistAdmins"
 }
 
-resource "aws_iam_group" "whist-engineering-group" {
-  name = "WhistEngineering"
+resource "aws_iam_group" "whist-ci" {
+  name = "WhistCI"
 }
 
-# This will add each managed policy to the group
-resource "aws_iam_group_policy_attachment" "WhistEngineering_policy" {
-  group      = aws_iam_group.whist-engineering-group.name
-  for_each   = var.whist-engineering-managed-policies
+resource "aws_iam_group" "whist-engineers" {
+  name = "WhistEngineers"
+}
+
+# Custom group policies
+
+resource "aws_iam_group_policy" "2FA-policy" {
+  name = "Force_MFA"
+  group = aws_iam_group.whist-2FA
+  policy = jsonencode
+}
+
+# AWS managed group policies
+
+resource "aws_iam_group_policy_attachment" "admin-policy" {
+  group      = aws_iam_group.whist-admins.name
+  for_each   = var.whist-admins-managed-policies
   policy_arn = each.key
 }
 
-resource "aws_iam_group" "mfa" {
-  name = "2FA"
-}
-
-resource "aws_iam_group_policy" "mfa-policy" {
-  name  = "2FA"
-  group = aws_iam_group.mfa.name
-  policy = jsonencode(var.mfa-policy)
-}
-
-resource "aws_iam_group" "whist-aws-admins" {
-  name = "WhistAWSAdmins"
-}
-
-resource "aws_iam_group_policy_attachment" "whist-aws-admin-policy" {
-  group      = aws_iam_group.whist-aws-admins.name
-  for_each   = var.whist-aws-admins-managed-policies
+resource "aws_iam_group_policy_attachment" "ci-policy" {
+  group      = aws_iam_group.whist-engineers.name
+  for_each   = var.whist-ci-managed-policies
   policy_arn = each.key
 }
 
-resource "aws_iam_user_group_membership" "employee-membership" {
-  for_each = aws_iam_user.whist-employees
-  user   = aws_iam_user.whist-employees[each.key].name
-  groups = [aws_iam_group.whist-engineering-group.name, aws_iam_group.mfa.name]
+resource "aws_iam_group_policy_attachment" "engineering-policy" {
+  group      = aws_iam_group.whist-engineers.name
+  for_each   = var.whist-engineers-managed-policies
+  policy_arn = each.key
 }
