d7c53034af5fc8925aa9ccbeaa77d87641534417
Ming Ying <ming@whist.com>
2022-01-27 18:03:19 -0500

    add applescript

--- frontend/client-applications/src/constants/applescript.ts
+++ frontend/client-applications/src/constants/applescript.ts
@@ -1,1 +1,24 @@
-const GET_ALL_WINDOWS = ""
+const getAllWindows = (browser: string) => `
+    tell application "${browser}"
+        
+        --Variables
+        set winlist to every window
+        set totaltabcount to 0
+        
+        -- Loop through each window to count the number of open tabs
+        repeat with win in winlist
+            try
+                set tabcount to number of tabs in win
+                set totaltabcount to totaltabcount + tabcount
+                -- log "tab count: " & tabcount & " totaltabcount: " & totaltabcount
+            on error errmsg
+                -- Often getting error message like this: 
+                -- "Safari got an error: AppleEvent handler failed."
+                -- log "error message: " & errmsg
+            end try
+        end repeat
+        
+        log "There are " & totaltabcount & " Safari tabs open."
+        
+    end tell
+`
