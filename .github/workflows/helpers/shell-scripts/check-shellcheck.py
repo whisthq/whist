2549d8766c9de78f52367d468cb6773efc9a9f40
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-01-27 14:51:35 -0500

    formatting

--- .github/workflows/helpers/shell-scripts/check-shellcheck.py
+++ .github/workflows/helpers/shell-scripts/check-shellcheck.py
@@ -28,34 +28,35 @@ parser.add_argument(
 
 if __name__ == "__main__":
 
-	args = parser.parse_args()
-	exclude_dirs = args.exclude_dirs.split()
+    args = parser.parse_args()
+    exclude_dirs = args.exclude_dirs.split()
 
-	# Obtain list of all .sh files after excluding directories
-	sh_files = set(glob.glob("**/*.sh", recursive = True))
-	for dir_path in exclude_dirs:
-		sh_files -= set(glob.glob(dir_path + "**/*.sh", recursive = True))
+    # Obtain list of all .sh files after excluding directories
+    sh_files = set(glob.glob("**/*.sh", recursive=True))
+    for dir_path in exclude_dirs:
+        sh_files -= set(glob.glob(dir_path + "**/*.sh", recursive=True))
 
-	codes_to_exclude = {}
-	exclude_codes = args.exclude_codes.split()
-	for code in exclude_codes:
-		filename, codes = code.split(":")
-		# Don't split codes!
-		codes_to_exclude[filename] = codes
+    codes_to_exclude = {}
+    exclude_codes = args.exclude_codes.split()
+    for code in exclude_codes:
+        filename, codes = code.split(":")
+        # Don't split codes!
+        codes_to_exclude[filename] = codes
 
-	
-	for file in sh_files:
-		if file not in codes_to_exclude:
-			p = subprocess.run("shellcheck {}".format(file), shell=True, capture_output=True)
-			print(p.stdout.decode())
-			if p.returncode != 0:
-				print("[Shellcheck did not pass] {}".format(file))
-				exit(1)
-		else:
-			codes = codes_to_exclude[file]
-			p = subprocess.run("shellcheck -e {} {}".format(codes, file), shell=True, capture_output=True)
-			print(p.stdout.decode())
-			if p.returncode != 0:
-				print("[Shellcheck did not pass] {}".format(file))
-				exit(1)
-		print("[Shellcheck passed] {}".format(file))
+    for file in sh_files:
+        if file not in codes_to_exclude:
+            p = subprocess.run("shellcheck {}".format(file), shell=True, capture_output=True)
+            print(p.stdout.decode())
+            if p.returncode != 0:
+                print("[Shellcheck did not pass] {}".format(file))
+                exit(1)
+        else:
+            codes = codes_to_exclude[file]
+            p = subprocess.run(
+                "shellcheck -e {} {}".format(codes, file), shell=True, capture_output=True
+            )
+            print(p.stdout.decode())
+            if p.returncode != 0:
+                print("[Shellcheck did not pass] {}".format(file))
+                exit(1)
+        print("[Shellcheck passed] {}".format(file))
