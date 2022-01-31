2bd16250ef31f046d46253299ea69a70ee3818ad
Roshan Padaki <roshan@whist.com>
2022-01-31 14:54:08 -0500

    disable opacity (#5376)

--- protocol/client/main.c
+++ protocol/client/main.c
@@ -439,19 +439,6 @@ int main(int argc, char* argv[]) {
         // This code will run for as long as there are events queued, or once every millisecond if
         // there are no events queued
         while (connected && !client_exiting && exit_code == WHIST_EXIT_SUCCESS) {
-            if (get_timer(&window_fade_timer) * MS_IN_SECOND < 300.0) {
-                ret = SDL_SetWindowOpacity((SDL_Window*)window,
-                                           get_timer(&window_fade_timer) * MS_IN_SECOND / 300.0);
-                if (ret != 0) {
-                    LOG_WARNING("Failed to set window opacity: %s", SDL_GetError());
-                }
-            } else {
-                ret = SDL_SetWindowOpacity((SDL_Window*)window, 1.0);
-                if (ret != 0) {
-                    LOG_WARNING("Failed to set window opacity: %s", SDL_GetError());
-                }
-            }
-
             // This should be called BEFORE the call to read_piped_arguments,
             // otherwise one URL may get lost.
             send_new_tab_url_if_needed();
