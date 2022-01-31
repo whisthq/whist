48e58d74e8e5875d980291195370deb782879092
Roshan Padaki <roshan@whist.com>
2022-01-31 17:43:29 -0500

    disable tab dragout using chrome extension (#5380)
    * disable tab dragout using chrome extension
    
    * remove logs

--- mandelboxes/browsers/chrome/teleport-extension/src/serviceWorker.ts
+++ mandelboxes/browsers/chrome/teleport-extension/src/serviceWorker.ts
@@ -1,4 +1,5 @@
-// Listen for file download state changes.
+// Listen for file download state changes and propagate
+// to the filesystem.
 const initDownloadHandler = () => {
   // Disable the downloads shelf at the bottom.
   chrome.downloads.setShelfEnabled(false)
@@ -29,4 +30,36 @@ const initDownloadHandler = () => {
   )
 }
 
+// Try to cancel or undo a tab drag-out
+const tryRestoreTabLocation = async (
+  tabId: number,
+  detachInfo: chrome.tabs.TabDetachInfo
+) => {
+  try {
+    await chrome.tabs.move(tabId, {
+      index: detachInfo.oldPosition,
+      windowId: detachInfo.oldWindowId,
+    })
+  } catch (err) {
+    if (
+      err ==
+      "Error: Tabs cannot be edited right now (user may be dragging a tab)."
+    ) {
+      await new Promise<void>((resolve) =>
+        setTimeout(() => {
+          tryRestoreTabLocation(tabId, detachInfo)
+          resolve()
+        }, 50)
+      )
+    }
+  }
+}
+
+// Listen for tab drag-out detach events and instantly
+// cancel/undo them.
+const initTabDetachSuppressor = () => {
+  chrome.tabs.onDetached.addListener(tryRestoreTabLocation)
+}
+
 initDownloadHandler()
+initTabDetachSuppressor()
