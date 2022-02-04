b9a630d4d861077d6db68b5f0b3816776bca5768
Roshan Padaki <roshan@whist.com>
2022-02-04 17:10:46 -0500

    up freq and gitignore graph.svg

--- protocol/tools/dtrace-flamegraph.sh
+++ protocol/tools/dtrace-flamegraph.sh
@@ -14,8 +14,9 @@ if [[ ! $# -eq 1 ]]; then
 fi
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FREQUENCY=1001hz
 
-dtrace -x ustackframes=100 -n "profile-97 /pid == $1/ { @[ustack()] = count(); } tick-60s { exit(0); }" \
+dtrace -x ustackframes=100 -n "profile-$FREQUENCY /pid == $1/ { @[ustack()] = count(); } tick-60s { exit(0); }" \
   | "$SRC_DIR"/flamegraph-helpers/stackcollapse.pl \
   | "$SRC_DIR"/flamegraph-helpers/flamegraph.pl \
   > graph.svg
