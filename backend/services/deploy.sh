d2a0ef6958373423d78401530fdce233c61ccf16
Mauricio <mauricio@whist.com>
2022-02-05 12:03:20 -0600

    Move deploy.sh and Procfile to services dir, edit paths

--- backend/services/deploy.sh
+++ backend/services/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Deploy the scaling service to Heroku.
+# Deploy the services project to Heroku.
 
 # Arguments
 # ${1}: REGION_IMAGE_MAP: map of instance image ids by region.
@@ -12,16 +12,15 @@ set -Eeuo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Working directory is monorepo root
-cd "$DIR/../../.."
+cd "$DIR/../../"
 
 REGION_IMAGE_MAP=${1}
 SCALING_SERVICE_DIR="./backend/services/scaling-service"
-BUILD_DIR="$SCALING_SERVICE_DIR"
+BUILD_DIR="$SCALING_SERVICE_DIR/bin"
 IMAGE_FILE="$SCALING_SERVICE_DIR/images.json"
 
-
-# Copy the binary to the scaling service dir. This is necessary because we will use as standalone repo.
-cp ./backend/services/build/scaling-service "$BUILD_DIR"
+# Copy the binary to the scaling service bin directory. This is necessary because we will use as standalone repo.
+mkdir -p "$BUILD_DIR" && cp ./backend/services/build/scaling-service "$BUILD_DIR"
 
 # Write region image map to var file so the Procfile can read it.
 echo "$REGION_IMAGE_MAP" > "$IMAGE_FILE"
@@ -32,8 +31,8 @@ git commit -m "Add scaling service build and images.json file for splitsh"
 
 # Use splitsh to checkout to standalone scaling service repo.
 echo "Checking out the scaling service folder as a standalone git repo..."
-splitsh-lite --prefix backend/services/scaling-service --target refs/heads/workflows-private/scaling-service
-git checkout workflows-private/scaling-service
+splitsh-lite --prefix backend/services --target refs/heads/workflows-private/services
+git checkout workflows-private/services
 
 # we are now in the scaling-service folder as a standalone git repo, push to Heroku.
 echo "Redeploying scaling service..."
