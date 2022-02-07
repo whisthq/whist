82218c006e36750cc7b893a85d4f0498b8470162
Mauricio <mauricio@whist.com>
2022-02-07 12:56:03 -0600

    Remove splitsh and use an empty git branch

--- backend/services/scaling-service/deploy.sh
+++ backend/services/scaling-service/deploy.sh
@@ -4,6 +4,8 @@
 
 # Arguments
 # ${1}: REGION_IMAGE_MAP: map of instance image ids by region.
+# ${2}: HEROKU_APP_NAME: name of the Heroku app we are deploying to.
+# ${3}: MONOREPO_COMMIT_HASH: commit SHA that triggered the deploy.
 
 set -Eeuo pipefail
 
@@ -15,6 +17,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/../../.."
 
 REGION_IMAGE_MAP=${1}
+HEROKU_APP_NAME=${2}
+MONOREPO_COMMIT_HASH=${3}
+
 SCALING_SERVICE_DIR="./backend/services/scaling-service"
 DEPLOY_DIR="$SCALING_SERVICE_DIR/deploy"
 IMAGE_FILE="$DEPLOY_DIR/images.json"
@@ -29,18 +34,17 @@ echo "$REGION_IMAGE_MAP" > "$IMAGE_FILE"
 # Write Procfile
 echo -e "web: ./scaling-service" > "$PROCFILE"
 
-# Persist changes on local repo so that they are read by splitsh
+# populate the deploy/ directory
+mv deploy ..
+git switch --orphan deploy-branch
+git clean -dfx # remove any .gitignored files that might remain
+mv ../deploy/* .
 git add .
-git commit -m "Create deploy directory for scaling service"
-
-# Use splitsh to checkout to standalone scaling service repo.
-echo "Checking out the scaling service folder as a standalone git repo..."
-splitsh-lite --prefix backend/services/scaling-service/deploy --target refs/heads/workflows-private/scaling-service
-git checkout workflows-private/scaling-service
+git commit -m "scaling-service deploy for $MONOREPO_COMMIT_HASH"
 
 # Create null buildpack, see: https://elements.heroku.com/buildpacks/ryandotsmith/null-buildpack
 heroku buildpacks:set http://github.com/ryandotsmith/null-buildpack.git -a "$HEROKU_APP_NAME"
 
 # Push deploy directory to Heroku
 echo "Deploying scaling service..."
-git push -f heroku-whist-scaling-service workflows-private/scaling-service:master
+git push -f heroku-whist-scaling-service deploy-branch:master
