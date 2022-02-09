00f633c181add2ebea50f18bdd503a816034de7f
Ming Ying <ming@whist.com>
2022-02-09 15:42:16 -0500

    clean config code

--- frontend/whist-extension/scripts/helpers.js
+++ frontend/whist-extension/scripts/helpers.js
@@ -6,27 +6,8 @@ module.exports = {
     fs.writeFileSync("src/config.json", JSON.stringify(json))
   },
   createConfigJSON: (env) => {
-    if (env === "dev") {
-      return {
-        AUTH_DOMAIN_URL: config.AUTH_DOMAIN_URL_DEV,
-        AUTH_CLIENT_ID: config.AUTH_CLIENT_ID_DEV,
-        AUTH_API_IDENTIFIER: config.AUTH_API_IDENTIFIER,
-        CLIENT_CALLBACK_URL: config.CLIENT_CALLBACK_URL,
-      }
-    } else if (env === "staging") {
-      return {
-        AUTH_DOMAIN_URL: config.AUTH_DOMAIN_URL_STAGING,
-        AUTH_CLIENT_ID: config.AUTH_CLIENT_ID_STAGING,
-        AUTH_API_IDENTIFIER: config.AUTH_API_IDENTIFIER,
-        CLIENT_CALLBACK_URL: config.CLIENT_CALLBACK_URL,
-      }
-    } else {
-      return {
-        AUTH_DOMAIN_URL: config.AUTH_DOMAIN_URL_PROD,
-        AUTH_CLIENT_ID: config.AUTH_CLIENT_ID_PROD,
-        AUTH_API_IDENTIFIER: config.AUTH_API_IDENTIFIER,
-        CLIENT_CALLBACK_URL: config.CLIENT_CALLBACK_URL,
-      }
-    }
+    if (env === "dev") return config.dev
+    if (env === "staging") return config.staging
+    return config.prod
   },
 }
