a8f476d9a289a359a2998c5e9f0a7a00caee0a56
Ming Ying <ming@whist.com>
2022-02-09 14:45:05 -0500

    build script with json file

---
+++ frontend/whist-extension/scripts/build.js
@@ -0,0 +1,41 @@
+const fs = require("fs")
+const config = require("./config")
+const execCommand = require("./execCommand").execCommand
+
+const createConfigFileFromJSON = (json) => {
+  fs.writeFileSync("public/config.json", JSON.stringify(json))
+}
+
+const createConfigJSON = (env) => {
+  if (env === "dev") {
+    return {
+      AUTH_DOMAIN_URL: config.AUTH_DOMAIN_URL_DEV,
+      AUTH_CLIENT_ID: config.AUTH_CLIENT_ID_DEV,
+      AUTH_API_IDENTIFIER: config.AUTH_API_IDENTIFIER,
+      CLIENT_CALLBACK_URL: config.CLIENT_CALLBACK_URL,
+    }
+  } else if (env === "staging") {
+    return {
+      AUTH_DOMAIN_URL: config.AUTH_DOMAIN_URL_STAGING,
+      AUTH_CLIENT_ID: config.AUTH_CLIENT_ID_STAGING,
+      AUTH_API_IDENTIFIER: config.AUTH_API_IDENTIFIER,
+      CLIENT_CALLBACK_URL: config.CLIENT_CALLBACK_URL,
+    }
+  } else {
+    return {
+      AUTH_DOMAIN_URL: config.AUTH_DOMAIN_URL_PROD,
+      AUTH_CLIENT_ID: config.AUTH_CLIENT_ID_PROD,
+      AUTH_API_IDENTIFIER: config.AUTH_API_IDENTIFIER,
+      CLIENT_CALLBACK_URL: config.CLIENT_CALLBACK_URL,
+    }
+  }
+}
+
+if (require.main === module) {
+  const env = process.argv[2]
+
+  console.log(`Creating config JSON file for ${env}`)
+  createConfigFileFromJSON(createConfigJSON(env))
+
+  execCommand("webpack --config webpack/webpack.prod.js")
+}
---
+++ frontend/whist-extension/scripts/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  AUTH_DOMAIN_URL_DEV: "fractal-dev.us.auth0.com",
+  AUTH_DOMAIN_URL_STAGING: "fractal-staging.us.auth0.com",
+  AUTH_DOMAIN_URL_PROD: "auth.whist.com",
+  AUTH_CLIENT_ID_DEV: "F4M4J6VjbXlT2UzCfnxFaJK2yKJXbxcF",
+  AUTH_CLIENT_ID_STAGING: "WXO2cphPECuDc7DkDQeuQzYUtCR3ehjz",
+  AUTH_CLIENT_ID_PROD: "Ulk5B2RfB7mM8BVjA3JtkrZT7HhWIBLD",
+  AUTH_API_IDENTIFIER: "https://api.fractal.co",
+  CLIENT_CALLBACK_URL: "http://localhost/callback",
+}
