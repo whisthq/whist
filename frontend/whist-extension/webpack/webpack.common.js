caba8647ee33e2be2207c6edd1f54112acdd013e
Ming Ying <ming@whist.com>
2022-02-09 12:00:10 -0500

    adding whist extension

---
+++ frontend/whist-extension/webpack/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { ProvidePlugin } = require("webpack");
+
+const srcDir = path.join(__dirname, "..", "src");
+const outDir = path.join(__dirname, "..", "build", "js");
+
+module.exports = {
+  entry: {
+    index: path.join(srcDir, "index.ts"),
+  },
+  output: {
+    path: outDir,
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    fallback: {
+      path: require.resolve("path-browserify"),
+    },
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: "../", context: "public" }],
+    }),
+    new ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
