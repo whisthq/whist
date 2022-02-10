3d3b9efe70c0ccf12b359e339ef47361ac632864
Ming Ying <ming@whist.com>
2022-02-09 23:48:29 -0500

    got auth launch working, just need to parse and store now

--- frontend/whist-extension/src/worker/index.ts
+++ frontend/whist-extension/src/worker/index.ts
@@ -1,10 +1,9 @@
 import { config } from "@app/constants/app"
 
 export const authPortalURL = () => {
-  const parsedConfig = JSON.parse(JSON.stringify(config))
   return [
-    `https://${parsedConfig.AUTH_DOMAIN_URL}/authorize`,
-    `?audience=${parsedConfig.AUTH_API_IDENTIFIER}`,
+    `https://${config.AUTH_DOMAIN_URL}/authorize`,
+    `?audience=${config.AUTH_API_IDENTIFIER}`,
     // We need to request the admin scope here. If the user is enabled for the admin scope
     // (e.g. marked as a developer in Auth0), then the returned JWT token will be granted
     // the admin scope, thus bypassing Stripe checks and granting additional privileges in
@@ -13,11 +12,23 @@ export const authPortalURL = () => {
     // https://auth0.com/docs/scopes#requested-scopes-versus-granted-scopes
     "&scope=openid profile offline_access email admin",
     "&response_type=code",
-    `&client_id=${parsedConfig.AUTH_CLIENT_ID}`,
-    `&redirect_uri=${parsedConfig.CLIENT_CALLBACK_URL}`,
+    `&client_id=${config.AUTH_CLIENT_ID}`,
+    `&redirect_uri=${chrome.identity.getRedirectURL("auth0")}`,
   ].join("")
 }
 
-chrome.tabs.create({
-  url: chrome.runtime.getURL("src/newtab.html"),
-})
+// chrome.tabs.create({
+//   url: chrome.runtime.getURL("src/newtab.html"),
+// })
+
+// console.log(chrome.identity.getRedirectURL("auth0"))
+
+chrome.identity.launchWebAuthFlow(
+  {
+    url: authPortalURL(),
+    interactive: true,
+  },
+  (callbackUrl) => {
+    console.log("done", callbackUrl)
+  }
+)
