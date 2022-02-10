79b6dbcf7bed22210301422def2f55ea74b5a226
Ming Ying <ming@whist.com>
2022-02-10 13:29:53 -0500

    wip

--- frontend/whist-extension/src/worker/index.ts
+++ frontend/whist-extension/src/worker/index.ts
@@ -1,27 +1,4 @@
-import { config } from "@app/constants/app"
-
-export const authPortalURL = () => {
-  return [
-    `https://${config.AUTH_DOMAIN_URL}/authorize`,
-    `?audience=${config.AUTH_API_IDENTIFIER}`,
-    // We need to request the admin scope here. If the user is enabled for the admin scope
-    // (e.g. marked as a developer in Auth0), then the returned JWT token will be granted
-    // the admin scope, thus bypassing Stripe checks and granting additional privileges in
-    // the webserver. If the user is not enabled for the admin scope, then the JWT token
-    // will be generated but will not have the admin scope, as documented by Auth0 in
-    // https://auth0.com/docs/scopes#requested-scopes-versus-granted-scopes
-    "&scope=openid profile offline_access email admin",
-    "&response_type=code",
-    `&client_id=${config.AUTH_CLIENT_ID}`,
-    `&redirect_uri=${chrome.identity.getRedirectURL("auth0")}`,
-  ].join("")
-}
-
-// chrome.tabs.create({
-//   url: chrome.runtime.getURL("src/newtab.html"),
-// })
-
-// console.log(chrome.identity.getRedirectURL("auth0"))
+import { authPortalURL } from "./utils/auth"
 
 chrome.identity.launchWebAuthFlow(
   {
---
+++ frontend/whist-extension/src/worker/utils/auth.ts
@@ -0,0 +1,165 @@
+import jwtDecode from "jwt-decode"
+import { randomBytes } from "crypto"
+
+import { config } from "@app/constants/app"
+import { getStorage, setStorage } from "@app/worker/utils/storage"
+import { CACHED_AUTH_INFO } from "@app/constants/storage"
+
+export const authPortalURL = () => {
+  return [
+    `https://${config.AUTH_DOMAIN_URL}/authorize`,
+    `?audience=${config.AUTH_API_IDENTIFIER}`,
+    // We need to request the admin scope here. If the user is enabled for the admin scope
+    // (e.g. marked as a developer in Auth0), then the returned JWT token will be granted
+    // the admin scope, thus bypassing Stripe checks and granting additional privileges in
+    // the webserver. If the user is not enabled for the admin scope, then the JWT token
+    // will be generated but will not have the admin scope, as documented by Auth0 in
+    // https://auth0.com/docs/scopes#requested-scopes-versus-granted-scopes
+    `&client_id=${config.AUTH_CLIENT_ID}`,
+    `&redirect_uri=${chrome.identity.getRedirectURL("auth0")}`,
+    `&connection=google-oauth2`,
+    "&scope=openid profile offline_access email admin",
+    "&response_type=code",
+  ].join("")
+}
+
+export const authInfoCallbackRequest = async (callbackURL: string) => {
+  /*
+  Description:
+    Given a callback URL, generates an {email, accessToken, refreshToken} response
+  Arguments:
+    callbackURL (string): Callback URL
+  Returns:
+    {email, accessToken, refreshToken}
+  */
+
+  const url = new URL(callbackURL)
+  const code = url.searchParams.get("code")
+
+  return await fetch(`https://${config.AUTH_DOMAIN_URL}/oauth/token`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      grant_type: "authorization_code",
+      client_id: config.AUTH_CLIENT_ID,
+      code,
+      redirect_uri: config.CLIENT_CALLBACK_URL,
+    }),
+  })
+}
+
+export const authInfoParse = (res: {
+  json?: {
+    id_token?: string
+    access_token?: string
+  }
+}) => {
+  /*
+  Description:
+    Helper function that takes an Auth0 response and extracts a {email, sub, accessToken, refreshToken} object
+  Arguments:
+    Response (Record): Auth0 response object
+  Returns:
+    {email, sub, accessToken, refreshToken, subscriptionStatus}
+  */
+
+  try {
+    const accessToken = res?.json?.access_token
+    const decodedAccessToken = jwtDecode(accessToken ?? "") as any
+    const decodedIdToken = jwtDecode(res?.json?.id_token ?? "") as any
+    const userEmail = decodedIdToken?.email
+    const subscriptionStatus =
+      decodedAccessToken["https://api.fractal.co/subscription_status"]
+
+    if (typeof accessToken !== "string")
+      return {
+        error: {
+          message: "Response does not have .json.access_token",
+          data: res,
+        },
+      }
+    if (typeof userEmail !== "string")
+      return {
+        error: {
+          message: "Decoded JWT does not have property .email",
+          data: res,
+        },
+      }
+    return { userEmail, accessToken, subscriptionStatus }
+  } catch (err) {
+    return {
+      error: {
+        message: `Error while decoding JWT: ${err as string}`,
+        data: res,
+      },
+    }
+  }
+}
+
+export const generateRandomConfigToken = () => {
+  /*
+  Description:
+    Generates a config token for app config security
+  Returns:
+    string: Config token
+  */
+
+  const buffer = randomBytes(48)
+  return buffer.toString("base64")
+}
+
+export const isTokenExpired = (accessToken: string) => {
+  // Extract the expiry in seconds since epoch
+  try {
+    const profile: { exp: number } = jwtDecode(accessToken)
+    // Get current time in seconds since epoch
+    const currentTime = Date.now() / 1000
+    // Allow for ten seconds so we don't compare the access token to the current time right
+    // before the expiry
+    const secondsBuffer = 10
+    return currentTime + secondsBuffer > profile.exp
+  } catch (err) {
+    console.error(`Failed to decode access token: ${err}`)
+    return true
+  }
+}
+
+export const authInfoRefreshRequest = async (refreshToken: string) => {
+  /*
+  Description:
+    Given a valid Auth0 refresh token, generates a new {email, accessToken, refreshToken} object
+  Arguments:
+    refreshToken (string): Refresh token
+  Returns:
+    {email, accessToken, refreshToken}
+  */
+  return await fetch(`https://${config.AUTH_DOMAIN_URL}/oauth/token`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      grant_type: "refresh_token",
+      client_id: config.AUTH_CLIENT_ID,
+      refresh_token: refreshToken,
+    }),
+  })
+}
+
+export const refreshTokensIfNeeded = async () => {
+  const _authInfo = ((await getStorage(CACHED_AUTH_INFO)) as any) ?? {}
+  const authInfo = JSON.parse(JSON.stringify(_authInfo))
+  const isExpired = isTokenExpired(authInfo?.accessToken ?? "")
+
+  if (isExpired && authInfo?.refreshToken !== undefined) {
+    const response = await authInfoRefreshRequest(authInfo.refreshToken)
+    const json = await response.json()
+    const refreshedTokens = authInfoParse(json)
+    setStorage(
+      CACHED_AUTH_INFO,
+      JSON.stringify({
+        userEmail: refreshedTokens.userEmail,
+        accessToken: refreshedTokens.accessToken,
+        refreshToken: authInfo?.refreshToken,
+      })
+    )
+  }
+}
