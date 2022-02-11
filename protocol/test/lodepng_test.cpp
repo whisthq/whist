d6f6c69c094d6d652c26b9c3d5fdb2e514bdec8b
Roshan Padaki <roshan@whist.com>
2022-02-11 17:13:18 -0500

    disable warnings

--- protocol/test/lodepng_test.cpp
+++ protocol/test/lodepng_test.cpp
@@ -170,6 +170,11 @@ git difftool -y
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#pragma warning(disable : 4334) /* WHIST: bit shift warning */
+#pragma warning(disable : 4267) /* WHIST: size_t to unsigned conversion */
+#endif
+
 class LodePNGTest : public CaptureStdoutFixture {};
 
 ////////////////////////////////////////////////////////////////////////////////
