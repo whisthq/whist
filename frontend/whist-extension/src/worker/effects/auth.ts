b37298144800dfa143869f1d6036b7ee5c7645e7
Ming Ying <ming@whist.com>
2022-02-14 19:41:56 +0100

    wip

--- frontend/whist-extension/src/worker/effects/auth.ts
+++ frontend/whist-extension/src/worker/effects/auth.ts
@@ -1,15 +1,16 @@
 import { merge } from "rxjs"
+import { switchMap } from "rxjs/operators"
 
 import { openGoogleAuth, shouldAuthenticate } from "@app/worker/events/auth"
 import { storageDidChange } from "@app/worker/events/storage"
 
 import { googleAuth, authenticate } from "@app/worker/flows/auth"
-import { createLoggedInTab } from "@app/worker/utils/tabs"
+import { createEvent } from "@app/worker/utils/events"
 
 // Checks to see if the user is authenticated
 merge(shouldAuthenticate, storageDidChange).subscribe(async () => {
   const authenticated = await authenticate()
-  if (authenticated) createLoggedInTab()
+  if (authenticated) createEvent("authenticated")
 })
 // Opens the Google Auth popup
 openGoogleAuth.subscribe(() => {
