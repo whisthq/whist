dddccbe2e4d5404c7419cf038bd0fa0e4d046336
Serina <serina@whist.com>
2022-02-26 21:17:07 +0000

    Add input mode switching for anthy

--- protocol/whist/utils/os_utils.c
+++ protocol/whist/utils/os_utils.c
@@ -27,43 +27,50 @@ Defines
 ============================
 */
 
-#define WHIST_KB_DEFAULT_LAYOUT "us"
+#define WHIST_KB_DEFAULT_LAYOUT "xkb:us::eng"
 
 #ifdef __APPLE__
+
+#define MAX_APPLE_KB_NAME_LENGTH 75
 // If this array gets any longer than 1000 or so,
 // we should probably swap to a C++ std::map<string, string>
+
+struct AppleKeyboardMapping {
+    char apple_name[MAX_APPLE_KB_NAME_LENGTH];
+    WhistKeyboardLayout mapped_layout;
+}
 
 // To add a new keyboard layout, run ibus list-engine in a mandelbox and find the appropriate one
 // for foreign characters, use https://wiki.archlinux.org/title/Input_method or something similar
 // to locate a packageand sudo apt install METHOD in the Dockerfile, then add the ibus engine to
 // this mapping
-static const char *const apple_keyboard_mappings[][2] = {
-    {"com.apple.keylayout.USExtended", "xkb:us::eng"},
-    {"com.apple.keylayout.US", "xkb:us::eng"},
-    {"com.apple.keylayout.Italian-Pro", "xkb:it::ita"},
-    {"com.apple.keylayout.Italian", "xkb:it::ita"},
-    {"com.apple.keylayout.Arabic", "xkb:ara::ara"},
-    {"com.apple.keylayout.ABC-QWERTZ", "xkb:de:nodeadkeys:ger"},
-    {"com.apple.keylayout.German", "xkb:de::ger"},
-    {"com.apple.keylayout.Canadian-CSA", "xkb:ca:eng:eng"},
-    {"com.apple.keylayout.ABC-AZERTY", "xkb:fr::fra"},
-    {"com.apple.keylayout.French", "xkb:fr::fra"},
-    {"com.apple.keylayout.SwissFrench", "xkb:ch:fr:fra"},
-    {"com.apple.keylayout.LatinAmerican", "xkb:latam::spa"},
-    {"com.apple.keylayout.Spanish", "xkb:es::spa"},
-    {"com.apple.keylayout.Hebrew", "xkb:il::heb"},
-    {"com.apple.keylayout.Canadian", "xkb:ca:eng:eng"},
-    {"com.apple.keylayout.DVORAK-QWERTYCMD", "xkb:us:dvorak:eng"},
-    {"com.apple.keylayout.ABC-India", "xkb:us:intl:eng"},
-    {"com.apple.keylayout.Dvorak", "xkb:us:dvorak:eng"},
-    {"com.apple.keylayout.British", "xkb:gb:extd:eng"},
-    {"com.apple.keylayout.ABC", "xkb:us:intl:eng"},
-    {"com.apple.inputmethod.SCIM.ITABC", "pinyin"},
-    {"com.sogou.inputmethod.sogou.pinyin", "pinyin"},
-    {"com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese", "anthy"},
-    {"com.apple.inputmethod.Kotoeri.RomajiTyping.Roman", "anthy"},
-    {"com.apple.keylayout.Vietnamese", "unikey"},
-    {"com.apple.inputmethod.Korean.2SetKorean", "hangul"},
+static AppleKeyboardMapping apple_keyboard_mappings[] = {
+    {"com.apple.keylayout.USExtended", {"xkb:us::eng", ""}},
+    {"com.apple.keylayout.US", {"xkb:us::eng", ""}},
+    {"com.apple.keylayout.Italian-Pro", {"xkb:it::ita", ""}},
+    {"com.apple.keylayout.Italian", {"xkb:it::ita", ""}},
+    {"com.apple.keylayout.Arabic", {"xkb:ara::ara", ""}},
+    {"com.apple.keylayout.ABC-QWERTZ", {"xkb:de:nodeadkeys:ger", ""}},
+    {"com.apple.keylayout.German", {"xkb:de::ger", ""}},
+    {"com.apple.keylayout.Canadian-CSA", {"xkb:ca:eng:eng", ""}},
+    {"com.apple.keylayout.ABC-AZERTY", {"xkb:fr::fra", ""}},
+    {"com.apple.keylayout.French", {"xkb:fr::fra", ""}},
+    {"com.apple.keylayout.SwissFrench", {"xkb:ch:fr:fra", ""}},
+    {"com.apple.keylayout.LatinAmerican", {"xkb:latam::spa", ""}},
+    {"com.apple.keylayout.Spanish", {"xkb:es::spa", ""}},
+    {"com.apple.keylayout.Hebrew", {"xkb:il::heb", ""}},
+    {"com.apple.keylayout.Canadian", {"xkb:ca:eng:eng", ""}},
+    {"com.apple.keylayout.DVORAK-QWERTYCMD", {"xkb:us:dvorak:eng", ""}},
+    {"com.apple.keylayout.ABC-India", {"xkb:us:intl:eng", ""}},
+    {"com.apple.keylayout.Dvorak", {"xkb:us:dvorak:eng", ""}},
+    {"com.apple.keylayout.British", {"xkb:gb:extd:eng", ""}},
+    {"com.apple.keylayout.ABC", {"xkb:us:intl:eng", ""}},
+    {"com.apple.inputmethod.SCIM.ITABC", {"pinyin", ""}},
+    {"com.sogou.inputmethod.sogou.pinyin", {"pinyin", ""}},
+    {"com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese", {"anthy", "gsetting set org.freedesktop.ibus.engine.anthy.common input-mode 0"}},
+    {"com.apple.inputmethod.Kotoeri.RomajiTyping.Roman", {"anthy",  "gsetting set org.freedesktop.ibus.engine.anthy.common input-mode 3"}},
+    {"com.apple.keylayout.Vietnamese", {"unikey", ""}},
+    {"com.apple.inputmethod.Korean.2SetKorean", {"hangul", ""}},
 };
 
 #define NUM_APPLE_KEYBOARD_MAPPINGS \
@@ -101,6 +108,7 @@ WhistKeyboardLayout get_keyboard_layout(void) {
         // Use the default layout if we can't get the layout
         safe_strncpy(whist_layout.layout_name, WHIST_KB_DEFAULT_LAYOUT,
                      sizeof(whist_layout.layout_name));
+        safe_strncpy(whist_layout.additional_command, "", sizeof(whist_layout.additional_command));
         return whist_layout;
     }
     // if the layout hasn't changed, just return our old whist_layout
@@ -111,10 +119,9 @@ WhistKeyboardLayout get_keyboard_layout(void) {
     // otherwise, look for the keyboard layout and set whist_layout
     bool found = false;
     for (int i = 0; i < NUM_APPLE_KEYBOARD_MAPPINGS; i++) {
-        if (strncmp(apple_keyboard_mappings[i][0], layout, sizeof(layout)) == 0) {
-            // Copy new name
-            safe_strncpy(whist_layout.layout_name, apple_keyboard_mappings[i][1],
-                         sizeof(whist_layout.layout_name));
+        if (strncmp(apple_keyboard_mappings[i].apple_name, layout, sizeof(layout)) == 0) {
+            // memcpy the discovered layout to whist_layout
+            memcpy(&whist_layout, &apple_keyboard_mappings[i].mapped_layout, sizeof(whist_layout));
             LOG_INFO("Recognized keyboard layout %s", whist_layout.layout_name);
             found = true;
             break;
@@ -127,6 +134,7 @@ WhistKeyboardLayout get_keyboard_layout(void) {
         // so we can add support for it if we see usage of it
         safe_strncpy(whist_layout.layout_name, WHIST_KB_DEFAULT_LAYOUT,
                      sizeof(whist_layout.layout_name));
+        safe_strncpy(whist_layout.additional_command, "", sizeof(whist_layout.additional_command));
         LOG_ERROR("Mac Keyboard Layout %s was not recognized! Defaulting to %s", layout,
                   WHIST_KB_DEFAULT_LAYOUT);
     }
@@ -191,6 +199,7 @@ WhistKeyboardLayout get_keyboard_layout(void) {
 }
 
 void set_keyboard_layout(WhistKeyboardLayout requested_layout) {
+    static WhistKeyboardLayout current_layout = {WHIST_KB_DEFAULT_LAYOUT, ""};
     static char current_layout_name[WHIST_KB_LAYOUT_NAME_MAX_LENGTH] = WHIST_KB_DEFAULT_LAYOUT;
 
     if (requested_layout.layout_name[WHIST_KB_LAYOUT_NAME_MAX_LENGTH - 1] != '\0') {
@@ -199,21 +208,40 @@ void set_keyboard_layout(WhistKeyboardLayout requested_layout) {
     }
 
     // Don't set the keyboard if nothing changed
-    if (memcmp(current_layout_name, requested_layout.layout_name, sizeof(current_layout_name)) ==
-        0) {
+    if (memcmp(&current_layout, &requested_layout, sizeof(WhistKeyboardLayout)) == 0) {
         return;
     }
 
     // Otherwise, copy into current_layout_name and handle the new current_layout_name
-    memcpy(current_layout_name, requested_layout.layout_name, sizeof(current_layout_name));
+    memcpy(&current_layout, &requested_layout, sizeof(WhistKeyboardLayout));
+    LOG_INFO("Current layout: %s; Additional Command: %s", current_layout.layout_name, current_layout.additional_command);
 
 #ifdef __linux__
     char cmd_buf[1024];
-    int bytes_written =
+    int bytes_written;
+    // first, run the additional command
+    if (strlen(current_layout.additional_command) != 0) {
+        bytes_written = snprintf(cmd_buf, sizeof(cmd_buf), "/usr/share/whist/run-as-whist-user.sh %s",
+                     current_layout.additional_command);
+
+        if (bytes_written >= 0) {
+            LOG_INFO("Running %s", cmd_buf);
+            runcmd(cmd_buf, NULL);
+        }
+    // then, restart ibus
+        bytes_written = snprintf(cmd_buf, sizeof(cmd_buf), "/usr/share/whist/run-as-whist-user.sh 'ibus restart'");
+        if (bytes_written >= 0) {
+            LOG_INFO("Running %s", cmd_buf);
+            runcmd(cmd_buf, NULL);
+        }
+    }
+    // then, change the engine
+    bytes_written =
         snprintf(cmd_buf, sizeof(cmd_buf), "/usr/share/whist/run-as-whist-user.sh 'ibus engine %s'",
-                 current_layout_name);
+                 current_layout.layout_name);
 
     if (bytes_written >= 0) {
+        LOG_INFO("Running %s", cmd_buf);
         runcmd(cmd_buf, NULL);
     }
 #else
