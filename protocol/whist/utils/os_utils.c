7bbe92fd8b0b253f0defaec73ae9c66d6f5393b3
Philippe Noel <phil@whist.com>
2022-02-28 11:15:25 -0500

    remove unused variable

--- protocol/whist/utils/os_utils.c
+++ protocol/whist/utils/os_utils.c
@@ -208,7 +208,6 @@ WhistKeyboardLayout get_keyboard_layout(void) {
 
 void set_keyboard_layout(WhistKeyboardLayout requested_layout) {
     static WhistKeyboardLayout current_layout = {WHIST_KB_DEFAULT_LAYOUT, ""};
-    static char current_layout_name[WHIST_KB_LAYOUT_NAME_MAX_LENGTH] = WHIST_KB_DEFAULT_LAYOUT;
 
     if (requested_layout.layout_name[WHIST_KB_LAYOUT_NAME_MAX_LENGTH - 1] != '\0') {
         LOG_ERROR("Could not set layout name! last character was not NULL!");
@@ -220,7 +219,7 @@ void set_keyboard_layout(WhistKeyboardLayout requested_layout) {
         return;
     }
 
-    // Otherwise, copy into current_layout_name and handle the new current_layout_name
+    // Otherwise, copy into current_layout and handle the new current_layout
     memcpy(&current_layout, &requested_layout, sizeof(WhistKeyboardLayout));
     LOG_INFO("Current layout: %s; Additional Command: %s", current_layout.layout_name,
              current_layout.additional_command);
