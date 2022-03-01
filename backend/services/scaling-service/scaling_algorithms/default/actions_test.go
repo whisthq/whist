055144843bcae9ee963470334965bfca9303f283
Mauricio Araujo <mauricio@whist.com>
2022-02-28 18:13:28 -0600

    Add mock db client on actions test

--- backend/services/scaling-service/scaling_algorithms/default/actions_test.go
+++ backend/services/scaling-service/scaling_algorithms/default/actions_test.go
@@ -1,8 +1,67 @@
 package scaling_algorithms
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestVerifyInstanceScaleDown(t *testing.T) {}
+	"github.com/whisthq/whist/backend/services/scaling-service/dbclient"
+	"github.com/whisthq/whist/backend/services/subscriptions"
+)
+
+type mockDBClient struct{}
+
+func (db *mockDBClient) QueryInstance(context.Context, *subscriptions.GraphQLClient, string) (subscriptions.WhistInstances, error) {
+	return subscriptions.WhistInstances{}, nil
+}
+
+func (db *mockDBClient) QueryInstancesByStatusOnRegion(context.Context, *subscriptions.GraphQLClient, string) (subscriptions.WhistInstances, error) {
+	return subscriptions.WhistInstances{}, nil
+}
+
+func (db *mockDBClient) QueryInstancesByImage(context.Context, *subscriptions.GraphQLClient, string) (subscriptions.WhistInstances, error) {
+	return subscriptions.WhistInstances{}, nil
+}
+
+func (db *mockDBClient) InsertInstances(context.Context, *subscriptions.GraphQLClient, []subscriptions.Instance) (int, error) {
+	return 0, nil
+}
+
+func (db *mockDBClient) UpdateInstance(context.Context, *subscriptions.GraphQLClient, map[string]interface{}) (int, error) {
+	return 0, nil
+}
+
+func (db *mockDBClient) DeleteInstance(context.Context, *subscriptions.GraphQLClient, string) (int, error) {
+	return 0, nil
+}
+
+func (db *mockDBClient) QueryImage(context.Context, *subscriptions.GraphQLClient, string, string) (subscriptions.WhistImages, error) {
+	return subscriptions.WhistImages{}, nil
+}
+
+func (db *mockDBClient) InsertImages(context.Context, *subscriptions.GraphQLClient, []subscriptions.Image) (int, error) {
+	return 0, nil
+}
+
+func (db *mockDBClient) UpdateImage(context.Context, *subscriptions.GraphQLClient, subscriptions.Image) (int, error) {
+	return 0, nil
+}
+
+func TestVerifyInstanceScaleDown(t *testing.T) {
+	context, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	testAlgorithm := &DefaultScalingAlgorithm{
+		Region: "test-region",
+	}
+	testDBClient := &mockDBClient{}
+	testAlgorithm.CreateBuffer()
+	testAlgorithm.CreateDBClient((*dbclient.DBClient)(testDBClient))
+
+	err := testAlgorithm.VerifyCapacity(context, ScalingEvent{Region: "test-region"})
+	if err != nil {
+		t.Errorf("Failed while testing verify capacity action. Err: %v", err)
+	}
+}
 
 func TestVerifyCapacity(t *testing.T) {}
 
