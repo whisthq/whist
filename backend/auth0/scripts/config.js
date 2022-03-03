e7e382bb7ad6bc482ce2a9fe427c9f7010cc0e2c
Roshan Padaki <roshan@whist.com>
2022-03-03 17:41:05 -0500

    fix/clean up auth0 (#5733)
    * move modified file from incorrect push to dev
    
    * scripts
    
    * fix auth
    
    * newline

---
+++ backend/auth0/scripts/config.js
@@ -0,0 +1,43 @@
+const clientIDs = {
+  dev: "6cd4nskyIHQOePVM6q7FU3x7i5sZLwl1",
+  staging: "2VZTg2ZT1DNUr6JquMfeezRXCc8V1nC5",
+  prod: "dnhVmqdHkF1O6aWwakv7jDVMd5Ii6VfX",
+}
+
+const getConfig = (env) => {
+  // List of environment variables that must be defined in order for deploy to succeed.
+  const REQUIRED_ENV_VARS = [
+    "SENDGRID_API_KEY",
+    "GOOGLE_OAUTH_SECRET",
+    "APPLE_OAUTH_SECRET",
+  ]
+
+  REQUIRED_ENV_VARS.forEach((v) => {
+    if (!process.env[v]) {
+      console.error(`Environment variable "${v}" not defined`)
+      process.exit(1)
+    }
+  })
+
+  return {
+    AUTH0_DOMAIN: `fractal-${env}.us.auth0.com`,
+    AUTH0_CLIENT_ID: clientIDs[env],
+    AUTH0_BASE_PATH: "src",
+    // Only auto-delete resources on dev
+    AUTH0_ALLOW_DELETE: env === "dev",
+    AUTH0_KEYWORD_REPLACE_MAPPINGS: {
+      WHIST_ELECTRON_APP_CALLBACK: ["http://localhost/callback"],
+      WHIST_AUTHENTICATION_API: "https://api.fractal.co",
+      SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+      GOOGLE_OAUTH_SECRET: process.env.GOOGLE_OAUTH_SECRET,
+      APPLE_OAUTH_SECRET: process.env.APPLE_OAUTH_SECRET,
+    },
+    EXCLUDED_PROPS: {
+      clients: ["client_secret"],
+    },
+  }
+}
+
+module.exports = {
+  getConfig,
+}
--- backend/auth0/scripts/deploy.js
+++ backend/auth0/scripts/deploy.js
@@ -5,22 +5,17 @@
   yarn deploy:[env]
 */
 
-const { execSync } = require("child_process")
-
-// List of environment variables that must be defined in order for deploy to succeed.
-const REQUIRED_ENV_VARS = ["GOOGLE_OAUTH_SECRET", "APPLE_OAUTH_SECRET"]
-
-REQUIRED_ENV_VARS.forEach((v) => {
-    if (!process.env[v]) {
-        console.log(`Environment variable "${v}" not defined`)
-        process.exit()
-    }
-})
+const { deploy } = require("auth0-deploy-cli")
+const { getConfig } = require("./config")
 
 const env = process.argv[2]
 
 // Deploy to Auth0 tenant
-execSync(
-    `a0deploy import --input_file tenant.yaml --config_file ./config/${env}.json`,
-    { stdio: "inherit" }
-)
+deploy({
+  input_file: "tenant.yaml",
+  config: getConfig(env),
+})
+  .then(() => console.log(`Successfully deployed to Auth0 ${env} tenant!`))
+  .catch((err) =>
+    console.error(`Failed to deploy to Auth0 ${env} tenant. Error: ${err}`)
+  )
