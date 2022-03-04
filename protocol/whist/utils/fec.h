f2eb1a77b95b34cc789a61a9f852f969f15fe26e
wangyu- <yancey@whist.com>
2022-03-04 10:03:42 -0500

    newlines

--- protocol/whist/utils/fec.h
+++ protocol/whist/utils/fec.h
@@ -71,8 +71,8 @@ FECEncoder* create_fec_encoder(int num_real_buffers, int num_fec_buffers, int ma
  *
  * @returns                        The num of real buffers needed
  */
-
 int fec_encoder_get_num_real_buffers(int buffer_size, int real_buffer_size);
+
 /**
  * @brief                          Registers a buffer into the encoder.
  *
@@ -86,7 +86,6 @@ int fec_encoder_get_num_real_buffers(int buffer_size, int real_buffer_size);
  * @note                           The data pointed to by the buffer being passed in, which
  *                                 must be held alive for as long as the fec_encoder is alive.
  */
-
 void fec_encoder_register_buffer(FECEncoder* fec_encoder, void* buffer, int buffer_size);
 
 /**
