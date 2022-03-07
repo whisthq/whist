17cd20d5d7c2ef05d85bfcc49acb8b5bf6f4b348
Philippe Noel <phil@whist.com>
2022-03-07 13:17:16 -0500

    enable huge pages on Linux

--- protocol/whist/core/whist_memory.c
+++ protocol/whist/core/whist_memory.c
@@ -389,7 +389,7 @@ void* allocate_region(size_t region_size) {
     }
     ((RegionHeader*)p)->size = region_size;
 #else
-    void* p = mmap(0, region_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    void* p = mmap(0, region_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
     if (p == NULL) {
         LOG_FATAL("mmap failed!");
     }
