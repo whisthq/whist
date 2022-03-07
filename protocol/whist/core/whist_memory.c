940290e5365bd7c433444a804abdc4941eca6979
Philippe Noel <phil@whist.com>
2022-03-07 13:29:46 -0500

    only enable on Linux

--- protocol/whist/core/whist_memory.c
+++ protocol/whist/core/whist_memory.c
@@ -389,7 +389,11 @@ void* allocate_region(size_t region_size) {
     }
     ((RegionHeader*)p)->size = region_size;
 #else
+#ifdef __APPLE__
+    void* p = mmap(0, region_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#else
     void* p = mmap(0, region_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
+#endif
     if (p == NULL) {
         LOG_FATAL("mmap failed!");
     }
