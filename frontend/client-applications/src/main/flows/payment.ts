2788bb2e1afc5a5986d8e93b18a5326d38acced5
Ming Ying <ming@whist.com>
2022-03-09 16:56:52 -0500

    wip, code not clean bc of issue with fork

--- frontend/client-applications/src/main/flows/payment.ts
+++ frontend/client-applications/src/main/flows/payment.ts
@@ -1,27 +1,54 @@
-import { filter, map } from "rxjs/operators"
+import { merge } from "rxjs"
+import { map, filter, share } from "rxjs/operators"
 import {
   accessToken,
-  subscriptionStatus,
+  refreshToken,
   subscriptionStatusParse,
   hasValidSubscription,
 } from "@whist/core-ts"
 
-import { flow } from "@app/main/utils/flows"
+import { flow, fork } from "@app/main/utils/flows"
+import { authRefreshFlow } from "@app/main/flows/auth"
 
-export default flow<accessToken>("checkPaymentFlow", (trigger) => {
-  const parsed = trigger.pipe(
-    map((x: accessToken) => ({
-      ...x,
-      ...subscriptionStatusParse(x),
-    }))
-  )
+export default flow<accessToken & refreshToken>(
+  "checkPaymentFlow",
+  (trigger) => {
+    const success = trigger.pipe(
+      map((x: accessToken & refreshToken) => ({
+        ...x,
+        ...subscriptionStatusParse(x),
+      })),
+      filter((res) => hasValidSubscription(res))
+    )
 
-  return {
-    success: parsed.pipe(
-      filter((tokens: subscriptionStatus) => hasValidSubscription(tokens))
-    ),
-    failure: parsed.pipe(
-      filter((tokens: subscriptionStatus) => !hasValidSubscription(tokens))
-    ),
+    const failure = trigger.pipe(
+      map((x: accessToken & refreshToken) => ({
+        ...x,
+        ...subscriptionStatusParse(x),
+      })),
+      filter((res) => !hasValidSubscription(res))
+    )
+
+    const refreshed = authRefreshFlow(failure)
+
+    const successAfterRefresh = refreshed.success.pipe(
+      map((x: accessToken & refreshToken) => ({
+        ...x,
+        ...subscriptionStatusParse(x),
+      })),
+      filter((res) => hasValidSubscription(res))
+    )
+
+    const failureAfterRefresh = refreshed.success.pipe(
+      map((x: accessToken & refreshToken) => ({
+        ...x,
+        ...subscriptionStatusParse(x),
+      })),
+      filter((res) => !hasValidSubscription(res))
+    )
+    return {
+      success: merge(success, successAfterRefresh),
+      failure: merge(refreshed.failure, failureAfterRefresh),
+    }
   }
-})
+)
