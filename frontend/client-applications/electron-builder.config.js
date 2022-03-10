2050a80bcee32a41496029d10b2023390f7b558d
Ming Ying <ming@whist.com>
2022-03-10 11:00:19 -0500

    lower electron builder requirement to 10.13

--- frontend/client-applications/electron-builder.config.js
+++ frontend/client-applications/electron-builder.config.js
@@ -55,7 +55,7 @@ const bundleConfig = {
     gatekeeperAssess: false,
     hardenedRuntime: true,
     icon: `build/${iconName}.png`,
-    minimumSystemVersion: "10.15.0",
+    minimumSystemVersion: "10.13.0",
     // For auto-update to work, "zip" format must be present in addition to .dmg
     target: ["dmg", "zip"],
     type: "distribution",
