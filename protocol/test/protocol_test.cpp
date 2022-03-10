cf4b7bfdc80e814c47c13969efb9fc40374d23b2
Philippe Noel <phil@whist.com>
2022-03-10 11:43:30 -0500

    add comment for detect_leak, remove faulty test and experimentation code

--- protocol/test/protocol_test.cpp
+++ protocol/test/protocol_test.cpp
@@ -2118,13 +2118,6 @@ TEST_F(ProtocolTest, ClientParseArgs) {
     EXPECT_EQ(free_parsed_args(), 0);
 }
 
-#ifdef __APPLE__
-TEST_F(ProtocolTest, LeakTest) {
-    void* ptr = malloc(42);
-    EXPECT_TRUE(ptr);
-}
-#endif
-
 /*
 ============================
 Run Tests
