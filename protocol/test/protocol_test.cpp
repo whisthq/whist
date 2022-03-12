053bbda03e41befe813d2a237ac03f9f98b90804
Roshan Padaki <roshan@whist.com>
2022-03-12 10:10:45 -0500

    fix gtest warning about death tests (#5833)

--- protocol/test/protocol_test.cpp
+++ protocol/test/protocol_test.cpp
@@ -2145,5 +2145,6 @@ Run Tests
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    ::testing::GTEST_FLAG(death_test_style) = "threadsafe";
     return RUN_ALL_TESTS();
 }
