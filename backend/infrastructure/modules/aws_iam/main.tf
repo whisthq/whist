72ec68f9dd7e27f1713753f280c062ae584e1795
Philippe Noel <phil@whist.com>
2022-03-14 15:35:30 -0400

    add misisng Terraform permissions to WhistCI

--- backend/infrastructure/modules/aws_iam/main.tf
+++ backend/infrastructure/modules/aws_iam/main.tf
@@ -150,6 +150,8 @@ resource "aws_iam_group_policy_attachment" "CIPolicy" {
     "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
     "arn:aws:iam::aws:policy/IAMFullAccess",
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSecretsManagerReadWrite",
   ]) : []
   policy_arn = each.value
 }
