4d534b7d65f308cca54f914dd938d81027ee2ebc
Ming Ying <ming@whist.com>
2022-03-14 13:54:12 -0400

    another attempt at redirecting chrome://welcome

--- mandelboxes/browsers/chrome/teleport-extension/src/worker/navigation.ts
+++ mandelboxes/browsers/chrome/teleport-extension/src/worker/navigation.ts
@@ -1,10 +1,25 @@
 import { WELCOME_URL } from "@app/constants/pages"
 
+const redirectIfWelcome = (tab: chrome.tabs.Tab) => {
+  if (tab.url !== "chrome://welcome/" && tab.pendingUrl !== "chrome://welcome/")
+    return
+
+  if (tab.id === undefined) {
+    chrome.tabs.update({ url: WELCOME_URL })
+  } else {
+    chrome.tabs.update(tab.id, { url: WELCOME_URL })
+  }
+}
+
 const initChromeWelcomeRedirect = () => {
-  chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-    if (details.url === "chrome://welcome/") {
-      chrome.tabs.update({ url: WELCOME_URL })
-    }
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    tabs.forEach((tab) => {
+      redirectIfWelcome(tab)
+    })
+  })
+
+  chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
+    redirectIfWelcome(tab)
   })
 }
 
