36d3a8d7ba2c4a26226c5531934db4086626a1d6
Philippe Noel <phil@whist.com>
2022-03-19 15:50:48 -0400

    it's not pretty, but it works

--- frontend/core-ts/src/config/index.ts
+++ frontend/core-ts/src/config/index.ts
@@ -2,54 +2,68 @@
     All environment variables.
 */
 const configs = {
-  local: {
+  LOCAL: {
     urls: {
       WEBSERVER_URL: "https://dev-server.whist.com",
       CLIENT_CALLBACK_URL: "http://localhost/callback",
     },
     auth0: {
-      auth0Domain: "fractal-dev.us.auth0.com",
-      clientId: "F4M4J6VjbXlT2UzCfnxFaJK2yKJXbxcF", // public; not a secret
-      apiIdentifier: "https://api.fractal.co",
+      AUTH_DOMAIN_URL: "fractal-dev.us.auth0.com",
+      AUTH_CLIENT_ID: "F4M4J6VjbXlT2UzCfnxFaJK2yKJXbxcF", // public; not a secret
+      AUTH_API_IDENTIFIER: "https://api.fractal.co",
     },
   },
-  dev: {
+  DEVELOPMENT: {
     urls: {
       WEBSERVER_URL: "https://dev-server.whist.com",
       CLIENT_CALLBACK_URL: "http://localhost/callback",
     },
     auth0: {
-      auth0Domain: "fractal-dev.us.auth0.com",
-      clientId: "F4M4J6VjbXlT2UzCfnxFaJK2yKJXbxcF", // public; not a secret
-      apiIdentifier: "https://api.fractal.co",
+      AUTH_DOMAIN_URL: "fractal-dev.us.auth0.com",
+      AUTH_CLIENT_ID: "F4M4J6VjbXlT2UzCfnxFaJK2yKJXbxcF", // public; not a secret
+      AUTH_API_IDENTIFIER: "https://api.fractal.co",
     },
   },
-  staging: {
+  STAGING: {
     urls: {
       WEBSERVER_URL: "https://staging-server.whist.com",
       CLIENT_CALLBACK_URL: "http://localhost/callback",
     },
     auth0: {
-      auth0Domain: "fractal-staging.us.auth0.com",
-      clientId: "WXO2cphPECuDc7DkDQeuQzYUtCR3ehjz", // public; not a secret
-      apiIdentifier: "https://api.fractal.co",
+      AUTH_DOMAIN_URL: "fractal-staging.us.auth0.com",
+      AUTH_CLIENT_ID: "WXO2cphPECuDc7DkDQeuQzYUtCR3ehjz", // public; not a secret
+      AUTH_API_IDENTIFIER: "https://api.fractal.co",
     },
   },
-  prod: {
+  PRODUCTION: {
     urls: {
       WEBSERVER_URL: "https://prod-server.whist.com",
       CLIENT_CALLBACK_URL: "http://localhost/callback",
     },
     auth0: {
-      auth0Domain: "auth.whist.com",
-      clientId: "Ulk5B2RfB7mM8BVjA3JtkrZT7HhWIBLD", // public; not a secret
-      apiIdentifier: "https://api.fractal.co",
+      AUTH_DOMAIN_URL: "auth.whist.com",
+      AUTH_CLIENT_ID: "Ulk5B2RfB7mM8BVjA3JtkrZT7HhWIBLD", // public; not a secret
+      AUTH_API_IDENTIFIER: "https://api.fractal.co",
     },
   },
 }
 
 export const config = (() => {
   // This should be local, dev, staging or prod
-  const environment = process.env.ENVIRONMENT ?? "local"
-  return configs[environment]
+  const devEnv = process.env.DEVELOPMENT_ENV as string ?? "local"
+  switch (devEnv) {
+    case "local":
+      return configs.LOCAL
+    case "dev":
+      return configs.DEVELOPMENT
+    case "staging":
+      return configs.STAGING
+    case "prod":
+      return configs.PRODUCTION
+    default:
+      console.warn(
+        `Got an unrecognized DEVELOPMENT_ENV: ${devEnv}. Defaulting to "local"}`
+      )
+      return configs.LOCAL
+  }
 })()
