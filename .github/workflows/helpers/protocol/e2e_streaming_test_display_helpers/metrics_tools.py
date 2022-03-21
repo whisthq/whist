1ed2f2f186d04e64b6025758feb245571b200e62
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-03-21 17:12:54 -0400

    minor refactor to display scripts

--- .github/workflows/helpers/protocol/e2e_streaming_test_display_helpers/metrics_tools.py
+++ .github/workflows/helpers/protocol/e2e_streaming_test_display_helpers/metrics_tools.py
@@ -2,10 +2,7 @@
 
 import os
 import sys
-import time
 import numpy as np
-import math
-from datetime import datetime, timedelta
 
 # add the current directory to the path no matter where this is called from
 sys.path.append(os.path.join(os.getcwd(), os.path.dirname(__file__), "."))
