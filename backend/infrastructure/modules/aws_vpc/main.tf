cd09f13ac260c78e7a457ce21075f3c42f10dded
Mauricio <mauricio@whist.com>
2022-03-24 16:04:45 -0600

    Remove MandelboxesDevelopmentGroup and add SSH rule only for dev and staging

--- backend/infrastructure/modules/aws_vpc/main.tf
+++ backend/infrastructure/modules/aws_vpc/main.tf
@@ -109,6 +109,16 @@ resource "aws_security_group" "MandelboxesSecurityGroup" {
     }
   }
 
+  # Allow incoming SSH traffic on dev and staging
+  dynamic "ingress" {
+    for_each = var.env != "prod" ? [1] : []
+    description = "whist-ssh-rule"
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow all outgoing IPv4 traffic
   egress {
     description      = "whist-ipv4-rule"
@@ -121,66 +131,6 @@ resource "aws_security_group" "MandelboxesSecurityGroup" {
 
   tags = {
     Name      = "MandelboxesSecurityGroup${var.env}"
-    Env       = var.env
-    Terraform = true
-  }
-}
-
-# This security group is used for development instances, so it will
-# only exist on the dev environment.
-resource "aws_security_group" "MandelboxesDevelopmentGroup" {
-  count       = var.env == "dev" ? 1 : 0
-  name        = "MandelboxesDevelopmentGroup"
-  description = "This security group is used by WhistEngineers development instances. It opens the necessary ports for developing Whist, such as SSH, and ports assigned to mandelboxes."
-  vpc_id      = aws_vpc.MainVPC.id
-
-  # Port ranges that can be allocated to mandelboxes by Docker
-  ingress {
-    description = "whist-udp-rule"
-    protocol    = "udp"
-    from_port   = 1025
-    to_port     = 49150
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "whist-tcp-rule"
-    protocol    = "tcp"
-    from_port   = 1025
-    to_port     = 49150
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow incoming SSH traffic
-  ingress {
-    description = "whist-ssh-rule"
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow incoming HTTP traffic
-  ingress {
-    description = "whist-http-rule"
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outgoing IPv4 traffic
-  egress {
-    description      = "whist-ipv4-rule"
-    protocol         = "-1"
-    to_port          = 0
-    from_port        = 0
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name      = "MandelboxesDevelopmentGroup"
     Env       = var.env
     Terraform = true
   }
