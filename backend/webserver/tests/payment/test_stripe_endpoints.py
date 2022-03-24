9fc819f32a6e904947af3302423622c825a8d222
Ming Ying <ming@whist.com>
2022-03-24 14:46:59 -0400

    add missing comma

--- backend/webserver/tests/payment/test_stripe_endpoints.py
+++ backend/webserver/tests/payment/test_stripe_endpoints.py
@@ -58,10 +58,7 @@ def test_create_checkout_session(
     assert response.json == {"url": url}
 
 
-@pytest.mark.parametrize(
-    "subscription_status",
-    ("active"),
-)
+@pytest.mark.parametrize("subscription_status", ("active",))
 def test_create_billing_portal_session(
     client: WhistAPITestClient,
     make_user: Callable[..., str],
