54e38fb0eb3ac010219e22335807323d485f5b43
Philippe Noel <phil@whist.com>
2022-03-25 11:52:15 -0400

    port changes from rebase

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/index.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/index.ts
@@ -1,14 +1,21 @@
 import { initFileSyncHandler } from "./downloads"
-import { initTabDetachSuppressor } from "./tabs"
 import { initChromeWelcomeRedirect } from "./navigation"
 import { initNativeHostIpc } from "./ipc"
 import { initCursorLockHandler } from "./cursor"
 import { refreshExtension } from "./update"
+import {
+  initTabDetachSuppressor,
+  initTabCreationHandler,
+  initTabSwitchingHandler,
+} from "./tabs"
 
 const nativeHostPort = initNativeHostIpc()
+
 // If this is a new mandelbox, refresh the extension to get the latest version.
 refreshExtension(nativeHostPort)
 initFileSyncHandler(nativeHostPort)
 initCursorLockHandler(nativeHostPort)
 initTabDetachSuppressor()
+initTabCreationHandler()
+initTabSwitchingHandler()
 initChromeWelcomeRedirect()
--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/tabs.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/tabs.ts
@@ -23,10 +23,80 @@ const tryRestoreTabLocation = async (
   }
 }
 
+// Switch focused tab to match the requested URL
+const createNewTab = async (tabId: number, newtabUrl: string) => {
+  try {
+    await chrome.tabs.create({
+      openerTabId: tabId,
+      active: true, // Switch to the new tab
+      url: newtabUrl,
+    })
+  } catch (err) {
+    if (
+      err ==
+      "Error: Failed to create and switch to the new tab for the provided URL."
+    ) {
+      await new Promise<void>((resolve) =>
+        setTimeout(() => {
+          createNewTab(tabId, newtabUrl)
+          resolve()
+        }, 50)
+      )
+    }
+  }
+}
+
+// Switch currently-active tab to match the requested tab
+const switchActiveTab = async (tabId: number, tabToFocusUrl: string) => {
+  try {
+    var found: boolean = false
+    var desiredTabId: number = -1
+    // Loop over all tabs to find the one we want to switch to
+    chrome.tabs.query({}, function (tabs: chrome.tabs.Tab[]) {
+      for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i].url.search(tabToFocusUrl) > -1) {
+          found = true
+          desiredTabId = tabs[i].id
+        }
+      }
+      if (found) {
+        chrome.tabs.update(desiredTabId, { active: true })
+      } else {
+        throw "Error: Could not find tab to switch to."
+      }
+    })
+  } catch (err) {
+    if (err == "Error: Failed to switch to the tab for the provided URL.") {
+      await new Promise<void>((resolve) =>
+        setTimeout(() => {
+          switchActiveTab(tabId, tabToFocusUrl)
+          resolve()
+        }, 50)
+      )
+    }
+  }
+}
+
 // Listen for tab drag-out detach events and instantly
 // cancel/undo them.
 const initTabDetachSuppressor = () => {
   chrome.tabs.onDetached.addListener(tryRestoreTabLocation)
 }
 
-export { initTabDetachSuppressor }
+// Listen for tab creation events, create a new tab to
+// the requested URL, and switch focus to it
+const initTabCreationHandler = () => {
+  chrome.tabs.onCreated.addListener(createNewTab)
+}
+
+// Listen for tab switching events and switch the focused
+// tab to the requested URL
+const initTabSwitchingHandler = () => {
+  chrome.tabs.onUpdated.addListener(switchActiveTab)
+}
+
+export {
+  initTabDetachSuppressor,
+  initTabCreationHandler,
+  initTabSwitchingHandler,
+}
