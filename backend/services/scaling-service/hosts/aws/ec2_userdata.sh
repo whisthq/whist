ceba9f69bf66ca1b0e2d4036c7b884b7e0647435
Mauricio <mauricio@whist.com>
2022-03-29 14:09:44 -0600

    Add common steps after setting ephemeral storage

--- backend/services/scaling-service/hosts/aws/ec2_userdata.sh
+++ backend/services/scaling-service/hosts/aws/ec2_userdata.sh
@@ -3,38 +3,12 @@
 # Note: all commands here are run with the `root` user. It is not necessary to use sudo.
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html#user-data-shell-scripts
 if [ "$EUID" -ne 0 ]; then
-  echo "Command should be run as root."
-  exit
+    echo "Command should be run as root."
+    exit
 fi
 
 # Exit on subcommand errors
 set -Eeuo pipefail
-
-# Authenticate to ghcr.io and pull Chrome and Brave images.
-pull_docker_images() {
-  USERDATA_ENV=/usr/share/whist/app_env.env
-
-  # Populate env vars
-  eval "$(cat $USERDATA_ENV)"
-
-  # Login wit docker
-  echo "$GH_PAT" | docker login ghcr.io -u "$GH_USERNAME" --password-stdin
-
-  # Pull Docker images for Chrome and Brave directly to the ephemeral volume
-  pull_image_base_chrome="ghcr.io/whisthq/$GIT_BRANCH/browsers/chrome"
-  pull_image_chrome="$pull_image_base_chrome:$GIT_HASH"
-
-  pull_image_base_brave="ghcr.io/whisthq/$GIT_BRANCH/browsers/brave"
-  pull_image_brave="$pull_image_base_brave:$GIT_HASH"
-
-  docker pull "$pull_image_chrome"
-  docker tag "$pull_image_chrome" "$pull_image_base_chrome:current-build"
-
-  docker pull "$pull_image_brave"
-  docker tag "$pull_image_brave" "$pull_image_base_brave:current-build"
-
-  echo "Finished pulling images"
-}
 
 echo "Whist EC2 userdata started"
 
@@ -45,6 +19,7 @@ cd /home/ubuntu
 EPHEMERAL_DEVICE_PATH=$(nvme list -o json | jq -r '.Devices | map(select(.ModelNumber == "Amazon EC2 NVMe Instance Storage")) | max_by(.PhysicalSize) | .DevicePath')
 EPHEMERAL_FS_PATH=/ephemeral
 MAX_CONCURRENT_DOWNLOADS=8
+USERDATA_ENV=/usr/share/whist/app_env.env
 
 # We use ephemeral storage if it exists on our host instances to avoid needing to warm up the filesystem,
 # speeding up instance launch time. We move the docker data directory to the ephemeral volume, and then
@@ -52,35 +27,54 @@ MAX_CONCURRENT_DOWNLOADS=8
 
 if [ "$EPHEMERAL_DEVICE_PATH" != "null" ]
 then
-  echo "Ephemeral device path found: $EPHEMERAL_DEVICE_PATH"
+    echo "Ephemeral device path found: $EPHEMERAL_DEVICE_PATH"
 
-  mkfs -t ext4 "$EPHEMERAL_DEVICE_PATH"
-  mkdir -p "$EPHEMERAL_FS_PATH"
-  mount "$EPHEMERAL_DEVICE_PATH" "$EPHEMERAL_FS_PATH"
-  echo "Mounted ephemeral storage at $EPHEMERAL_FS_PATH"
+    mkfs -t ext4 "$EPHEMERAL_DEVICE_PATH"
+    mkdir -p "$EPHEMERAL_FS_PATH"
+    mount "$EPHEMERAL_DEVICE_PATH" "$EPHEMERAL_FS_PATH"
+    echo "Mounted ephemeral storage at $EPHEMERAL_FS_PATH"
 
-  # Stop docker and copy the data directory to the ephemeral storage
-  systemctl stop docker
-  mv /var/lib/docker "$EPHEMERAL_FS_PATH"
-  echo "Moved /var/lib/docker to ephemeral volume"
+    # Stop docker and copy the data directory to the ephemeral storage
+    systemctl stop docker
+    mv /var/lib/docker "$EPHEMERAL_FS_PATH"
+    echo "Moved /var/lib/docker to ephemeral volume"
 
-  # Modify configuration to use the new data directory and persist in daemon config file
-  # and start docker again. Set a higher concurrent download count to speed up the pull.
-  jq '. + {"data-root": "'"$EPHEMERAL_FS_PATH/docker"'"}' /etc/docker/daemon.json > tmp.json && mv tmp.json /etc/docker/daemon.json
-  jq '. + {"max-concurrent-downloads": '"$MAX_CONCURRENT_DOWNLOADS"'}' /etc/docker/daemon.json > tmp.json && mv tmp.json /etc/docker/daemon.json
+    # Modify configuration to use the new data directory and persist in daemon config file
+    # and start docker again. Set a higher concurrent download count to speed up the pull.
+    jq '. + {"data-root": "'"$EPHEMERAL_FS_PATH/docker"'"}' /etc/docker/daemon.json > tmp.json && mv tmp.json /etc/docker/daemon.json
+    jq '. + {"max-concurrent-downloads": '"$MAX_CONCURRENT_DOWNLOADS"'}' /etc/docker/daemon.json > tmp.json && mv tmp.json /etc/docker/daemon.json
 
-
-  systemctl start docker
-  pull_docker_images
-
-
+    systemctl start docker
 else
-  echo "No ephemeral device path found. Warming up EBS volume with fio."
-  # Warm Up EBS Volume
-  # For more information, see: https://github.com/whisthq/whist/pull/5333
-  fio --filename=/dev/nvme0n1 --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
-  pull_docker_images
+    echo "No ephemeral device path found. Warming up EBS volume with fio."
+    # Warm Up EBS Volume
+    # For more information, see: https://github.com/whisthq/whist/pull/5333
+    fio --filename=/dev/nvme0n1 --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 fi
+
+# Pull the images regardless if the instance has ephemeral storage or not.
+# This way we avoid ending with an instance that can't launch mandelboxes.
+
+# Populate env vars
+eval "$(cat $USERDATA_ENV)"
+
+# Login wit docker
+echo "$GH_PAT" | docker login ghcr.io -u "$GH_USERNAME" --password-stdin
+
+# Pull Docker images for Chrome and Brave directly to the ephemeral volume
+pull_image_base_chrome="ghcr.io/whisthq/$GIT_BRANCH/browsers/chrome"
+pull_image_chrome="$pull_image_base_chrome:$GIT_HASH"
+
+pull_image_base_brave="ghcr.io/whisthq/$GIT_BRANCH/browsers/brave"
+pull_image_brave="$pull_image_base_brave:$GIT_HASH"
+
+docker pull "$pull_image_chrome"
+docker tag "$pull_image_chrome" "$pull_image_base_chrome:current-build"
+
+docker pull "$pull_image_brave"
+docker tag "$pull_image_brave" "$pull_image_base_brave:current-build"
+
+echo "Finished pulling images"
 
 # The Host Service gets built in the `whist-build-and-deploy.yml` workflow and
 # uploaded from this Git repository to the AMI during Packer via ami_config.json
