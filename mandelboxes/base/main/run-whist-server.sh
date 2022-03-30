119df69c3da0bbcfd007db6b2f7745fe44e7ac38
Philippe Noel <phil@whist.com>
2022-03-30 10:31:58 -0400

    nitpick

--- mandelboxes/base/main/run-whist-server.sh
+++ mandelboxes/base/main/run-whist-server.sh
@@ -128,7 +128,7 @@ if [ "$ENV_NAME" != "localdev" ]; then
   # Make whist-application (i.e. Chrome or Brave) .config foler case-insensitive,
   # since certain Chrome Themes fail to install on Linux otherwise
   #
-  # Note: We only do this for non-local dev environments, since localdev does not
+  # Note: We only do this for non-localdev environments, since localdev does not
   # have the ephemeral filesystem activated, and EBS volumes are case-sensitive
   chattr +F /home/whist/.config
 
