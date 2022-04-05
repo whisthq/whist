d9995cb9570adf1c1ee5a20b93b61cb6a51ab532
Yancey <yancey@whist.com>
2022-04-04 20:12:34 -0400

    fix problem in handshake

--- protocol/whist/network/udp.c
+++ protocol/whist/network/udp.c
@@ -1068,6 +1068,7 @@ bool create_udp_socket_context(SocketContext* network_context, char* destination
     if (destination == NULL) {
         // Create the server context
         ret = create_udp_server_context(context, port, connection_timeout_ms);
+
         if (ret == 0) {
             // On the server, we create a network throttler to limit the
             // outgoing bitrate.
@@ -1344,7 +1345,10 @@ int create_udp_server_context(UDPContext* context, int port, int connection_time
     start_timer(&server_creation_timer);
 
     // Create a new listening socket on that port
-    create_udp_listen_socket(&context->socket, port, connection_timeout_ms);
+    if (create_udp_listen_socket(&context->socket, port, connection_timeout_ms)) {
+        LOG_ERROR("Failed to create UDP listen socket");
+        return -1;
+    }
 
     // While we still have some remaining connection time,
     // wait for a connection attempt message from a client
