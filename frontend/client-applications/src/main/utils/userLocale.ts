24f5b49a8689fc4f81d623df6145769291a8143d
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-04-06 17:01:53 -0400

    added new file

---
+++ frontend/client-applications/src/main/utils/userLocale.ts
@@ -0,0 +1,29 @@
+import { execCommandByOS } from "@app/main/utils/execCommand"
+
+const getUserLocale = () => {
+  const userLocaleRaw = execCommandByOS(
+    "locale",
+    /* eslint-disable no-template-curly-in-string */
+    "locale",
+    "",
+    ".",
+    {},
+    "pipe"
+  )
+  console.log("LOCALE")
+  console.log(userLocaleRaw)
+
+  const parsedUserLocale = userLocaleRaw?.toString()?.split(/\r?\n/) ?? ""
+  const userLocaleDictionary = {}
+  for (const s of parsedUserLocale) {
+    const keyValuePair = s.split(/=/)
+    if (keyValuePair.length === 2) {
+      userLocaleDictionary[keyValuePair[0]] = keyValuePair[1]
+        .split('"')
+        .join("")
+    }
+  }
+  return userLocaleDictionary
+}
+
+export { getUserLocale }
