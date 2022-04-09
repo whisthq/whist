0654641826d60bb0d29fc4d88e3cb41baf7f1167
Ming Ying <ming@whist.com>
2022-04-09 12:08:12 -0400

    more logs

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/zoom.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/zoom.ts
@@ -108,10 +108,57 @@ const handleScrollEvent = () => {
   updateTranslationFromScroll()
 }
 
+const listAllEventListeners = () => {
+  const allElements = Array.prototype.slice.call(document.querySelectorAll("*"))
+  allElements.push(document)
+  allElements.push(window)
+
+  const types = []
+
+  for (let ev in window) {
+    if (/^on/.test(ev)) types[types.length] = ev
+  }
+
+  let elements = []
+  for (let i = 0; i < allElements.length; i++) {
+    const currentElement = allElements[i]
+
+    // Events defined in attributes
+    for (let j = 0; j < types.length; j++) {
+      if (typeof currentElement[types[j]] === "function") {
+        elements.push({
+          node: currentElement,
+          type: types[j],
+          func: currentElement[types[j]].toString(),
+        })
+      }
+    }
+
+    // Events defined with addEventListener
+    if (typeof currentElement._getEventListeners === "function") {
+      let evts = currentElement._getEventListeners()
+      if (Object.keys(evts).length > 0) {
+        for (let evt of Object.keys(evts)) {
+          for (let k = 0; k < evts[evt].length; k++) {
+            elements.push({
+              node: currentElement,
+              type: evt,
+              func: evts[evt][k].listener.toString(),
+            })
+          }
+        }
+      }
+    }
+  }
+
+  return elements.sort()
+}
+
 const initPinchToZoom = () => {
   const canvas = document.getElementsByTagName("canvas") ?? []
 
   console.log("THE CANVASES ARE", canvas)
+  console.log("all event listeners are", listAllEventListeners())
 
   // This browser-hint may be an optimization, but seems to behave poorly on https://maps.google.com and similar.
   // pageElement.style.willChange = 'transform'
