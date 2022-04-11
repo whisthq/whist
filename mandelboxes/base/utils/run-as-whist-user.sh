cdaca373329d49c6250029b5bd9301d3a4beec65
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-04-11 11:55:43 -0400

    fix

--- mandelboxes/base/utils/run-as-whist-user.sh
+++ mandelboxes/base/utils/run-as-whist-user.sh
@@ -158,6 +158,5 @@ exec runuser --login whist --whitelist-environment=TZ,DARK_MODE,RESTORE_LAST_SES
   'DISPLAY=:10 \
     LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/nvidia/lib:/usr/local/nvidia/lib64 \
     LOCAL=yes \
-    ${USER_LOCALE//$"\n"/ } \
     PATH=/usr/local/cuda-11.0/bin:/usr/local/nvidia/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-  DEBIAN_FRONTEND=noninteractive '"$1"
+  DEBIAN_FRONTEND=noninteractive '"${USER_LOCALE//$'\n'/ } " "$1"
