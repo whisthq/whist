9bad859ef2a6ba7c23492d06c1bf6491747ae5c4
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-04-12 15:17:29 -0400

    linting

--- mandelboxes/development/client/simulate_mouse_scrolling.sh
+++ mandelboxes/development/client/simulate_mouse_scrolling.sh
@@ -6,8 +6,8 @@
 set -Eeuo pipefail
 
 if [[ "$#" -ne 1  ||  ! ( "$1" =~ ^[0-9]+$ ) ]]; then
-    echo "Usage: ./simulate_mouse_scrolling.sh <integer number of repeats>"
-    exit 1
+  echo "Usage: ./simulate_mouse_scrolling.sh <integer number of repeats>"
+  exit 1
 fi
 repeats=$1
 
@@ -15,9 +15,9 @@ echo "Simulating the mouse scroll events in the client $repeats times..."
 
 for (( i=1; i<=$repeats; i++ ))
 do
-    sleep 5
-    echo "Mouse scroll events - round $i"
-    python3 /usr/share/whist/mouse_events.py
+  sleep 5
+  echo "Mouse scroll events - round $i"
+  python3 /usr/share/whist/mouse_events.py
 done
 
 echo "Mouse scroll simulation complete."
