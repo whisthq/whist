85b1282b63807d01880cd776cd090a3f3713de49
Ming Ying <ming@whist.com>
2022-04-13 17:26:07 -0400

    wip

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
@@ -4,28 +4,31 @@ import {
 } from "@app/constants/ipc"
 
 let previousOffset = 0
-let hasNavigated = false
+let throttled = false
 
 const navigateOnGesture = (e: WheelEvent) => {
-  if (hasNavigated) {
-    window.removeEventListener("wheel", navigateOnGesture)
-    return
-  }
-
   console.log(e.deltaX)
 
-  if (e.offsetX - previousOffset === 0 && e.deltaX > 50) {
-    hasNavigated = true
-    chrome.runtime.sendMessage(<ContentScriptMessage>{
-      type: ContentScriptMessageType.HISTORY_GO_FORWARD,
-    })
+  if (e.offsetX - previousOffset === 0 && e.deltaX > 50 && !throttled) {
+    throttled = true
+    // chrome.runtime.sendMessage(<ContentScriptMessage>{
+    //   type: ContentScriptMessageType.HISTORY_GO_FORWARD,
+    // })
+
+    setTimeout(() => {
+      throttled = false
+    }, 2000)
   }
 
   if (e.offsetX - previousOffset === 0 && e.deltaX < -50) {
-    hasNavigated = true
-    chrome.runtime.sendMessage(<ContentScriptMessage>{
-      type: ContentScriptMessageType.HISTORY_GO_BACK,
-    })
+    throttled = true
+    // chrome.runtime.sendMessage(<ContentScriptMessage>{
+    //   type: ContentScriptMessageType.HISTORY_GO_BACK,
+    // })
+
+    setTimeout(() => {
+      throttled = false
+    }, 2000)
   }
 
   previousOffset = e.offsetX
