536f2c28fa21a75939c91b354643de269598b3d9
Ming Ying <ming@whist.com>
2022-04-13 16:53:37 -0400

    testing

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
@@ -5,7 +5,7 @@ const wheelHandler = (e: WheelEvent) => {
     "Delta X",
     e.deltaX,
     "Overflowing",
-    e.offsetX - previousOffset === 0
+    e.offsetX - previousOffset === 0 && Math.abs(e.deltaX) > 25
   )
   previousOffset = e.offsetX
 }
