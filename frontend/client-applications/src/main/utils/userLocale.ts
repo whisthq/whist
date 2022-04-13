38ec4d6e80c535ccde56d9cff45639104519d138
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-04-13 10:30:37 -0400

    update and test

--- frontend/client-applications/src/main/utils/userLocale.ts
+++ frontend/client-applications/src/main/utils/userLocale.ts
@@ -2,6 +2,7 @@ import { execCommandByOS } from "@app/main/utils/execCommand"
 
 const getUserLocale = () => {
   const userLocaleRaw = execCommandByOS(
+    //"defaults read -g AppleLocale",
     "locale",
     "locale",
     "",
@@ -13,15 +14,16 @@ const getUserLocale = () => {
   console.log(userLocaleRaw)
 
   const parsedUserLocale = userLocaleRaw?.toString()?.split(/\r?\n/) ?? ""
-  const userLocaleDictionary = {}
+  const userLocaleDictionary: { [key: string]: string } = {}
   for (const s of parsedUserLocale) {
     const keyValuePair = s.split(/=/)
-    if (keyValuePair.length === 2) {
+    if (keyValuePair.length === 2 && keyValuePair[1].length >= 1) {
       userLocaleDictionary[keyValuePair[0]] = keyValuePair[1]
         .split('"')
         .join("")
     }
   }
+  userLocaleDictionary["LANGUAGE"] = "it_IT"
   return userLocaleDictionary
 }
 
