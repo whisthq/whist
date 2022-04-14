e843a77f25bbf903165eaa148b6e6d7eb416b824
Ming Ying <ming@whist.com>
2022-04-14 00:37:38 -0400

    testing my new left scroll algo

--- mandelboxes/browsers/base-chromium/teleport-extension/src/resources/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/resources/overscroll.ts
@@ -10,37 +10,58 @@ let throttled = false
 let previousYDeltas = cyclingArray<{ timestamp: number; delta: number }>(10, [])
 let previousXDeltas = cyclingArray<{ timestamp: number; delta: number }>(10, [])
 
-const detectVerticalOverscroll = (e: WheelEvent) =>
-  e.offsetX - previousOffset === 0 &&
-  Math.abs(previousXDeltas.get().slice(-1).pop()?.delta ?? 0) > 100
+const detectLeftRelease = (args: {
+  offsetX: number
+  d3: number
+  v0: number
+  v1: number
+}) => {
+  const movementX = args.offsetX - previousOffset
+  return movementX === 0 && args.d3 > 150 && args.v0 > 500 && args.v1 > 500
+}
 
 const detectHorizontalScroll = () =>
   previousYDeltas.get().some((args) => Math.abs(args.delta) > 10)
 
 const navigateOnGesture = (e: WheelEvent) => {
-  previousYDeltas.add({ timestamp: Date.now(), delta: e.deltaY })
-  previousXDeltas.add({ timestamp: Date.now(), delta: e.deltaX })
+  previousYDeltas.add({ timestamp: Date.now() / 1000, delta: e.deltaY })
+  previousXDeltas.add({ timestamp: Date.now() / 1000, delta: e.deltaX })
 
-  console.log(e.deltaX)
-  console.log(previousXDeltas.get().slice(-1).pop()?.delta)
+  const d0 = previousXDeltas.get().at(-1)?.delta ?? 0
+  const d1 = previousXDeltas.get().at(-2)?.delta ?? 0
+  const d2 = previousXDeltas.get().at(-3)?.delta ?? 0
+  const d3 = previousXDeltas.get().at(-4)?.delta ?? 0
+  const t0 = previousXDeltas.get().at(-1)?.timestamp ?? 1
+  const t1 = previousXDeltas.get().at(-2)?.timestamp ?? 1
+  const t2 = previousXDeltas.get().at(-3)?.timestamp ?? 1
+  const v0 = (d1 - d0) / (t1 - t0)
+  const v1 = (d2 - d1) / (t2 - t1)
 
-  const gestureDetected =
-    detectVerticalOverscroll(e) && // The user is overscrolling horizontally
+  const filter =
     !detectHorizontalScroll() && // The user isn't scrolling vertically
     !throttled // Ensures we don't fire multiple gesture events in a row
 
-  const leftGestureDetected = gestureDetected && e.deltaX < -100
-  const rightGestureDetected = gestureDetected && e.deltaX > 100
+  const leftGestureDetected =
+    filter &&
+    detectLeftRelease({
+      offsetX: e.offsetX,
+      d3,
+      v0,
+      v1,
+    })
 
   if (leftGestureDetected) drawLeftArrow(document)
-  if (rightGestureDetected) drawRightArrow(document)
-  if (leftGestureDetected || rightGestureDetected) {
+
+  if (leftGestureDetected) {
     throttled = true
-    // chrome.runtime.sendMessage(<ContentScriptMessage>{
-    //   type: leftGestureDetected
-    //     ? ContentScriptMessageType.HISTORY_GO_BACK
-    //     : ContentScriptMessageType.HISTORY_GO_FORWARD,
-    // })
+
+    setTimeout(() => {
+      // chrome.runtime.sendMessage(<ContentScriptMessage>{
+      //   type: leftGestureDetected
+      //     ? ContentScriptMessageType.HISTORY_GO_BACK
+      //     : ContentScriptMessageType.HISTORY_GO_FORWARD,
+      // })
+    }, 1000)
 
     setTimeout(() => {
       throttled = false
