bcea32966ea2e43e82501221fdb7e96e89570006
Ming Ying <ming@whist.com>
2022-04-15 16:49:39 -0400

    toggle speed more

--- mandelboxes/browsers/base-chromium/teleport-extension/src/constants/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/constants/overscroll.ts
@@ -1,4 +1,4 @@
 export const MAXIMUM_X_OVERSCROLL = 600
 export const MINIMUM_X_OVERSCROLL = 4
 export const MAXIMUM_X_UPDATE = 50
-export const MINIMUM_X_UPDATE = 20
+export const MINIMUM_X_UPDATE = 30
