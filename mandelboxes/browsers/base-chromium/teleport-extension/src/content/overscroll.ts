796504fb7fa9bb1829577010d22e38b0ddc22b1d
Ming Ying <ming@whist.com>
2022-04-16 16:17:18 -0400

    throttle remove arrow

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
@@ -6,14 +6,10 @@ import {
   ContentScriptMessageType,
 } from "@app/constants/ipc"
 
-// How many seconds to look back when detecting gestures
-const rollingLookbackPeriod = 1
-
 let arrow: any = undefined
 let previousYDeltas = cyclingArray<number>(4, [])
 let previousXOffset = 0
 let previousArrowDirection: string | undefined = undefined
-let lastTimestamp = 0
 
 const now = () => Date.now() / 1000
 
@@ -48,8 +44,6 @@ const initNavigationArrow = () => {
   chrome.runtime.onMessage.addListener((msg: ContentScriptMessage) => {
     if (msg.type !== ContentScriptMessageType.DRAW_NAVIGATION_ARROW) return
 
-    lastTimestamp = now()
-
     if (!msg.value.draw) {
       removeArrow()
       return
@@ -65,22 +59,9 @@ const initNavigationArrow = () => {
   })
 }
 
-const refreshNavigationArrow = () => {
-  if (now() - lastTimestamp > rollingLookbackPeriod) {
-    chrome.runtime.sendMessage(<ContentScriptMessage>{
-      type: ContentScriptMessageType.GESTURE_DETECTED,
-      value: {
-        reset: true,
-      },
-    })
-  }
-}
-
 const initSwipeGestures = () => {
   // Fires whenever the wheel moves
   window.addEventListener("wheel", detectGesture)
-  // Fires every rollingLookbackPeriod seconds to see if the wheel is still moving
-  setInterval(refreshNavigationArrow, rollingLookbackPeriod)
   // Respond to draw arrow commands from the worker
   initNavigationArrow()
 
--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/overscroll.ts
@@ -13,6 +13,7 @@ import { sameSign, trim } from "@app/utils/operators"
 
 let throttled = false
 let scrollX = 0
+let lastTimestamp = 0
 
 const throttle = (ms: number) => {
   throttled = true
@@ -38,6 +39,7 @@ const updateOverscroll = (deltaX: number, reset = false) => {
 }
 
 const drawArrow = () => {
+  lastTimestamp = Date.now() / 1000
   runInActiveTab((tabID: number) => {
     chrome.tabs.sendMessage(tabID, <ContentScriptMessage>{
       type: ContentScriptMessageType.DRAW_NAVIGATION_ARROW,
@@ -60,7 +62,22 @@ const navigate = () => {
   throttle(500)
 }
 
+const refreshNavigationArrow = () => {
+  if (Date.now() / 1000 - lastTimestamp > 1) {
+    throttle(100)
+    chrome.runtime.sendMessage(<ContentScriptMessage>{
+      type: ContentScriptMessageType.GESTURE_DETECTED,
+      value: {
+        reset: true,
+      },
+    })
+  }
+}
+
 const initGestureHandler = () => {
+  // Fires every rollingLookbackPeriod seconds to see if the wheel is still moving
+  setInterval(refreshNavigationArrow, 1)
+
   chrome.runtime.onMessage.addListener((msg: ContentScriptMessage) => {
     if (msg.type !== ContentScriptMessageType.GESTURE_DETECTED) return
     if (throttled) return
