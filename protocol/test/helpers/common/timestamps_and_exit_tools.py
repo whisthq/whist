e96c79a94ca5c1787e07a0bd4fcd9b6e50ca2e1c
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-04-18 11:42:20 -0400

    fixup

--- protocol/test/helpers/common/timestamps_and_exit_tools.py
+++ protocol/test/helpers/common/timestamps_and_exit_tools.py
@@ -8,38 +8,6 @@ import os
 # Add the current directory to the path no matter where this is called from
 sys.path.append(os.path.join(os.getcwd(), os.path.dirname(__file__), "."))
 
-
-class TimeStamps:
-    def __init__(self):
-        self.start_time = time.time()
-        self.events = []
-        self.max_event_name_len = 0
-
-    def get_time_elapsed(self, index):
-        if index < 0 or index >= len(self.events):
-            return -1
-        return self.events[index][1] - self.events[max(0, index - 1)][1]
-
-    def add_event(self, event_name):
-        timestamp = time.time()
-        self.events.append((event_name, timestamp))
-        self.max_event_name_len = max(self.max_event_name_len, len(event_name))
-        time_elapsed = str(datetime.timedelta(seconds=self.get_time_elapsed(len(self.events) - 1)))
-        print(f"{event_name} took {time_elapsed}")
-
-    def print_timestamps(self):
-        print()
-        print("**** E2E time breakdown ****")
-        for i, event in enumerate(self.events):
-            event_name = event[0]
-            time_elapsed = str(datetime.timedelta(seconds=self.get_time_elapsed(i)))
-            # Add padding for nicer formatting
-            padding_len = self.max_event_name_len - len(event_name)
-            padding = " " * padding_len if padding_len > 0 else ""
-            print(f"{i+1}. {event_name}{padding}:\t{time_elapsed}")
-        print("############################")
-        print()
-
 # Acknowledged to https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal-in-python
 class PrintFormats:
     blue = '\033[94m'
@@ -71,6 +39,38 @@ def print_in_bold(text):
 
 def print_in_underline(text):
     print(f"{PrintFormats.underline}{text}{PrintFormats.ENDC}")
+
+
+class TimeStamps:
+    def __init__(self):
+        self.start_time = time.time()
+        self.events = []
+        self.max_event_name_len = 0
+
+    def get_time_elapsed(self, index):
+        if index < 0 or index >= len(self.events):
+            return -1
+        return self.events[index][1] - self.events[max(0, index - 1)][1]
+
+    def add_event(self, event_name):
+        timestamp = time.time()
+        self.events.append((event_name, timestamp))
+        self.max_event_name_len = max(self.max_event_name_len, len(event_name))
+        time_elapsed = str(datetime.timedelta(seconds=self.get_time_elapsed(len(self.events) - 1)))
+        print(f"{event_name} took {time_elapsed}")
+
+    def print_timestamps(self):
+        print()
+        print("**** E2E time breakdown ****")
+        for i, event in enumerate(self.events):
+            event_name = event[0]
+            time_elapsed = str(datetime.timedelta(seconds=self.get_time_elapsed(i)))
+            # Add padding for nicer formatting
+            padding_len = self.max_event_name_len - len(event_name)
+            padding = " " * padding_len if padding_len > 0 else ""
+            print(f"{i+1}. {event_name}{padding}:\t{time_elapsed}")
+        print("############################")
+        print()
 
 
 def exit_with_error(error_message, timestamps=None):
