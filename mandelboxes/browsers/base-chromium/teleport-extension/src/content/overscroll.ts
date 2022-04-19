7f7cc91c4a526f32dec18846699795ed4aad6840
Ming Ying <ming@whist.com>
2022-04-19 18:20:14 -0400

    test

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
@@ -1,5 +1,5 @@
 // A lower damping value causes the spring to bounce back more quickly
-const damping = 0.85
+const damping = 0.2
 // A higher maxOffset means the page can bounce further
 const maxOffset = 100
 const minDelta = 20
