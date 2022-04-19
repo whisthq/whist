6b42ee35a540a70a47243c7f698dfe7afd123510
Ming Ying <ming@whist.com>
2022-04-19 18:59:54 -0400

    lower dampen and increase throttle

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
@@ -1,8 +1,7 @@
 // A lower damping value causes the spring to bounce back more quickly
-const damping = 0.9
+const damping = 0.8
 // A higher maxOffset means the page can bounce further
 const maxOffset = 100
-const minDelta = 150
 
 // Number of pixels to bounce
 let offset = 0
@@ -28,7 +27,7 @@ const resetFlag = () => {
   clearTimeout(timer)
   timer = setTimeout(() => {
     backFlag = false
-  }, 20)
+  }, 50)
 }
 
 const trim = (update: number) => {
