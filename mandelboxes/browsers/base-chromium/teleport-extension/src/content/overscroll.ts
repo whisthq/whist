35e822ec9db5579e5a1d48e955eb2103aa7f0a5c
Ming Ying <ming@whist.com>
2022-04-19 14:39:44 -0400

    fix margin bug

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/overscroll.ts
@@ -3,41 +3,41 @@ const element =
     ? document.body
     : document.documentElement
 
-let topOverscroll = 0
+let topMargin = 0
 
 const trim = (delta: number) => {
   let abs = Math.abs(delta)
 
-  if (abs > 8) abs = 8
+  if (abs > 5) abs = 5
 
   return delta > 0 ? abs : -1 * abs
 }
 
-const updateTotalMargin = (delta: number) => {
-  const update = topOverscroll + delta
+const updateTopMargin = (delta: number) => {
+  const update = topMargin + delta
   if (update < -50) {
-    topOverscroll = -50
+    topMargin = -50
   } else {
-    topOverscroll = update
+    topMargin = update
   }
 }
 
 const handleYOverscroll = (e: WheelEvent) => {
   const canScroll = element.scrollHeight > element.clientHeight
   const isOverscrollingTop = element.scrollTop === 0 && Math.abs(e.deltaY) > 0
-
-  if (!isOverscrollingTop || !canScroll) {
-    topOverscroll = 0
-    return
-  }
-
   const body = (
     document.getElementsByTagName("BODY") as HTMLCollectionOf<HTMLElement>
   )[0]
 
-  updateTotalMargin(trim(e.deltaY))
+  if (!isOverscrollingTop || !canScroll) {
+    topMargin = 0
+    body.style.marginTop = "0px"
+    return
+  }
 
-  body.style.marginTop = `${Math.abs(topOverscroll)}px`
+  updateTopMargin(trim(e.deltaY))
+
+  body.style.marginTop = `${Math.abs(topMargin)}px`
 }
 
 const initYOverscrollHandler = () => {
