efacf1d5db07afb1dc815c76f462e788d3a1dfe1
Ming <ming@whist.com>
2022-04-21 14:40:45 -0400

    update changes (#6374)

--- frontend/client-applications/src/main/effects/autoupdate.ts
+++ frontend/client-applications/src/main/effects/autoupdate.ts
@@ -11,7 +11,7 @@ import { takeUntil, withLatestFrom } from "rxjs/operators"
 import { ChildProcess } from "child_process"
 import * as Sentry from "@sentry/electron"
 
-import { CHECK_UPDATE_INTERVAL_IN_MS } from "@app/constants/app"
+import { CHECK_UPDATE_INTERVAL_IN_MS, sessionID } from "@app/constants/app"
 
 import { appEnvironment, WhistEnvironments } from "../../../config/configs"
 import { fromTrigger } from "@app/main/utils/flows"
@@ -22,36 +22,44 @@ import { destroyProtocol } from "@app/main/utils/protocol"
 
 // If an update is available, show the update window and download the update
 fromTrigger(WhistTrigger.updateAvailable).subscribe(() => {
-  autoUpdater.downloadUpdate().catch((err) => Sentry.captureException(err))
-})
-
-withAppActivated(timer(0, CHECK_UPDATE_INTERVAL_IN_MS)).subscribe(() => {
-  // We want to manually control when we download the update via autoUpdater.quitAndInstall(),
-  // so we need to set autoDownload = false
-  autoUpdater.autoDownload = false
-  // In dev and staging, the file containing the version is called {channel}-mac.yml, so we need to set the
-  // channel down below. In prod, the file is called latest-mac.yml, which channel defaults to, so
-  // we don't need to set it.
-  switch (appEnvironment) {
-    case WhistEnvironments.PRODUCTION:
-      break
-    case WhistEnvironments.STAGING:
-      autoUpdater.channel = "staging-rc"
-      break
-    case WhistEnvironments.DEVELOPMENT:
-      autoUpdater.channel = "dev-rc"
-      break
-    default:
-      autoUpdater.channel = "dev-rc"
-      break
-  }
-
-  // This is what looks for a latest.yml file in the S3 bucket in electron-builder.config.js,
-  // and fires an update if the current version is less than the version in latest.yml
   autoUpdater
-    .checkForUpdatesAndNotify()
-    .catch((err) => Sentry.captureException(err))
+    .downloadUpdate()
+    .catch((err: Error) =>
+      Sentry.captureMessage(`Error downloading update: ${err.message}`)
+    )
 })
+
+timer(0, CHECK_UPDATE_INTERVAL_IN_MS)
+  .pipe(takeUntil(fromTrigger(WhistTrigger.updateAvailable)))
+  .subscribe(() => {
+    // We want to manually control when we download the update via autoUpdater.quitAndInstall(),
+    // so we need to set autoDownload = false
+    autoUpdater.autoDownload = false
+    // In dev and staging, the file containing the version is called {channel}-mac.yml, so we need to set the
+    // channel down below. In prod, the file is called latest-mac.yml, which channel defaults to, so
+    // we don't need to set it.
+    switch (appEnvironment) {
+      case WhistEnvironments.PRODUCTION:
+        break
+      case WhistEnvironments.STAGING:
+        autoUpdater.channel = "staging-rc"
+        break
+      case WhistEnvironments.DEVELOPMENT:
+        autoUpdater.channel = "dev-rc"
+        break
+      default:
+        autoUpdater.channel = "dev-rc"
+        break
+    }
+
+    // This is what looks for a latest.yml file in the S3 bucket in electron-builder.config.js,
+    // and fires an update if the current version is less than the version in latest.yml
+    autoUpdater
+      .checkForUpdatesAndNotify()
+      .catch((err: Error) =>
+        Sentry.captureMessage(`Error checking for update: ${err.message}`)
+      )
+  })
 
 withAppActivated(
   fromTrigger(WhistTrigger.updateAvailable).pipe(
@@ -67,12 +75,17 @@ withAppActivated(
   })
 })
 
-fromTrigger(WhistTrigger.updateDownloaded)
-  .pipe(
+withAppActivated(
+  fromTrigger(WhistTrigger.updateDownloaded).pipe(
     withLatestFrom(fromTrigger(WhistTrigger.protocol)),
     takeUntil(fromTrigger(WhistTrigger.mandelboxFlowSuccess))
   )
-  .subscribe(([, p]: [any, ChildProcess]) => {
+).subscribe(([, p]: [any, ChildProcess]) => {
+  const msElapsed = Date.now() - sessionID
+  const delay = Math.max(0, 4000 - msElapsed)
+
+  setTimeout(() => {
     destroyProtocol(p)
     autoUpdater.quitAndInstall()
-  })
+  }, delay)
+})
--- frontend/client-applications/src/renderer/pages/update.tsx
+++ frontend/client-applications/src/renderer/pages/update.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Progress } from "@app/components/progress"
+import Logo from "@app/components/icons/logo.svg"
 
 import { useMainState } from "@app/renderer/utils/ipc"
 
@@ -20,26 +21,53 @@ const Update = () => {
 
     if (Number(updateInfo.percent) < percentageDownloaded) return
 
-    setDownloadSpeed(sanitizeBytes(updateInfo.bytesPerSecond))
-    setPercentageDownloaded(Number(updateInfo.percent))
-    setDownloadedSize(sanitizeBytes(updateInfo.transferred))
-    setTotalDownloadSize(sanitizeBytes(updateInfo.total))
+    if (updateInfo.bytesPerSecond !== undefined)
+      setDownloadSpeed(sanitizeBytes(updateInfo.bytesPerSecond))
+    if (updateInfo.percent !== undefined)
+      setPercentageDownloaded(Number(updateInfo.percent))
+    if (updateInfo.transferred !== undefined)
+      setDownloadedSize(sanitizeBytes(updateInfo.transferred))
+    if (updateInfo.total !== undefined)
+      setTotalDownloadSize(sanitizeBytes(updateInfo.total))
   }, [mainState])
 
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-900 h-screen text-center">
+    <div className="flex flex-col h-screen w-full font-body bg-gray-900">
       <div className="absolute top-0 left-0 w-full h-8 draggable"></div>
-      <div className="w-full max-w-xs m-auto font-body">
-        <div className="font-body text-xl font-bold text-gray-300">
-          Please wait while we download the latest version of Whist
+      <div className="w-full text-center pt-16">
+        <img
+          src={Logo}
+          className="w-24 h-24 mx-auto opacity-0 animate-fade-in-up"
+        />
+        <div
+          className="mt-12 text-xl text-gray-300 font-bold opacity-0 animate-fade-in-up"
+          style={{ animationDelay: "800ms" }}
+        >
+          Whist is updating
         </div>
-        <div className="text-sm text-gray-400 mt-1">
+      </div>
+      <div
+        className="mt-4 w-full max-w-xs mx-auto opacity-0 animate-fade-in-up"
+        style={{ animationDelay: "1200ms" }}
+      >
+        <Progress percent={percentageDownloaded} className="h-2" />
+      </div>
+      <div
+        className="w-full text-center opacity-0 animate-fade-in-up"
+        style={{ animationDelay: "1600ms" }}
+      >
+        <div className="text-gray-500 text-sm">
+          {" "}
           Downloaded {downloadedSize.toString()} MB /{" "}
           {totalDownloadSize.toString()} MB at {downloadSpeed.toString()} Mbps
         </div>
-        <div className="mt-6">
-          <Progress percent={percentageDownloaded} className="h-2" />
-        </div>
+      </div>
+      <div
+        className="absolute bottom-4 left-0 right-0 w-full bg-gray-800 bg-opacity-50 px-8 py-4 text-center text-gray-400 text-sm max-w-md rounded m-auto opacity-0 animate-fade-in-up"
+        style={{ animationDelay: "2000ms" }}
+      >
+        Please allow a minute while we download your update. Exciting new
+        changes are ahead!
       </div>
     </div>
   )
