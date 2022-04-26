2f41b70ab4ee756497045065a30569e429cd446b
Karthick Jeyapal <karthick@whist.com>
2022-04-26 08:17:56 +0000

    Fix "The capture session must be recreated" sentry error
    As per the NVFBC API doc,
    NVFBC_ERR_MUST_RECREATE - This indicates that the application must recreate the capture session.
    This error can be returned if a modeset event occurred while capturing frames, and NVFBC_CREATE_- HANDLE_PARAMS::bDisableAutoModesetRecovery was set to NVFBC_TRUE.
    
    7.3.2.2 NVFBC_BOOL _NVFBC_CREATE_CAPTURE_SESSION_- PARAMS::bDisableAutoModesetRecovery
    [in] Whether NvFBC should not attempt to recover from modesets.
    NvFBC is able to detect when a modeset event occured and can automatically re-create a capture session with the same settings as before, then resume its frame capture session transparently.
    This option allows to disable this behavior. NVFBC_ERR_MUST_RECREATE will be returned in that case.
    It can be useful in the cases when an application needs to do some work between setting up a capture and grabbing the first frame.
    
    Since this is not applicable for us, let us set bDisableAutoModesetRecovery to FALSE, for transparent handling of this corner-case issue.

--- protocol/whist/video/capture/nvidiacapture.c
+++ protocol/whist/video/capture/nvidiacapture.c
@@ -156,7 +156,6 @@ NvidiaCaptureDevice* create_nvidia_capture_device(void) {
     create_capture_params.frameSize = frame_size;
     create_capture_params.bRoundFrameSize = NVFBC_TRUE;
     create_capture_params.eTrackingType = NVFBC_TRACKING_DEFAULT;
-    create_capture_params.bDisableAutoModesetRecovery = NVFBC_TRUE;
 
     status = device->p_fbc_fn.nvFBCCreateCaptureSession(device->fbc_handle, &create_capture_params);
     if (status != NVFBC_SUCCESS) {
