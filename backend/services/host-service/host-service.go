b527c8a47c0d18a4fb97119726e9feb626b5d96f
Mauricio Araujo <mauricio@whist.com>
2022-04-27 16:36:41 -0500

    Add create mandelbox function

--- backend/services/host-service/host-service.go
+++ backend/services/host-service/host-service.go
@@ -44,7 +44,9 @@ import (
 	// to import the fmt package either, instead separating required
 	// functionality in this imported package as well.
 
+	"github.com/google/uuid"
 	"github.com/whisthq/whist/backend/services/httputils"
+	"github.com/whisthq/whist/backend/services/types"
 	logger "github.com/whisthq/whist/backend/services/whistlogger"
 
 	"github.com/whisthq/whist/backend/services/host-service/dbdriver"
@@ -129,6 +131,231 @@ func drainAndShutdown(globalCtx context.Context, globalCancel context.CancelFunc
 // ------------------------------------
 // Mandelbox event handlers
 // ------------------------------------
+
+func CreateMandelbox(globalCtx context.Context, globalCancel context.CancelFunc, goroutineTracker *sync.WaitGroup, dockerClient dockerclient.CommonAPIClient, sub *subscriptions.MandelboxEvent, transportRequestMap map[mandelboxtypes.MandelboxID]chan *JSONTransportRequest, transportMapLock *sync.Mutex) {
+	logAndReturnError := func(fmt string, v ...interface{}) {
+		err := utils.MakeError("SpinUpMandelbox(): "+fmt, v...)
+		logger.Error(err)
+		metrics.Increment("ErrorRate")
+	}
+
+	mandelboxID := types.MandelboxID(uuid.New())
+	AppName := "chrome"
+	logger.Infof("SpinUpMandelbox(): spinup started for mandelbox %s", mandelboxID)
+
+	mandelbox := mandelboxData.New(context.Background(), goroutineTracker, mandelboxID)
+	logger.Infof("SpinUpMandelbox(): created Mandelbox object %s", mandelbox.GetID())
+
+	// If the creation of the mandelbox fails, we want to clean up after it. We
+	// do this by setting `createFailed` to true until all steps are done, and
+	// closing the mandelbox's context on function exit if `createFailed` is
+	// still set to true.
+	var createFailed bool = true
+	defer func() {
+		if createFailed {
+			mandelbox.Close()
+		}
+	}()
+
+	// // Begin loading user configs in parallel with the rest of the mandelbox startup procedure.
+	// sendEncryptionInfoChan, configDownloadErrChan := mandelbox.StartLoadingUserConfigs(globalCtx, globalCancel, goroutineTracker)
+	// defer close(sendEncryptionInfoChan)
+
+	// Do all startup tasks that can be done before Docker container creation in
+	// parallel, stopping at the first error encountered
+	preCreateGroup, _ := errgroup.WithContext(mandelbox.GetContext())
+
+	// Request port bindings for the mandelbox.
+	var hostPortForTCP32262, hostPortForUDP32263, hostPortForTCP32273 uint16
+	preCreateGroup.Go(func() error {
+		if err := mandelbox.AssignPortBindings([]portbindings.PortBinding{
+			{MandelboxPort: 32262, HostPort: 0, BindIP: "", Protocol: "tcp"},
+			{MandelboxPort: 32263, HostPort: 0, BindIP: "", Protocol: "udp"},
+			{MandelboxPort: 32273, HostPort: 0, BindIP: "", Protocol: "tcp"},
+		}); err != nil {
+			return utils.MakeError("Error assigning port bindings: %s", err)
+		}
+		logger.Infof("SpinUpMandelbox(): successfully assigned port bindings %v", mandelbox.GetPortBindings())
+
+		var err32262, err32263, err32273 error
+		hostPortForTCP32262, err32262 = mandelbox.GetHostPort(32262, portbindings.TransportProtocolTCP)
+		hostPortForUDP32263, err32263 = mandelbox.GetHostPort(32263, portbindings.TransportProtocolUDP)
+		hostPortForTCP32273, err32273 = mandelbox.GetHostPort(32273, portbindings.TransportProtocolTCP)
+		if err32262 != nil || err32263 != nil || err32273 != nil {
+			return utils.MakeError("Couldn't return host port bindings.")
+		}
+
+		return nil
+	})
+
+	// Initialize Uinput devices for the mandelbox
+	var devices []dockercontainer.DeviceMapping
+	preCreateGroup.Go(func() error {
+		if err := mandelbox.InitializeUinputDevices(goroutineTracker); err != nil {
+			return utils.MakeError("Error initializing uinput devices: %s", err)
+		}
+
+		logger.Infof("SpinUpMandelbox(): successfully initialized uinput devices.")
+		devices = mandelbox.GetDeviceMappings()
+		return nil
+	})
+
+	// Allocate a TTY and GPU for the mandelbox
+	preCreateGroup.Go(func() error {
+		if err := mandelbox.InitializeTTY(); err != nil {
+			return utils.MakeError("Error initializing TTY: %s", err)
+		}
+
+		// CI does not have GPUs
+		if !metadata.IsRunningInCI() {
+			if err := mandelbox.AssignGPU(); err != nil {
+				return utils.MakeError("Error assigning GPU: %s", err)
+			}
+		}
+
+		logger.Infof("SpinUpMandelbox(): successfully allocated TTY %v and assigned GPU %v for mandelbox %s", mandelbox.GetTTY(), mandelbox.GetGPU(), mandelboxID)
+		return nil
+	})
+
+	err := preCreateGroup.Wait()
+	if err != nil {
+		logAndReturnError(err.Error())
+		return
+	}
+
+	// We need to compute the Docker image to use for this mandelbox. In local
+	// development, we want to accept any string so that we can run arbitrary
+	// containers as mandelboxes, including custom-tagged ones. However, in
+	// deployments we only want to accept the specific apps that are enabled, and
+	// don't want to leak the fact that we're using containers as the technology
+	// for mandelboxes, so we construct the image string ourselves.
+	var image string
+	if metadata.IsLocalEnv() {
+
+		regexes := []string{
+			string(AppName) + ":current-build",
+			string(AppName),
+		}
+
+		image = dockerImageFromRegexes(globalCtx, dockerClient, regexes)
+	} else {
+		image = utils.Sprintf("ghcr.io/whisthq/%s/%s:current-build", metadata.GetAppEnvironmentLowercase(), AppName)
+	}
+
+	// We now create the underlying Docker container for this mandelbox.
+	exposedPorts := make(dockernat.PortSet)
+	exposedPorts[dockernat.Port("32262/tcp")] = struct{}{}
+	exposedPorts[dockernat.Port("32263/udp")] = struct{}{}
+	exposedPorts[dockernat.Port("32273/tcp")] = struct{}{}
+
+	aesKey := utils.RandHex(16)
+	envs := []string{
+		utils.Sprintf("WHIST_AES_KEY=%s", aesKey),
+		utils.Sprintf("NVIDIA_VISIBLE_DEVICES=%v", "all"),
+		"NVIDIA_DRIVER_CAPABILITIES=all",
+		utils.Sprintf("SENTRY_ENV=%s", metadata.GetAppEnvironment()),
+		utils.Sprintf("WHIST_INITIAL_USER_DATA_FILE=%v/%v", mandelboxData.UserInitialBrowserDirInMbox, mandelboxData.UserInitialBrowserFile),
+	}
+	config := dockercontainer.Config{
+		ExposedPorts: exposedPorts,
+		Env:          envs,
+		Image:        image,
+		AttachStdin:  true,
+		AttachStdout: true,
+		AttachStderr: true,
+		Tty:          true,
+	}
+	natPortBindings := make(dockernat.PortMap)
+	natPortBindings[dockernat.Port("32262/tcp")] = []dockernat.PortBinding{{HostPort: utils.Sprintf("%v", hostPortForTCP32262)}}
+	natPortBindings[dockernat.Port("32263/udp")] = []dockernat.PortBinding{{HostPort: utils.Sprintf("%v", hostPortForUDP32263)}}
+	natPortBindings[dockernat.Port("32273/tcp")] = []dockernat.PortBinding{{HostPort: utils.Sprintf("%v", hostPortForTCP32273)}}
+
+	tmpfs := make(map[string]string)
+	tmpfs["/run"] = "size=52428800"
+	tmpfs["/run/lock"] = "size=52428800"
+
+	// Sanitize the received session ID. First, verify that the sessionID is a valid timestamp string.
+	// If not, use a new timestamp.
+	// _, err = strconv.ParseInt(mandelboxSubscription.SessionID, 10, 64)
+	// if err != nil {
+	// 	sessionID := strconv.FormatInt(time.Now().UnixMilli(), 10)
+	// 	logger.Warningf("Malformed session ID: %v Using new session ID: %v", mandelboxSubscription.SessionID, sessionID)
+	// 	mandelboxSubscription.SessionID = sessionID
+	// }
+
+	// mandelbox.SetSessionID(mandelboxtypes.SessionID(mandelboxSubscription.SessionID))
+
+	hostConfig := dockercontainer.HostConfig{
+		Binds: []string{
+			"/sys/fs/cgroup:/sys/fs/cgroup:ro",
+			path.Join(utils.WhistDir, mandelbox.GetID().String(), "mandelboxResourceMappings", ":", "/whist", "resourceMappings"),
+			path.Join(utils.TempDir, mandelbox.GetID().String(), "sockets", ":", "tmp", "sockets"),
+			path.Join(utils.TempDir, "logs", mandelbox.GetID().String(), ":", "var", "log", "whist"),
+			"/run/udev/data:/run/udev/data:ro",
+			path.Join(utils.WhistDir, mandelbox.GetID().String(), "userConfigs", "unpacked_configs", ":", "/whist", "userConfigs", ":rshared"),
+		},
+		PortBindings: natPortBindings,
+		CapDrop:      strslice.StrSlice{"ALL"},
+		CapAdd: strslice.StrSlice([]string{
+			"SETPCAP",
+			"MKNOD",
+			"AUDIT_WRITE",
+			"CHOWN",
+			"NET_RAW",
+			"DAC_OVERRIDE",
+			"FOWNER",
+			"FSETID",
+			"KILL",
+			"SETGID",
+			"SETUID",
+			"NET_BIND_SERVICE",
+			"SYS_CHROOT",
+			"SETFCAP",
+			// NOTE THAT THE FOLLOWING ARE NOT ENABLED BY DEFAULT BY DOCKER --- THIS IS OUR DOING
+			"SYS_NICE",
+			"IPC_LOCK",
+		}),
+		ShmSize: 2147483648,
+		Tmpfs:   tmpfs,
+		Resources: dockercontainer.Resources{
+			CPUShares: 2,
+			Memory:    6552550944,
+			NanoCPUs:  0,
+			// Don't need to set CgroupParent, since each mandelbox is its own task.
+			// We're not using anything like AWS services, where we'd want to put
+			// several mandelboxes under one limit.
+			Devices:            devices,
+			KernelMemory:       0,
+			KernelMemoryTCP:    0,
+			MemoryReservation:  0,
+			MemorySwap:         0,
+			Ulimits:            []*dockerunits.Ulimit{},
+			CPUCount:           0,
+			CPUPercent:         0,
+			IOMaximumIOps:      0,
+			IOMaximumBandwidth: 0,
+		},
+		SecurityOpt: []string{
+			"apparmor:mandelbox-apparmor-profile",
+		},
+	}
+	mandelboxName := utils.Sprintf("%s-%s", AppName, mandelboxID)
+	re := regexp.MustCompile(`[^a-zA-Z0-9_.-]`)
+	mandelboxName = re.ReplaceAllString(mandelboxName, "-")
+
+	dockerBody, err := dockerClient.ContainerCreate(mandelbox.GetContext(), &config, &hostConfig, nil, &v1.Platform{Architecture: "amd64", OS: "linux"}, mandelboxName)
+	if err != nil {
+		logAndReturnError("Error running `create` for %s:\n%s", mandelbox.GetID(), err)
+		return
+	}
+
+	logger.Infof("SpinUpMandelbox(): Value returned from ContainerCreate: %#v", dockerBody)
+	dockerID := mandelboxtypes.DockerID(dockerBody.ID)
+
+	logger.Infof("SpinUpMandelbox(): Successfully ran `create` command and got back runtime ID %s", dockerID)
+
+	// Register on database
+}
 
 // SpinUpMandelbox is the request used to create a mandelbox on this host.
 func SpinUpMandelbox(globalCtx context.Context, globalCancel context.CancelFunc, goroutineTracker *sync.WaitGroup, dockerClient dockerclient.CommonAPIClient, sub *subscriptions.MandelboxEvent, transportRequestMap map[mandelboxtypes.MandelboxID]chan *JSONTransportRequest, transportMapLock *sync.Mutex) {
