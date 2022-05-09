e41f5ed3d8d53a136c4517d7143e3c3640f5a8fe
Philippe Noel <phil@whist.com>
2022-05-09 14:24:50 -0400

    remove debug logs

--- protocol/whist/network/network_algorithm.c
+++ protocol/whist/network/network_algorithm.c
@@ -143,7 +143,6 @@ bool whist_congestion_controller(GroupStats *curr_group_stats, GroupStats *prev_
         // Not enough data to take any decision. Let the bits start flowing.
         return false;
     }
-    const int verbose_log = 1;
     static WhistTimer overuse_timer;
     static WhistTimer last_update_timer;
     static WhistTimer last_decrease_timer;
@@ -246,17 +245,14 @@ bool whist_congestion_controller(GroupStats *curr_group_stats, GroupStats *prev_
     // +-------------+-----------+------------+--------+
     if (overuse_detector_signal == OVERUSE_SIGNAL) {
         delay_controller_state = DELAY_CONTROLLER_DECREASE;
-        if (verbose_log) fprintf(stderr, "!!!! overuse\n");
     } else if (overuse_detector_signal == NORMAL_SIGNAL) {
         if (delay_controller_state == DELAY_CONTROLLER_HOLD) {
             delay_controller_state = DELAY_CONTROLLER_INCREASE;
         } else if (delay_controller_state == DELAY_CONTROLLER_DECREASE) {
             delay_controller_state = DELAY_CONTROLLER_HOLD;
         }
-        if (verbose_log) fprintf(stderr, "!!!! normal_singal\n");
     } else if (overuse_detector_signal == UNDERUSE_SIGNAL) {
         delay_controller_state = DELAY_CONTROLLER_HOLD;
-        if (verbose_log) fprintf(stderr, "!!!! underuse\n");
     }
 
     // If the latency suddenly increases, then it might mean congestion due to longer queue length.
@@ -285,7 +281,6 @@ bool whist_congestion_controller(GroupStats *curr_group_stats, GroupStats *prev_
     } else if (short_term_latency > latency_threshold_hold_state &&
                delay_controller_state == DELAY_CONTROLLER_INCREASE) {
         delay_controller_state = DELAY_CONTROLLER_HOLD;
-        if (verbose_log) fprintf(stderr, "!!!! change to hold\n");
     }
 
     // Delay-based controller selects based on overuse signal
