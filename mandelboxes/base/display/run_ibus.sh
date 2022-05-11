bcdeacf0db87c325537d82301c425fb84b1ecf0a
Philippe Noel <phil@whist.com>
2022-05-11 14:48:45 -0400

    clean things up

--- mandelboxes/base/display/run_ibus.sh
+++ mandelboxes/base/display/run_ibus.sh
@@ -2,13 +2,9 @@
 export GTK_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 export QT_IM_MODULE=ibus
-#DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
 # Exit on subcommand errors
 set -Eeuo pipefail
-
-gsettings set org.gnome.desktop.interface gtk-theme Radiance
-
 
 # Disable any triggers for switching keyboards on the mandelbox
 # gsettings set org.freedesktop.ibus.general preload-engines "['xkb:us::eng', 'anthy', 'pinyin', 'hangul', 'Unikey']"
