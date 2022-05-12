65ac9d752d49fd16810b6af6cef2d8b8f406a6da
Serina Hu <serina_hu@college.harvard.edu>
2022-05-12 11:20:32 -0400

    documentation and clang-format

--- protocol/whist/utils/window_info.h
+++ protocol/whist/utils/window_info.h
@@ -3,7 +3,7 @@
 /**
  * Copyright (c) 2021-2022 Whist Technologies, Inc.
  * @file window_info.h
- * @brief This file contains all the code for getting the name of a window.
+ * @brief This file contains all the code for getting and setting window attributes
 
 ============================
 Usage
@@ -21,6 +21,10 @@ destroy_window_info_getter();
 
 // Note that this library is not thread-safe,
 // there must only be one window_info_getter live at any one time
+//
+Create a capture device for the display whose windows you want to work with. To get the active
+window or a list of windows for the client to display, call get_active_window or get_valid_windows,
+respectively; pass the resulting WhistWindows to the getter/setter functions here.
 
 */
 
@@ -83,21 +87,146 @@ bool is_focused_window_fullscreen(void);
  */
 void destroy_window_info_getter(void);
 
-// Added functions for multiwindow
-void get_valid_windows(CaptureDevice* capture_device, LinkedList* list);
-void get_window_attributes(CaptureDevice* capture_device, WhistWindow whist_window, WhistWindowData* d);
+/**
+ * @brief                          Get a linked list of windows we should stream
+ *                                 (e.g. Chrome windows and popups)
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param window_list              Linked list to append the window list into
+ *
+ */
+void get_valid_windows(CaptureDevice* capture_device, LinkedList* window_list);
+
+/**
+ * @brief                          Fill WhistWindowData* struct with x/y/w/h/etc. of whist_window
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             The WhistWindow to get attributes from
+ *
+ * @param d                        Struct to fill with window attributes
+ *
+ */
+void get_window_attributes(CaptureDevice* capture_device, WhistWindow whist_window,
+                           WhistWindowData* d);
+
+/**
+ * @brief                          Return the active window, either using NET_ACTIVE_WINDOW atom
+ * (preferred) or XGetInputFocus
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @returns                        WhistWindow struct representing the active window
+ *
+ */
 WhistWindow get_active_window(CaptureDevice* capture_device);
 // TODO: replace with a function that doesn't just return the raw string?
-// or explicitly make one for Window (private) and one for WhistWindow (publi)
+// or explicitly make one for Window (private) and one for WhistWindow (public)
 // char* get_window_name(CaptureDevice* capture_device, WhistWindow whist_window);
+//
+/**
+ * @brief                          Return whether or not the window is resizable (e.g. popups or
+ * right click menus are not resizable, but Chrome is)
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow whose properties we are querying
+ *
+ * @returns                        whether or not whist_window is resizable
+ *
+ */
 bool is_window_resizable(CaptureDevice* capture_device, WhistWindow whist_window);
-void move_resize_window(CaptureDevice* device, WhistWindow whist_window, int x, int y, int width, int height);
-void close_window(CaptureDevice* device, WhistWindow whist_window);
+
+/**
+ * @brief                          Move the window to (x, y) and resize it to (width, height). This
+ * only sends a request to the window manager to do so; the window manager can reject the request,
+ * e.g. if the window manager is started maximized.
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow we are moving and resizing
+ *
+ * @param x                        Desired x-coordinate
+ *
+ * @param y                        Desired y-coordinate
+ *
+ * @param width                    Desired width
+ *
+ * @param height                   Desired height
+ *
+ */
+void move_resize_window(CaptureDevice* capture_device, WhistWindow whist_window, int x, int y,
+                        int width, int height);
+
+/**
+ * @brief                          Close the given window
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow to close
+ *
+ */
+void close_window(CaptureDevice* capture_device, WhistWindow whist_window);
+
+/**
+ * @brief                          Minimize the given window
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow to minimize
+ *
+ */
 void minimize_window(CaptureDevice* capture_device, WhistWindow whist_window);
+
+/**
+ * @brief                          Maximize the given window
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow to maximize
+ *
+ */
 void maximize_window(CaptureDevice* capture_device, WhistWindow whist_window);
+
+/**
+ * @brief                          Fullscreen the given window
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow to fullscreen
+ *
+ */
 void fullscreen_window(CaptureDevice* capture_device, WhistWindow whist_window);
+
+/**
+ * @brief                          Bring given window to top
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow to bring to top
+ *
+ */
 void bring_window_to_top(CaptureDevice* capture_device, WhistWindow whist_window);
+
+/**
+ * @brief                          Unminimize the given window
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow to unminimize
+ *
+ */
 void unminimize_window(CaptureDevice* capture_device, WhistWindow whist_window);
+
+/**
+ * @brief                          Unfullscreen the given window
+ *
+ * @param capture_device           Capture device whose display we are using
+ *
+ * @param whist_window             WhistWindow to unfullscreen
+ *
+ */
 void unfullscreen_window(CaptureDevice* capture_device, WhistWindow whist_window);
 
 #endif  // WINDOW_INFO_H
--- protocol/whist/utils/x11_window_info.c
+++ protocol/whist/utils/x11_window_info.c
@@ -1,8 +1,21 @@
 /**
  * Copyright (c) 2021-2022 Whist Technologies, Inc.
  * @file x11_window_info.c
- * @brief This file contains all the code for getting X11-specific window information.
- */
+ * @brief This file contains all the code for getting and setting X11-specific window information.
+============================
+Usage
+============================
+
+Create a capture device for the display whose windows you want to work with. To get the active
+window or a list of windows for the client to display, call get_active_window or get_valid_windows,
+respectively; pass the resulting WhistWindows to the getter/setter functions here.
+*/
+
+/*
+============================
+Includes
+============================
+*/
 
 #include <whist/core/whist.h>
 #include "window_info.h"
@@ -13,10 +26,12 @@
 #include <X11/Xutil.h>
 #include <stdbool.h>
 
+// See the EWMH specification; for ease of reading
 #define _NET_WM_STATE_REMOVE 0
 #define _NET_WM_STATE_ADD 1
 #define _NET_WM_STATE_TOGGLE 2
 
+// Macro to make initializing X atoms (which we use a lot of) easier
 #define INIT_ATOM(DEVICE, ATOM_VAR, NAME)                    \
     static Atom ATOM_VAR = 0;                                \
     if (!ATOM_VAR) {                                         \
@@ -27,46 +42,98 @@ static Display* display;
 static char last_window_name[WINDOW_NAME_MAXLEN + 1];
 static bool last_window_name_valid;
 
-// Privates
+/*
+============================
+Private Functions
+============================
+*/
 
+/**
+ * @brief                          Helper function to call XGetWindowProperty.
+ *
+ * @param device                   Device whose display we are using
+ *
+ * @param w                        Window to get property for
+ *
+ * @param property                 Property to get
+ *
+ * @param req_type                 Required type (can pass in AnyPropertyType if N/A)
+ *
+ * @param nitems_return            Returns number of items stored in prop_return, will be nontrivial
+ * if the property is an array
+ *
+ * @param prop_return              Returns data for specified property
+ *
+ * @returns                        Whether XGetWindowProperty was successful
+ */
 bool x11_get_window_property(X11CaptureDevice* device, Window w, Atom property, Atom req_type,
                              unsigned long* nitems_return, unsigned char** prop_return);
+
+/**
+ * @brief                          Helper function to call XSendEvent on a root message, as
+ * described in the EWMH spec.
+ *
+ * @param device                   Device whose display we are using
+ *
+ * @param event_send               The XEvent to send
+ */
 void send_message_to_root(X11CaptureDevice* device, XEvent* event_send);
+
+/**
+ * @brief                          Helper function to create an XEvent that requests a window state
+ * change, e.g. minimize/maximize/fullscreen, as described in the EWMH spec
+ *
+ * @param device                   Device whose display we are using
+ *
+ * @param w                        The window whose state we want to change
+ *
+ * @param action                   The action to perform (set/unset/toggle)
+ *
+ * @param property1                First property to perform action on
+ *
+ * @param property2                Second property to perform action on. This must be 0 if there is
+ * no second property to change
+ *
+ * @return                         An XEvent that can then be passed to send_message_to_root for the
+ * state change
+ */
 XEvent create_change_state_message(X11CaptureDevice* device, Window w, long action, Atom property1,
                                    Atom property2);
+
+/**
+ * @brief                          Returns the window name of the specified window
+ *
+ * @param device                   Device whose display we are using
+ *
+ * @param w                        The window whose name we want
+ *
+ * @returns                        UTF-8 string for window name
+ */
 char* get_window_name(X11CaptureDevice* device, Window w);
+
+/**
+ * @brief                          Helper for recursive get_valid_windows calls, since
+ * get_valid_windows recursively searches the window tree
+ *
+ * @param device                   Device whose display we are using
+ *
+ * @param list                     Linked list to append children of curr who are valid windows
+ *
+ * @param curr                     Current position in the X11 window tree
+ */
 void get_valid_windows_helper(X11CaptureDevice* device, LinkedList* list, Window curr);
+
+/*
+============================
+Public Function Implementations
+============================
+*/
 
 void init_window_info_getter(void) {
     if (display == NULL) {
         display = XOpenDisplay(NULL);
     }
     last_window_name_valid = false;
-}
-
-void get_valid_windows_helper(X11CaptureDevice* device, LinkedList* list, Window curr) {
-    LOG_DEBUG("Current window %lu", curr);
-    Window parent;
-    Window* children;
-    unsigned int nchildren;
-    XQueryTree(device->display, curr, &device->root, &parent, &children, &nchildren);
-    // check the dimensions of each window
-    XWindowAttributes attr;
-    XGetWindowAttributes(device->display, curr, &attr);
-    if (attr.x >= 0 && attr.y >= 0 && attr.width >= MIN_SCREEN_WIDTH &&
-        attr.height >= MIN_SCREEN_HEIGHT && *get_window_name(device, curr) != '\0') {
-        LOG_INFO("Valid window %s has %d children, position %d, %d, dimensions %d x %d",
-                 get_window_name(device, curr), nchildren, attr.x, attr.y, attr.width, attr.height);
-        WhistWindow* valid_window = safe_malloc(sizeof(WhistWindow));
-        valid_window->window = curr;
-        linked_list_add_tail(list, valid_window);
-    }
-
-    if (nchildren != 0) {
-        for (unsigned int i = 0; i < nchildren; i++) {
-            get_valid_windows_helper(device, list, children[i]);
-        }
-    }
 }
 
 void get_valid_windows(CaptureDevice* capture_device, LinkedList* list) {
@@ -84,20 +151,6 @@ void get_window_attributes(CaptureDevice* capture_device, WhistWindow whist_wind
     d->height = attr.height;
     d->x = attr.x;
     d->y = attr.y;
-}
-
-// HELPER
-// wrapper around XGetWindowProperty, returns whether successful or not
-bool x11_get_window_property(X11CaptureDevice* device, Window w, Atom property, Atom req_type,
-                             unsigned long* nitems_return, unsigned char** prop_return) {
-    static Atom actual_type;
-    static int actual_format;
-    static unsigned long bytes_after;
-    return (XGetWindowProperty(
-                device->display, w, property, 0,
-                65536,  // the internet doesn't seem to agree on what long_length should be
-                False, req_type, &actual_type, &actual_format, nitems_return, &bytes_after,
-                prop_return) == Success);
 }
 
 WhistWindow get_active_window(CaptureDevice* capture_device) {
@@ -126,135 +179,6 @@ WhistWindow get_active_window(CaptureDevice* capture_device) {
         }
     }
     return w;
-}
-
-// utf-8 string; helper (processed by something like get_focused_window_name with the
-// last_valid_title and everything)
-char* get_window_name(X11CaptureDevice* device, Window w) {
-    // first try using EWMH
-    INIT_ATOM(device, net_wm_name, "_NET_WM_NAME");
-    INIT_ATOM(device, utf8_string, "UTF8_STRING");
-    static unsigned long nitems;
-    static unsigned char* name;  // name stored here
-
-    if (x11_get_window_property(device, w, net_wm_name, utf8_string, &nitems, &name)) {
-        LOG_INFO("Window name %s", name);
-        return (char*)name;
-    } else {
-        // fall back to XGetWMName
-        XTextProperty prop;
-        // unclear if needed
-        XLockDisplay(device->display);
-        Status s = XGetWMName(device->display, w, &prop);
-        if (s) {
-            int count = 0;
-            char** list = NULL;
-            int result = XmbTextPropertyToTextList(device->display, &prop, &list, &count);
-            XUnlockDisplay(device->display);
-            if (count && result == Success) {
-                return list[0];
-            } else {
-                return "";
-            }
-        } else {
-            XUnlockDisplay(device->display);
-            return "";
-        }
-    }
-}
-
-bool is_window_resizable(CaptureDevice* capture_device, WhistWindow whist_window) {
-    Window w = whist_window.window;
-    X11CaptureDevice* device = capture_device->x11_capture_device;
-    INIT_ATOM(device, net_wm_allowed_actions, "_NET_WM_ALLOWED_ACTIONS");
-    INIT_ATOM(device, atom_array, "ATOM[]");
-    INIT_ATOM(device, net_wm_action_resize, "_NET_WM_ACTION_RESIZE");
-    static unsigned long nitems;
-    static unsigned char* actions;  // name stored here
-
-    if (x11_get_window_property(device, w, net_wm_allowed_actions, atom_array, &nitems, &actions)) {
-        Atom* allowed_actions = (Atom*)actions;
-        // check if one of the actions is net_wm_action_resize
-        for (int i = 0; i < (int)nitems; i++) {
-            if (allowed_actions[i] == net_wm_action_resize) {
-                return true;
-            }
-        }
-        return false;
-    }
-    LOG_ERROR("Couldn't get allowed actions, assuming window is resizable!");
-    return true;
-}
-
-// HELPER
-void send_message_to_root(X11CaptureDevice* device, XEvent* event_send) {
-    XSendEvent(device->display, device->root, False,
-               SubstructureNotifyMask | SubstructureRedirectMask, event_send);
-}
-
-void move_resize_window(CaptureDevice* capture_device, WhistWindow whist_window, int x, int y,
-                        int width, int height) {
-    Window w = whist_window.window;
-    X11CaptureDevice* device = capture_device->x11_capture_device;
-    INIT_ATOM(device, net_moveresize_window, "_NET_MOVERESIZE_WINDOW");
-    static long gravity_flags = 15 << 8;  // sets gravity to 0, x/y/w/h to 1, source to 0
-
-    XEvent xevent = {0};
-    XClientMessageEvent xclient = {0};
-    xclient.type = ClientMessage;
-    xclient.window = w;
-    xclient.display = device->display;
-    xclient.message_type = net_moveresize_window;
-    xclient.format = 32;
-    xclient.data.l[0] = gravity_flags;
-    xclient.data.l[1] = x;
-    xclient.data.l[2] = y;
-    xclient.data.l[3] = width;
-    xclient.data.l[4] = height;
-    xevent.xclient = xclient;
-    send_message_to_root(device, &xevent);
-}
-
-void close_window(CaptureDevice* capture_device, WhistWindow whist_window) {
-    Window w = whist_window.window;
-    X11CaptureDevice* device = capture_device->x11_capture_device;
-    INIT_ATOM(device, net_close_window, "_NET_CLOSE_WINDOW");
-
-    XEvent xevent = {0};
-    XClientMessageEvent xclient = {0};
-    xclient.type = ClientMessage;
-    xclient.window = w;
-    xclient.display = device->display;
-    xclient.message_type = net_close_window;
-    xclient.format = 32;
-    xclient.data.l[0] = 0;  // timestamp: not sure if it should be 0
-    xclient.data.l[1] = 0;  // source: 0 for now
-    xclient.data.l[2] = 0;
-    xclient.data.l[3] = 0;
-    xclient.data.l[4] = 0;
-    xevent.xclient = xclient;
-    send_message_to_root(device, &xevent);
-}
-
-// HELPER
-XEvent create_change_state_message(X11CaptureDevice* device, Window w, long action, Atom property1,
-                                   Atom property2) {
-    INIT_ATOM(device, net_wm_state, "_NET_WM_STATE");
-
-    XEvent xevent = {0};
-    XClientMessageEvent xclient = {0};
-    xclient.type = ClientMessage;
-    xclient.window = w;
-    xclient.display = device->display;
-    xclient.message_type = net_wm_state;
-    xclient.format = 32;
-    xclient.data.l[0] = action;           // timestamp: not sure if it should be 0
-    xclient.data.l[1] = (long)property1;  // source: 0 for now
-    xclient.data.l[2] = (long)property2;
-    xclient.data.l[3] = 0;
-    xclient.data.l[4] = 0;
-    xevent.xclient = xclient;
-    return xevent;
 }
 
 void minimize_window(CaptureDevice* capture_device, WhistWindow whist_window) {
@@ -438,4 +362,178 @@ void destroy_window_info_getter(void) {
         XCloseDisplay(display);
         display = NULL;
     }
+}
+
+void move_resize_window(CaptureDevice* capture_device, WhistWindow whist_window, int x, int y,
+                        int width, int height) {
+    Window w = whist_window.window;
+    X11CaptureDevice* device = capture_device->x11_capture_device;
+    INIT_ATOM(device, net_moveresize_window, "_NET_MOVERESIZE_WINDOW");
+    static long gravity_flags = 15 << 8;  // sets gravity to 0, x/y/w/h to 1, source to 0
+
+    XEvent xevent = {0};
+    XClientMessageEvent xclient = {0};
+    xclient.type = ClientMessage;
+    xclient.window = w;
+    xclient.display = device->display;
+    xclient.message_type = net_moveresize_window;
+    xclient.format = 32;
+    xclient.data.l[0] = gravity_flags;
+    xclient.data.l[1] = x;
+    xclient.data.l[2] = y;
+    xclient.data.l[3] = width;
+    xclient.data.l[4] = height;
+    xevent.xclient = xclient;
+    send_message_to_root(device, &xevent);
+}
+
+void close_window(CaptureDevice* capture_device, WhistWindow whist_window) {
+    Window w = whist_window.window;
+    X11CaptureDevice* device = capture_device->x11_capture_device;
+    INIT_ATOM(device, net_close_window, "_NET_CLOSE_WINDOW");
+
+    XEvent xevent = {0};
+    XClientMessageEvent xclient = {0};
+    xclient.type = ClientMessage;
+    xclient.window = w;
+    xclient.display = device->display;
+    xclient.message_type = net_close_window;
+    xclient.format = 32;
+    xclient.data.l[0] = 0;  // timestamp: not sure if it should be 0
+    xclient.data.l[1] = 0;  // source: 0 for now
+    xclient.data.l[2] = 0;
+    xclient.data.l[3] = 0;
+    xclient.data.l[4] = 0;
+    xevent.xclient = xclient;
+    send_message_to_root(device, &xevent);
+}
+
+bool is_window_resizable(CaptureDevice* capture_device, WhistWindow whist_window) {
+    Window w = whist_window.window;
+    X11CaptureDevice* device = capture_device->x11_capture_device;
+    INIT_ATOM(device, net_wm_allowed_actions, "_NET_WM_ALLOWED_ACTIONS");
+    INIT_ATOM(device, atom_array, "ATOM[]");
+    INIT_ATOM(device, net_wm_action_resize, "_NET_WM_ACTION_RESIZE");
+    static unsigned long nitems;
+    static unsigned char* actions;  // name stored here
+
+    if (x11_get_window_property(device, w, net_wm_allowed_actions, atom_array, &nitems, &actions)) {
+        Atom* allowed_actions = (Atom*)actions;
+        // check if one of the actions is net_wm_action_resize
+        for (int i = 0; i < (int)nitems; i++) {
+            if (allowed_actions[i] == net_wm_action_resize) {
+                return true;
+            }
+        }
+        return false;
+    }
+    LOG_ERROR("Couldn't get allowed actions, assuming window is resizable!");
+    return true;
+}
+
+/*
+============================
+Private Function Implementations
+============================
+*/
+
+void get_valid_windows_helper(X11CaptureDevice* device, LinkedList* list, Window curr) {
+    LOG_DEBUG("Current window %lu", curr);
+    Window parent;
+    Window* children;
+    unsigned int nchildren;
+    XQueryTree(device->display, curr, &device->root, &parent, &children, &nchildren);
+    // check the dimensions of each window
+    XWindowAttributes attr;
+    XGetWindowAttributes(device->display, curr, &attr);
+    if (attr.x >= 0 && attr.y >= 0 && attr.width >= MIN_SCREEN_WIDTH &&
+        attr.height >= MIN_SCREEN_HEIGHT && *get_window_name(device, curr) != '\0') {
+        LOG_DEBUG("Valid window %s has %d children, position %d, %d, dimensions %d x %d",
+                  get_window_name(device, curr), nchildren, attr.x, attr.y, attr.width,
+                  attr.height);
+        WhistWindow* valid_window = safe_malloc(sizeof(WhistWindow));
+        valid_window->window = curr;
+        linked_list_add_tail(list, valid_window);
+    }
+
+    if (nchildren != 0) {
+        for (unsigned int i = 0; i < nchildren; i++) {
+            get_valid_windows_helper(device, list, children[i]);
+        }
+    }
+}
+
+// wrapper around XGetWindowProperty, returns whether successful or not
+bool x11_get_window_property(X11CaptureDevice* device, Window w, Atom property, Atom req_type,
+                             unsigned long* nitems_return, unsigned char** prop_return) {
+    static Atom actual_type;
+    static int actual_format;
+    static unsigned long bytes_after;
+    return (XGetWindowProperty(
+                device->display, w, property, 0,
+                65536,  // the internet doesn't seem to agree on what long_length should be
+                False, req_type, &actual_type, &actual_format, nitems_return, &bytes_after,
+                prop_return) == Success);
+}
+
+// utf-8 string; helper (processed by something like get_focused_window_name with the
+// last_valid_title and everything)
+char* get_window_name(X11CaptureDevice* device, Window w) {
+    // first try using EWMH
+    INIT_ATOM(device, net_wm_name, "_NET_WM_NAME");
+    INIT_ATOM(device, utf8_string, "UTF8_STRING");
+    static unsigned long nitems;
+    static unsigned char* name;  // name stored here
+
+    if (x11_get_window_property(device, w, net_wm_name, utf8_string, &nitems, &name)) {
+        LOG_INFO("Window name %s", name);
+        return (char*)name;
+    } else {
+        // fall back to XGetWMName
+        XTextProperty prop;
+        // unclear if needed
+        XLockDisplay(device->display);
+        Status s = XGetWMName(device->display, w, &prop);
+        if (s) {
+            int count = 0;
+            char** list = NULL;
+            int result = XmbTextPropertyToTextList(device->display, &prop, &list, &count);
+            XUnlockDisplay(device->display);
+            if (count && result == Success) {
+                return list[0];
+            } else {
+                return "";
+            }
+        } else {
+            XUnlockDisplay(device->display);
+            return "";
+        }
+    }
+}
+
+// HELPER
+void send_message_to_root(X11CaptureDevice* device, XEvent* event_send) {
+    XSendEvent(device->display, device->root, False,
+               SubstructureNotifyMask | SubstructureRedirectMask, event_send);
+}
+
+// HELPER
+XEvent create_change_state_message(X11CaptureDevice* device, Window w, long action, Atom property1,
+                                   Atom property2) {
+    INIT_ATOM(device, net_wm_state, "_NET_WM_STATE");
+
+    XEvent xevent = {0};
+    XClientMessageEvent xclient = {0};
+    xclient.type = ClientMessage;
+    xclient.window = w;
+    xclient.display = device->display;
+    xclient.message_type = net_wm_state;
+    xclient.format = 32;
+    xclient.data.l[0] = action;           // timestamp: not sure if it should be 0
+    xclient.data.l[1] = (long)property1;  // source: 0 for now
+    xclient.data.l[2] = (long)property2;
+    xclient.data.l[3] = 0;
+    xclient.data.l[4] = 0;
+    xevent.xclient = xclient;
+    return xevent;
 }
