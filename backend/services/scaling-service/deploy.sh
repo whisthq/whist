2598def3329ad877c033e86798d4f59978ba25e6
Philippe Noel <phil@whist.com>
2022-05-19 11:56:17 -0400

    cheeky fix

--- backend/services/scaling-service/deploy.sh
+++ backend/services/scaling-service/deploy.sh
@@ -62,7 +62,7 @@ deploy_scaling_service() {
   # To get around this, we simply try to push the deploy until it succeeds, up to 5 retries.
   echo "Deploying scaling-service..."
   count=0
-  until $(git push -f heroku-whist-scaling-service deploy-branch:master) || ((count++ >= 5))
+  until git push -f heroku-whist-scaling-service deploy-branch:master || ((count++ >= 5))
   do
     sleep 1
     echo "Failed to deploy to Heroku on attempt #$count of 5, retrying..."
