f3a61c8e650a549dddc4a41ac1ee04ef1758658b
Roshan Padaki <roshan@whist.com>
2022-05-19 18:49:55 -0400

    fix sentry environment detection

--- mandelboxes/browsers/chrome/start-chrome.sh
+++ mandelboxes/browsers/chrome/start-chrome.sh
@@ -2,7 +2,7 @@
 
 # Enable Sentry bash error handler, this will catch errors if `set -e` is set in a Bash script
 # This is called via `./run-as-whist-user.sh`, which passes sentry environment in.
-case $(cat "$SENTRY_ENVIRONMENT") in
+case "$SENTRY_ENVIRONMENT" in
   dev|staging|prod)
     export SENTRY_ENVIRONMENT=${SENTRY_ENV}
     eval "$(sentry-cli bash-hook)"
