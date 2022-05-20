b0e982ec45b89c77f8645932e05a4fdf8c558fd7
Philippe Noel <phil@whist.com>
2022-05-20 15:09:27 -0400

    try agai

--- frontend/core-ts/src/http/withJSON.ts
+++ frontend/core-ts/src/http/withJSON.ts
@@ -9,7 +9,7 @@ import { ServerDecorator } from "../types/api"
  * @returns a Promise with the HTTP response
  */
 export const withJSON: ServerDecorator = async (fn, req) => {
-  const { response } = await fn(req)
+  const { response } = await fn(req)!
   // Certain badly formed responses, like internal server 500,
   // will throw an error if you call the response.json() method.
   // This can happen even though a json method exists on the
@@ -22,9 +22,9 @@ export const withJSON: ServerDecorator = async (fn, req) => {
   // The json method is still available on the response key,
   // which is holding the original response object, in case
   // the user wants to try for themselves.
-  if (response?.json!)
+  if (response?.json)
     try {
-      return { response, json: await response.json() }
+      return { response, json: await response.json()! }
     } catch (e) {}
   return { response }
 }
