614ebacae4b59a1736fa5e499037a037924d3769
Mauricio <mauricio@whist.com>
2022-05-26 14:57:43 -0500

    Disable non-US regions in scaling service

--- backend/services/scaling-service/scaling_algorithms/default/config.go
+++ backend/services/scaling-service/scaling_algorithms/default/config.go
@@ -118,13 +118,6 @@ func GetEnabledRegions() []string {
 			"us-west-1",
 			"us-west-2",
 			"ca-central-1",
-			"eu-west-1",
-			"eu-west-2",
-			"eu-west-3",
-			"eu-central-1",
-			"ap-south-1",
-			"ap-southeast-1",
-			"ap-southeast-2",
 		}
 	default:
 		enabledRedions = []string{
