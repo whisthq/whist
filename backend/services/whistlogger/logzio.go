d90fd7b146433f754ef24aaf4d2b4d48a1346a08
Mauricio Araujo <mauricio@whist.com>
2022-06-09 16:44:23 -0400

    Remove comments, get tokens from env var

--- backend/services/whistlogger/logzio.go
+++ backend/services/whistlogger/logzio.go
@@ -2,6 +2,7 @@ package whistlogger // import "github.com/whisthq/whist/backend/services/whistlo
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/logzio/logzio-go"
@@ -16,8 +17,9 @@ type logzioCore struct {
 }
 
 func NewLogzioCore(encoder zapcore.Encoder, levelEnab zapcore.LevelEnabler) zapcore.Core {
+	logzioShippingToken := os.Getenv("LOGZIO_SHIPPING_TOKEN")
 	sender, err := logzio.New(
-		"MoaZIzGkBxpsbbquDpwGlOTasLqKvtGJ",
+		logzioShippingToken,
 		logzio.SetUrl("https://listener.logz.io:8071"),
 		logzio.SetDrainDuration(time.Second*3),
 		logzio.SetCheckDiskSpace(false),
@@ -99,130 +101,3 @@ func (lc *logzioCore) Sync() error {
 	//Flush logzio
 	return lc.sender.Sync()
 }
-
-// package whistlogger // import "github.com/whisthq/whist/backend/services/whistlogger"
-
-// import (
-// 	"encoding/json"
-// 	"log"
-// 	"os"
-// 	"time"
-
-// 	"github.com/whisthq/whist/backend/services/metadata"
-// 	"github.com/whisthq/whist/backend/services/metadata/aws"
-// 	"github.com/whisthq/whist/backend/services/utils"
-
-// 	"github.com/logzio/logzio-go"
-// )
-
-// // We use a pointer of this type so we can check if it is nil in our logging
-// // functions, and therefore always call them safely.
-// var logzioTransport *logzioSender
-
-// // We define a custom type to be able to override the Send() function with sane
-// // error handling and our custom marshalling logic.
-// type logzioSender logzio.LogzioSender
-
-// type logzioMessageType string
-
-// // message is a generic log message with basic data.
-// type message struct {
-// 	Message      string                  `json:"message"`
-// 	Type         string                  `json:"type"`
-// 	Component    string                  `json:"component"`
-// 	SubComponent string                  `json:"sub_component"`
-// 	Environment  metadata.AppEnvironment `json:"environment"`
-// }
-
-// // hostMessage includes data specific for the host service logs.
-// type hostMessage struct {
-// 	AWSInstanceID aws.InstanceID `json:"aws_instance_id"`
-// 	AWSAmiID      aws.AmiID      `json:"aws_ami_id"`
-// 	message
-// }
-
-// const (
-// 	logzioTypeInfo    logzioMessageType = "info"
-// 	logzioTypeWarning logzioMessageType = "warning"
-// 	logzioTypeError   logzioMessageType = "error"
-// )
-
-// var hostMsg hostMessage
-// var scalingMsg message
-
-// func (sender *logzioSender) send(payload string, msgType logzioMessageType) {
-// 	var (
-// 		byteMsg []byte
-// 		err     error
-// 	)
-
-// 	if hostMsg != (hostMessage{}) {
-// 		hostMsg.Message = payload
-// 		hostMsg.Type = string(msgType)
-// 		byteMsg, err = json.Marshal(hostMsg)
-// 	} else if scalingMsg != (message{}) {
-// 		scalingMsg.Message = payload
-// 		scalingMsg.Type = string(msgType)
-// 		byteMsg, err = json.Marshal(scalingMsg)
-// 	} else {
-// 		return
-// 	}
-
-// 	if err != nil {
-// 		log.Print(utils.ColorRed(utils.Sprintf("Couldn't marshal payload for logz.io. Error: %s", err)))
-// 		return
-// 	}
-
-// 	err = (*logzio.LogzioSender)(sender).Send(byteMsg)
-// 	if err != nil {
-// 		log.Print(utils.ColorRed(utils.Sprintf("Couldn't send payload to logz.io. Error: %s", err)))
-// 		return
-// 	}
-// }
-
-// func initializeLogzIO() (*logzioSender, error) {
-// 	if usingProdLogging() {
-// 		Info("Setting up logz.io integration.")
-// 	} else {
-// 		Info("Not setting up logz.io integration.")
-// 		return nil, nil
-// 	}
-
-// 	logzioShippingToken := os.Getenv("LOGZIO_SHIPPING_TOKEN")
-
-// 	if logzioShippingToken == "" {
-// 		return nil, utils.MakeError("Error initializing logz.io integration: logzioShippingToken is uninitialized")
-// 	}
-
-// 	sender, err := logzio.New(
-// 		logzioShippingToken,
-// 		logzio.SetUrl("https://listener.logz.io:8071"),
-// 		logzio.SetDrainDuration(time.Second*3),
-// 		logzio.SetCheckDiskSpace(false),
-// 	)
-// 	if err != nil {
-// 		return nil, utils.MakeError("Error initializing logz.io integration: %s", err)
-// 	}
-
-// 	return (*logzioSender)(sender), nil
-// }
-
-// // FlushLogzio flushes events in the Logzio queue but does not stop new ones from being recorded.
-// func FlushLogzio() {
-// 	if logzioTransport != nil {
-// 		if err := (*logzio.LogzioSender)(logzioTransport).Sync(); err != nil {
-// 			Errorf("Unable to flush logzio: %s", err)
-// 			return
-// 		}
-
-// 		(*logzio.LogzioSender)(logzioTransport).Drain()
-// 	}
-// }
-
-// // stopAndDrainLogzio flushes events in the Logzio queue and stops new ones
-// // from being recorded.
-// func stopAndDrainLogzio() {
-// 	if logzioTransport != nil {
-// 		(*logzio.LogzioSender)(logzioTransport).Stop()
-// 	}
-// }
--- backend/services/whistlogger/sentry.go
+++ backend/services/whistlogger/sentry.go
@@ -2,6 +2,7 @@ package whistlogger // import "github.com/whisthq/whist/backend/services/whistlo
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/getsentry/sentry-go"
@@ -16,10 +17,9 @@ type sentryCore struct {
 }
 
 func NewSentryCore(encoder zapcore.Encoder, levelEnab zapcore.LevelEnabler) zapcore.Core {
-	// sentryDsn := os.Getenv("SENTRY_DSN")
-	sentryDSN := "https://774bb2996acb4696944e1c847c41773c@o400459.ingest.sentry.io/5461239"
+	sentryDsn := os.Getenv("SENTRY_DSN")
 	err := sentry.Init(sentry.ClientOptions{
-		Dsn:         sentryDSN,
+		Dsn:         sentryDsn,
 		Release:     metadata.GetGitCommit(),
 		Environment: string(metadata.GetAppEnvironment()),
 	})
@@ -98,61 +98,3 @@ func (lc *sentryCore) Sync() error {
 
 	return nil
 }
-
-// import (
-// 	"log"
-// 	"os"
-// 	"time"
-
-// 	"github.com/getsentry/sentry-go"
-// 	"github.com/whisthq/whist/backend/services/metadata"
-
-// 	"github.com/whisthq/whist/backend/services/utils"
-// )
-
-// type sentrySender struct{}
-
-// // We use a pointer of this type so we can check if it is nil in our logging
-// // functions, and therefore always call them safely.
-// var sentryTransport *sentrySender
-
-// func (*sentrySender) send(err error) {
-// 	sentry.CaptureException(err)
-// }
-
-// // InitializeSentry initializes Sentry for use. It requires a configuration function `f`
-// // as an argument where the Sentry tags and messages should be set.
-// func initializeSentry(f func(scope *sentry.Scope)) (*sentrySender, error) {
-// 	if usingProdLogging() {
-// 		log.Print("Setting up Sentry.")
-// 	} else {
-// 		log.Print("Not setting up Sentry.")
-// 		return nil, nil
-// 	}
-
-// 	sentryDsn := os.Getenv("SENTRY_DSN")
-
-// 	if sentryDsn == "" {
-// 		return nil, utils.MakeError("Error initializing Sentry integration: Sentry Dsn is uninitialized")
-// 	}
-
-// 	err := sentry.Init(sentry.ClientOptions{
-// 		Dsn:         sentryDsn,
-// 		Release:     metadata.GetGitCommit(),
-// 		Environment: string(metadata.GetAppEnvironment()),
-// 	})
-// 	if err != nil {
-// 		return nil, utils.MakeError("Error calling Sentry.init: %v", err)
-// 	}
-// 	log.Printf("Set Sentry release to git commit hash: %s", metadata.GetGitCommit())
-
-// 	// Configure Sentry's scope with some instance-specific information
-// 	sentry.ConfigureScope(f)
-
-// 	return new(sentrySender), nil
-// }
-
-// // FlushSentry flushes events in the Sentry queue
-// func FlushSentry() {
-// 	sentry.Flush(5 * time.Second)
-// }
