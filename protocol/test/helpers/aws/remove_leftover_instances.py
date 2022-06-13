924a7353d09fe01e4dcc5a05e281ed926788bda8
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-06-13 19:19:55 +0200

    bug fix

--- protocol/test/helpers/aws/remove_leftover_instances.py
+++ protocol/test/helpers/aws/remove_leftover_instances.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         name_tag_match = {"Name": "tag:Name", "Values": [instance_name]}
         instance_creator_match = {"Name": "tag:RunID", "Values": [github_run_id]}
         boto3client = boto3.client("ec2", region_name=region_name)
-        response = boto3client.describe_instances(Filters=[name_tag_match, name_tag_match])
+        response = boto3client.describe_instances(Filters=[name_tag_match, instance_creator_match])
 
         leftover_instances = []
 
