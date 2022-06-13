151ca6dc66ae3135e0e733c25ea5275b33da13a8
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-06-13 22:20:09 +0000

    fixes

--- backend/services/host-service/mandelbox/init_browser_data_test.go
+++ backend/services/host-service/mandelbox/init_browser_data_test.go
@@ -250,7 +250,7 @@ func TestUserInitialBrowserParseEmpty(t *testing.T) {
 }
 
 func TestUserInitialBrowserParseEmptyBookmarks(t *testing.T) {
-	// Define browser data
+	// Define variables that will be reused
 	testCookie1 := "{'creation_utc': 13280861983875934, 'host_key': 'test_host_key_1.com'}"
 	testCookie2 := "{'creation_utc': 4228086198342934, 'host_key': 'test_host_key_2.com'}"
 
@@ -258,7 +258,8 @@ func TestUserInitialBrowserParseEmptyBookmarks(t *testing.T) {
 	cookiesJSON := "[" + testCookie1 + "," + testCookie2 + "]"
 	extensions := "not_real_extension_id,not_real_second_extension_id"
 
-	// Create browser data
+	
+	// 1. Test case where no bookmark variable is defined (the stringified JSON will not have a `bookmars` variable)
 	userInitialBrowserData := BrowserData{
 		CookiesJSON: types.Cookies(cookiesJSON),
 		Extensions:  types.Extensions(extensions),
@@ -290,7 +291,8 @@ func TestUserInitialBrowserParseEmptyBookmarks(t *testing.T) {
 		t.Fatalf("UnmarshalBrowserData returned %v, expected %v", unmarshalledBrowserData, userInitialBrowserData)
 	}
 
-	// Create browser data
+
+	// 2. Test case where bookmarks are nil
 	userInitialBrowserData = BrowserData{
 		CookiesJSON: types.Cookies(cookiesJSON),
 		Bookmarks:   nil,
@@ -324,7 +326,10 @@ func TestUserInitialBrowserParseEmptyBookmarks(t *testing.T) {
 	}
 
 
+	// 3. Test case where bookmars are defined as an empty string in JSON
 	browserDataString := `{"cookiesJSON":"[{'creation_utc': 13280861983875934, 'host_key': 'test_host_key_1.com'},{'creation_utc': 4228086198342934, 'host_key': 'test_host_key_2.com'}]","bookmarks":"", "extensions":"not_real_extension_id,not_real_second_extension_id"}`
+	expectedBookmarks = configutils.Bookmarks{}
+	
 	deflatedBrowserData, err = configutils.GzipDeflateString(string(browserDataString))
 	if err != nil {
 		t.Fatalf("could not deflate browser data: %v", err)
@@ -340,6 +345,12 @@ func TestUserInitialBrowserParseEmptyBookmarks(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Error unmarshalling user browser data: %v", err)
 	}
+
+	if !cmp.Equal(*unmarshalledBrowserData.Bookmarks, expectedBookmarks, cmpopts.EquateEmpty()) {
+		t.Fatalf("UnmarshalBrowserData returned Bookmarks: %v, expected Bookmarks: %v", *unmarshalledBrowserData.Bookmarks, expectedBookmarks)
+	}
+
+	userInitialBrowserData.Bookmarks = &expectedBookmarks
 
 	if !cmp.Equal(unmarshalledBrowserData, userInitialBrowserData, cmpopts.EquateEmpty()) {
 		t.Fatalf("UnmarshalBrowserData returned %v, expected %v", unmarshalledBrowserData, userInitialBrowserData)
