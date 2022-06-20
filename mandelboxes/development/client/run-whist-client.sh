3a4ca1eedf820ec7d8d1f77465013620c07c0a4d
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2022-06-20 11:40:38 -0400

    piping args to client

--- mandelboxes/development/client/run-whist-client.sh
+++ mandelboxes/development/client/run-whist-client.sh
@@ -8,67 +8,50 @@ set -Eeuo pipefail
 #WHIST_MAPPINGS_DIR=/whist/resourceMappings
 PROTOCOL_LOG_FILENAME=/usr/share/whist/client.log
 SERVER_IP_ADDRESS=127.0.0.1
-SERVER_PORT_32262=""
-SERVER_PORT_32263=""
-SERVER_PORT_32273=""
+SERVER_PORT_MAPPINGS=""
 SERVER_AES_KEY=""
+INITIAL_URLS=""
 
 # Create list of command-line arguments to pass to the Whist protocol client
 OPTIONS=""
 
-# Sample JSON: {"dev_client_server_ip": "35.170.79.124", "dev_client_server_port_32262": 40618, "dev_client_server_port_32263": 31680, "dev_client_server_port_32273": 5923, "dev_client_server_aes_key": "70512c062ff1101f253be70e4cac81bc"}
+# Sample JSON: {"dev_client_server_ip": "35.170.79.124", "dev_client_server_port_mappings": "32262:19020.32263:17242.32273:29843", "dev_client_server_aes_key": "70512c062ff1101f253be70e4cac81bc"}
 WHIST_JSON_FILE=/whist/resourceMappings/config.json
 if [[ -f $WHIST_JSON_FILE ]]; then
   if [ "$( jq -rc 'has("dev_client_server_ip")' < $WHIST_JSON_FILE )" == "true"  ]; then
     SERVER_IP_ADDRESS="$(jq -rc '.dev_client_server_ip' < $WHIST_JSON_FILE)"
-    # Add server IP address to options
-    OPTIONS="$OPTIONS $SERVER_IP_ADDRESS"
   fi
-  if [ "$( jq -rc 'has("dev_client_server_port_32262")' < $WHIST_JSON_FILE )" == "true"  ]; then
-    SERVER_PORT_32262="$(jq -rc '.dev_client_server_port_32262' < $WHIST_JSON_FILE)"
-    # Add server port 32262 address to options
-    OPTIONS="$OPTIONS -p32262:$SERVER_PORT_32262"
+  if [ "$( jq -rc 'has("dev_client_server_port_mappings")' < $WHIST_JSON_FILE )" == "true"  ]; then
+    SERVER_PORT_MAPPINGS="$(jq -rc '.dev_client_server_port_mappings' < $WHIST_JSON_FILE)"
   else
-    echo "Server port 32262 not found in JSON data!"
-    exit 1
-  fi
-  if [ "$( jq -rc 'has("dev_client_server_port_32263")' < $WHIST_JSON_FILE )" == "true"  ]; then
-    SERVER_PORT_32263="$(jq -rc '.dev_client_server_port_32263' < $WHIST_JSON_FILE)"
-    # Add server port 32263 address to options
-    OPTIONS="$OPTIONS.32263:$SERVER_PORT_32263"
-  else
-    echo "Server port 32263 not found in JSON data!"
-    exit 1
-  fi
-  if [ "$( jq -rc 'has("dev_client_server_port_32273")' < $WHIST_JSON_FILE )" == "true"  ]; then
-    SERVER_PORT_32273="$(jq -rc '.dev_client_server_port_32273' < $WHIST_JSON_FILE)"
-    # Add server port 32273 address to options
-    OPTIONS="$OPTIONS.32273:$SERVER_PORT_32273"
-  else
-    echo "Server port 32273 not found in JSON data!"
+    echo "Server port mappings not found in JSON data!"
     exit 1
   fi
   if [ "$( jq -rc 'has("dev_client_server_aes_key")' < $WHIST_JSON_FILE )" == "true"  ]; then
     SERVER_AES_KEY="$(jq -rc '.dev_client_server_aes_key' < $WHIST_JSON_FILE)"
-    # Add server AES key address to options
-    OPTIONS="$OPTIONS -k $SERVER_AES_KEY"
   else
     echo "Server AES key not found in JSON data!"
     exit 1
   fi
-  if [ "$( jq -rc 'has("initial_url")' < $WHIST_JSON_FILE )" == "true"  ]; then
-    INITIAL_URL="$(jq -rc '.initial_url' < $WHIST_JSON_FILE)"
-    # Add initial url to options
-    OPTIONS="$OPTIONS -x $INITIAL_URL"
+  if [ "$( jq -rc 'has("initial_urls")' < $WHIST_JSON_FILE )" == "true"  ]; then
+    INITIAL_URLS="$(jq -rc '.initial_urls' < $WHIST_JSON_FILE)"
   else
-    echo "Initial URL not found in JSON data!"
-    exit 1
+    echo "Initial URL(s) not found in JSON data!"
   fi
 fi
 
+# Use pipe to pass arguments to the protocol to be able to open multiple URLs
+(
+  cat <<EOF
+ports?$SERVER_PORT_MAPPINGS
+private-key?$SERVER_AES_KEY
+server-ip?$SERVER_IP_ADDRESS
+open-url?$INITIAL_URLS
+finished
+EOF
+) | timeout 240s /usr/share/whist/WhistClient --dynamic-arguments &> >(tee $PROTOCOL_LOG_FILENAME) &
 # The point of the named pipe redirection is so that $! will give us the PID of WhistServer, not of tee.
 # Timeout will turn off the client once we are done gathering metrics data. This value here should match the one in the protocol/test/streaming_e2e_tester.py file
-timeout 240s /usr/share/whist/WhistClient $OPTIONS &> >(tee $PROTOCOL_LOG_FILENAME) &
 whist_client_pid=$!
 
 # TODO: once our mandelboxes have bash 5.1 we will be able to deduce _which_
