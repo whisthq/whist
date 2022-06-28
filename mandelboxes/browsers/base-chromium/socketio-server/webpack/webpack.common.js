523bc0b70491069c5b2b440f949424966429278a
Ming Ying <ming@Mings-Mac-Studio.local>
2022-06-28 13:18:44 -0400

    Remove webpack-node-externals

--- mandelboxes/browsers/base-chromium/socketio-server/webpack/webpack.common.js
+++ mandelboxes/browsers/base-chromium/socketio-server/webpack/webpack.common.js
@@ -1,5 +1,4 @@
 const path = require("path")
-const nodeExternals = require('webpack-node-externals');
 
 const srcDir = path.join(__dirname, "..", "src")
 const outDir = path.join(__dirname, "..", "build")
