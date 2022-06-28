4535290e0c98481a94ccf7aaf3eac96ced73a30e
Mauricio <mauricio@whist.com>
2022-06-28 15:48:54 -0500

    Add correct bucket name

--- backend/infrastructure/modules/aws_s3_control/main.tf
+++ backend/infrastructure/modules/aws_s3_control/main.tf
@@ -2,12 +2,10 @@ resource "aws_s3control_multi_region_access_point" "example" {
   details {
     name = "user-configs-access-point"
 
-    dynamic {
+    dynamic "region" {
       for_each = var.regions
       content {
-        region {
-          bucket = aws_s3_bucket.foo_bucket.id
-        }
+          bucket = "whist-user-app-configs-${region}-${var.env}"
       }
     }
   }
