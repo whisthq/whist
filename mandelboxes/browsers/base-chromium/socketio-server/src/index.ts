0c67cec39d36352b6002fa867e4f7045062de78b
Ming <ming@whist.com>
2022-06-28 16:32:35 -0400

    Add connection acknowledgement and message forwarding to socketio server

--- mandelboxes/browsers/base-chromium/socketio-server/src/index.ts
+++ mandelboxes/browsers/base-chromium/socketio-server/src/index.ts
@@ -1,19 +1,23 @@
 import express from "express"
 import http from "http"
-import { Server as Socketio } from "socket.io"
+import { Server as Socketio, Socket } from "socket.io"
 
+// Initialize socket.io server
 const expressServer = express()
 const server = http.createServer(expressServer)
 const io = new Socketio(server, {
-    cors: {
-        origin: "*",
-    },
+  cors: {
+    origin: "*",
+  },
 })
 
-console.log("Server has started")
-
-io.on("connection", () => {
-    console.log("Socket connected!")
+// Listens for client/server events and broadcasts them to the other side
+io.on("connection", (socket: Socket) => {
+  socket.emit("connected")
+  socket.onAny((eventName, ...args) => {
+    socket.broadcast.emit(eventName, args)
+  })
 })
 
+// Listens for connections on port 32261
 server.listen(32261)
