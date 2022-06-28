306e0d3fbf3522c5a17604f701689769450cbd42
Ming <ming@whist.com>
2022-06-28 15:11:42 -0400

    add transport websocket

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
@@ -4,6 +4,7 @@ const initSocketioConnection = () => {
   console.log("trying to connect")
   const socket = io(`http://localhost:32261`, {
     reconnectionDelayMax: 10000,
+    transports: ["websocket"]
   })
 
   socket.on("connect_error", (err) => {
