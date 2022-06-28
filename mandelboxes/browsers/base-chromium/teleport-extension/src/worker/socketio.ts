0a2bc36a06866996462b6cc2b5217d0c2f258b00
Ming <ming@whist.com>
2022-06-28 18:42:48 -0400

    added TODO function for illustration

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client"
 
 import { SOCKETIO_SERVER_URL } from "@app/constants/urls"
+import { Socket } from "socket.io-client"
 
 const initSocketioConnection = () => {
   const socket = io(SOCKETIO_SERVER_URL, {
@@ -11,4 +12,19 @@ const initSocketioConnection = () => {
   return socket
 }
 
-export { initSocketioConnection }
+// TODO: Implement socketio event handler
+const initSocketioListener = (socket: Socket) => {
+    // CLIENT SIDE:
+
+    // socket.emit("open-url", {
+    //  newTab: true,
+    //  url: "https://google.com"   
+    // })
+
+    // SERVER SIDE (i.e. here):
+    // socket.on("open-url", (payload) =>{
+    //     openUrl(payload.url, payload.newTab)
+    // })
+}
+
+export { initSocketioConnection, initSocketioListener }
