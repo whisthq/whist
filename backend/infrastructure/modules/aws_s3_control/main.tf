94cacd8acf817a7d67407b82153183dab085b8b3
Mauricio <mauricio@whist.com>
2022-06-28 16:16:54 -0500

    Use value of region variable

--- backend/infrastructure/modules/aws_s3_control/main.tf
+++ backend/infrastructure/modules/aws_s3_control/main.tf
@@ -1,11 +1,11 @@
-resource "aws_s3control_multi_region_access_point" "example" {
+resource "aws_s3control_multi_region_access_point" "user-configs-access-point" {
   details {
     name = "user-configs-access-point"
 
     dynamic "region" {
       for_each = var.regions
       content {
-          bucket = "whist-user-app-configs-${region}-${var.env}"
+          bucket = "whist-user-app-configs-${region["value"]}-${var.env}"
       }
     }
   }
