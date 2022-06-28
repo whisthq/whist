1ce8b5015b02adc0819c009d380c870bf8a92daa
Ming <ming@whist.com>
2022-06-28 15:08:15 -0400

    add error catching

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
@@ -1,10 +1,14 @@
 import { io } from "socket.io-client"
 
 const initSocketioConnection = () => {
-    console.log("trying to connect")
-    io(`http://localhost:32261`, {
-        reconnectionDelayMax: 10000,
-    })
+  console.log("trying to connect")
+  const socket = io(`http://localhost:32261`, {
+    reconnectionDelayMax: 10000,
+  })
+
+  socket.on("connect_error", (err) => {
+    console.log("connection error", err)
+  })
 }
 
 export { initSocketioConnection }
