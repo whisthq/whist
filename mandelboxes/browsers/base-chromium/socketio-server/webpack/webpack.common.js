d2b4cccac6d8410a75644a460289070b9b9dc548
Ming Ying <ming@Mings-Mac-Studio.local>
2022-06-28 13:17:17 -0400

    Introduce webpack into socketio server

---
+++ mandelboxes/browsers/base-chromium/socketio-server/webpack/webpack.common.js
@@ -0,0 +1,27 @@
+const path = require("path")
+const nodeExternals = require('webpack-node-externals');
+
+const srcDir = path.join(__dirname, "..", "src")
+const outDir = path.join(__dirname, "..", "build")
+
+module.exports = {
+  entry: {
+    index: path.join(srcDir, "index.ts")
+  },
+  target: "node",
+  output: {
+    path: outDir,
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      "@app": srcDir,
+    },
+  },
+  externals: {
+    modules: [path.join(srcDir, "..", "node_modules")],
+    bufferutil: "commonjs bufferutil",
+    "utf-8-validate": "commonjs utf-8-validate"
+  },
+}
