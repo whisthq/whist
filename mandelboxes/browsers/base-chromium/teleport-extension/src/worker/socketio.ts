a13ebeeca8ff3599e1dbc498fdd8542c4ace11b6
Ming <ming@whist.com>
2022-06-28 15:15:01 -0400

    change ip to 127.0.0.1

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/socketio.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client"
 
 const initSocketioConnection = () => {
   console.log("trying to connect")
-  const socket = io(`http://localhost:32261`, {
+  const socket = io(`http://127.0.0.1:32261`, {
     reconnectionDelayMax: 10000,
     transports: ["websocket"]
   })
