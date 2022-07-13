bb6694630544fb0947ac13494262350ba049889d
yancey <yancey@whist.com>
2022-07-13 12:29:13 -0400

    try to fix

--- protocol/run-clang-tidy.sh
+++ protocol/run-clang-tidy.sh
@@ -80,6 +80,7 @@ if [[ -n "$isWindows" ]]; then
   sed -i 's/\/external:W0//g' "$compileCommands"
   sed -i 's/\/external:I/-I/g' "$compileCommands"
   sed -i 's/\/MP//g' "$compileCommands"
+  sed -i 's/-external:W0//g' "$compileCommands"
 fi
 
 if [[ ! -f ".clang-tidy" ]]; then
