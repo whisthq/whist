26ccc364a2ee43cc9fafc1e1e63c6d34e893d167
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-07-15 11:17:09 -0400

    download fonts only if changed

--- mandelboxes/build.sh
+++ mandelboxes/build.sh
@@ -85,7 +85,7 @@ mv nvidia-driver-installer.run base/build-assets/build-temp/nvidia-driver
 # Copy the fonts used in Whist
 echo "Fetching the Whist fonts..."
 mkdir base/build-assets/build-temp/fonts
-aws s3 cp --only-show-errors s3://whist-fonts base/build-assets/build-temp/fonts --recursive
+aws s3 sync s3://whist-fonts base/build-assets/build-temp/fonts
 
 # Bundle these build assets into a cached Docker image
 echo "Bundling build assets..."
