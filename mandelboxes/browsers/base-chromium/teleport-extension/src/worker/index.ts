eae329d2b41f4a756f74929af3395e4674c228ff
Ming <ming@whist.com>
2022-07-22 11:03:06 -0400

    log chrome storage

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/index.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/index.ts
@@ -20,6 +20,15 @@ import {
   initUpdateTabIDListener,
 } from "./socketio"
 
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
+
 const socket = initSocketioConnection()
 const nativeHostPort = initNativeHostIpc()
 
