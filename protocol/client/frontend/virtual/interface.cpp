d5470473b8e7f29cff9c97f5e3e3f0405b3f09de
Nicholas Pipitone <npip99@gmail.com>
2022-07-22 11:44:24 -0400

    Change interface to have multiple video frame subscribers

--- protocol/client/frontend/virtual/interface.cpp
+++ protocol/client/frontend/virtual/interface.cpp
@@ -1,9 +1,13 @@
+#include <whist/core/whist.h>
+#include <mutex>
+extern "C" {
 #include "common.h"
 // This is a crutch. Once video is callback-ized we won't need it anymore.
 #define FROM_WHIST_PROTOCOL true
 #include "interface.h"
 #include <whist/utils/queue.h>
 #include <whist/network/network_algorithm.h>
+}
 
 // Just chosen a very large number for events queue size. If required we can optimize/reduce it.
 #define MAX_EVENTS_QUEUED 10000
@@ -16,13 +20,19 @@ OnFileUploadCallback on_file_upload = NULL;
 OnFileDownloadStart on_file_download_start = NULL;
 OnFileDownloadUpdate on_file_download_update = NULL;
 OnFileDownloadComplete on_file_download_complete = NULL;
-static VideoFrameCallback video_frame_callback_ptr = NULL;
 
 static WhistMutex lock;
 static AVFrame* pending = NULL;
 static bool connected = false;
 static int requested_width;
 static int requested_height;
+
+// Whist window management
+struct WhistWindowInformation {
+    VideoFrameCallback video_frame_callback_ptr;
+};
+static std::mutex whist_window_mutex;
+static std::map<int, WhistWindowInformation> whist_windows;
 
 static void virtual_interface_connect(void) {
     lock = whist_create_mutex();
@@ -35,18 +45,15 @@ static void virtual_interface_set_on_cursor_change_callback(OnCursorChangeCallba
 }
 
 static void* virtual_interface_get_frame_ref(void) {
-    whist_lock_mutex(lock);
-    // Ensure that the interface isn't in callback mode
-    FATAL_ASSERT(video_frame_callback_ptr == NULL);
+    std::lock_guard<std::mutex> guard(whist_window_mutex);
     // Consume the pending AVFrame, and return it
-    void* out = pending;
+    void* frame_ref = pending;
     pending = NULL;
-    whist_unlock_mutex(lock);
-    return out;
+    return frame_ref;
 }
 
 static void* virtual_interface_get_handle_from_frame_ref(void* frame_ref) {
-    AVFrame* frame = frame_ref;
+    AVFrame* frame = (AVFrame*)frame_ref;
     // Assuming we want CVPixelBufferRef for now
     return frame->data[3];
 }
@@ -54,7 +61,7 @@ static void* virtual_interface_get_handle_from_frame_ref(void* frame_ref) {
 static void virtual_interface_get_frame_ref_yuv_data(void* frame_ref, uint8_t*** data,
                                                      int** linesize, int* width, int* height,
                                                      int* visible_width, int* visible_height) {
-    AVFrame* frame = frame_ref;
+    AVFrame* frame = (AVFrame*)frame_ref;
     *data = frame->data;
     *linesize = frame->linesize;
     *width = frame->width;
@@ -74,34 +81,39 @@ static void virtual_interface_get_frame_ref_yuv_data(void* frame_ref, uint8_t***
 }
 
 static void virtual_interface_free_frame_ref(void* frame_ref) {
-    AVFrame* frame = frame_ref;
+    AVFrame* frame = (AVFrame*)frame_ref;
     av_frame_free(&frame);
 }
 
 void virtual_interface_send_frame(AVFrame* frame) {
     if (!connected) return;
+    std::lock_guard<std::mutex> guard(whist_window_mutex);
 
-    whist_lock_mutex(lock);
-    if (video_frame_callback_ptr != NULL) {
-        // If we have a callback, pass into the callback
-        AVFrame* new_frame = av_frame_clone(frame);
-        video_frame_callback_ptr((void*)new_frame);
-    } else {
-        // Otherwise, keep the frame as pending
-        av_frame_free(&pending);
-        pending = av_frame_clone(frame);
+    // Update the pending frame
+    av_frame_free(&pending);
+    pending = av_frame_clone(frame);
+
+    // For each window,
+    for (auto const& [window_id, window_info] : whist_windows) {
+        // If that window wants a callback, give it
+        if (window_info.video_frame_callback_ptr) {
+            AVFrame* frame_ref = av_frame_clone(frame);
+            window_info.video_frame_callback_ptr(window_id, frame_ref);
+        }
     }
-    whist_unlock_mutex(lock);
 }
 
-static void virtual_interface_set_video_frame_callback(VideoFrameCallback callback_ptr) {
-    whist_lock_mutex(lock);
-    // If we're newly setting this callback, free any pending frames
-    if (video_frame_callback_ptr == NULL) {
-        av_frame_free(&pending);
+static void virtual_interface_set_video_frame_callback(int window_id,
+                                                       VideoFrameCallback callback_ptr) {
+    std::lock_guard<std::mutex> guard(whist_window_mutex);
+    // If there's a pending avframe, and that window hasn't been capturing yet,
+    // pass the existant frame into the callback ptr
+    if (pending != NULL && whist_windows[window_id].video_frame_callback_ptr == NULL) {
+        AVFrame* frame_ref = av_frame_clone(pending);
+        callback_ptr(window_id, frame_ref);
     }
-    video_frame_callback_ptr = callback_ptr;
-    whist_unlock_mutex(lock);
+    // Store the callback ptr for future frames
+    whist_windows[window_id].video_frame_callback_ptr = callback_ptr;
 }
 
 static void virtual_interface_disconnect(void) {
@@ -136,12 +148,29 @@ static void virtual_interface_set_on_file_download_complete_callback(OnFileDownl
     on_file_download_complete = cb;
 }
 
+static int virtual_interface_create_window(void) {
+    std::lock_guard<std::mutex> guard(whist_window_mutex);
+    // Use serial window IDs, so that each window gets a unique ID
+    static int serial_window_ids = 1;
+    int next_window_id = serial_window_ids++;
+    // Store window info, and return the new window ID
+    whist_windows[next_window_id] = {};
+    return next_window_id;
+}
+
+static void virtual_interface_destroy_window(int window_id) {
+    std::lock_guard<std::mutex> guard(whist_window_mutex);
+    whist_windows.erase(window_id);
+}
+
 static void virtual_interface_set_callback_context(void* context) { callback_context = context; }
 
 static const VirtualInterface vi = {
     .lifecycle =
         {
             .connect = virtual_interface_connect,
+            .create_window = virtual_interface_create_window,
+            .destroy_window = virtual_interface_destroy_window,
             .set_callback_context = virtual_interface_set_callback_context,
             .start = whist_client_main,
             .disconnect = virtual_interface_disconnect,
