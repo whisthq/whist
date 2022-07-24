c943dd75c9cd2147496caee8b792e5b9839c1ba7
Ming <ming@whist.com>
2022-07-24 13:22:26 -0400

    Update ipc.ts
    Signed-off-by: Ming <ming@whist.com>

--- mandelboxes/browsers/base-chromium/teleport-extension/src/constants/ipc.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/constants/ipc.ts
@@ -8,7 +8,6 @@ export enum NativeHostMessageType {
   NATIVE_HOST_EXIT = "NATIVE_HOST_EXIT",
   NATIVE_HOST_UPDATE = "NATIVE_HOST_UPDATE",
   POINTER_LOCK = "POINTER_LOCK",
-  KEEP_ALIVE = "KEEP_ALIVE"
 }
 
 export interface ContentScriptMessage {
