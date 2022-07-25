0b91732f777db81ea334d48341b7f8c03c0ad247
Nicholas Pipitone <npip99@gmail.com>
2022-07-25 06:47:21 -0400

    Chrome multitab support (#6948)
    * Change interface to have multiple video frame subscribers
    
    * Let globals be extern-able from C

--- protocol/client/frontend/virtual/interface.h
+++ protocol/client/frontend/virtual/interface.h
@@ -10,11 +10,13 @@ typedef void (*OnCursorChangeCallback)(void* data, const char* cursor_type,
 typedef void* (*OnFileDownloadStart)(const char* file_path, int64_t file_size);
 typedef void (*OnFileDownloadUpdate)(void* opaque, int64_t bytes_so_far, int64_t bytes_per_sec);
 typedef void (*OnFileDownloadComplete)(void* opaque);
-typedef void (*VideoFrameCallback)(void* frame_ref);
+typedef void (*VideoFrameCallback)(int window_id, void* frame_ref);
 
 typedef struct VirtualInterface {
     struct {
         void (*connect)(void);
+        int (*create_window)(void);
+        void (*destroy_window)(int window_id);
         void (*set_callback_context)(void* context);
         int (*start)(int argc, const char* argv[]);
         void (*disconnect)(void);
@@ -26,7 +28,7 @@ typedef struct VirtualInterface {
                                        int* height, int* visible_width, int* visible_height);
         void (*free_frame_ref)(void* frame_ref);
         void (*set_on_cursor_change_callback)(OnCursorChangeCallback cb);
-        void (*set_video_frame_callback)(VideoFrameCallback cb);
+        void (*set_video_frame_callback)(int window_id, VideoFrameCallback cb);
     } video;
     struct {
         void (*send)(const WhistFrontendEvent* event);
