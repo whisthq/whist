bec25dbc62497b4187f6b7c7b5a54722a36a56b4
Mauricio Araujo <mauricio@whist.com>
2022-07-25 13:53:25 -0500

    Add tests for parse request

--- backend/services/httputils/httputils_test.go
+++ backend/services/httputils/httputils_test.go
@@ -3,7 +3,12 @@ package httputils
 import (
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/google/uuid"
+	"github.com/whisthq/whist/backend/services/types"
 )
 
 func TestGetAccessToken(t *testing.T) {
@@ -34,6 +39,65 @@ func TestGetAccessToken(t *testing.T) {
 }
 
 func TestParseRequest(t *testing.T) {
+	var tests = []struct {
+		name     string
+		request  ServerRequest
+		jsonBody string
+		expected ServerRequest
+	}{
+		{"Assign request", &MandelboxAssignRequest{}, `{
+			"regions": [
+				"us-east-1"
+			],
+			"client_commit_hash": "test_sha",
+			"user_email": "user@whist.com",
+			"version": "1.0.0",
+			"session_id": 1234567890
+		}`, &MandelboxAssignRequest{
+			Regions: []string{
+				"us-east-1",
+			},
+			CommitHash: "test_sha",
+			UserEmail:  "user@whist.com",
+			Version:    "1.0.0",
+			SessionID:  1234567890,
+		}},
+		{"JSON transport request", &JSONTransportRequest{}, `{
+			"ip": "0.0.0.0",
+			"app_name": "chrome",
+			"config_encryption_token": "test_config_encryption",
+			"mandelbox_id": "e0aa0e21-3350-49b6-95a6-152dd2a13e3c",
+			"is_new_config_encryption_token": false,
+			"json_data": "test_json_data",
+			"browser_data": "test_browser_data"
+
+		}`, &JSONTransportRequest{
+			IP:                    "0.0.0.0",
+			AppName:               "CHROME",
+			ConfigEncryptionToken: "test_config_encryption",
+			MandelboxID:           types.MandelboxID(uuid.MustParse("e0aa0e21-3350-49b6-95a6-152dd2a13e3c")),
+			IsNewConfigToken:      false,
+			JSONData:              types.JSONData(`"location":"Americas/NewYork"`),
+			BrowserData:           types.BrowserData(`"cookies": "test_cookies"`),
+		}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body := strings.NewReader(tt.jsonBody)
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodPost, "https://localhost", body)
+
+			_, err := ParseRequest(w, r, tt.request)
+			if err != nil {
+				t.Errorf("Did not expect error, got %s", err)
+			}
+
+			if ok := reflect.DeepEqual(reflect.TypeOf(tt.request).Elem(), reflect.TypeOf(tt.expected).Elem()); !ok {
+				t.Errorf("Expected request to be parsed to %v, got %v", tt.expected, tt.request)
+			}
+		})
+	}
 }
 
 func TestVerifyRequestType(t *testing.T) {
