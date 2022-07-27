37ef14fe0551cb4d99626d6d79b8da08df7de234
Ming <ming@whist.com>
2022-07-27 14:41:08 -0400

    correctly clear local storage on start

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/geolocation.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/geolocation.ts
@@ -3,7 +3,7 @@ import { injectResourceIntoDOM } from "@app/utils/dom"
 const initLocationSpoofer = () => {
   injectResourceIntoDOM(document, "js/geolocation.js")
 
-  chrome.storage.local.get(["geolocation"], (result) => {
+  chrome.storage.sync.get(["geolocation"], (result) => {
     const meta_longitude = document.createElement("meta")
     meta_longitude.name = "longitude"
     meta_longitude.content = result.geolocation.longitude
