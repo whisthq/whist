f203f666ca20a8666b9b51495970a48b8bc7dc71
Ming <ming@whist.com>
2022-08-02 14:36:19 -0400

    Add waitlist status to auth0 metadata

---
+++ backend/auth0/src/rules/add-waitlist-status.js
@@ -0,0 +1,12 @@
+/* add-waitlist-status.js
+ *
+ * Add the status of a user's waitlist status
+ * For now, every new user is automatically added to the waitlist
+ * 
+ */
+
+async function addWaitlistStatus(user, context, callback) {  
+    context.accessToken["waitlisted"] = true;
+    return callback(null, user, context);
+}
+  
