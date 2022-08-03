6f267d657d1081777555a139a239b8db14f3afdb
Ubuntu <ubuntu@ip-172-33-98-170.ec2.internal>
2022-08-03 19:50:21 +0000

    wip

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/cookies.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/cookies.ts
@@ -31,7 +31,46 @@ const initAddCookieListener = (socket: Socket) => {
     } as chrome.cookies.SetDetails
 
     alreadyAddedCookies.push(cookie)
+    console.log("Setting", cookie)
     chrome.cookies.set(details)
+  })
+
+  socket.on("waiting-cookies", (info: any) => {
+    console.log("Got waiting cookies", info)
+    while(info.length > 0) {
+      const cookie = info.shift()
+      if (cookie === undefined || cookie.cookie.name.startsWith("fractal")) 
+	 return
+
+      if (cookie.removed) {
+        console.log("removing", cookie.cookie)
+        const details = {
+          ...(!cookie.hostOnly && { domain: cookie.domain }),
+          ...(!cookie.session && { expirationDate: cookie.expirationDate }),
+          httpOnly: cookie.httpOnly,
+          name: cookie.name,
+          path: cookie.path,
+          sameSite: cookie.sameSite,
+          secure: cookie.secure,
+          storeId: cookie.storeId,
+          value: cookie.value,
+          url,
+        } as chrome.cookies.SetDetails
+
+        alreadyAddedCookies.push(cookie)
+        console.log("Setting", cookie)
+        chrome.cookies.set(details)
+      } else {
+        console.log("adding", cookie.cookie)
+	const url = cookie.domain.startsWith(".")
+          ? `https://${cookie.domain.slice(1)}`
+          : `https://${cookie.domain}`
+        chrome.cookies.remove({
+          name: cookie.name,
+          url,
+        })
+      }
+    }
   })
 }
 
@@ -41,6 +80,7 @@ const initRemoveCookieListener = (socket: Socket) => {
     const url = cookie.domain.startsWith(".")
       ? `https://${cookie.domain.slice(1)}`
       : `https://${cookie.domain}`
+    console.log("Removing", cookie)
     chrome.cookies.remove({
       name: cookie.name,
       url,
@@ -58,10 +98,13 @@ const initCookieAddedListener = (socket: Socket) => {
       if (
         find(alreadyAddedCookies, (c) => isEqual(c, details.cookie)) !==
         undefined
-      )
+      ) {
+	console.log("Flagged, already added", details.cookie)
         return
+      }
 
       if (!details.removed && details.cause === "explicit") {
+	console.log("Client should add", details.cookie)
         socket.emit("client-add-cookie", details.cookie)
       }
     }
@@ -79,6 +122,7 @@ const initCookieRemovedListener = (socket: Socket) => {
         details.removed &&
         ["expired", "expired_overwrite", "evicted"].includes(details.cause)
       ) {
+	console.log("Client should remove", details.cookie)
         socket.emit("client-remove-cookie", details.cookie)
       }
     }
