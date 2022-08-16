0195c71c4e35ef7d4c0e3a5674bedce2228b4f22
Ming <ming@whist.com>
2022-08-16 18:08:55 -0400

    implement set zoom

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/zoom.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/zoom.ts
@@ -1,8 +1,8 @@
 import { Socket } from "socket.io-client"
 
 const initZoomListener = (socket: Socket) => {
-  socket.on("zoom-tab", (args) => {
-    console.log("GOT ZOOM TAB", args)
+  socket.on("zoom-tab", ([zoomChangeInfo]: [{newZoomFactor: number, tabId: number}]) => {
+    chrome.tabs.setZoom(zoomChangeInfo.tabId, zoomChangeInfo.newZoomFactor,)
   })
 }
 
