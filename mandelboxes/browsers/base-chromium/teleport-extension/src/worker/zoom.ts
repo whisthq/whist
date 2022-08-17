3a92cfbff78d8d23ef5afacd75e3fd28aee20564
Ming <ming@whist.com>
2022-08-17 09:37:26 -0400

    fix server side zoom

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/zoom.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/zoom.ts
@@ -1,10 +1,22 @@
 import { Socket } from "socket.io-client"
+import find from "lodash.find"
+
+import { getOpenTabs } from "@app/utils/tabs"
 
 const initZoomListener = (socket: Socket) => {
-  socket.on("zoom-tab", ([zoomChangeInfo]: [{newZoomFactor: number, tabId: number}]) => {
-    console.log("setting", zoomChangeInfo.tabId, zoomChangeInfo.newZoomFactor)
-    chrome.tabs.setZoom(zoomChangeInfo.tabId, zoomChangeInfo.newZoomFactor)
-  })
+  socket.on(
+    "zoom-tab",
+    async ([zoomChangeInfo]: [{ newZoomFactor: number; tabId: number }]) => {
+      const openTabs = await getOpenTabs()
+      const foundTab = find(
+        openTabs,
+        (t) => t.clientTabId === zoomChangeInfo.tabId
+      )
+
+      if (foundTab?.tab.id !== undefined)
+        chrome.tabs.setZoom(foundTab?.tab.id, zoomChangeInfo.newZoomFactor)
+    }
+  )
 }
 
 export { initZoomListener }
