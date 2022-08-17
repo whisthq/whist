2de81d6034dbe476f0377122040aa48f10897876
Ming <ming@whist.com>
2022-08-17 13:47:25 -0400

    clean up worker/cookies.ts file

---
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/utils/cookies.ts
@@ -0,0 +1,20 @@
+const cookieToDetails = (cookie: chrome.cookies.Cookie) => {
+    const url = cookie.domain.startsWith(".")
+    ? `https://${cookie.domain.slice(1)}${cookie.path}`
+    : `https://${cookie.domain}${cookie.path}`
+
+  return {
+    ...(!cookie.hostOnly && { domain: cookie.domain }),
+    ...(!cookie.session && { expirationDate: cookie.expirationDate }),
+    httpOnly: cookie.httpOnly,
+    name: cookie.name,
+    path: cookie.path,
+    sameSite: cookie.sameSite,
+    secure: cookie.secure,
+    storeId: cookie.storeId,
+    value: cookie.value,
+    url,
+  } as chrome.cookies.SetDetails
+}
+
+export { cookieToDetails }
--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/cookies.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/cookies.ts
@@ -2,42 +2,22 @@ import { Socket } from "socket.io-client"
 import find from "lodash.find"
 import isEqual from "lodash.isequal"
 
+import { cookieToDetails } from "@app/utils/cookies"
+
 // v3 global variables don't persist, but we only need to store cookies we've already added
 // for a few seconds so we don't fall into a loop of adding a cookie and then thinking that
 // the cookie we've added was added by the user and not us
 let alreadyAddedCookies: chrome.cookies.Cookie[] = []
 
 const initAddCookieListener = (socket: Socket) => {
-  socket.on("server-add-cookie", (cookies: chrome.cookies.Cookie[]) => {
-    let cookie = cookies[0]
-
-    const url = cookie.domain.startsWith(".")
-      ? `https://${cookie.domain.slice(1)}${cookie.path}`
-      : `https://${cookie.domain}${cookie.path}`
-
-    // See https://developer.chrome.com/docs/extensions/reference/cookies/#method-set
-    // for why we construct the cookie details this way
-    const details = {
-      ...(!cookie.hostOnly && { domain: cookie.domain }),
-      ...(!cookie.session && { expirationDate: cookie.expirationDate }),
-      httpOnly: cookie.httpOnly,
-      name: cookie.name,
-      path: cookie.path,
-      sameSite: cookie.sameSite,
-      secure: cookie.secure,
-      storeId: cookie.storeId,
-      value: cookie.value,
-      url,
-    } as chrome.cookies.SetDetails
-
+  socket.on("server-add-cookie", ([cookie]: [chrome.cookies.Cookie]) => {
     alreadyAddedCookies.push(cookie)
-    chrome.cookies.set(details)
+    chrome.cookies.set(cookieToDetails(cookie))
   })
 }
 
 const initRemoveCookieListener = (socket: Socket) => {
-  socket.on("server-remove-cookie", (cookies: chrome.cookies.Cookie[]) => {
-    const cookie = cookies[0]
+  socket.on("server-remove-cookie", ([cookie]: [chrome.cookies.Cookie]) => {
     const url = cookie.domain.startsWith(".")
       ? `https://${cookie.domain.slice(1)}`
       : `https://${cookie.domain}`
@@ -54,25 +34,8 @@ const initCookieSyncHandler = (socket: Socket) => {
       const cookie = cookies.shift()
       if (cookie === undefined) return
 
-      const url = cookie.domain.startsWith(".")
-        ? `https://${cookie.domain.slice(1)}${cookie.path}`
-        : `https://${cookie.domain}${cookie.path}`
-
-      const details = {
-        ...(!cookie.hostOnly && { domain: cookie.domain }),
-        ...(!cookie.session && { expirationDate: cookie.expirationDate }),
-        httpOnly: cookie.httpOnly,
-        name: cookie.name,
-        path: cookie.path,
-        sameSite: cookie.sameSite,
-        secure: cookie.secure,
-        storeId: cookie.storeId,
-        value: cookie.value,
-        url,
-      } as chrome.cookies.SetDetails
-
       alreadyAddedCookies.push(cookie)
-      await chrome.cookies.set(details)
+      await chrome.cookies.set(cookieToDetails(cookie))
     }
   })
 }
