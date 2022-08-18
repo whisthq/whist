e71c7836d32f1d165225d6cab2c46675f8321eb5
Ming Ying <ming@whist.com>
2022-08-18 16:54:37 -0400

    try to wait for tab in new window

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/windows.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/windows.ts
@@ -1,12 +1,27 @@
 import { Socket } from "socket.io-client"
 
 const initWindowCreatedListener = (socket: Socket) => {
-  chrome.windows.onCreated.addListener((window) => {
-    socket.emit("server-window-created", window)
-    if(window.id !== undefined) chrome.windows.remove(window.id)
-  }, {
-    windowTypes: ["normal"],
-  })
+  chrome.windows.onCreated.addListener(
+    (window) => {
+      const listener = async (
+        _tabId: number,
+        changeInfo: { url?: string; title?: string; status?: string },
+        tab: chrome.tabs.Tab
+      ) => {
+        if (window.id !== undefined && tab.windowId === window.id) {
+          if (changeInfo.status === "loading" && tab.url !== undefined) {
+            socket.emit("server-window-created", { ...window, url: tab.url })
+            chrome.windows.remove(window.id)
+            chrome.windows.onCreated.removeListener(listener)
+          }
+        }
+      }
+      chrome.tabs.onUpdated.addListener(listener)
+    },
+    {
+      windowTypes: ["normal"],
+    }
+  )
 }
 
 export { initWindowCreatedListener }
