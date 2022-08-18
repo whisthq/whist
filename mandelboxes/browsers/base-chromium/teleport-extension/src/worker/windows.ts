616f55283161941571560a2a310bce912fb93c4f
Ming Ying <ming@whist.com>
2022-08-18 17:36:48 -0400

    new window done

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/windows.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/windows.ts
@@ -8,15 +8,6 @@ const initWindowCreatedListener = (socket: Socket) => {
         changeInfo: { url?: string; title?: string; status?: string },
         tab: chrome.tabs.Tab
       ) => {
-        // TECH DEBT: Because extensions can't run in incognito windows we have
-        // no way of detecting what URL they open in an incognito window
-        // For now, we just close all incognito windows, the long-term solution is 
-        // to remove the incognito window option from the right-click context menu
-        if (window.incognito && window.id !== undefined) {
-          chrome.windows.remove(window.id)
-          return
-        }
-
         if (window.id !== undefined && tab.windowId === window.id) {
           if (changeInfo.status === "loading" && tab.url !== undefined) {
             socket.emit("server-window-created", { ...window, url: tab.url })
