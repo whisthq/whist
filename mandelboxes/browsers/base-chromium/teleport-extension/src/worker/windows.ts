7a5d48617e44dd4e71cb9d125bfba9903f095bed
Ming Ying <ming@whist.com>
2022-08-18 16:14:12 -0400

    also remove window

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/windows.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/windows.ts
@@ -3,6 +3,7 @@ import { Socket } from "socket.io-client"
 const initWindowCreatedListener = (socket: Socket) => {
   chrome.windows.onCreated.addListener((window) => {
     socket.emit("server-window-created", window)
+    chrome.windows.remove(window.id)
   }, {
     windowTypes: ["normal"],
   })
