6671ab9cc84eaa7d1b29f5e6390e21ec09a632db
Ming <ming@whist.com>
2022-08-19 06:50:05 -0400

    Server side handling of new windows (#7102)
    * add listneer for newly created windows
    
    * also remove window
    
    * fix typeerror
    
    * try to wait for tab in new window
    
    * fix typo
    
    * try context menu
    
    * dont allow context menus
    
    * new window done
    
    * Update index.ts
    
    Signed-off-by: Ming <ming@whist.com>
    
    Signed-off-by: Ming <ming@whist.com>

---
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/windows.ts
@@ -0,0 +1,28 @@
+import { Socket } from "socket.io-client"
+
+const initWindowCreatedListener = (socket: Socket) => {
+  chrome.windows.onCreated.addListener(
+    (window) => {
+      const listener = (
+        _tabId: number,
+        changeInfo: { url?: string; title?: string; status?: string },
+        tab: chrome.tabs.Tab
+      ) => {
+        if (window.id !== undefined && tab.windowId === window.id) {
+          if (changeInfo.status === "loading" && tab.url !== undefined) {
+            socket.emit("server-window-created", { ...window, url: tab.url })
+            chrome.windows.remove(window.id)
+            chrome.tabs.onUpdated.removeListener(listener)
+          }
+        }
+      }
+
+      chrome.tabs.onUpdated.addListener(listener)
+    },
+    {
+      windowTypes: ["normal"],
+    }
+  )
+}
+
+export { initWindowCreatedListener }
