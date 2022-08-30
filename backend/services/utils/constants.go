3249702a5140636c5e13a3d159763ee762e6ff5a
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2022-08-30 13:12:27 -0400

    update var to const
    Signed-off-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>

--- backend/services/utils/constants.go
+++ backend/services/utils/constants.go
@@ -6,7 +6,7 @@ import "github.com/google/uuid"
 // service. They're used in a lot of packages, so we put them in the least
 // common denominator --- this package.
 
-var (
+const (
 	// This is the application which gets in the mandelboxes. We define it here as a constant since our
 	// backend assumes that it is uniform across the entire system. To change the application, either for
 	// testing or for production, simply change this constant to another Whist mandelbox, like `browsers/chrome`.
