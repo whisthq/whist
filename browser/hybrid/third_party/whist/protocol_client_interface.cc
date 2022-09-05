f1b91dee3499e400e25ab8ef27c7ac91df8bdd91
Ming <ming@whist.com>
2022-09-05 14:04:50 -0400

    remove last session_id

--- browser/hybrid/third_party/whist/protocol_client_interface.cc
+++ browser/hybrid/third_party/whist/protocol_client_interface.cc
@@ -105,7 +105,7 @@ void InitializeWhistClient() {
   path = path.Append("whist_protocol_client.log");
   whist_logs_out.open(path.AsUTF8Unsafe().c_str());
   
-  WHIST_VIRTUAL_INTERFACE_CALL(logging.set_callback, [](unsigned int level, const char* line, double session_id) {
+  WHIST_VIRTUAL_INTERFACE_CALL(logging.set_callback, [](unsigned int level, const char* line) {
     whist_logs_out << line;
   });
 }
