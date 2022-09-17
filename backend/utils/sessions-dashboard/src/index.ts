3cff5c119c2d698e71076e9dbf83350dfb270d62
Roshan Padaki <roshan@whist.com>
2022-09-17 15:04:27 -0400

    make table cleaner

--- backend/utils/sessions-dashboard/src/index.ts
+++ backend/utils/sessions-dashboard/src/index.ts
@@ -1,5 +1,30 @@
+import { DeployEnvironment } from "./config"
 import { retrieveSessions } from "./retrieve"
 import { getUser } from "./users"
+import { Console } from "node:console"
+import { Transform } from "node:stream"
+
+const cleanTable = (input: any) => {
+  // @see https://stackoverflow.com/a/67859384
+  const ts = new Transform({
+    transform(chunk, _, cb) {
+      cb(null, chunk)
+    },
+  })
+  const logger = new Console({ stdout: ts })
+  logger.table(input)
+  const table = (ts.read() || "").toString()
+  let result = ""
+  for (let row of table.split(/[\r\n]+/)) {
+    let r = row.replace(/[^┬]*┬/, "┌")
+    r = r.replace(/^├─*┼/, "├")
+    r = r.replace(/│[^│]*/, "")
+    r = r.replace(/^└─*┴/, "└")
+    r = r.replace(/'/g, " ")
+    result += `${r}\n`
+  }
+  console.log(result)
+}
 
 retrieveSessions().then(async (sessions) => {
   if (!sessions) return
@@ -13,14 +38,17 @@ retrieveSessions().then(async (sessions) => {
     })
   )
 
-  data.map((s) => {
-    console.log(
-      `${s.user.name} (${s.user.email}) is running commit ${
-        s.instance.client_sha
-      } of ${s.app.toLowerCase()} on mandelbox ${s.id} on instance ${
-        s.instance.id
-      } at ${s.instance.ip_addr} in ${s.instance.region}.`
-    )
+  console.log(`Active sessions for environment "${DeployEnvironment}":`)
+  const table = data.map((s) => {
+    return {
+      name: s.user.name,
+      email: s.user.email,
+      commit: s.instance.client_sha.substring(0, 8),
+      mandelbox: s.id,
+      instance: s.instance.id,
+      ip: s.instance.ip_addr.split("/")[0],
+      region: s.instance.region,
+    }
   })
-  // console.dir(data, { depth: null })
+  cleanTable(table)
 })
