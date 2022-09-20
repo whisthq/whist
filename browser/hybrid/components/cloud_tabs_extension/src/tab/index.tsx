a635ccbaa256ff75cdf767909eb671566aec5215
Ming Ying <ming@whist.com>
2022-09-19 21:34:38 -0400

    respect dark mode for chat

--- browser/hybrid/components/cloud_tabs_extension/src/tab/index.tsx
+++ browser/hybrid/components/cloud_tabs_extension/src/tab/index.tsx
@@ -1,11 +1,20 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { useState, useEffect } from "react"
 
 import WithInterCom from "./help"
 
 const App = () => {
+  const [dark, setDark] = useState(true)
+
+  useEffect(() => {
+    ;(chrome as any).braveTheme.getBraveThemeType((themeType: string) => {
+      setDark(themeType === "Dark")
+    })
+  }, [])
+
   return (
-    <div className="dark">
+    <div className={dark ? "dark" : ""}>
       <WithInterCom onClose={() => {}} userEmail={undefined} />
     </div>
   )
