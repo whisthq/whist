8b6198ab6e14b8aa7ef8c074df490c3545745585
Philippe Noël <phil@whist.com>
2022-09-23 16:48:02 -0400

    try w/ python_args

--- mandelboxes/build.sh
+++ mandelboxes/build.sh
@@ -63,14 +63,12 @@ mkdir -p base/build-assets/build-temp
 # Build and copy the protocol
 if [[ "$mode" == "dev" ]]; then
   cmake_build_type=Debug
-  quiet=""
 elif [[ "$mode" == "metrics" ]]; then
   cmake_build_type=Metrics
   mode=dev
-  quiet="--quiet"
+  python_args+=(--quiet) # Don't print the output of Docker build, to keep logs more readable
 else
   cmake_build_type=Release
-  quiet=""
 fi
 if [[ -n "$cmake_build_type_opt" ]]; then
   cmake_build_type=$cmake_build_type_opt
@@ -114,4 +112,4 @@ docker build --quiet -t whist/build-assets:protocol -f base/build-assets/Dockerf
 # and whist/build-assets:protocol when they determine that
 # they want to copy the protocol based on the mode.
 
-python3 ./scripts/build_mandelbox_image.py "${python_args[@]}" --mode="$mode" --beta="$beta" "$quiet"
+python3 ./scripts/build_mandelbox_image.py "${python_args[@]}" --mode="$mode" --beta="$beta"
