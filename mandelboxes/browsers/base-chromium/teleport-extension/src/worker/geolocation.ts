605c447a6305edc3d87811bd3354101132dce176
Suriya Kandaswamy <suriya@whist.com>
2022-09-29 16:17:27 -0400

    get client tab id

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/geolocation.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/geolocation.ts
@@ -11,9 +11,16 @@ const initLocationHandler = (socket: Socket) => {
 
     console.log("received geolocation-requested from tab ", sender.tab)
 
+    var clientTabId = undefined
+    if (sender.tab) {
+      const openTabs = await getOpenTabs()
+      const foundTab = find(openTabs, (t) => t.tab.id === sender.tab.id)
+      clientTabId = foundTab?.clientTabId
+    }
+
     socket.emit(
       "geolocation-requested", 
-      msg.value.params, msg.value.metaTagName, sender.tab?.id
+      msg.value.params, msg.value.metaTagName, clientTabId
     )
   })
 }
