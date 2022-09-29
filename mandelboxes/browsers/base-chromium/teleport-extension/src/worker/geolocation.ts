d6847974497f64217b5340ba41566ccf81ff19bb
Suriya Kandaswamy <suriya@whist.com>
2022-09-29 14:17:09 -0400

    fix error

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/geolocation.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/geolocation.ts
@@ -6,7 +6,7 @@ import { Socket } from "socket.io-client"
 
 const initLocationHandler = (socket: Socket) => {
   // Listen for geolocation request event and send payload to client extension 
-  chrome.runtime.onMessage.addListener((msg: ContentScriptMessage, sender: MessageSender) => {
+  chrome.runtime.onMessage.addListener((msg: ContentScriptMessage, sender: chrome.runtime.MessageSender) => {
     if (msg.type !== ContentScriptMessageType.GEOLOCATION_REQUEST) return
 
     socket.emit(
