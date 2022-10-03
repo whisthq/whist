d4c58766666a232570c72c66544a625a37d60c35
Suriya Kandaswamy <suriya@whist.com>
2022-10-03 15:20:33 -0400

    test navigator from content script

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/language.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/language.ts
@@ -1,7 +1,24 @@
 import { injectResourceIntoDOM } from "@app/utils/dom"
 
 const initLanguageSpoofer = () => {
-	injectResourceIntoDOM(document "js/language.ts")
+	injectResourceIntoDOM(document, "js/language.ts")
+
+	console.log(navigator)
+	Object.defineProperty(navigator, 'language', {value: "boi"})
+	console.log("updated ", navigator)
+
+	// // Listen for LANGUAGE_UPDATE message and update corresponding meta tag
+	// chrome.runtime.onMessage.addListener(async (msg: ContentScriptMessage) => {
+	// 	if (msg.type !== ContentScriptMessageType.LANGUAGE_UPDATE) return
+
+	// 	const metaLanguage = document.documentElement.querySelector(
+	// 		`meta[name="browser-language"]`
+	// 	) as HTMLMetaElement
+
+	// 	if (metaLanguage) {
+	// 		metaLanguage.content = JSON.stringify(msg.value)
+	// 	}
+	// })
 }
 
 export { initLanguageSpoofer }
