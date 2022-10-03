005d859e1bac580ad96a67abf55afe5432d9e9ea
Suriya Kandaswamy <suriya@whist.com>
2022-10-03 15:23:30 -0400

    fix errors

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/language.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/language.ts
@@ -3,21 +3,25 @@ import {
   ContentScriptMessageType,
 } from "@app/constants/ipc"
 
+import { Socket } from "socket.io-client"
+
 const initLanguageHandler = (socket: Socket) => {
-	socket.on("language-changed", ([language, languages]) => {
+	socket.on("language-changed", ([language, languages]: [string, [string]]) => {
 		// When language has changed, tell all tabs about the
 		//     updated languages.
 		chrome.tabs.query({}, (tabs) => {
 			tabs.forEach((tab) => {
-				chrome.tabs.sendMessage(
-					tab.id,
-					{
-						type: ContentScriptMessageType.LANGUAGE_UPDATE,
-						value: {
-							language, languages
-						}
-					} as ContentScriptMessage,
-				)
+				if (tab.id) {
+					chrome.tabs.sendMessage(
+						tab.id,
+						{
+							type: ContentScriptMessageType.LANGUAGE_UPDATE,
+							value: {
+								language, languages
+							}
+						} as ContentScriptMessage,
+					)
+				}
 			})
 		})
 	})
