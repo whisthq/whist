a387519806c4aa42ba2c040969c6d5b35ff7d5ba
Serina Hu <serina_hu@college.harvard.edu>
2022-10-05 18:43:26 -0400

    disable page reset

--- protocol/whist/core/mimalloc_hook.cpp
+++ protocol/whist/core/mimalloc_hook.cpp
@@ -261,7 +261,7 @@ static void mlock_statics() {
 extern malloc_zone_t* malloc_default_purgeable_zone(void) __attribute__((weak_import));
 
 void init_whist_malloc_hook() {
-    mi_option_set(mi_option_reset_delay, 1000);
+    mi_option_set(mi_option_page_reset, 0);
     LOG_INFO("mlocking statics");
     mlock_statics();
     LOG_INFO("Initializing custom malloc");
