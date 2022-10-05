46da0d79f5a30dd366c73ca92d0e9246edb667d5
Suriya Kandaswamy <suriya@whist.com>
2022-10-05 12:45:11 -0400

    remove development webpack mode

--- browser/hybrid/components/cloud_tabs_extension/scripts/build.js
+++ browser/hybrid/components/cloud_tabs_extension/scripts/build.js
@@ -27,7 +27,7 @@ const build = (argv) => {
   helpers.buildTailwind()
 
   execCommand(
-    `webpack --config webpack.config.js --env webpackMode=development ${
+    `webpack --config webpack.config.js ${
       argv.output_dir ? `--env targetDir=${argv.output_dir}` : ""
     }`
   )
