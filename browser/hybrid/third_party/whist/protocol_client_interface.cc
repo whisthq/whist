c7735c99f0c60377ca7f3d134357aba44d7e9f18
Roshan Padaki <roshan@whist.com>
2022-10-07 15:35:29 -0400

    cleanup

--- browser/hybrid/third_party/whist/protocol_client_interface.cc
+++ browser/hybrid/third_party/whist/protocol_client_interface.cc
@@ -92,6 +92,15 @@ static base::NativeLibrary LoadWhistClientLibrary() {
 }
 static std::ofstream whist_logs_out;
 
+static void DestroyWhistClient() {
+  base::AutoLock whist_client_auto_lock(whist_virtual_interface_lock);
+  if (whist_virtual_interface == NULL) return;
+  WHIST_VIRTUAL_INTERFACE_CALL(lifecycle.destroy);
+  whist_virtual_interface = NULL;
+  whist_logs_out.close();
+  base::UnloadNativeLibrary(whist_client_library);
+}
+
 void InitializeWhistClient() {
   base::AutoLock whist_client_auto_lock(whist_virtual_interface_lock);
 
@@ -128,19 +137,10 @@ void InitializeWhistClient() {
   WHIST_VIRTUAL_INTERFACE_CALL(logging.set_callback, [](unsigned int level, const char* line) {
     whist_logs_out << line;
   });
+
+  // Note: This atexit doesn't seem to trigger because of the way Chromium
+  // exit()s renderer processes instead of allowing them to gracefully exit.
+  // We register it anyways for the sake of completeness.
+  atexit(DestroyWhistClient);
 }
 
-void DestroyWhistClient() {
-  base::AutoLock whist_client_auto_lock(whist_virtual_interface_lock);
-
-  if (whist_virtual_interface == NULL) {
-    return;
-  }
-
-  WHIST_VIRTUAL_INTERFACE_CALL(lifecycle.destroy);
-  whist_virtual_interface = NULL;
-
-  whist_logs_out.close();
-
-  base::UnloadNativeLibrary(whist_client_library);
-}
