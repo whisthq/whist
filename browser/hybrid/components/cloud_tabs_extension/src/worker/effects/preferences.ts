c2110319e1bbc569ac8391e575a813563796ab1d
Suriya Kandaswamy <suriya@whist.com>
2022-10-13 16:55:04 -0400

    change chrome to chrome as any for runtime

--- browser/hybrid/components/cloud_tabs_extension/src/worker/effects/preferences.ts
+++ browser/hybrid/components/cloud_tabs_extension/src/worker/effects/preferences.ts
@@ -7,17 +7,17 @@ import { socket, socketConnected } from "@app/worker/events/socketio"
 import { whistState } from "@app/worker/utils/state"
 
 socketConnected.subscribe(async (socket: Socket) => {
-	const usingDarkMode = new Promise((resolve) => {
-	    ;(chrome as any).braveTheme.getBraveThemeType((themeType: string) => {
-	      resolve(themeType === "Dark")
-	    })
-	})
+    const usingDarkMode = new Promise((resolve) => {
+        ;(chrome as any).braveTheme.getBraveThemeType((themeType: string) => {
+          resolve(themeType === "Dark")
+        })
+    })
 
-	socket.emit("init-preferences", await usingDarkMode)
+    socket.emit("init-preferences", await usingDarkMode)
 })
 
 darkModeChanged
-	.pipe(withLatestFrom(socket))
-	.subscribe(([isDarkMode, socket]: [boolean, Socket]) => {
-		socket.emit("darkmode-changed", isDarkMode)
-	})
+    .pipe(withLatestFrom(socket))
+    .subscribe(([isDarkMode, socket]: [boolean, Socket]) => {
+        socket.emit("darkmode-changed", isDarkMode)
+    })
