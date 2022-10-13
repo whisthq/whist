67d99abe55d7331a737da1eec040290d1030322f
Suriya Kandaswamy <suriya@whist.com>
2022-10-13 16:55:04 -0400

    change chrome to chrome as any for runtime

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/preferences.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/preferences.ts
@@ -1,18 +1,18 @@
 import { Socket } from "socket.io-client"
 
 const initPreferencesInitHandler = (socket: Socket) => {
-	socket.on("init-preferences", ([isDarkMode]: [boolean]) => {
-		chrome.runtime.setDarkMode(isDarkMode, () => socket.emit("darkmode-initialized"))
-	})
+    socket.on("init-preferences", ([isDarkMode]: [boolean]) => {
+        ;(chrome as any).runtime.setDarkMode(isDarkMode, () => socket.emit("darkmode-initialized"))
+    })
 }
 
 const initDarkModeChangeHandler = (socket: Socket) => {
-	socket.on("darkmode-changed", ([isDarkMode]: [boolean]) => {
-		chrome.runtime.setDarkMode(isDarkMode)
-	})
+    socket.on("darkmode-changed", ([isDarkMode]: [boolean]) => {
+        ;(chrome as any).runtime.setDarkMode(isDarkMode)
+    })
 }
 
 export {
-	initPreferencesInitHandler,
-	initDarkModeChangeHandler,
+    initPreferencesInitHandler,
+    initDarkModeChangeHandler,
 }
