8ebd9ae7c9133279f75ad9f13053f9d38bd8d3da
Suriya Kandaswamy <suriya@whist.com>
2022-10-14 16:55:05 -0400

    wait until keyboard rate file exists

--- mandelboxes/base/input/handle-keyboard-repeat-rate-update.sh
+++ mandelboxes/base/input/handle-keyboard-repeat-rate-update.sh
@@ -1,8 +1,12 @@
-KEYBOARD_REPEAT_RATE_UPDATE_TRIGGER_FILE="/home/whist/.teleport/keyboard-repeat-rate-update"
+TELEPORT_DIRECTORY=/home/whist/.teleport
+KEYBOARD_REPEAT_RATE_UPDATE_TRIGGER_FILE=$TELEPORT_DIRECTORY/keyboard-repeat-rate-update
 
 while true; do
-	if -f $KEYBOARD_REPEAT_RATE_UPDATE_TRIGGER_FILE; then
+	# If file doesn't exist, keep polling until it exists.
+	if [ -f $KEYBOARD_REPEAT_RATE_UPDATE_TRIGGER_FILE ]; then
 		xset r rate $(head -n 1 filename)
+	else
+		sleep 20
 	fi
-	inotifywait -e modify -e create $KEYBOARD_REPEAT_RATE_UPDATE_TRIGGER_FILE
+	inotifywait -e modify $KEYBOARD_REPEAT_RATE_UPDATE_TRIGGER_FILE
 done
