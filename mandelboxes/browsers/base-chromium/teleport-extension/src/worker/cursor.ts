eec7207e93dd95d2408f8b244f70f465724afd70
Suriya Kandaswamy <suriya@whist.com>
2022-10-14 15:32:26 -0400

    fix errors

--- mandelboxes/browsers/base-chromium/teleport-extension/src/worker/cursor.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/cursor.ts
@@ -7,7 +7,7 @@ import {
 
 import { Socket } from "socket.io-client"
 
-const initCursorLockHandler = (socket: Socket, nativeHostPort: chrome.runtime.Port) => {
+const initCursorLockHandler = (nativeHostPort: chrome.runtime.Port) => {
   chrome.runtime.onMessage.addListener((msg: ContentScriptMessage) => {
     if (msg.type !== ContentScriptMessageType.POINTER_LOCK) return
 
