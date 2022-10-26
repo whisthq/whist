2ac820ea4b556c603f67eb286f104077a8e92f38
Suriya Kandaswamy <suriya@whist.com>
2022-10-26 14:17:31 -0400

    remove unnecessary import

--- browser/hybrid/components/cloud_tabs_extension/src/worker/events/cookies.ts
+++ browser/hybrid/components/cloud_tabs_extension/src/worker/events/cookies.ts
@@ -1,5 +1,5 @@
 import { fromEventPattern, fromEvent } from "rxjs"
-import { filter, map, switchMap, share } from "rxjs/operators"
+import { filter, map, switchMap } from "rxjs/operators"
 import { Socket } from "socket.io-client"
 import find from "lodash.find"
 import isEqual from "lodash.isequal"
