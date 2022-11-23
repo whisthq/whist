9470fb12d48de16cbb471499c1778bfd78d2642b
Roshan Padaki <roshan@whist.com>
2022-11-23 15:59:58 -0500

    add updatePolicy extension API

--- browser/hybrid/browser/extensions/api/whist_api.cc
+++ browser/hybrid/browser/extensions/api/whist_api.cc
@@ -12,20 +12,57 @@
 
 #include "base/command_line.h"
 #include "base/environment.h"
+#include "base/files/file_util.h"
+#include "base/path_service.h"
+#include "base/threading/thread_restrictions.h"
 #include "brave/common/pref_names.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_paths.h"
+#include "components/policy/core/common/policy_service.h"
 #include "components/prefs/pref_service.h"
 #include "extensions/browser/event_router.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
+
 #include "whist/browser/hybrid/common/extensions/api/whist.h"
 
 #include "whist_api_native.h"
 
 namespace extensions {
 namespace api {
+
+ExtensionFunction::ResponseAction WhistUpdatePoliciesFunction::Run() {
+  std::unique_ptr<whist::UpdatePolicies::Params> params(
+      whist::UpdatePolicies::Params::Create(args()));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+
+  // We don't call this often, and only from the extension, so it is okay
+  // to block. Especially because it's very fast.
+  base::ScopedAllowBlocking allow_blocking;
+
+  base::FilePath config_dir_path;
+  base::PathService::Get(chrome::DIR_POLICY_FILES, &config_dir_path);
+  // Create "mandatory" directory if it doesn't exist
+  base::FilePath mandatory_dir_path = config_dir_path.Append("mandatory");
+  if (!base::PathExists(mandatory_dir_path)) {
+    base::CreateDirectory(mandatory_dir_path);
+  }
+  base::FilePath policy_file_path = mandatory_dir_path.Append("whist_policy.json");
+  base::WriteFile(policy_file_path, params->policy_json.c_str(), params->policy_json.length());
+
+  // TODO: Validate policy JSON before writing it.
+
+  // Chromium automatically refreshes policies quite often, but
+  // it's probably smart to force a refresh here.
+  ProfileManager::GetActiveUserProfile()
+    ->GetProfilePolicyConnector()
+    ->policy_service()
+    ->RefreshPolicies(base::NullCallback());
+  return RespondNow(NoArguments());
+}
 
 ExtensionFunction::ResponseAction WhistBroadcastWhistMessageFunction::Run() {
   std::unique_ptr<whist::BroadcastWhistMessage::Params> params(
