083f30b1713d09f6feba39b7c50ac49c6974e686
Serina <ubuntu@ip-172-32-161-184.ec2.internal>
2022-11-28 22:00:30 +0000

    add notification handler to send stuff from server to client

---
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/worker/notifications.ts
@@ -0,0 +1,18 @@
+import {
+  ContentScriptMessage,
+  ContentScriptMessageType,
+} from "@app/constants/ipc"
+import { Socket } from "socket.io-client"
+
+const initNotificationHandler = (socket: Socket) => {
+    // Listen for notification creation messages (from the server)
+    // and send them along to the client
+    chrome.runtime.onMessage.addListener(async (msg: ContentScriptMessage, sender: chrome.runtime.MessageSender) => {
+        if (msg.type !== ContentScriptMessageType.SERVER_NOTIFICATION) return
+        socket.emit("server-notification", msg.value.title, msg.value.opt)
+    })
+    // Listen for client messages about notification clicks, dismisses, etc
+    // and supposedly send another contentscriptmessage
+}
+
+export { initNotificationHandler }
