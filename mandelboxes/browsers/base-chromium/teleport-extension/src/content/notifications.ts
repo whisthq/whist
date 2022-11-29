e35244776c66a0b46eca7cc89c11b9187af16bda
Serina <ubuntu@ip-172-32-161-184.ec2.internal>
2022-11-29 02:55:31 +0000

    change to using const instead of function?

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/notifications.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/notifications.ts
@@ -1,57 +1,46 @@
 import {
-  ContentScriptMessage,
-  ContentScriptMessageType,
+    ContentScriptMessage,
+    ContentScriptMessageType,
 } from "@app/constants/ipc"
 import { injectResourceIntoDOM } from "@app/utils/dom"
 
 const initNotificationSender = () => {
-    injectResourceIntoDOM(document, "js/notifications.js");
+    injectResourceIntoDOM(document, "js/notifications.js")
 
     // Intercept server-side notifications and send them to the client
     // currently copied from https://github.com/nativefier/nativefier/blob/master/app/src/preload.ts
     // TODO: also send over permissions? is this needed? or the client has to communicate permissions
-    function setNotificationCallback(
-        createCallback: {
-            (title: string, opt?: NotificationOptions | undefined): void;
-        },
-    ): void {
-        console.log("setNotificationCallback called");
+    const setNotificationCallback = (
+        createCallback: ((title: string, opt: NotificationOptions | undefined) => void)
+    ) => {
+        console.log("setNotificationCallback called!");
+        const oldNotify = window.Notification;
         class newNotify extends Notification {
             constructor(title: string, options?: NotificationOptions | undefined) {
+                console.log("window notification created", title, options);
                 createCallback(title, options);
                 super(title, options);
+                // return this;
             }
         }
 
-        /*
-        const newNotify = function (
-            title: string,
-            opt: NotificationOptions,
-        ): Notification {
-            createCallback(title, opt);
-            return new oldNotify(title, opt);
-        };
-        newNotify.requestPermission = oldNotify.requestPermission.bind(oldNotify);
-        Object.defineProperty(newNotify.prototype, 'permission', {
-            get: () => oldNotify.permission,
-        });
-        */
         window.Notification = newNotify;
     }
 
-    function notificationCallback(
+    const notificationCallback = (
         title: string,
-        opt?: NotificationOptions | undefined,
-    ): void {
-        console.log({title, opt});
+        opt: NotificationOptions | undefined,
+    ) => {
+        console.log("Notification callback called");
+        console.log(title, opt);
         chrome.runtime.sendMessage(<ContentScriptMessage>{
             type: ContentScriptMessageType.SERVER_NOTIFICATION,
             value: {
                 title: title,
                 opt: opt
             },
-        })
-    }
+        });
+    };
     setNotificationCallback(notificationCallback);
 
     // TODO: listen for client-side interactions with the notification and send them back?
