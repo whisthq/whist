c7b532aa3b4a4c0f3b86597f440f88f712c394f2
Serina <ubuntu@ip-172-32-161-184.ec2.internal>
2022-11-29 04:30:54 +0000

    switch to using Proxy

--- mandelboxes/browsers/base-chromium/teleport-extension/src/content/notifications.ts
+++ mandelboxes/browsers/base-chromium/teleport-extension/src/content/notifications.ts
@@ -7,40 +7,41 @@ import { injectResourceIntoDOM } from "@app/utils/dom"
 const initNotificationSender = () => {
     injectResourceIntoDOM(document, "js/notifications.js")
 
-    // Intercept server-side notifications and send them to the client
-    // currently copied from https://github.com/nativefier/nativefier/blob/master/app/src/preload.ts
-    // TODO: also send over permissions? is this needed? or the client has to communicate permissions
-    const setNotificationCallback = (
-        createCallback: ((title: string, opt: NotificationOptions | undefined) => void)
-    ) => {
-        console.log("setNotificationCallback called!");
-        const oldNotify = window.Notification;
-        class newNotify extends Notification {
-            constructor(title: string, options?: NotificationOptions | undefined) {
-                console.log("window notification created", title, options);
-                createCallback(title, options);
-                super(title, options);
-                // return this;
-            }
-        }
-
-        window.Notification = newNotify;
-    }
-
     const notificationCallback = (
         title: string,
         opt: NotificationOptions | undefined,
     ) => {
-        console.log("Notification callback called");
-        console.log(title, opt);
+        console.log("Notification callback called", title, opt);
         chrome.runtime.sendMessage(<ContentScriptMessage>{
             type: ContentScriptMessageType.SERVER_NOTIFICATION,
             value: {
                 title: title,
                 opt: opt
-            },
+            }
         });
-    };
+    }
+
+    // Intercept server-side notifications and send them to the client
+    // currently copied from https://github.com/nativefier/nativefier/blob/master/app/src/preload.ts
+    // TODO: also send over permissions? is this needed? or the client has to communicate permissions
+    const setNotificationCallback = (
+        // createCallback: ((title: string, opt: NotificationOptions | undefined) => void)
+        createCallback: any
+    ) => {
+        console.log("setNotificationCallback called!");
+        const oldNotify = window.Notification;
+        console.log("oldNotify", oldNotify);
+        const newNotify = new Proxy(Notification, {
+            construct : (target: any, args: any[]) => {
+                createCallback(args);
+                return new target(args);
+            }
+        });
+        console.log("newNotify", newNotify);
+
+        window.Notification = newNotify;
+        console.log("supposedly set window.Notification", window.Notification);
+    }
     setNotificationCallback(notificationCallback);
 
     // TODO: listen for client-side interactions with the notification and send them back?
